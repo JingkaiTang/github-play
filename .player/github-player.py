64e6febad1669fc9bd7c686476526fbaa06dd3de
Jingkai Tang <jingkaitang@gmail.com>
2014-01-04 12:28:11 +0800

    add: sys util

--- .player/github-player.py
+++ .player/github-player.py
@@ -19,14 +19,6 @@ conjs = rc_words('conjs')
 preps = rc_words('preps')
 
 
-def cmd_wrap(*args):
-    return ' '.join(args)
-
-
-def str_wrap(s):
-    return '"' + s + '"'
-
-
 def gen_code():
     return r'''
 #! /usr/bin/env python
@@ -37,21 +29,6 @@ if __name__ == '__main__':
 
 def write2file(raw_code, file_name):
     open(file_name, 'w').write(raw_code)
-
-
-def git_add(files):
-    cmd = cmd_wrap('git add', ' '.join(files))
-    os.system(cmd)
-
-
-def git_commit(msg):
-    cmd = cmd_wrap('git commit -m', str_wrap(msg))
-    os.system(cmd)
-
-
-def git(files, msg):
-    git_add(files)
-    git_commit(msg)
 
 
 def randmsg(files):
---
+++ .player/sys_util.py
@@ -0,0 +1,36 @@
+#! /usr/bin/env python
+import os
+
+
+def cmd_wrap(*args):
+    return ' '.join(args)
+
+
+def str_wrap(s):
+    return '"' + s + '"'
+
+
+# git
+def git_init():
+    cmd = 'git init'
+    os.system(cmd)
+
+def git_add(files):
+    cmd = cmd_wrap('git add', ' '.join(files))
+    os.system(cmd)
+
+
+def git_commit(msg):
+    cmd = cmd_wrap('git commit -m', str_wrap(msg))
+    os.system(cmd)
+
+
+def git(files, msg):
+    git_add(files)
+    git_commit(msg)
+
+
+# date
+def mod_date(dt):
+    cmd = cmd_wrap('sudo date -s', str_wrap(dt))
+    os.system(cmd)
