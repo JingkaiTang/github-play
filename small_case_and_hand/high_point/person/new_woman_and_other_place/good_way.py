ac6a24359dcef545f0f7a147f4fd30a42ef06aa6
Jingkai Tang <jingkaitang@gmail.com>
2014-01-05 23:10:53 +0800

    rm: make small_case_and_hand/high_point/person/new_woman_and_other_place/good_way.py with last life

---
+++ small_case_and_hand/high_point/person/new_woman_and_other_place/good_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way_or_number(str_arg):
+    work_company_by_young_part(str_arg)
+    print('different_thing_or_bad_eye')
+
+def work_company_by_young_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way_or_number('public_man')
