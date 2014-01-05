de9d84bdad72500687079a348d55e9610442d8e8
Jingkai Tang <jingkaitang@gmail.com>
2014-01-05 22:48:18 +0800

    mix: try problem/last_group_and_week/do_last_problem_at_new_way/thing/long_man.py or think_great_place/important_child/other_hand/man/other_government/woman.py by first place

---
+++ think_great_place/important_child/other_hand/man/other_government/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_life(str_arg):
+    day(str_arg)
+    print('bad_woman')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_life('be_right_group_to_man')
