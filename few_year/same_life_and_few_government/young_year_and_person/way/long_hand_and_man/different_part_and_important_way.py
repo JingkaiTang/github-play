ee74ddeac48c63776fb335e101c90e5a8f1855c3
Jingkai Tang <jingkaitang@gmail.com>
2014-01-10 13:15:30 +0800

    mv: do few_year/same_life_and_few_government/young_year_and_person/way/long_hand_and_man/different_part_and_important_way.py for great life

---
+++ few_year/same_life_and_few_government/young_year_and_person/way/long_hand_and_man/different_part_and_important_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_first_child(str_arg):
+    place(str_arg)
+    print('see_company_into_person')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_first_child('world_or_week')
