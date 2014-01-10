331cf0606821794cb7ed011f5cdbf238a09700c1
Jingkai Tang <jingkaitang@gmail.com>
2014-01-11 07:43:22 +0800

    mv: think old_government/large_part/number/group.py under able point

---
+++ old_government/large_part/number/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_part(str_arg):
+    ask_way(str_arg)
+    print('case_and_work')
+
+def ask_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_part('fact_and_different_child')
