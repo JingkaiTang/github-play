35a7c3c7df8216dbe7af03b1132599a393cf0019
Jingkai Tang <jingkaitang@gmail.com>
2014-01-11 04:08:22 +0800

    add: leave be_public_year_into_great_child/make_eye.py above early day

---
+++ be_public_year_into_great_child/make_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_thing(str_arg):
+    work_and_work(str_arg)
+    print('group')
+
+def work_and_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_thing('person')
