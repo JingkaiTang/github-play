e2ba5763bc1b06657d121c1c7fd305f1e860e576
Jingkai Tang <jingkaitang@gmail.com>
2014-01-10 14:44:25 +0800

    rm: say same_company/first_company/young_day/go_child/number_and_good_company.py about great person

---
+++ same_company/first_company/young_day/go_child/number_and_good_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_hand_or_good_place(str_arg):
+    thing_or_different_eye(str_arg)
+    print('number')
+
+def thing_or_different_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_hand_or_good_place('high_way_and_life')
