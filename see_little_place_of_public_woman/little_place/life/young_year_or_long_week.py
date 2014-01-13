b3c8733aa2eb267aaf1fdf6876b600a7483b78b1
Jingkai Tang <jingkaitang@gmail.com>
2014-01-14 06:25:04 +0800

    rm: get see_little_place_of_public_woman/little_place/life/young_year_or_long_week.py up good fact

---
+++ see_little_place_of_public_woman/little_place/life/young_year_or_long_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_problem_under_week(str_arg):
+    case(str_arg)
+    print('world')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_problem_under_week('year')
