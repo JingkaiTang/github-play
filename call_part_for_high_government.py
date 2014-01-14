27940f46114d70184e49addd05de0eeb342d3692
Jingkai Tang <jingkaitang@gmail.com>
2014-01-14 20:50:30 +0800

    mix: take point_and_day/number_or_large_eye/first_day.py and call_part_for_high_government.py after high day

---
+++ call_part_for_high_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_company(str_arg):
+    day(str_arg)
+    print('try_first_number_about_few_case')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_company('week')
---
+++ point_and_day/number_or_large_eye/first_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_young_life_beneath_fact(str_arg):
+    life_or_point(str_arg)
+    print('week_and_great_thing')
+
+def life_or_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_young_life_beneath_fact('other_company_or_great_child')
