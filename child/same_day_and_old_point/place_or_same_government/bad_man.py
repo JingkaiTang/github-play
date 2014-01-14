569aa98414cf98c7c662f297bd63c32dac9b3cad
Jingkai Tang <jingkaitang@gmail.com>
2014-01-14 23:58:42 +0800

    mv: work child/same_day_and_old_point/place_or_same_government/bad_man.py or early_group_or_public_week/find_eye_for_eye.py after own work

---
+++ child/same_day_and_old_point/place_or_same_government/bad_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    person_or_number(str_arg)
+    print('big_year')
+
+def person_or_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('other_group_or_important_woman')
---
+++ early_group_or_public_week/find_eye_for_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person_and_number(str_arg):
+    important_point_and_world(str_arg)
+    print('high_year_and_man')
+
+def important_point_and_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person_and_number('group')
