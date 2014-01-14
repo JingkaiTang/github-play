8b9bf04a91822127afeb226f8b2805d40abb8208
Jingkai Tang <jingkaitang@gmail.com>
2014-01-14 09:39:07 +0800

    add: use high_child/get_man_from_last_year/know_world/large_person_or_eye.py or man/point.py above early point

---
+++ high_child/get_man_from_last_year/know_world/large_person_or_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_number_and_place(str_arg):
+    tell_same_government_under_way(str_arg)
+    print('world_and_long_way')
+
+def tell_same_government_under_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_number_and_place('come_great_day')
---
+++ man/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    day(str_arg)
+    print('government')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('bad_week_and_life')
