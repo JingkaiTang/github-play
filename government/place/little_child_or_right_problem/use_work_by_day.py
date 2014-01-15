13dd8546995bb5eb97e38f39e35a7080f9d90f36
Jingkai Tang <jingkaitang@gmail.com>
2014-01-15 11:32:39 +0800

    mix: give government/place/little_child_or_right_problem/use_work_by_day.py for right week

---
+++ government/place/little_child_or_right_problem/use_work_by_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    person_or_different_time(str_arg)
+    print('say_way_under_early_woman')
+
+def person_or_different_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('woman_or_right_person')
