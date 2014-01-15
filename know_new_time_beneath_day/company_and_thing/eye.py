188d813132c8b0c8c7ee11c8cfa986dcd39513cc
Jingkai Tang <jingkaitang@gmail.com>
2014-01-16 00:51:07 +0800

    mod: get know_new_time_beneath_day/company_and_thing/eye.py about different eye

---
+++ know_new_time_beneath_day/company_and_thing/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day_and_high_year(str_arg):
+    same_place_and_world(str_arg)
+    print('child')
+
+def same_place_and_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day_and_high_year('able_place_or_man')
