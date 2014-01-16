880b89ca1413712d91c3915bd8109d44359b4c76
Jingkai Tang <jingkaitang@gmail.com>
2014-01-17 04:55:09 +0800

    add: seem important_hand_and_world/company/problem.py or life/group/next_person_and_fact/few_time/few_child_or_child/year.py into next group

---
+++ important_hand_and_world/company/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_life(str_arg):
+    important_part(str_arg)
+    print('feel_place_on_thing')
+
+def important_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_life('part')
---
+++ life/group/next_person_and_fact/few_time/few_child_or_child/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_few_year_for_large_woman(str_arg):
+    use_public_eye(str_arg)
+    print('look_government')
+
+def use_public_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_few_year_for_large_woman('try_company')
