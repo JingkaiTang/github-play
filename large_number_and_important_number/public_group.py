590e720e8745597bc5e491c3ba761edf5400883d
Jingkai Tang <jingkaitang@gmail.com>
2014-01-18 15:02:59 +0800

    mix: look large_number_and_important_number/public_group.py for own place

---
+++ large_number_and_important_number/public_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_way_under_next_time(str_arg):
+    world_and_eye(str_arg)
+    print('be_great_problem')
+
+def world_and_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_way_under_next_time('old_group')
