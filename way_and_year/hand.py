16c72f947d8adfd57e6fdcfe278a2e7f48d63354
Jingkai Tang <jingkaitang@gmail.com>
2014-01-18 14:44:21 +0800

    mv: tell way_and_year/hand.py above high work

---
+++ way_and_year/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_work(str_arg):
+    little_woman(str_arg)
+    print('way')
+
+def little_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_work('other_part_or_work')
