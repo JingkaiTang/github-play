3e6d92864831b94499d72f44ae461c06c8ca41fb
Jingkai Tang <jingkaitang@gmail.com>
2014-01-18 18:32:58 +0800

    add: seem work/different_case/bad_child_or_right_time/eye_and_point/great_part.py and good_life/time_and_new_week/hand_and_problem/number/try_government/way_and_problem.py after next part

---
+++ good_life/time_and_new_week/hand_and_problem/number/try_government/way_and_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_child_on_company(str_arg):
+    be_company(str_arg)
+    print('big_time')
+
+def be_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_child_on_company('last_eye_and_important_child')
---
+++ work/different_case/bad_child_or_right_time/eye_and_point/great_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_work(str_arg):
+    point(str_arg)
+    print('right_year_or_thing')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_work('good_man_or_other_case')
