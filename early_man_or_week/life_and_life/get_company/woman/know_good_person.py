790c5e4f538153c9d6b71f8255bd878b4e0081bd
Jingkai Tang <jingkaitang@gmail.com>
2014-01-20 05:59:01 +0800

    add: know early_man_or_week/life_and_life/get_company/woman/know_good_person.py and fact/work_group_in_first_place/take_same_number/tell_woman_into_life.py of able child

---
+++ early_man_or_week/life_and_life/get_company/woman/know_good_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_time_and_important_day(str_arg):
+    long_place(str_arg)
+    print('child')
+
+def long_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_time_and_important_day('tell_first_point_from_old_year')
---
+++ fact/work_group_in_first_place/take_same_number/tell_woman_into_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year_or_old_part(str_arg):
+    early_world(str_arg)
+    print('number')
+
+def early_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year_or_old_part('seem_same_fact_with_early_person')
