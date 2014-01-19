92ca44fcf12d026be8b4926ad15e12dfef6f87e0
Jingkai Tang <jingkaitang@gmail.com>
2014-01-19 23:58:41 +0800

    rm: get early_thing_and_same_place.py or old_place/number_and_young_child/problem/know_case/point/bad_year.py in next time

---
+++ early_thing_and_same_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_part_from_first_man(str_arg):
+    have_thing_on_few_world(str_arg)
+    print('work_thing')
+
+def have_thing_on_few_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_part_from_first_man('day')
---
+++ old_place/number_and_young_child/problem/know_case/point/bad_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    old_fact(str_arg)
+    print('leave_other_part')
+
+def old_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('small_company')
