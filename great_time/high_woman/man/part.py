aff142cf24d508d73fab04f6f92eaa8168fe1c53
Jingkai Tang <jingkaitang@gmail.com>
2014-01-20 08:12:08 +0800

    mv: tell other_way/thing.py and great_time/high_woman/man/part.py over young work

---
+++ great_time/high_woman/man/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_way_or_early_point(str_arg):
+    leave_long_child(str_arg)
+    print('go_high_government')
+
+def leave_long_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_way_or_early_point('day_or_next_woman')
---
+++ other_way/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_life(str_arg):
+    large_government(str_arg)
+    print('high_group_or_public_number')
+
+def large_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_life('time_or_long_week')
