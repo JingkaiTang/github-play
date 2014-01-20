bcc26326fe990e2a46904c9394981fd1fb6434b2
Jingkai Tang <jingkaitang@gmail.com>
2014-01-20 12:50:20 +0800

    mod: make right_person/look_man.py over young hand

---
+++ right_person/look_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_eye(str_arg):
+    be_world(str_arg)
+    print('new_time')
+
+def be_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_eye('able_woman')
