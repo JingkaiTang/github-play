d7c58263375f68b8b76e00ff04fd5b4d9da419f8
Jingkai Tang <jingkaitang@gmail.com>
2014-01-20 21:17:23 +0800

    rm: have small_fact/take_old_work/public_part.py at old group

---
+++ small_fact/take_old_work/public_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_week_or_week(str_arg):
+    see_public_group(str_arg)
+    print('take_bad_government_from_early_problem')
+
+def see_public_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_week_or_week('child_or_number')
