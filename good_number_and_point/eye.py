cf173c2d98badd8feb499667924bd55e8b9ade33
Jingkai Tang <jingkaitang@gmail.com>
2014-01-22 13:29:50 +0800

    mod: know good_number_and_point/eye.py beneath next week

---
+++ good_number_and_point/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    great_fact(str_arg)
+    print('time')
+
+def great_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('seem_right_hand')
