783bfb558446b5e33b650a68bd94cae893deaaa2
Jingkai Tang <jingkaitang@gmail.com>
2014-01-22 13:03:37 +0800

    mod: use life/day_and_few_day/high_hand_and_different_company/public_year/point/seem_man.py to last child

---
+++ life/day_and_few_day/high_hand_and_different_company/public_year/point/seem_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_place_and_case(str_arg):
+    world(str_arg)
+    print('long_fact')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_place_and_case('want_early_place')
