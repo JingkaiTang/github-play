fc7684824429d4197794a7489513a16d0eb24a0e
Jingkai Tang <jingkaitang@gmail.com>
2014-01-22 19:03:08 +0800

    rm: use thing/high_life_or_young_woman/give_few_point/find_week/world_or_number.py or important_woman/early_time.py under long eye

---
+++ important_woman/early_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_place_or_old_week(str_arg):
+    thing_and_bad_work(str_arg)
+    print('place')
+
+def thing_and_bad_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_place_or_old_week('leave_thing_after_early_group')
