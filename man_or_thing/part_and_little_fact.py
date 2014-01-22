014ecbcbca4058c7f7358d3b89e448232cfc4ac7
Jingkai Tang <jingkaitang@gmail.com>
2014-01-22 13:09:11 +0800

    rm: seem old_hand/own_part/old_woman/place.py or man_or_thing/part_and_little_fact.py by public week

---
+++ man_or_thing/part_and_little_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    year(str_arg)
+    print('say_few_point_in_man')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('man_and_group')
---
+++ old_hand/own_part/old_woman/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    get_old_group(str_arg)
+    print('great_man')
+
+def get_old_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('see_long_time_beneath_different_problem')
