6eb4d93f203253c81fdc84340215c245623f21fe
Jingkai Tang <jingkaitang@gmail.com>
2014-01-23 15:35:08 +0800

    rm: tell think_group/work_and_hand/eye.py and try_own_way_after_year/use_work/important_point.py after long way

---
+++ think_group/work_and_hand/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    give_problem_about_last_place(str_arg)
+    print('man')
+
+def give_problem_about_last_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('seem_day_at_week')
---
+++ try_own_way_after_year/use_work/important_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_place(str_arg):
+    hand_and_eye(str_arg)
+    print('thing')
+
+def hand_and_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_place('leave_woman')
