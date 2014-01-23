7f9cae854772a542c6b2c02dd620bc29e940862e
Jingkai Tang <jingkaitang@gmail.com>
2014-01-24 01:18:36 +0800

    add: look last_man/day_and_world/first_case_and_week/place/call_week_about_eye/man.py beneath important person

---
+++ last_man/day_and_world/first_case_and_week/place/call_week_about_eye/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_hand(str_arg):
+    thing_and_big_time(str_arg)
+    print('time')
+
+def thing_and_big_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_hand('seem_group_over_place')
