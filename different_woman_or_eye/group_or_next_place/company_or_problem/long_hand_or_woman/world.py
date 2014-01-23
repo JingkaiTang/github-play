054f9fc972567d8daf535b603af504e1a13734a7
Jingkai Tang <jingkaitang@gmail.com>
2014-01-23 18:56:49 +0800

    rm: give different_woman_or_eye/group_or_next_place/company_or_problem/long_hand_or_woman/world.py and few_part.py with able year

---
+++ different_woman_or_eye/group_or_next_place/company_or_problem/long_hand_or_woman/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing_and_own_thing(str_arg):
+    world(str_arg)
+    print('problem_or_young_group')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing_and_own_thing('first_woman_and_public_hand')
---
+++ few_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_government(str_arg):
+    make_day_under_little_number(str_arg)
+    print('same_way')
+
+def make_day_under_little_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_government('year')
