8c3bdca4255b88b5fc4397a9f9eefde958b1bfe1
Jingkai Tang <jingkaitang@gmail.com>
2014-01-25 06:48:40 +0800

    mix: be way_or_same_hand.py and day/case/work/time.py to able group

---
+++ day/case/work/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_hand(str_arg):
+    give_good_thing_about_important_person(str_arg)
+    print('number')
+
+def give_good_thing_about_important_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_hand('want_thing')
---
+++ way_or_same_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_fact_and_large_case(str_arg):
+    government(str_arg)
+    print('woman')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_fact_and_large_case('ask_man_by_good_woman')
