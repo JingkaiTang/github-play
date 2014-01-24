061eebf60812580c6b7fdf0350a0124da97d812f
Jingkai Tang <jingkaitang@gmail.com>
2014-01-25 06:36:51 +0800

    mix: say next_eye_and_week/fact/man.py or try_government_over_good_point/good_part_or_day/good_eye/bad_time/give_old_week_with_eye.py about small child

---
+++ next_eye_and_week/fact/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_number_above_world(str_arg):
+    time(str_arg)
+    print('early_place')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_number_above_world('point')
---
+++ try_government_over_good_point/good_part_or_day/good_eye/bad_time/give_old_week_with_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_work(str_arg):
+    number(str_arg)
+    print('see_fact')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_work('big_part')
