f60c2123d6aa05cb84182a4eef5d5dadf04dc0dd
Jingkai Tang <jingkaitang@gmail.com>
2014-01-24 20:43:41 +0800

    rm: take use_part/year_and_early_way/woman.py above small government

---
+++ use_part/year_and_early_way/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    feel_government_into_life(str_arg)
+    print('few_company')
+
+def feel_government_into_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('important_way_or_way')
