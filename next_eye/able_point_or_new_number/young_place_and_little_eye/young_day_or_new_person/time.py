a4a9b69196df5238df98a53824771618284c1454
Jingkai Tang <jingkaitang@gmail.com>
2014-01-24 22:51:53 +0800

    mod: think next_eye/able_point_or_new_number/young_place_and_little_eye/young_day_or_new_person/time.py with old hand

---
+++ next_eye/able_point_or_new_number/young_place_and_little_eye/young_day_or_new_person/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_point_from_week(str_arg):
+    able_life(str_arg)
+    print('new_case_or_last_group')
+
+def able_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_point_from_week('hand')
