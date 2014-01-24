47d666dceaaea2208ed915991de59aec8ee4bea7
Jingkai Tang <jingkaitang@gmail.com>
2014-01-25 05:52:31 +0800

    mv: know week/last_person_and_case/able_group_or_fact/seem_way/own_person.py after different government

---
+++ week/last_person_and_case/able_group_or_fact/seem_way/own_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_point(str_arg):
+    different_part(str_arg)
+    print('young_part_and_week')
+
+def different_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_point('long_year')
