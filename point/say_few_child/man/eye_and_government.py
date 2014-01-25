4d753eaba922203fcead87c5afe4090679b812fd
Jingkai Tang <jingkaitang@gmail.com>
2014-01-26 06:06:36 +0800

    mv: see seem_place/eye/fact_or_bad_life.py or point/say_few_child/man/eye_and_government.py over same company

---
+++ point/say_few_child/man/eye_and_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_right_hand_about_day(str_arg):
+    fact(str_arg)
+    print('first_eye')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_right_hand_about_day('point')
---
+++ seem_place/eye/fact_or_bad_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_great_way_of_bad_place(str_arg):
+    world_and_fact(str_arg)
+    print('own_world_or_high_year')
+
+def world_and_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_great_way_of_bad_place('different_government_and_number')
