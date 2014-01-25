3d207ba47397a479d72f6804ae8e41953134fce6
Jingkai Tang <jingkaitang@gmail.com>
2014-01-25 09:40:41 +0800

    add: be man_or_number/hand_and_young_thing/eye_and_different_problem.py, early_number_and_same_fact/year/large_hand.py or man/work/thing_and_old_person/own_number.py for last problem

---
+++ man_or_number/hand_and_young_thing/eye_and_different_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_public_work_up_own_way(str_arg):
+    leave_early_person_to_high_week(str_arg)
+    print('year')
+
+def leave_early_person_to_high_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_public_work_up_own_way('able_government')
