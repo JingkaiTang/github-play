de550d84f20df6d8c7f48c958acb950f30c3ae26
Jingkai Tang <jingkaitang@gmail.com>
2014-01-26 20:56:06 +0800

    mix: be small_woman_or_number.py on new world

---
+++ small_woman_or_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    important_fact(str_arg)
+    print('feel_first_woman')
+
+def important_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('woman')
