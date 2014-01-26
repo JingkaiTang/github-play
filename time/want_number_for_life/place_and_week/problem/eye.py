ccd3758f985af6474babbe68c65e9e5707d252dc
Jingkai Tang <jingkaitang@gmail.com>
2014-01-26 15:55:37 +0800

    rm: feel time/want_number_for_life/place_and_week/problem/eye.py with own child

---
+++ time/want_number_for_life/place_and_week/problem/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_week(str_arg):
+    little_hand(str_arg)
+    print('other_day')
+
+def little_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_week('thing_or_big_problem')
