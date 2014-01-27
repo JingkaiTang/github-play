42b5c7cbc0cb62c32afd6a06b72797c4053be7e5
Jingkai Tang <jingkaitang@gmail.com>
2014-01-27 18:26:15 +0800

    add: be know_case.py over few year

---
+++ know_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_child_and_old_woman(str_arg):
+    own_day(str_arg)
+    print('tell_new_group_in_own_life')
+
+def own_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_child_and_old_woman('own_man')
