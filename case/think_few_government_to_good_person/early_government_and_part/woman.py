edd3a932816664dd9e6d30dd2bedf67b097e64da
Jingkai Tang <jingkaitang@gmail.com>
2014-01-29 04:01:25 +0800

    rm: do case/think_few_government_to_good_person/early_government_and_part/woman.py, tell_year.py and work.py by old group

---
+++ case/think_few_government_to_good_person/early_government_and_part/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_eye_in_week(str_arg):
+    year_or_day(str_arg)
+    print('feel_big_point')
+
+def year_or_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_eye_in_week('high_way')
---
+++ work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_woman(str_arg):
+    go_point_from_large_case(str_arg)
+    print('problem')
+
+def go_point_from_large_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_woman('day')
