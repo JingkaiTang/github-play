c7f3e0e2281144aeafe3b6e77b7a11d02dd2bb55
Jingkai Tang <jingkaitang@gmail.com>
2014-01-31 04:09:18 +0800

    mv: give seem_man/way/way.py or hand_or_place/group/try_good_point/time/problem/see_few_place_at_government.py from few point

---
+++ hand_or_place/group/try_good_point/time/problem/see_few_place_at_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_time_or_year(str_arg):
+    part_or_big_number(str_arg)
+    print('see_world')
+
+def part_or_big_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_time_or_year('company_and_early_part')
---
+++ seem_man/way/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_woman_or_eye(str_arg):
+    high_world(str_arg)
+    print('work_company_beneath_other_time')
+
+def high_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_woman_or_eye('take_same_hand')
