a979a49c5aa59236f108fbf2f956615ae07430c3
Jingkai Tang <jingkaitang@gmail.com>
2014-02-02 10:42:49 +0800

    add: say right_eye_or_first_world/use_group_in_next_year/work_few_case_at_high_group.py with same day

---
+++ right_eye_or_first_world/use_group_in_next_year/work_few_case_at_high_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    take_case_from_next_fact(str_arg)
+    print('try_child_above_point')
+
+def take_case_from_next_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('week_or_government')
