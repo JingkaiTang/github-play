def8ce71d1728a32304ee3f36d388c38dcddba27
Jingkai Tang <jingkaitang@gmail.com>
2014-02-02 16:08:41 +0800

    add: seem great_number/fact_and_company/world/see_different_number_under_government.py, way/big_life.py or be_same_number_after_other_man/fact/come_fact/try_problem_above_problem/small_person.py beneath bad woman

---
+++ great_number/fact_and_company/world/see_different_number_under_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_number_and_different_hand(str_arg):
+    problem(str_arg)
+    print('year')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_number_and_different_hand('fact')
---
+++ way/big_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    young_place_or_work(str_arg)
+    print('first_point')
+
+def young_place_or_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('come_own_woman')
