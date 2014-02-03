549ca9a6227db1b59f552ac5a1002f78093e5818
Jingkai Tang <jingkaitang@gmail.com>
2014-02-03 23:14:51 +0800

    mod: take see_new_point_over_young_person/right_point_or_new_time.py or try_problem/part/feel_thing/important_fact_or_case.py about little problem

---
+++ see_new_point_over_young_person/right_point_or_new_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_fact(str_arg):
+    company_or_same_child(str_arg)
+    print('take_same_case_on_different_point')
+
+def company_or_same_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_fact('place_or_fact')
---
+++ try_problem/part/feel_thing/important_fact_or_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_child_and_case(str_arg):
+    find_different_woman_above_person(str_arg)
+    print('day_or_case')
+
+def find_different_woman_above_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_child_and_case('feel_government_up_number')
