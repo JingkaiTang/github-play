8904d738289c5a54e3bd7a23f5d3072bf1a5021d
Jingkai Tang <jingkaitang@gmail.com>
2014-02-07 00:11:04 +0800

    mv: tell great_place.py or bad_company_or_own_case/be_person.py by public person

---
+++ bad_company_or_own_case/be_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_man(str_arg):
+    see_own_year(str_arg)
+    print('next_way')
+
+def see_own_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_man('bad_world')
---
+++ great_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man_or_important_eye(str_arg):
+    new_company(str_arg)
+    print('get_different_world_in_thing')
+
+def new_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man_or_important_eye('feel_other_company')
