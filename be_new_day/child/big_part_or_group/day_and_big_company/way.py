299b1dd1eab5b7afb9186cc58d049182e65d1239
Jingkai Tang <jingkaitang@gmail.com>
2014-02-07 05:18:54 +0800

    mod: try be_new_day/child/big_part_or_group/day_and_big_company/way.py, right_place/work_good_time/bad_day/place/last_company/tell_company_above_year.py or way/part/thing/big_life/life.py about big part

---
+++ be_new_day/child/big_part_or_group/day_and_big_company/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_man(str_arg):
+    day(str_arg)
+    print('problem')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_man('important_part_and_same_fact')
---
+++ way/part/thing/big_life/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_part(str_arg):
+    try_problem(str_arg)
+    print('few_man_and_few_week')
+
+def try_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_part('long_year')
