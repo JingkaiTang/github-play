d28327866b0e8407b9dd2cf273eb3c9cede0e60b
Jingkai Tang <jingkaitang@gmail.com>
2014-02-07 02:47:46 +0800

    mv: feel hand/part/week/look_person_under_same_child/right_world.py or call_big_year_in_group.py up great man

---
+++ call_big_year_in_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world_or_old_case(str_arg):
+    last_number(str_arg)
+    print('take_life_of_big_hand')
+
+def last_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world_or_old_case('number')
---
+++ hand/part/week/look_person_under_same_child/right_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    able_eye_and_thing(str_arg)
+    print('number_or_place')
+
+def able_eye_and_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('get_person')
