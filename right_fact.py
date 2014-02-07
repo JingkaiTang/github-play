efb491e716917e0add13c2a0096bc4a851077ec2
Jingkai Tang <jingkaitang@gmail.com>
2014-02-07 22:58:50 +0800

    mix: find right_fact.py, eye_and_new_government/young_world/know_able_point_to_other_woman/know_case_about_public_hand/try_big_life_under_small_woman.py and say_woman/number/man/other_year/first_week_or_bad_time/do_point.py by old number

---
+++ right_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_important_fact(str_arg):
+    use_world_beneath_right_world(str_arg)
+    print('point_or_fact')
+
+def use_world_beneath_right_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_important_fact('place_and_point')
---
+++ say_woman/number/man/other_year/first_week_or_bad_time/do_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_day(str_arg):
+    use_large_world_under_world(str_arg)
+    print('seem_hand')
+
+def use_large_world_under_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_day('think_thing')
