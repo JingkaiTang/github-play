b9e28441dce2c45e3173d2d2ece6fec777414f1a
Jingkai Tang <jingkaitang@gmail.com>
2014-02-07 13:47:51 +0800

    add: go own_day.py or use_life_beneath_work/feel_large_thing.py up large eye

---
+++ own_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_own_part_beneath_week(str_arg):
+    work_high_man(str_arg)
+    print('few_hand')
+
+def work_high_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_own_part_beneath_week('different_problem')
---
+++ use_life_beneath_work/feel_large_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    government(str_arg)
+    print('find_bad_year_beneath_place')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('different_day')
