a36c2169c8a6a6579aeff5586e26b7b74a8cf971
Jingkai Tang <jingkaitang@gmail.com>
2014-02-07 18:37:24 +0800

    add: see thing/company/same_problem.py from own eye

---
+++ thing/company/same_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_company(str_arg):
+    great_government(str_arg)
+    print('eye_or_last_hand')
+
+def great_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_company('new_point')
