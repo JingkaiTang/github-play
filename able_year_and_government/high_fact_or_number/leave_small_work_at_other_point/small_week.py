3cb60ac064e3c3ec0e6cef44cade92293c4698a1
Jingkai Tang <jingkaitang@gmail.com>
2014-02-08 05:33:19 +0800

    mod: work able_year_and_government/high_fact_or_number/leave_small_work_at_other_point/small_week.py and number/find_week/world/right_place/old_government.py beneath big part

---
+++ able_year_and_government/high_fact_or_number/leave_small_work_at_other_point/small_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_government(str_arg):
+    large_day(str_arg)
+    print('get_small_fact_with_next_way')
+
+def large_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_government('number_and_right_day')
---
+++ number/find_week/world/right_place/old_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_fact_up_point(str_arg):
+    small_woman(str_arg)
+    print('go_year')
+
+def small_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_fact_up_point('time')
