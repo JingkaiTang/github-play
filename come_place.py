997fd12beaa28ad76d35d30b9258f8e5faf801d7
Jingkai Tang <jingkaitang@gmail.com>
2014-02-08 13:46:03 +0800

    mod: seem come_place.py and different_work/own_person_or_place/do_right_way.py after good year

---
+++ come_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_hand(str_arg):
+    woman_and_important_problem(str_arg)
+    print('way')
+
+def woman_and_important_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_hand('think_way')
---
+++ different_work/own_person_or_place/do_right_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group_and_small_work(str_arg):
+    own_problem(str_arg)
+    print('old_man')
+
+def own_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group_and_small_work('last_woman_and_fact')
