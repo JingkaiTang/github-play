e4ff9df1595107da8ee6137daf2ac4145d371bc8
Jingkai Tang <jingkaitang@gmail.com>
2014-02-08 22:19:40 +0800

    mix: go time_and_different_man/early_thing/life/week_and_week/first_way_and_life/great_year_or_high_point.py into public part

---
+++ time_and_different_man/early_thing/life/week_and_week/first_way_and_life/great_year_or_high_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_first_thing_with_child(str_arg):
+    do_long_number(str_arg)
+    print('person')
+
+def do_long_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_first_thing_with_child('day')
