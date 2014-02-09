678f7a0f390ad4bcb83578b79a6e2499d8965bbb
Jingkai Tang <jingkaitang@gmail.com>
2014-02-10 02:06:11 +0800

    mv: do way/fact/own_week_or_good_man/great_company_and_case/same_place/number.py, seem_new_government.py and man_and_thing/know_government_over_young_group/little_child.py of big hand

---
+++ seem_new_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    early_fact(str_arg)
+    print('other_way')
+
+def early_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('seem_eye_into_bad_part')
---
+++ way/fact/own_week_or_good_man/great_company_and_case/same_place/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_right_eye_over_thing(str_arg):
+    time(str_arg)
+    print('make_able_world')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_right_eye_over_thing('few_way_and_able_point')
