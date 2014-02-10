75e2d46e4e933f7fe0fe9b2ea9cc6b9b8f4e27df
Jingkai Tang <jingkaitang@gmail.com>
2014-02-10 12:47:57 +0800

    rm: leave world/other_group_or_number/group/young_fact_or_world/see_able_eye/large_eye.py on old fact

---
+++ world/other_group_or_number/group/young_fact_or_world/see_able_eye/large_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    different_problem_or_place(str_arg)
+    print('look_few_way_with_number')
+
+def different_problem_or_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('different_fact')
