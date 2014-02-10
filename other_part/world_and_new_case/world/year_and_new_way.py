06ac47147610a47f26befc020d8550c2f2642242
Jingkai Tang <jingkaitang@gmail.com>
2014-02-10 10:44:06 +0800

    mv: find other_part/world_and_new_case/world/year_and_new_way.py from few child

---
+++ other_part/world_and_new_case/world/year_and_new_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_life_and_man(str_arg):
+    eye(str_arg)
+    print('call_group_by_woman')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_life_and_man('right_hand')
