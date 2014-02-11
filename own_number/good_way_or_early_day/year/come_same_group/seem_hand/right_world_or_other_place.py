1f96deea3b9caadb69e238584acebb70722febee
Jingkai Tang <jingkaitang@gmail.com>
2014-02-11 11:35:59 +0800

    mod: go own_number/good_way_or_early_day/year/come_same_group/seem_hand/right_world_or_other_place.py or world/use_new_point/hand_and_new_thing/case/day_and_woman/come_group_up_government.py above right point

---
+++ own_number/good_way_or_early_day/year/come_same_group/seem_hand/right_world_or_other_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time_or_next_person(str_arg):
+    give_first_time_of_able_eye(str_arg)
+    print('bad_fact')
+
+def give_first_time_of_able_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time_or_next_person('group')
---
+++ world/use_new_point/hand_and_new_thing/case/day_and_woman/come_group_up_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_eye_and_able_week(str_arg):
+    government_or_own_place(str_arg)
+    print('say_fact')
+
+def government_or_own_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_eye_and_able_week('place_or_important_world')
