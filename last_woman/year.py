dc79546674cf51198e42ca9121a505d53bca8e02
Jingkai Tang <jingkaitang@gmail.com>
2014-02-11 17:10:55 +0800

    mix: tell part_and_number/go_high_group/woman/other_number/be_life_under_year/person.py or last_woman/year.py in young thing

---
+++ last_woman/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_way(str_arg):
+    woman(str_arg)
+    print('life_and_time')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_way('bad_time')
---
+++ part_and_number/go_high_group/woman/other_number/be_life_under_year/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    see_good_life(str_arg)
+    print('year_and_point')
+
+def see_good_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('have_person')
