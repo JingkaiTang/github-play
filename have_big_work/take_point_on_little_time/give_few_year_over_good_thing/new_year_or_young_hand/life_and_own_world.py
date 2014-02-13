cee2c37417e51b60e2d59f04fe37606746c5bd8a
Jingkai Tang <jingkaitang@gmail.com>
2014-02-13 16:00:19 +0800

    add: get have_big_work/take_point_on_little_time/give_few_year_over_good_thing/new_year_or_young_hand/life_and_own_world.py in different group

---
+++ have_big_work/take_point_on_little_time/give_few_year_over_good_thing/new_year_or_young_hand/life_and_own_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    take_big_problem(str_arg)
+    print('different_work')
+
+def take_big_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('long_work')
