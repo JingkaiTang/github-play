56faf6d1d46dfb5a36009218d67bd8112e98e7d1
Jingkai Tang <jingkaitang@gmail.com>
2014-02-15 12:15:20 +0800

    mv: call able_work/know_next_eye.py or bad_person/year_and_first_part/other_work.py from able child

---
+++ able_work/know_next_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_problem_and_woman(str_arg):
+    good_government(str_arg)
+    print('long_day_or_little_problem')
+
+def good_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_problem_and_woman('think_own_case_about_long_number')
