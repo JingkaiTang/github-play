2d30a97b814896721dde5f125adc7dc3d9f781d9
Jingkai Tang <jingkaitang@gmail.com>
2014-02-16 19:01:29 +0800

    rm: get government_or_week.py over great man

---
+++ government_or_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way_or_company(str_arg):
+    government(str_arg)
+    print('able_person')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way_or_company('important_man')
