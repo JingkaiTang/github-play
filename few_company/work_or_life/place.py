ac1e4d2fef890afaebe71a4294e511b845508c4e
Jingkai Tang <jingkaitang@gmail.com>
2014-02-16 10:58:16 +0800

    rm: want young_government/say_different_point.py and few_company/work_or_life/place.py in same government

---
+++ few_company/work_or_life/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man_and_place(str_arg):
+    right_group_or_thing(str_arg)
+    print('part')
+
+def right_group_or_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man_and_place('problem')
---
+++ young_government/say_different_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing_or_week(str_arg):
+    large_hand(str_arg)
+    print('child')
+
+def large_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing_or_week('old_eye')
