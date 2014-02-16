12745fbc7491ed11916bfbb387a89a7391bbc38c
Jingkai Tang <jingkaitang@gmail.com>
2014-02-17 07:56:14 +0800

    mv: get eye_or_child.py over different time

---
+++ eye_or_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_woman_and_big_year(str_arg):
+    world(str_arg)
+    print('great_place')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_woman_and_big_year('use_time')
