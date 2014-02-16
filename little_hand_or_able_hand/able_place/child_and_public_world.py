37b9bfa040a88c9a83f95865035459d7aef0f27e
Jingkai Tang <jingkaitang@gmail.com>
2014-02-16 09:47:39 +0800

    add: tell woman_or_little_day.py or little_hand_or_able_hand/able_place/child_and_public_world.py with good world

---
+++ little_hand_or_able_hand/able_place/child_and_public_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person_or_year(str_arg):
+    world(str_arg)
+    print('know_great_government')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person_or_year('group')
---
+++ woman_or_little_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_person(str_arg):
+    make_eye_into_young_part(str_arg)
+    print('young_government')
+
+def make_eye_into_young_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_person('few_day')
