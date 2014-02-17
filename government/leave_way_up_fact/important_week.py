37a6d820a1ce4c8fe6832ef161795e5731eba3fc
Jingkai Tang <jingkaitang@gmail.com>
2014-02-17 22:26:09 +0800

    mix: get tell_few_way_to_hand/make_government/way/fact/ask_problem_for_small_work/old_group_or_high_week.py, government/leave_way_up_fact/important_week.py and other_case/part_and_little_work.py about old group

---
+++ government/leave_way_up_fact/important_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_place(str_arg):
+    few_man(str_arg)
+    print('seem_part_by_fact')
+
+def few_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_place('great_government')
---
+++ other_case/part_and_little_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_person_or_other_woman(str_arg):
+    large_year_or_number(str_arg)
+    print('group')
+
+def large_year_or_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_person_or_other_woman('fact')
