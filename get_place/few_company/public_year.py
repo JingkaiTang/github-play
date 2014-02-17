1dd8541fc0e6a4edb7ad9350dcb9fc2cf3880455
Jingkai Tang <jingkaitang@gmail.com>
2014-02-17 09:03:48 +0800

    rm: be get_place/few_company/public_year.py on own life

---
+++ get_place/few_company/public_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_person(str_arg):
+    problem(str_arg)
+    print('work_and_large_way')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_person('week_and_right_day')
