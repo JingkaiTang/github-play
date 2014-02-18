3f8db1c706bba7d43b2345d9e7cc2ffb17382788
Jingkai Tang <jingkaitang@gmail.com>
2014-02-18 21:03:55 +0800

    add: take way.py above other fact

---
+++ way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_day_under_day(str_arg):
+    come_big_person_under_time(str_arg)
+    print('use_work_above_way')
+
+def come_big_person_under_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_day_under_day('high_way')
