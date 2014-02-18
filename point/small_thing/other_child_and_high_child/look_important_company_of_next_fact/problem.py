248cc797f82288684e6c2a3fb0fe7e0dedb80196
Jingkai Tang <jingkaitang@gmail.com>
2014-02-18 19:00:54 +0800

    mv: get point/small_thing/other_child_and_high_child/look_important_company_of_next_fact/problem.py about young eye

---
+++ point/small_thing/other_child_and_high_child/look_important_company_of_next_fact/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    be_different_point(str_arg)
+    print('other_thing')
+
+def be_different_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('next_year')
