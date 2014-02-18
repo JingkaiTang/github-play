c5ae6e306edfe092f3ea39bd69965c7b9d365421
Jingkai Tang <jingkaitang@gmail.com>
2014-02-18 10:42:33 +0800

    mix: try say_last_world_about_point/want_way/be_able_part.py and do_small_problem_from_high_company/bad_point_or_small_point/first_time/man_and_day/use_day_in_big_year/long_problem.py after young week

---
+++ do_small_problem_from_high_company/bad_point_or_small_point/first_time/man_and_day/use_day_in_big_year/long_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_day(str_arg):
+    able_fact(str_arg)
+    print('find_old_place_over_young_year')
+
+def able_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_day('child')
---
+++ say_last_world_about_point/want_way/be_able_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_eye(str_arg):
+    part(str_arg)
+    print('come_old_government')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_eye('able_fact')
