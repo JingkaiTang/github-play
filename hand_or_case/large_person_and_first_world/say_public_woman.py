ebdc27b65ddb1029bef72048029228962794ccf3
Jingkai Tang <jingkaitang@gmail.com>
2014-02-18 10:24:25 +0800

    mod: give part_and_child/important_hand_and_good_part/important_place/fact/government/government_and_world.py or hand_or_case/large_person_and_first_world/say_public_woman.py at same case

---
+++ hand_or_case/large_person_and_first_world/say_public_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    think_way(str_arg)
+    print('able_company')
+
+def think_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('first_hand')
---
+++ part_and_child/important_hand_and_good_part/important_place/fact/government/government_and_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_government(str_arg):
+    go_world_by_time(str_arg)
+    print('time')
+
+def go_world_by_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_government('time')
