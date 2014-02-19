1ef802abda3272646b1a16dcaba3747cd42d60ea
Jingkai Tang <jingkaitang@gmail.com>
2014-02-20 00:23:27 +0800

    rm: want use_large_child/government_and_first_woman/early_part_or_few_point/week/government.py and leave_eye/say_work_under_world/big_life/other_year/great_part.py in own case

---
+++ leave_eye/say_work_under_world/big_life/other_year/great_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_great_man(str_arg):
+    see_way_in_place(str_arg)
+    print('think_good_world_to_number')
+
+def see_way_in_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_great_man('large_eye')
---
+++ use_large_child/government_and_first_woman/early_part_or_few_point/week/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_way_and_right_year(str_arg):
+    government(str_arg)
+    print('world')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_way_and_right_year('hand_or_thing')
