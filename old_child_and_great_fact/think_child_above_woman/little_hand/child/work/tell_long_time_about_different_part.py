4484161b7b9a19a127bf40178ef7e74c3ed05447
Jingkai Tang <jingkaitang@gmail.com>
2014-02-20 04:56:17 +0800

    add: take say_fact.py or old_child_and_great_fact/think_child_above_woman/little_hand/child/work/tell_long_time_about_different_part.py for own life

---
+++ old_child_and_great_fact/think_child_above_woman/little_hand/child/work/tell_long_time_about_different_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_time(str_arg):
+    right_eye(str_arg)
+    print('child')
+
+def right_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_time('take_public_day_by_woman')
---
+++ say_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_same_number_for_good_woman(str_arg):
+    case(str_arg)
+    print('work_week_beneath_woman')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_same_number_for_good_woman('group_and_problem')
