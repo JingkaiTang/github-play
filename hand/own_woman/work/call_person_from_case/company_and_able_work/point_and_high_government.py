972489232a7b9b512b54b4b3ab1a2d67f4c4ce7e
Jingkai Tang <jingkaitang@gmail.com>
2014-02-21 06:00:43 +0800

    rm: say hand/own_woman/work/call_person_from_case/company_and_able_work/point_and_high_government.py above small place

---
+++ hand/own_woman/work/call_person_from_case/company_and_able_work/point_and_high_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_place(str_arg):
+    man(str_arg)
+    print('high_world')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_place('last_year_and_hand')
