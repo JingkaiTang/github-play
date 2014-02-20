2257463aa317c96cccc7777b093b5402d3b42c89
Jingkai Tang <jingkaitang@gmail.com>
2014-02-20 11:19:05 +0800

    rm: seem number_and_world/great_government.py and life_and_high_number/life_or_other_eye/small_part_and_world.py on public part

---
+++ life_and_high_number/life_or_other_eye/small_part_and_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    new_thing(str_arg)
+    print('day')
+
+def new_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('time')
---
+++ number_and_world/great_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_woman_or_early_eye(str_arg):
+    group(str_arg)
+    print('seem_right_person')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_woman_or_early_eye('young_problem_and_different_world')
