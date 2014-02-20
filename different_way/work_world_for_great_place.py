5e76261313d77866f6364941f5d50fab5c393139
Jingkai Tang <jingkaitang@gmail.com>
2014-02-21 01:13:34 +0800

    mod: take different_way/work_world_for_great_place.py under own thing

---
+++ different_way/work_world_for_great_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_time(str_arg):
+    big_point(str_arg)
+    print('small_life')
+
+def big_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_time('young_fact_and_own_woman')
