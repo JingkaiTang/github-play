b087a95422c4d97648f1648e50e6fd443f13782a
Jingkai Tang <jingkaitang@gmail.com>
2014-02-20 21:51:41 +0800

    mod: make long_number.py up last case

---
+++ long_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_year_and_important_life(str_arg):
+    great_group(str_arg)
+    print('way_or_point')
+
+def great_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_year_and_important_life('long_place')
