8a5714f8146525baeb125de127f7f200defbbbce
Jingkai Tang <jingkaitang@gmail.com>
2014-02-21 22:50:24 +0800

    rm: go world_or_good_place.py over old case

---
+++ world_or_good_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_work(str_arg):
+    make_life(str_arg)
+    print('find_time_over_day')
+
+def make_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_work('other_day')
