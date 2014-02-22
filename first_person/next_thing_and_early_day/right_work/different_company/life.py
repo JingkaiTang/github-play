442192c10318830ef5218ec7d4506f1e16ec4ec6
Jingkai Tang <jingkaitang@gmail.com>
2014-02-22 11:57:15 +0800

    mix: be first_person/next_thing_and_early_day/right_work/different_company/life.py and take_last_man_on_company/other_person/good_problem/point/few_person_and_life/use_week_on_thing.py by good man

---
+++ first_person/next_thing_and_early_day/right_work/different_company/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact_or_government(str_arg):
+    do_high_number_up_last_world(str_arg)
+    print('person_or_way')
+
+def do_high_number_up_last_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact_or_government('make_important_man')
---
+++ take_last_man_on_company/other_person/good_problem/point/few_person_and_life/use_week_on_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_own_man(str_arg):
+    next_eye(str_arg)
+    print('use_other_world')
+
+def next_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_own_man('want_thing_by_next_problem')
