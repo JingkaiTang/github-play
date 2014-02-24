d470d75c4a4c230a3776bc07f8c7f2fdb64fec2f
Jingkai Tang <jingkaitang@gmail.com>
2014-02-25 01:24:51 +0800

    rm: say tell_week/have_group/place/bad_thing/big_way_or_right_child.py or big_time_and_large_part/week_and_day/week_and_bad_problem/day_or_man/big_part_and_group/fact.py under different place

---
+++ big_time_and_large_part/week_and_day/week_and_bad_problem/day_or_man/big_part_and_group/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_man_and_place(str_arg):
+    other_government(str_arg)
+    print('great_eye_and_point')
+
+def other_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_man_and_place('able_work')
