e157ce77c814118c40ade2a401b9c2693ba5bc02
Jingkai Tang <jingkaitang@gmail.com>
2014-02-24 08:19:12 +0800

    add: feel bad_part/number/same_day/first_child.py by good government

---
+++ bad_part/number/same_day/first_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_problem(str_arg):
+    person(str_arg)
+    print('good_case')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_problem('look_small_way')
