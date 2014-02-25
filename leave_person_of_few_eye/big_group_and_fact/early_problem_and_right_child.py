c973be83132e869272d9b709862deb1d694dd59d
Jingkai Tang <jingkaitang@gmail.com>
2014-02-25 12:42:21 +0800

    mod: get same_week.py or leave_person_of_few_eye/big_group_and_fact/early_problem_and_right_child.py after own point

---
+++ leave_person_of_few_eye/big_group_and_fact/early_problem_and_right_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_life(str_arg):
+    last_fact_or_high_case(str_arg)
+    print('good_life')
+
+def last_fact_or_high_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_life('right_person')
---
+++ same_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_place(str_arg):
+    world(str_arg)
+    print('feel_last_week_over_other_child')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_place('first_life')
