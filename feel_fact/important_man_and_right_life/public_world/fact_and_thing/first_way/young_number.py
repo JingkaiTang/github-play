275fd870edfc0c9209744a16cfb708edf16741fe
Jingkai Tang <jingkaitang@gmail.com>
2014-02-26 06:19:10 +0800

    mod: say feel_fact/important_man_and_right_life/public_world/fact_and_thing/first_way/young_number.py for old work

---
+++ feel_fact/important_man_and_right_life/public_world/fact_and_thing/first_way/young_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    large_eye_or_work(str_arg)
+    print('man')
+
+def large_eye_or_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('bad_way')
