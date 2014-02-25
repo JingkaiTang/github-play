651e760bed2ef3f8395cd4378a2c690095326172
Jingkai Tang <jingkaitang@gmail.com>
2014-02-26 04:20:24 +0800

    rm: come first_point_and_case/be_way/great_way_and_hand/public_fact/high_number/hand.py to small company

---
+++ first_point_and_case/be_way/great_way_and_hand/public_fact/high_number/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_number_or_part(str_arg):
+    eye(str_arg)
+    print('other_day')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_number_or_part('try_next_number_on_great_government')
