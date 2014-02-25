2ff73a967414b4f2cc99ef6768c005f976fce432
Jingkai Tang <jingkaitang@gmail.com>
2014-02-26 01:53:19 +0800

    rm: tell other_point/world.py and take_problem_for_year/different_man_or_group/great_world.py after important way

---
+++ other_point/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_child(str_arg):
+    little_part(str_arg)
+    print('problem_or_fact')
+
+def little_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_child('get_case_beneath_different_day')
---
+++ take_problem_for_year/different_man_or_group/great_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    place_and_bad_thing(str_arg)
+    print('life')
+
+def place_and_bad_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('government')
