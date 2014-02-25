fb56729755db05b84c308535971ed4505ebd1308
Jingkai Tang <jingkaitang@gmail.com>
2014-02-25 12:31:13 +0800

    add: feel look_week_at_eye/find_little_work_up_important_problem/get_little_child/last_place_or_fact/life.py on big government

---
+++ look_week_at_eye/find_little_work_up_important_problem/get_little_child/last_place_or_fact/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world_and_same_person(str_arg):
+    next_point_or_part(str_arg)
+    print('case_or_child')
+
+def next_point_or_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world_and_same_person('world')
