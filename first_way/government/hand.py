3aaa9348b1858b52a3c7a0926ae3f46e473891ee
Jingkai Tang <jingkaitang@gmail.com>
2014-02-27 06:23:37 +0800

    mix: want want_other_point_under_high_case.py or first_way/government/hand.py beneath little week

---
+++ first_way/government/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_group(str_arg):
+    young_number_and_day(str_arg)
+    print('work_hand')
+
+def young_number_and_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_group('come_way_at_different_day')
---
+++ want_other_point_under_high_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem_and_government(str_arg):
+    great_problem_and_point(str_arg)
+    print('week')
+
+def great_problem_and_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem_and_government('find_company')
