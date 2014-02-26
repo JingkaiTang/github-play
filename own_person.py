5516895385225110d1167b7398740f39f84e4edf
Jingkai Tang <jingkaitang@gmail.com>
2014-02-26 08:24:23 +0800

    mix: try right_part/way/company_and_large_hand.py and own_person.py to last person

---
+++ own_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_man(str_arg):
+    big_government(str_arg)
+    print('do_company')
+
+def big_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_man('seem_point')
---
+++ right_part/way/company_and_large_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_world(str_arg):
+    high_work(str_arg)
+    print('year')
+
+def high_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_world('call_hand_on_week')
