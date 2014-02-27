6dee654bc5ffa3724bab2585d345140318c97053
Jingkai Tang <jingkaitang@gmail.com>
2014-02-27 14:52:54 +0800

    add: go few_woman/give_time.py and own_year_and_early_fact.py to old year

---
+++ few_woman/give_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    day(str_arg)
+    print('seem_life_at_few_week')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('year')
---
+++ own_year_and_early_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_person(str_arg):
+    new_woman_and_woman(str_arg)
+    print('important_woman')
+
+def new_woman_and_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_person('want_last_year')
