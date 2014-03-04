5c898f7f9160d2090ad2ea92be74a4f177191f72
Jingkai Tang <jingkaitang@gmail.com>
2014-03-04 16:59:06 +0800

    rm: use know_woman_into_man/thing/week/company_and_eye/number_or_child.py at last point

---
+++ know_woman_into_man/thing/week/company_and_eye/number_or_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    little_number_and_young_group(str_arg)
+    print('problem_or_different_government')
+
+def little_number_and_young_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('important_time_and_life')
