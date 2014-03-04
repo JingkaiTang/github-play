4278d4395c2d99540fac9af26474087bfb936af0
Jingkai Tang <jingkaitang@gmail.com>
2014-03-04 09:52:46 +0800

    mod: work good_week/last_fact/be_group/government_and_little_part/see_different_work.py on first person

---
+++ good_week/last_fact/be_group/government_and_little_part/see_different_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_time(str_arg):
+    government(str_arg)
+    print('man')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_time('own_way')
