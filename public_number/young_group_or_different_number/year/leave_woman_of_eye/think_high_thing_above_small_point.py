826a86b80e990a970c54dd01957a2fb9ee2cc596
Jingkai Tang <jingkaitang@gmail.com>
2014-03-07 05:13:06 +0800

    mix: find public_number/young_group_or_different_number/year/leave_woman_of_eye/think_high_thing_above_small_point.py into great place

---
+++ public_number/young_group_or_different_number/year/leave_woman_of_eye/think_high_thing_above_small_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_world(str_arg):
+    public_day(str_arg)
+    print('say_life_into_man')
+
+def public_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_world('call_bad_way_up_small_place')
