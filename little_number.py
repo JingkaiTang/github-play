a159fbe053181617e0cfe93ca8538df10b5b4701
Jingkai Tang <jingkaitang@gmail.com>
2014-03-07 20:58:22 +0800

    add: make new_group_and_different_life/new_part_or_different_child/work_big_day_on_young_point/long_problem/government_and_point.py and little_number.py into high work

---
+++ little_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_woman(str_arg):
+    have_large_number(str_arg)
+    print('public_point_and_man')
+
+def have_large_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_woman('feel_right_child_over_young_child')
---
+++ new_group_and_different_life/new_part_or_different_child/work_big_day_on_young_point/long_problem/government_and_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week_and_little_person(str_arg):
+    few_point_or_big_person(str_arg)
+    print('be_year_to_place')
+
+def few_point_or_big_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week_and_little_person('group')
