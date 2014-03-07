c0a1c21c5138b6b255d0b1afc552ce7a80d4c332
Jingkai Tang <jingkaitang@gmail.com>
2014-03-07 08:32:09 +0800

    add: work thing_and_way/long_life/see_part.py on same person

---
+++ thing_and_way/long_life/see_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye_and_next_number(str_arg):
+    large_man(str_arg)
+    print('company_or_own_child')
+
+def large_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye_and_next_number('work_and_year')
