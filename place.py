6316c82fdf53f9c4e530f16946305671aa6ddc02
Jingkai Tang <jingkaitang@gmail.com>
2014-03-08 08:39:36 +0800

    rm: take place.py for next case

---
+++ place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    try_few_way(str_arg)
+    print('great_eye')
+
+def try_few_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('say_small_problem')
