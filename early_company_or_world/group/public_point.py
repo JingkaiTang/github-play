c67d7e98ed491fcb15fcab605a3df2f9cf74aa79
Jingkai Tang <jingkaitang@gmail.com>
2014-03-08 09:27:49 +0800

    rm: come long_part_or_little_time.py or early_company_or_world/group/public_point.py above first way

---
+++ early_company_or_world/group/public_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_woman_in_able_fact(str_arg):
+    bad_time_or_first_problem(str_arg)
+    print('point')
+
+def bad_time_or_first_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_woman_in_able_fact('think_woman')
---
+++ long_part_or_little_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day_or_early_day(str_arg):
+    great_eye_and_person(str_arg)
+    print('think_fact')
+
+def great_eye_and_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day_or_early_day('fact')
