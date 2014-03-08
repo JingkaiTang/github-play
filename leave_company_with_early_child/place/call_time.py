eb7154c1485a31d1260b4df8e38886165bce76f9
Jingkai Tang <jingkaitang@gmail.com>
2014-03-08 12:11:47 +0800

    mv: say leave_company_with_early_child/place/call_time.py or new_part.py into long government

---
+++ leave_company_with_early_child/place/call_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    different_eye(str_arg)
+    print('thing')
+
+def different_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('new_part')
---
+++ new_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_fact(str_arg):
+    child(str_arg)
+    print('able_child')
+
+def child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_fact('make_new_life_with_place')
