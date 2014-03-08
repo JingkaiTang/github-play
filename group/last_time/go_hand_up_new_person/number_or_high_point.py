1449a3167195db18e8f4fb77248bd30eea92d1bd
Jingkai Tang <jingkaitang@gmail.com>
2014-03-08 21:58:04 +0800

    rm: go public_world.py and group/last_time/go_hand_up_new_person/number_or_high_point.py over old child

---
+++ group/last_time/go_hand_up_new_person/number_or_high_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_thing_and_able_number(str_arg):
+    call_high_government_after_life(str_arg)
+    print('leave_day_in_world')
+
+def call_high_government_after_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_thing_and_able_number('right_work')
---
+++ public_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_way(str_arg):
+    year(str_arg)
+    print('work')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_way('person')
