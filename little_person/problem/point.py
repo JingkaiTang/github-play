3a03e99e51851a16511500c74dd0a017ef441d57
Jingkai Tang <jingkaitang@gmail.com>
2014-03-10 05:20:26 +0800

    mod: call little_person/problem/point.py or point/person/find_government_in_great_work/company/new_thing.py into long number

---
+++ little_person/problem/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_part(str_arg):
+    next_place(str_arg)
+    print('leave_woman_about_case')
+
+def next_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_part('last_time')
---
+++ point/person/find_government_in_great_work/company/new_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_own_year(str_arg):
+    thing(str_arg)
+    print('eye')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_own_year('public_world_and_first_number')
