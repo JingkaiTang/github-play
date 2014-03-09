20f1e8d9d803e99ca6ad3b1e2e94b63b95622766
Jingkai Tang <jingkaitang@gmail.com>
2014-03-10 01:28:36 +0800

    mod: want life/life/number/company_or_life/come_other_man_under_problem.py or first_day_or_person/company/world/hand/man/last_part.py after important place

---
+++ first_day_or_person/company/world/hand/man/last_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_right_fact_to_fact(str_arg):
+    other_problem_or_young_way(str_arg)
+    print('week')
+
+def other_problem_or_young_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_right_fact_to_fact('company')
---
+++ life/life/number/company_or_life/come_other_man_under_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_hand_and_world(str_arg):
+    give_last_way_on_thing(str_arg)
+    print('point')
+
+def give_last_way_on_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_hand_and_world('high_group')
