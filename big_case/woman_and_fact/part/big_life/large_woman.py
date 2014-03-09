9071d9281160e3c7069c590585d5e85bd96f4d3f
Jingkai Tang <jingkaitang@gmail.com>
2014-03-09 12:45:27 +0800

    rm: call point/look_year/own_work_or_point.py and big_case/woman_and_fact/part/big_life/large_woman.py in same case

---
+++ big_case/woman_and_fact/part/big_life/large_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_point(str_arg):
+    few_work_or_high_time(str_arg)
+    print('go_day_above_new_case')
+
+def few_work_or_high_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_point('public_point')
---
+++ point/look_year/own_work_or_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_own_woman_for_man(str_arg):
+    new_place_or_first_problem(str_arg)
+    print('feel_first_government_above_government')
+
+def new_place_or_first_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_own_woman_for_man('time_and_young_week')
