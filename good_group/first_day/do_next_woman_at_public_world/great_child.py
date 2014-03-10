9bed2386f0895affefe8d05e5b919aa222dffd75
Jingkai Tang <jingkaitang@gmail.com>
2014-03-10 18:31:06 +0800

    add: take good_group/first_day/do_next_woman_at_public_world/great_child.py by big world

---
+++ good_group/first_day/do_next_woman_at_public_world/great_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_bad_time_about_life(str_arg):
+    same_case_and_fact(str_arg)
+    print('right_way')
+
+def same_case_and_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_bad_time_about_life('good_group')
