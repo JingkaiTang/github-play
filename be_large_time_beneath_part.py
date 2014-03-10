2510042b2bc1fcdec3bdf14059aee9ecb8fcdd10
Jingkai Tang <jingkaitang@gmail.com>
2014-03-11 03:23:19 +0800

    rm: work be_large_time_beneath_part.py over little man

---
+++ be_large_time_beneath_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_first_world_under_man(str_arg):
+    young_time(str_arg)
+    print('time')
+
+def young_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_first_world_under_man('few_week_and_hand')
