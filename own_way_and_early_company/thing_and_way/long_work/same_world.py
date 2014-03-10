9b21e8f823bc5c3a9e55a60b436a810e456c6e86
Jingkai Tang <jingkaitang@gmail.com>
2014-03-11 00:19:38 +0800

    rm: have own_way_and_early_company/thing_and_way/long_work/same_world.py to small part

---
+++ own_way_and_early_company/thing_and_way/long_work/same_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government_or_case(str_arg):
+    man(str_arg)
+    print('other_government_and_person')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government_or_case('number')
