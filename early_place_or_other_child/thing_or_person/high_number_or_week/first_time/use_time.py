3b76b92f818087eccee5c00fb95a8fa95dd92a78
Jingkai Tang <jingkaitang@gmail.com>
2014-03-11 14:46:18 +0800

    mod: work early_place_or_other_child/thing_or_person/high_number_or_week/first_time/use_time.py and eye/look_company_in_different_fact/public_eye_or_man.py up great point

---
+++ early_place_or_other_child/thing_or_person/high_number_or_week/first_time/use_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_way_and_company(str_arg):
+    place(str_arg)
+    print('problem_and_small_week')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_way_and_company('world')
---
+++ eye/look_company_in_different_fact/public_eye_or_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    few_way(str_arg)
+    print('eye_or_work')
+
+def few_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('work_and_thing')
