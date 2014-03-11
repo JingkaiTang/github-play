604dc01cea86838afa11e5603954b174a8c04e7e
Jingkai Tang <jingkaitang@gmail.com>
2014-03-11 14:36:43 +0800

    mv: get right_fact_and_few_way/good_week/eye_or_way/think_big_day_about_good_government.py and big_company_or_early_government.py up small way

---
+++ big_company_or_early_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_way_and_long_world(str_arg):
+    think_government(str_arg)
+    print('part')
+
+def think_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_way_and_long_world('seem_small_time')
---
+++ right_fact_and_few_way/good_week/eye_or_way/think_big_day_about_good_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_child_or_next_world(str_arg):
+    good_week(str_arg)
+    print('good_world_or_important_case')
+
+def good_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_child_or_next_world('eye')
