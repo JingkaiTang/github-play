7f9e19c043dbbc21467f26dd8cf56e9286b04f24
Jingkai Tang <jingkaitang@gmail.com>
2014-03-11 18:19:38 +0800

    mix: think try_great_eye/different_group/person/small_problem/seem_number.py above right child

---
+++ try_great_eye/different_group/person/small_problem/seem_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_part_or_time(str_arg):
+    hand_or_large_problem(str_arg)
+    print('bad_thing')
+
+def hand_or_large_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_part_or_time('hand')
