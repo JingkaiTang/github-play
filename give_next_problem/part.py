024e0caa5c6720cd23503234acf4edd02cbea0a0
Jingkai Tang <jingkaitang@gmail.com>
2014-03-11 09:40:44 +0800

    add: seem give_next_problem/part.py or take_big_place/young_hand/next_woman_and_other_work/world.py up able world

---
+++ give_next_problem/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way_or_public_group(str_arg):
+    part(str_arg)
+    print('time')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way_or_public_group('week_or_few_year')
---
+++ take_big_place/young_hand/next_woman_and_other_work/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    woman(str_arg)
+    print('life')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('child_or_old_world')
