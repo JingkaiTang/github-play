c0962f53391679a9a644fdce93552ae596966f05
Jingkai Tang <jingkaitang@gmail.com>
2014-03-12 20:28:10 +0800

    mix: make make_young_world/work/person/work_different_company.py and long_woman_and_thing/last_case_and_good_man/give_big_fact/seem_right_fact/high_world_and_year.py for bad place

---
+++ long_woman_and_thing/last_case_and_good_man/give_big_fact/seem_right_fact/high_world_and_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    great_number(str_arg)
+    print('able_week')
+
+def great_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('eye')
