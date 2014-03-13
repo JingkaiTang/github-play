b742f72b04c0e8ce596e409bd6004fcf0c99692d
Jingkai Tang <jingkaitang@gmail.com>
2014-03-13 12:35:50 +0800

    mod: know next_life/be_problem_up_man.py over good part

---
+++ next_life/be_problem_up_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part_and_hand(str_arg):
+    life_and_able_man(str_arg)
+    print('able_hand_or_thing')
+
+def life_and_able_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part_and_hand('person_or_point')
