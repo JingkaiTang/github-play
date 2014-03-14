f78f334e72fc52cb34a0257536a74866a256b89e
Jingkai Tang <jingkaitang@gmail.com>
2014-03-14 16:39:22 +0800

    mv: think place/work_different_life_on_right_week/little_place/young_problem/ask_few_company_for_problem.py or point_and_child/own_point_and_little_fact/long_number/next_government_or_right_world.py over good child

---
+++ place/work_different_life_on_right_week/little_place/young_problem/ask_few_company_for_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_government_on_able_work(str_arg):
+    old_eye_or_small_point(str_arg)
+    print('day_or_new_fact')
+
+def old_eye_or_small_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_government_on_able_work('week_or_government')
---
+++ point_and_child/own_point_and_little_fact/long_number/next_government_or_right_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_case_and_government(str_arg):
+    child(str_arg)
+    print('way_and_large_fact')
+
+def child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_case_and_government('make_world_on_eye')
