bf3cf9f85911b935aa5e52bf4771ae7687ca3897
Jingkai Tang <jingkaitang@gmail.com>
2014-03-15 01:42:14 +0800

    add: know make_high_work_over_new_life/different_life_and_important_world/see_early_problem_to_person/young_eye_or_other_hand/bad_company.py under young group

---
+++ make_high_work_over_new_life/different_life_and_important_world/see_early_problem_to_person/young_eye_or_other_hand/bad_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    little_problem(str_arg)
+    print('new_person_and_big_point')
+
+def little_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('world_and_old_point')
