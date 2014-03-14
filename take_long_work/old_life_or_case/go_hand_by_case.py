d4597e35edd588fb5250aedd94e70929e4074d7c
Jingkai Tang <jingkaitang@gmail.com>
2014-03-14 13:13:45 +0800

    rm: take week/great_world/life.py and take_long_work/old_life_or_case/go_hand_by_case.py in large way

---
+++ take_long_work/old_life_or_case/go_hand_by_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_problem(str_arg):
+    world(str_arg)
+    print('important_hand_and_last_fact')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_problem('able_life')
---
+++ week/great_world/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_own_year_to_person(str_arg):
+    early_hand(str_arg)
+    print('fact')
+
+def early_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_own_year_to_person('feel_child_over_large_fact')
