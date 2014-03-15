3b358fc2d6552006b9181936f44bce73e71b0104
Jingkai Tang <jingkaitang@gmail.com>
2014-03-16 05:18:31 +0800

    mod: ask large_case_and_good_problem/new_world_and_first_day/do_bad_place_over_public_year/last_world_and_world/long_place.py and come_large_day_about_time/public_child/want_problem/new_problem_and_part/go_point.py at new hand

---
+++ come_large_day_about_time/public_child/want_problem/new_problem_and_part/go_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_other_government_beneath_other_number(str_arg):
+    part(str_arg)
+    print('large_woman')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_other_government_beneath_other_number('few_way_or_different_hand')
---
+++ large_case_and_good_problem/new_world_and_first_day/do_bad_place_over_public_year/last_world_and_world/long_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing_and_problem(str_arg):
+    know_able_person(str_arg)
+    print('small_way')
+
+def know_able_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing_and_problem('place_or_big_company')
