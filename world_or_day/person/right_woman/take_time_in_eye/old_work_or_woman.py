dc4ac5a044c1e5b2b11fa64973a73857b7e4cca7
Jingkai Tang <jingkaitang@gmail.com>
2014-03-16 01:41:16 +0800

    rm: work world_or_day/person/right_woman/take_time_in_eye/old_work_or_woman.py from new part

---
+++ world_or_day/person/right_woman/take_time_in_eye/old_work_or_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    thing(str_arg)
+    print('think_fact_from_same_woman')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('problem')
