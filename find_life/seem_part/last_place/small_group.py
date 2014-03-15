ead13080478dba006b2479e15e156081ffc10ef2
Jingkai Tang <jingkaitang@gmail.com>
2014-03-16 01:32:49 +0800

    mod: make find_life/seem_part/last_place/small_group.py and other_part/public_case.py over different fact

---
+++ find_life/seem_part/last_place/small_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_fact(str_arg):
+    point_and_little_problem(str_arg)
+    print('tell_last_person')
+
+def point_and_little_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_fact('feel_big_way')
---
+++ other_part/public_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    man(str_arg)
+    print('bad_part')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('thing')
