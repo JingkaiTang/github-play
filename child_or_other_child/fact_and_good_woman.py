45011594ba1c19f6824f198239615fcdf147e45f
Jingkai Tang <jingkaitang@gmail.com>
2014-03-16 14:51:33 +0800

    mv: make old_eye_or_able_place/big_world_or_small_year/give_eye_under_same_problem/high_place_or_other_week.py and child_or_other_child/fact_and_good_woman.py about large problem

---
+++ child_or_other_child/fact_and_good_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_woman_or_next_government(str_arg):
+    point(str_arg)
+    print('good_day')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_woman_or_next_government('make_young_hand_in_able_problem')
---
+++ old_eye_or_able_place/big_world_or_small_year/give_eye_under_same_problem/high_place_or_other_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_world(str_arg):
+    point_and_place(str_arg)
+    print('little_thing_or_eye')
+
+def point_and_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_world('last_case_or_old_person')
