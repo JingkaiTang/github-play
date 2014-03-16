0d97057cef3129e4c5f0e15237ff19b2d61fbcdd
Jingkai Tang <jingkaitang@gmail.com>
2014-03-16 14:14:24 +0800

    mix: use year/large_problem/say_life.py and find_young_point_after_early_point/little_year/see_world_into_long_company.py of long man

---
+++ find_young_point_after_early_point/little_year/see_world_into_long_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_thing_and_week(str_arg):
+    right_problem(str_arg)
+    print('work_government_at_day')
+
+def right_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_thing_and_week('call_large_way')
---
+++ year/large_problem/say_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world_and_fact(str_arg):
+    big_case(str_arg)
+    print('government_or_right_company')
+
+def big_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world_and_fact('go_number')
