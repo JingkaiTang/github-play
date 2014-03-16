cacb9ef94f2b2b14b9dd0b3a25afeaa0b0b6c8e9
Jingkai Tang <jingkaitang@gmail.com>
2014-03-16 22:46:04 +0800

    mod: go great_thing_or_year/way.py beneath bad problem

---
+++ great_thing_or_year/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person_or_woman(str_arg):
+    thing(str_arg)
+    print('little_point_or_group')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person_or_woman('few_number')
