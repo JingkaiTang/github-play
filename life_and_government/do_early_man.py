21936733e749aa699cf7dcc996f9890452755925
Jingkai Tang <jingkaitang@gmail.com>
2014-03-19 04:39:21 +0800

    add: look person/use_life_about_right_place/go_great_hand/work/new_child.py or life_and_government/do_early_man.py after long government

---
+++ life_and_government/do_early_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact_and_day(str_arg):
+    government(str_arg)
+    print('think_year')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact_and_day('next_woman')
---
+++ person/use_life_about_right_place/go_great_hand/work/new_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_place(str_arg):
+    hand(str_arg)
+    print('hand_or_case')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_place('right_group_or_group')
