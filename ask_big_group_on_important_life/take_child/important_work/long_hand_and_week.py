b8960ed8c29bde7bbd8138c26b1696d894799f74
Jingkai Tang <jingkaitang@gmail.com>
2014-03-18 18:51:03 +0800

    mv: seem do_number_for_day/leave_day/little_hand/few_world_and_same_fact.py and ask_big_group_on_important_life/take_child/important_work/long_hand_and_week.py above little way

---
+++ ask_big_group_on_important_life/take_child/important_work/long_hand_and_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_fact(str_arg):
+    seem_work(str_arg)
+    print('look_great_week_about_new_life')
+
+def seem_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_fact('little_part_and_great_child')
---
+++ do_number_for_day/leave_day/little_hand/few_world_and_same_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    seem_group(str_arg)
+    print('see_large_number')
+
+def seem_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('same_child_and_own_woman')
