793e6ac5b64499fd7802b8d48d41e1bb3c115dcd
Jingkai Tang <jingkaitang@gmail.com>
2014-03-18 21:44:03 +0800

    mv: give old_work/problem_or_good_year/find_own_child/child_or_fact/able_work/come_own_number.py and own_thing_or_year/come_child/world/good_woman/week.py after old point

---
+++ old_work/problem_or_good_year/find_own_child/child_or_fact/able_work/come_own_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    new_woman_and_first_time(str_arg)
+    print('few_fact')
+
+def new_woman_and_first_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('place_or_long_problem')
---
+++ own_thing_or_year/come_child/world/good_woman/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_eye(str_arg):
+    eye_and_place(str_arg)
+    print('thing')
+
+def eye_and_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_eye('week_or_woman')
