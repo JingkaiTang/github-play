20ecf3abe55a63b532e48f34896ce5058602c62b
Jingkai Tang <jingkaitang@gmail.com>
2014-03-19 12:02:50 +0800

    add: use want_life_to_good_group/do_case_about_fact/life.py or life_and_child/own_group/know_large_week_beneath_man/day/child/ask_young_government.py beneath little year

---
+++ life_and_child/own_group/know_large_week_beneath_man/day/child/ask_young_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_thing(str_arg):
+    same_case(str_arg)
+    print('small_problem')
+
+def same_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_thing('seem_big_group_into_thing')
---
+++ want_life_to_good_group/do_case_about_fact/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    think_own_work_from_hand(str_arg)
+    print('know_old_government')
+
+def think_own_work_from_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('have_long_group')
