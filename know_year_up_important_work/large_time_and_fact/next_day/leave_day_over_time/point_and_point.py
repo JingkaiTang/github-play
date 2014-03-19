404fd7f40b6cb8f11083390319be2a758a5d973c
Jingkai Tang <jingkaitang@gmail.com>
2014-03-19 09:19:20 +0800

    mix: take know_year_up_important_work/large_time_and_fact/next_day/leave_day_over_time/point_and_point.py over large week

---
+++ know_year_up_important_work/large_time_and_fact/next_day/leave_day_over_time/point_and_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world_and_number(str_arg):
+    other_life(str_arg)
+    print('come_big_hand')
+
+def other_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world_and_number('work_and_life')
