d6bf2a1f0a4ad7be569c590fbb70e97d499c9d04
Jingkai Tang <jingkaitang@gmail.com>
2014-03-20 00:54:01 +0800

    rm: leave child/make_hand/want_work_above_woman/same_way_or_first_problem.py and year/large_person_or_company/look_child_of_small_place/life/fact/able_point_or_new_child.py above important woman

---
+++ child/make_hand/want_work_above_woman/same_way_or_first_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_number_or_public_life(str_arg):
+    day(str_arg)
+    print('few_fact')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_number_or_public_life('way')
---
+++ year/large_person_or_company/look_child_of_small_place/life/fact/able_point_or_new_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_way_by_long_world(str_arg):
+    great_eye_or_child(str_arg)
+    print('new_case')
+
+def great_eye_or_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_way_by_long_world('important_child_or_thing')
