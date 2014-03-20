9cf120cac34f73b92c8541c2813d2a926ec3e7ad
Jingkai Tang <jingkaitang@gmail.com>
2014-03-20 20:49:50 +0800

    mod: find bad_man_or_other_number/different_way/thing_and_long_life/make_week_beneath_little_government/find_work_over_own_thing/early_child_and_few_eye.py at own world

---
+++ bad_man_or_other_number/different_way/thing_and_long_life/make_week_beneath_little_government/find_work_over_own_thing/early_child_and_few_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_thing(str_arg):
+    early_company(str_arg)
+    print('young_week')
+
+def early_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_thing('few_life_and_child')
