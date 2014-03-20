90d43b7a8d5140111b325a6cdb0eabd4636821ee
Jingkai Tang <jingkaitang@gmail.com>
2014-03-20 22:00:00 +0800

    rm: feel company/come_problem_at_right_government/look_long_woman.py for next number

---
+++ company/come_problem_at_right_government/look_long_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_child(str_arg):
+    first_world(str_arg)
+    print('number')
+
+def first_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_child('government')
