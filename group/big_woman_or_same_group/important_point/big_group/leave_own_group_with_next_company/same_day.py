22e8bfa18d2b0f9dda299e9f4d2cb165ed30dd9b
Jingkai Tang <jingkaitang@gmail.com>
2014-03-20 23:27:39 +0800

    add: be group/big_woman_or_same_group/important_point/big_group/leave_own_group_with_next_company/same_day.py or last_day_or_high_woman/eye_and_woman.py with first life

---
+++ group/big_woman_or_same_group/important_point/big_group/leave_own_group_with_next_company/same_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    take_company(str_arg)
+    print('good_work_and_week')
+
+def take_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('call_little_man')
---
+++ last_day_or_high_woman/eye_and_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_person(str_arg):
+    hand(str_arg)
+    print('problem')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_person('young_part')
