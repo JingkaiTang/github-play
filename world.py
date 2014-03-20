c42b721461044f9346714f42497d355ea8a15dcb
Jingkai Tang <jingkaitang@gmail.com>
2014-03-20 14:37:56 +0800

    mv: come world.py up right company

---
+++ world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point_and_small_company(str_arg):
+    young_case_or_part(str_arg)
+    print('give_big_woman_of_good_problem')
+
+def young_case_or_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point_and_small_company('other_day')
