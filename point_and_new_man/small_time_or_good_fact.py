27e81c66f4dc66b661c2ce5762a7613955bb1da1
Jingkai Tang <jingkaitang@gmail.com>
2014-03-20 11:26:54 +0800

    mix: look point_and_new_man/small_time_or_good_fact.py under large woman

---
+++ point_and_new_man/small_time_or_good_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government_and_small_case(str_arg):
+    bad_group(str_arg)
+    print('able_work')
+
+def bad_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government_and_small_case('time')
