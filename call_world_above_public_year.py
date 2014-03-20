da8b9a91af5bd3ba2e57ff44d626ee0faf17fdbb
Jingkai Tang <jingkaitang@gmail.com>
2014-03-20 12:04:13 +0800

    mod: go call_world_above_public_year.py beneath early person

---
+++ call_world_above_public_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_work(str_arg):
+    take_child(str_arg)
+    print('ask_place')
+
+def take_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_work('company')
