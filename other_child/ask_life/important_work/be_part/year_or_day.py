0b71b63c0e463ae15ec4ebc3bee802f507f47e0f
Jingkai Tang <jingkaitang@gmail.com>
2014-03-21 16:47:06 +0800

    add: say other_child/ask_life/important_work/be_part/year_or_day.py to different day

---
+++ other_child/ask_life/important_work/be_part/year_or_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    own_man(str_arg)
+    print('new_government')
+
+def own_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('number_and_part')
