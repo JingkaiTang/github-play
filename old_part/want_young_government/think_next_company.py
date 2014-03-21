5bae71d3525e464330aabdcb207ea10984baf5d1
Jingkai Tang <jingkaitang@gmail.com>
2014-03-21 22:46:19 +0800

    add: tell old_part/want_young_government/think_next_company.py on last place

---
+++ old_part/want_young_government/think_next_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    man_and_part(str_arg)
+    print('place_or_time')
+
+def man_and_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('little_person_or_woman')
