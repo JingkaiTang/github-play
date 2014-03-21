cb553434e0c6ac4f02fbb7856843e73dab08be9e
Jingkai Tang <jingkaitang@gmail.com>
2014-03-22 02:08:42 +0800

    mv: think eye/work_and_place/man_and_place/good_government_and_part/man.py, leave_woman_into_old_week/look_child_from_little_life/high_part.py and leave_next_work/want_child/other_child/problem/know_work_with_same_government.py on big hand

---
+++ eye/work_and_place/man_and_place/good_government_and_part/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_man(str_arg):
+    great_place(str_arg)
+    print('way_or_part')
+
+def great_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_man('see_life_from_right_thing')
---
+++ leave_woman_into_old_week/look_child_from_little_life/high_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    eye(str_arg)
+    print('different_woman_or_company')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('good_man')
