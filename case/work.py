4665f7aad9b8cc614e56bbe920635221e5dd1095
Jingkai Tang <jingkaitang@gmail.com>
2014-03-22 01:00:07 +0800

    mix: want case/work.py from same work

---
+++ case/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_eye(str_arg):
+    work_day(str_arg)
+    print('way')
+
+def work_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_eye('own_time')
