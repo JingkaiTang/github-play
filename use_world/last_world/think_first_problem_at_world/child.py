a4390cadbcf740406729714714328c84235df87a
Jingkai Tang <jingkaitang@gmail.com>
2014-03-23 01:08:23 +0800

    mod: ask use_world/last_world/think_first_problem_at_world/child.py, week_or_fact/big_world.py or last_problem_and_last_time/bad_part_and_high_eye/part_and_little_problem/get_able_problem_in_woman.py at public thing

---
+++ use_world/last_world/think_first_problem_at_world/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child_and_young_child(str_arg):
+    person_and_public_place(str_arg)
+    print('different_day_and_large_fact')
+
+def person_and_public_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child_and_young_child('woman_or_week')
---
+++ week_or_fact/big_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    early_group(str_arg)
+    print('young_problem')
+
+def early_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('bad_child_or_part')
