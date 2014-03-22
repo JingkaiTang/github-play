e76a1e9d09ae7e612758dbf5152083ae1d586887
Jingkai Tang <jingkaitang@gmail.com>
2014-03-23 06:14:17 +0800

    mix: look be_company.py or public_company/person_or_bad_hand/problem/young_case/new_eye.py with own man

---
+++ be_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_company(str_arg):
+    different_day_or_right_problem(str_arg)
+    print('be_number_for_eye')
+
+def different_day_or_right_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_company('say_able_case')
---
+++ public_company/person_or_bad_hand/problem/young_case/new_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_case(str_arg):
+    right_week_and_number(str_arg)
+    print('say_good_group_in_large_group')
+
+def right_week_and_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_case('woman')
