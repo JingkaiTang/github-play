853d66e705b23ec22ca430be786466aeed5724ee
Jingkai Tang <jingkaitang@gmail.com>
2014-03-24 09:43:47 +0800

    mod: give young_group/great_year_or_person/young_place/thing_and_person/think_different_company/last_place_or_way.py to own case

---
+++ young_group/great_year_or_person/young_place/thing_and_person/think_different_company/last_place_or_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    get_own_place_up_man(str_arg)
+    print('own_part')
+
+def get_own_place_up_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('eye_or_government')
