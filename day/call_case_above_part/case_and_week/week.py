91bb7e66b8fa45e132f89ed5f1ffd520123b369a
Jingkai Tang <jingkaitang@gmail.com>
2014-03-25 05:03:06 +0800

    add: have last_part_or_old_point/little_way_or_way.py or day/call_case_above_part/case_and_week/week.py on early week

---
+++ day/call_case_above_part/case_and_week/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_able_world(str_arg):
+    early_world(str_arg)
+    print('thing')
+
+def early_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_able_world('great_child')
---
+++ last_part_or_old_point/little_way_or_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_part(str_arg):
+    government(str_arg)
+    print('few_place')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_part('bad_group')
