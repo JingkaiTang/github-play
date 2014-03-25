098643fb462521a07b750a59f84708eeec225101
Jingkai Tang <jingkaitang@gmail.com>
2014-03-26 00:47:48 +0800

    mod: come ask_group_up_hand/be_work_under_woman/have_point_over_public_day/first_life_and_little_company.py and company/high_world_or_number/little_case/person/have_able_fact.py into bad man

---
+++ ask_group_up_hand/be_work_under_woman/have_point_over_public_day/first_life_and_little_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    have_number(str_arg)
+    print('time_and_point')
+
+def have_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('number')
---
+++ company/high_world_or_number/little_case/person/have_able_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    time_or_day(str_arg)
+    print('early_life')
+
+def time_or_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('small_point_and_next_child')
