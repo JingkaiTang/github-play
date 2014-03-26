b1306a94ce9198122b2a004a40cd0eae05b43674
Jingkai Tang <jingkaitang@gmail.com>
2014-03-26 23:03:01 +0800

    mv: come next_year_or_world/way/leave_young_problem_with_good_company.py above young thing

---
+++ next_year_or_world/way/leave_young_problem_with_good_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_woman(str_arg):
+    ask_part(str_arg)
+    print('call_part_for_few_life')
+
+def ask_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_woman('case')
