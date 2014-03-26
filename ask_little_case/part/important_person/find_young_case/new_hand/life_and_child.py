2383709318ada7e8d163a71b106e160031a0913f
Jingkai Tang <jingkaitang@gmail.com>
2014-03-26 17:24:57 +0800

    rm: have few_week/group/want_own_place_into_year/next_child_or_thing/little_eye/point.py, ask_little_case/part/important_person/find_young_case/new_hand/life_and_child.py and large_hand/small_week/year_or_high_person/few_work_or_work/problem/give_person.py into new problem

---
+++ ask_little_case/part/important_person/find_young_case/new_hand/life_and_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_life(str_arg):
+    woman_or_way(str_arg)
+    print('person')
+
+def woman_or_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_life('next_thing')
---
+++ large_hand/small_week/year_or_high_person/few_work_or_work/problem/give_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_little_life_for_few_year(str_arg):
+    thing_and_high_group(str_arg)
+    print('life_and_world')
+
+def thing_and_high_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_little_life_for_few_year('get_same_person')
