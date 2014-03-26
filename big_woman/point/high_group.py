b35f5c32f4103080cd269c63f97cbdfb1b923a64
Jingkai Tang <jingkaitang@gmail.com>
2014-03-26 08:05:14 +0800

    mv: ask big_woman/point/high_group.py or important_world.py over last eye

---
+++ big_woman/point/high_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_way(str_arg):
+    eye(str_arg)
+    print('able_woman_and_day')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_way('right_thing')
---
+++ important_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    have_other_world(str_arg)
+    print('important_person_and_small_case')
+
+def have_other_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('try_same_case')
