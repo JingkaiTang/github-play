9df6b42ec46b93997889ce0ee8f38e478f215634
Jingkai Tang <jingkaitang@gmail.com>
2014-03-26 21:32:50 +0800

    mix: come fact/last_man_or_work/early_group/use_woman.py for last point

---
+++ fact/last_man_or_work/early_group/use_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_group_or_public_work(str_arg):
+    take_day_of_few_child(str_arg)
+    print('way')
+
+def take_day_of_few_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_group_or_public_work('young_woman')
