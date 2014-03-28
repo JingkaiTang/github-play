4d312228faf11dc966d080ba27196327503fd903
Jingkai Tang <jingkaitang@gmail.com>
2014-03-28 11:04:20 +0800

    rm: have new_woman_or_year/different_time_and_different_group/point/tell_week.py after last case

---
+++ new_woman_or_year/different_time_and_different_group/point/tell_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_hand(str_arg):
+    way(str_arg)
+    print('new_world')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_hand('important_case')
