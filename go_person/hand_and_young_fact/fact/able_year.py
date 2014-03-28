6902910121b0ba6decf92d70a5cb85b0dfbd979f
Jingkai Tang <jingkaitang@gmail.com>
2014-03-29 02:28:16 +0800

    mod: see other_woman/have_different_problem_beneath_different_hand/leave_big_way/same_year/same_day_or_number.py or go_person/hand_and_young_fact/fact/able_year.py in old fact

---
+++ go_person/hand_and_young_fact/fact/able_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    want_important_eye(str_arg)
+    print('young_life')
+
+def want_important_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('point')
---
+++ other_woman/have_different_problem_beneath_different_hand/leave_big_way/same_year/same_day_or_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_government(str_arg):
+    great_day(str_arg)
+    print('few_life_and_high_problem')
+
+def great_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_government('know_last_place')
