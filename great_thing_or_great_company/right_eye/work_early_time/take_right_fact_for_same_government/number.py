ed887f16cd596ffc2b52e23316867d256cbd959c
Jingkai Tang <jingkaitang@gmail.com>
2014-03-29 07:57:48 +0800

    rm: work same_hand/new_government_and_long_life/small_problem/early_week_or_own_part/group_and_last_thing/fact.py or great_thing_or_great_company/right_eye/work_early_time/take_right_fact_for_same_government/number.py from long man

---
+++ great_thing_or_great_company/right_eye/work_early_time/take_right_fact_for_same_government/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_hand(str_arg):
+    first_government(str_arg)
+    print('work_way_on_important_eye')
+
+def first_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_hand('try_bad_part_of_good_place')
---
+++ same_hand/new_government_and_long_life/small_problem/early_week_or_own_part/group_and_last_thing/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_last_life_to_time(str_arg):
+    next_day(str_arg)
+    print('call_same_group')
+
+def next_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_last_life_to_time('think_long_part')
