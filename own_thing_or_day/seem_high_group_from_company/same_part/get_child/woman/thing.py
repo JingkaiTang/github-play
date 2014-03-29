b5cf3c1343b7c1a3ccc08abd7f8d11470e2e6165
Jingkai Tang <jingkaitang@gmail.com>
2014-03-29 15:38:47 +0800

    rm: say own_thing_or_day/seem_high_group_from_company/same_part/get_child/woman/thing.py at first time

---
+++ own_thing_or_day/seem_high_group_from_company/same_part/get_child/woman/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    fact(str_arg)
+    print('life')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('woman_and_early_time')
