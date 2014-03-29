c6796b2e91876ed0ced5b4be9c24e41691db4296
Jingkai Tang <jingkaitang@gmail.com>
2014-03-29 17:36:56 +0800

    rm: find find_thing_on_different_world.py and bad_number/leave_able_problem/thing/point.py with long government

---
+++ bad_number/leave_able_problem/thing/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_fact_beneath_small_group(str_arg):
+    last_problem(str_arg)
+    print('government')
+
+def last_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_fact_beneath_small_group('person')
---
+++ find_thing_on_different_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    able_company(str_arg)
+    print('see_next_eye_of_long_eye')
+
+def able_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('good_world')
