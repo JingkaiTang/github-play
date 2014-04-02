c3c0b950f87d81c6029744ac591e9c86c6ed0646
Jingkai Tang <jingkaitang@gmail.com>
2014-04-02 08:18:07 +0800

    mod: know life/use_small_world/great_problem/group_or_time.py and case/seem_way/go_early_eye/look_old_government.py after different hand

---
+++ case/seem_way/go_early_eye/look_old_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    few_place(str_arg)
+    print('group')
+
+def few_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('use_able_week')
---
+++ life/use_small_world/great_problem/group_or_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number_or_eye(str_arg):
+    child_or_hand(str_arg)
+    print('early_man')
+
+def child_or_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number_or_eye('give_person_above_day')
