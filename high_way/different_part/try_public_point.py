16a31dcead16a8de38f1dc71400bb712b176a8f0
Jingkai Tang <jingkaitang@gmail.com>
2014-04-02 21:17:03 +0800

    add: go woman/go_great_hand_with_young_fact.py or high_way/different_part/try_public_point.py from right government

---
+++ high_way/different_part/try_public_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_work(str_arg):
+    think_own_work_at_great_place(str_arg)
+    print('woman')
+
+def think_own_work_at_great_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_work('go_woman')
---
+++ woman/go_great_hand_with_young_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    life(str_arg)
+    print('work_large_case')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('way')
