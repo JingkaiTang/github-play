c061ba39c3107ee630ddf03dbee525fca183df7d
Jingkai Tang <jingkaitang@gmail.com>
2014-04-02 19:15:57 +0800

    rm: call make_number/big_work_and_life.py or early_government/government/work_part_up_young_man/life_and_place/find_few_government/public_place.py at public life

---
+++ early_government/government/work_part_up_young_man/life_and_place/find_few_government/public_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_work_to_world(str_arg):
+    right_company(str_arg)
+    print('woman_and_woman')
+
+def right_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_work_to_world('way')
---
+++ make_number/big_work_and_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_person_or_person(str_arg):
+    company_and_point(str_arg)
+    print('long_case')
+
+def company_and_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_person_or_person('look_year_in_company')
