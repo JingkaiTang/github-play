abd6d5fe33933719e137dd1cbc5b457d6ff581f8
Jingkai Tang <jingkaitang@gmail.com>
2014-04-02 08:56:55 +0800

    mod: have little_woman_or_early_place/young_case_or_eye/new_problem/young_hand.py about bad point

---
+++ little_woman_or_early_place/young_case_or_eye/new_problem/young_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    company_and_week(str_arg)
+    print('important_woman_or_woman')
+
+def company_and_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('company')
