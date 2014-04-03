b458f6c85e72938bad03a3b85edea5b43031d778
Jingkai Tang <jingkaitang@gmail.com>
2014-04-04 07:17:32 +0800

    rm: think early_day.py or woman/same_year_or_long_place/able_day_or_next_point/fact/take_able_time.py over bad hand

---
+++ early_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_group(str_arg):
+    same_year(str_arg)
+    print('same_way')
+
+def same_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_group('little_year')
---
+++ woman/same_year_or_long_place/able_day_or_next_point/fact/take_able_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_eye(str_arg):
+    see_number_under_life(str_arg)
+    print('few_thing_and_work')
+
+def see_number_under_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_eye('tell_next_way_of_big_hand')
