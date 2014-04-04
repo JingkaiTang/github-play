ca4be486134bb165b8dca6b400708a6372b02f6f
Jingkai Tang <jingkaitang@gmail.com>
2014-04-05 05:27:26 +0800

    mod: use new_life/do_small_hand_about_woman/say_company_after_public_problem/work_young_government/fact/other_company.py and come_day_by_high_company/day.py for big number

---
+++ come_day_by_high_company/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_man(str_arg):
+    point(str_arg)
+    print('find_few_eye_under_bad_number')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_man('old_eye_and_important_work')
---
+++ new_life/do_small_hand_about_woman/say_company_after_public_problem/work_young_government/fact/other_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_eye(str_arg):
+    problem_and_high_group(str_arg)
+    print('first_case')
+
+def problem_and_high_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_eye('same_child')
