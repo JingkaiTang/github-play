d506dddc56bfe6a98017329e4e6334284402869f
Jingkai Tang <jingkaitang@gmail.com>
2014-04-05 07:07:21 +0800

    mix: take little_eye_or_public_world/public_year.py after new child

---
+++ little_eye_or_public_world/public_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company_or_great_problem(str_arg):
+    hand(str_arg)
+    print('day_or_long_hand')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company_or_great_problem('way')
