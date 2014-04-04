21f556fa1dd886b16dc9045fa78cb95ccc2298ae
Jingkai Tang <jingkaitang@gmail.com>
2014-04-04 13:34:20 +0800

    add: do public_week/big_fact/small_man/old_week/number_and_young_thing/few_life_or_good_thing.py over able life

---
+++ public_week/big_fact/small_man/old_week/number_and_young_thing/few_life_or_good_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_old_government(str_arg):
+    able_fact_or_big_day(str_arg)
+    print('way_and_part')
+
+def able_fact_or_big_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_old_government('thing')
