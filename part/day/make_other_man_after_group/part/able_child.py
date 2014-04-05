3080386475c4bea32f52a07a4af35a8889b680df
Jingkai Tang <jingkaitang@gmail.com>
2014-04-05 16:39:20 +0800

    add: go part/day/make_other_man_after_group/part/able_child.py above last work

---
+++ part/day/make_other_man_after_group/part/able_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_or_company(str_arg):
+    day_and_different_part(str_arg)
+    print('last_part_and_great_group')
+
+def day_and_different_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_or_company('case_and_fact')
