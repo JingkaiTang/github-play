840dacb01a34711b3cd8a19fe802a86c1f82cd80
Jingkai Tang <jingkaitang@gmail.com>
2014-04-05 16:32:27 +0800

    add: leave little_number_and_place.py or government/own_eye/find_first_government.py into public case

---
+++ government/own_eye/find_first_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_day_about_day(str_arg):
+    few_work(str_arg)
+    print('old_way_and_eye')
+
+def few_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_day_about_day('tell_long_work')
---
+++ little_number_and_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    part_or_right_point(str_arg)
+    print('feel_old_child_over_first_work')
+
+def part_or_right_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('get_other_fact_on_thing')
