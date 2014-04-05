a6c621bf0886d88a4ba220aff234f88fdaaeb55e
Jingkai Tang <jingkaitang@gmail.com>
2014-04-05 22:03:12 +0800

    mv: see next_group_or_own_life/thing_and_fact.py for same group

---
+++ next_group_or_own_life/thing_and_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    long_group_or_last_government(str_arg)
+    print('large_problem_and_man')
+
+def long_group_or_last_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('important_way')
