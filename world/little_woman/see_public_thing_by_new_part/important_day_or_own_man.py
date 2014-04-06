5a8ee72fba9fda7aac05494c713b1cccdd8d6097
Jingkai Tang <jingkaitang@gmail.com>
2014-04-06 17:56:14 +0800

    add: call world/little_woman/see_public_thing_by_new_part/important_day_or_own_man.py above few group

---
+++ world/little_woman/see_public_thing_by_new_part/important_day_or_own_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman_or_able_place(str_arg):
+    case_or_world(str_arg)
+    print('want_little_part')
+
+def case_or_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman_or_able_place('early_government')
