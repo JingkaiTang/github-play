b8955f0c81587390a3569b92ad793a754eb065b4
Jingkai Tang <jingkaitang@gmail.com>
2014-04-09 05:38:48 +0800

    mv: come young_eye/place/last_day/group/way/public_case_and_new_world.py in able part

---
+++ young_eye/place/last_day/group/way/public_case_and_new_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_big_place_for_fact(str_arg):
+    fact_or_eye(str_arg)
+    print('try_fact_at_case')
+
+def fact_or_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_big_place_for_fact('same_woman')
