75947b2614ffae91a90959bdc40650aea70c5a84
Jingkai Tang <jingkaitang@gmail.com>
2014-04-09 02:18:33 +0800

    add: see few_company.py in next case

---
+++ few_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    seem_work(str_arg)
+    print('young_number')
+
+def seem_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('day_or_company')
