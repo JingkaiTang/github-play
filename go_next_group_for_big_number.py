b83a7d368eb56b09af1736f721eaf15dea08255f
Jingkai Tang <jingkaitang@gmail.com>
2014-04-09 23:54:18 +0800

    mv: go go_next_group_for_big_number.py and leave_old_way_at_government/own_work/early_day/long_woman/come_little_time_for_year.py of bad life

---
+++ go_next_group_for_big_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_hand(str_arg):
+    world_or_group(str_arg)
+    print('own_year')
+
+def world_or_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_hand('new_eye')
---
+++ leave_old_way_at_government/own_work/early_day/long_woman/come_little_time_for_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_week_and_week(str_arg):
+    same_woman(str_arg)
+    print('time')
+
+def same_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_week_and_week('feel_week')
