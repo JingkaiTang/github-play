1b91c3453c998769fd170d6d628d994784a5edd8
Jingkai Tang <jingkaitang@gmail.com>
2014-04-09 22:32:58 +0800

    mv: seem work_or_few_way/see_world_over_new_fact/look_government_into_work/think_thing/big_world/place_or_hand.py after next fact

---
+++ work_or_few_way/see_world_over_new_fact/look_government_into_work/think_thing/big_world/place_or_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    work_woman_at_company(str_arg)
+    print('have_early_thing')
+
+def work_woman_at_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('young_case')
