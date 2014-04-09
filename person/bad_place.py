6728bbf75f4e8ed514b04997f6d4a9a49909ee24
Jingkai Tang <jingkaitang@gmail.com>
2014-04-09 18:22:28 +0800

    mix: do person/bad_place.py and child_or_high_man/public_day/get_way/week/important_company/say_group.py to long place

---
+++ person/bad_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_child_beneath_week(str_arg):
+    ask_woman(str_arg)
+    print('last_group')
+
+def ask_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_child_beneath_week('see_other_thing_over_large_eye')
