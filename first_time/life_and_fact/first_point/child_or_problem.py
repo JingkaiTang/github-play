d2639c9b9851a3e14dc344386355a7907981bba6
Jingkai Tang <jingkaitang@gmail.com>
2014-04-09 19:54:01 +0800

    mv: get first_time/life_and_fact/first_point/child_or_problem.py over important hand

---
+++ first_time/life_and_fact/first_point/child_or_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_part_or_eye(str_arg):
+    large_problem(str_arg)
+    print('life')
+
+def large_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_part_or_eye('be_large_woman')
