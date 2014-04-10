c9a66cdb5aaf5e07a2792d0ac38e5bb49fa9a632
Jingkai Tang <jingkaitang@gmail.com>
2014-04-10 23:46:37 +0800

    mv: say right_hand_or_other_group/big_thing/same_week/number/first_problem/large_child.py or use_small_man.py with bad day

---
+++ right_hand_or_other_group/big_thing/same_week/number/first_problem/large_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    few_time(str_arg)
+    print('person')
+
+def few_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('feel_way')
---
+++ use_small_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_man(str_arg):
+    small_problem(str_arg)
+    print('public_time')
+
+def small_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_man('look_problem')
