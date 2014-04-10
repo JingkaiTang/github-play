f13e78c2be38bfb4a9358ec030b2f294301b1cf0
Jingkai Tang <jingkaitang@gmail.com>
2014-04-10 22:38:06 +0800

    mix: be think_old_number_about_small_work/want_number/find_way/year/ask_part_to_problem/give_public_fact.py up great number

---
+++ think_old_number_about_small_work/want_number/find_way/year/ask_part_to_problem/give_public_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    leave_new_thing(str_arg)
+    print('woman')
+
+def leave_new_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('number_or_last_work')
