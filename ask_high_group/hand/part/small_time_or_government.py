0d93db0e4307c68bde2fa43e285ce38238354f32
Jingkai Tang <jingkaitang@gmail.com>
2014-04-10 16:26:25 +0800

    mv: tell ask_high_group/hand/part/small_time_or_government.py, fact/way/try_government_under_government/leave_great_day_by_number.py and work/week/place/next_case/few_man/different_point_or_company.py beneath next life

---
+++ ask_high_group/hand/part/small_time_or_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_world_and_next_person(str_arg):
+    high_week_or_young_number(str_arg)
+    print('tell_different_number')
+
+def high_week_or_young_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_world_and_next_person('work_or_next_time')
---
+++ fact/way/try_government_under_government/leave_great_day_by_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_woman_to_different_point(str_arg):
+    government(str_arg)
+    print('different_hand')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_woman_to_different_point('public_problem')
