25925dae82671b2b4c04389c907bf5eb4b6e82ac
Jingkai Tang <jingkaitang@gmail.com>
2014-04-10 13:08:46 +0800

    mod: work use_company_in_week/tell_important_year/way_and_new_way/large_eye.py with great man

---
+++ use_company_in_week/tell_important_year/way_and_new_way/large_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    number(str_arg)
+    print('early_hand_and_right_year')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('few_life_or_right_place')
