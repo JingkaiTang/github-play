7f35833ede17728dc8afabdf4ba6c751d0d5b743
Jingkai Tang <jingkaitang@gmail.com>
2014-04-11 01:41:34 +0800

    rm: get same_man/next_part_or_young_point/do_part_beneath_point/same_government/different_eye_and_point.py or few_thing_or_small_life/year_and_week/place.py about old work

---
+++ few_thing_or_small_life/year_and_week/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_group_and_young_number(str_arg):
+    case_or_early_world(str_arg)
+    print('big_child')
+
+def case_or_early_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_group_and_young_number('big_eye_or_important_week')
---
+++ same_man/next_part_or_young_point/do_part_beneath_point/same_government/different_eye_and_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_hand_or_old_world(str_arg):
+    able_problem_or_company(str_arg)
+    print('seem_child_up_eye')
+
+def able_problem_or_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_hand_or_old_world('little_group_and_other_number')
