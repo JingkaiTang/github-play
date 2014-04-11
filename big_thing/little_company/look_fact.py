7a3ce1c739643a7c481034ac2b9933ccb41013d3
Jingkai Tang <jingkaitang@gmail.com>
2014-04-12 06:20:20 +0800

    mv: feel big_thing/little_company/look_fact.py under little group

---
+++ big_thing/little_company/look_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_child_and_way(str_arg):
+    thing(str_arg)
+    print('new_world_and_bad_person')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_child_and_way('thing')
