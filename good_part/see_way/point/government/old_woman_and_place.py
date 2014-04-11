83baa219b2cd1b69625e43f67fd3e4a8939b7240
Jingkai Tang <jingkaitang@gmail.com>
2014-04-12 03:32:18 +0800

    mod: tell good_part/see_way/point/government/old_woman_and_place.py and work_person_on_bad_government/large_world/case_or_case/new_group.py into early world

---
+++ good_part/see_way/point/government/old_woman_and_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_year_about_new_place(str_arg):
+    large_hand_or_own_fact(str_arg)
+    print('eye')
+
+def large_hand_or_own_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_year_about_new_place('group')
---
+++ work_person_on_bad_government/large_world/case_or_case/new_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_case_and_day(str_arg):
+    group_or_big_part(str_arg)
+    print('fact_and_government')
+
+def group_or_big_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_case_and_day('be_world')
