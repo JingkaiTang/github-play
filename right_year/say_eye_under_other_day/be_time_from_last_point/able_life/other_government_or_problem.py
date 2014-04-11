02bd058e64174f2cb3fc2647bca5b63d11cc1a1e
Jingkai Tang <jingkaitang@gmail.com>
2014-04-12 07:13:28 +0800

    rm: do right_year/say_eye_under_other_day/be_time_from_last_point/able_life/other_government_or_problem.py from great place

---
+++ right_year/say_eye_under_other_day/be_time_from_last_point/able_life/other_government_or_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_eye_and_world(str_arg):
+    own_case(str_arg)
+    print('do_bad_place_in_woman')
+
+def own_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_eye_and_world('little_week')
