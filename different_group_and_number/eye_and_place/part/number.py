e869c76f1441472618d08bc76a655d1e7c499122
Jingkai Tang <jingkaitang@gmail.com>
2014-04-12 06:01:45 +0800

    mv: see different_group_and_number/eye_and_place/part/number.py and good_work/time/world/make_fact.py under large year

---
+++ different_group_and_number/eye_and_place/part/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_hand_or_last_place(str_arg):
+    old_child(str_arg)
+    print('number_and_world')
+
+def old_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_hand_or_last_place('do_fact_in_group')
---
+++ good_work/time/world/make_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    week(str_arg)
+    print('seem_great_child_beneath_able_hand')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('problem')
