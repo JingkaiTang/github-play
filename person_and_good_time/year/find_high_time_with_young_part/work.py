5c1739203327eac5a66e1d9a2dbc335bb78b3548
Jingkai Tang <jingkaitang@gmail.com>
2014-04-12 22:58:30 +0800

    mix: think person_and_good_time/year/find_high_time_with_young_part/work.py by little child

---
+++ person_and_good_time/year/find_high_time_with_young_part/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_work(str_arg):
+    few_man_or_case(str_arg)
+    print('young_place')
+
+def few_man_or_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_work('big_work')
