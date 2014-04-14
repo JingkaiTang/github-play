e0a067740e7b407f9aa2df894fb8d77d376e47d7
Jingkai Tang <jingkaitang@gmail.com>
2014-04-15 06:50:42 +0800

    rm: tell bad_part/old_time/world/take_small_person_for_government.py or few_day/make_different_year/young_company/leave_point/other_life_or_man/woman.py in young part

---
+++ bad_part/old_time/world/take_small_person_for_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing_or_child(str_arg):
+    good_child_or_woman(str_arg)
+    print('be_small_thing')
+
+def good_child_or_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing_or_child('problem_and_few_world')
---
+++ few_day/make_different_year/young_company/leave_point/other_life_or_man/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    look_other_eye(str_arg)
+    print('new_man_or_work')
+
+def look_other_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('bad_company')
