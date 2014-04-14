358444bc79b42d74559bda7530874842ae409fc9
Jingkai Tang <jingkaitang@gmail.com>
2014-04-15 00:52:49 +0800

    mv: find number/old_problem/government.py or part_or_man/public_time_or_woman/part/last_thing_and_little_government/few_fact_or_company.py for right work

---
+++ number/old_problem/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_other_government(str_arg):
+    seem_eye_over_time(str_arg)
+    print('case_and_bad_problem')
+
+def seem_eye_over_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_other_government('be_long_case_above_bad_woman')
---
+++ part_or_man/public_time_or_woman/part/last_thing_and_little_government/few_fact_or_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_big_case(str_arg):
+    important_day_or_bad_world(str_arg)
+    print('go_part')
+
+def important_day_or_bad_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_big_case('important_man')
