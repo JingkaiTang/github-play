814f7a8466141167df314ca44b21ee6188266c83
Jingkai Tang <jingkaitang@gmail.com>
2014-04-15 20:25:31 +0800

    mv: see own_group/be_day.py from large number

---
+++ own_group/be_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_day(str_arg):
+    work(str_arg)
+    print('thing')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_day('hand_and_week')
