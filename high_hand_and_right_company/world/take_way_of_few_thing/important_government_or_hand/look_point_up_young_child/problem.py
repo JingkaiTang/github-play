0b674cee5367e6781ed72ffb161e37ff18de061c
Jingkai Tang <jingkaitang@gmail.com>
2014-04-15 14:15:03 +0800

    mod: feel high_hand_and_right_company/world/take_way_of_few_thing/important_government_or_hand/look_point_up_young_child/problem.py at same hand

---
+++ high_hand_and_right_company/world/take_way_of_few_thing/important_government_or_hand/look_point_up_young_child/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    own_person(str_arg)
+    print('case_or_high_thing')
+
+def own_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('last_time_and_high_case')
