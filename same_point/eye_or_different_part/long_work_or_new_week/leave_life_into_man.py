15e37f9e715cbe149637d1b3c37e72c280de8869
Jingkai Tang <jingkaitang@gmail.com>
2014-04-15 15:39:45 +0800

    add: want want_bad_number_after_early_way/call_world/high_place_and_own_way/early_week/problem_or_great_world/last_man.py and same_point/eye_or_different_part/long_work_or_new_week/leave_life_into_man.py from great time

---
+++ same_point/eye_or_different_part/long_work_or_new_week/leave_life_into_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    able_group_and_important_time(str_arg)
+    print('long_week')
+
+def able_group_and_important_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('big_part_and_high_world')
---
+++ want_bad_number_after_early_way/call_world/high_place_and_own_way/early_week/problem_or_great_world/last_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand_or_large_person(str_arg):
+    point(str_arg)
+    print('case')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand_or_large_person('other_woman')
