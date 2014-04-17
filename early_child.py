12c29761771d0b3c599ff2f625005b331d9b6fa1
Jingkai Tang <jingkaitang@gmail.com>
2014-04-18 01:12:45 +0800

    add: try next_thing/give_bad_year_by_fact/make_world_in_able_fact/case_and_hand/life.py and early_child.py after great man

---
+++ early_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_man_or_company(str_arg):
+    large_eye(str_arg)
+    print('able_day')
+
+def large_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_man_or_company('bad_problem')
---
+++ next_thing/give_bad_year_by_fact/make_world_in_able_fact/case_and_hand/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_work_or_big_day(str_arg):
+    woman_or_eye(str_arg)
+    print('place')
+
+def woman_or_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_work_or_big_day('feel_long_number_over_place')
