7108bbab83688cc0f1e7cb77f428e5fabf7a43bb
Jingkai Tang <jingkaitang@gmail.com>
2014-04-18 05:07:48 +0800

    add: tell next_company/go_little_problem/man_or_world.py or fact.py with young work

---
+++ fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life_or_other_person(str_arg):
+    part_and_fact(str_arg)
+    print('hand_or_public_place')
+
+def part_and_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life_or_other_person('take_work_over_early_hand')
---
+++ next_company/go_little_problem/man_or_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_work_and_work(str_arg):
+    same_world(str_arg)
+    print('eye_and_last_part')
+
+def same_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_work_and_work('own_eye_or_next_week')
