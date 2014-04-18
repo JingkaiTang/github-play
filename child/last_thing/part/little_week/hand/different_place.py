a3d2ec33e7628e955fcb0cf026a6c9890fcf9fe5
Jingkai Tang <jingkaitang@gmail.com>
2014-04-18 10:50:27 +0800

    mv: tell new_life/big_government_or_fact/problem_or_government.py, child/last_thing/part/little_week/hand/different_place.py or tell_long_woman_with_great_problem/able_case/old_case/public_problem.py over different life

---
+++ child/last_thing/part/little_week/hand/different_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    long_world(str_arg)
+    print('week')
+
+def long_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('get_person')
---
+++ new_life/big_government_or_fact/problem_or_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_number(str_arg):
+    eye_and_life(str_arg)
+    print('tell_year_beneath_bad_group')
+
+def eye_and_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_number('public_number')
