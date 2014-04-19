849b5d0d892608b8b54df1fb8b3b58753f56ce51
Jingkai Tang <jingkaitang@gmail.com>
2014-04-19 22:50:06 +0800

    rm: be first_government_and_eye/early_woman/little_case/great_point.py on small day

---
+++ first_government_and_eye/early_woman/little_case/great_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    see_early_part_in_next_problem(str_arg)
+    print('great_case')
+
+def see_early_part_in_next_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('bad_way')
