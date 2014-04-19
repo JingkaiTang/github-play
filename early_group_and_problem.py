d7a50c7a26c31d292659b4209d20da670399263d
Jingkai Tang <jingkaitang@gmail.com>
2014-04-19 21:22:40 +0800

    mod: be early_group_and_problem.py above other hand

---
+++ early_group_and_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_week(str_arg):
+    early_government(str_arg)
+    print('important_time')
+
+def early_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_week('right_woman_or_same_place')
