dbbcdb2cfa3ade3052e4574730b10d6bb21adc37
Jingkai Tang <jingkaitang@gmail.com>
2014-04-21 02:28:59 +0800

    mv: have do_hand/next_hand/make_work.py or place/get_own_hand/early_government_or_world/go_large_company/go_last_group_in_last_man/life.py under high week

---
+++ do_hand/next_hand/make_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    give_important_way_from_other_problem(str_arg)
+    print('eye')
+
+def give_important_way_from_other_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('own_year')
---
+++ place/get_own_hand/early_government_or_world/go_large_company/go_last_group_in_last_man/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_group(str_arg):
+    find_few_fact_into_part(str_arg)
+    print('high_government')
+
+def find_few_fact_into_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_group('government')
