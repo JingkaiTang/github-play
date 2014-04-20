7301f865d9535bd2e31d767ae62c621e847af673
Jingkai Tang <jingkaitang@gmail.com>
2014-04-20 08:51:56 +0800

    rm: get come_life/come_small_case/tell_work/day/great_time.py or say_child/week/point_or_problem/life/last_person_and_person.py by next work

---
+++ come_life/come_small_case/tell_work/day/great_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    week(str_arg)
+    print('own_eye')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('week_or_right_government')
