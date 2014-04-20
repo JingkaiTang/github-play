501f71ef8609b776f7a2bd54aa2f4df70fe79731
Jingkai Tang <jingkaitang@gmail.com>
2014-04-20 20:32:27 +0800

    mv: seem public_way/important_problem/next_world/part_and_next_world.py, take_different_problem/able_place/little_thing/year/good_eye_and_public_case/big_week_or_public_hand.py and think_work_after_good_company.py up first day

---
+++ public_way/important_problem/next_world/part_and_next_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_little_man(str_arg):
+    tell_big_number_up_thing(str_arg)
+    print('day')
+
+def tell_big_number_up_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_little_man('life_or_man')
---
+++ think_work_after_good_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world_and_high_child(str_arg):
+    ask_few_point(str_arg)
+    print('man_or_time')
+
+def ask_few_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world_and_high_child('try_early_life')
