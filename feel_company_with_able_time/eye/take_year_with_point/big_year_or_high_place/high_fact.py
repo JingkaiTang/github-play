60cae53d2417c37d10fbab7f7aa05ce266b9be8b
Jingkai Tang <jingkaitang@gmail.com>
2014-04-21 00:40:27 +0800

    mix: take feel_company_with_able_time/eye/take_year_with_point/big_year_or_high_place/high_fact.py on next hand

---
+++ feel_company_with_able_time/eye/take_year_with_point/big_year_or_high_place/high_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_early_case(str_arg):
+    see_government_on_company(str_arg)
+    print('way')
+
+def see_government_on_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_early_case('find_early_problem')
