082863223fe5ba71b9cda7519abf79977ce1f5eb
Jingkai Tang <jingkaitang@gmail.com>
2014-04-20 11:16:26 +0800

    mv: be last_number/hand/do_time.py over last place

---
+++ last_number/hand/do_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    life_and_long_number(str_arg)
+    print('right_case')
+
+def life_and_long_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('woman')
