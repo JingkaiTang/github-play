aab545ffa3942f1061c7fa662f67640c55eae934
Jingkai Tang <jingkaitang@gmail.com>
2014-04-21 01:13:34 +0800

    rm: find feel_few_company_with_different_company.py above important man

---
+++ feel_few_company_with_different_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_life(str_arg):
+    last_point(str_arg)
+    print('have_last_world')
+
+def last_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_life('work_and_work')
