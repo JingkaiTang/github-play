8fb375c50c4314ece09ca9f32c132cb9f5e94fe6
Jingkai Tang <jingkaitang@gmail.com>
2014-04-20 09:15:05 +0800

    add: look fact/little_part_and_day/early_group/group/place.py and point/want_other_case/come_different_place.py after right way

---
+++ fact/little_part_and_day/early_group/group/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_part(str_arg):
+    case(str_arg)
+    print('big_case')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_part('part')
