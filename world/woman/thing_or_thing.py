a8dae3eb277e876c4c9af881eff354de1593030c
Jingkai Tang <jingkaitang@gmail.com>
2014-04-21 09:34:57 +0800

    rm: try world/woman/thing_or_thing.py up long number

---
+++ world/woman/thing_or_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_fact(str_arg):
+    be_time(str_arg)
+    print('group')
+
+def be_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_fact('see_year_on_year')
