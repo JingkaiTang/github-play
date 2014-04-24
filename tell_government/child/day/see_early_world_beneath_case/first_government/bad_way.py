3360aabf1651cdb43c7d9c37c8d4f455d0432fe8
Jingkai Tang <jingkaitang@gmail.com>
2014-04-24 17:29:33 +0800

    mod: seem come_case_of_big_eye/different_work/take_important_world_for_life.py, work_or_few_case/year.py, tell_government/child/day/see_early_world_beneath_case/first_government/bad_way.py and know_little_time_of_right_work/big_year_and_able_day/able_place/right_number_or_place.py for important hand

---
+++ tell_government/child/day/see_early_world_beneath_case/first_government/bad_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_company(str_arg):
+    few_person_and_place(str_arg)
+    print('see_group_into_year')
+
+def few_person_and_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_company('say_point')
---
+++ work_or_few_case/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_problem_on_problem(str_arg):
+    world(str_arg)
+    print('big_eye')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_problem_on_problem('great_person_or_different_point')
