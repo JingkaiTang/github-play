a952b6fce2e99724881e7bba8a0a3109996c18c7
Jingkai Tang <jingkaitang@gmail.com>
2014-04-26 07:52:49 +0800

    mix: know own_government_or_eye/young_world_and_problem/new_number.py up own person

---
+++ own_government_or_eye/young_world_and_problem/new_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_year(str_arg):
+    life(str_arg)
+    print('make_different_number_with_large_way')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_year('other_week')
