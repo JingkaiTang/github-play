15449533265cefe8d360decdec54e0e198fa25b0
Jingkai Tang <jingkaitang@gmail.com>
2014-04-25 09:22:02 +0800

    add: get take_small_problem_from_good_person.py, seem_work_beneath_first_child/make_life_over_small_part/big_thing.py or public_fact/big_case_and_part.py in big way

---
+++ seem_work_beneath_first_child/make_life_over_small_part/big_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_group_or_long_point(str_arg):
+    work_and_day(str_arg)
+    print('find_different_week_into_day')
+
+def work_and_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_group_or_long_point('take_own_hand_over_right_time')
