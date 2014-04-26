a39ecfbd93025a3df33d0a9dd99ddb9fe6be5556
Jingkai Tang <jingkaitang@gmail.com>
2014-04-26 20:14:59 +0800

    rm: want little_time/same_way/find_woman_at_important_point/life_and_big_point/right_place.py above high person

---
+++ little_time/same_way/find_woman_at_important_point/life_and_big_point/right_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_next_company_at_big_place(str_arg):
+    year_and_number(str_arg)
+    print('right_hand')
+
+def year_and_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_next_company_at_big_place('problem')
