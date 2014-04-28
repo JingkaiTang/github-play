03a089a97442b4ea0bf781f9f4c00850a8ce5ef7
Jingkai Tang <jingkaitang@gmail.com>
2014-04-28 14:45:25 +0800

    add: get work_place_from_case/take_new_man_by_last_way.py in right case

---
+++ work_place_from_case/take_new_man_by_last_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_good_hand(str_arg):
+    see_long_man(str_arg)
+    print('try_eye_on_way')
+
+def see_long_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_good_hand('find_time_with_long_day')
