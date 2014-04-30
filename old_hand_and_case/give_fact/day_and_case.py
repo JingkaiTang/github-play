a8329cea784fa107955d50c253389444364cd899
Jingkai Tang <jingkaitang@gmail.com>
2014-04-30 09:54:37 +0800

    add: seem old_hand_and_case/give_fact/day_and_case.py on high part

---
+++ old_hand_and_case/give_fact/day_and_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_case_to_place(str_arg):
+    year(str_arg)
+    print('work_able_point')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_case_to_place('other_part')
