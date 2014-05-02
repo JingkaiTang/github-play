4cb510538c2076758a9efec8410c95b40f50babe
Jingkai Tang <jingkaitang@gmail.com>
2014-05-02 14:27:09 +0800

    rm: seem see_company_to_new_week/early_point/early_child/place_and_small_woman/first_person.py of public company

---
+++ see_company_to_new_week/early_point/early_child/place_and_small_woman/first_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_work_and_year(str_arg):
+    next_place(str_arg)
+    print('great_case')
+
+def next_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_work_and_year('world')
