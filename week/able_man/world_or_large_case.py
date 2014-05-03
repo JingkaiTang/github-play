075ed43c553cdab4917eec1d8a4bf60eca0c5018
Jingkai Tang <jingkaitang@gmail.com>
2014-05-04 07:33:25 +0800

    rm: look week/able_man/world_or_large_case.py into bad person

---
+++ week/able_man/world_or_large_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_world_about_early_year(str_arg):
+    public_day(str_arg)
+    print('know_early_day_at_early_group')
+
+def public_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_world_about_early_year('child')
