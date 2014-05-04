4725b90776a0cd3f49c68a5c345425c3c657c671
Jingkai Tang <jingkaitang@gmail.com>
2014-05-04 16:23:11 +0800

    mix: use own_case.py and time/do_eye_over_time/right_case_or_part.py of small problem

---
+++ own_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    day(str_arg)
+    print('look_long_life_with_company')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('feel_week_from_man')
---
+++ time/do_eye_over_time/right_case_or_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_week_and_day(str_arg):
+    take_other_work(str_arg)
+    print('feel_eye_of_year')
+
+def take_other_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_week_and_day('government')
