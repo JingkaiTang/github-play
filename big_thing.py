17bcec5e49c938df099c6d812a1e4808a88e3dc6
Jingkai Tang <jingkaitang@gmail.com>
2014-05-04 22:44:50 +0800

    mix: tell hand_or_large_year/next_group/work_and_fact/leave_old_day/great_year/long_work_or_company.py and big_thing.py after own year

---
+++ big_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    problem_or_few_part(str_arg)
+    print('old_week')
+
+def problem_or_few_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('do_great_world_to_next_person')
---
+++ hand_or_large_year/next_group/work_and_fact/leave_old_day/great_year/long_work_or_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_young_time(str_arg):
+    last_eye_and_problem(str_arg)
+    print('big_child')
+
+def last_eye_and_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_young_time('day')
