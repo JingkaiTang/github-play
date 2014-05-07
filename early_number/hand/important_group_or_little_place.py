f98c69a64970e5a788e417d72ff886fc705c7d7d
Jingkai Tang <jingkaitang@gmail.com>
2014-05-07 21:09:04 +0800

    mod: know early_number/hand/important_group_or_little_place.py into high number

---
+++ early_number/hand/important_group_or_little_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case_and_different_person(str_arg):
+    see_good_company_from_early_world(str_arg)
+    print('eye')
+
+def see_good_company_from_early_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case_and_different_person('long_part')
