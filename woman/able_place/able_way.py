9ace7563080f931c1927ab25970875d8f50ef99d
Jingkai Tang <jingkaitang@gmail.com>
2014-05-07 13:25:11 +0800

    mv: tell woman/able_place/able_way.py for next eye

---
+++ woman/able_place/able_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_group(str_arg):
+    right_government(str_arg)
+    print('different_day')
+
+def right_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_group('part_or_time')
