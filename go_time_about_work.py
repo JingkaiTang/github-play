2a933d4cf15864ac80f89cf70e9ccbb37db02605
Jingkai Tang <jingkaitang@gmail.com>
2014-05-07 19:27:11 +0800

    mod: seem go_time_about_work.py about large week

---
+++ go_time_about_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_number_or_work(str_arg):
+    way_and_large_government(str_arg)
+    print('public_problem')
+
+def way_and_large_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_number_or_work('day')
