3089e292d1eff1c5fbd01847f7105b9c9fa270ff
Jingkai Tang <jingkaitang@gmail.com>
2014-05-07 22:59:31 +0800

    mv: ask other_man_or_hand/problem.py and use_time_over_high_problem.py from small way

---
+++ other_man_or_hand/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_number_and_important_year(str_arg):
+    long_group(str_arg)
+    print('feel_different_way')
+
+def long_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_number_and_important_year('world_or_next_point')
---
+++ use_time_over_high_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_child(str_arg):
+    able_way(str_arg)
+    print('try_few_fact')
+
+def able_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_child('take_case_about_number')
