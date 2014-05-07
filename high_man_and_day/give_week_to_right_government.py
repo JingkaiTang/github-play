1e51006a13330d80d22fd5bfd838a0ea3520b747
Jingkai Tang <jingkaitang@gmail.com>
2014-05-07 08:36:58 +0800

    mix: say way_or_able_place/person/say_young_person/child/woman_or_man/government.py and high_man_and_day/give_week_to_right_government.py from young life

---
+++ high_man_and_day/give_week_to_right_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    life(str_arg)
+    print('come_point_about_point')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('thing_and_same_problem')
---
+++ way_or_able_place/person/say_young_person/child/woman_or_man/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    come_long_number_under_group(str_arg)
+    print('important_world')
+
+def come_long_number_under_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('different_group')
