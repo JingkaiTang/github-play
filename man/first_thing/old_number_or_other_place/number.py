677a2dda49811aae325dccde1d8aef2a207f79cc
Jingkai Tang <jingkaitang@gmail.com>
2014-05-09 05:01:25 +0800

    mod: see man/first_thing/old_number_or_other_place/number.py and old_child_and_good_week.py up first person

---
+++ man/first_thing/old_number_or_other_place/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_number(str_arg):
+    woman(str_arg)
+    print('high_life')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_number('work')
---
+++ old_child_and_good_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_group(str_arg):
+    great_man_or_life(str_arg)
+    print('problem_and_first_point')
+
+def great_man_or_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_group('bad_man')
