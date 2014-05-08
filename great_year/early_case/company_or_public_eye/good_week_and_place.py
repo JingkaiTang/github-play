de6d0a9fcef4bac23b08a88edab475bc96be2761
Jingkai Tang <jingkaitang@gmail.com>
2014-05-09 07:35:23 +0800

    add: know great_year/early_case/company_or_public_eye/good_week_and_place.py at next point

---
+++ great_year/early_case/company_or_public_eye/good_week_and_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week_and_government(str_arg):
+    day(str_arg)
+    print('day')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week_and_government('child_or_company')
