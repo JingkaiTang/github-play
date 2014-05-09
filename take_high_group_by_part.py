aada4d88ddf161e253082169bc5fe81cc1d7cc17
Jingkai Tang <jingkaitang@gmail.com>
2014-05-09 17:09:01 +0800

    add: seem use_child/do_first_hand/bad_world_and_point/great_woman_or_number/own_way.py or take_high_group_by_part.py at first way

---
+++ take_high_group_by_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_day(str_arg):
+    way_and_fact(str_arg)
+    print('problem_and_life')
+
+def way_and_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_day('call_way_for_small_group')
---
+++ use_child/do_first_hand/bad_world_and_point/great_woman_or_number/own_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_good_part_by_own_thing(str_arg):
+    tell_little_company_from_important_thing(str_arg)
+    print('high_point_or_man')
+
+def tell_little_company_from_important_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_good_part_by_own_thing('part_and_old_life')
