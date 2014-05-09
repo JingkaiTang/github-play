83ff32f13490f990c85cc56ed66937b1101d98dc
Jingkai Tang <jingkaitang@gmail.com>
2014-05-09 09:54:03 +0800

    mix: come ask_hand/right_thing/get_work.py up few part

---
+++ ask_hand/right_thing/get_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group_and_time(str_arg):
+    group_or_week(str_arg)
+    print('next_time')
+
+def group_or_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group_and_time('world')
