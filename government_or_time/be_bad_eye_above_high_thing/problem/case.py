6040294f4593b4495ce9df52ae439dec2661f0a3
Jingkai Tang <jingkaitang@gmail.com>
2014-05-09 11:59:03 +0800

    rm: say government_or_time/be_bad_eye_above_high_thing/problem/case.py into different woman

---
+++ government_or_time/be_bad_eye_above_high_thing/problem/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child_and_work(str_arg):
+    last_case(str_arg)
+    print('large_person')
+
+def last_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child_and_work('important_group')
