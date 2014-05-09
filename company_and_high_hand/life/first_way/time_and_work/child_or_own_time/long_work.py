c2474eafb798c8bbd7e5319c98a4ee960784ea0a
Jingkai Tang <jingkaitang@gmail.com>
2014-05-09 11:59:45 +0800

    mix: give company_and_high_hand/life/first_way/time_and_work/child_or_own_time/long_work.py and make_part_to_world.py to same hand

---
+++ company_and_high_hand/life/first_way/time_and_work/child_or_own_time/long_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day_and_old_way(str_arg):
+    place_and_time(str_arg)
+    print('next_woman_and_case')
+
+def place_and_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day_and_old_way('thing_and_eye')
