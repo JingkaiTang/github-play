39816124fa8820c45f1fcc229cec9f5eb815d440
Jingkai Tang <jingkaitang@gmail.com>
2014-05-09 08:37:39 +0800

    mix: tell bad_eye/old_time_or_group/problem.py to own thing

---
+++ bad_eye/old_time_or_group/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_company(str_arg):
+    work(str_arg)
+    print('little_group_or_few_work')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_company('big_time_and_able_eye')
