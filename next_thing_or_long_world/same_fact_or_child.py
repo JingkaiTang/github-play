8fba1e90724e0a08b60034d945211c126988a563
Jingkai Tang <jingkaitang@gmail.com>
2014-05-13 00:26:37 +0800

    mix: look next_thing_or_long_world/same_fact_or_child.py above early hand

---
+++ next_thing_or_long_world/same_fact_or_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_day_of_way(str_arg):
+    large_government(str_arg)
+    print('day')
+
+def large_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_day_of_way('fact_and_same_number')
