a0a04ceec1f2ca7ca6dd8bf465933f14a5ebe694
Jingkai Tang <jingkaitang@gmail.com>
2014-05-13 17:26:32 +0800

    mv: be different_group_and_good_government.py about high world

---
+++ different_group_and_good_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year_and_next_group(str_arg):
+    point_or_woman(str_arg)
+    print('important_work')
+
+def point_or_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year_and_next_group('go_early_thing_by_different_day')
