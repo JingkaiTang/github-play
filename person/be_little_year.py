e2713d22756b0eeb50c006923da593eef886d75f
Jingkai Tang <jingkaitang@gmail.com>
2014-05-13 17:45:09 +0800

    mv: feel person/be_little_year.py from old point

---
+++ person/be_little_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    fact_and_big_thing(str_arg)
+    print('woman')
+
+def fact_and_big_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('right_fact')
