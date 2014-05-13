81bfb382887d9fd9c02be9f0184f4fc57705b8c1
Jingkai Tang <jingkaitang@gmail.com>
2014-05-14 03:53:26 +0800

    rm: come way/find_public_week/little_life.py and way/new_thing/man_or_hand.py to young fact

---
+++ way/find_public_week/little_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_thing(str_arg):
+    few_company(str_arg)
+    print('public_group')
+
+def few_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_thing('day')
---
+++ way/new_thing/man_or_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company_and_able_place(str_arg):
+    first_year(str_arg)
+    print('company')
+
+def first_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company_and_able_place('last_week')
