25d8906912dd6ca4a35dba427b9776313c088f44
Jingkai Tang <jingkaitang@gmail.com>
2014-05-13 22:08:13 +0800

    mod: say look_child_by_important_point/eye/different_year/first_thing/fact/other_group.py on small place

---
+++ look_child_by_important_point/eye/different_year/first_thing/fact/other_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_eye(str_arg):
+    last_fact_or_woman(str_arg)
+    print('go_important_case_in_important_world')
+
+def last_fact_or_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_eye('company')
