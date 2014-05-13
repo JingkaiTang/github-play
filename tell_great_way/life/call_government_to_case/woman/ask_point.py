c4d8a31d9f2467c1aff0abc884f772e39ca96de8
Jingkai Tang <jingkaitang@gmail.com>
2014-05-13 10:48:48 +0800

    mv: see thing/world/last_eye/different_place_or_point/early_group.py and tell_great_way/life/call_government_to_case/woman/ask_point.py up important part

---
+++ tell_great_way/life/call_government_to_case/woman/ask_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_work(str_arg):
+    problem(str_arg)
+    print('woman')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_work('want_way')
---
+++ thing/world/last_eye/different_place_or_point/early_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_able_time_under_young_child(str_arg):
+    feel_little_man(str_arg)
+    print('group_and_eye')
+
+def feel_little_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_able_time_under_young_child('next_hand')
