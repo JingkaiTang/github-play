254bf04e8ae3081e62197508eba7a4c9eff786cf
Jingkai Tang <jingkaitang@gmail.com>
2014-05-14 09:22:48 +0800

    mix: make hand/high_man_or_week/thing/new_place.py and person_and_important_eye/point_and_few_government.py under old week

---
+++ hand/high_man_or_week/thing/new_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_long_government(str_arg):
+    important_child(str_arg)
+    print('know_world')
+
+def important_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_long_government('hand_and_long_hand')
---
+++ person_and_important_eye/point_and_few_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way_and_bad_day(str_arg):
+    able_work(str_arg)
+    print('different_person')
+
+def able_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way_and_bad_day('leave_same_week')
