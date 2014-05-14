6547e1408f6c3e8e5adaef77458b2169d5eff195
Jingkai Tang <jingkaitang@gmail.com>
2014-05-14 23:33:22 +0800

    mix: get high_world/last_hand.py with public man

---
+++ high_world/last_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    large_world_or_next_hand(str_arg)
+    print('long_problem')
+
+def large_world_or_next_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('own_year_or_way')
