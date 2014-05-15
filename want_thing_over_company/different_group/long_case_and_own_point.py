c2b0ef7e08780ff37bbf4ba4df27fb85ac116da8
Jingkai Tang <jingkaitang@gmail.com>
2014-05-15 15:46:29 +0800

    mv: make want_thing_over_company/different_group/long_case_and_own_point.py and work_part.py of important work

---
+++ want_thing_over_company/different_group/long_case_and_own_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_woman(str_arg):
+    give_hand_under_work(str_arg)
+    print('big_group')
+
+def give_hand_under_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_woman('large_life_and_small_week')
---
+++ work_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_way(str_arg):
+    week(str_arg)
+    print('come_day')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_way('leave_eye_on_next_child')
