a81a94fa273fff78f65bdf63cfe684bf9eef575e
Jingkai Tang <jingkaitang@gmail.com>
2014-05-15 20:11:20 +0800

    rm: come want_big_hand_over_world/get_great_way/man_or_case/know_fact/next_person.py, think_next_way_under_time/point/case/old_number_and_little_problem/try_able_world_on_number/first_day.py or big_part.py after large government

---
+++ think_next_way_under_time/point/case/old_number_and_little_problem/try_able_world_on_number/first_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    high_company(str_arg)
+    print('important_child')
+
+def high_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('man')
---
+++ want_big_hand_over_world/get_great_way/man_or_case/know_fact/next_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_government(str_arg):
+    week_or_life(str_arg)
+    print('last_fact_or_government')
+
+def week_or_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_government('place')
