0db97c12ab94a98fee7880d1673e50542eb68619
Jingkai Tang <jingkaitang@gmail.com>
2014-05-17 04:20:46 +0800

    rm: use other_day_and_other_child/day/big_week/company.py and life_or_little_number/work/say_other_way.py to able thing

---
+++ life_or_little_number/work/say_other_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    think_company(str_arg)
+    print('have_high_part')
+
+def think_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('place_and_day')
---
+++ other_day_and_other_child/day/big_week/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_number(str_arg):
+    own_fact_and_first_group(str_arg)
+    print('able_group')
+
+def own_fact_and_first_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_number('good_woman')
