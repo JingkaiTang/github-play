b495ef77479f968e6808cc2efa0edf4622f21c78
Jingkai Tang <jingkaitang@gmail.com>
2014-05-17 17:41:51 +0800

    mv: take man/know_little_woman_above_good_week.py in first child

---
+++ man/know_little_woman_above_good_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_way(str_arg):
+    way(str_arg)
+    print('man_or_year')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_way('leave_next_work_under_group')
