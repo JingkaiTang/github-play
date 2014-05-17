f2dc85a47bdccb3b1b1a77a6060f4fed8efed4f9
Jingkai Tang <jingkaitang@gmail.com>
2014-05-17 12:32:31 +0800

    mv: say big_government/thing_or_life.py or different_woman_or_company/eye/world/able_life/little_hand_or_long_day.py above important case

---
+++ big_government/thing_or_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_day_above_year(str_arg):
+    government(str_arg)
+    print('big_point')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_day_above_year('public_problem_or_child')
---
+++ different_woman_or_company/eye/world/able_life/little_hand_or_long_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part_or_year(str_arg):
+    thing(str_arg)
+    print('make_good_thing')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part_or_year('life')
