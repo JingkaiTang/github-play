8232b3153a5c8e65a1bb91654371d600c31ed1d7
Jingkai Tang <jingkaitang@gmail.com>
2014-05-17 20:42:44 +0800

    add: make try_other_work_on_different_case/early_life_or_week.py by long number

---
+++ try_other_work_on_different_case/early_life_or_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    look_public_case_about_hand(str_arg)
+    print('big_time')
+
+def look_public_case_about_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('man')
