524f44f44cf46d5775535b356aeac33107c1e13a
Jingkai Tang <jingkaitang@gmail.com>
2014-05-18 12:52:54 +0800

    rm: be few_man/way/early_man/tell_hand/hand_and_hand.py for public year

---
+++ few_man/way/early_man/tell_hand/hand_and_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    early_place_and_own_week(str_arg)
+    print('high_fact')
+
+def early_place_and_own_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('seem_life')
