c351a8efc7b27ae5e0b6a0bc579b77a76fc1219c
Jingkai Tang <jingkaitang@gmail.com>
2014-05-18 22:32:42 +0800

    mod: tell same_person_and_woman.py at large government

---
+++ same_person_and_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_own_woman(str_arg):
+    person(str_arg)
+    print('way')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_own_woman('other_fact_and_high_child')
