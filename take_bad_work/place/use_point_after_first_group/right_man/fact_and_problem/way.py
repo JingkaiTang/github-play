1dccd94d451a753802205aa0eef9a9d7be505773
Jingkai Tang <jingkaitang@gmail.com>
2014-05-19 21:32:03 +0800

    add: give take_bad_work/place/use_point_after_first_group/right_man/fact_and_problem/way.py by own fact

---
+++ take_bad_work/place/use_point_after_first_group/right_man/fact_and_problem/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_same_year(str_arg):
+    have_life(str_arg)
+    print('say_work')
+
+def have_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_same_year('little_week')
