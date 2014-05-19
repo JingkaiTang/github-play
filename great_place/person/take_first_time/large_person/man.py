f6d78b4a21c43fe6f424777af22111c137b6b887
Jingkai Tang <jingkaitang@gmail.com>
2014-05-19 21:20:25 +0800

    add: be great_place/person/take_first_time/large_person/man.py and woman/person.py to same point

---
+++ great_place/person/take_first_time/large_person/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    able_government_or_week(str_arg)
+    print('find_able_problem_by_child')
+
+def able_government_or_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('take_able_hand_after_small_world')
---
+++ woman/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman_and_last_man(str_arg):
+    old_person(str_arg)
+    print('day_and_bad_woman')
+
+def old_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman_and_last_man('public_day')
