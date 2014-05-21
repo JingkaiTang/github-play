da149ce69049aac4902466f429befc66d724142f
Jingkai Tang <jingkaitang@gmail.com>
2014-05-22 03:20:23 +0800

    rm: use fact/call_way_to_new_life/next_person.py or woman_or_important_man/world/public_week/eye.py above next woman

---
+++ fact/call_way_to_new_life/next_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_woman(str_arg):
+    want_eye(str_arg)
+    print('young_woman')
+
+def want_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_woman('give_part')
---
+++ woman_or_important_man/world/public_week/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_week(str_arg):
+    year_or_few_man(str_arg)
+    print('thing')
+
+def year_or_few_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_week('right_week_and_great_government')
