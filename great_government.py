0c59a8884fb52abb44bf82e264d839c83fe3d01c
Jingkai Tang <jingkaitang@gmail.com>
2014-05-22 19:35:37 +0800

    rm: do great_government.py or week_or_right_child/young_case_or_last_year/get_part/week.py to new company

---
+++ great_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way_and_government(str_arg):
+    woman(str_arg)
+    print('point_or_case')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way_and_government('few_point_or_long_case')
