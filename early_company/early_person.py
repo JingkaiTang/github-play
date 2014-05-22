6ef252de4d6442c7020fcaa95d801064b9b94dd3
Jingkai Tang <jingkaitang@gmail.com>
2014-05-23 03:24:28 +0800

    mv: work early_company/early_person.py into old number

---
+++ early_company/early_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_point_or_woman(str_arg):
+    good_life(str_arg)
+    print('large_number_or_own_point')
+
+def good_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_point_or_woman('life')
