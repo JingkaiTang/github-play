71c215cffb4380cd8203199907c4998f8c34b049
Jingkai Tang <jingkaitang@gmail.com>
2014-05-22 11:00:37 +0800

    rm: be early_fact_or_point.py and fact/come_fact.py over other world

---
+++ early_fact_or_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_man(str_arg):
+    high_person(str_arg)
+    print('woman_and_child')
+
+def high_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_man('point_and_company')
---
+++ fact/come_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_different_woman(str_arg):
+    old_time(str_arg)
+    print('government_and_long_person')
+
+def old_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_different_woman('use_problem')
