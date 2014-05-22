1ca70f92bccfeb183ce03f94becd0f9a92853080
Jingkai Tang <jingkaitang@gmail.com>
2014-05-22 22:09:49 +0800

    add: come get_large_day/new_time_and_high_part.py at same part

---
+++ get_large_day/new_time_and_high_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_public_week(str_arg):
+    look_company(str_arg)
+    print('first_problem')
+
+def look_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_public_week('great_eye')
