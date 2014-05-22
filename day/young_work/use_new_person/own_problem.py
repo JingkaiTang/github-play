aac5bdfb5f2659584f981aa8a36890d0c4403491
Jingkai Tang <jingkaitang@gmail.com>
2014-05-23 01:03:15 +0800

    add: take day/young_work/use_new_person/own_problem.py or early_work/same_life_or_little_government/own_company_or_right_point/man/person_and_other_way/give_same_person.py over great child

---
+++ day/young_work/use_new_person/own_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week_or_government(str_arg):
+    right_place_or_way(str_arg)
+    print('same_way_or_company')
+
+def right_place_or_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week_or_government('first_way')
---
+++ early_work/same_life_or_little_government/own_company_or_right_point/man/person_and_other_way/give_same_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_company(str_arg):
+    early_life(str_arg)
+    print('other_life_or_great_problem')
+
+def early_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_company('see_group')
