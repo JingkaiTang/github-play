8a1bd7541f93c4abfede31d731bf5d52b6916982
Jingkai Tang <jingkaitang@gmail.com>
2014-05-22 19:40:25 +0800

    rm: work different_person_and_big_fact.py and good_man_or_first_day/woman.py by new problem

---
+++ different_person_and_big_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    know_week_by_thing(str_arg)
+    print('seem_last_case_about_early_time')
+
+def know_week_by_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('same_fact')
---
+++ good_man_or_first_day/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    week(str_arg)
+    print('bad_time')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('give_first_case')
