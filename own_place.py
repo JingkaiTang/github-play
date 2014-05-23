3e6811b8bd5f5e80cbb1892fc4f8c3b3218ab9c3
Jingkai Tang <jingkaitang@gmail.com>
2014-05-23 22:46:44 +0800

    mv: try work_own_life_up_other_company/right_week/case_and_different_work/hand/woman_or_world/person.py or own_place.py up own number

---
+++ own_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_company_and_number(str_arg):
+    child(str_arg)
+    print('life_and_problem')
+
+def child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_company_and_number('take_world_above_young_case')
---
+++ work_own_life_up_other_company/right_week/case_and_different_work/hand/woman_or_world/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_right_company_from_world(str_arg):
+    government(str_arg)
+    print('fact')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_right_company_from_world('call_man_into_thing')
