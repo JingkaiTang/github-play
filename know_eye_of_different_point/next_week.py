d86d7d3c0d15b2b070472b26cb6712e6ade16541
Jingkai Tang <jingkaitang@gmail.com>
2014-05-25 05:46:32 +0800

    rm: work know_eye_of_different_point/next_week.py at new way

---
+++ know_eye_of_different_point/next_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_child(str_arg):
+    large_hand(str_arg)
+    print('other_number')
+
+def large_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_child('get_eye_of_world')
