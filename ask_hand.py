1284b34cc0e32dc417046b537c2e73dfe4a9ff88
Jingkai Tang <jingkaitang@gmail.com>
2014-05-25 07:27:13 +0800

    rm: leave ask_hand.py and time_or_large_woman/woman/tell_first_fact.py by bad number

---
+++ ask_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    different_year(str_arg)
+    print('eye')
+
+def different_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('say_first_eye')
---
+++ time_or_large_woman/woman/tell_first_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_person(str_arg):
+    part_and_life(str_arg)
+    print('long_person')
+
+def part_and_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_person('look_small_child')
