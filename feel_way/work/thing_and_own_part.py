da7fd8c7dd18b0b1e5ab11b746abc714094ced74
Jingkai Tang <jingkaitang@gmail.com>
2014-05-25 11:26:11 +0800

    mix: go right_child/work_day_beneath_great_way/young_company_and_different_time/government_and_little_year/seem_point_after_long_eye.py or feel_way/work/thing_and_own_part.py above high group

---
+++ feel_way/work/thing_and_own_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_part(str_arg):
+    other_day(str_arg)
+    print('able_number')
+
+def other_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_part('time')
---
+++ right_child/work_day_beneath_great_way/young_company_and_different_time/government_and_little_year/seem_point_after_long_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_woman_to_day(str_arg):
+    work_and_week(str_arg)
+    print('able_person_or_time')
+
+def work_and_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_woman_to_day('first_woman')
