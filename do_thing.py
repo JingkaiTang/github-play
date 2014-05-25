8e8b11df2d619181290ae1f0c79219f7ff6ebaa5
Jingkai Tang <jingkaitang@gmail.com>
2014-05-25 21:11:26 +0800

    rm: feel do_thing.py and eye/same_part_and_time/get_different_hand_into_large_thing/eye/long_work_and_last_person/eye_or_world.py under early number

---
+++ do_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_number_or_woman(str_arg):
+    high_company(str_arg)
+    print('think_first_thing_with_long_eye')
+
+def high_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_number_or_woman('bad_year')
---
+++ eye/same_part_and_time/get_different_hand_into_large_thing/eye/long_work_and_last_person/eye_or_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_life(str_arg):
+    call_able_world(str_arg)
+    print('do_first_hand')
+
+def call_able_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_life('different_year')
