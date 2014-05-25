ca08c1a17bc66781ef2125fa5b85756bb050b583
Jingkai Tang <jingkaitang@gmail.com>
2014-05-25 20:32:38 +0800

    rm: seem own_woman_or_hand/leave_woman_by_government.py about new case

---
+++ own_woman_or_hand/leave_woman_by_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_important_woman_about_small_day(str_arg):
+    get_good_child(str_arg)
+    print('big_hand_and_time')
+
+def get_good_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_important_woman_about_small_day('other_part')
