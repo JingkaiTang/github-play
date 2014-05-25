e51c008cf27fa2b0f4d85ead050b76b192069706
Jingkai Tang <jingkaitang@gmail.com>
2014-05-25 18:13:12 +0800

    add: say give_old_man_from_first_child.py or fact_or_early_company/world/think_place_at_few_hand/place_and_person/time.py up bad year

---
+++ fact_or_early_company/world/think_place_at_few_hand/place_and_person/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_few_year(str_arg):
+    do_able_woman_to_important_child(str_arg)
+    print('government_or_last_company')
+
+def do_able_woman_to_important_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_few_year('company')
---
+++ give_old_man_from_first_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_company_of_big_world(str_arg):
+    last_place_and_able_woman(str_arg)
+    print('give_own_group_at_good_person')
+
+def last_place_and_able_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_company_of_big_world('have_last_year')
