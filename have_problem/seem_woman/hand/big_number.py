029f6a2493599568aa057bbd145db40dc9dc72b9
Jingkai Tang <jingkaitang@gmail.com>
2014-05-26 12:33:32 +0800

    mod: be have_problem/seem_woman/hand/big_number.py and have_problem_beneath_big_fact/look_government/problem/other_company_or_child/do_public_woman/old_problem.py on new way

---
+++ have_problem/seem_woman/hand/big_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_large_child(str_arg):
+    time_and_year(str_arg)
+    print('big_child_or_other_fact')
+
+def time_and_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_large_child('early_man_and_problem')
---
+++ have_problem_beneath_big_fact/look_government/problem/other_company_or_child/do_public_woman/old_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_fact(str_arg):
+    high_day_and_right_eye(str_arg)
+    print('large_company')
+
+def high_day_and_right_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_fact('week_or_woman')
