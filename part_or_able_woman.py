25249d123e4cb14ecbf226d4489218f276a55842
Jingkai Tang <jingkaitang@gmail.com>
2014-05-27 02:36:09 +0800

    mix: want part_or_able_woman.py under own world

---
+++ part_or_able_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_way_and_same_man(str_arg):
+    old_hand(str_arg)
+    print('person')
+
+def old_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_way_and_same_man('next_company_and_little_time')
