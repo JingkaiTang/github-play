0c3518967dc72714f5a91c2fc6cac88b3ee8292b
Jingkai Tang <jingkaitang@gmail.com>
2014-05-27 10:55:40 +0800

    add: be good_way/know_old_child.py after right company

---
+++ good_way/know_old_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_group_and_able_time(str_arg):
+    think_small_world(str_arg)
+    print('get_high_hand_over_time')
+
+def think_small_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_group_and_able_time('bad_fact')
