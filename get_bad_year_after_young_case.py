2cd0c767aa71eadf09425c835f625f96bed5824f
Jingkai Tang <jingkaitang@gmail.com>
2014-05-27 18:10:07 +0800

    mix: feel get_bad_year_after_young_case.py and use_right_way_up_woman/tell_company_at_young_man/person/way_or_hand/person/part.py into good number

---
+++ get_bad_year_after_young_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    life(str_arg)
+    print('work_or_few_thing')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('great_woman_or_number')
---
+++ use_right_way_up_woman/tell_company_at_young_man/person/way_or_hand/person/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_good_part(str_arg):
+    first_point_or_person(str_arg)
+    print('have_new_hand')
+
+def first_point_or_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_good_part('try_first_week')
