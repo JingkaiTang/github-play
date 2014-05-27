8742a15430553b689e1803e2891c9f11ac385504
Jingkai Tang <jingkaitang@gmail.com>
2014-05-28 00:42:28 +0800

    mv: use problem_and_long_point/hand_and_other_way/other_way_or_able_hand/day/find_large_number_on_public_problem.py on able life

---
+++ problem_and_long_point/hand_and_other_way/other_way_or_able_hand/day/find_large_number_on_public_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child_or_little_year(str_arg):
+    company_and_life(str_arg)
+    print('think_long_time')
+
+def company_and_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child_or_little_year('other_person')
