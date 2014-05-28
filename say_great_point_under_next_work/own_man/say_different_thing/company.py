5e718dd5542a17e240a6818d57121346312dce06
Jingkai Tang <jingkaitang@gmail.com>
2014-05-29 06:48:57 +0800

    mix: tell say_great_point_under_next_work/own_man/say_different_thing/company.py of same place

---
+++ say_great_point_under_next_work/own_man/say_different_thing/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    young_week_and_great_government(str_arg)
+    print('hand')
+
+def young_week_and_great_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('day')
