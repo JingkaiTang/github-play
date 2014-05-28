91593d0e08cfd78746a39b7cf338f6e5f4eab90d
Jingkai Tang <jingkaitang@gmail.com>
2014-05-28 17:23:05 +0800

    add: find high_person_and_next_place.py for same woman

---
+++ high_person_and_next_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    number_and_other_year(str_arg)
+    print('thing_and_part')
+
+def number_and_other_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('year_and_fact')
