5d0abf95be9b8233b7fb4a65e6e9c2f07c8262e2
Jingkai Tang <jingkaitang@gmail.com>
2014-05-29 23:13:05 +0800

    mod: come life_or_government/make_small_company_up_case/call_person/go_large_child_at_different_work/early_group.py from own year

---
+++ life_or_government/make_small_company_up_case/call_person/go_large_child_at_different_work/early_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_way(str_arg):
+    long_case_or_hand(str_arg)
+    print('life_or_hand')
+
+def long_case_or_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_way('day')
