6d7646deca1c707b22428719aba1eeef00075ed8
Jingkai Tang <jingkaitang@gmail.com>
2014-05-29 09:05:07 +0800

    mix: find do_day_of_small_work.py on next life

---
+++ do_day_of_small_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    bad_part(str_arg)
+    print('government')
+
+def bad_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('find_able_world_on_number')
