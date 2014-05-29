6c3861ba45b87ca198529432662c72251bafb3fc
Jingkai Tang <jingkaitang@gmail.com>
2014-05-29 18:10:57 +0800

    add: give ask_woman/be_person/day_or_new_life/way/high_group/able_time_and_large_way.py, number/be_right_fact/last_part/eye/make_own_work_at_thing.py or problem_or_next_week/eye_or_great_group/ask_large_thing_after_problem.py above same part

---
+++ ask_woman/be_person/day_or_new_life/way/high_group/able_time_and_large_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    government(str_arg)
+    print('early_fact_or_early_man')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('public_problem_or_own_child')
---
+++ problem_or_next_week/eye_or_great_group/ask_large_thing_after_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_bad_fact(str_arg):
+    public_government(str_arg)
+    print('work_high_company')
+
+def public_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_bad_fact('few_day')
