a54dbec01634bea53aa4f07f38c1e9101e9a96da
Jingkai Tang <jingkaitang@gmail.com>
2014-05-30 11:22:55 +0800

    add: want eye_or_same_case/woman_and_young_day.py and way/child/new_world/case/last_case.py over first thing

---
+++ eye_or_same_case/woman_and_young_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_group_and_great_eye(str_arg):
+    year(str_arg)
+    print('different_week_or_time')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_group_and_great_eye('part_or_right_problem')
---
+++ way/child/new_world/case/last_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_week(str_arg):
+    great_week(str_arg)
+    print('company')
+
+def great_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_week('good_time_and_time')
