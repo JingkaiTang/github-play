839bc52b8842b63996df5b97c27c7f644a05990e
Jingkai Tang <jingkaitang@gmail.com>
2014-05-31 22:32:12 +0800

    mod: work early_woman.py above young year

---
+++ early_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_week_above_important_work(str_arg):
+    first_number(str_arg)
+    print('world_and_day')
+
+def first_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_week_above_important_work('child')
