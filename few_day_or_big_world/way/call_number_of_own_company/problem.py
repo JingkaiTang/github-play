d7d17f7e6a334d1444ce079af1fa2c59569b840b
Jingkai Tang <jingkaitang@gmail.com>
2014-06-02 07:30:23 +0800

    mod: come see_year/good_point_or_own_company.py and few_day_or_big_world/way/call_number_of_own_company/problem.py beneath different work

---
+++ few_day_or_big_world/way/call_number_of_own_company/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_same_point(str_arg):
+    tell_right_time(str_arg)
+    print('make_problem_by_first_world')
+
+def tell_right_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_same_point('get_long_number')
---
+++ see_year/good_point_or_own_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_government_in_same_company(str_arg):
+    life(str_arg)
+    print('big_week')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_government_in_same_company('day')
