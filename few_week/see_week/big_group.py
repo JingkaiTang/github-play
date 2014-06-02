87ce0d4b2b4979834a1f06528154671b54c25458
Jingkai Tang <jingkaitang@gmail.com>
2014-06-02 23:08:03 +0800

    add: do man.py or few_week/see_week/big_group.py beneath same hand

---
+++ few_week/see_week/big_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_day(str_arg):
+    big_government(str_arg)
+    print('big_man')
+
+def big_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_day('able_man')
--- man.py
+++ man.py
@@ -1,12 +1,12 @@
 
 #! /usr/bin/env python
 
-def leave_high_case_by_first_life(str_arg):
-    find_other_number_over_company(str_arg)
-    print('young_fact')
+def week(str_arg):
+    want_government(str_arg)
+    print('group')
 
-def find_other_number_over_company(str_arg):
+def want_government(str_arg):
     print(str_arg)
 
 if __name__ == '__main__':
-    leave_high_case_by_first_life('time_and_early_time')
+    week('able_work')
