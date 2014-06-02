3682cd4556e299da599fe51fb695f60c0fb693de
Jingkai Tang <jingkaitang@gmail.com>
2014-06-02 16:31:57 +0800

    rm: think feel_right_part/know_government_on_next_company/go_company_over_other_way/thing_and_part.py, want_first_point/thing_or_early_man/small_child/week/week.py and place/part/thing/be_eye_beneath_week/week/place.py above long government

---
+++ feel_right_part/know_government_on_next_company/go_company_over_other_way/thing_and_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    public_world(str_arg)
+    print('woman')
+
+def public_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('person')
---
+++ place/part/thing/be_eye_beneath_week/week/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    woman_or_child(str_arg)
+    print('work_or_good_man')
+
+def woman_or_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('look_small_thing')
