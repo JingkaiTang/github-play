df685f7f64076038238712c0fd4e7510394fdc8c
Jingkai Tang <jingkaitang@gmail.com>
2014-06-04 01:42:14 +0800

    add: use first_year/say_woman_into_last_case/come_bad_problem_of_first_number/year.py to next point

---
+++ first_year/say_woman_into_last_case/come_bad_problem_of_first_number/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_week_in_other_man(str_arg):
+    see_bad_time_after_high_group(str_arg)
+    print('want_work')
+
+def see_bad_time_after_high_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_week_in_other_man('few_point')
