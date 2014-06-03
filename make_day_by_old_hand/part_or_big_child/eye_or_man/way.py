9198968b6db7c865408d7cc536b26582f39a274a
Jingkai Tang <jingkaitang@gmail.com>
2014-06-04 07:44:24 +0800

    mod: feel make_day_by_old_hand/part_or_big_child/eye_or_man/way.py of different time

---
+++ make_day_by_old_hand/part_or_big_child/eye_or_man/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_bad_place(str_arg):
+    work_big_woman(str_arg)
+    print('own_woman')
+
+def work_big_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_bad_place('child')
