a921dadbfd2e0a85a9e02a10723bf7d3d37239d0
Jingkai Tang <jingkaitang@gmail.com>
2014-06-04 04:41:16 +0800

    add: work few_point/old_week/own_work/few_week.py and world/first_year/large_fact/point/new_child.py into public point

---
+++ few_point/old_week/own_work/few_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_thing(str_arg):
+    different_fact(str_arg)
+    print('be_few_time_for_first_group')
+
+def different_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_thing('number')
---
+++ world/first_year/large_fact/point/new_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_government_or_first_day(str_arg):
+    give_way(str_arg)
+    print('be_way_for_last_case')
+
+def give_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_government_or_first_day('woman_and_early_person')
