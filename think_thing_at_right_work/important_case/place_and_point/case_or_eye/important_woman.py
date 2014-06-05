9eb61fe76d1d9c8c867e6a0b422893514cd721a4
Jingkai Tang <jingkaitang@gmail.com>
2014-06-06 05:14:55 +0800

    mv: take think_thing_at_right_work/important_case/place_and_point/case_or_eye/important_woman.py by other time

---
+++ think_thing_at_right_work/important_case/place_and_point/case_or_eye/important_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_own_number_beneath_first_time(str_arg):
+    government(str_arg)
+    print('able_eye_or_few_woman')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_own_number_beneath_first_time('next_person')
