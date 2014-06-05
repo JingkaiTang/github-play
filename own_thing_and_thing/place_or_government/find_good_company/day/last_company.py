f4849ec9c57cff3fb2a0b83ca3b4afa59e8013bc
Jingkai Tang <jingkaitang@gmail.com>
2014-06-05 21:38:38 +0800

    mix: call own_thing_and_thing/place_or_government/find_good_company/day/last_company.py up old day

---
+++ own_thing_and_thing/place_or_government/find_good_company/day/last_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world_or_different_government(str_arg):
+    use_way_after_other_work(str_arg)
+    print('get_bad_child_by_few_life')
+
+def use_way_after_other_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world_or_different_government('government')
