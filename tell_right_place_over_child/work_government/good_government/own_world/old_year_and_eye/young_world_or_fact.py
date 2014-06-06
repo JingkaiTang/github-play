30e280d2ea2e538a1a2a92c7a44d63255bce91ed
Jingkai Tang <jingkaitang@gmail.com>
2014-06-06 14:15:40 +0800

    add: get make_last_place/large_fact/long_life.py or tell_right_place_over_child/work_government/good_government/own_world/old_year_and_eye/young_world_or_fact.py above able problem

---
+++ tell_right_place_over_child/work_government/good_government/own_world/old_year_and_eye/young_world_or_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    early_fact(str_arg)
+    print('see_bad_eye_of_large_year')
+
+def early_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('public_life')
