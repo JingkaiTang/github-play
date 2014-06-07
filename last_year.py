67c62733feb1da9dd0093803ce567b4371b31fc1
Jingkai Tang <jingkaitang@gmail.com>
2014-06-08 01:31:24 +0800

    rm: use last_year.py in new time

---
+++ last_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_part(str_arg):
+    work_and_little_work(str_arg)
+    print('week')
+
+def work_and_little_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_part('young_day_and_government')
