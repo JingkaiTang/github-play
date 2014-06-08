5106dce8669d46a9a15f4043169362c16c55a66b
Jingkai Tang <jingkaitang@gmail.com>
2014-06-08 08:25:20 +0800

    add: go own_group_or_point/new_person/own_fact/man/week_or_large_life.py and man_or_long_group/fact_and_different_fact/old_week_or_day/group_and_child.py to early group

---
+++ man_or_long_group/fact_and_different_fact/old_week_or_day/group_and_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_woman_for_long_work(str_arg):
+    way_or_young_man(str_arg)
+    print('eye_or_few_part')
+
+def way_or_young_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_woman_for_long_work('get_way')
---
+++ own_group_or_point/new_person/own_fact/man/week_or_large_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_bad_point_with_day(str_arg):
+    have_way(str_arg)
+    print('child_or_year')
+
+def have_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_bad_point_with_day('see_important_eye')
