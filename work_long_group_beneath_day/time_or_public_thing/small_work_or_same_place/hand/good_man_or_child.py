a90395f19c386a818526e28d92ba32ff7ac80908
Jingkai Tang <jingkaitang@gmail.com>
2014-06-09 06:55:36 +0800

    mv: tell work_long_group_beneath_day/time_or_public_thing/small_work_or_same_place/hand/good_man_or_child.py at early thing

---
+++ work_long_group_beneath_day/time_or_public_thing/small_work_or_same_place/hand/good_man_or_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_eye_and_place(str_arg):
+    part(str_arg)
+    print('eye')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_eye_and_place('try_child')
