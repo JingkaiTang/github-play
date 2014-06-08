3ec8b6ed6ff3a4d5ce6433a81dd2533ddccae4f0
Jingkai Tang <jingkaitang@gmail.com>
2014-06-09 04:19:10 +0800

    mv: say leave_woman/part.py after next fact

---
+++ leave_woman/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_new_child(str_arg):
+    find_government(str_arg)
+    print('world')
+
+def find_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_new_child('get_world')
