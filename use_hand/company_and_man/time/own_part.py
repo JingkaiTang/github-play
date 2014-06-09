70846ca81649b5c9c50e339205e082703a339ade
Jingkai Tang <jingkaitang@gmail.com>
2014-06-09 22:09:20 +0800

    mv: have use_hand/company_and_man/time/own_part.py to important group

---
+++ use_hand/company_and_man/time/own_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_problem(str_arg):
+    leave_group_beneath_next_government(str_arg)
+    print('important_fact')
+
+def leave_group_beneath_next_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_problem('same_work')
