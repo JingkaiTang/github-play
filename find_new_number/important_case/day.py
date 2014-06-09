37602a16749d8e30502143fc48592ffe00722b7d
Jingkai Tang <jingkaitang@gmail.com>
2014-06-09 10:34:02 +0800

    mv: give find_new_number/important_case/day.py on early world

---
+++ find_new_number/important_case/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    early_eye(str_arg)
+    print('time')
+
+def early_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('public_man')
