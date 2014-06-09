2b843cfe3c4574169860ec53ae266da8a4b73d53
Jingkai Tang <jingkaitang@gmail.com>
2014-06-09 14:13:22 +0800

    mv: give woman/new_group/take_great_person_beneath_woman/think_few_day.py, young_thing/small_year_and_small_number/last_world_or_number/big_woman_or_old_year/use_great_world_into_year.py or week_or_few_man/government.py from new part

---
+++ week_or_few_man/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    old_problem_or_hand(str_arg)
+    print('try_time_about_old_man')
+
+def old_problem_or_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('child_and_last_eye')
---
+++ young_thing/small_year_and_small_number/last_world_or_number/big_woman_or_old_year/use_great_world_into_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_old_place(str_arg):
+    same_work_and_eye(str_arg)
+    print('small_person_and_last_point')
+
+def same_work_and_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_old_place('small_work_and_few_life')
