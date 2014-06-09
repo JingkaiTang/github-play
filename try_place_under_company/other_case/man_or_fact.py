c85daec24544380faa92e1e2f9785fb0f4aa968c
Jingkai Tang <jingkaitang@gmail.com>
2014-06-10 07:06:19 +0800

    mv: be try_place_under_company/other_case/man_or_fact.py at bad child

---
+++ try_place_under_company/other_case/man_or_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_high_child_with_young_place(str_arg):
+    way(str_arg)
+    print('problem')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_high_child_with_young_place('seem_problem_over_old_world')
