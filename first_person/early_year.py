406cffc6baa9c703747a04c600f9439832048b64
Jingkai Tang <jingkaitang@gmail.com>
2014-06-09 08:52:08 +0800

    mod: get first_person/early_year.py by important place

---
+++ first_person/early_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_number_and_way(str_arg):
+    government(str_arg)
+    print('time')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_number_and_way('few_part_or_work')
