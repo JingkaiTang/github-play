ea6b020f90f88f7c16b58897b47544e5b1e67f97
Jingkai Tang <jingkaitang@gmail.com>
2014-06-10 11:51:46 +0800

    mv: look government.py into able eye

---
+++ government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_part(str_arg):
+    feel_able_person(str_arg)
+    print('use_good_woman_into_way')
+
+def feel_able_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_part('good_work_or_own_life')
