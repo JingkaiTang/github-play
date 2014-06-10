b2a1c0853b9d982880ca93b2420c17930fc1ea1b
Jingkai Tang <jingkaitang@gmail.com>
2014-06-10 17:06:54 +0800

    mod: see find_week_at_point.py by different person

---
+++ find_week_at_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    come_woman(str_arg)
+    print('call_large_government')
+
+def come_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('government')
