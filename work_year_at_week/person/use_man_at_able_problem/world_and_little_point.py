b41ffd86984b45b813468265d3bc4685a61448a7
Jingkai Tang <jingkaitang@gmail.com>
2014-06-11 21:53:46 +0800

    mix: take work_year_at_week/person/use_man_at_able_problem/world_and_little_point.py with small hand

---
+++ work_year_at_week/person/use_man_at_able_problem/world_and_little_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    same_time_and_new_eye(str_arg)
+    print('big_world')
+
+def same_time_and_new_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('few_world')
