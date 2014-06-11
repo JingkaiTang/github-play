ff06d4a533c84e1b2175893165f81d0957c46216
Jingkai Tang <jingkaitang@gmail.com>
2014-06-12 04:38:36 +0800

    mod: do give_company_of_work/high_year/way/young_life.py and year_and_life/public_man/know_good_government.py at bad point

---
+++ give_company_of_work/high_year/way/young_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_hand_and_man(str_arg):
+    same_eye(str_arg)
+    print('place')
+
+def same_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_hand_and_man('place_and_young_number')
---
+++ year_and_life/public_man/know_good_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    look_important_problem_for_group(str_arg)
+    print('look_right_eye')
+
+def look_important_problem_for_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('work_or_week')
