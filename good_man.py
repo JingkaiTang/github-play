890706703a15066ec34a37ed7ade2d01170b36eb
Jingkai Tang <jingkaitang@gmail.com>
2014-06-11 14:22:40 +0800

    mv: make good_man.py about new thing

---
+++ good_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_man(str_arg):
+    group(str_arg)
+    print('good_part')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_man('point')
