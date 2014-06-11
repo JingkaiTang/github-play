6d6506dd776636968a83f822453fbf857581cd6a
Jingkai Tang <jingkaitang@gmail.com>
2014-06-11 10:27:21 +0800

    mod: get able_life_and_last_life/year_or_group/great_hand.py or ask_way/new_part_or_case/world.py in different work

---
+++ able_life_and_last_life/year_or_group/great_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    be_fact(str_arg)
+    print('government')
+
+def be_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('year_and_great_day')
---
+++ ask_way/new_part_or_case/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_case_or_long_group(str_arg):
+    high_hand(str_arg)
+    print('go_little_case')
+
+def high_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_case_or_long_group('own_point')
