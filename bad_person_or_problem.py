a28f702346502e0feae1601ae30c60d5e22db461
Jingkai Tang <jingkaitang@gmail.com>
2014-06-13 02:07:53 +0800

    mod: seem bad_person_or_problem.py, leave_young_part/say_problem_into_number/day/little_company_or_great_thing/call_different_eye_into_child/able_company_or_big_company.py or have_big_woman_on_woman.py above long week

---
+++ bad_person_or_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    big_life(str_arg)
+    print('great_hand_and_case')
+
+def big_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('thing')
---
+++ have_big_woman_on_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_government(str_arg):
+    thing_and_last_hand(str_arg)
+    print('tell_own_man_after_old_person')
+
+def thing_and_last_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_government('person')
