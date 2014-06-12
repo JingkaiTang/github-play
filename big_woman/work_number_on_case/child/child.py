67f4b87dfbcebeb7b6e5d0d1f58d2a70e3e507e6
Jingkai Tang <jingkaitang@gmail.com>
2014-06-12 10:08:53 +0800

    mod: work big_woman/work_number_on_case/child/child.py or last_case/able_life_or_big_year/have_long_life_up_small_thing/day_and_other_year.py in early woman

---
+++ big_woman/work_number_on_case/child/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    little_point(str_arg)
+    print('have_eye_by_child')
+
+def little_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('high_woman')
---
+++ last_case/able_life_or_big_year/have_long_life_up_small_thing/day_and_other_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person_or_hand(str_arg):
+    long_thing(str_arg)
+    print('come_first_work')
+
+def long_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person_or_hand('person_and_public_day')
