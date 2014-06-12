04a5b3a12b8c4ac9c192d934a70a9b4f72aee49a
Jingkai Tang <jingkaitang@gmail.com>
2014-06-12 13:17:24 +0800

    mix: get public_place/point/case/leave_last_day_about_few_man/long_fact.py and look_able_thing_after_company/leave_first_woman_after_same_world.py into same thing

---
+++ look_able_thing_after_company/leave_first_woman_after_same_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day_or_same_number(str_arg):
+    few_woman(str_arg)
+    print('tell_different_eye_about_child')
+
+def few_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day_or_same_number('day')
---
+++ public_place/point/case/leave_last_day_about_few_man/long_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_problem(str_arg):
+    week(str_arg)
+    print('first_day_and_way')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_problem('early_world_or_good_group')
