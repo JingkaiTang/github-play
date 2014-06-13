a9269d13421fdb8b588223d3a81806750d83105b
Jingkai Tang <jingkaitang@gmail.com>
2014-06-13 13:33:17 +0800

    add: see try_child.py to public part

---
+++ try_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_group(str_arg):
+    world(str_arg)
+    print('give_new_person')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_group('company')
