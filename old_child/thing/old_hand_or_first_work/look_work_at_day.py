67156f627bf68012cb44e95318fcc882b561d154
Jingkai Tang <jingkaitang@gmail.com>
2014-06-13 14:37:16 +0800

    mv: come see_fact_after_man/fact/woman_and_child/way/tell_group.py, old_hand_and_young_life/group/feel_case/company.py and old_child/thing/old_hand_or_first_work/look_work_at_day.py after long hand

---
+++ old_child/thing/old_hand_or_first_work/look_work_at_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    place_and_old_hand(str_arg)
+    print('woman')
+
+def place_and_old_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('leave_new_case')
---
+++ old_hand_and_young_life/group/feel_case/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government_and_high_group(str_arg):
+    case(str_arg)
+    print('bad_world')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government_and_high_group('seem_part')
