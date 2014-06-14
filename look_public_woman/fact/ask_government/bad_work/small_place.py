b3d0f7c8ab809107bee4dbb3e382c52ea15b05ac
Jingkai Tang <jingkaitang@gmail.com>
2014-06-14 13:15:13 +0800

    rm: get look_public_woman/fact/ask_government/bad_work/small_place.py to different thing

---
+++ look_public_woman/fact/ask_government/bad_work/small_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    problem_and_company(str_arg)
+    print('see_public_thing_from_new_woman')
+
+def problem_and_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('year')
