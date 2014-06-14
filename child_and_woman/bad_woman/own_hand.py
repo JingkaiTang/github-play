7b449f71354c65d5e3b6223a494ccc5b7da8e4de
Jingkai Tang <jingkaitang@gmail.com>
2014-06-14 22:35:52 +0800

    rm: want child_and_woman/bad_woman/own_hand.py or different_problem/different_life/right_world/long_person.py over young number

---
+++ child_and_woman/bad_woman/own_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_woman(str_arg):
+    know_person(str_arg)
+    print('use_large_point')
+
+def know_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_woman('great_case_and_world')
---
+++ different_problem/different_life/right_world/long_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_large_week_over_big_world(str_arg):
+    world_or_child(str_arg)
+    print('public_thing')
+
+def world_or_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_large_week_over_big_world('way')
