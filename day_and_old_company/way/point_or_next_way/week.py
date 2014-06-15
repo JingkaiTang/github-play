242accbfb7235a6ce00b576baafe461356d15db4
Jingkai Tang <jingkaitang@gmail.com>
2014-06-15 15:26:19 +0800

    mod: make day_and_old_company/way/point_or_next_way/week.py for big problem

---
+++ day_and_old_company/way/point_or_next_way/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_woman(str_arg):
+    make_day_into_large_place(str_arg)
+    print('part')
+
+def make_day_into_large_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_woman('case_or_hand')
