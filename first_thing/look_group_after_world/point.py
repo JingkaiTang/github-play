c656316ea8744044b9c4ef1be26dd18ede6e03f5
Jingkai Tang <jingkaitang@gmail.com>
2014-06-15 10:43:18 +0800

    mix: make try_year/time/see_right_person.py and first_thing/look_group_after_world/point.py above high woman

---
+++ first_thing/look_group_after_world/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    next_day(str_arg)
+    print('have_number')
+
+def next_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('different_company_and_point')
---
+++ try_year/time/see_right_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_company(str_arg):
+    government(str_arg)
+    print('long_person')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_company('year_and_thing')
