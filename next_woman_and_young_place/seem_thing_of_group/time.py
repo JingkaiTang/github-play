43551e84455381a9fa017f1fb7720eb9c0a2d769
Jingkai Tang <jingkaitang@gmail.com>
2014-06-16 13:40:07 +0800

    mix: call next_woman_and_young_place/seem_thing_of_group/time.py at young part

---
+++ next_woman_and_young_place/seem_thing_of_group/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world_and_company(str_arg):
+    few_place(str_arg)
+    print('part')
+
+def few_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world_and_company('small_child')
