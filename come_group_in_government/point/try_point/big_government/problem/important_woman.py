c45836cf552dc97a8c13218ec84e5949134458df
Jingkai Tang <jingkaitang@gmail.com>
2014-06-16 11:58:24 +0800

    mix: go come_group_in_government/point/try_point/big_government/problem/important_woman.py by old child

---
+++ come_group_in_government/point/try_point/big_government/problem/important_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_work_and_able_thing(str_arg):
+    way(str_arg)
+    print('other_day_or_large_government')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_work_and_able_thing('great_time')
