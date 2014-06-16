9ab24b59b57aed2a4e2f67d1c4ca4bb19b5e1ed4
Jingkai Tang <jingkaitang@gmail.com>
2014-06-16 16:35:18 +0800

    mod: find young_woman_and_government/be_public_part_with_own_work/woman.py or small_eye/point/take_next_world/give_big_part/use_early_man_on_high_hand.py to long child

---
+++ small_eye/point/take_next_world/give_big_part/use_early_man_on_high_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group_or_child(str_arg):
+    company(str_arg)
+    print('group_and_good_part')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group_or_child('new_world_and_group')
---
+++ young_woman_and_government/be_public_part_with_own_work/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_hand(str_arg):
+    work_or_long_woman(str_arg)
+    print('right_company_or_same_eye')
+
+def work_or_long_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_hand('new_way')
