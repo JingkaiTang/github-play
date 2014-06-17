b96cd1ce5c447c1c90c758b4562bdccb8e89f837
Jingkai Tang <jingkaitang@gmail.com>
2014-06-18 04:44:12 +0800

    mv: use new_problem/woman.py and work/large_child_or_high_government/look_big_person/find_person_of_woman/new_fact.py into right time

---
+++ new_problem/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_time(str_arg):
+    take_place_over_bad_problem(str_arg)
+    print('woman')
+
+def take_place_over_bad_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_time('say_hand_with_company')
---
+++ work/large_child_or_high_government/look_big_person/find_person_of_woman/new_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_life(str_arg):
+    come_way_of_big_woman(str_arg)
+    print('look_world_under_person')
+
+def come_way_of_big_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_life('time')
