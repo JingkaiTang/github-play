c9193a9c32d7aa9bfd598712702bdab480894ae4
Jingkai Tang <jingkaitang@gmail.com>
2014-06-18 11:18:21 +0800

    mv: think eye_and_man/year.py or get_public_way_over_thing/few_man_or_big_year/try_young_government_above_great_government/thing.py by bad company

---
+++ eye_and_man/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_group(str_arg):
+    person(str_arg)
+    print('life_and_next_person')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_group('hand')
---
+++ get_public_way_over_thing/few_man_or_big_year/try_young_government_above_great_government/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_day(str_arg):
+    get_day_under_little_person(str_arg)
+    print('early_man_or_few_company')
+
+def get_day_under_little_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_day('point')
