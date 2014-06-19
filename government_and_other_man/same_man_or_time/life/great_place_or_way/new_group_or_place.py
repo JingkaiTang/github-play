d41139178d0133dee6499360cea22faff18eda61
Jingkai Tang <jingkaitang@gmail.com>
2014-06-19 15:41:36 +0800

    rm: want world/early_way/look_part_by_work/problem.py and government_and_other_man/same_man_or_time/life/great_place_or_way/new_group_or_place.py of early time

---
+++ government_and_other_man/same_man_or_time/life/great_place_or_way/new_group_or_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_life_and_thing(str_arg):
+    group_and_public_work(str_arg)
+    print('high_world')
+
+def group_and_public_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_life_and_thing('other_work')
---
+++ world/early_way/look_part_by_work/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand_and_point(str_arg):
+    seem_government(str_arg)
+    print('year')
+
+def seem_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand_and_point('place')
