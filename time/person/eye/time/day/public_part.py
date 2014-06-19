ea0fbedc216198eec77293f9bb9b967a737d8eca
Jingkai Tang <jingkaitang@gmail.com>
2014-06-19 18:29:11 +0800

    mv: get time/person/eye/time/day/public_part.py with few day

---
+++ time/person/eye/time/day/public_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_child_into_thing(str_arg):
+    make_number_of_eye(str_arg)
+    print('work_person_with_hand')
+
+def make_number_of_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_child_into_thing('early_year')
