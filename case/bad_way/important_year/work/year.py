bf4d2e3fe36174e7a811889825d737a8e6dbf798
Jingkai Tang <jingkaitang@gmail.com>
2014-06-19 22:49:36 +0800

    mod: seem case/bad_way/important_year/work/year.py and know_small_child/public_government.py under new week

---
+++ case/bad_way/important_year/work/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_work(str_arg):
+    thing(str_arg)
+    print('day')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_work('point_and_few_day')
---
+++ know_small_child/public_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_and_hand(str_arg):
+    other_week(str_arg)
+    print('group')
+
+def other_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_and_hand('government_or_woman')
