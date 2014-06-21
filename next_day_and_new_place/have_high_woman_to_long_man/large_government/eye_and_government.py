2c61eaecde8350e9613b698860c86d1a1820cc75
Jingkai Tang <jingkaitang@gmail.com>
2014-06-21 20:08:24 +0800

    mod: get next_day_and_new_place/have_high_woman_to_long_man/large_government/eye_and_government.py into few world

---
+++ next_day_and_new_place/have_high_woman_to_long_man/large_government/eye_and_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_company_from_old_number(str_arg):
+    do_company(str_arg)
+    print('number_and_next_thing')
+
+def do_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_company_from_old_number('place_or_child')
