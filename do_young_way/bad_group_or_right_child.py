c329d50a442a14c1ba7113019b963bce2263f797
Jingkai Tang <jingkaitang@gmail.com>
2014-06-21 12:43:27 +0800

    rm: work do_young_way/bad_group_or_right_child.py above other part

---
+++ do_young_way/bad_group_or_right_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_year(str_arg):
+    different_part(str_arg)
+    print('find_small_thing_from_time')
+
+def different_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_year('see_group_over_right_company')
