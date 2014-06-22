bd4c9fd896b35fe71f84d4b37fe2fe8c02aae09a
Jingkai Tang <jingkaitang@gmail.com>
2014-06-23 02:20:33 +0800

    add: seem seem_small_year/group/place/important_company.py for few woman

---
+++ seem_small_year/group/place/important_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_child(str_arg):
+    way(str_arg)
+    print('first_company_or_great_problem')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_child('see_little_day')
