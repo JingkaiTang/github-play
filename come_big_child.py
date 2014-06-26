adfd4d8b8ce27fbe3c25f18edae49c742b347dfd
Jingkai Tang <jingkaitang@gmail.com>
2014-06-27 06:43:56 +0800

    mod: do come_big_child.py into few number

---
+++ come_big_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    old_place_and_work(str_arg)
+    print('bad_time')
+
+def old_place_and_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('week_or_way')
