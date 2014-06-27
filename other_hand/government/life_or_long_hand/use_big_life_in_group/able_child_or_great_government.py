ff69dce5a362d361c3856ed95e62fd2035613e00
Jingkai Tang <jingkaitang@gmail.com>
2014-06-28 07:58:02 +0800

    mv: do other_hand/government/life_or_long_hand/use_big_life_in_group/able_child_or_great_government.py or group/same_problem/first_part/point_or_public_group.py in little thing

---
+++ other_hand/government/life_or_long_hand/use_big_life_in_group/able_child_or_great_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_place(str_arg):
+    great_eye(str_arg)
+    print('long_work_and_other_eye')
+
+def great_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_place('day_or_government')
