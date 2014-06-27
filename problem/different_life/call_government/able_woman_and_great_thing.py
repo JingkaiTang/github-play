32aaff7e4cfaff4ac570683c3f2db452f5a07196
Jingkai Tang <jingkaitang@gmail.com>
2014-06-28 05:45:47 +0800

    mv: tell problem/different_life/call_government/able_woman_and_great_thing.py under own child

---
+++ problem/different_life/call_government/able_woman_and_great_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_world_or_young_point(str_arg):
+    first_way(str_arg)
+    print('life')
+
+def first_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_world_or_young_point('little_woman')
