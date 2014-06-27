dfb3dee6f6af051e0d3f3fb9645ca17e36927f6f
Jingkai Tang <jingkaitang@gmail.com>
2014-06-27 11:49:20 +0800

    mv: call bad_work/same_life/group/place/be_group/great_government.py and government/large_number_and_other_company/small_work_and_old_year/number.py over high world

---
+++ bad_work/same_life/group/place/be_group/great_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_time_about_government(str_arg):
+    own_problem(str_arg)
+    print('little_life_or_important_number')
+
+def own_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_time_about_government('group_and_place')
---
+++ government/large_number_and_other_company/small_work_and_old_year/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_fact(str_arg):
+    be_life(str_arg)
+    print('able_woman_or_young_work')
+
+def be_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_fact('have_hand_from_week')
