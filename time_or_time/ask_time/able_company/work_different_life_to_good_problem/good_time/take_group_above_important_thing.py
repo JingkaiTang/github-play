77f3b0f3e1753449c017e56863d1720c50aad50d
Jingkai Tang <jingkaitang@gmail.com>
2014-06-28 05:11:57 +0800

    mix: leave time_or_time/ask_time/able_company/work_different_life_to_good_problem/good_time/take_group_above_important_thing.py about high day

---
+++ time_or_time/ask_time/able_company/work_different_life_to_good_problem/good_time/take_group_above_important_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_problem_and_time(str_arg):
+    few_day(str_arg)
+    print('say_other_day')
+
+def few_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_problem_and_time('same_man_and_eye')
