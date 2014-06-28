f54760603f38a162cd998e944d778ed36ba57cba
Jingkai Tang <jingkaitang@gmail.com>
2014-06-28 11:16:57 +0800

    add: use number.py after long work

--- number.py
+++ number.py
@@ -1,12 +1,12 @@
 
 #! /usr/bin/env python
 
-def small_work(str_arg):
-    same_place(str_arg)
-    print('use_person_under_good_company')
+def small_problem(str_arg):
+    hand_or_different_number(str_arg)
+    print('other_case')
 
-def same_place(str_arg):
+def hand_or_different_number(str_arg):
     print(str_arg)
 
 if __name__ == '__main__':
-    small_work('day')
+    small_problem('last_time')
