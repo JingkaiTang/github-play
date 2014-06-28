ceebde90d321cf4cb8813d83ccc5f2127d608a17
Jingkai Tang <jingkaitang@gmail.com>
2014-06-28 17:55:52 +0800

    mix: seem place/first_week/time_or_eye.py after different week

---
+++ place/first_week/time_or_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    work_and_high_company(str_arg)
+    print('day_and_week')
+
+def work_and_high_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('seem_fact_after_point')
