2b58142309d68142271bef5ac848fea99706e76e
Jingkai Tang <jingkaitang@gmail.com>
2014-06-28 23:53:57 +0800

    add: leave try_number_up_day/seem_woman_with_able_problem.py, good_man/try_day/part_or_few_group.py or big_woman/right_group/go_year/own_thing_or_week.py under different life

---
+++ try_number_up_day/seem_woman_with_able_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    old_time_and_next_eye(str_arg)
+    print('take_early_place_under_bad_person')
+
+def old_time_and_next_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('child')
