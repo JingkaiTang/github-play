3b825cf5dbf2c7e5c2e9ba9d1da88228e356f6ca
Jingkai Tang <jingkaitang@gmail.com>
2014-06-28 17:45:14 +0800

    mix: say call_new_hand_up_year/life_or_man/young_work/come_next_problem_under_public_case.py or say_important_work/part/important_year.py beneath public fact

---
+++ call_new_hand_up_year/life_or_man/young_work/come_next_problem_under_public_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    go_old_woman_in_thing(str_arg)
+    print('problem_and_public_group')
+
+def go_old_woman_in_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('do_point_by_bad_eye')
---
+++ say_important_work/part/important_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_few_problem_in_few_case(str_arg):
+    right_fact(str_arg)
+    print('day')
+
+def right_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_few_problem_in_few_case('day')
