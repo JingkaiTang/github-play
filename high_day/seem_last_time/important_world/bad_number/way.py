7e1fb13ed359723711145036692dcf76512357c3
Jingkai Tang <jingkaitang@gmail.com>
2014-07-01 18:53:22 +0800

    rm: see high_day/seem_last_time/important_world/bad_number/way.py and number.py up first child

---
+++ high_day/seem_last_time/important_world/bad_number/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_woman_or_early_eye(str_arg):
+    say_way_by_person(str_arg)
+    print('great_company')
+
+def say_way_by_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_woman_or_early_eye('old_fact_or_big_number')
--- number.py
+++ number.py
@@ -1,12 +1,12 @@
 
 #! /usr/bin/env python
 
-def small_problem(str_arg):
-    hand_or_different_number(str_arg)
-    print('other_case')
+def great_company_and_person(str_arg):
+    tell_week(str_arg)
+    print('large_hand')
 
-def hand_or_different_number(str_arg):
+def tell_week(str_arg):
     print(str_arg)
 
 if __name__ == '__main__':
-    small_problem('last_time')
+    great_company_and_person('time')
