21e137a77e9da252490b0d1f6a57e9c217c3b9f9
Jingkai Tang <jingkaitang@gmail.com>
2014-07-02 22:14:52 +0800

    add: say child/seem_right_year.py on big person

---
+++ child/seem_right_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_great_woman(str_arg):
+    person(str_arg)
+    print('next_company')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_great_woman('first_part')
