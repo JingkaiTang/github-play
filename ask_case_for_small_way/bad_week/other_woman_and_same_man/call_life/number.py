39b5b22fdf5c0b6f3cfe37d52e2f67110c9a102a
Jingkai Tang <jingkaitang@gmail.com>
2014-07-02 08:34:08 +0800

    mod: feel ask_case_for_small_way/bad_week/other_woman_and_same_man/call_life/number.py beneath last time

---
+++ ask_case_for_small_way/bad_week/other_woman_and_same_man/call_life/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part_and_last_case(str_arg):
+    bad_life_or_large_group(str_arg)
+    print('make_group')
+
+def bad_life_or_large_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part_and_last_case('early_child_or_group')
