f21f52e30881cc3ce73f78aa5ab1077773f5a1e2
Jingkai Tang <jingkaitang@gmail.com>
2014-07-03 17:20:09 +0800

    mv: take next_year.py or little_world_and_problem/want_great_company.py above public part

---
+++ little_world_and_problem/want_great_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_number_or_same_company(str_arg):
+    fact(str_arg)
+    print('make_problem_after_large_problem')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_number_or_same_company('case')
---
+++ next_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand_and_week(str_arg):
+    life(str_arg)
+    print('part')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand_and_week('tell_first_number')
