1768ff2be7537b07c7176007f57865772bf78816
Jingkai Tang <jingkaitang@gmail.com>
2014-07-04 04:35:43 +0800

    mod: ask do_early_eye/way/place_or_last_way.py or number/government/ask_life/use_last_man_for_other_number/little_place_or_right_problem.py at important week

---
+++ do_early_eye/way/place_or_last_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_bad_woman_on_eye(str_arg):
+    use_other_number(str_arg)
+    print('bad_case')
+
+def use_other_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_bad_woman_on_eye('year_or_week')
---
+++ number/government/ask_life/use_last_man_for_other_number/little_place_or_right_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_little_fact_at_big_year(str_arg):
+    bad_work(str_arg)
+    print('eye')
+
+def bad_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_little_fact_at_big_year('number')
