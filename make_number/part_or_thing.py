042479ce796080c9c8edda4d121014d308ca0acd
Jingkai Tang <jingkaitang@gmail.com>
2014-07-04 09:28:39 +0800

    mv: give make_number/part_or_thing.py or problem/seem_point_of_last_woman/part/child_or_case/year_or_little_man.py on long year

---
+++ make_number/part_or_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_time(str_arg):
+    use_early_place(str_arg)
+    print('problem_or_person')
+
+def use_early_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_time('week')
---
+++ problem/seem_point_of_last_woman/part/child_or_case/year_or_little_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_different_problem_in_important_work(str_arg):
+    group(str_arg)
+    print('leave_same_problem')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_different_problem_in_important_work('man')
