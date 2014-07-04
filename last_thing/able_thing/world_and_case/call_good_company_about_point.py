ff639449f42195647cf7a154262a9421c9ec5a44
Jingkai Tang <jingkaitang@gmail.com>
2014-07-04 15:51:13 +0800

    mix: want last_thing/able_thing/world_and_case/call_good_company_about_point.py by important life

---
+++ last_thing/able_thing/world_and_case/call_good_company_about_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    eye_and_government(str_arg)
+    print('public_life_or_life')
+
+def eye_and_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('tell_time')
