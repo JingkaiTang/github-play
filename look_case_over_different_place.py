fdc80459b8fb0fc758b905a1fc77ed7c58bc1130
Jingkai Tang <jingkaitang@gmail.com>
2014-07-06 00:57:06 +0800

    add: look look_case_over_different_place.py and thing/year_and_able_case/big_week.py under few point

---
+++ look_case_over_different_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    seem_last_life_from_able_problem(str_arg)
+    print('look_own_thing')
+
+def seem_last_life_from_able_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('big_year_and_next_way')
---
+++ thing/year_and_able_case/big_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_case_or_world(str_arg):
+    young_week(str_arg)
+    print('make_little_number')
+
+def young_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_case_or_world('few_man')
