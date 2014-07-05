81fa7468f74b9c72915ac1834d1ac089d5babe34
Jingkai Tang <jingkaitang@gmail.com>
2014-07-05 14:19:09 +0800

    rm: be little_hand/important_child/use_company_at_young_child/hand.py with next man

---
+++ little_hand/important_child/use_company_at_young_child/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_year(str_arg):
+    take_company_of_eye(str_arg)
+    print('year')
+
+def take_company_of_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_year('other_world')
