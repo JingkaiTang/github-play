68a10469e8d6be9a6d50a3b2aa83d7e019292b00
Jingkai Tang <jingkaitang@gmail.com>
2014-07-05 20:23:24 +0800

    rm: use company/feel_week/say_day/way_or_great_person.py over right place

---
+++ company/feel_week/say_day/way_or_great_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_thing(str_arg):
+    big_week(str_arg)
+    print('young_thing')
+
+def big_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_thing('small_thing')
