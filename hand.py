7f5e075502976f6a315d561d2e58aca332f0699a
Jingkai Tang <jingkaitang@gmail.com>
2014-07-05 10:05:40 +0800

    rm: try week/important_government.py and hand.py by high world

--- hand.py
+++ hand.py
@@ -1,12 +1,12 @@
 
 #! /usr/bin/env python
 
-def leave_time_about_week(str_arg):
-    find_new_thing(str_arg)
-    print('bad_year_and_large_time')
+def point(str_arg):
+    eye(str_arg)
+    print('large_place')
 
-def find_new_thing(str_arg):
+def eye(str_arg):
     print(str_arg)
 
 if __name__ == '__main__':
-    leave_time_about_week('first_child')
+    point('person')
---
+++ week/important_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point_or_important_fact(str_arg):
+    point(str_arg)
+    print('week')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point_or_important_fact('last_year')
