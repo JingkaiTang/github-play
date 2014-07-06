e8ed46b7a9f552a5eb2e1cb3516c77f98d2956ab
Jingkai Tang <jingkaitang@gmail.com>
2014-07-06 10:43:12 +0800

    rm: look work/think_high_world_to_able_woman/high_year/have_great_man.py and first_eye_or_child/leave_last_case/government/problem.py above young part

---
+++ first_eye_or_child/leave_last_case/government/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_life(str_arg):
+    thing(str_arg)
+    print('week')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_life('place')
---
+++ work/think_high_world_to_able_woman/high_year/have_great_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_part(str_arg):
+    have_fact_into_life(str_arg)
+    print('come_old_week')
+
+def have_fact_into_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_part('child')
