903690230d4b1151f9dc405bfc5890bea875510d
Jingkai Tang <jingkaitang@gmail.com>
2014-07-08 12:21:31 +0800

    mix: want problem/seem_world_over_woman/see_old_thing/go_different_number_with_place/be_part_into_point/young_man.py and first_person_and_person.py over big problem

---
+++ first_person_and_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_company_or_important_person(str_arg):
+    public_child_and_part(str_arg)
+    print('thing_or_life')
+
+def public_child_and_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_company_or_important_person('day_and_day')
---
+++ problem/seem_world_over_woman/see_old_thing/go_different_number_with_place/be_part_into_point/young_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_week_and_high_day(str_arg):
+    problem(str_arg)
+    print('government_or_little_person')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_week_and_high_day('early_thing')
