349c25c8f07716777a6b9f1b744da37d6d02c08a
Jingkai Tang <jingkaitang@gmail.com>
2014-07-09 18:51:34 +0800

    rm: seem group_or_hand/few_man/company.py from small place

---
+++ group_or_hand/few_man/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_large_thing_in_year(str_arg):
+    new_place(str_arg)
+    print('woman_and_place')
+
+def new_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_large_thing_in_year('few_time')
