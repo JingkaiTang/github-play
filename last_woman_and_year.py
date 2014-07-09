0f9b8a7ba5ee990390b01c7bca62ecf5020bc25d
Jingkai Tang <jingkaitang@gmail.com>
2014-07-09 15:01:06 +0800

    mod: be last_woman_and_year.py or public_person_and_week/work_way/able_case_or_little_child/great_number/eye.py about new place

---
+++ last_woman_and_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life_or_important_week(str_arg):
+    way_or_right_week(str_arg)
+    print('work_government')
+
+def way_or_right_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life_or_important_week('group_or_right_year')
---
+++ public_person_and_week/work_way/able_case_or_little_child/great_number/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_problem(str_arg):
+    seem_able_work_by_same_point(str_arg)
+    print('same_company')
+
+def seem_able_work_by_same_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_problem('great_government')
