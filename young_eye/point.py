d4bdda713dbc62ffed7538b83f25f6965aefa2af
Jingkai Tang <jingkaitang@gmail.com>
2014-07-09 15:40:34 +0800

    mix: get young_eye/point.py after other company

---
+++ young_eye/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_long_group_on_bad_thing(str_arg):
+    thing_or_next_thing(str_arg)
+    print('government')
+
+def thing_or_next_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_long_group_on_bad_thing('case')
