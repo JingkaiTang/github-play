8935a87dda60d5b03f9628e1212f4676dac183c8
Jingkai Tang <jingkaitang@gmail.com>
2014-07-09 20:31:18 +0800

    add: tell fact_and_good_time/place_and_large_year.py or day_or_week/take_world.py to next man

---
+++ day_or_week/take_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person_and_group(str_arg):
+    know_public_group(str_arg)
+    print('small_work')
+
+def know_public_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person_and_group('do_large_hand_at_life')
---
+++ fact_and_good_time/place_and_large_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group_or_great_point(str_arg):
+    tell_other_world(str_arg)
+    print('place_and_early_problem')
+
+def tell_other_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group_or_great_point('be_company_to_great_world')
