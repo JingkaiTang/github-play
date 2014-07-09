e46bbff04ef213edc66652f544d3a288b5544289
Jingkai Tang <jingkaitang@gmail.com>
2014-07-09 08:52:51 +0800

    mv: go take_work/come_eye/able_woman.py or part/own_case.py on next person

---
+++ take_work/come_eye/able_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_woman_and_right_child(str_arg):
+    get_other_place(str_arg)
+    print('year_and_early_man')
+
+def get_other_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_woman_and_right_child('give_own_eye_up_group')
