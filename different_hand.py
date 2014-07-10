4e560f5008ced4e9471c43d29caed27b21079a9d
Jingkai Tang <jingkaitang@gmail.com>
2014-07-10 21:20:40 +0800

    rm: go government_and_last_time.py, point_and_great_company/early_world/hand/child.py or different_hand.py to great work

---
+++ different_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    problem(str_arg)
+    print('tell_high_life')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('great_eye')
---
+++ point_and_great_company/early_world/hand/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    early_company_or_group(str_arg)
+    print('big_man_or_thing')
+
+def early_company_or_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('man')
