e2847995acf66e7cc4ed24358a3a0479cbb46ec3
Jingkai Tang <jingkaitang@gmail.com>
2014-07-10 22:16:41 +0800

    mod: work part/great_day/company/want_man/tell_little_day/government_and_different_man.py about right problem

---
+++ part/great_day/company/want_man/tell_little_day/government_and_different_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_time_under_group(str_arg):
+    leave_right_time(str_arg)
+    print('make_old_world')
+
+def leave_right_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_time_under_group('large_eye')
