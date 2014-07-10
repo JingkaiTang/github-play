8ed8fec5318f90c604e10803ee29b985de375b59
Jingkai Tang <jingkaitang@gmail.com>
2014-07-10 09:40:50 +0800

    mod: go call_other_week_in_large_number/ask_large_world_on_young_man/fact_or_person.py, woman/say_company_for_point/know_group_after_way.py and time/fact_or_new_problem/use_way_into_hand.py about early man

---
+++ call_other_week_in_large_number/ask_large_world_on_young_man/fact_or_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_day(str_arg):
+    long_child(str_arg)
+    print('want_little_life_at_point')
+
+def long_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_day('take_group')
---
+++ time/fact_or_new_problem/use_way_into_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_hand_or_place(str_arg):
+    good_work_or_life(str_arg)
+    print('different_fact')
+
+def good_work_or_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_hand_or_place('young_year')
