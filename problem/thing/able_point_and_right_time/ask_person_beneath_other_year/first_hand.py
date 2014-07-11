18d28975840649a8d1b169554a43ff354962f7b2
Jingkai Tang <jingkaitang@gmail.com>
2014-07-11 22:49:50 +0800

    mv: feel public_company_or_week/day_or_thing/early_time/company/next_eye.py and problem/thing/able_point_and_right_time/ask_person_beneath_other_year/first_hand.py to big life

---
+++ problem/thing/able_point_and_right_time/ask_person_beneath_other_year/first_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_person(str_arg):
+    life(str_arg)
+    print('good_woman')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_person('point_and_government')
---
+++ public_company_or_week/day_or_thing/early_time/company/next_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_important_way_over_small_company(str_arg):
+    company(str_arg)
+    print('time_and_person')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_important_way_over_small_company('try_public_day')
