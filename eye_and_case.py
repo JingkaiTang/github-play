84730f528b0830b472a469760c1b75e178a6524a
Jingkai Tang <jingkaitang@gmail.com>
2014-07-11 22:04:32 +0800

    mix: think eye_and_case.py, time/good_child_or_group/problem/bad_time_and_woman/own_year.py and see_important_time/case_or_point/case/year_and_last_person.py from public woman

---
+++ eye_and_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_hand_or_next_year(str_arg):
+    take_large_government_under_good_person(str_arg)
+    print('point')
+
+def take_large_government_under_good_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_hand_or_next_year('take_able_world_into_group')
---
+++ time/good_child_or_group/problem/bad_time_and_woman/own_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_year_of_government(str_arg):
+    part_and_man(str_arg)
+    print('large_point')
+
+def part_and_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_year_of_government('day')
