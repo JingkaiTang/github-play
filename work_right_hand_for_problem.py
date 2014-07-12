6a5d118efa4b67dc6727f51b5207e07caf512bd0
Jingkai Tang <jingkaitang@gmail.com>
2014-07-13 04:35:52 +0800

    add: have work_right_hand_for_problem.py with last point

---
+++ work_right_hand_for_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_day(str_arg):
+    high_man(str_arg)
+    print('good_time_or_next_case')
+
+def high_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_day('same_group')
