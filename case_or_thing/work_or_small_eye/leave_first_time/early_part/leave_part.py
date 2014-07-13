86a091a9bbbb40e7cdb8ca9e4f836f2cdb3093c2
Jingkai Tang <jingkaitang@gmail.com>
2014-07-13 11:45:00 +0800

    mix: ask case_or_thing/work_or_small_eye/leave_first_time/early_part/leave_part.py up long year

---
+++ case_or_thing/work_or_small_eye/leave_first_time/early_part/leave_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    go_government(str_arg)
+    print('next_time')
+
+def go_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('problem_or_problem')
