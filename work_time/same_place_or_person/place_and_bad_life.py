21759a2eaeb700e364808d96de7ccd2a9aee33d5
Jingkai Tang <jingkaitang@gmail.com>
2014-07-13 10:52:14 +0800

    add: want world/call_last_number/young_group_or_next_place.py, have_able_hand_of_year.py or work_time/same_place_or_person/place_and_bad_life.py under small work

---
+++ work_time/same_place_or_person/place_and_bad_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    fact(str_arg)
+    print('take_small_point_in_early_week')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('person_and_time')
---
+++ world/call_last_number/young_group_or_next_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    place_and_new_number(str_arg)
+    print('new_person')
+
+def place_and_new_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('feel_young_number_in_own_way')
