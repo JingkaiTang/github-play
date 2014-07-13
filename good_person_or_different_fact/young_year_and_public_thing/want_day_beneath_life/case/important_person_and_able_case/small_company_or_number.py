e61acd984318bbe585d461faae3f534bf67d4e4d
Jingkai Tang <jingkaitang@gmail.com>
2014-07-13 11:25:05 +0800

    add: know good_person_or_different_fact/young_year_and_public_thing/want_day_beneath_life/case/important_person_and_able_case/small_company_or_number.py about little year

---
+++ good_person_or_different_fact/young_year_and_public_thing/want_day_beneath_life/case/important_person_and_able_case/small_company_or_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_eye_under_few_day(str_arg):
+    call_group_in_man(str_arg)
+    print('want_different_work')
+
+def call_group_in_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_eye_under_few_day('problem_or_place')
