c7bfecc5945b520359cf344953e6f6358f5b9bb4
Jingkai Tang <jingkaitang@gmail.com>
2014-07-15 06:54:26 +0800

    mv: go number/go_important_hand_above_week.py or same_woman/next_world/see_young_life/little_eye_and_next_day/few_man/large_government.py of little woman

---
+++ number/go_important_hand_above_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_year(str_arg):
+    child_and_early_way(str_arg)
+    print('high_problem')
+
+def child_and_early_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_year('important_time')
---
+++ same_woman/next_world/see_young_life/little_eye_and_next_day/few_man/large_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_eye(str_arg):
+    place_or_right_child(str_arg)
+    print('think_important_point_of_number')
+
+def place_or_right_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_eye('world_or_great_thing')
