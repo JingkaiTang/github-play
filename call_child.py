19e4acf3fa2b1a06aec2d68d3da35ee5530bd0d1
Jingkai Tang <jingkaitang@gmail.com>
2014-07-14 20:33:44 +0800

    mod: ask call_child.py over able time

---
+++ call_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_year_and_new_thing(str_arg):
+    big_way_and_person(str_arg)
+    print('government')
+
+def big_way_and_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_year_and_new_thing('be_next_part')
