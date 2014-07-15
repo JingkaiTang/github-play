82f48671a212dc9344cc6cdbf5ac81f2a63e8c6b
Jingkai Tang <jingkaitang@gmail.com>
2014-07-16 07:18:59 +0800

    add: say public_week_or_good_eye/woman_or_small_place/go_right_government/woman.py or public_fact/problem/bad_time/point.py up different point

---
+++ public_fact/problem/bad_time/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_thing_about_large_life(str_arg):
+    call_group(str_arg)
+    print('old_man')
+
+def call_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_thing_about_large_life('tell_fact_for_eye')
---
+++ public_week_or_good_eye/woman_or_small_place/go_right_government/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_number(str_arg):
+    last_part(str_arg)
+    print('fact_or_person')
+
+def last_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_number('way_and_way')
