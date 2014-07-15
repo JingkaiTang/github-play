57e51836dae50a09c846c305f4e6ef5406b99e23
Jingkai Tang <jingkaitang@gmail.com>
2014-07-15 20:30:29 +0800

    mod: use do_high_point_under_same_woman/way_or_fact/want_young_woman_by_able_point/know_little_eye_beneath_other_case/feel_problem.py with new part

---
+++ do_high_point_under_same_woman/way_or_fact/want_young_woman_by_able_point/know_little_eye_beneath_other_case/feel_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    same_case(str_arg)
+    print('long_work')
+
+def same_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('person')
