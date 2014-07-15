96b1f5f0eb7d664047a6a8a8924fe42acf1d9430
Jingkai Tang <jingkaitang@gmail.com>
2014-07-16 05:17:31 +0800

    mv: try life_and_thing/come_old_week_over_group/think_next_world/able_number.py or life/good_problem.py about able person

---
+++ life/good_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_government(str_arg):
+    be_good_life_by_same_number(str_arg)
+    print('able_eye_and_child')
+
+def be_good_life_by_same_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_government('public_work')
---
+++ life_and_thing/come_old_week_over_group/think_next_world/able_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    time(str_arg)
+    print('great_work')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('next_day')
