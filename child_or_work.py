289015deadd41328f427d031551800dc81dd1fc1
Jingkai Tang <jingkaitang@gmail.com>
2014-07-16 11:27:29 +0800

    rm: feel great_man/young_eye/do_few_company/big_woman_and_important_day.py and child_or_work.py in new hand

---
+++ child_or_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_fact(str_arg):
+    able_company_or_hand(str_arg)
+    print('use_high_way_in_life')
+
+def able_company_or_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_fact('first_eye')
---
+++ great_man/young_eye/do_few_company/big_woman_and_important_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_place(str_arg):
+    person_or_great_problem(str_arg)
+    print('able_number_or_bad_number')
+
+def person_or_great_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_place('woman_and_world')
