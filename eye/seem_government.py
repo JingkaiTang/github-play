6a02acbcd3a9fda6d03b8b5de31afe854323aced
Jingkai Tang <jingkaitang@gmail.com>
2014-07-16 12:13:04 +0800

    mix: find eye/seem_government.py in great hand

---
+++ eye/seem_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person_or_public_place(str_arg):
+    call_high_person_of_other_way(str_arg)
+    print('point_and_long_company')
+
+def call_high_person_of_other_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person_or_public_place('company_or_eye')
