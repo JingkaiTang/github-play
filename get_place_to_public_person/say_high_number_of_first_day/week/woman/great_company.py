5009a0a371f3959fd8d76dc75e73f7a8f0abea93
Jingkai Tang <jingkaitang@gmail.com>
2014-07-17 20:32:46 +0800

    mix: seem part/ask_right_part_at_child/company.py or get_place_to_public_person/say_high_number_of_first_day/week/woman/great_company.py under few part

---
+++ get_place_to_public_person/say_high_number_of_first_day/week/woman/great_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_problem_after_old_company(str_arg):
+    thing_and_way(str_arg)
+    print('own_part_and_other_place')
+
+def thing_and_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_problem_after_old_company('big_thing_or_old_point')
---
+++ part/ask_right_part_at_child/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    new_world(str_arg)
+    print('day_and_life')
+
+def new_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('little_year_or_high_government')
