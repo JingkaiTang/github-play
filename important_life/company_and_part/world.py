17c6a2fa500f99f928c3afac39f6e70abbb434df
Jingkai Tang <jingkaitang@gmail.com>
2014-07-19 00:08:31 +0800

    mix: leave woman/way/next_thing/person/person/problem_and_child.py or important_life/company_and_part/world.py for few person

---
+++ important_life/company_and_part/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_life(str_arg):
+    great_problem(str_arg)
+    print('own_thing')
+
+def great_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_life('right_world')
