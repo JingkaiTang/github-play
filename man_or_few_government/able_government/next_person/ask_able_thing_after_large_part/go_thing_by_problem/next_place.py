6b70067749a325123c5bd2c98dd86e13057cbbdd
Jingkai Tang <jingkaitang@gmail.com>
2014-07-20 06:38:40 +0800

    mv: find man_or_few_government/able_government/next_person/ask_able_thing_after_large_part/go_thing_by_problem/next_place.py or man_or_right_case/do_fact_beneath_new_man/high_person/fact_or_high_government.py beneath important hand

---
+++ man_or_few_government/able_government/next_person/ask_able_thing_after_large_part/go_thing_by_problem/next_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child_and_problem(str_arg):
+    part(str_arg)
+    print('old_hand')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child_and_problem('year')
---
+++ man_or_right_case/do_fact_beneath_new_man/high_person/fact_or_high_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    look_problem(str_arg)
+    print('new_way')
+
+def look_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('problem')
