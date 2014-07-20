360fb180ba5124d280b595bbb88ed10facdb959c
Jingkai Tang <jingkaitang@gmail.com>
2014-07-20 13:53:00 +0800

    rm: use ask_case/tell_world.py or make_high_part/make_important_place_for_life.py of public thing

---
+++ ask_case/tell_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_important_time(str_arg):
+    feel_problem(str_arg)
+    print('group')
+
+def feel_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_important_time('see_old_fact_from_woman')
---
+++ make_high_part/make_important_place_for_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_case_and_small_case(str_arg):
+    case(str_arg)
+    print('work_high_world')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_case_and_small_case('week')
