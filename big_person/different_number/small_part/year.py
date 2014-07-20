fcaca7c8094592e05f31cb60539ba72f434a7181
Jingkai Tang <jingkaitang@gmail.com>
2014-07-20 18:42:19 +0800

    rm: get big_person/different_number/small_part/year.py and company/company/year_or_case/make_week/problem/old_problem_and_important_thing.py on different way

---
+++ big_person/different_number/small_part/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    look_big_day(str_arg)
+    print('eye_or_public_week')
+
+def look_big_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('few_work')
---
+++ company/company/year_or_case/make_week/problem/old_problem_and_important_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_day_in_great_thing(str_arg):
+    work_fact(str_arg)
+    print('hand')
+
+def work_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_day_in_great_thing('eye_and_able_point')
