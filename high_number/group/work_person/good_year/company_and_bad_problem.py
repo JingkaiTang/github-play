1abc711d7122ab9e0e73c81b8fc42fc972ec4886
Jingkai Tang <jingkaitang@gmail.com>
2014-07-21 14:06:33 +0800

    rm: call important_woman_and_few_fact/want_number_under_own_problem.py, know_day_of_next_way/own_point/leave_government_at_way.py or high_number/group/work_person/good_year/company_and_bad_problem.py about long point

---
+++ high_number/group/work_person/good_year/company_and_bad_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    work(str_arg)
+    print('government')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('hand')
---
+++ important_woman_and_few_fact/want_number_under_own_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_part(str_arg):
+    do_group_under_high_life(str_arg)
+    print('next_hand')
+
+def do_group_under_high_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_part('part')
