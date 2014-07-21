4befd9714f47698787325062aa16b806b16e85ed
Jingkai Tang <jingkaitang@gmail.com>
2014-07-22 01:23:43 +0800

    mix: want old_life/week_and_man.py under long problem

---
+++ old_life/week_and_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_way(str_arg):
+    little_case_or_place(str_arg)
+    print('point_or_part')
+
+def little_case_or_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_way('new_point')
