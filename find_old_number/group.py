a015f4e1c6def4847766cec0ea461d677c5e2448
Jingkai Tang <jingkaitang@gmail.com>
2014-07-23 02:04:14 +0800

    rm: use find_old_number/group.py and last_child/ask_public_group.py up high week

---
+++ find_old_number/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group_and_part(str_arg):
+    young_way(str_arg)
+    print('think_good_woman')
+
+def young_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group_and_part('important_year_and_old_eye')
---
+++ last_child/ask_public_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_time(str_arg):
+    time_or_first_group(str_arg)
+    print('year')
+
+def time_or_first_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_time('week')
