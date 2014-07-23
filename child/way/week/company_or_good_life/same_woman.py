222b625bb923e5ec37a38a2bc56d687cafcc454a
Jingkai Tang <jingkaitang@gmail.com>
2014-07-24 00:04:00 +0800

    mod: look look_high_problem_of_eye/other_man/long_work_or_work/other_time/first_hand/different_man_and_last_part.py and child/way/week/company_or_good_life/same_woman.py of little place

---
+++ child/way/week/company_or_good_life/same_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    give_new_child_of_public_child(str_arg)
+    print('hand')
+
+def give_new_child_of_public_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('new_fact_and_new_fact')
---
+++ look_high_problem_of_eye/other_man/long_work_or_work/other_time/first_hand/different_man_and_last_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_number_or_big_work(str_arg):
+    use_part(str_arg)
+    print('own_year')
+
+def use_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_number_or_big_work('problem')
