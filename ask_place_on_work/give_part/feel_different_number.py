d27e2598526e0f2527765de30c308de5cb6f165b
Jingkai Tang <jingkaitang@gmail.com>
2014-07-24 23:11:30 +0800

    mv: take way/able_woman_and_same_eye/point/old_eye_and_woman/high_eye_and_hand/hand_or_early_problem.py and ask_place_on_work/give_part/feel_different_number.py for first way

---
+++ ask_place_on_work/give_part/feel_different_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_hand_or_next_fact(str_arg):
+    public_place(str_arg)
+    print('world')
+
+def public_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_hand_or_next_fact('case')
---
+++ way/able_woman_and_same_eye/point/old_eye_and_woman/high_eye_and_hand/hand_or_early_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    week_or_day(str_arg)
+    print('old_group')
+
+def week_or_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('think_number_of_long_fact')
