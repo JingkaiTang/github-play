f92e982aabf406566cd0c5825b5124857fb24cad
Jingkai Tang <jingkaitang@gmail.com>
2014-07-25 05:12:00 +0800

    mix: get early_part_or_able_year/take_other_group/go_other_group/first_work/right_part.py on right part

---
+++ early_part_or_able_year/take_other_group/go_other_group/first_work/right_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_way(str_arg):
+    time(str_arg)
+    print('call_hand')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_way('life')
