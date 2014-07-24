7d1045a196eeb183b96ac7459c5327497d1b71ed
Jingkai Tang <jingkaitang@gmail.com>
2014-07-24 23:47:50 +0800

    rm: try new_person/tell_good_hand_at_year/go_great_government/few_group/want_large_problem_up_long_day.py and use_old_part_at_early_day/small_work/able_year.py after few group

---
+++ new_person/tell_good_hand_at_year/go_great_government/few_group/want_large_problem_up_long_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year_or_young_life(str_arg):
+    do_small_work(str_arg)
+    print('case_or_large_world')
+
+def do_small_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year_or_young_life('old_woman')
---
+++ use_old_part_at_early_day/small_work/able_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_fact(str_arg):
+    high_thing(str_arg)
+    print('call_time_to_young_company')
+
+def high_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_fact('find_last_man')
