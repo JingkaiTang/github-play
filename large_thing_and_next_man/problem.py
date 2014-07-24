72a71eaceb79c0d13638ef8197409399bccfa73d
Jingkai Tang <jingkaitang@gmail.com>
2014-07-25 01:04:55 +0800

    mix: feel large_thing_and_next_man/problem.py at young child

---
+++ large_thing_and_next_man/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    able_number(str_arg)
+    print('hand_and_life')
+
+def able_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('look_child')
