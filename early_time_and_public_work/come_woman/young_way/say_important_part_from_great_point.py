4d31ffa3405fbdc28c4765705a42bd3847cafce9
Jingkai Tang <jingkaitang@gmail.com>
2014-07-25 20:51:10 +0800

    mv: want early_time_and_public_work/come_woman/young_way/say_important_part_from_great_point.py and time/great_woman_and_high_week/call_first_number/small_eye/time.py under little part

---
+++ early_time_and_public_work/come_woman/young_way/say_important_part_from_great_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_eye(str_arg):
+    new_thing(str_arg)
+    print('world')
+
+def new_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_eye('high_problem')
---
+++ time/great_woman_and_high_week/call_first_number/small_eye/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    find_hand_to_number(str_arg)
+    print('case')
+
+def find_hand_to_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('number_and_company')
