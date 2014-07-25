1eadcf4a0575c048a0e9c8cfbbc419273cdf8479
Jingkai Tang <jingkaitang@gmail.com>
2014-07-25 16:46:30 +0800

    mix: seem right_man_and_week/few_child_and_week/place/seem_year.py for large day

---
+++ right_man_and_week/few_child_and_week/place/seem_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week_and_first_point(str_arg):
+    world_or_day(str_arg)
+    print('last_problem')
+
+def world_or_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week_and_first_point('day')
