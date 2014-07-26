df4e085ba1a4cb289fa083cc69f646f798ec30ff
Jingkai Tang <jingkaitang@gmail.com>
2014-07-27 01:08:30 +0800

    rm: do get_fact/woman/case/hand/big_government.py under few way

---
+++ get_fact/woman/case/hand/big_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_hand_up_early_number(str_arg):
+    week(str_arg)
+    print('work')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_hand_up_early_number('do_person')
