8e012eaa6a2296113463004461b23efede9d6967
Jingkai Tang <jingkaitang@gmail.com>
2014-07-27 14:20:51 +0800

    add: be last_time_and_little_day/hand_and_thing.py under important man

---
+++ last_time_and_little_day/hand_and_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_case(str_arg):
+    little_eye(str_arg)
+    print('group')
+
+def little_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_case('way')
