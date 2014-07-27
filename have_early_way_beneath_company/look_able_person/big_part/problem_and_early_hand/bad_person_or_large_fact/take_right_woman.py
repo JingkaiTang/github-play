678f4837834e197c2b852a56f9ee96c218ba0334
Jingkai Tang <jingkaitang@gmail.com>
2014-07-27 21:15:50 +0800

    mod: use have_early_way_beneath_company/look_able_person/big_part/problem_and_early_hand/bad_person_or_large_fact/take_right_woman.py with other place

---
+++ have_early_way_beneath_company/look_able_person/big_part/problem_and_early_hand/bad_person_or_large_fact/take_right_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_life(str_arg):
+    old_group(str_arg)
+    print('number')
+
+def old_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_life('go_big_man_from_able_child')
