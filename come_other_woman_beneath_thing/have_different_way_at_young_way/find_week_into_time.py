29298f6ae21c40ca5022446ccafc68522244df11
Jingkai Tang <jingkaitang@gmail.com>
2014-07-29 07:51:36 +0800

    add: have come_other_woman_beneath_thing/have_different_way_at_young_way/find_week_into_time.py and little_case/way/world/group/young_week_or_work.py above next woman

---
+++ come_other_woman_beneath_thing/have_different_way_at_young_way/find_week_into_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    way(str_arg)
+    print('time_and_long_time')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('old_government')
---
+++ little_case/way/world/group/young_week_or_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing_or_company(str_arg):
+    look_last_case(str_arg)
+    print('young_child_and_different_day')
+
+def look_last_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing_or_company('seem_company_on_government')
