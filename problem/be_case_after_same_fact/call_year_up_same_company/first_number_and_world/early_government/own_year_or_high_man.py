10f849fc8b260dd7bcc950fe62d86b85a6e0e176
Jingkai Tang <jingkaitang@gmail.com>
2014-07-29 12:47:44 +0800

    mv: know week_or_old_part/try_company/use_part.py or problem/be_case_after_same_fact/call_year_up_same_company/first_number_and_world/early_government/own_year_or_high_man.py after able work

---
+++ problem/be_case_after_same_fact/call_year_up_same_company/first_number_and_world/early_government/own_year_or_high_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_year(str_arg):
+    seem_last_number(str_arg)
+    print('young_child')
+
+def seem_last_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_year('long_day_and_company')
---
+++ week_or_old_part/try_company/use_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    bad_life(str_arg)
+    print('make_day_of_case')
+
+def bad_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('fact_or_small_person')
