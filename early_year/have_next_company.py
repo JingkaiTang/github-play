4038321b339b678dd75c6ae9b6ae1f137dfa2551
Jingkai Tang <jingkaitang@gmail.com>
2014-07-29 22:10:01 +0800

    rm: look early_year/have_next_company.py to same eye

---
+++ early_year/have_next_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_case(str_arg):
+    bad_government(str_arg)
+    print('person_or_small_part')
+
+def bad_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_case('eye')
