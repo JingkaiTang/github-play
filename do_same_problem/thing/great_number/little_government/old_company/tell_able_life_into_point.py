c2bf9d8a7672391e2b523861444fbb594b3c9c0e
Jingkai Tang <jingkaitang@gmail.com>
2014-07-30 05:30:52 +0800

    mod: say do_same_problem/thing/great_number/little_government/old_company/tell_able_life_into_point.py and point/bad_woman_or_big_woman.py under other number

---
+++ do_same_problem/thing/great_number/little_government/old_company/tell_able_life_into_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_child_over_year(str_arg):
+    small_hand(str_arg)
+    print('see_first_woman_for_own_man')
+
+def small_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_child_over_year('year')
---
+++ point/bad_woman_or_big_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact_or_government(str_arg):
+    next_thing(str_arg)
+    print('say_same_year_of_important_work')
+
+def next_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact_or_government('fact')
