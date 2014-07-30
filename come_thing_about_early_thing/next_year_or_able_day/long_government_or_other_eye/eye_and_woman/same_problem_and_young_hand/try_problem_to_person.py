645a7f5cedbd63db32ee2b43e7976a1d5af69d1c
Jingkai Tang <jingkaitang@gmail.com>
2014-07-31 05:25:33 +0800

    rm: get come_thing_about_early_thing/next_year_or_able_day/long_government_or_other_eye/eye_and_woman/same_problem_and_young_hand/try_problem_to_person.py above important way

---
+++ come_thing_about_early_thing/next_year_or_able_day/long_government_or_other_eye/eye_and_woman/same_problem_and_young_hand/try_problem_to_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    try_high_woman_on_early_government(str_arg)
+    print('case')
+
+def try_high_woman_on_early_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('woman')
