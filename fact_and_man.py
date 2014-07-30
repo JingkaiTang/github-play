fdaa74d755bdb5e683d4784e6c9ce22512aac50f
Jingkai Tang <jingkaitang@gmail.com>
2014-07-30 20:15:36 +0800

    rm: try old_year_or_life/way/part.py and fact_and_man.py at large case

---
+++ fact_and_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_early_place(str_arg):
+    case(str_arg)
+    print('fact')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_early_place('great_work')
---
+++ old_year_or_life/way/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way_and_big_week(str_arg):
+    child_and_number(str_arg)
+    print('way')
+
+def child_and_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way_and_big_week('next_man')
