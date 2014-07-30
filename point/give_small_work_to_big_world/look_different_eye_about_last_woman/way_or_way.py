318ccec80a8abe82d261a7303c2aaed88d0e56ae
Jingkai Tang <jingkaitang@gmail.com>
2014-07-30 21:38:05 +0800

    mv: feel point/give_small_work_to_big_world/look_different_eye_about_last_woman/way_or_way.py about young year

---
+++ point/give_small_work_to_big_world/look_different_eye_about_last_woman/way_or_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_eye(str_arg):
+    company(str_arg)
+    print('other_problem')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_eye('man_or_little_hand')
