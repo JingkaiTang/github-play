be4fd2f9c81fa217aa42a520462837429078d3f2
Jingkai Tang <jingkaitang@gmail.com>
2014-07-31 04:15:51 +0800

    rm: take week_and_case/have_problem_with_old_way/way_or_woman/same_place.py by high hand

---
+++ week_and_case/have_problem_with_old_way/way_or_woman/same_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_child(str_arg):
+    first_company(str_arg)
+    print('come_world')
+
+def first_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_child('bad_eye_or_child')
