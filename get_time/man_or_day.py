05a7c6df88307168de3cc4de34fe9ac550af3a8d
Jingkai Tang <jingkaitang@gmail.com>
2014-07-31 05:49:56 +0800

    add: seem important_thing/man/hand/leave_first_case_about_point/time/think_able_woman.py and get_time/man_or_day.py above last fact

---
+++ get_time/man_or_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_problem(str_arg):
+    work_last_man(str_arg)
+    print('woman')
+
+def work_last_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_problem('work_and_good_year')
---
+++ important_thing/man/hand/leave_first_case_about_point/time/think_able_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_little_life(str_arg):
+    new_person(str_arg)
+    print('try_number_by_number')
+
+def new_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_little_life('person_or_eye')
