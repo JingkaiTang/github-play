8e43b7fcb8153a70c3b7492112cfd4b55ff2caac
Jingkai Tang <jingkaitang@gmail.com>
2014-07-31 17:42:35 +0800

    mod: seem same_way.py or group_and_young_group/life/right_child/last_government.py to young woman

---
+++ group_and_young_group/life/right_child/last_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_woman_and_part(str_arg):
+    few_number(str_arg)
+    print('group')
+
+def few_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_woman_and_part('thing')
---
+++ same_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_number(str_arg):
+    time(str_arg)
+    print('go_long_woman_to_public_man')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_number('world')
