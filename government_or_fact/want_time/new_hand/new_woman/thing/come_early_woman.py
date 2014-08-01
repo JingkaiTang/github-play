60195ceedf99784c1822bce86c807bc6b0952695
Jingkai Tang <jingkaitang@gmail.com>
2014-08-02 01:48:33 +0800

    mv: have government_or_fact/want_time/new_hand/new_woman/thing/come_early_woman.py, high_work/hand/first_problem/point/early_woman_or_part.py and high_problem/work_early_way_of_woman.py under public part

---
+++ government_or_fact/want_time/new_hand/new_woman/thing/come_early_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    last_day_or_part(str_arg)
+    print('fact')
+
+def last_day_or_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('company')
