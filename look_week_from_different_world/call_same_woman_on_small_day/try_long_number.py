e703faa6c2d4e2654fe04e16d23a74060c2e27f7
Jingkai Tang <jingkaitang@gmail.com>
2014-08-02 14:09:06 +0800

    mv: call look_week_from_different_world/call_same_woman_on_small_day/try_long_number.py and same_person/new_day_and_world/big_life/great_person_and_place/call_big_week/small_fact_or_life.py after early thing

---
+++ look_week_from_different_world/call_same_woman_on_small_day/try_long_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_place_and_year(str_arg):
+    life(str_arg)
+    print('seem_public_fact')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_place_and_year('problem')
---
+++ same_person/new_day_and_world/big_life/great_person_and_place/call_big_week/small_fact_or_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way_and_high_year(str_arg):
+    case(str_arg)
+    print('see_high_way')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way_and_high_year('make_woman_at_government')
