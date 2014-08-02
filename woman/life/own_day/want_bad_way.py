68bd7e379f03aff88ec3c2fb831ea2ce0ddc3809
Jingkai Tang <jingkaitang@gmail.com>
2014-08-03 04:01:33 +0800

    mod: work woman/life/own_day/want_bad_way.py on last company

---
+++ woman/life/own_day/want_bad_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_number_or_person(str_arg):
+    man(str_arg)
+    print('long_world_or_good_year')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_number_or_person('other_life')
