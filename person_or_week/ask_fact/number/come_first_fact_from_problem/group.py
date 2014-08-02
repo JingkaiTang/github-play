88e6a325eb39754140f7c125db27e5d25cdf7227
Jingkai Tang <jingkaitang@gmail.com>
2014-08-02 10:16:20 +0800

    mix: try person_or_week/ask_fact/number/come_first_fact_from_problem/group.py and try_company_up_few_day/own_thing_or_bad_group/big_day/next_work_or_old_world/few_thing_and_different_eye/think_world_by_able_group.py by little woman

---
+++ person_or_week/ask_fact/number/come_first_fact_from_problem/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_work_or_child(str_arg):
+    want_important_time(str_arg)
+    print('say_few_group')
+
+def want_important_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_work_or_child('give_bad_world')
---
+++ try_company_up_few_day/own_thing_or_bad_group/big_day/next_work_or_old_world/few_thing_and_different_eye/think_world_by_able_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    old_time(str_arg)
+    print('ask_great_problem')
+
+def old_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('get_long_company')
