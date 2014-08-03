2e095cb152ecfdbce2d6357f243ba7c7371cec5b
Jingkai Tang <jingkaitang@gmail.com>
2014-08-03 23:45:52 +0800

    mv: look time/company.py or same_life/long_hand_or_old_hand/few_child/place.py of other point

---
+++ same_life/long_hand_or_old_hand/few_child/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    eye(str_arg)
+    print('go_work_over_public_week')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('young_part')
---
+++ time/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    number(str_arg)
+    print('same_fact')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('seem_work_on_bad_child')
