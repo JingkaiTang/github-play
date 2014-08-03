e1b868e05cb1d14e3531328d15813812a7bdcd48
Jingkai Tang <jingkaitang@gmail.com>
2014-08-03 18:41:30 +0800

    rm: want early_point.py with few world

---
+++ early_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    case(str_arg)
+    print('public_woman')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('woman_and_last_week')
