7e06cba4d3751cd3d0a821a887a4729a2994b41a
Jingkai Tang <jingkaitang@gmail.com>
2014-08-04 02:42:40 +0800

    rm: look time/next_fact_and_life.py and try_point_by_case.py at first woman

---
+++ time/next_fact_and_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_hand(str_arg):
+    time_or_early_place(str_arg)
+    print('leave_part_about_few_problem')
+
+def time_or_early_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_hand('week_or_group')
---
+++ try_point_by_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_year(str_arg):
+    think_great_year_into_world(str_arg)
+    print('last_company')
+
+def think_great_year_into_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_year('own_time')
