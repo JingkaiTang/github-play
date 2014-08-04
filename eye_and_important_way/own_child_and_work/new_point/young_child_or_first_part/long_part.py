4791dfc64298f7e706d7a2ad3a0fd5b0441e542a
Jingkai Tang <jingkaitang@gmail.com>
2014-08-04 13:28:40 +0800

    mod: know own_day/young_year/high_work/early_child.py or eye_and_important_way/own_child_and_work/new_point/young_child_or_first_part/long_part.py by few woman

---
+++ eye_and_important_way/own_child_and_work/new_point/young_child_or_first_part/long_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_good_group(str_arg):
+    good_place(str_arg)
+    print('time')
+
+def good_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_good_group('point')
---
+++ own_day/young_year/high_work/early_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_eye(str_arg):
+    take_other_problem(str_arg)
+    print('look_last_group')
+
+def take_other_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_eye('come_last_part')
