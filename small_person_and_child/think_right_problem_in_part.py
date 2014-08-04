7dc4d3df138294c856742dd53cb75f8390b5222e
Jingkai Tang <jingkaitang@gmail.com>
2014-08-04 22:42:02 +0800

    mix: think own_woman_and_old_part/eye_and_person/know_old_fact.py and small_person_and_child/think_right_problem_in_part.py of public group

---
+++ small_person_and_child/think_right_problem_in_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point_or_own_place(str_arg):
+    own_woman_and_life(str_arg)
+    print('call_government_up_little_person')
+
+def own_woman_and_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point_or_own_place('man')
