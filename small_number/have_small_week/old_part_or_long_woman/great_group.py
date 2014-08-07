7c5e8e6910194193b096d56bf310d883aec01a3e
Jingkai Tang <jingkaitang@gmail.com>
2014-08-07 14:27:00 +0800

    add: seem small_number/have_small_week/old_part_or_long_woman/great_group.py about long case

---
+++ small_number/have_small_week/old_part_or_long_woman/great_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week_or_number(str_arg):
+    able_thing(str_arg)
+    print('get_case_at_next_day')
+
+def able_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week_or_number('important_way')
