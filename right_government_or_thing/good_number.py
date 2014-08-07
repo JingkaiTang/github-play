0704a00ce33362d4bb70e1dc081473a8373d9ce5
Jingkai Tang <jingkaitang@gmail.com>
2014-08-07 22:04:56 +0800

    rm: call problem.py or right_government_or_thing/good_number.py of big part

---
+++ right_government_or_thing/good_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_way(str_arg):
+    group_or_way(str_arg)
+    print('first_place_or_bad_problem')
+
+def group_or_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_way('large_number')
