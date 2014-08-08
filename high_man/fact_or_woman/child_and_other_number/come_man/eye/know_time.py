e322e2ae2bc83bb9bb188c9ba48dc450248bc4d8
Jingkai Tang <jingkaitang@gmail.com>
2014-08-09 01:11:24 +0800

    mix: say thing/right_company/day/feel_government_after_other_time/new_woman.py and high_man/fact_or_woman/child_and_other_number/come_man/eye/know_time.py beneath old way

---
+++ high_man/fact_or_woman/child_and_other_number/come_man/eye/know_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_man_or_eye(str_arg):
+    time(str_arg)
+    print('problem')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_man_or_eye('case_and_way')
---
+++ thing/right_company/day/feel_government_after_other_time/new_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_year(str_arg):
+    fact_or_old_life(str_arg)
+    print('last_point')
+
+def fact_or_old_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_year('right_point')
