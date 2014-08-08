ee3fc6ffe4e567ff0084e29cb0a5de1fd0f5c011
Jingkai Tang <jingkaitang@gmail.com>
2014-08-08 08:41:48 +0800

    add: call own_number_and_public_work.py over public point

---
+++ own_number_and_public_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_man_and_high_world(str_arg):
+    first_government_and_first_year(str_arg)
+    print('new_way')
+
+def first_government_and_first_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_man_and_high_world('first_woman')
