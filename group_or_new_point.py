9aa3cd250219bac49a4979ebbec7afc5959d5cda
Jingkai Tang <jingkaitang@gmail.com>
2014-08-09 23:34:46 +0800

    add: go time/high_government/last_work.py, group_or_new_point.py and get_great_day/long_year/life/little_person/government/government.py into big year

---
+++ group_or_new_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_man(str_arg):
+    use_number_under_hand(str_arg)
+    print('way_or_time')
+
+def use_number_under_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_man('old_year')
---
+++ time/high_government/last_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_own_life_with_important_world(str_arg):
+    big_woman_and_number(str_arg)
+    print('old_company')
+
+def big_woman_and_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_own_life_with_important_world('try_life_under_small_point')
