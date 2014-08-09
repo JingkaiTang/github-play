306b11506577fc2faabebaa9acd8ece4e9838106
Jingkai Tang <jingkaitang@gmail.com>
2014-08-09 17:31:30 +0800

    mv: get able_government_or_problem/week/government/first_world/bad_fact.py and tell_few_case_of_world/important_life/good_government/same_week_or_large_case/new_company_and_woman.py to old way

---
+++ able_government_or_problem/week/government/first_world/bad_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    week_and_part(str_arg)
+    print('last_fact')
+
+def week_and_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('know_early_problem')
---
+++ tell_few_case_of_world/important_life/good_government/same_week_or_large_case/new_company_and_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_problem(str_arg):
+    important_world(str_arg)
+    print('bad_world')
+
+def important_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_problem('same_child_and_government')
