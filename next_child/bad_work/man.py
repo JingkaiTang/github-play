86c1c858b7ab3844c05a76eeea19e3792f548b5e
Jingkai Tang <jingkaitang@gmail.com>
2014-08-09 14:29:02 +0800

    add: try next_child/bad_work/man.py by early point

---
+++ next_child/bad_work/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day_or_year(str_arg):
+    big_time(str_arg)
+    print('last_man')
+
+def big_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day_or_year('call_company')
