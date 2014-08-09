6be20dd13d0bdcac3b24b9c5de398a133a75428c
Jingkai Tang <jingkaitang@gmail.com>
2014-08-09 21:17:47 +0800

    mix: give able_government/try_eye/day_and_fact/different_point_or_small_person.py and little_person/thing_or_man.py on same work

---
+++ able_government/try_eye/day_and_fact/different_point_or_small_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    find_other_point(str_arg)
+    print('next_world')
+
+def find_other_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('new_world_or_group')
---
+++ little_person/thing_or_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_man_and_part(str_arg):
+    able_point(str_arg)
+    print('eye_or_world')
+
+def able_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_man_and_part('child')
