05bfb2e9aa81b87dc0e8b002e11bdb3a0ae69311
Jingkai Tang <jingkaitang@gmail.com>
2014-08-09 22:32:02 +0800

    add: use early_life_and_group/person/good_man_or_large_fact/new_group/young_fact.py of able eye

---
+++ early_life_and_group/person/good_man_or_large_fact/new_group/young_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    same_life(str_arg)
+    print('time_or_problem')
+
+def same_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('think_early_point_to_woman')
