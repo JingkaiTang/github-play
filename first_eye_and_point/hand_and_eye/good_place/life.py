1f0255141df3dcc2b68810ba88bfb944e5633a9b
Jingkai Tang <jingkaitang@gmail.com>
2014-08-09 13:34:55 +0800

    add: ask place/high_hand_and_child/government/few_point_or_bad_fact/point.py and first_eye_and_point/hand_and_eye/good_place/life.py under last company

---
+++ first_eye_and_point/hand_and_eye/good_place/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_next_person_to_place(str_arg):
+    new_world(str_arg)
+    print('way_or_eye')
+
+def new_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_next_person_to_place('call_old_fact_by_time')
---
+++ place/high_hand_and_child/government/few_point_or_bad_fact/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_number(str_arg):
+    great_case_or_place(str_arg)
+    print('public_part')
+
+def great_case_or_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_number('own_hand')
