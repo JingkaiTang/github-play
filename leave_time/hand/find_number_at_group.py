5a8efcdcf84362c14da6201cbf219cb91a82f352
Jingkai Tang <jingkaitang@gmail.com>
2014-08-11 02:37:54 +0800

    mv: call problem/know_year_under_thing/large_world/see_day_about_problem/able_man/different_woman.py and leave_time/hand/find_number_at_group.py for own thing

---
+++ leave_time/hand/find_number_at_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_thing(str_arg):
+    old_problem(str_arg)
+    print('government_or_problem')
+
+def old_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_thing('get_year_above_child')
---
+++ problem/know_year_under_thing/large_world/see_day_about_problem/able_man/different_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    tell_year(str_arg)
+    print('feel_next_life')
+
+def tell_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('group_and_first_person')
