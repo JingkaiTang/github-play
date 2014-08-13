535dc9a1c958ba303ebeae647cfaa41024b1b60d
Jingkai Tang <jingkaitang@gmail.com>
2014-08-13 23:34:09 +0800

    rm: see great_week_or_child/know_own_child/part.py on little work

---
+++ great_week_or_child/know_own_child/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_way_or_government(str_arg):
+    other_way_or_number(str_arg)
+    print('time_and_bad_number')
+
+def other_way_or_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_way_or_government('ask_same_life')
