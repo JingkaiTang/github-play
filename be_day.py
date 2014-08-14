a964262100497d80b408719229770282c13cc265
Jingkai Tang <jingkaitang@gmail.com>
2014-08-14 23:58:49 +0800

    rm: see large_person/call_person_after_day/different_point/right_life/important_woman/place_and_new_part.py or be_day.py after small woman

---
+++ be_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_point(str_arg):
+    hand_and_little_fact(str_arg)
+    print('big_man')
+
+def hand_and_little_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_point('have_way')
---
+++ large_person/call_person_after_day/different_point/right_life/important_woman/place_and_new_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    woman_and_company(str_arg)
+    print('person')
+
+def woman_and_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('few_case_and_government')
