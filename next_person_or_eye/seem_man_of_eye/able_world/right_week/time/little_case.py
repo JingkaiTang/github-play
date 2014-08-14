d4e18778855de69b7908b4454df6525ce3bcd013
Jingkai Tang <jingkaitang@gmail.com>
2014-08-14 10:03:51 +0800

    rm: call next_person_or_eye/seem_man_of_eye/able_world/right_week/time/little_case.py in public group

---
+++ next_person_or_eye/seem_man_of_eye/able_world/right_week/time/little_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_eye(str_arg):
+    take_person(str_arg)
+    print('able_number_and_long_woman')
+
+def take_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_eye('life')
