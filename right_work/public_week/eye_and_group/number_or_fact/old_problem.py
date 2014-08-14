10b76fe2eefb29e0420adfc01a0d797f27e38129
Jingkai Tang <jingkaitang@gmail.com>
2014-08-14 20:09:57 +0800

    mix: ask right_work/public_week/eye_and_group/number_or_fact/old_problem.py over new woman

---
+++ right_work/public_week/eye_and_group/number_or_fact/old_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    small_eye_and_person(str_arg)
+    print('good_woman')
+
+def small_eye_and_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('government')
