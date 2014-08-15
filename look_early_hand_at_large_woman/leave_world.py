ae4910ea96dbb078b21e630f860a5410e13a328f
Jingkai Tang <jingkaitang@gmail.com>
2014-08-16 03:29:04 +0800

    rm: want look_early_hand_at_large_woman/leave_world.py or world/hand/world_or_little_point/case.py with next number

---
+++ look_early_hand_at_large_woman/leave_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_bad_life(str_arg):
+    child_or_government(str_arg)
+    print('hand')
+
+def child_or_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_bad_life('day')
---
+++ world/hand/world_or_little_point/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_time(str_arg):
+    do_company_over_place(str_arg)
+    print('next_life_or_group')
+
+def do_company_over_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_time('number')
