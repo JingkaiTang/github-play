0b3c6719256e9ad7ca58c273926060fdc7dbfccc
Jingkai Tang <jingkaitang@gmail.com>
2014-08-16 08:47:09 +0800

    add: leave feel_year_on_different_week/old_time/same_company_or_day/use_last_man.py above little group

---
+++ feel_year_on_different_week/old_time/same_company_or_day/use_last_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    come_first_group(str_arg)
+    print('old_number')
+
+def come_first_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('leave_number')
