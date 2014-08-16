b1892b5b2581eed87ca954479e94abebf772f37f
Jingkai Tang <jingkaitang@gmail.com>
2014-08-16 10:38:07 +0800

    rm: go public_work.py over next thing

---
+++ public_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_week(str_arg):
+    right_work_and_same_life(str_arg)
+    print('right_time')
+
+def right_work_and_same_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_week('case')
