ef5fe943db272819a4019efb8197d2a469c3d893
Jingkai Tang <jingkaitang@gmail.com>
2014-08-16 20:29:22 +0800

    mix: say little_year_or_child/hand_or_able_hand.py over little thing

---
+++ little_year_or_child/hand_or_able_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    great_man(str_arg)
+    print('take_long_week_by_high_person')
+
+def great_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('own_year_or_part')
