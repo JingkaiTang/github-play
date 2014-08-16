e9108f1243312a786f6da5d94613e49ae2f2be2a
Jingkai Tang <jingkaitang@gmail.com>
2014-08-16 09:49:59 +0800

    add: make woman_and_way.py from early problem

---
+++ woman_and_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_woman(str_arg):
+    government_or_company(str_arg)
+    print('little_man')
+
+def government_or_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_woman('day')
