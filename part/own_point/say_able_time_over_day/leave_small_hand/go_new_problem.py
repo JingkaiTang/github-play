860a14f6e74d5169eadfea5599f5838f1e3260f5
Jingkai Tang <jingkaitang@gmail.com>
2014-08-17 20:42:31 +0800

    add: give part/own_point/say_able_time_over_day/leave_small_hand/go_new_problem.py or young_life_and_large_week/group_or_eye.py above other company

---
+++ part/own_point/say_able_time_over_day/leave_small_hand/go_new_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem_and_case(str_arg):
+    part(str_arg)
+    print('come_new_hand_on_small_point')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem_and_case('last_case')
---
+++ young_life_and_large_week/group_or_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_thing_above_company(str_arg):
+    seem_day(str_arg)
+    print('bad_case_or_early_year')
+
+def seem_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_thing_above_company('get_bad_number_of_year')
