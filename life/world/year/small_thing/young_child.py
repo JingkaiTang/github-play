a80e4a4b98591a716c042ad63298d86d6b5dfd97
Jingkai Tang <jingkaitang@gmail.com>
2014-08-18 02:51:17 +0800

    mod: seem public_group/tell_early_life_under_day/own_place.py or life/world/year/small_thing/young_child.py at bad eye

---
+++ life/world/year/small_thing/young_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    important_problem(str_arg)
+    print('group')
+
+def important_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('get_own_point')
---
+++ public_group/tell_early_life_under_day/own_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_woman(str_arg):
+    tell_hand(str_arg)
+    print('public_woman')
+
+def tell_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_woman('feel_good_child')
