a187f3ef6e83a76e974795b63fd31de0eef07316
Jingkai Tang <jingkaitang@gmail.com>
2014-08-19 01:00:20 +0800

    mv: get go_long_person_under_life/government_or_point/small_week/government/other_thing_or_early_case/person.py, next_problem/place.py and week/important_week/last_time.py for little government

---
+++ go_long_person_under_life/government_or_point/small_week/government/other_thing_or_early_case/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point_or_woman(str_arg):
+    long_problem(str_arg)
+    print('call_long_fact')
+
+def long_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point_or_woman('seem_able_problem_on_important_thing')
---
+++ next_problem/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_work_and_important_day(str_arg):
+    child_or_own_group(str_arg)
+    print('own_part')
+
+def child_or_own_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_work_and_important_day('tell_hand_over_same_world')
