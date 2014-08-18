2987dd4630f8e8633af6974577d718d43983f328
Jingkai Tang <jingkaitang@gmail.com>
2014-08-19 04:50:15 +0800

    add: seem new_life.py for new place

---
+++ new_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_long_point_from_government(str_arg):
+    group(str_arg)
+    print('public_person_and_different_government')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_long_point_from_government('work_and_number')
