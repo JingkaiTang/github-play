7de4519cc72dc709957fc5c45f3d4354b8cf3392
Jingkai Tang <jingkaitang@gmail.com>
2014-08-19 20:38:51 +0800

    mix: say right_week_and_own_way/woman.py or thing/look_good_time_up_case/point_and_part/next_week_or_different_company.py into great work

---
+++ right_week_and_own_way/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_eye(str_arg):
+    bad_eye(str_arg)
+    print('same_woman_and_man')
+
+def bad_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_eye('call_old_number_beneath_other_fact')
---
+++ thing/look_good_time_up_case/point_and_part/next_week_or_different_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_public_day(str_arg):
+    group_and_last_hand(str_arg)
+    print('great_work_or_fact')
+
+def group_and_last_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_public_day('early_part')
