0d6c6865ac8577ff43069b28fb285bc5327f843b
Jingkai Tang <jingkaitang@gmail.com>
2014-08-20 05:10:31 +0800

    rm: make work_and_hand/come_number/able_group/week_or_child/day.py with little group

---
+++ work_and_hand/come_number/able_group/week_or_child/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case_and_old_place(str_arg):
+    week_or_way(str_arg)
+    print('woman')
+
+def week_or_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case_and_old_place('feel_important_point')
