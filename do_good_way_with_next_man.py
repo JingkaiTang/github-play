66d1c69b3704e643087ce2757251132b7586619e
Jingkai Tang <jingkaitang@gmail.com>
2014-08-20 15:49:24 +0800

    mix: want right_part/young_hand.py and do_good_way_with_next_man.py from good way

---
+++ do_good_way_with_next_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_bad_company_by_group(str_arg):
+    big_part_and_child(str_arg)
+    print('early_eye')
+
+def big_part_and_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_bad_company_by_group('early_place')
---
+++ right_part/young_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_old_place(str_arg):
+    good_time_or_world(str_arg)
+    print('small_number')
+
+def good_time_or_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_old_place('want_right_eye_up_man')
