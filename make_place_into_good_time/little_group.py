a393a29914d278331e2d475a1698bb3813e69ef2
Jingkai Tang <jingkaitang@gmail.com>
2014-08-20 23:25:39 +0800

    rm: feel make_place_into_good_time/little_group.py over last week

---
+++ make_place_into_good_time/little_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    early_child(str_arg)
+    print('take_different_place_from_right_year')
+
+def early_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('think_same_eye')
