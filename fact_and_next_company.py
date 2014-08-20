089247a75048550ad863c89a21f907180a78e64b
Jingkai Tang <jingkaitang@gmail.com>
2014-08-21 02:21:13 +0800

    rm: work fact_and_next_company.py by old person

---
+++ fact_and_next_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_place_and_public_person(str_arg):
+    place(str_arg)
+    print('eye')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_place_and_public_person('day')
