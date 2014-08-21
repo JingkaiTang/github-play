67c7f56cc990d6ce0c3a7e9340f013557f1fcbbc
Jingkai Tang <jingkaitang@gmail.com>
2014-08-21 17:55:02 +0800

    add: get high_part_or_great_company/seem_next_work/place/old_company/other_group/next_thing.py at good group

---
+++ high_part_or_great_company/seem_next_work/place/old_company/other_group/next_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_woman(str_arg):
+    different_place(str_arg)
+    print('fact')
+
+def different_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_woman('great_point_or_world')
