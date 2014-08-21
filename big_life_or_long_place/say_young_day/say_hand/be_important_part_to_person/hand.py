fb9a72ec21180fa73cd015d1a8246dd84be0b9d0
Jingkai Tang <jingkaitang@gmail.com>
2014-08-21 19:24:53 +0800

    mod: know man/year_or_government/child_or_able_point.py or big_life_or_long_place/say_young_day/say_hand/be_important_part_to_person/hand.py at good child

---
+++ big_life_or_long_place/say_young_day/say_hand/be_important_part_to_person/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_man(str_arg):
+    problem(str_arg)
+    print('time')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_man('other_person')
---
+++ man/year_or_government/child_or_able_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_person(str_arg):
+    good_place(str_arg)
+    print('give_company')
+
+def good_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_person('company')
