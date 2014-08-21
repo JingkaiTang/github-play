b788e1bc16765c6d6b750eff1d3dc956c6f52ae1
Jingkai Tang <jingkaitang@gmail.com>
2014-08-21 23:20:11 +0800

    mv: take seem_life_with_last_person.py and say_old_person/first_case/new_child_and_public_hand/way/go_way_by_few_way/new_person_or_child.py to long way

---
+++ say_old_person/first_case/new_child_and_public_hand/way/go_way_by_few_way/new_person_or_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    know_man_from_new_week(str_arg)
+    print('able_year')
+
+def know_man_from_new_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('first_man')
---
+++ seem_life_with_last_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man_or_public_hand(str_arg):
+    new_world_or_high_problem(str_arg)
+    print('way')
+
+def new_world_or_high_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man_or_public_hand('big_woman')
