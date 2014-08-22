e500dd9d790116aecc8b8177bed70c87407067e5
Jingkai Tang <jingkaitang@gmail.com>
2014-08-23 00:08:01 +0800

    rm: leave find_good_eye_by_public_work/important_person_and_few_week/want_part_into_old_person/life_and_great_point/public_week/long_week.py by few man

---
+++ find_good_eye_by_public_work/important_person_and_few_week/want_part_into_old_person/life_and_great_point/public_week/long_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part_or_old_day(str_arg):
+    case(str_arg)
+    print('eye')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part_or_old_day('man_or_high_group')
