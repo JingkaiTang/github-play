8c6ef5beb42c3a684c2740af22e46c2ba2b8a2ef
Jingkai Tang <jingkaitang@gmail.com>
2014-08-22 09:24:20 +0800

    mv: seem say_good_week_after_right_way/own_place/eye.py with good place

---
+++ say_good_week_after_right_way/own_place/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    look_government_after_number(str_arg)
+    print('public_way')
+
+def look_government_after_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('life')
