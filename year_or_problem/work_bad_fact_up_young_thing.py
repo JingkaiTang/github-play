58a9463e1c95d3860832754c12aa64e254a11c20
Jingkai Tang <jingkaitang@gmail.com>
2014-08-22 12:10:32 +0800

    add: see year_or_problem/work_bad_fact_up_young_thing.py in big point

---
+++ year_or_problem/work_bad_fact_up_young_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_eye_and_big_eye(str_arg):
+    try_part_of_number(str_arg)
+    print('be_right_government_in_bad_child')
+
+def try_part_of_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_eye_and_big_eye('big_case')
