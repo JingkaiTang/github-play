bd723651573c629123cd39e1200fe8128dcb334e
Jingkai Tang <jingkaitang@gmail.com>
2014-08-24 06:27:53 +0800

    mv: have big_place.py about high number

---
+++ big_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_problem(str_arg):
+    same_life_or_week(str_arg)
+    print('get_high_child_on_fact')
+
+def same_life_or_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_problem('big_company')
