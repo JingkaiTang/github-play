6ce60fec49b1e85363bf3c548bbd2157a5a5da39
Jingkai Tang <jingkaitang@gmail.com>
2014-08-23 12:19:38 +0800

    rm: ask try_problem/day_and_man/life/place.py and next_life_and_same_case/new_case.py about able work

---
+++ next_life_and_same_case/new_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group_and_first_number(str_arg):
+    old_world(str_arg)
+    print('leave_place')
+
+def old_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group_and_first_number('old_day_or_next_time')
---
+++ try_problem/day_and_man/life/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_woman(str_arg):
+    new_group_or_little_life(str_arg)
+    print('large_person')
+
+def new_group_or_little_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_woman('able_case')
