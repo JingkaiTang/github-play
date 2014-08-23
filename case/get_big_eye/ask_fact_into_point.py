8e96ce57d434d9b18531845764e4df0e00efb9a9
Jingkai Tang <jingkaitang@gmail.com>
2014-08-23 15:51:35 +0800

    mix: seem other_government/government_and_last_number/able_company/new_government_or_hand.py and case/get_big_eye/ask_fact_into_point.py to first week

---
+++ case/get_big_eye/ask_fact_into_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    hand_and_work(str_arg)
+    print('number_and_small_problem')
+
+def hand_and_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('early_world_and_long_company')
