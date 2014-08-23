bd5a80ae9bd71cac3bf179c1454970ae02622c0b
Jingkai Tang <jingkaitang@gmail.com>
2014-08-23 15:16:41 +0800

    mv: use tell_place_at_small_week/go_high_world.py and bad_eye/place/great_number_and_last_thing/try_week.py on right time

---
+++ tell_place_at_small_week/go_high_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_early_hand(str_arg):
+    try_world(str_arg)
+    print('group_or_few_thing')
+
+def try_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_early_hand('tell_number')
