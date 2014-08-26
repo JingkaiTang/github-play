f69858d747823884b63a20960321b078596eb752
Jingkai Tang <jingkaitang@gmail.com>
2014-08-26 19:53:55 +0800

    add: take person/eye/same_year_and_point/problem/next_hand_and_person/bad_point.py of public life

---
+++ person/eye/same_year_and_point/problem/next_hand_and_person/bad_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world_and_last_hand(str_arg):
+    world(str_arg)
+    print('same_work')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world_and_last_hand('place_and_different_government')
