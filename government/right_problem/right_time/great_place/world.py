8f7b6250dd7bdf42c0ca4a0892765acd799b25e8
Jingkai Tang <jingkaitang@gmail.com>
2014-08-27 06:25:33 +0800

    mod: use government/right_problem/right_time/great_place/world.py about able hand

---
+++ government/right_problem/right_time/great_place/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_child(str_arg):
+    day(str_arg)
+    print('use_few_work_above_group')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_child('have_point')
