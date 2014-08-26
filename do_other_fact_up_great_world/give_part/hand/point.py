fddb75d54b39dded6deb4d05275af3b6dee71ba2
Jingkai Tang <jingkaitang@gmail.com>
2014-08-27 00:54:49 +0800

    rm: think do_other_fact_up_great_world/give_part/hand/point.py beneath big hand

---
+++ do_other_fact_up_great_world/give_part/hand/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_number_about_own_man(str_arg):
+    own_place(str_arg)
+    print('high_year')
+
+def own_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_number_about_own_man('high_child')
