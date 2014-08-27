d16064a75777583a692ed571819109a33ac2316a
Jingkai Tang <jingkaitang@gmail.com>
2014-08-28 01:59:33 +0800

    mod: get look_part/own_part/fact_or_part/right_problem/day_and_own_number.py or good_way/use_important_place_to_group.py with right number

---
+++ look_part/own_part/fact_or_part/right_problem/day_and_own_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_way_or_important_fact(str_arg):
+    work_and_same_child(str_arg)
+    print('ask_problem_up_few_government')
+
+def work_and_same_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_way_or_important_fact('big_way_and_other_child')
