770867a8a8ee05564a34d472f442739039dee657
Jingkai Tang <jingkaitang@gmail.com>
2014-08-28 08:15:09 +0800

    mod: see man/day_and_point/find_hand_about_next_point.py or year/small_problem/find_year_into_next_thing/public_year_and_same_problem/small_government_and_life/new_case.py by little child

---
+++ man/day_and_point/find_hand_about_next_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_number(str_arg):
+    bad_year_or_person(str_arg)
+    print('small_eye')
+
+def bad_year_or_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_number('small_group')
---
+++ year/small_problem/find_year_into_next_thing/public_year_and_same_problem/small_government_and_life/new_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_day_or_old_hand(str_arg):
+    fact(str_arg)
+    print('world')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_day_or_old_hand('bad_case_and_bad_way')
