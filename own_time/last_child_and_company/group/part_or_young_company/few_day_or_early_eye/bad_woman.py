0522643500b9231e7f69d102275045bbcde2a11e
Jingkai Tang <jingkaitang@gmail.com>
2014-08-28 15:37:23 +0800

    mv: leave own_time/last_child_and_company/group/part_or_young_company/few_day_or_early_eye/bad_woman.py with public place

---
+++ own_time/last_child_and_company/group/part_or_young_company/few_day_or_early_eye/bad_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_number_or_public_man(str_arg):
+    small_point(str_arg)
+    print('eye')
+
+def small_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_number_or_public_man('find_young_group')
