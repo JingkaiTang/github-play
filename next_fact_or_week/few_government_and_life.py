f2906c22b03c526a76596a2d98028a7776688a87
Jingkai Tang <jingkaitang@gmail.com>
2014-08-28 16:41:15 +0800

    add: feel next_fact_or_week/few_government_and_life.py after first day

---
+++ next_fact_or_week/few_government_and_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    case(str_arg)
+    print('eye')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('little_company')
