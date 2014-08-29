72b956bf6ee753ea4dc08bf98f5eefb581921cf5
Jingkai Tang <jingkaitang@gmail.com>
2014-08-29 19:52:28 +0800

    mod: tell public_year/other_work.py above first point

---
+++ public_year/other_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_week(str_arg):
+    have_young_thing(str_arg)
+    print('ask_thing_over_point')
+
+def have_young_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_week('great_day')
