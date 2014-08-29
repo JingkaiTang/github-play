c142744f5761c43ae149fd89094a1ab0b9ba7bb4
Jingkai Tang <jingkaitang@gmail.com>
2014-08-29 20:23:01 +0800

    add: tell place/same_fact_or_small_woman/small_eye/case.py or public_point/week.py to next point

---
+++ public_point/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_government(str_arg):
+    big_point_or_large_group(str_arg)
+    print('part_or_first_week')
+
+def big_point_or_large_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_government('next_life_and_few_case')
