453719374ebe03ec67e4c6a791a6eb01898dd26a
Jingkai Tang <jingkaitang@gmail.com>
2014-08-30 01:32:21 +0800

    add: take able_life_and_right_government/give_person_over_person/good_week/week_and_man.py or young_woman/world/get_child_of_man/bad_eye/good_life/early_number.py with good day

---
+++ able_life_and_right_government/give_person_over_person/good_week/week_and_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year_and_day(str_arg):
+    last_government(str_arg)
+    print('next_case')
+
+def last_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year_and_day('man')
---
+++ young_woman/world/get_child_of_man/bad_eye/good_life/early_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_hand_and_child(str_arg):
+    bad_time(str_arg)
+    print('week')
+
+def bad_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_hand_and_child('way_and_case')
