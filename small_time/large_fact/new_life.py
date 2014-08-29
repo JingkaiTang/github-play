090fedbd99f35513211b94f23b401d7fab5863d4
Jingkai Tang <jingkaitang@gmail.com>
2014-08-30 07:55:42 +0800

    mv: make year_or_other_eye/take_hand/person/call_small_person/day.py and small_time/large_fact/new_life.py by high case

---
+++ small_time/large_fact/new_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_company_or_company(str_arg):
+    world(str_arg)
+    print('next_hand')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_company_or_company('small_group_and_good_place')
---
+++ year_or_other_eye/take_hand/person/call_small_person/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_problem(str_arg):
+    work_able_government(str_arg)
+    print('know_week')
+
+def work_able_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_problem('company')
