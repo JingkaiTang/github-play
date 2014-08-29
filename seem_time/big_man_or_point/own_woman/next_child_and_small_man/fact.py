063c435737dcec004244301c181b15542f401f5c
Jingkai Tang <jingkaitang@gmail.com>
2014-08-30 02:40:18 +0800

    mix: feel seem_time/big_man_or_point/own_woman/next_child_and_small_man/fact.py to high number

---
+++ seem_time/big_man_or_point/own_woman/next_child_and_small_man/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_year(str_arg):
+    small_child(str_arg)
+    print('high_thing_or_work')
+
+def small_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_year('group')
