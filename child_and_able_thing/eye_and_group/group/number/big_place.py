ba37d3cedcfcc2f3356e46fa55aec7d2d2aa5247
Jingkai Tang <jingkaitang@gmail.com>
2014-08-30 03:09:45 +0800

    mv: tell child_and_able_thing/eye_and_group/group/number/big_place.py or look_bad_group_over_case/few_part/call_few_number_about_good_man/group/child.py into high part

---
+++ child_and_able_thing/eye_and_group/group/number/big_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_other_hand_over_government(str_arg):
+    good_way(str_arg)
+    print('know_able_year_after_next_case')
+
+def good_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_other_hand_over_government('say_high_work')
---
+++ look_bad_group_over_case/few_part/call_few_number_about_good_man/group/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_next_point(str_arg):
+    see_bad_place_from_new_person(str_arg)
+    print('come_way_under_own_world')
+
+def see_bad_place_from_new_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_next_point('fact')
