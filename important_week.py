c7edc70ebdc8f6868555e5826542808cc6424e80
Jingkai Tang <jingkaitang@gmail.com>
2014-08-30 09:02:31 +0800

    mv: find important_week.py about own time

---
+++ important_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_life(str_arg):
+    small_company(str_arg)
+    print('part')
+
+def small_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_life('go_good_case')
