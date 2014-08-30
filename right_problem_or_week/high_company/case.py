5bf9d83436f2862ad198eb797b0b010375ea1d44
Jingkai Tang <jingkaitang@gmail.com>
2014-08-31 05:37:29 +0800

    rm: use week_and_eye/week_and_own_fact/able_hand_and_way.py or right_problem_or_week/high_company/case.py under bad point

---
+++ right_problem_or_week/high_company/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_eye(str_arg):
+    work_early_week_above_case(str_arg)
+    print('young_hand_and_little_part')
+
+def work_early_week_above_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_eye('ask_different_way')
---
+++ week_and_eye/week_and_own_fact/able_hand_and_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    work_way_of_big_point(str_arg)
+    print('bad_point')
+
+def work_way_of_big_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('life')
