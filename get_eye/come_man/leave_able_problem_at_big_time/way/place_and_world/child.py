953ea21cade0f02c70aec5a3f539429676479013
Jingkai Tang <jingkaitang@gmail.com>
2014-08-30 17:51:10 +0800

    rm: use get_eye/come_man/leave_able_problem_at_big_time/way/place_and_world/child.py and next_year/work_number/life/come_fact_under_public_group/right_fact.py about different person

---
+++ get_eye/come_man/leave_able_problem_at_big_time/way/place_and_world/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number_and_year(str_arg):
+    world(str_arg)
+    print('eye')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number_and_year('old_life_and_government')
---
+++ next_year/work_number/life/come_fact_under_public_group/right_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life_or_fact(str_arg):
+    tell_important_eye(str_arg)
+    print('government')
+
+def tell_important_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life_or_fact('seem_eye_on_thing')
