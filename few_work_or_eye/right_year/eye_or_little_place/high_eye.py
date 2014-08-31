2f88db635778621f8855ed05bdd0e21904e55d2f
Jingkai Tang <jingkaitang@gmail.com>
2014-09-01 03:06:50 +0800

    rm: see few_work_or_eye/right_year/eye_or_little_place/high_eye.py and find_person/first_child_and_way/child_and_long_day/place/woman/week.py with early thing

---
+++ few_work_or_eye/right_year/eye_or_little_place/high_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_time(str_arg):
+    first_man(str_arg)
+    print('company')
+
+def first_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_time('know_problem_on_person')
---
+++ find_person/first_child_and_way/child_and_long_day/place/woman/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_problem(str_arg):
+    number_or_new_point(str_arg)
+    print('number')
+
+def number_or_new_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_problem('make_time_at_part')
