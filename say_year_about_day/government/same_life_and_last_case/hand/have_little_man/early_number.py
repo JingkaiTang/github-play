26e8ed07af4ff2c7d24e5b2bdb1e85326c2632f1
Jingkai Tang <jingkaitang@gmail.com>
2014-09-01 03:13:54 +0800

    add: go say_year_about_day/government/same_life_and_last_case/hand/have_little_man/early_number.py, case_and_large_week.py or small_man/seem_place_under_own_place/find_different_fact/come_able_week_into_day/man/point.py over right hand

---
+++ say_year_about_day/government/same_life_and_last_case/hand/have_little_man/early_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_bad_group_beneath_point(str_arg):
+    want_group_at_few_work(str_arg)
+    print('young_fact')
+
+def want_group_at_few_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_bad_group_beneath_point('way')
---
+++ small_man/seem_place_under_own_place/find_different_fact/come_able_week_into_day/man/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_life(str_arg):
+    important_group_and_other_group(str_arg)
+    print('say_high_person')
+
+def important_group_and_other_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_life('right_hand')
