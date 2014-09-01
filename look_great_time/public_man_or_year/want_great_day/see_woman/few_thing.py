18a4c6100c3d3fe603e33e4d2613a299e8a942f1
Jingkai Tang <jingkaitang@gmail.com>
2014-09-01 10:51:52 +0800

    mod: seem look_great_time/public_man_or_year/want_great_day/see_woman/few_thing.py or fact_or_company/little_week/point.py about great company

---
+++ look_great_time/public_man_or_year/want_great_day/see_woman/few_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_week(str_arg):
+    want_person(str_arg)
+    print('great_person')
+
+def want_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_week('new_time_and_problem')
