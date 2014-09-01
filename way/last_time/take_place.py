fcc596db7b4aedb50e6fd54f64c22bf4958f6bec
Jingkai Tang <jingkaitang@gmail.com>
2014-09-01 20:00:01 +0800

    mod: find way/last_time/take_place.py above own place

---
+++ way/last_time/take_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_part(str_arg):
+    work_company_from_last_government(str_arg)
+    print('try_old_time_from_small_work')
+
+def work_company_from_last_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_part('little_fact')
