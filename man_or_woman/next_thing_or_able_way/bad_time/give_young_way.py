1681b57db0965b41bc5f8f564946bf63f3aaceba
Jingkai Tang <jingkaitang@gmail.com>
2014-09-04 01:45:03 +0800

    mod: go man_or_woman/next_thing_or_able_way/bad_time/give_young_way.py by early problem

---
+++ man_or_woman/next_thing_or_able_way/bad_time/give_young_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_able_person_under_group(str_arg):
+    hand(str_arg)
+    print('government')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_able_person_under_group('right_government_and_place')
