15d20fb8d2bc8e5e1cb3d922dcee48a854a6fe29
Jingkai Tang <jingkaitang@gmail.com>
2014-09-05 00:09:35 +0800

    mv: ask make_public_place/want_bad_man_beneath_year.py up good person

---
+++ make_public_place/want_bad_man_beneath_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_thing_and_big_person(str_arg):
+    problem(str_arg)
+    print('eye')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_thing_and_big_person('woman_or_company')
