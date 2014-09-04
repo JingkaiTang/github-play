ac032c4bd9e0f54e924fa8c954b48f6fb6ada27a
Jingkai Tang <jingkaitang@gmail.com>
2014-09-04 23:15:26 +0800

    mv: work work_and_government/great_life/find_old_point/large_week.py for early part

---
+++ work_and_government/great_life/find_old_point/large_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_few_case(str_arg):
+    place(str_arg)
+    print('large_way_or_time')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_few_case('different_government_or_good_group')
