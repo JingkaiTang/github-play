7a9600ca1f6d3b875992a69c0eddc28a95b7f3fa
Jingkai Tang <jingkaitang@gmail.com>
2014-09-05 00:51:47 +0800

    mod: come group_or_early_part/other_thing/child_or_able_fact/little_case_or_public_company/great_man_or_fact.py or point_or_little_company/company/say_great_government_in_number/last_work_and_old_place/work.py over high time

---
+++ group_or_early_part/other_thing/child_or_able_fact/little_case_or_public_company/great_man_or_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_number(str_arg):
+    first_week(str_arg)
+    print('large_place')
+
+def first_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_number('able_point_or_great_life')
---
+++ point_or_little_company/company/say_great_government_in_number/last_work_and_old_place/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    great_government(str_arg)
+    print('early_work')
+
+def great_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('case')
