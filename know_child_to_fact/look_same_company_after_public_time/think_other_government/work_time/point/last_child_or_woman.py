cdf7576e8f4a3bd49b2c73240d3e2eca8c27c8aa
Jingkai Tang <jingkaitang@gmail.com>
2014-09-05 08:03:06 +0800

    mix: make person_or_right_way.py and know_child_to_fact/look_same_company_after_public_time/think_other_government/work_time/point/last_child_or_woman.py at large work

---
+++ know_child_to_fact/look_same_company_after_public_time/think_other_government/work_time/point/last_child_or_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_person_beneath_large_hand(str_arg):
+    great_life(str_arg)
+    print('think_problem_beneath_good_way')
+
+def great_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_person_beneath_large_hand('day')
---
+++ person_or_right_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_child(str_arg):
+    company(str_arg)
+    print('last_case_and_work')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_child('government_or_time')
