2711e74376d3b7c0bd3d1de79410fc99ab110671
Jingkai Tang <jingkaitang@gmail.com>
2014-09-05 09:55:31 +0800

    rm: see go_person_with_last_way/know_first_eye/own_man.py or important_child/way/know_point_under_right_man/know_fact_up_long_child/work_small_thing.py at last man

---
+++ go_person_with_last_way/know_first_eye/own_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_or_new_work(str_arg):
+    problem_and_few_group(str_arg)
+    print('point')
+
+def problem_and_few_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_or_new_work('first_eye')
---
+++ important_child/way/know_point_under_right_man/know_fact_up_long_child/work_small_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_eye(str_arg):
+    great_hand(str_arg)
+    print('number_and_place')
+
+def great_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_eye('problem_and_group')
