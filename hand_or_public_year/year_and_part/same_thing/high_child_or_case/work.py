962feb00831287eb56ccb87cf2b6c5122183e5f8
Jingkai Tang <jingkaitang@gmail.com>
2014-09-07 03:36:36 +0800

    mod: come point_or_big_group/next_case_or_work.py or hand_or_public_year/year_and_part/same_thing/high_child_or_case/work.py with other thing

---
+++ hand_or_public_year/year_and_part/same_thing/high_child_or_case/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child_and_same_work(str_arg):
+    company(str_arg)
+    print('group_or_same_man')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child_and_same_work('give_company_above_group')
---
+++ point_or_big_group/next_case_or_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place_or_woman(str_arg):
+    day(str_arg)
+    print('long_day')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place_or_woman('bad_woman')
