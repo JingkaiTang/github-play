d71d247f2cd1178a545186b2fce5eb47953460a4
Jingkai Tang <jingkaitang@gmail.com>
2014-09-06 18:42:39 +0800

    rm: work bad_eye_or_small_work/bad_day/eye.py with public man

---
+++ bad_eye_or_small_work/bad_day/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way_or_important_woman(str_arg):
+    way_or_few_place(str_arg)
+    print('long_thing_or_day')
+
+def way_or_few_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way_or_important_woman('able_fact')
