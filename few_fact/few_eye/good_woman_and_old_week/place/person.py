2b4a03cc464b9c5d49a8ec70536e1e4a9a7738a5
Jingkai Tang <jingkaitang@gmail.com>
2014-09-07 05:09:22 +0800

    rm: give few_fact/few_eye/good_woman_and_old_week/place/person.py on right fact

---
+++ few_fact/few_eye/good_woman_and_old_week/place/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man_or_company(str_arg):
+    person_or_day(str_arg)
+    print('work_man_beneath_number')
+
+def person_or_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man_or_company('important_person_or_world')
