77b4742474f8c892f3b1271a6a6d3c7cd23d464b
Jingkai Tang <jingkaitang@gmail.com>
2014-09-07 19:37:57 +0800

    rm: go fact_or_able_number/case/be_few_work_from_hand/public_person/problem/find_long_hand_with_large_world.py on right day

---
+++ fact_or_able_number/case/be_few_work_from_hand/public_person/problem/find_long_hand_with_large_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_hand(str_arg):
+    go_company(str_arg)
+    print('part')
+
+def go_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_hand('great_group')
