79ddf85e8a910bb0db87eabbd5ae23badfdb731b
Jingkai Tang <jingkaitang@gmail.com>
2014-09-07 14:40:58 +0800

    mv: find way_and_fact/work_work_above_own_life/government/man.py up first woman

---
+++ way_and_fact/work_work_above_own_life/government/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_good_fact(str_arg):
+    company_or_early_fact(str_arg)
+    print('day_and_long_person')
+
+def company_or_early_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_good_fact('have_same_case')
