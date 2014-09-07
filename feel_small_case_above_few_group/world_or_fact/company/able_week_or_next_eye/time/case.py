318db19dc5cb48124817fdd1d8db947c4d4d5ef6
Jingkai Tang <jingkaitang@gmail.com>
2014-09-07 13:48:39 +0800

    mix: see feel_small_case_above_few_group/world_or_fact/company/able_week_or_next_eye/time/case.py with good government

---
+++ feel_small_case_above_few_group/world_or_fact/company/able_week_or_next_eye/time/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    other_child_or_work(str_arg)
+    print('long_person')
+
+def other_child_or_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('child')
