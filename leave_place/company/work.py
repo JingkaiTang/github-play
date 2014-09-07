21877974108497bc7b5498d62042051b56607863
Jingkai Tang <jingkaitang@gmail.com>
2014-09-07 08:43:32 +0800

    rm: want see_government/give_man/little_work_and_early_time/eye.py or leave_place/company/work.py after able eye

---
+++ leave_place/company/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    place_or_other_child(str_arg)
+    print('use_young_number')
+
+def place_or_other_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('company')
---
+++ see_government/give_man/little_work_and_early_time/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_group(str_arg):
+    work_week(str_arg)
+    print('fact')
+
+def work_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_group('first_problem')
