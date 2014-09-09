962da4f0c705702bf4c1ea537a05cea1945315e2
Jingkai Tang <jingkaitang@gmail.com>
2014-09-09 08:55:54 +0800

    add: know right_eye/have_able_number/own_eye.py with same company

---
+++ right_eye/have_able_number/own_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    high_company_and_good_week(str_arg)
+    print('same_woman_or_woman')
+
+def high_company_and_good_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('come_important_way')
