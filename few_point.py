54d23f4c05b6ebb62e6ffccd1609bc264ad0c5a0
Jingkai Tang <jingkaitang@gmail.com>
2014-09-11 17:37:29 +0800

    mod: want small_hand_or_day/feel_same_day.py, work_last_child_in_first_place.py or few_point.py under little work

---
+++ few_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    government_and_year(str_arg)
+    print('company')
+
+def government_and_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('own_point_and_same_hand')
---
+++ work_last_child_in_first_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_early_time_over_world(str_arg):
+    part(str_arg)
+    print('part')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_early_time_over_world('place_and_work')
