2abad04b47b8139d8d9185b4c5e7873cfd508025
Jingkai Tang <jingkaitang@gmail.com>
2014-09-13 05:11:55 +0800

    add: find get_early_hand.py and high_thing.py from public time

---
+++ get_early_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    long_child(str_arg)
+    print('year')
+
+def long_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('important_year')
---
+++ high_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_eye(str_arg):
+    hand(str_arg)
+    print('next_thing_or_government')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_eye('new_part')
