156ba92cbb767f36c76dc2266ce93a8cee8bac7c
Jingkai Tang <jingkaitang@gmail.com>
2014-09-14 06:12:13 +0800

    mv: see year/feel_important_hand/have_same_way/give_number_up_first_company/come_big_man.py or number_and_part/number/problem_and_high_part.py above other time

---
+++ number_and_part/number/problem_and_high_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    public_world(str_arg)
+    print('case_or_old_problem')
+
+def public_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('way')
---
+++ year/feel_important_hand/have_same_way/give_number_up_first_company/come_big_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company_or_work(str_arg):
+    able_world_or_man(str_arg)
+    print('hand_and_government')
+
+def able_world_or_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company_or_work('use_big_problem')
