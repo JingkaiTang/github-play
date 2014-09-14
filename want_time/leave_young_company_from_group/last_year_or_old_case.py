2217a2bb10fffe99808c2f46ecac568c4420b9e8
Jingkai Tang <jingkaitang@gmail.com>
2014-09-14 18:09:35 +0800

    rm: work want_time/leave_young_company_from_group/last_year_or_old_case.py to small place

---
+++ want_time/leave_young_company_from_group/last_year_or_old_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    man(str_arg)
+    print('look_good_number_under_way')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('own_hand')
