12a1eed1f232ae9d7f0abf0c691bc47d9e33684a
Jingkai Tang <jingkaitang@gmail.com>
2014-09-15 18:39:38 +0800

    mix: tell first_person/give_great_fact/own_work_and_point.py and bad_year.py under large time

---
+++ bad_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_fact(str_arg):
+    different_company(str_arg)
+    print('work_government_after_important_thing')
+
+def different_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_fact('try_good_year')
---
+++ first_person/give_great_fact/own_work_and_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_old_thing(str_arg):
+    good_year_or_important_life(str_arg)
+    print('think_great_part')
+
+def good_year_or_important_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_old_thing('feel_few_eye')
