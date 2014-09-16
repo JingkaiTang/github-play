54e48062b5b88fad84017cf63134bb32318b7bbb
Jingkai Tang <jingkaitang@gmail.com>
2014-09-17 02:48:04 +0800

    mod: ask different_week.py and other_woman/next_work/new_time/own_problem.py up different woman

---
+++ different_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_case(str_arg):
+    work_part_at_young_man(str_arg)
+    print('eye')
+
+def work_part_at_young_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_case('last_week_and_young_week')
---
+++ other_woman/next_work/new_time/own_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    number(str_arg)
+    print('child_or_first_problem')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('week_and_thing')
