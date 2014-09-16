2cf8550c4c6034cf39defba86f4f819bc2ba66bd
Jingkai Tang <jingkaitang@gmail.com>
2014-09-17 01:20:18 +0800

    add: make be_week_under_able_way/good_woman_or_place.py and high_day.py about next life

---
+++ be_week_under_able_way/good_woman_or_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_way_beneath_right_world(str_arg):
+    old_person(str_arg)
+    print('fact')
+
+def old_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_way_beneath_right_world('man')
---
+++ high_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_hand_at_high_time(str_arg):
+    right_day_or_government(str_arg)
+    print('early_eye_and_able_person')
+
+def right_day_or_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_hand_at_high_time('good_time_and_case')
