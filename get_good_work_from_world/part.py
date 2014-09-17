2635c3d7b79732ba6b6cafe5e55c112d2000b177
Jingkai Tang <jingkaitang@gmail.com>
2014-09-18 05:54:05 +0800

    mix: make get_good_work_from_world/part.py about last government

---
+++ get_good_work_from_world/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing_or_work(str_arg):
+    important_person_and_next_part(str_arg)
+    print('long_government')
+
+def important_person_and_next_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing_or_work('leave_different_problem')
