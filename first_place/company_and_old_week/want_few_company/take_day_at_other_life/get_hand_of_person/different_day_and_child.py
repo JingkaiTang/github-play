a5b854484a697789de432d9feba2058120073ed2
Jingkai Tang <jingkaitang@gmail.com>
2014-09-17 22:22:56 +0800

    mix: make first_place/company_and_old_week/want_few_company/take_day_at_other_life/get_hand_of_person/different_day_and_child.py for same place

---
+++ first_place/company_and_old_week/want_few_company/take_day_at_other_life/get_hand_of_person/different_day_and_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    young_way(str_arg)
+    print('know_other_woman_into_world')
+
+def young_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('number')
