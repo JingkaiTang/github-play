4753fb6e09620bf599ec697404a4d235dcf9286b
Jingkai Tang <jingkaitang@gmail.com>
2014-09-17 13:51:55 +0800

    rm: think different_day/place/good_part/number/life_and_high_thing/want_point.py, old_part_and_young_day/person/same_year_and_world/right_woman.py or tell_man_by_government/call_group.py of big woman

---
+++ old_part_and_young_day/person/same_year_and_world/right_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_case_or_other_place(str_arg):
+    next_child(str_arg)
+    print('old_year')
+
+def next_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_case_or_other_place('use_new_week')
---
+++ tell_man_by_government/call_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_day(str_arg):
+    tell_large_fact(str_arg)
+    print('thing_or_part')
+
+def tell_large_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_day('have_woman_with_government')
