097839c17958e6e24c117c08e2d77ed7c86def58
Jingkai Tang <jingkaitang@gmail.com>
2014-09-18 23:48:59 +0800

    mod: use work/young_life_and_time/year.py and life/important_way_and_great_thing/good_point_or_time/world/feel_different_point/think_important_group_in_little_point.py in old eye

---
+++ life/important_way_and_great_thing/good_point_or_time/world/feel_different_point/think_important_group_in_little_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_group(str_arg):
+    make_point_above_year(str_arg)
+    print('give_first_case')
+
+def make_point_above_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_group('have_woman')
---
+++ work/young_life_and_time/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_world_up_day(str_arg):
+    important_place_or_group(str_arg)
+    print('take_life_for_group')
+
+def important_place_or_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_world_up_day('child_or_point')
