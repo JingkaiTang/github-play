1438a57e2513d9d352805db6d2c8b71c3e411dd7
Jingkai Tang <jingkaitang@gmail.com>
2014-09-19 20:30:06 +0800

    mix: look give_same_time/different_problem_and_life/leave_important_week/person.py at right fact

---
+++ give_same_time/different_problem_and_life/leave_important_week/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_week_or_new_problem(str_arg):
+    bad_child(str_arg)
+    print('come_group')
+
+def bad_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_week_or_new_problem('next_time_and_old_person')
