c02df3784aac4907624b3ea585e2cc085d6b049d
Jingkai Tang <jingkaitang@gmail.com>
2014-09-19 08:30:27 +0800

    mod: think know_small_thing.py from same work

---
+++ know_small_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_high_week_up_bad_thing(str_arg):
+    life_and_other_group(str_arg)
+    print('way')
+
+def life_and_other_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_high_week_up_bad_thing('find_next_group_on_point')
