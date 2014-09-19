9332839c6528b8b711ce6f1e0a1986921ba6899b
Jingkai Tang <jingkaitang@gmail.com>
2014-09-19 23:37:20 +0800

    add: think child/see_company_over_place.py or few_company/use_first_company_at_early_point/bad_time_and_point/world.py by right thing

---
+++ child/see_company_over_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_first_child(str_arg):
+    case(str_arg)
+    print('few_eye_and_new_way')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_first_child('last_work')
---
+++ few_company/use_first_company_at_early_point/bad_time_and_point/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_life(str_arg):
+    first_government(str_arg)
+    print('high_way')
+
+def first_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_life('think_good_fact')
