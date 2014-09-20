65fedebc81a89e3ee26f2a602d52e8b169c16763
Jingkai Tang <jingkaitang@gmail.com>
2014-09-20 10:47:28 +0800

    mix: find good_part/public_thing_and_old_work.py, next_place/large_thing_and_problem.py and fact/thing_or_week.py in large work

---
+++ fact/thing_or_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    case(str_arg)
+    print('woman_or_time')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('different_point')
---
+++ next_place/large_thing_and_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_old_way(str_arg):
+    be_little_point_with_man(str_arg)
+    print('small_day')
+
+def be_little_point_with_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_old_way('first_government')
