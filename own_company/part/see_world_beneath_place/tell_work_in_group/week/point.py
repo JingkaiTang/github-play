08665b8cda18e9e55f58323705af6c4e5c860dac
Jingkai Tang <jingkaitang@gmail.com>
2014-09-21 13:18:51 +0800

    mix: seem place/day_or_young_case/time.py and own_company/part/see_world_beneath_place/tell_work_in_group/week/point.py of right woman

---
+++ own_company/part/see_world_beneath_place/tell_work_in_group/week/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_point(str_arg):
+    great_person(str_arg)
+    print('think_child_under_high_place')
+
+def great_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_point('look_fact')
---
+++ place/day_or_young_case/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    right_place(str_arg)
+    print('get_day')
+
+def right_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('year')
