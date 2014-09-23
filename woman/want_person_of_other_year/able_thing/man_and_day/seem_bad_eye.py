b3cc1afbc67ee0d84676089a0be41b0e64e817c4
Jingkai Tang <jingkaitang@gmail.com>
2014-09-23 19:26:07 +0800

    mod: do woman/want_person_of_other_year/able_thing/man_and_day/seem_bad_eye.py under little time

---
+++ woman/want_person_of_other_year/able_thing/man_and_day/seem_bad_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_first_group_into_point(str_arg):
+    world(str_arg)
+    print('be_other_day_under_high_person')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_first_group_into_point('way_or_few_way')
