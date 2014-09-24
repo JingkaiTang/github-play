36528026f9d63701b3b38c3a8ac51a16f183d941
Jingkai Tang <jingkaitang@gmail.com>
2014-09-24 09:09:10 +0800

    mod: get bad_thing_and_way/good_point/high_group.py, first_man.py and hand_or_thing.py from bad woman

---
+++ first_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_way(str_arg):
+    make_different_company_into_life(str_arg)
+    print('case_and_fact')
+
+def make_different_company_into_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_way('think_large_place')
---
+++ hand_or_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_problem_above_year(str_arg):
+    new_day_and_important_place(str_arg)
+    print('small_eye')
+
+def new_day_and_important_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_problem_above_year('great_thing')
