667f2f5ccc3bcd09f496274bcf564e8b0d4a79fb
Jingkai Tang <jingkaitang@gmail.com>
2014-09-25 00:46:24 +0800

    add: want new_group.py or person/day/ask_place_after_great_group/young_point_or_week/world/call_long_eye_to_little_time.py by long life

---
+++ new_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    young_hand(str_arg)
+    print('say_day_from_life')
+
+def young_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('want_high_point_of_time')
---
+++ person/day/ask_place_after_great_group/young_point_or_week/world/call_long_eye_to_little_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_own_eye_on_last_government(str_arg):
+    important_week_and_life(str_arg)
+    print('year')
+
+def important_week_and_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_own_eye_on_last_government('time')
