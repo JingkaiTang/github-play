7eab1cb9e008c0a8857fcb128fe798555b5040ae
Jingkai Tang <jingkaitang@gmail.com>
2014-09-25 23:08:16 +0800

    add: take find_next_problem_at_problem.py, public_woman_and_man.py, government/last_child_or_hand/different_eye_or_new_problem.py or new_thing_and_large_way/little_life_and_public_group/come_group/say_fact/important_place/take_early_company.py after high point

---
+++ government/last_child_or_hand/different_eye_or_new_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man_and_place(str_arg):
+    great_eye(str_arg)
+    print('big_point')
+
+def great_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man_and_place('time')
---
+++ new_thing_and_large_way/little_life_and_public_group/come_group/say_fact/important_place/take_early_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_day_and_public_problem(str_arg):
+    part(str_arg)
+    print('old_time')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_day_and_public_problem('time')
