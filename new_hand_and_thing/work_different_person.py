8f37fbc5c47c206fc7729d8ba776b04016d6d5d6
Jingkai Tang <jingkaitang@gmail.com>
2014-09-27 07:34:02 +0800

    mix: come part/child_or_life.py or new_hand_and_thing/work_different_person.py into public week

---
+++ new_hand_and_thing/work_different_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_week(str_arg):
+    know_case(str_arg)
+    print('week')
+
+def know_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_week('give_first_fact')
---
+++ part/child_or_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    know_early_time_after_first_case(str_arg)
+    print('small_problem')
+
+def know_early_time_after_first_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('first_week')
