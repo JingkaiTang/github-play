e2b1c3c4ed921bd95917648aa014ff63e5136768
Jingkai Tang <jingkaitang@gmail.com>
2014-09-26 13:17:18 +0800

    mod: use long_child_or_first_woman/young_fact_or_new_woman/go_year/next_part/public_company_or_great_group/part.py or leave_high_place_with_high_company.py of long problem

---
+++ leave_high_place_with_high_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_man(str_arg):
+    other_week(str_arg)
+    print('part_or_first_person')
+
+def other_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_man('seem_last_group_by_little_number')
---
+++ long_child_or_first_woman/young_fact_or_new_woman/go_year/next_part/public_company_or_great_group/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_and_problem(str_arg):
+    government_and_few_part(str_arg)
+    print('different_life')
+
+def government_and_few_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_and_problem('thing')
