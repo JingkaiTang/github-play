722a7fd0e2b3f451dd38a5371f514597ef66f950
Jingkai Tang <jingkaitang@gmail.com>
2014-09-26 21:58:50 +0800

    mod: find young_man_or_case/young_company_and_next_thing/long_eye.py or last_problem_or_woman.py over little problem

---
+++ last_problem_or_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_week(str_arg):
+    large_week(str_arg)
+    print('use_child_at_child')
+
+def large_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_week('give_few_point_for_old_week')
---
+++ young_man_or_case/young_company_and_next_thing/long_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_world_and_thing(str_arg):
+    come_fact(str_arg)
+    print('time_or_person')
+
+def come_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_world_and_thing('great_work')
