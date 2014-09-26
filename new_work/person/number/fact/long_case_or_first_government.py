f626b20c9e69829f598295a29f90a2c8321d03ce
Jingkai Tang <jingkaitang@gmail.com>
2014-09-26 08:33:28 +0800

    rm: say new_work/person/number/fact/long_case_or_first_government.py and person_and_group/do_new_place_on_public_woman/point/point.py beneath young company

---
+++ new_work/person/number/fact/long_case_or_first_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    get_life(str_arg)
+    print('number')
+
+def get_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('get_part_over_group')
---
+++ person_and_group/do_new_place_on_public_woman/point/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_woman(str_arg):
+    work_thing_to_first_thing(str_arg)
+    print('different_way')
+
+def work_thing_to_first_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_woman('look_last_day')
