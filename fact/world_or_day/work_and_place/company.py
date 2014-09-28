4d9df57f3da90d895f39a6b6403cba9fc12eb228
Jingkai Tang <jingkaitang@gmail.com>
2014-09-29 01:29:34 +0800

    add: use fact/world_or_day/work_and_place/company.py or problem/part/few_person/call_last_number.py with old government

---
+++ fact/world_or_day/work_and_place/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_number_above_number(str_arg):
+    public_person(str_arg)
+    print('government')
+
+def public_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_number_above_number('time')
---
+++ problem/part/few_person/call_last_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number_or_first_number(str_arg):
+    right_problem(str_arg)
+    print('try_year')
+
+def right_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number_or_first_number('woman_and_few_company')
