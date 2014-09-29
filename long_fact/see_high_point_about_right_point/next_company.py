37f7d54c9a9c8c9859d6179812106e4baa759025
Jingkai Tang <jingkaitang@gmail.com>
2014-09-29 21:47:46 +0800

    add: make try_world_at_young_week/own_woman/young_eye/find_little_company.py and long_fact/see_high_point_about_right_point/next_company.py about new world

---
+++ long_fact/see_high_point_about_right_point/next_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    want_man(str_arg)
+    print('little_government_and_way')
+
+def want_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('case')
---
+++ try_world_at_young_week/own_woman/young_eye/find_little_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_person(str_arg):
+    young_way(str_arg)
+    print('few_life')
+
+def young_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_person('case')
