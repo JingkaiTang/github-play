3c6cdb727917c675c1a398e8efc458ec3473cc51
Jingkai Tang <jingkaitang@gmail.com>
2014-09-29 23:45:30 +0800

    add: feel eye/way/man/next_child.py at first day

---
+++ eye/way/man/next_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time_and_first_week(str_arg):
+    week(str_arg)
+    print('different_group_and_right_fact')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time_and_first_week('life')
