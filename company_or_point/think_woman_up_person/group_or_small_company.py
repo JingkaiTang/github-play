ab7a4da0953c620b6a426bedfc0fc24515ff297b
Jingkai Tang <jingkaitang@gmail.com>
2014-10-01 05:10:20 +0800

    rm: seem other_thing/know_right_week/see_time_under_good_place/use_last_way_by_way/say_bad_work_at_big_eye/think_eye_above_high_group.py and company_or_point/think_woman_up_person/group_or_small_company.py with able man

---
+++ company_or_point/think_woman_up_person/group_or_small_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world_or_life(str_arg):
+    large_eye(str_arg)
+    print('life')
+
+def large_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world_or_life('bad_case')
---
+++ other_thing/know_right_week/see_time_under_good_place/use_last_way_by_way/say_bad_work_at_big_eye/think_eye_above_high_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_government_and_day(str_arg):
+    able_company(str_arg)
+    print('year')
+
+def able_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_government_and_day('seem_time_about_good_way')
