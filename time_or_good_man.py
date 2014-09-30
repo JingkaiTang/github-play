b0c21e22fa19d01c516662a6ba141ff28e2fcdfd
Jingkai Tang <jingkaitang@gmail.com>
2014-09-30 16:23:45 +0800

    rm: call time_or_good_man.py on different man

---
+++ time_or_good_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    different_thing_or_small_world(str_arg)
+    print('right_place')
+
+def different_thing_or_small_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('good_life')
