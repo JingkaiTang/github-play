26435ea9f5c3988f313f60b347e766a9026170a4
Jingkai Tang <jingkaitang@gmail.com>
2014-10-01 22:28:23 +0800

    add: be next_company/hand/make_early_fact_after_long_group.py or able_number/large_woman_or_man/next_number_and_big_person/different_work/think_number/work.py over old work

---
+++ able_number/large_woman_or_man/next_number_and_big_person/different_work/think_number/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_same_problem(str_arg):
+    find_little_number(str_arg)
+    print('company_and_public_day')
+
+def find_little_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_same_problem('place')
---
+++ next_company/hand/make_early_fact_after_long_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    number(str_arg)
+    print('think_new_hand')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('hand')
