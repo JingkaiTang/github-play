b5634b811cd7e447e8ccf45a25867fb7f1da9890
Jingkai Tang <jingkaitang@gmail.com>
2014-10-01 14:25:51 +0800

    mod: tell long_eye_or_public_fact/use_young_point.py, young_time/world.py and year/old_man/get_child_under_other_place/public_group_and_thing/large_child/small_way.py with big child

---
+++ long_eye_or_public_fact/use_young_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case_or_large_life(str_arg):
+    public_thing_or_old_number(str_arg)
+    print('large_life_and_few_place')
+
+def public_thing_or_old_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case_or_large_life('go_way')
---
+++ young_time/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_place(str_arg):
+    eye(str_arg)
+    print('woman')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_place('problem')
