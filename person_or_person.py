a0fed03c863ec24e36af0cc5f991231ffe159fbd
Jingkai Tang <jingkaitang@gmail.com>
2014-10-01 08:59:22 +0800

    mod: try person_or_person.py with bad number

---
+++ person_or_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_fact(str_arg):
+    new_week(str_arg)
+    print('right_fact_or_young_fact')
+
+def new_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_fact('say_right_problem_in_case')
