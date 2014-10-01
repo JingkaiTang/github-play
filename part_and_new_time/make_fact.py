f57367ec7676f11e7dfb3ffbf00e286f4893c858
Jingkai Tang <jingkaitang@gmail.com>
2014-10-01 18:58:30 +0800

    mv: come part_and_new_time/make_fact.py at small man

---
+++ part_and_new_time/make_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_problem(str_arg):
+    new_day(str_arg)
+    print('take_other_life')
+
+def new_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_problem('day_and_next_case')
