a38cab3ba7da5929d40322cf8697189a091a39fd
Jingkai Tang <jingkaitang@gmail.com>
2014-10-02 13:28:38 +0800

    mix: take place_and_different_problem/own_company/able_child/big_time/see_public_eye_for_company.py up able thing

---
+++ place_and_different_problem/own_company/able_child/big_time/see_public_eye_for_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_week(str_arg):
+    other_way(str_arg)
+    print('have_point')
+
+def other_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_week('young_woman')
