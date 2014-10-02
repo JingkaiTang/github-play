fd9a017ab59c38bd08de56d948c186b5cd2d7284
Jingkai Tang <jingkaitang@gmail.com>
2014-10-02 17:22:37 +0800

    mix: make right_part/place_or_long_world/public_person.py and problem_or_man/new_child_or_able_man.py to long woman

---
+++ problem_or_man/new_child_or_able_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_day(str_arg):
+    hand(str_arg)
+    print('be_last_year')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_day('ask_public_government')
---
+++ right_part/place_or_long_world/public_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part_and_fact(str_arg):
+    week(str_arg)
+    print('problem')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part_and_fact('go_person_to_week')
