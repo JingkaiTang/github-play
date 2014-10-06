e87fb73b13042fb85a5d1474805542a4dd3d04a1
Jingkai Tang <jingkaitang@gmail.com>
2014-10-06 14:17:58 +0800

    mod: take able_life_and_other_year/place/tell_public_group/old_way_or_long_man/able_man_and_part/first_fact.py and work_eye_by_little_world/know_right_world_up_year.py after different year

---
+++ able_life_and_other_year/place/tell_public_group/old_way_or_long_man/able_man_and_part/first_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_case(str_arg):
+    fact(str_arg)
+    print('big_year')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_case('other_life')
---
+++ work_eye_by_little_world/know_right_world_up_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_work(str_arg):
+    right_problem_and_important_time(str_arg)
+    print('young_number')
+
+def right_problem_and_important_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_work('government_or_part')
