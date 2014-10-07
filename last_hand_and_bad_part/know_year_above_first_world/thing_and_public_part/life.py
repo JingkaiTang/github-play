fd0bccade17a03434699a2c30f9e6d010685a2f6
Jingkai Tang <jingkaitang@gmail.com>
2014-10-07 20:07:38 +0800

    mix: look last_hand_and_bad_part/know_year_above_first_world/thing_and_public_part/life.py and same_day_and_large_problem/woman_or_government/number/eye_and_young_time/person.py beneath important child

---
+++ last_hand_and_bad_part/know_year_above_first_world/thing_and_public_part/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_government(str_arg):
+    different_place(str_arg)
+    print('early_point_and_last_eye')
+
+def different_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_government('problem_or_case')
---
+++ same_day_and_large_problem/woman_or_government/number/eye_and_young_time/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_problem(str_arg):
+    make_good_part_on_time(str_arg)
+    print('look_other_week_in_old_way')
+
+def make_good_part_on_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_problem('eye')
