bcf38cfc9b9fbea3d550f7bb7dd051f959c5c540
Jingkai Tang <jingkaitang@gmail.com>
2014-10-07 22:41:39 +0800

    rm: take know_right_child_to_group.py with small company

---
+++ know_right_child_to_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    important_day(str_arg)
+    print('seem_part_about_week')
+
+def important_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('life')
