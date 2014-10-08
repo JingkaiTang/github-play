4e5e7283610886b3dc815a9aad577668d3041686
Jingkai Tang <jingkaitang@gmail.com>
2014-10-09 03:25:28 +0800

    mod: find able_eye/long_time_or_high_number.py by public world

---
+++ able_eye/long_time_or_high_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_young_week_at_new_way(str_arg):
+    own_time_and_important_world(str_arg)
+    print('leave_able_life')
+
+def own_time_and_important_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_young_week_at_new_way('way')
