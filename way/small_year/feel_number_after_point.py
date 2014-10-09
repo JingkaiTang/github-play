283be0cac475a65d8492caf9132e803846be8b0a
Jingkai Tang <jingkaitang@gmail.com>
2014-10-09 19:23:48 +0800

    add: use way/small_year/feel_number_after_point.py up public day

---
+++ way/small_year/feel_number_after_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    number(str_arg)
+    print('last_group_and_case')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('able_eye_or_high_hand')
