2463fe340ba855bf1ec94a16a8dfc36fb01ff5b1
Jingkai Tang <jingkaitang@gmail.com>
2014-10-09 09:07:29 +0800

    add: look child/case/work_or_child.py under public man

---
+++ child/case/work_or_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    old_week(str_arg)
+    print('group')
+
+def old_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('part_and_life')
