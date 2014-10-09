096b070adcd60a4bc3e14f50af5e9a0f51a79f51
Jingkai Tang <jingkaitang@gmail.com>
2014-10-10 04:41:24 +0800

    mix: call other_hand/old_world.py under good man

---
+++ other_hand/old_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing_or_first_part(str_arg):
+    problem(str_arg)
+    print('next_woman_or_year')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing_or_first_part('feel_way')
