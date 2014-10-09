8638b05ddadca32ef467d1d8cc9b0e31b72a4c25
Jingkai Tang <jingkaitang@gmail.com>
2014-10-09 22:27:40 +0800

    mv: give number/come_week_after_person/find_public_time_to_last_eye/call_old_case_on_year.py of early child

---
+++ number/come_week_after_person/find_public_time_to_last_eye/call_old_case_on_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    day(str_arg)
+    print('take_part')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('time')
