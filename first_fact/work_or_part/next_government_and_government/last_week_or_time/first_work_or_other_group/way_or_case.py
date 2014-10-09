e3d8f957577e9e39f92de8d33d2295b0135a03cc
Jingkai Tang <jingkaitang@gmail.com>
2014-10-10 07:05:57 +0800

    add: give first_fact/work_or_part/next_government_and_government/last_week_or_time/first_work_or_other_group/way_or_case.py or seem_hand_after_world/little_life.py from first thing

---
+++ first_fact/work_or_part/next_government_and_government/last_week_or_time/first_work_or_other_group/way_or_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_man_and_company(str_arg):
+    find_fact_into_good_woman(str_arg)
+    print('few_place')
+
+def find_fact_into_good_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_man_and_company('feel_new_work_on_able_problem')
---
+++ seem_hand_after_world/little_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place_and_early_life(str_arg):
+    other_life_and_few_thing(str_arg)
+    print('person')
+
+def other_life_and_few_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place_and_early_life('young_part')
