8db84473c067c896ab6b4833999db532a001a458
Jingkai Tang <jingkaitang@gmail.com>
2014-10-09 14:05:22 +0800

    mix: go child/old_problem_or_big_world.py in last world

---
+++ child/old_problem_or_big_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_company(str_arg):
+    say_other_government_by_good_thing(str_arg)
+    print('work')
+
+def say_other_government_by_good_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_company('day')
