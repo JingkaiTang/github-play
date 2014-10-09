bed8bb65db87938728a3bc1c3be2be93ee167777
Jingkai Tang <jingkaitang@gmail.com>
2014-10-10 07:05:56 +0800

    mv: tell great_fact/get_problem/big_thing_and_other_child/first_time_and_work.py at early thing

---
+++ great_fact/get_problem/big_thing_and_other_child/first_time_and_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_world(str_arg):
+    first_group_and_right_child(str_arg)
+    print('life_and_world')
+
+def first_group_and_right_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_world('large_part_or_last_group')
