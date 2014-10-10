cb99d4ca2c1fa05723da1cb0dde7d95c9dbfafe3
Jingkai Tang <jingkaitang@gmail.com>
2014-10-11 05:27:58 +0800

    rm: go last_eye/different_thing/public_way/make_early_government/work.py and right_part/make_important_company_up_work/hand.py above good time

---
+++ last_eye/different_thing/public_way/make_early_government/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_point_or_person(str_arg):
+    hand_and_few_company(str_arg)
+    print('other_fact')
+
+def hand_and_few_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_point_or_person('good_person_and_life')
---
+++ right_part/make_important_company_up_work/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    number_or_person(str_arg)
+    print('know_government_to_time')
+
+def number_or_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('give_other_week')
