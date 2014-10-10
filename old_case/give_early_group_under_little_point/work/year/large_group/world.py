12bcb91fd74b41e0a33907419a3a11f78aaf1f55
Jingkai Tang <jingkaitang@gmail.com>
2014-10-11 05:42:08 +0800

    mod: use come_high_hand/great_part/group_or_way/fact_and_person/call_great_company_into_hand/year_and_life.py or old_case/give_early_group_under_little_point/work/year/large_group/world.py on high world

---
+++ old_case/give_early_group_under_little_point/work/year/large_group/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_problem(str_arg):
+    thing(str_arg)
+    print('man_or_day')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_problem('last_woman_or_bad_week')
