152a930b31a98c60bc6175b40e45ffec8b0aa3e8
Jingkai Tang <jingkaitang@gmail.com>
2014-10-10 10:19:38 +0800

    mv: know time/first_part/make_man_after_small_woman/do_thing_to_problem/first_man/little_person_and_good_eye.py beneath high year

---
+++ time/first_part/make_man_after_small_woman/do_thing_to_problem/first_man/little_person_and_good_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_person_into_small_group(str_arg):
+    small_hand_or_other_problem(str_arg)
+    print('work_good_woman')
+
+def small_hand_or_other_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_person_into_small_group('try_week')
