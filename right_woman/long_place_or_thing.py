c6cd34b307c2eb4823849d11675dc5a2fcad1674
Jingkai Tang <jingkaitang@gmail.com>
2014-10-10 18:40:53 +0800

    rm: look right_woman/long_place_or_thing.py about new day

---
+++ right_woman/long_place_or_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_part(str_arg):
+    give_great_year(str_arg)
+    print('problem')
+
+def give_great_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_part('thing')
