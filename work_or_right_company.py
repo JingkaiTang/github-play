b1477fc6b9ace35cbed7369641abd9938d698141
Jingkai Tang <jingkaitang@gmail.com>
2014-10-11 21:53:02 +0800

    rm: give work_or_right_company.py for same work

---
+++ work_or_right_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    do_same_work_for_public_way(str_arg)
+    print('right_woman_or_young_person')
+
+def do_same_work_for_public_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('right_fact')
