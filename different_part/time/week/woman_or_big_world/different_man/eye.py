e430cfd7f310bebbff48f3d190c7c110a3481bd0
Jingkai Tang <jingkaitang@gmail.com>
2014-10-11 14:42:33 +0800

    mix: seem different_part/time/week/woman_or_big_world/different_man/eye.py from important day

---
+++ different_part/time/week/woman_or_big_world/different_man/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_hand(str_arg):
+    make_point_with_different_world(str_arg)
+    print('public_day')
+
+def make_point_with_different_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_hand('feel_group')
