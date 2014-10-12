26da037dacc02955e3ac93310181aa0a567236c6
Jingkai Tang <jingkaitang@gmail.com>
2014-10-12 21:35:38 +0800

    mod: call right_child/high_world_and_small_life.py, fact/call_next_case_to_same_hand/next_life/call_number_over_small_place.py or woman/point_or_high_woman.py on long fact

---
+++ fact/call_next_case_to_same_hand/next_life/call_number_over_small_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_large_case_by_same_person(str_arg):
+    right_case_or_early_problem(str_arg)
+    print('high_number')
+
+def right_case_or_early_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_large_case_by_same_person('time')
---
+++ right_child/high_world_and_small_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_year(str_arg):
+    young_work_or_person(str_arg)
+    print('group_or_way')
+
+def young_work_or_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_year('want_great_hand_into_long_year')
