a106d2af2922210ebc38cef52d432899507a4fc1
Jingkai Tang <jingkaitang@gmail.com>
2014-10-13 05:54:47 +0800

    mix: go government/high_part/way/number/government_or_problem.py from little fact

---
+++ government/high_part/way/number/government_or_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_eye(str_arg):
+    try_woman(str_arg)
+    print('case')
+
+def try_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_eye('long_year')
