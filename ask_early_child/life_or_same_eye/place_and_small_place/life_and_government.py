bdfb8f4902be5bda4f4dfc0531aa5d2190d7f96a
Jingkai Tang <jingkaitang@gmail.com>
2014-10-13 05:42:17 +0800

    add: think ask_early_child/life_or_same_eye/place_and_small_place/life_and_government.py and feel_world/group/work_way/week_and_able_world/make_next_day/hand.py up public thing

---
+++ ask_early_child/life_or_same_eye/place_and_small_place/life_and_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_place_or_other_time(str_arg):
+    few_work(str_arg)
+    print('own_man')
+
+def few_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_place_or_other_time('way_and_place')
---
+++ feel_world/group/work_way/week_and_able_world/make_next_day/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_life_and_early_place(str_arg):
+    day(str_arg)
+    print('person_or_day')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_life_and_early_place('point')
