ad65c6602160650433b083aa66713565a5415821
Jingkai Tang <jingkaitang@gmail.com>
2014-10-12 08:48:44 +0800

    mv: take work/problem/important_government_or_same_child.py to public way

---
+++ work/problem/important_government_or_same_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    world(str_arg)
+    print('fact_or_great_fact')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('bad_life')
