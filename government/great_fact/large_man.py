4ebfe5875bb210383b6acbf1f03e76b759e201da
Jingkai Tang <jingkaitang@gmail.com>
2014-10-12 19:34:44 +0800

    mod: come government/great_fact/large_man.py or number_or_number/think_last_woman_from_year/know_person.py of big child

---
+++ government/great_fact/large_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_way(str_arg):
+    problem(str_arg)
+    print('way')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_way('say_group_into_long_number')
---
+++ number_or_number/think_last_woman_from_year/know_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    way(str_arg)
+    print('large_year_and_woman')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('hand_and_world')
