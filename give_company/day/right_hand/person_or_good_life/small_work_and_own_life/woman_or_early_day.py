69fbce5ad1ef0204c90f33e175f4fbffdc145683
Jingkai Tang <jingkaitang@gmail.com>
2014-10-12 19:03:40 +0800

    mv: make give_company/day/right_hand/person_or_good_life/small_work_and_own_life/woman_or_early_day.py beneath small government

---
+++ give_company/day/right_hand/person_or_good_life/small_work_and_own_life/woman_or_early_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_year_and_person(str_arg):
+    time_or_small_group(str_arg)
+    print('look_bad_problem')
+
+def time_or_small_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_year_and_person('different_point')
