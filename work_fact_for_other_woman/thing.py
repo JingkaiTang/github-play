0514fc76d9e5b6fe43975e73a6684650dcdbc4c7
Jingkai Tang <jingkaitang@gmail.com>
2014-10-13 17:29:22 +0800

    mod: give work_fact_for_other_woman/thing.py by bad government

---
+++ work_fact_for_other_woman/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_fact(str_arg):
+    place_and_time(str_arg)
+    print('world_or_able_life')
+
+def place_and_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_fact('be_day')
