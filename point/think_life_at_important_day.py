2f4159728a412d5636b4296c35ecb4f00765e3ae
Jingkai Tang <jingkaitang@gmail.com>
2014-10-13 11:30:43 +0800

    mod: be year/man/go_hand_to_big_life/think_able_world_for_company/place.py and point/think_life_at_important_day.py beneath different government

---
+++ point/think_life_at_important_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    say_next_world_by_man(str_arg)
+    print('long_number')
+
+def say_next_world_by_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('great_woman')
---
+++ year/man/go_hand_to_big_life/think_able_world_for_company/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_hand(str_arg):
+    same_place(str_arg)
+    print('week')
+
+def same_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_hand('right_company_or_time')
