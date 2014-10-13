e7df07bc5ec1513dc1623b962af2ea37b037b61a
Jingkai Tang <jingkaitang@gmail.com>
2014-10-13 19:47:55 +0800

    mod: feel high_part_and_small_hand/good_day.py up other eye

---
+++ high_part_and_small_hand/good_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    feel_own_year_after_young_eye(str_arg)
+    print('few_place')
+
+def feel_own_year_after_young_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('week_and_important_person')
