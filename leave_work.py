436b2d915ab938070afabe100c737d995d1b7333
Jingkai Tang <jingkaitang@gmail.com>
2014-10-13 13:34:34 +0800

    rm: do leave_work.py in old company

---
+++ leave_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    thing_or_last_way(str_arg)
+    print('man')
+
+def thing_or_last_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('work_or_right_number')
