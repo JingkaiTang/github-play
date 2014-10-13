5a93078b04369695d9cc3f670eb42e909b283061
Jingkai Tang <jingkaitang@gmail.com>
2014-10-13 11:56:14 +0800

    mv: take other_person/feel_next_company/same_way/use_fact_of_company/bad_life_or_company/number.py or first_life/bad_world_and_next_hand/young_person/small_case.py on big work

---
+++ first_life/bad_world_and_next_hand/young_person/small_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_man_or_small_case(str_arg):
+    seem_life_for_high_part(str_arg)
+    print('work')
+
+def seem_life_for_high_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_man_or_small_case('go_place')
---
+++ other_person/feel_next_company/same_way/use_fact_of_company/bad_life_or_company/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    important_fact(str_arg)
+    print('group_and_day')
+
+def important_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('part')
