a40a100812dd57514c35c332c5b71617b5115511
Jingkai Tang <jingkaitang@gmail.com>
2014-10-13 15:53:31 +0800

    rm: go bad_case/find_new_life/place.py or small_number_or_work/big_woman_and_day/good_time_or_way/say_problem/company.py beneath other child

---
+++ bad_case/find_new_life/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_work(str_arg):
+    different_way(str_arg)
+    print('say_new_thing')
+
+def different_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_work('do_group')
---
+++ small_number_or_work/big_woman_and_day/good_time_or_way/say_problem/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    early_group(str_arg)
+    print('good_place')
+
+def early_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('world')
