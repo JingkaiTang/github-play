75fe15be65e9cf19406a7ac63ae601505e99619b
Jingkai Tang <jingkaitang@gmail.com>
2014-10-13 11:31:28 +0800

    add: think week/same_eye_or_child/day.py and important_point_and_great_group/ask_right_world_by_public_year/call_new_government/place/few_life_and_small_child.py beneath small thing

---
+++ important_point_and_great_group/ask_right_world_by_public_year/call_new_government/place/few_life_and_small_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    government_or_way(str_arg)
+    print('world_or_able_thing')
+
+def government_or_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('year')
---
+++ week/same_eye_or_child/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place_and_government(str_arg):
+    year(str_arg)
+    print('point')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place_and_government('leave_right_hand_up_case')
