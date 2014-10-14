dd9657008e53089b5b44e2b76ab28fe8b560e671
Jingkai Tang <jingkaitang@gmail.com>
2014-10-14 13:02:30 +0800

    add: feel high_case/large_number.py in small group

---
+++ high_case/large_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_way_or_last_year(str_arg):
+    see_man_from_hand(str_arg)
+    print('little_life_or_long_woman')
+
+def see_man_from_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_way_or_last_year('be_world_in_year')
