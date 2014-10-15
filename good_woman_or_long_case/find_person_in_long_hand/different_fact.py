9debf69de1a0a01d00169a68e4002fddb13c594a
Jingkai Tang <jingkaitang@gmail.com>
2014-10-15 22:08:17 +0800

    mix: do good_woman_or_long_case/find_person_in_long_hand/different_fact.py and be_first_woman.py after small world

---
+++ good_woman_or_long_case/find_person_in_long_hand/different_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point_and_work(str_arg):
+    government(str_arg)
+    print('new_place')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point_and_work('company')
