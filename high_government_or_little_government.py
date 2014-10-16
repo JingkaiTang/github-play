8f74712ccd21da32bd490241fe73ad713fc7dec4
Jingkai Tang <jingkaitang@gmail.com>
2014-10-16 13:31:35 +0800

    mv: say high_government_or_little_government.py or own_way_and_government.py with right place

---
+++ high_government_or_little_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    eye_or_different_life(str_arg)
+    print('able_work')
+
+def eye_or_different_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('know_day_into_government')
---
+++ own_way_and_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_way_and_man(str_arg):
+    little_thing(str_arg)
+    print('public_point')
+
+def little_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_way_and_man('be_person_above_different_man')
