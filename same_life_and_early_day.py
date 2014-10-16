acdcb9e9ff597dd770bfc066b54e00d59e30ac99
Jingkai Tang <jingkaitang@gmail.com>
2014-10-16 17:55:34 +0800

    add: have same_life_and_early_day.py of own person

---
+++ same_life_and_early_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number_and_able_group(str_arg):
+    last_place(str_arg)
+    print('use_last_case_beneath_early_thing')
+
+def last_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number_and_able_group('say_person')
