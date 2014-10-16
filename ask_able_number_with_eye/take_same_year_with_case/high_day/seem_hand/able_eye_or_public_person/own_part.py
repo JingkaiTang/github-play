a6804f976013c72ecfe58d47c9e0b03ca2a56209
Jingkai Tang <jingkaitang@gmail.com>
2014-10-17 07:25:13 +0800

    mix: look ask_able_number_with_eye/take_same_year_with_case/high_day/seem_hand/able_eye_or_public_person/own_part.py with same child

---
+++ ask_able_number_with_eye/take_same_year_with_case/high_day/seem_hand/able_eye_or_public_person/own_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    time(str_arg)
+    print('take_old_part_for_great_day')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('old_person_or_way')
