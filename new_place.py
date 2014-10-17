6dd4afe830e4e898427552ea7a1be5ebf0c66f2f
Jingkai Tang <jingkaitang@gmail.com>
2014-10-17 09:24:33 +0800

    mod: know new_place.py to other problem

---
+++ new_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_man_and_bad_woman(str_arg):
+    give_last_case_into_child(str_arg)
+    print('see_part_by_way')
+
+def give_last_case_into_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_man_and_bad_woman('man')
