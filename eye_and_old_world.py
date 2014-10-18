38249e3e71de1f16bc58d83636521e2e343c968e
Jingkai Tang <jingkaitang@gmail.com>
2014-10-19 07:31:42 +0800

    mix: go eye_and_old_world.py with last company

---
+++ eye_and_old_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_place(str_arg):
+    leave_good_company(str_arg)
+    print('call_week_at_able_eye')
+
+def leave_good_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_place('great_day')
