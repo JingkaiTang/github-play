b4dbb593221d2804c9e8197e6a694d42f7b84ae6
Jingkai Tang <jingkaitang@gmail.com>
2014-10-19 04:38:31 +0800

    add: go able_government/man_or_old_year.py over young government

---
+++ able_government/man_or_old_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact_and_young_part(str_arg):
+    good_year_or_bad_hand(str_arg)
+    print('long_group_and_company')
+
+def good_year_or_bad_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact_and_young_part('long_number')
