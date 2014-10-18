7a8fc3eaa7d13ad8519d3a5430de7d08bc6a3ac4
Jingkai Tang <jingkaitang@gmail.com>
2014-10-19 07:42:13 +0800

    rm: ask group_and_eye.py from last week

---
+++ group_and_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_company(str_arg):
+    man(str_arg)
+    print('important_case')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_company('do_way_from_different_day')
