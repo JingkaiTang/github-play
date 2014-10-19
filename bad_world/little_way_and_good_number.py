aee25157b1bc1f633f81e02c468e6314f0c07adc
Jingkai Tang <jingkaitang@gmail.com>
2014-10-19 10:51:56 +0800

    add: try company/woman/bad_case_or_place/last_time_and_place/public_hand_and_group/want_public_group.py and bad_world/little_way_and_good_number.py from good week

---
+++ bad_world/little_way_and_good_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_great_time_up_thing(str_arg):
+    company_or_life(str_arg)
+    print('woman_or_child')
+
+def company_or_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_great_time_up_thing('find_place')
---
+++ company/woman/bad_case_or_place/last_time_and_place/public_hand_and_group/want_public_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government_and_big_thing(str_arg):
+    large_number_or_last_group(str_arg)
+    print('small_week')
+
+def large_number_or_last_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government_and_big_thing('same_child')
