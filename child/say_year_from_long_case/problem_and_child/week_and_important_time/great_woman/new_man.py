4952139077b8ab42226a1521aadfb1077481bd29
Jingkai Tang <jingkaitang@gmail.com>
2014-10-20 01:26:56 +0800

    mix: try public_case.py or child/say_year_from_long_case/problem_and_child/week_and_important_time/great_woman/new_man.py by few fact

---
+++ child/say_year_from_long_case/problem_and_child/week_and_important_time/great_woman/new_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_part(str_arg):
+    see_woman_at_child(str_arg)
+    print('look_part_to_big_woman')
+
+def see_woman_at_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_part('little_place_and_next_week')
---
+++ public_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_hand(str_arg):
+    big_life(str_arg)
+    print('right_time_and_government')
+
+def big_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_hand('group')
