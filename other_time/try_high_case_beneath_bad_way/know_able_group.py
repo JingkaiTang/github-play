7afc069120bbb8986059f120e388bb1eb6e18cf3
Jingkai Tang <jingkaitang@gmail.com>
2014-10-20 01:29:14 +0800

    mix: see other_time/try_high_case_beneath_bad_way/know_able_group.py up important group

---
+++ other_time/try_high_case_beneath_bad_way/know_able_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_life_with_hand(str_arg):
+    work_or_eye(str_arg)
+    print('public_place_or_right_company')
+
+def work_or_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_life_with_hand('last_work_and_day')
