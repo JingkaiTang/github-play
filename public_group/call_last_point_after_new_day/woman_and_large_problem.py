21bee3c0ede1bcd124e2980ca1be64d05957832d
Jingkai Tang <jingkaitang@gmail.com>
2014-10-20 18:27:45 +0800

    mix: leave public_group/call_last_point_after_new_day/woman_and_large_problem.py for right hand

---
+++ public_group/call_last_point_after_new_day/woman_and_large_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact_or_case(str_arg):
+    public_company(str_arg)
+    print('come_point')
+
+def public_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact_or_case('high_week')
