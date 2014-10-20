21626a7246536afb80a7d2cf4bea4f95bf52dd60
Jingkai Tang <jingkaitang@gmail.com>
2014-10-20 18:40:44 +0800

    add: get make_bad_year/world/use_group/eye_and_way/right_group/point.py into young child

---
+++ make_bad_year/world/use_group/eye_and_way/right_group/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_child(str_arg):
+    fact(str_arg)
+    print('time')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_child('government')
