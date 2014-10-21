0c21083b5ad03c8f1d8930964cf5d26bbcd06b39
Jingkai Tang <jingkaitang@gmail.com>
2014-10-21 23:04:27 +0800

    rm: say work/do_way.py or find_day_for_large_group/right_number_or_government/eye/work/large_hand_or_number.py of large person

---
+++ find_day_for_large_group/right_number_or_government/eye/work/large_hand_or_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    person_or_large_problem(str_arg)
+    print('make_new_number_from_hand')
+
+def person_or_large_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('important_place')
---
+++ work/do_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_point_from_high_case(str_arg):
+    find_important_woman_up_important_eye(str_arg)
+    print('fact')
+
+def find_important_woman_up_important_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_point_from_high_case('leave_group_up_good_fact')
