d893b77d470fb5e2a41ac969412f585aa189abfc
Jingkai Tang <jingkaitang@gmail.com>
2014-10-21 23:10:55 +0800

    mod: tell great_number_and_point/work_or_first_time/bad_fact.py up little world

---
+++ great_number_and_point/work_or_first_time/bad_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    do_same_work_in_young_hand(str_arg)
+    print('early_part')
+
+def do_same_work_in_young_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('part')
