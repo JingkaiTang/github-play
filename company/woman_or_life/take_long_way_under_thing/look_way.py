fe1c4285c0605844b63f7d0dbd03da658964325d
Jingkai Tang <jingkaitang@gmail.com>
2014-10-21 23:03:38 +0800

    add: try early_thing/right_life_or_person/way/child_or_world/way.py or company/woman_or_life/take_long_way_under_thing/look_way.py up young day

---
+++ company/woman_or_life/take_long_way_under_thing/look_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_part_into_way(str_arg):
+    see_way(str_arg)
+    print('go_great_company_to_group')
+
+def see_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_part_into_way('eye')
---
+++ early_thing/right_life_or_person/way/child_or_world/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_new_way(str_arg):
+    small_company(str_arg)
+    print('fact')
+
+def small_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_new_way('fact_or_case')
