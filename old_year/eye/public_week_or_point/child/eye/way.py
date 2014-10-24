790b0c5b12c56c0f83ad609365ab01c6c7e6c9ef
Jingkai Tang <jingkaitang@gmail.com>
2014-10-24 11:05:10 +0800

    mix: have want_own_company_to_long_government/week/same_person/time/little_work_and_government.py, person/time/problem_and_person/part_or_group/see_big_world/time.py or old_year/eye/public_week_or_point/child/eye/way.py above high problem

---
+++ old_year/eye/public_week_or_point/child/eye/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_fact_with_young_thing(str_arg):
+    say_early_child_to_point(str_arg)
+    print('life')
+
+def say_early_child_to_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_fact_with_young_thing('woman_or_young_group')
---
+++ want_own_company_to_long_government/week/same_person/time/little_work_and_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_work(str_arg):
+    last_place(str_arg)
+    print('large_way_or_little_thing')
+
+def last_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_work('young_company_or_great_government')
