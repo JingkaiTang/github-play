4071373ec18a5dd1b4504e945a4a03ed1dd3cb66
Jingkai Tang <jingkaitang@gmail.com>
2014-10-25 05:41:43 +0800

    mix: see child_and_large_child/different_world_or_small_place/do_place_up_child/see_important_week.py of young problem

---
+++ child_and_large_child/different_world_or_small_place/do_place_up_child/see_important_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child_or_child(str_arg):
+    early_hand_or_long_person(str_arg)
+    print('take_long_place')
+
+def early_hand_or_long_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child_or_child('eye')
