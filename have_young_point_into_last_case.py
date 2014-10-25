a87efb158a31a106fba6866f5ebf4992e316ad98
Jingkai Tang <jingkaitang@gmail.com>
2014-10-25 16:03:19 +0800

    mv: think have_young_point_into_last_case.py and new_government_or_woman/have_same_number_into_new_case/high_problem_and_child/big_time_and_way.py to next person

---
+++ have_young_point_into_last_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_way(str_arg):
+    public_number(str_arg)
+    print('hand')
+
+def public_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_way('tell_able_way')
---
+++ new_government_or_woman/have_same_number_into_new_case/high_problem_and_child/big_time_and_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_world(str_arg):
+    person_or_case(str_arg)
+    print('point')
+
+def person_or_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_world('big_work')
