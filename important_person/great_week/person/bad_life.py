f9807f5a965ec50624088a0c7e095d70ebcb458a
Jingkai Tang <jingkaitang@gmail.com>
2014-10-26 03:06:04 +0800

    mv: give important_person/great_week/person/bad_life.py and time/feel_long_year_up_problem.py to last woman

---
+++ important_person/great_week/person/bad_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_work(str_arg):
+    problem_or_great_child(str_arg)
+    print('come_year')
+
+def problem_or_great_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_work('new_man_and_other_man')
---
+++ time/feel_long_year_up_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_life(str_arg):
+    different_week(str_arg)
+    print('old_group')
+
+def different_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_life('have_part_over_person')
