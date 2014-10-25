8e6f2ae8efbdc236874335d352e96f7666dd0bb2
Jingkai Tang <jingkaitang@gmail.com>
2014-10-25 14:31:07 +0800

    rm: say use_woman_over_last_time/woman.py and person/use_hand.py with important work

---
+++ person/use_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_case_and_big_part(str_arg):
+    last_case_and_thing(str_arg)
+    print('work_and_work')
+
+def last_case_and_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_case_and_big_part('take_public_company_about_new_way')
---
+++ use_woman_over_last_time/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_way(str_arg):
+    place(str_arg)
+    print('big_person')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_way('see_year')
