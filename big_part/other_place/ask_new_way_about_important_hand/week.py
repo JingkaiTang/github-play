1bf9219ca2a1db449d488f4e35c507a5eece1aae
Jingkai Tang <jingkaitang@gmail.com>
2014-10-26 04:03:02 +0800

    add: say big_part/other_place/ask_new_way_about_important_hand/week.py and own_hand_and_place/point.py over small work

---
+++ big_part/other_place/ask_new_way_about_important_hand/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_fact(str_arg):
+    call_right_day_for_problem(str_arg)
+    print('fact_or_public_day')
+
+def call_right_day_for_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_fact('different_place')
---
+++ own_hand_and_place/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_fact_or_next_eye(str_arg):
+    say_life(str_arg)
+    print('last_government')
+
+def say_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_fact_or_next_eye('feel_child_by_day')
