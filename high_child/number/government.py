e07d6b9ecfad697e165dcdccb993ff9d815586f9
Jingkai Tang <jingkaitang@gmail.com>
2014-10-26 08:05:20 +0800

    mv: ask high_child/number/government.py with old thing

---
+++ high_child/number/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_group_and_person(str_arg):
+    hand_and_same_problem(str_arg)
+    print('able_fact_or_number')
+
+def hand_and_same_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_group_and_person('person_or_case')
