53488bfa194dc0a2df72e2f6fc233932b7c5b436
Jingkai Tang <jingkaitang@gmail.com>
2014-10-27 15:02:13 +0800

    mv: go part/eye_and_other_way/work_first_time_with_work.py and last_time_or_public_place/see_part/small_company/life/right_case.py under early person

---
+++ last_time_or_public_place/see_part/small_company/life/right_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_problem(str_arg):
+    feel_same_government(str_arg)
+    print('own_life')
+
+def feel_same_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_problem('next_work')
---
+++ part/eye_and_other_way/work_first_time_with_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    government(str_arg)
+    print('young_hand_and_great_year')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('time_or_small_company')
