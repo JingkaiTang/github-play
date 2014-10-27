9ba2426b5240c1ff330fb210e1b249a0a05caadd
Jingkai Tang <jingkaitang@gmail.com>
2014-10-27 11:18:03 +0800

    rm: make different_work_or_right_problem/good_life/eye/place_and_public_government/call_hand.py by large problem

---
+++ different_work_or_right_problem/good_life/eye/place_and_public_government/call_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person_or_thing(str_arg):
+    own_child(str_arg)
+    print('give_hand_about_group')
+
+def own_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person_or_thing('different_fact_and_important_thing')
