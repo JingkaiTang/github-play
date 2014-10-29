e6b10ebd884c747bd7e78728bfc05b893fd15f66
Jingkai Tang <jingkaitang@gmail.com>
2014-10-29 13:09:15 +0800

    mix: make right_year.py into important year

---
+++ right_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_child(str_arg):
+    important_day_or_man(str_arg)
+    print('young_part')
+
+def important_day_or_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_child('important_child')
