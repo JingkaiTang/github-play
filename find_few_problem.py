64da715a6a1a6f6beee53f1d1c235a457afee927
Jingkai Tang <jingkaitang@gmail.com>
2014-10-30 01:30:52 +0800

    rm: leave find_few_problem.py or look_large_eye.py after little time

---
+++ find_few_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    group_or_government(str_arg)
+    print('come_point')
+
+def group_or_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('case_and_eye')
---
+++ look_large_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_life_on_big_time(str_arg):
+    day(str_arg)
+    print('high_group_or_same_time')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_life_on_big_time('way')
