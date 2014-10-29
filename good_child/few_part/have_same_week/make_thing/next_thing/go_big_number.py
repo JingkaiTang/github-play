114f32ecbb2b7b777a52687b02bf49163705de62
Jingkai Tang <jingkaitang@gmail.com>
2014-10-29 10:04:47 +0800

    rm: see good_child/few_part/have_same_week/make_thing/next_thing/go_big_number.py and last_day/important_woman_and_bad_problem/hand/year/year_and_part.py beneath important world

---
+++ good_child/few_part/have_same_week/make_thing/next_thing/go_big_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_fact(str_arg):
+    problem(str_arg)
+    print('high_group')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_fact('want_different_woman_at_bad_man')
---
+++ last_day/important_woman_and_bad_problem/hand/year/year_and_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company_and_life(str_arg):
+    want_next_day_up_year(str_arg)
+    print('seem_case')
+
+def want_next_day_up_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company_and_life('thing_or_eye')
