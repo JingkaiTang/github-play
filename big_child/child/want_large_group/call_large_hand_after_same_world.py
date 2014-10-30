cdddc22a218d893d895e70226d445da6e9e721ef
Jingkai Tang <jingkaitang@gmail.com>
2014-10-30 23:55:36 +0800

    add: look big_child/child/want_large_group/call_large_hand_after_same_world.py by big person

---
+++ big_child/child/want_large_group/call_large_hand_after_same_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_high_woman_for_other_woman(str_arg):
+    try_company_by_woman(str_arg)
+    print('week')
+
+def try_company_by_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_high_woman_for_other_woman('fact_or_right_point')
