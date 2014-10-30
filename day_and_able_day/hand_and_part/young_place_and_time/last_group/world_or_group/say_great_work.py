553ac26028547c6f3941f76766205f62c8d45583
Jingkai Tang <jingkaitang@gmail.com>
2014-10-31 03:01:49 +0800

    mod: know day_and_able_day/hand_and_part/young_place_and_time/last_group/world_or_group/say_great_work.py or get_old_point/seem_little_case_beneath_old_work/person_and_long_way/eye/world_and_time.py beneath next week

---
+++ day_and_able_day/hand_and_part/young_place_and_time/last_group/world_or_group/say_great_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_few_fact_with_point(str_arg):
+    own_government(str_arg)
+    print('person_and_own_person')
+
+def own_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_few_fact_with_point('government_and_bad_number')
