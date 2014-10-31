08cd5ec85f76acb431b04046047fa0aeac663231
Jingkai Tang <jingkaitang@gmail.com>
2014-10-31 12:47:58 +0800

    mod: tell eye/own_part.py, be_person/part_or_week/woman.py and own_way/fact/long_man/same_case/long_life/last_man.py by own number

---
+++ eye/own_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand_and_year(str_arg):
+    point(str_arg)
+    print('use_other_group')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand_and_year('child_and_new_government')
---
+++ own_way/fact/long_man/same_case/long_life/last_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    part(str_arg)
+    print('able_group_or_long_life')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('be_long_company_at_right_child')
