69a4b16b095c8da0c740bc8772fbcf753a924e8d
Jingkai Tang <jingkaitang@gmail.com>
2014-11-02 04:48:06 +0800

    mod: take few_work/thing.py at high point

---
+++ few_work/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part_and_way(str_arg):
+    day_and_way(str_arg)
+    print('leave_high_problem_with_long_time')
+
+def day_and_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part_and_way('know_other_fact')
