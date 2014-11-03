bf0a6b633aef2cb92654537361cf52293eacc44e
Jingkai Tang <jingkaitang@gmail.com>
2014-11-03 17:56:57 +0800

    mod: get group_and_other_case/go_day/good_case/government.py on few group

---
+++ group_and_other_case/go_day/good_case/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    life(str_arg)
+    print('great_woman')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('see_great_time_beneath_world')
