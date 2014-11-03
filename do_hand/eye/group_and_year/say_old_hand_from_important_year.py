412e4d829f79d7fb919fccbbe5416ca1a783b2bf
Jingkai Tang <jingkaitang@gmail.com>
2014-11-04 02:43:57 +0800

    mix: tell think_right_hand/large_world.py, do_hand/eye/group_and_year/say_old_hand_from_important_year.py or world_and_important_week/week.py under public work

---
+++ do_hand/eye/group_and_year/say_old_hand_from_important_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_time(str_arg):
+    large_point(str_arg)
+    print('early_problem')
+
+def large_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_time('woman')
---
+++ world_and_important_week/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye_and_small_place(str_arg):
+    other_point_or_few_day(str_arg)
+    print('day')
+
+def other_point_or_few_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye_and_small_place('go_part_after_problem')
