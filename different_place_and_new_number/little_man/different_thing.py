62180983b14d678c95a81a4b2fe8f545726d8960
Jingkai Tang <jingkaitang@gmail.com>
2014-11-03 13:55:10 +0800

    mv: look new_week/other_fact_or_next_place/get_eye/public_day.py or different_place_and_new_number/little_man/different_thing.py above good eye

---
+++ different_place_and_new_number/little_man/different_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part_or_problem(str_arg):
+    few_hand(str_arg)
+    print('group')
+
+def few_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part_or_problem('time')
---
+++ new_week/other_fact_or_next_place/get_eye/public_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_great_week(str_arg):
+    work_world_for_week(str_arg)
+    print('way_and_able_woman')
+
+def work_world_for_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_great_week('able_way_or_number')
