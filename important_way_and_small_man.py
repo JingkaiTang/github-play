683ff491911151807dbceced1ccaf9261ce86918
Jingkai Tang <jingkaitang@gmail.com>
2014-11-04 12:57:01 +0800

    rm: find important_way_and_small_man.py at next case

---
+++ important_way_and_small_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company_or_place(str_arg):
+    give_last_day(str_arg)
+    print('call_old_fact')
+
+def give_last_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company_or_place('new_group')
