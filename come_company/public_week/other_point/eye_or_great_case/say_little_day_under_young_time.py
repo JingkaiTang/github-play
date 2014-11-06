f4486efa28f1f3d802127ac27dea42b285e46d79
Jingkai Tang <jingkaitang@gmail.com>
2014-11-06 18:13:18 +0800

    mod: take come_company/public_week/other_point/eye_or_great_case/say_little_day_under_young_time.py and eye/see_time.py of little group

---
+++ come_company/public_week/other_point/eye_or_great_case/say_little_day_under_young_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    look_place_up_small_life(str_arg)
+    print('see_bad_point_after_young_group')
+
+def look_place_up_small_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('point_and_first_point')
---
+++ eye/see_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_long_point_over_time(str_arg):
+    week_or_day(str_arg)
+    print('call_company')
+
+def week_or_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_long_point_over_time('right_part')
