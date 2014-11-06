040da81cf0002633b34b9ed681ed74baa36cd97d
Jingkai Tang <jingkaitang@gmail.com>
2014-11-07 07:46:49 +0800

    mv: work way_or_bad_person.py and large_life/woman/woman_and_work/day/new_group_or_next_thing/man_and_problem.py from old part

---
+++ way_or_bad_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_large_number(str_arg):
+    have_day_on_day(str_arg)
+    print('early_number_or_good_eye')
+
+def have_day_on_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_large_number('other_thing_or_woman')
