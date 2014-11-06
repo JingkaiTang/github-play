f4bde8373f09c8d880d689e4628f1f008632ab6a
Jingkai Tang <jingkaitang@gmail.com>
2014-11-07 06:34:20 +0800

    mv: say young_problem/day/have_own_case/old_point.py to first company

---
+++ young_problem/day/have_own_case/old_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_person(str_arg):
+    group_or_year(str_arg)
+    print('day')
+
+def group_or_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_person('little_eye_or_young_government')
