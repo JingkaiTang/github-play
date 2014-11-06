7a835e5329adf46ab14b56fb5ca0ec0357c11d45
Jingkai Tang <jingkaitang@gmail.com>
2014-11-07 00:18:30 +0800

    mix: use call_public_place_beneath_other_year/day/problem.py beneath small part

---
+++ call_public_place_beneath_other_year/day/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    different_hand(str_arg)
+    print('work')
+
+def different_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('say_world')
