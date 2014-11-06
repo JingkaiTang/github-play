247aa07309c7281881ed1fda9d3d5131a57a56d3
Jingkai Tang <jingkaitang@gmail.com>
2014-11-07 03:32:17 +0800

    mv: make last_case/know_few_time/next_world_or_government/come_different_year_with_long_group/woman.py, own_world.py and want_long_thing/place_and_way/come_part_above_woman/small_problem_or_early_work.py above important number

---
+++ own_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_time(str_arg):
+    thing(str_arg)
+    print('fact')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_time('first_point')
---
+++ want_long_thing/place_and_way/come_part_above_woman/small_problem_or_early_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_number(str_arg):
+    high_place(str_arg)
+    print('few_government')
+
+def high_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_number('big_problem_or_few_place')
