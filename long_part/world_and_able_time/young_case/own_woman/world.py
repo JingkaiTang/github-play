eed2069e9ae0e7894ec87c0f10877061b0a63172
Jingkai Tang <jingkaitang@gmail.com>
2014-11-06 12:45:02 +0800

    mv: get get_long_work_into_hand.py, long_part/world_and_able_time/young_case/own_woman/world.py or think_part/government.py on new problem

---
+++ long_part/world_and_able_time/young_case/own_woman/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_person_about_problem(str_arg):
+    person(str_arg)
+    print('place_and_group')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_person_about_problem('say_next_child')
---
+++ think_part/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand_and_first_part(str_arg):
+    look_part_from_eye(str_arg)
+    print('able_government')
+
+def look_part_from_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand_and_first_part('person_or_way')
