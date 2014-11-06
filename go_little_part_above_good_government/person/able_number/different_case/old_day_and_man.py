25d68afbe0bfd540953ac83c78fc821ddd9ea9d8
Jingkai Tang <jingkaitang@gmail.com>
2014-11-06 09:01:10 +0800

    add: take hand/part_or_man/look_problem_about_government.py and go_little_part_above_good_government/person/able_number/different_case/old_day_and_man.py into right year

---
+++ go_little_part_above_good_government/person/able_number/different_case/old_day_and_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    think_company_beneath_public_company(str_arg)
+    print('make_old_thing')
+
+def think_company_beneath_public_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('new_way')
---
+++ hand/part_or_man/look_problem_about_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    eye(str_arg)
+    print('day')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('company')
