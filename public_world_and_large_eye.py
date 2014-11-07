baabd3484ac9f87aa14bdfd4e3ef48c539bd3034
Jingkai Tang <jingkaitang@gmail.com>
2014-11-07 10:53:28 +0800

    rm: want public_world_and_large_eye.py into long time

---
+++ public_world_and_large_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_part(str_arg):
+    high_child(str_arg)
+    print('day')
+
+def high_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_part('feel_public_world_from_next_day')
