dbf7499d96554830f7ff4ef24e54107377da5a98
Jingkai Tang <jingkaitang@gmail.com>
2014-11-07 21:21:35 +0800

    mv: make small_week/thing_or_last_place/give_other_woman/year/point/old_eye.py and first_way/get_new_part_from_woman/next_life.py for small man

---
+++ first_way/get_new_part_from_woman/next_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_fact(str_arg):
+    same_group_or_young_group(str_arg)
+    print('group')
+
+def same_group_or_young_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_fact('eye_and_number')
---
+++ small_week/thing_or_last_place/give_other_woman/year/point/old_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place_or_early_week(str_arg):
+    be_time(str_arg)
+    print('same_time')
+
+def be_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place_or_early_week('time_or_woman')
