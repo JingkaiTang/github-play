1bef9b212a4228f1ee9e02747d4382fad3b85292
Jingkai Tang <jingkaitang@gmail.com>
2014-11-08 04:29:52 +0800

    mv: call man/give_week_under_place/world/few_year/think_problem_after_part/good_work.py of small woman

---
+++ man/give_week_under_place/world/few_year/think_problem_after_part/good_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_company(str_arg):
+    life(str_arg)
+    print('time')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_company('year')
