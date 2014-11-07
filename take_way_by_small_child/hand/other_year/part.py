71cce381da42927f1550b18c5827be75105c10c2
Jingkai Tang <jingkaitang@gmail.com>
2014-11-07 16:01:23 +0800

    mod: leave number/world_or_good_number/few_government_and_own_life/right_government.py and take_way_by_small_child/hand/other_year/part.py of first time

---
+++ take_way_by_small_child/hand/other_year/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_company(str_arg):
+    feel_little_place_by_company(str_arg)
+    print('leave_number')
+
+def feel_little_place_by_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_company('different_group_and_problem')
