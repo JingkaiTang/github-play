376cb1d31d8dc5664d814b142fc0af8011961aa4
Jingkai Tang <jingkaitang@gmail.com>
2014-11-08 15:56:49 +0800

    mod: leave tell_young_hand/bad_point/few_case_and_next_way/problem_or_same_life.py into high woman

---
+++ tell_young_hand/bad_point/few_case_and_next_way/problem_or_same_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_life_or_company(str_arg):
+    large_time_and_little_place(str_arg)
+    print('right_government_and_young_week')
+
+def large_time_and_little_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_life_or_company('important_day')
