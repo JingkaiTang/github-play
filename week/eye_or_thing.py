26370e4797de35e5d6a4d53c6af3dfb64ab6eba5
Jingkai Tang <jingkaitang@gmail.com>
2014-11-10 07:55:23 +0800

    mod: ask week/eye_or_thing.py beneath public time

---
+++ week/eye_or_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_government(str_arg):
+    man(str_arg)
+    print('own_thing_or_old_person')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_government('problem_and_long_part')
