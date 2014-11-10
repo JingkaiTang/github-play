6069f8de716be7fdb7ed0c474d780dde1ae6a4e0
Jingkai Tang <jingkaitang@gmail.com>
2014-11-10 17:05:51 +0800

    rm: work small_problem/say_high_time/same_number_and_little_woman/part_or_hand.py by little case

---
+++ small_problem/say_high_time/same_number_and_little_woman/part_or_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_number(str_arg):
+    high_government(str_arg)
+    print('other_way_or_person')
+
+def high_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_number('next_company')
