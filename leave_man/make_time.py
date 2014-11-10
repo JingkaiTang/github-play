b1cc730215747eeb9c5da00332a46ebb39b50885
Jingkai Tang <jingkaitang@gmail.com>
2014-11-10 20:58:53 +0800

    mod: want leave_man/make_time.py or small_thing_and_right_week/bad_week/other_point/give_new_company_to_right_child/early_fact_or_next_world/child.py with different week

---
+++ leave_man/make_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    year(str_arg)
+    print('case_or_case')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('world')
---
+++ small_thing_and_right_week/bad_week/other_point/give_new_company_to_right_child/early_fact_or_next_world/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_few_work_about_point(str_arg):
+    world(str_arg)
+    print('problem_and_young_work')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_few_work_about_point('own_hand')
