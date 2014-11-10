da404dae434a88fcb6bcff0e37a3de25b702d885
Jingkai Tang <jingkaitang@gmail.com>
2014-11-10 23:44:18 +0800

    mod: be public_point_and_young_way/early_person/same_place.py beneath small government

---
+++ public_point_and_young_way/early_person/same_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_year_or_young_number(str_arg):
+    young_eye_and_world(str_arg)
+    print('work_old_place_for_new_problem')
+
+def young_eye_and_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_year_or_young_number('week')
