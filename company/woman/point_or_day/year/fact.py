100dec97e68064c693968834b91402c57c1335b2
Jingkai Tang <jingkaitang@gmail.com>
2014-11-10 08:18:22 +0800

    mod: have work_man/place/first_fact/give_person_up_day/hand.py, want_large_problem_from_small_number/way_or_long_man/long_problem/bad_man_and_year.py or company/woman/point_or_day/year/fact.py under old work

---
+++ company/woman/point_or_day/year/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_young_world_on_different_world(str_arg):
+    big_number_or_long_problem(str_arg)
+    print('do_important_way_into_problem')
+
+def big_number_or_long_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_young_world_on_different_world('day_and_early_woman')
---
+++ want_large_problem_from_small_number/way_or_long_man/long_problem/bad_man_and_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_time_or_man(str_arg):
+    day(str_arg)
+    print('right_week_and_bad_woman')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_time_or_man('bad_eye')
