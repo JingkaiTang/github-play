56dde3c12cb4764386e5675480529223534b9555
Jingkai Tang <jingkaitang@gmail.com>
2014-11-11 14:24:19 +0800

    mod: have call_good_group_by_great_problem/public_hand/day/few_time_and_person.py from young woman

---
+++ call_good_group_by_great_problem/public_hand/day/few_time_and_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    same_life(str_arg)
+    print('group')
+
+def same_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('person_or_good_group')
