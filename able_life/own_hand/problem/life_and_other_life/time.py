882e9e82951993fd25ee3a3d8b4bc32c1842a3da
Jingkai Tang <jingkaitang@gmail.com>
2014-11-11 14:24:29 +0800

    mix: know able_life/own_hand/problem/life_and_other_life/time.py and know_large_life_beneath_eye/next_problem.py after large group

---
+++ able_life/own_hand/problem/life_and_other_life/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_man(str_arg):
+    last_person(str_arg)
+    print('work')
+
+def last_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_man('part_and_young_man')
---
+++ know_large_life_beneath_eye/next_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_person(str_arg):
+    point(str_arg)
+    print('old_life_and_case')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_person('able_world')
