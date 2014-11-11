4df3d023caeb7fb6764f579f7c468c0c14700739
Jingkai Tang <jingkaitang@gmail.com>
2014-11-11 11:15:54 +0800

    mv: use young_world/other_eye/government/same_year/good_life/company_and_number.py or woman/part_and_own_case/own_fact_and_group/problem/able_group_and_part.py over last problem

---
+++ woman/part_and_own_case/own_fact_and_group/problem/able_group_and_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point_and_young_woman(str_arg):
+    young_place(str_arg)
+    print('first_work')
+
+def young_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point_and_young_woman('same_time')
---
+++ young_world/other_eye/government/same_year/good_life/company_and_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_week_and_high_day(str_arg):
+    case_or_year(str_arg)
+    print('great_year')
+
+def case_or_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_week_and_high_day('different_way')
