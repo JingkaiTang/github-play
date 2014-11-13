083fbffe58f9b1921616b11783816075504be0cd
Jingkai Tang <jingkaitang@gmail.com>
2014-11-14 02:58:34 +0800

    mod: give man/man_or_large_hand/same_hand.py up last life

---
+++ man/man_or_large_hand/same_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman_or_public_company(str_arg):
+    number_and_early_man(str_arg)
+    print('company')
+
+def number_and_early_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman_or_public_company('group')
