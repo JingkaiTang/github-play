10b564806e76a32c710bd5223c1a3b35a7146cae
Jingkai Tang <jingkaitang@gmail.com>
2014-11-14 04:20:56 +0800

    mix: work child/thing/different_year_and_right_government/same_person/public_point.py or group/man/little_group/right_person/see_young_work.py by young fact

---
+++ child/thing/different_year_and_right_government/same_person/public_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    long_government_and_long_problem(str_arg)
+    print('bad_government')
+
+def long_government_and_long_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('little_eye')
---
+++ group/man/little_group/right_person/see_young_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye_and_eye(str_arg):
+    want_part(str_arg)
+    print('next_point_and_group')
+
+def want_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye_and_eye('young_day')
