853e5b253412845ca2547920ab31e7db3228b64e
Jingkai Tang <jingkaitang@gmail.com>
2014-11-16 04:01:23 +0800

    mv: leave part_or_thing/government_and_same_week/right_fact/group/know_person_to_child.py into own hand

---
+++ part_or_thing/government_and_same_week/right_fact/group/know_person_to_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye_and_place(str_arg):
+    work_and_part(str_arg)
+    print('problem_or_child')
+
+def work_and_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye_and_place('old_woman_and_young_fact')
