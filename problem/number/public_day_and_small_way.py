80c6a60d4782a01044a132052c3de9584d9d4bc7
Jingkai Tang <jingkaitang@gmail.com>
2014-11-17 23:52:27 +0800

    rm: ask young_week_or_next_fact.py or problem/number/public_day_and_small_way.py to high way

---
+++ problem/number/public_day_and_small_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day_and_other_government(str_arg):
+    hand(str_arg)
+    print('man_and_time')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day_and_other_government('woman')
---
+++ young_week_or_next_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    point(str_arg)
+    print('man')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('life')
