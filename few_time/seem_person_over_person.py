1dbd0bf4784cb55c19ddc0a4a8990b6fdc89f854
Jingkai Tang <jingkaitang@gmail.com>
2014-11-20 22:21:57 +0800

    mv: make few_time/seem_person_over_person.py and leave_number/young_way/last_week_or_man.py over different problem

---
+++ few_time/seem_person_over_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    same_life_and_able_point(str_arg)
+    print('same_government_or_thing')
+
+def same_life_and_able_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('next_day')
---
+++ leave_number/young_way/last_week_or_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_group(str_arg):
+    want_bad_woman(str_arg)
+    print('child')
+
+def want_bad_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_group('go_part_with_fact')
