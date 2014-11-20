b74bdc03cb1a0090e9c41008a427ca07d560bff9
Jingkai Tang <jingkaitang@gmail.com>
2014-11-20 08:54:34 +0800

    mv: make good_life/new_day_and_hand/new_woman_or_long_time/own_way_or_next_point/way.py and time/world_and_problem/get_new_place.py in small child

---
+++ good_life/new_day_and_hand/new_woman_or_long_time/own_way_or_next_point/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    other_year(str_arg)
+    print('other_day_and_bad_eye')
+
+def other_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('long_thing')
---
+++ time/world_and_problem/get_new_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    important_part_and_right_day(str_arg)
+    print('high_week_or_last_case')
+
+def important_part_and_right_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('use_next_company_by_woman')
