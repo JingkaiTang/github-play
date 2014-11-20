198c6174160fdb0126e866b84f021785605304b9
Jingkai Tang <jingkaitang@gmail.com>
2014-11-21 00:38:32 +0800

    mv: want case_and_fact/take_last_year/long_child/work_and_eye.py into new life

---
+++ case_and_fact/take_last_year/long_child/work_and_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_problem(str_arg):
+    say_group_by_case(str_arg)
+    print('thing')
+
+def say_group_by_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_problem('eye')
