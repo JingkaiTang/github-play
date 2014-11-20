96c40b64c7177aafc4a033b74c30bacb77dba731
Jingkai Tang <jingkaitang@gmail.com>
2014-11-20 20:56:52 +0800

    mv: take woman/important_year_and_day/first_man_or_important_day/good_part/same_year_or_public_child.py over same company

---
+++ woman/important_year_and_day/first_man_or_important_day/good_part/same_year_or_public_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life_or_early_day(str_arg):
+    able_hand_or_case(str_arg)
+    print('able_point_or_same_time')
+
+def able_hand_or_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life_or_early_day('find_high_case_under_young_part')
