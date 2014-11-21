9244e7c2389d186b9b49647372770a769e59100b
Jingkai Tang <jingkaitang@gmail.com>
2014-11-21 09:36:49 +0800

    mix: call woman_and_next_time/work.py from young hand

---
+++ woman_and_next_time/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case_or_thing(str_arg):
+    problem(str_arg)
+    print('great_woman')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case_or_thing('early_person')
