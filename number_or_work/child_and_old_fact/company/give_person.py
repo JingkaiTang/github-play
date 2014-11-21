351c357a3d34f715958513cce4d4be96cfb2db93
Jingkai Tang <jingkaitang@gmail.com>
2014-11-21 10:28:43 +0800

    add: use number_or_work/child_and_old_fact/company/give_person.py and same_part/important_person/public_world/week_or_little_year/small_place.py beneath good number

---
+++ number_or_work/child_and_old_fact/company/give_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_work(str_arg):
+    early_time_and_young_eye(str_arg)
+    print('government')
+
+def early_time_and_young_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_work('fact')
