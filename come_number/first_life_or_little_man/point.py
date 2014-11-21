473fd0412a1233c9cf3cfaab90a5dfaaa4812308
Jingkai Tang <jingkaitang@gmail.com>
2014-11-22 03:09:59 +0800

    mix: say come_number/first_life_or_little_man/point.py to right part

---
+++ come_number/first_life_or_little_man/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world_and_group(str_arg):
+    small_work(str_arg)
+    print('way')
+
+def small_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world_and_group('take_government_for_own_life')
