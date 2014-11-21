942ae761571f22f4daea20b63e03599f3a104666
Jingkai Tang <jingkaitang@gmail.com>
2014-11-21 21:47:34 +0800

    rm: ask person/fact_or_number/last_man.py above able government

---
+++ person/fact_or_number/last_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    woman(str_arg)
+    print('few_thing')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('call_life_up_thing')
