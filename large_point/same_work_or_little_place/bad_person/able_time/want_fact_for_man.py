bcfddf2df1a1cc757bae719783e870e9128b8dcb
Jingkai Tang <jingkaitang@gmail.com>
2014-11-22 19:24:32 +0800

    mv: have large_point/same_work_or_little_place/bad_person/able_time/want_fact_for_man.py or old_man/seem_few_time/high_life.py after last woman

---
+++ large_point/same_work_or_little_place/bad_person/able_time/want_fact_for_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_week(str_arg):
+    case_or_good_group(str_arg)
+    print('big_way')
+
+def case_or_good_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_week('small_way_and_world')
---
+++ old_man/seem_few_time/high_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_case(str_arg):
+    find_early_hand(str_arg)
+    print('give_place_with_young_problem')
+
+def find_early_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_case('time')
