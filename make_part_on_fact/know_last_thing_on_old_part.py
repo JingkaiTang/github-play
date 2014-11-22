09802bfe89b6c32285ef9959c72b51df1a7a5001
Jingkai Tang <jingkaitang@gmail.com>
2014-11-23 00:39:17 +0800

    add: look make_part_on_fact/know_last_thing_on_old_part.py after good life

---
+++ make_part_on_fact/know_last_thing_on_old_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    large_company_and_number(str_arg)
+    print('case_and_hand')
+
+def large_company_and_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('say_point')
