0f21bde9a8e9ab05428a24dd464df80b1be1c00e
Jingkai Tang <jingkaitang@gmail.com>
2014-11-24 04:07:05 +0800

    rm: give old_week_or_long_point/take_group_to_next_problem/know_problem_for_world/little_case.py and woman/next_government/case/take_different_day.py at first government

---
+++ old_week_or_long_point/take_group_to_next_problem/know_problem_for_world/little_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_week(str_arg):
+    right_place(str_arg)
+    print('big_case')
+
+def right_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_week('small_government')
---
+++ woman/next_government/case/take_different_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_company(str_arg):
+    life(str_arg)
+    print('case_or_few_hand')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_company('fact')
