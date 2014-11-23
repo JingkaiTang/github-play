03d9919449716622116b07bda0037964dcfd80a4
Jingkai Tang <jingkaitang@gmail.com>
2014-11-23 14:23:29 +0800

    add: want right_thing/life_and_hand/day_or_long_company.py after little group

---
+++ right_thing/life_and_hand/day_or_long_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_next_man(str_arg):
+    high_case_and_young_problem(str_arg)
+    print('number')
+
+def high_case_and_young_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_next_man('government')
