c3eab83ff3416bd71dd8c0e30337918f7c2a592d
Jingkai Tang <jingkaitang@gmail.com>
2014-11-23 19:54:43 +0800

    mix: have group/think_early_group/company/man.py up young life

---
+++ group/think_early_group/company/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_world(str_arg):
+    way_or_new_fact(str_arg)
+    print('few_hand')
+
+def way_or_new_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_world('high_government_and_year')
