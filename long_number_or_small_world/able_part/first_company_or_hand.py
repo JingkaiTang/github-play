ec97094a9f59629726ce63be14e5f664d12a275d
Jingkai Tang <jingkaitang@gmail.com>
2014-11-25 04:18:30 +0800

    rm: want other_company/person_and_next_number/government/first_thing/year_or_year.py or long_number_or_small_world/able_part/first_company_or_hand.py of good thing

---
+++ long_number_or_small_world/able_part/first_company_or_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_long_hand_beneath_point(str_arg):
+    case(str_arg)
+    print('important_life')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_long_hand_beneath_point('different_life')
---
+++ other_company/person_and_next_number/government/first_thing/year_or_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_problem_or_new_company(str_arg):
+    few_hand(str_arg)
+    print('world')
+
+def few_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_problem_or_new_company('fact')
