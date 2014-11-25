dd5115a064c61b6f6bd56096729e93358cd32321
Jingkai Tang <jingkaitang@gmail.com>
2014-11-25 22:57:47 +0800

    add: make number/go_case.py over next fact

---
+++ number/go_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_hand(str_arg):
+    thing(str_arg)
+    print('group_and_new_part')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_hand('look_time')
