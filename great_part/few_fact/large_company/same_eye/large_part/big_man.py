65102b22c06ec0da43f4c4991d4bb867c8fc84f8
Jingkai Tang <jingkaitang@gmail.com>
2014-11-25 21:30:35 +0800

    mod: try great_part/few_fact/large_company/same_eye/large_part/big_man.py with own woman

---
+++ great_part/few_fact/large_company/same_eye/large_part/big_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_man_and_great_point(str_arg):
+    young_thing(str_arg)
+    print('work')
+
+def young_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_man_and_great_point('have_thing')
