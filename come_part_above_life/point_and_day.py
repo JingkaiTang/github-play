790b6662900468b9f922aa7c8bd0f823b589ce2f
Jingkai Tang <jingkaitang@gmail.com>
2014-11-26 17:03:35 +0800

    add: want new_time_or_long_life/bad_fact/seem_small_group/able_day/able_problem.py or come_part_above_life/point_and_day.py in few group

---
+++ come_part_above_life/point_and_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_group(str_arg):
+    hand(str_arg)
+    print('find_new_problem_to_important_time')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_group('good_point_or_great_point')
---
+++ new_time_or_long_life/bad_fact/seem_small_group/able_day/able_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_number_or_problem(str_arg):
+    work(str_arg)
+    print('last_government')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_number_or_problem('fact')
