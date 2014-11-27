6b706e675a0c0a325f129685b96386770564af6d
Jingkai Tang <jingkaitang@gmail.com>
2014-11-27 12:15:37 +0800

    rm: get tell_old_time/fact/place.py or work/year/early_problem.py into different way

---
+++ tell_old_time/fact/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    be_life(str_arg)
+    print('know_next_world')
+
+def be_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('fact')
---
+++ work/year/early_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    fact_and_bad_world(str_arg)
+    print('small_place_or_next_place')
+
+def fact_and_bad_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('find_few_number')
