6bea0d20fe0b3f6858a689d1f2061622b1ebc149
Jingkai Tang <jingkaitang@gmail.com>
2014-11-27 15:57:33 +0800

    mod: work thing_and_time/high_number/high_time/see_great_case/bad_day/know_child.py or public_way_and_thing/thing/try_own_hand_on_public_group.py from same time

---
+++ public_way_and_thing/thing/try_own_hand_on_public_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_life_or_government(str_arg):
+    life_and_work(str_arg)
+    print('time')
+
+def life_and_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_life_or_government('right_point_or_man')
---
+++ thing_and_time/high_number/high_time/see_great_case/bad_day/know_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_new_company(str_arg):
+    own_work(str_arg)
+    print('young_man_or_first_world')
+
+def own_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_new_company('think_part')
