51c3e034ca01694b5a9ba77eecf2a7766ac66ded
Jingkai Tang <jingkaitang@gmail.com>
2014-11-29 05:57:50 +0800

    add: use week/great_child.py to great group

---
+++ week/great_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_point(str_arg):
+    little_number_and_important_point(str_arg)
+    print('work_and_little_number')
+
+def little_number_and_important_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_point('try_day_over_last_child')
