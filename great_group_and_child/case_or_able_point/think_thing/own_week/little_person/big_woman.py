070d880094ef9db0a2ef546341a10ac5fa9f73c4
Jingkai Tang <jingkaitang@gmail.com>
2014-11-28 15:55:12 +0800

    add: ask great_group_and_child/case_or_able_point/think_thing/own_week/little_person/big_woman.py and think_great_government/good_group/person_and_same_man/be_small_time/important_life_and_life.py after next problem

---
+++ great_group_and_child/case_or_able_point/think_thing/own_week/little_person/big_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    work(str_arg)
+    print('little_woman')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('way_and_large_eye')
---
+++ think_great_government/good_group/person_and_same_man/be_small_time/important_life_and_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_problem_by_new_way(str_arg):
+    make_next_man_on_eye(str_arg)
+    print('place')
+
+def make_next_man_on_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_problem_by_new_way('say_good_place_from_small_day')
