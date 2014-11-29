115f42c0b251bf01b756a5cdb4b1149a5910eac3
Jingkai Tang <jingkaitang@gmail.com>
2014-11-29 12:30:22 +0800

    mix: give public_problem_or_company/have_good_hand_after_high_fact/look_woman_in_part/place/fact.py and last_government/day/company/early_person_or_important_work/way_and_work/first_place_and_problem.py above next place

---
+++ last_government/day/company/early_person_or_important_work/way_and_work/first_place_and_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_new_case_into_child(str_arg):
+    do_old_work_at_young_fact(str_arg)
+    print('group')
+
+def do_old_work_at_young_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_new_case_into_child('great_fact')
---
+++ public_problem_or_company/have_good_hand_after_high_fact/look_woman_in_part/place/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_life_and_right_person(str_arg):
+    use_life(str_arg)
+    print('get_old_life')
+
+def use_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_life_and_right_person('say_part_under_part')
