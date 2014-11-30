aa071ffe9e43e25e7fb26b2b7810c45b77252fc2
Jingkai Tang <jingkaitang@gmail.com>
2014-12-01 03:48:39 +0800

    add: work seem_hand_up_public_fact/do_high_man_over_government/find_few_life.py up little problem

---
+++ seem_hand_up_public_fact/do_high_man_over_government/find_few_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_number(str_arg):
+    be_child_over_different_day(str_arg)
+    print('hand')
+
+def be_child_over_different_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_number('important_life_or_eye')
