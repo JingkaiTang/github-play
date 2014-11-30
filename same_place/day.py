f9fafe322506404bf3859bcf65cff347180a71e0
Jingkai Tang <jingkaitang@gmail.com>
2014-12-01 03:46:20 +0800

    mix: look same_place/day.py in other place

---
+++ same_place/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_government(str_arg):
+    little_year(str_arg)
+    print('person')
+
+def little_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_government('great_woman_and_group')
