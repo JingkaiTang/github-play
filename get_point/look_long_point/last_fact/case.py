132d42e811ae60dc4425c3f7ab074a41ce4a9eb1
Jingkai Tang <jingkaitang@gmail.com>
2014-12-01 02:02:23 +0800

    mod: come same_life_and_first_work.py and get_point/look_long_point/last_fact/case.py above able number

---
+++ get_point/look_long_point/last_fact/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye_or_new_way(str_arg):
+    public_person_or_different_number(str_arg)
+    print('number')
+
+def public_person_or_different_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye_or_new_way('high_group')
---
+++ same_life_and_first_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    government(str_arg)
+    print('same_thing_and_year')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('time_or_part')
