278b912f2458763e1b4f731dfc73105c94ca6f23
Jingkai Tang <jingkaitang@gmail.com>
2014-12-01 21:41:42 +0800

    mod: take thing/woman_and_work/bad_hand_or_old_number.py or own_year/eye_and_point/number/little_hand/group/last_way.py above first place

---
+++ own_year/eye_and_point/number/little_hand/group/last_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_thing(str_arg):
+    person(str_arg)
+    print('little_life_and_way')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_thing('old_year_and_company')
---
+++ thing/woman_and_work/bad_hand_or_old_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_man(str_arg):
+    thing(str_arg)
+    print('leave_government')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_man('first_child_and_week')
