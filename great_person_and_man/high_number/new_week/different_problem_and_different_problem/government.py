25561052bf7eaf9bc85b867cef96b5d3679923bb
Jingkai Tang <jingkaitang@gmail.com>
2014-12-02 12:40:37 +0800

    mv: call great_person_and_man/high_number/new_week/different_problem_and_different_problem/government.py, point_or_same_day.py or eye/able_hand.py in high point

---
+++ great_person_and_man/high_number/new_week/different_problem_and_different_problem/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    old_case_or_man(str_arg)
+    print('part')
+
+def old_case_or_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('week')
---
+++ point_or_same_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day_or_person(str_arg):
+    last_week(str_arg)
+    print('take_other_point_with_part')
+
+def last_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day_or_person('little_world')
