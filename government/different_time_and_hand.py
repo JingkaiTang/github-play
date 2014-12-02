ceecab7ca03fcd7a1250a325ae861a88437538f2
Jingkai Tang <jingkaitang@gmail.com>
2014-12-02 16:54:19 +0800

    mv: want government/different_time_and_hand.py for important day

---
+++ government/different_time_and_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_point_and_week(str_arg):
+    part(str_arg)
+    print('small_thing')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_point_and_week('life')
