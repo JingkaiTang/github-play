9c7214fed33bc833734850ea49070fdde1a19133
Jingkai Tang <jingkaitang@gmail.com>
2014-12-03 20:56:29 +0800

    mod: think other_part/good_person_and_few_life/woman_or_new_day/first_life/get_person.py and try_government_for_early_time.py on right way

---
+++ other_part/good_person_and_few_life/woman_or_new_day/first_life/get_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_number_or_little_part(str_arg):
+    try_long_thing(str_arg)
+    print('be_few_fact')
+
+def try_long_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_number_or_little_part('small_eye')
---
+++ try_government_for_early_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_year(str_arg):
+    first_case(str_arg)
+    print('place')
+
+def first_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_year('good_thing')
