d62ddaf43fbf6a9b923db4d6b2b314a2027e1ad4
Jingkai Tang <jingkaitang@gmail.com>
2014-12-05 06:07:42 +0800

    mix: tell little_work.py on other number

---
+++ little_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_place_and_next_time(str_arg):
+    way(str_arg)
+    print('able_life_and_child')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_place_and_next_time('call_bad_fact')
