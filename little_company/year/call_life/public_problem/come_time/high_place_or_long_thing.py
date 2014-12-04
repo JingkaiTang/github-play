5b92a0ad1dbf5a48773105bb0bfa61b17d7cbbd6
Jingkai Tang <jingkaitang@gmail.com>
2014-12-04 10:46:59 +0800

    mv: take see_week/important_place.py, get_old_fact/high_number/bad_number.py and little_company/year/call_life/public_problem/come_time/high_place_or_long_thing.py beneath long year

---
+++ little_company/year/call_life/public_problem/come_time/high_place_or_long_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    look_own_life(str_arg)
+    print('person_or_year')
+
+def look_own_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('few_fact_or_week')
---
+++ see_week/important_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_fact(str_arg):
+    life_or_same_year(str_arg)
+    print('first_woman')
+
+def life_or_same_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_fact('little_week')
