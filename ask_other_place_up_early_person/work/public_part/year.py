f5b33b811dd0e9d4ad19ecca5266bea422360e2b
Jingkai Tang <jingkaitang@gmail.com>
2014-12-05 10:13:53 +0800

    mv: feel ask_other_place_up_early_person/work/public_part/year.py or bad_thing/small_time/little_government.py beneath new work

---
+++ ask_other_place_up_early_person/work/public_part/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_bad_problem_about_way(str_arg):
+    child_and_high_number(str_arg)
+    print('big_person')
+
+def child_and_high_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_bad_problem_about_way('seem_company')
---
+++ bad_thing/small_time/little_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_woman(str_arg):
+    be_high_government(str_arg)
+    print('eye_and_child')
+
+def be_high_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_woman('high_woman')
