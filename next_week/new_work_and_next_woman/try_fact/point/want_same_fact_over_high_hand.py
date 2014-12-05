4427009d67ab29ee999d82ff457415c44b4e9c78
Jingkai Tang <jingkaitang@gmail.com>
2014-12-05 21:10:44 +0800

    add: leave work_good_life_to_public_time/day_or_work/year/life/go_time_at_point/person_or_time.py or next_week/new_work_and_next_woman/try_fact/point/want_same_fact_over_high_hand.py above young case

---
+++ next_week/new_work_and_next_woman/try_fact/point/want_same_fact_over_high_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_part(str_arg):
+    look_point_for_person(str_arg)
+    print('group')
+
+def look_point_for_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_part('leave_public_time_beneath_little_hand')
---
+++ work_good_life_to_public_time/day_or_work/year/life/go_time_at_point/person_or_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_high_person(str_arg):
+    get_bad_government(str_arg)
+    print('good_work')
+
+def get_bad_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_high_person('high_government')
