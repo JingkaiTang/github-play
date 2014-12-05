264e3b7cf295028038f690cdee5cfb4e89567d41
Jingkai Tang <jingkaitang@gmail.com>
2014-12-05 11:01:54 +0800

    add: get big_woman/fact/small_time/thing.py above young thing

---
+++ big_woman/fact/small_time/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    case(str_arg)
+    print('be_place')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('bad_way')
