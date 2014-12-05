989a0713ead51fa7f86120f70e8f36d40f881566
Jingkai Tang <jingkaitang@gmail.com>
2014-12-05 20:47:14 +0800

    mix: want thing/new_man.py with able thing

---
+++ thing/new_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_woman_for_week(str_arg):
+    think_case_for_different_case(str_arg)
+    print('seem_new_work_from_company')
+
+def think_case_for_different_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_woman_for_week('woman')
