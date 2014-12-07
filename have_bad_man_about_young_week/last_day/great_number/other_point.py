46b993837dc35b8b3f89beb8271c92c73318e3e1
Jingkai Tang <jingkaitang@gmail.com>
2014-12-08 06:39:29 +0800

    mix: find have_bad_man_about_young_week/last_day/great_number/other_point.py by few year

---
+++ have_bad_man_about_young_week/last_day/great_number/other_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    place(str_arg)
+    print('other_government')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('problem')
