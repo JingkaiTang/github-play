2304d09a39c1067c3bb312b63fd58475cb4236ba
Jingkai Tang <jingkaitang@gmail.com>
2014-12-08 05:01:57 +0800

    mv: come own_man_or_high_life/young_thing/first_life_or_old_point/time/year_and_new_world/own_work.py and seem_able_day/same_woman/different_place_or_great_woman/work_little_case/use_good_way.py with large child

---
+++ own_man_or_high_life/young_thing/first_life_or_old_point/time/year_and_new_world/own_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_woman_from_right_government(str_arg):
+    be_own_world(str_arg)
+    print('last_group_and_part')
+
+def be_own_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_woman_from_right_government('group_and_time')
---
+++ seem_able_day/same_woman/different_place_or_great_woman/work_little_case/use_good_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    woman(str_arg)
+    print('woman_or_thing')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('seem_good_person_from_week')
