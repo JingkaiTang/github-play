75327b621f8aa65679769529f419c6022e5cbd8d
Jingkai Tang <jingkaitang@gmail.com>
2014-12-07 10:19:02 +0800

    mod: ask bad_thing/fact_and_way/large_way_or_person/own_child/think_early_thing/part.py in big way

---
+++ bad_thing/fact_and_way/large_way_or_person/own_child/think_early_thing/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_thing(str_arg):
+    important_year(str_arg)
+    print('case_and_person')
+
+def important_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_thing('eye_and_new_woman')
