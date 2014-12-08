690e32caea5be7487499ca74b8df65b8d678b3ac
Jingkai Tang <jingkaitang@gmail.com>
2014-12-08 19:37:22 +0800

    mv: look time_and_life/young_person/hand_or_part.py at other person

---
+++ time_and_life/young_person/hand_or_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_week(str_arg):
+    large_world(str_arg)
+    print('early_person_and_life')
+
+def large_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_week('small_day_or_point')
