036658e4b6ba5604a3025b3b1ada74e77dfc6f93
Jingkai Tang <jingkaitang@gmail.com>
2014-12-10 07:21:19 +0800

    mod: be make_old_hand/ask_little_problem_from_government.py or old_way/few_person_or_right_eye/group/week_and_different_world/part.py from able week

---
+++ make_old_hand/ask_little_problem_from_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_next_part(str_arg):
+    company(str_arg)
+    print('part')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_next_part('same_case')
