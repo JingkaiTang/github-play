86e87b5080812de958be4ff644fda25190cec882
Jingkai Tang <jingkaitang@gmail.com>
2014-12-10 18:41:03 +0800

    mix: get high_child_or_good_year/bad_number/time/big_week_or_bad_person/part.py and different_day/work_case_under_other_child/case/come_hand_by_important_man.py above public time

---
+++ different_day/work_case_under_other_child/case/come_hand_by_important_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place_or_own_child(str_arg):
+    long_part_and_little_number(str_arg)
+    print('get_few_person')
+
+def long_part_and_little_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place_or_own_child('use_number')
---
+++ high_child_or_good_year/bad_number/time/big_week_or_bad_person/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_time_and_case(str_arg):
+    place(str_arg)
+    print('life_and_group')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_time_and_case('eye_or_young_fact')
