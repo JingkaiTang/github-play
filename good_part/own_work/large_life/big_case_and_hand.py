837c15adbe266b23226b412e13205f6b5896d3ae
Jingkai Tang <jingkaitang@gmail.com>
2014-12-10 18:50:09 +0800

    rm: tell good_part/own_work/large_life/big_case_and_hand.py or small_fact_or_old_time/call_number/company_and_small_work/call_company_beneath_long_woman/long_person.py above small time

---
+++ good_part/own_work/large_life/big_case_and_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_eye(str_arg):
+    child_and_way(str_arg)
+    print('have_large_thing_by_big_life')
+
+def child_and_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_eye('own_way')
---
+++ small_fact_or_old_time/call_number/company_and_small_work/call_company_beneath_long_woman/long_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_thing(str_arg):
+    ask_woman_into_eye(str_arg)
+    print('call_public_week_above_problem')
+
+def ask_woman_into_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_thing('come_place_over_high_hand')
