3a8e314bb8cafead5789859393c7e8a76b6db230
Jingkai Tang <jingkaitang@gmail.com>
2014-12-12 05:39:53 +0800

    rm: know problem/woman_or_year/able_way/work/little_government_or_good_part/time.py with good week

---
+++ problem/woman_or_year/able_way/work/little_government_or_good_part/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    same_eye(str_arg)
+    print('bad_person')
+
+def same_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('problem')
