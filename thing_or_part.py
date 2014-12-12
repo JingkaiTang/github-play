04eeeffd5b781e9a22cc7b0a4e0a98e247c471ec
Jingkai Tang <jingkaitang@gmail.com>
2014-12-12 16:14:10 +0800

    mv: know thing_or_part.py after last way

---
+++ thing_or_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_government(str_arg):
+    early_point(str_arg)
+    print('thing')
+
+def early_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_government('number')
