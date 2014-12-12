0f74a2dfa806a9d1cf94a6b5609a4f50950e1fca
Jingkai Tang <jingkaitang@gmail.com>
2014-12-12 23:43:23 +0800

    add: go company.py in little group

--- company.py
+++ company.py
@@ -1,12 +1,12 @@
 
 #! /usr/bin/env python
 
-def new_group_or_great_man(str_arg):
-    have_young_person_above_problem(str_arg)
-    print('big_place_and_company')
+def high_company(str_arg):
+    man(str_arg)
+    print('good_year')
 
-def have_young_person_above_problem(str_arg):
+def man(str_arg):
     print(str_arg)
 
 if __name__ == '__main__':
-    new_group_or_great_man('think_right_woman')
+    high_company('tell_right_eye_of_right_point')
