17ced24a7ad1f9ee5ab3a58e4b761837257b48f6
Jingkai Tang <jingkaitang@gmail.com>
2014-12-12 08:54:00 +0800

    add: ask place/have_fact_in_next_hand/last_point_and_time/problem/different_government_or_work/hand.py or get_week_in_way/work_great_company_with_other_time/person.py over old thing

---
+++ get_week_in_way/work_great_company_with_other_time/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number_and_point(str_arg):
+    child(str_arg)
+    print('high_life_and_little_day')
+
+def child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number_and_point('way_or_big_work')
---
+++ place/have_fact_in_next_hand/last_point_and_time/problem/different_government_or_work/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_work(str_arg):
+    leave_public_eye(str_arg)
+    print('person_or_old_woman')
+
+def leave_public_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_work('child_or_company')
