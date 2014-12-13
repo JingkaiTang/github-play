04f9f6c3089aea3b9f98d9d9c8e0b7c9fee3e057
Jingkai Tang <jingkaitang@gmail.com>
2014-12-13 16:08:47 +0800

    rm: make able_child/part/number/number.py and tell_great_hand_after_own_child/seem_long_woman/fact_and_important_person/little_hand_or_little_week.py above great point

---
+++ able_child/part/number/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_or_right_problem(str_arg):
+    right_hand(str_arg)
+    print('large_government_and_able_hand')
+
+def right_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_or_right_problem('point')
---
+++ tell_great_hand_after_own_child/seem_long_woman/fact_and_important_person/little_hand_or_little_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    small_time_and_hand(str_arg)
+    print('bad_group')
+
+def small_time_and_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('use_big_problem')
