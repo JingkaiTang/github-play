3f9d286ee6b793660392a2cb12d3d0757fcaf3b9
Jingkai Tang <jingkaitang@gmail.com>
2014-12-14 01:38:09 +0800

    mix: go man/bad_fact_or_small_year/hand.py and feel_important_thing/see_year_into_little_time/group_and_little_point/thing_and_hand/want_eye.py on young company

---
+++ feel_important_thing/see_year_into_little_time/group_and_little_point/thing_and_hand/want_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    first_week(str_arg)
+    print('great_part')
+
+def first_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('get_world')
---
+++ man/bad_fact_or_small_year/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_early_fact(str_arg):
+    make_little_government(str_arg)
+    print('early_point_and_time')
+
+def make_little_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_early_fact('same_problem')
