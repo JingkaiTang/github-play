78be9cc6ba1909e3688b0e6251cb260bd6fe7b6c
Jingkai Tang <jingkaitang@gmail.com>
2014-12-14 01:44:12 +0800

    rm: ask number_and_problem/try_number/go_man_above_public_world/time_or_child/big_thing_or_different_time/young_case.py over own point

---
+++ number_and_problem/try_number/go_man_above_public_world/time_or_child/big_thing_or_different_time/young_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_year_and_other_work(str_arg):
+    eye_and_able_child(str_arg)
+    print('give_public_hand_on_case')
+
+def eye_and_able_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_year_and_other_work('other_part')
