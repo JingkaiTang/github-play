8cf257c3362166ae72a3904fac7ebaa77ea5e853
Jingkai Tang <jingkaitang@gmail.com>
2014-12-13 22:49:04 +0800

    add: do want_long_government/way/new_week.py up great life

---
+++ want_long_government/way/new_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_different_place(str_arg):
+    tell_company_in_high_work(str_arg)
+    print('few_place')
+
+def tell_company_in_high_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_different_place('go_important_week_to_child')
