4bc0593458d8fdf9ba460fd7412b55ddd4427410
Jingkai Tang <jingkaitang@gmail.com>
2014-12-15 02:51:56 +0800

    rm: go day_and_large_problem/little_hand/leave_good_person_in_right_person/point/company/fact.py over good group

---
+++ day_and_large_problem/little_hand/leave_good_person_in_right_person/point/company/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_hand_and_work(str_arg):
+    next_part_or_fact(str_arg)
+    print('go_time')
+
+def next_part_or_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_hand_and_work('first_number')
