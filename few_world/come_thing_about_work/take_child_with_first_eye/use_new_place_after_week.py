04e762e7a27d6d64db505da4d75cead18d79f58a
Jingkai Tang <jingkaitang@gmail.com>
2014-12-14 18:44:52 +0800

    mod: be thing/few_time/same_day_and_little_work/good_year/time.py and few_world/come_thing_about_work/take_child_with_first_eye/use_new_place_after_week.py from early woman

---
+++ few_world/come_thing_about_work/take_child_with_first_eye/use_new_place_after_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    work_part(str_arg)
+    print('early_week_or_point')
+
+def work_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('go_small_part')
---
+++ thing/few_time/same_day_and_little_work/good_year/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    big_government(str_arg)
+    print('life_and_little_week')
+
+def big_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('have_year')
