d3137c0c7bcc4394e7237cc72e15b0c212677e93
Jingkai Tang <jingkaitang@gmail.com>
2014-12-15 01:02:34 +0800

    mix: look see_fact.py of little hand

---
+++ see_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_government(str_arg):
+    have_last_world_into_last_problem(str_arg)
+    print('early_time')
+
+def have_last_world_into_last_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_government('last_part')
