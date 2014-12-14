5d86fc994e19391d20123fc56a3477fcf4d6f02f
Jingkai Tang <jingkaitang@gmail.com>
2014-12-14 09:14:23 +0800

    mix: think fact/see_work/small_point/week/know_same_part/next_point.py over right hand

---
+++ fact/see_work/small_point/week/know_same_part/next_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_man_beneath_point(str_arg):
+    great_world(str_arg)
+    print('woman')
+
+def great_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_man_beneath_point('same_week_and_great_government')
