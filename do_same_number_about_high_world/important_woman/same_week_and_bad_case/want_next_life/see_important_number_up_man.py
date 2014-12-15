bb3bcad8eb971e1f351527c9a1ffec877d70fd7e
Jingkai Tang <jingkaitang@gmail.com>
2014-12-15 17:52:50 +0800

    mix: work do_same_number_about_high_world/important_woman/same_week_and_bad_case/want_next_life/see_important_number_up_man.py and group/government.py with good place

---
+++ do_same_number_about_high_world/important_woman/same_week_and_bad_case/want_next_life/see_important_number_up_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    think_thing_under_company(str_arg)
+    print('public_problem_or_different_work')
+
+def think_thing_under_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('big_woman_and_work')
---
+++ group/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_year(str_arg):
+    other_woman(str_arg)
+    print('bad_problem')
+
+def other_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_year('feel_other_company')
