a263bbb426c9cc8a2e58fec015cb0cf8711bac35
Jingkai Tang <jingkaitang@gmail.com>
2014-12-15 15:50:42 +0800

    mix: make same_week/be_long_point/public_day_or_child.py or time_and_problem/ask_person_from_same_point/young_week/child.py above important group

---
+++ same_week/be_long_point/public_day_or_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_work_or_old_life(str_arg):
+    public_day(str_arg)
+    print('other_number')
+
+def public_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_work_or_old_life('come_large_work')
---
+++ time_and_problem/ask_person_from_same_point/young_week/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man_or_part(str_arg):
+    number(str_arg)
+    print('own_part_and_next_person')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man_or_part('good_place_and_few_thing')
