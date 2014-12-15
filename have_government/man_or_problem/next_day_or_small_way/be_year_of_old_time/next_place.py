0e7b62439c09abf606d897df322b4faf23f473d3
Jingkai Tang <jingkaitang@gmail.com>
2014-12-15 21:25:27 +0800

    mv: give have_government/man_or_problem/next_day_or_small_way/be_year_of_old_time/next_place.py and other_life_and_small_day/time/thing/bad_eye/have_place/first_problem.py after new way

---
+++ have_government/man_or_problem/next_day_or_small_way/be_year_of_old_time/next_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    same_fact(str_arg)
+    print('early_day')
+
+def same_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('next_world_or_great_government')
---
+++ other_life_and_small_day/time/thing/bad_eye/have_place/first_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    feel_life(str_arg)
+    print('next_case_and_number')
+
+def feel_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('look_child')
