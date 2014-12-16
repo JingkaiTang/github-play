a4bfda836eaa1a4f45c4fd0645eef16da8999747
Jingkai Tang <jingkaitang@gmail.com>
2014-12-17 02:29:52 +0800

    rm: want week/new_work/number/last_case/high_person/life.py or same_person/call_life_after_same_number/make_day/place/tell_small_hand_about_old_way.py from old place

---
+++ same_person/call_life_after_same_number/make_day/place/tell_small_hand_about_old_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company_and_world(str_arg):
+    man(str_arg)
+    print('right_government')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company_and_world('big_thing_or_part')
---
+++ week/new_work/number/last_case/high_person/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_same_point_by_year(str_arg):
+    ask_group(str_arg)
+    print('day_and_other_thing')
+
+def ask_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_same_point_by_year('point')
