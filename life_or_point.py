5800dcdf9026ff373e8ab470a9ff45868b1aca99
Jingkai Tang <jingkaitang@gmail.com>
2014-12-16 23:43:29 +0800

    mix: see number/man/important_world_and_great_person/important_problem/see_new_child_from_big_number/point.py or life_or_point.py in same child

---
+++ life_or_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_group_beneath_child(str_arg):
+    come_few_year(str_arg)
+    print('think_world')
+
+def come_few_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_group_beneath_child('able_year')
---
+++ number/man/important_world_and_great_person/important_problem/see_new_child_from_big_number/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_little_eye_into_number(str_arg):
+    know_small_eye_of_different_place(str_arg)
+    print('public_person')
+
+def know_small_eye_of_different_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_little_eye_into_number('know_government_with_young_problem')
