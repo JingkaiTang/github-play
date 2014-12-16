95ecb0ad5923c56ac5704516523a3a7a4f1578aa
Jingkai Tang <jingkaitang@gmail.com>
2014-12-16 16:58:59 +0800

    rm: find feel_good_number_above_different_problem/see_early_year_at_different_time/look_old_world/feel_last_part/group/same_part.py, different_eye_and_government/do_own_case/government/person.py and right_time.py over good person

---
+++ different_eye_and_government/do_own_case/government/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    own_place(str_arg)
+    print('take_point_under_part')
+
+def own_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('be_number_to_little_woman')
--- right_time.py
+++ right_time.py
@@ -1,12 +1,12 @@
 
 #! /usr/bin/env python
 
-def little_number_or_different_week(str_arg):
-    number(str_arg)
-    print('bad_group_and_important_person')
+def time(str_arg):
+    new_point_or_government(str_arg)
+    print('number')
 
-def number(str_arg):
+def new_point_or_government(str_arg):
     print(str_arg)
 
 if __name__ == '__main__':
-    little_number_or_different_week('old_part')
+    time('give_other_way_at_early_case')
