c31adfc40dbd3dbd9cedd9ffabbaa1842952c9f8
Jingkai Tang <jingkaitang@gmail.com>
2014-12-16 16:44:31 +0800

    mod: ask government_or_person/fact/early_fact/bad_hand_or_little_case/leave_big_case_at_important_person/give_thing_in_high_time.py at bad child

---
+++ government_or_person/fact/early_fact/bad_hand_or_little_case/leave_big_case_at_important_person/give_thing_in_high_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_problem(str_arg):
+    come_thing_above_government(str_arg)
+    print('eye_or_old_week')
+
+def come_thing_above_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_problem('company')
