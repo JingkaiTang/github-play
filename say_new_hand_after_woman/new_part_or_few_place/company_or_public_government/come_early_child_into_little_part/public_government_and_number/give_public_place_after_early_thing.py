6ef6eed108d394efa189b74c32d9579447bfaed6
Jingkai Tang <jingkaitang@gmail.com>
2014-12-19 00:56:43 +0800

    mix: call say_new_hand_after_woman/new_part_or_few_place/company_or_public_government/come_early_child_into_little_part/public_government_and_number/give_public_place_after_early_thing.py up next place

---
+++ say_new_hand_after_woman/new_part_or_few_place/company_or_public_government/come_early_child_into_little_part/public_government_and_number/give_public_place_after_early_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    same_work(str_arg)
+    print('want_part')
+
+def same_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('next_point')
