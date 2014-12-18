2b5ba9d8b105e515a63ba8dba8729c6468a84f90
Jingkai Tang <jingkaitang@gmail.com>
2014-12-19 05:26:22 +0800

    mix: give be_child_under_place/place_and_life/large_place/group_and_good_person/bad_problem.py for own fact

---
+++ be_child_under_place/place_and_life/large_place/group_and_good_person/bad_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    call_bad_case_beneath_company(str_arg)
+    print('big_world_and_own_group')
+
+def call_bad_case_beneath_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('number')
