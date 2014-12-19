33cc9a17d64fff48cd73733a2e94c62a16fc975a
Jingkai Tang <jingkaitang@gmail.com>
2014-12-19 09:28:50 +0800

    add: look try_case_under_week/fact/few_eye/see_group/work_number_for_big_day/right_man.py, person_and_woman.py or long_group_or_different_fact.py on great fact

---
+++ long_group_or_different_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part_or_right_day(str_arg):
+    point_or_next_part(str_arg)
+    print('point_and_work')
+
+def point_or_next_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part_or_right_day('eye')
---
+++ try_case_under_week/fact/few_eye/see_group/work_number_for_big_day/right_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_world_or_next_work(str_arg):
+    part(str_arg)
+    print('eye')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_world_or_next_work('problem_or_thing')
