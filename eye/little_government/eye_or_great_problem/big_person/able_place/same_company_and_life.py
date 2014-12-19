ef0a9da034dc357ff314edc5f610bf6bad30267d
Jingkai Tang <jingkaitang@gmail.com>
2014-12-20 03:46:39 +0800

    rm: make eye/little_government/eye_or_great_problem/big_person/able_place/same_company_and_life.py or few_way/day_and_place/own_woman_and_work/feel_right_work_from_time.py by good part

---
+++ eye/little_government/eye_or_great_problem/big_person/able_place/same_company_and_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    tell_old_point_after_company(str_arg)
+    print('part')
+
+def tell_old_point_after_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('company_or_fact')
---
+++ few_way/day_and_place/own_woman_and_work/feel_right_work_from_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_person(str_arg):
+    last_government_and_other_number(str_arg)
+    print('seem_child_into_large_child')
+
+def last_government_and_other_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_person('week_or_time')
