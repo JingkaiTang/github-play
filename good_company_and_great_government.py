1235a7dc4f98fe54e1207e4657fa1ab6f748892a
Jingkai Tang <jingkaitang@gmail.com>
2014-12-20 01:28:26 +0800

    mix: think good_point/thing_or_year/look_public_year/same_number/fact_and_important_government/be_new_company.py and good_company_and_great_government.py on great year

---
+++ good_company_and_great_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_eye_and_good_child(str_arg):
+    company(str_arg)
+    print('case')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_eye_and_good_child('life_or_thing')
---
+++ good_point/thing_or_year/look_public_year/same_number/fact_and_important_government/be_new_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    different_eye_and_life(str_arg)
+    print('own_man')
+
+def different_eye_and_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('way')
