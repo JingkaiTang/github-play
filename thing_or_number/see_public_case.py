119798922adbe02a10398c06c07676707fe9e2ee
Jingkai Tang <jingkaitang@gmail.com>
2014-12-20 06:24:01 +0800

    rm: tell thing_or_number/see_public_case.py of other world

---
+++ thing_or_number/see_public_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_eye(str_arg):
+    seem_fact(str_arg)
+    print('right_week')
+
+def seem_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_eye('next_man')
