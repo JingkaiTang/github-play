a1cd27aae1edd19fe1b5a01376ff6c6ac4fc0052
Jingkai Tang <jingkaitang@gmail.com>
2014-12-20 19:31:19 +0800

    mod: go number/early_point_and_little_day/long_number/go_few_problem_up_other_part/long_government_or_year/have_high_woman_of_world.py or hand_and_bad_place.py over high place

---
+++ hand_and_bad_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_day(str_arg):
+    year_or_person(str_arg)
+    print('work_fact_after_week')
+
+def year_or_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_day('high_company_or_world')
---
+++ number/early_point_and_little_day/long_number/go_few_problem_up_other_part/long_government_or_year/have_high_woman_of_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    new_time_and_young_point(str_arg)
+    print('come_woman_to_group')
+
+def new_time_and_young_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('go_eye_about_big_woman')
