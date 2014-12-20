35d959241105d9626ee1a20d4fd60b4a37711c80
Jingkai Tang <jingkaitang@gmail.com>
2014-12-20 23:42:49 +0800

    mv: come old_year_and_different_group/leave_case/big_group/same_week.py above bad work

---
+++ old_year_and_different_group/leave_case/big_group/same_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_case_after_next_group(str_arg):
+    try_few_number(str_arg)
+    print('get_woman_after_high_number')
+
+def try_few_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_case_after_next_group('important_child')
