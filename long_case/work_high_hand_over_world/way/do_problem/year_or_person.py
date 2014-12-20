c59e54646ece7e069d63aa57a60e50a4fa0795f1
Jingkai Tang <jingkaitang@gmail.com>
2014-12-21 02:57:23 +0800

    mix: find long_case/work_high_hand_over_world/way/do_problem/year_or_person.py by big hand

---
+++ long_case/work_high_hand_over_world/way/do_problem/year_or_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_part(str_arg):
+    know_same_day(str_arg)
+    print('year')
+
+def know_same_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_part('point')
