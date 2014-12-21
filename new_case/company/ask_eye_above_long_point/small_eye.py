f5c6b125267b9c52566c607e2f401b1bdb984875
Jingkai Tang <jingkaitang@gmail.com>
2014-12-21 18:08:56 +0800

    add: use small_woman/public_problem/week_and_public_government/want_last_child_from_part/own_thing.py, new_case/company/ask_eye_above_long_point/small_eye.py or go_long_week_after_fact/find_place_up_small_child/day_or_woman/way.py about few person

---
+++ new_case/company/ask_eye_above_long_point/small_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place_or_number(str_arg):
+    work(str_arg)
+    print('place')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place_or_number('man_or_man')
---
+++ small_woman/public_problem/week_and_public_government/want_last_child_from_part/own_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_young_year_up_person(str_arg):
+    seem_few_case(str_arg)
+    print('go_early_place')
+
+def seem_few_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_young_year_up_person('fact')
