8176c38a837c1803ce0bf8f8995582fba8183ef3
Jingkai Tang <jingkaitang@gmail.com>
2014-12-22 23:34:21 +0800

    mv: call week/look_good_week/time.py and early_eye/man/problem/problem.py under bad week

---
+++ early_eye/man/problem/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_fact(str_arg):
+    able_woman_and_problem(str_arg)
+    print('work_and_day')
+
+def able_woman_and_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_fact('high_day')
---
+++ week/look_good_week/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_government(str_arg):
+    small_woman(str_arg)
+    print('year_and_day')
+
+def small_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_government('bad_place')
