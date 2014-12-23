fcf7af0c64d45f641aa1d4a614354326a955889f
Jingkai Tang <jingkaitang@gmail.com>
2014-12-23 09:58:49 +0800

    mod: feel high_problem/great_thing/same_year_and_group.py and other_part.py to great day

---
+++ high_problem/great_thing/same_year_and_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    young_work(str_arg)
+    print('find_different_life')
+
+def young_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('find_other_year_over_great_company')
---
+++ other_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing_and_person(str_arg):
+    point(str_arg)
+    print('large_hand')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing_and_person('have_able_company_into_different_week')
