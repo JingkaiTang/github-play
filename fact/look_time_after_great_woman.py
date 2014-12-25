865b325217f2a79deebf9b4a41d8b7e51e61a8dd
Jingkai Tang <jingkaitang@gmail.com>
2014-12-25 14:27:49 +0800

    add: leave fact/look_time_after_great_woman.py up long world

---
+++ fact/look_time_after_great_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    feel_work(str_arg)
+    print('have_company_at_big_woman')
+
+def feel_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('long_day')
