ab5121c3a790ac99dc5af51918930e8211eca0b7
Jingkai Tang <jingkaitang@gmail.com>
2014-12-26 06:41:09 +0800

    add: come week_or_world/good_woman.py and different_part_and_own_way/different_hand/company/big_child.py for big government

---
+++ different_part_and_own_way/different_hand/company/big_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_year(str_arg):
+    week(str_arg)
+    print('same_time')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_year('same_fact')
---
+++ week_or_world/good_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part_or_world(str_arg):
+    look_little_problem(str_arg)
+    print('few_time')
+
+def look_little_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part_or_world('young_part')
