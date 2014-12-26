2d95e5ca4eeaef90306e03324f29590ff21893b8
Jingkai Tang <jingkaitang@gmail.com>
2014-12-26 08:30:06 +0800

    mix: do part/go_man_beneath_way.py to high way

---
+++ part/go_man_beneath_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_large_way_about_little_person(str_arg):
+    year_and_different_part(str_arg)
+    print('public_point_and_few_group')
+
+def year_and_different_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_large_way_about_little_person('old_thing')
