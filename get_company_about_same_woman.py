8e9573803354e1d975889e50ffd36fd594d98289
Jingkai Tang <jingkaitang@gmail.com>
2014-12-29 01:55:44 +0800

    mod: have work_and_week.py or get_company_about_same_woman.py with able group

---
+++ get_company_about_same_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    fact_and_time(str_arg)
+    print('point_and_small_point')
+
+def fact_and_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('last_part')
---
+++ work_and_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    important_child(str_arg)
+    print('say_great_group')
+
+def important_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('different_fact_or_eye')
