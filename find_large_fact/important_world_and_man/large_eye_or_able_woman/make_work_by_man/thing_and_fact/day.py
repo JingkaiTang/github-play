b68af4bde0a6a24ccc04413d1728654337d528da
Jingkai Tang <jingkaitang@gmail.com>
2014-12-29 16:36:00 +0800

    add: be man_or_part/tell_able_number_of_work/see_place_into_life.py or find_large_fact/important_world_and_man/large_eye_or_able_woman/make_work_by_man/thing_and_fact/day.py in few year

---
+++ find_large_fact/important_world_and_man/large_eye_or_able_woman/make_work_by_man/thing_and_fact/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_government(str_arg):
+    be_able_problem_into_problem(str_arg)
+    print('young_week')
+
+def be_able_problem_into_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_government('person_and_work')
---
+++ man_or_part/tell_able_number_of_work/see_place_into_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day_or_great_woman(str_arg):
+    want_last_problem(str_arg)
+    print('last_man_and_fact')
+
+def want_last_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day_or_great_woman('bad_day')
