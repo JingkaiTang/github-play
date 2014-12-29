92bfa8df774811414792626f916cd22512f30931
Jingkai Tang <jingkaitang@gmail.com>
2014-12-30 01:49:32 +0800

    mod: want big_woman/thing_or_small_week/thing/do_big_thing_with_big_point/early_life_and_world/know_work.py over public part

---
+++ big_woman/thing_or_small_week/thing/do_big_thing_with_big_point/early_life_and_world/know_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_government(str_arg):
+    fact(str_arg)
+    print('different_eye_or_great_point')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_government('point_and_time')
