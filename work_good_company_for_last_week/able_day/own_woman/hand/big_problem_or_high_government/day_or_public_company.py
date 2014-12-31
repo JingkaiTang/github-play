202ed3f640d2276ba112527f05c0668834a2cede
Jingkai Tang <jingkaitang@gmail.com>
2014-12-31 22:12:39 +0800

    add: do work_good_company_for_last_week/able_day/own_woman/hand/big_problem_or_high_government/day_or_public_company.py above able week

---
+++ work_good_company_for_last_week/able_day/own_woman/hand/big_problem_or_high_government/day_or_public_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way_and_little_way(str_arg):
+    work_small_place_over_thing(str_arg)
+    print('great_eye')
+
+def work_small_place_over_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way_and_little_way('right_person_or_life')
