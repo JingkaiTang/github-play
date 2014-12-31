40fe1480f4a8813f1cf8a0d34e5bca611ca91b6b
Jingkai Tang <jingkaitang@gmail.com>
2014-12-31 21:57:57 +0800

    add: want own_number/see_different_part/little_thing/want_problem_after_week/look_little_place.py under early woman

---
+++ own_number/see_different_part/little_thing/want_problem_after_week/look_little_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    want_eye_of_last_woman(str_arg)
+    print('problem')
+
+def want_eye_of_last_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('next_woman')
