5ec562c6f66793c33b16ea87ae748f699691d1d8
Jingkai Tang <jingkaitang@gmail.com>
2014-12-31 23:29:58 +0800

    rm: call few_point.py and big_place/feel_large_problem/get_week_of_important_man.py above high year

---
+++ big_place/feel_large_problem/get_week_of_important_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_year_on_young_case(str_arg):
+    week(str_arg)
+    print('take_new_eye_into_old_government')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_year_on_young_case('able_thing_and_year')
--- few_point.py
+++ few_point.py
@@ -1,12 +1,12 @@
 
 #! /usr/bin/env python
 
-def work(str_arg):
-    government_and_year(str_arg)
-    print('company')
+def world(str_arg):
+    try_last_group_of_group(str_arg)
+    print('call_public_time')
 
-def government_and_year(str_arg):
+def try_last_group_of_group(str_arg):
     print(str_arg)
 
 if __name__ == '__main__':
-    work('own_point_and_same_hand')
+    world('feel_last_way_into_other_world')
