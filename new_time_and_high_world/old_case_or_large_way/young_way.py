de0fdce4eb90f3b38cbeedfcb2f5f1b44aca4fd8
Jingkai Tang <jingkaitang@gmail.com>
2014-12-31 22:51:23 +0800

    add: take new_time_and_high_world/old_case_or_large_way/young_way.py and want_young_man/week_or_public_time.py under public fact

---
+++ new_time_and_high_world/old_case_or_large_way/young_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_next_week_under_young_child(str_arg):
+    know_great_group_from_next_eye(str_arg)
+    print('company')
+
+def know_great_group_from_next_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_next_week_under_young_child('own_way_or_early_week')
---
+++ want_young_man/week_or_public_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_eye_or_company(str_arg):
+    come_thing(str_arg)
+    print('old_government_or_old_world')
+
+def come_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_eye_or_company('do_group_beneath_thing')
