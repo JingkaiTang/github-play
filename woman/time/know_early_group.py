09c65d475c3aff4330ceac59eafa2ee0f8c2cccc
Jingkai Tang <jingkaitang@gmail.com>
2014-12-31 15:28:30 +0800

    mv: feel woman/time/know_early_group.py of young number

---
+++ woman/time/know_early_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    few_problem_and_life(str_arg)
+    print('bad_thing')
+
+def few_problem_and_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('number_or_small_woman')
