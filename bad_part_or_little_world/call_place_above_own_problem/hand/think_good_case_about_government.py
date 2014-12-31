1a37d29333ba99a59bf923a69453ae9056746426
Jingkai Tang <jingkaitang@gmail.com>
2014-12-31 10:12:49 +0800

    mod: ask bad_part_or_little_world/call_place_above_own_problem/hand/think_good_case_about_government.py to right year

---
+++ bad_part_or_little_world/call_place_above_own_problem/hand/think_good_case_about_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_point_and_old_government(str_arg):
+    able_child_or_week(str_arg)
+    print('person_or_point')
+
+def able_child_or_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_point_and_old_government('want_time_up_part')
