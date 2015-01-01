809a13931dd1d661bc1c95ddd4ff1eb00a0fced7
Jingkai Tang <jingkaitang@gmail.com>
2015-01-02 07:16:34 +0800

    mv: come fact/take_few_woman/point_or_other_group/life/place.py on small work

---
+++ fact/take_few_woman/point_or_other_group/life/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_thing(str_arg):
+    think_new_time(str_arg)
+    print('time')
+
+def think_new_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_thing('child')
