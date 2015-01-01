9728ba8ea7e037a7145969941c763b9f90229871
Jingkai Tang <jingkaitang@gmail.com>
2015-01-01 13:58:29 +0800

    add: give group_or_few_point/week_and_part.py from high company

---
+++ group_or_few_point/week_and_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_or_life(str_arg):
+    hand(str_arg)
+    print('long_company')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_or_life('public_point')
