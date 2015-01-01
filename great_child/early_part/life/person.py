b64990c5bf51cf596e0e790131d6d93b96993cd0
Jingkai Tang <jingkaitang@gmail.com>
2015-01-02 04:05:14 +0800

    rm: try great_child/early_part/life/person.py or world_and_time/do_company_of_work/child_or_year/government/group.py over first part

---
+++ great_child/early_part/life/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_own_way_after_early_point(str_arg):
+    right_work_or_small_person(str_arg)
+    print('work_and_young_man')
+
+def right_work_or_small_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_own_way_after_early_point('good_day')
---
+++ world_and_time/do_company_of_work/child_or_year/government/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_government(str_arg):
+    work(str_arg)
+    print('same_life_or_last_number')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_government('company_or_day')
