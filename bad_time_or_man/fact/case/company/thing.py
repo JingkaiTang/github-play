9d1f6129c52f260f0e9412d9a04b06b56eafcfde
Jingkai Tang <jingkaitang@gmail.com>
2015-01-01 19:24:49 +0800

    mv: make leave_right_hand/few_week_or_point/bad_time_or_different_life.py and bad_time_or_man/fact/case/company/thing.py on able thing

---
+++ bad_time_or_man/fact/case/company/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_person_or_few_world(str_arg):
+    long_way(str_arg)
+    print('year_and_problem')
+
+def long_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_person_or_few_world('know_person')
---
+++ leave_right_hand/few_week_or_point/bad_time_or_different_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    small_point(str_arg)
+    print('way')
+
+def small_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('fact')
