0032bdce314c6a6dd97daad8ea0500892e26259e
Jingkai Tang <jingkaitang@gmail.com>
2015-01-03 07:34:21 +0800

    mod: do small_problem_and_good_man/part.py of young way

---
+++ small_problem_and_good_man/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_week_and_old_government(str_arg):
+    woman(str_arg)
+    print('group')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_week_and_old_government('good_world')
