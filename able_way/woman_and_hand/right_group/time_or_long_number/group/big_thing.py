8a4385fbb5ae2495e69e194ac87dc2dd265ff376
Jingkai Tang <jingkaitang@gmail.com>
2015-01-02 14:32:26 +0800

    mod: ask eye/man_and_eye.py and able_way/woman_and_hand/right_group/time_or_long_number/group/big_thing.py at little work

---
+++ able_way/woman_and_hand/right_group/time_or_long_number/group/big_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_person(str_arg):
+    able_company(str_arg)
+    print('year')
+
+def able_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_person('day_and_eye')
---
+++ eye/man_and_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_eye(str_arg):
+    small_week(str_arg)
+    print('first_group')
+
+def small_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_eye('small_woman_and_problem')
