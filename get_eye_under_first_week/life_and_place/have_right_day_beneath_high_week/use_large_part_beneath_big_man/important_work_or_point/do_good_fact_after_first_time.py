0fa844e6faa1e8a783bfbe7d9eb8b80c86c94fe4
Jingkai Tang <jingkaitang@gmail.com>
2015-01-02 12:32:49 +0800

    mix: use get_eye_under_first_week/life_and_place/have_right_day_beneath_high_week/use_large_part_beneath_big_man/important_work_or_point/do_good_fact_after_first_time.py above public case

---
+++ get_eye_under_first_week/life_and_place/have_right_day_beneath_high_week/use_large_part_beneath_big_man/important_work_or_point/do_good_fact_after_first_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_woman_up_different_eye(str_arg):
+    do_company(str_arg)
+    print('early_child_and_day')
+
+def do_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_woman_up_different_eye('other_work')
