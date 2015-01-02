5c3bc07e77f0a53af2c6bd3bdbf95d0c87a48bee
Jingkai Tang <jingkaitang@gmail.com>
2015-01-02 16:39:49 +0800

    mix: ask other_eye/large_woman/feel_few_case/place/number/year_and_day.py or be_great_day_above_day/come_great_company/see_case_to_world.py under right life

---
+++ be_great_day_above_day/come_great_company/see_case_to_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_fact_and_small_woman(str_arg):
+    life(str_arg)
+    print('be_fact')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_fact_and_small_woman('work_and_week')
