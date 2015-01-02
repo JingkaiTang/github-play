71aad5ef2c6adacaac335e20804209a4dd092707
Jingkai Tang <jingkaitang@gmail.com>
2015-01-02 15:56:13 +0800

    add: seem little_year.py about old eye

---
+++ little_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    take_able_number(str_arg)
+    print('look_next_work')
+
+def take_able_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('other_man_or_woman')
