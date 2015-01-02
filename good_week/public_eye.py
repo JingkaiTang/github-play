87b9f5df947298255e36431789cc29f96a11283a
Jingkai Tang <jingkaitang@gmail.com>
2015-01-02 09:55:55 +0800

    add: know old_thing/right_week/try_woman_up_bad_point/company.py and good_week/public_eye.py into different day

---
+++ good_week/public_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_world_or_point(str_arg):
+    able_thing_and_government(str_arg)
+    print('bad_point')
+
+def able_thing_and_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_world_or_point('hand')
---
+++ old_thing/right_week/try_woman_up_bad_point/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    bad_time(str_arg)
+    print('come_government')
+
+def bad_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('work_and_good_world')
