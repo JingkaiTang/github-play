b82e24c28c2402afe99c246d9cc70669c283f410
Jingkai Tang <jingkaitang@gmail.com>
2015-01-03 19:29:31 +0800

    mix: make small_point_or_big_fact/old_case/fact_or_company/feel_place/few_fact_and_person.py beneath little fact

---
+++ small_point_or_big_fact/old_case/fact_or_company/feel_place/few_fact_and_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_eye(str_arg):
+    government_and_long_time(str_arg)
+    print('week')
+
+def government_and_long_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_eye('woman')
