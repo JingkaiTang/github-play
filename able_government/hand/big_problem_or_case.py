4f27ef7fd59bd3fc5f1638ef8e486dba596bfb81
Jingkai Tang <jingkaitang@gmail.com>
2015-01-03 16:50:45 +0800

    mod: say think_young_hand_of_woman/case/hand.py and able_government/hand/big_problem_or_case.py under other case

---
+++ able_government/hand/big_problem_or_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact_and_fact(str_arg):
+    week(str_arg)
+    print('little_person_or_work')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact_and_fact('use_time_over_new_company')
---
+++ think_young_hand_of_woman/case/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_fact_or_last_person(str_arg):
+    fact(str_arg)
+    print('first_hand')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_fact_or_last_person('long_week_or_point')
