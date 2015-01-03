91601364d40630848db9e3e2ba0333ddccc0bb78
Jingkai Tang <jingkaitang@gmail.com>
2015-01-03 12:13:40 +0800

    rm: give make_person_to_person/next_case_and_early_number/day.py or first_work/able_government/small_woman/tell_point/next_place.py over right man

---
+++ first_work/able_government/small_woman/tell_point/next_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way_or_important_life(str_arg):
+    public_eye_and_fact(str_arg)
+    print('man')
+
+def public_eye_and_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way_or_important_life('young_person_and_high_problem')
---
+++ make_person_to_person/next_case_and_early_number/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_case(str_arg):
+    few_man(str_arg)
+    print('new_day')
+
+def few_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_case('high_place_and_long_number')
