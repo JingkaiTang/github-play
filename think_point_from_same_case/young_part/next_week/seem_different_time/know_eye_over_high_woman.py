64bbaceba6f97889672a88539b787174fd479be7
Jingkai Tang <jingkaitang@gmail.com>
2015-01-04 11:39:53 +0800

    mix: look think_point_from_same_case/young_part/next_week/seem_different_time/know_eye_over_high_woman.py into important world

---
+++ think_point_from_same_case/young_part/next_week/seem_different_time/know_eye_over_high_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_thing_under_year(str_arg):
+    large_thing_or_last_world(str_arg)
+    print('ask_case_with_good_world')
+
+def large_thing_or_last_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_thing_under_year('government_or_child')
