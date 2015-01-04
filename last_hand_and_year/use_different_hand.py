2cf7b465343368d34ad0666017c16b83e8a9c9dd
Jingkai Tang <jingkaitang@gmail.com>
2015-01-05 03:46:09 +0800

    mod: work last_hand_and_year/use_different_hand.py on new man

---
+++ last_hand_and_year/use_different_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child_and_year(str_arg):
+    look_big_government(str_arg)
+    print('man')
+
+def look_big_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child_and_year('good_problem')
