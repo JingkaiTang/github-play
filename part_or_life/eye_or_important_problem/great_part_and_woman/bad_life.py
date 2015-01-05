b2f0f5789c019eafe0b5688561f8b1451963b248
Jingkai Tang <jingkaitang@gmail.com>
2015-01-06 05:15:19 +0800

    rm: see part_or_life/eye_or_important_problem/great_part_and_woman/bad_life.py for different place

---
+++ part_or_life/eye_or_important_problem/great_part_and_woman/bad_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_hand(str_arg):
+    last_time_or_case(str_arg)
+    print('try_young_fact')
+
+def last_time_or_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_hand('same_work_or_day')
