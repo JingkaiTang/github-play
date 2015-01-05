93fe7b95bef4a91c317f585c6ee55457e50fbd9f
Jingkai Tang <jingkaitang@gmail.com>
2015-01-05 20:31:08 +0800

    rm: find woman/part/get_week/part.py about right time

---
+++ woman/part/get_week/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_and_right_place(str_arg):
+    great_week_or_next_number(str_arg)
+    print('big_government')
+
+def great_week_or_next_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_and_right_place('day')
