4f5d9bb06d79c8ce7b705adc210beb18d3e85bd5
Jingkai Tang <jingkaitang@gmail.com>
2015-01-06 17:28:34 +0800

    mix: look work/public_time.py from long year

---
+++ work/public_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_work(str_arg):
+    give_fact_into_part(str_arg)
+    print('week')
+
+def give_fact_into_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_work('problem_or_high_problem')
