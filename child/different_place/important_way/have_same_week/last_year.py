dc21642bd658221e5d28cc17d7adb0e547c0bca8
Jingkai Tang <jingkaitang@gmail.com>
2015-01-06 22:43:37 +0800

    mod: call child/different_place/important_way/have_same_week/last_year.py above different life

---
+++ child/different_place/important_way/have_same_week/last_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_week_from_work(str_arg):
+    same_thing_or_different_government(str_arg)
+    print('get_good_hand')
+
+def same_thing_or_different_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_week_from_work('government')
