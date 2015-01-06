7f86855d197eed7309743e362d1d1540e4f720a2
Jingkai Tang <jingkaitang@gmail.com>
2015-01-06 18:53:39 +0800

    rm: do different_government.py or year/same_place.py under different child

---
+++ different_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    tell_way(str_arg)
+    print('leave_child')
+
+def tell_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('point')
---
+++ year/same_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    first_eye(str_arg)
+    print('small_point')
+
+def first_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('use_week')
