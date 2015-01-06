cdfdc29f18327fe7be6841a88f8ffa26c1f14716
Jingkai Tang <jingkaitang@gmail.com>
2015-01-06 15:26:15 +0800

    mod: ask big_place/child/few_world_or_big_eye.py, new_person.py, year/year/few_time_or_government/life/public_hand_or_right_thing.py or old_eye/long_world_or_year/seem_new_company/way/way/part.py beneath next hand

---
+++ big_place/child/few_world_or_big_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_place(str_arg):
+    important_eye(str_arg)
+    print('feel_big_child_over_problem')
+
+def important_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_place('feel_thing_on_high_week')
---
+++ year/year/few_time_or_government/life/public_hand_or_right_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact_or_way(str_arg):
+    point_or_problem(str_arg)
+    print('take_same_government')
+
+def point_or_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact_or_way('come_group')
