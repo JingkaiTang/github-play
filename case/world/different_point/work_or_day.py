1c084fd956cac0af6579828123be641c9648a118
Jingkai Tang <jingkaitang@gmail.com>
2015-01-08 04:49:03 +0800

    rm: tell case/world/different_point/work_or_day.py from long case

---
+++ case/world/different_point/work_or_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_time(str_arg):
+    thing(str_arg)
+    print('be_woman_with_new_company')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_time('leave_different_person')
