e8503bb329fcb85875530b6d8a15606d78eff8e7
Jingkai Tang <jingkaitang@gmail.com>
2015-01-08 11:09:55 +0800

    add: work high_point/fact_or_person/have_able_fact.py at next child

---
+++ high_point/fact_or_person/have_able_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_man(str_arg):
+    take_small_problem_after_next_life(str_arg)
+    print('next_part')
+
+def take_small_problem_after_next_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_man('last_place_and_way')
