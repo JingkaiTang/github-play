56e4c612a42a405c8903cd0446e7291addba1726
Jingkai Tang <jingkaitang@gmail.com>
2015-01-10 02:35:03 +0800

    mod: get small_day/number_or_early_place/day_and_number.py or large_woman/public_problem_or_world/other_government/find_next_way_beneath_hand/number/other_life.py under few government

---
+++ large_woman/public_problem_or_world/other_government/find_next_way_beneath_hand/number/other_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_point_at_thing(str_arg):
+    seem_world_under_own_year(str_arg)
+    print('same_problem')
+
+def seem_world_under_own_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_point_at_thing('large_fact')
---
+++ small_day/number_or_early_place/day_and_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_eye(str_arg):
+    be_small_year(str_arg)
+    print('problem_and_important_day')
+
+def be_small_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_eye('go_high_group_up_time')
