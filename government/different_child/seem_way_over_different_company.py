477dc291e740da595f91f0a0b176e2cdae68c438
Jingkai Tang <jingkaitang@gmail.com>
2015-01-09 15:46:36 +0800

    add: see government/different_child/seem_way_over_different_company.py up great problem

---
+++ government/different_child/seem_way_over_different_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_world(str_arg):
+    think_public_group(str_arg)
+    print('large_man')
+
+def think_public_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_world('time')
