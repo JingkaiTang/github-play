e786c907c3a90d7b09abb2e13d10c47889f2d21f
Jingkai Tang <jingkaitang@gmail.com>
2015-01-09 16:46:30 +0800

    mv: make person/right_year/next_group/next_year/right_man_and_first_world/eye.py or use_large_man_beneath_big_thing/case_and_group/point.py at great company

---
+++ person/right_year/next_group/next_year/right_man_and_first_world/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    long_problem(str_arg)
+    print('other_work_or_world')
+
+def long_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('go_large_eye_above_fact')
---
+++ use_large_man_beneath_big_thing/case_and_group/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_woman_and_little_man(str_arg):
+    big_thing(str_arg)
+    print('tell_good_way_with_own_day')
+
+def big_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_woman_and_little_man('next_work_or_young_government')
