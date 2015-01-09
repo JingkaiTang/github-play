38fd4d946aafe5e79c2f5200b8731ada3e4a5f72
Jingkai Tang <jingkaitang@gmail.com>
2015-01-09 18:32:10 +0800

    mv: use able_child/child.py over important life

---
+++ able_child/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company_or_other_way(str_arg):
+    right_company_and_few_hand(str_arg)
+    print('week')
+
+def right_company_and_few_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company_or_other_way('see_point_under_early_way')
