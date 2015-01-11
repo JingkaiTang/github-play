e73ad92600a27e00bc95c468d82f9f3e123c4898
Jingkai Tang <jingkaitang@gmail.com>
2015-01-12 01:36:57 +0800

    mod: do little_place.py from young person

---
+++ little_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_right_life_of_group(str_arg):
+    child(str_arg)
+    print('go_right_day_above_fact')
+
+def child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_right_life_of_group('number')
