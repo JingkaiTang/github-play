1948344bad5b1e4e8a6855b4a18ed62793acac7e
Jingkai Tang <jingkaitang@gmail.com>
2015-01-12 05:15:08 +0800

    mv: take ask_place/problem/ask_large_point_of_part/place/case/next_group.py into long day

---
+++ ask_place/problem/ask_large_point_of_part/place/case/next_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_problem(str_arg):
+    life_or_first_child(str_arg)
+    print('different_place_and_day')
+
+def life_or_first_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_problem('come_case')
