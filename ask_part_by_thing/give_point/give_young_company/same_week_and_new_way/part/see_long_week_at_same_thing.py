ab678b2b483065330cc21a8fffa1d36420b71480
Jingkai Tang <jingkaitang@gmail.com>
2015-01-12 18:06:47 +0800

    mix: go ask_part_by_thing/give_point/give_young_company/same_week_and_new_way/part/see_long_week_at_same_thing.py by small week

---
+++ ask_part_by_thing/give_point/give_young_company/same_week_and_new_way/part/see_long_week_at_same_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    long_government(str_arg)
+    print('person_or_year')
+
+def long_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('point')
