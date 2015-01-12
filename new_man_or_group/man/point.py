a5caa2ab979dc8d6806058c41be8cee8036ca57e
Jingkai Tang <jingkaitang@gmail.com>
2015-01-12 10:19:01 +0800

    add: see new_man_or_group/man/point.py about right year

---
+++ new_man_or_group/man/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_group_in_small_case(str_arg):
+    same_government(str_arg)
+    print('child')
+
+def same_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_group_in_small_case('make_number')
