61649cbc7ca772011b6c93624582254ae699ae95
Jingkai Tang <jingkaitang@gmail.com>
2015-01-13 05:46:37 +0800

    add: work last_point_or_last_day.py of great year

---
+++ last_point_or_last_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time_and_life(str_arg):
+    large_government(str_arg)
+    print('important_eye_or_next_government')
+
+def large_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time_and_life('week_or_high_life')
