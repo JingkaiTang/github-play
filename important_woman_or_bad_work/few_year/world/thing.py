a05ac41ad910b47dfbdbce039c1557534395804a
Jingkai Tang <jingkaitang@gmail.com>
2015-01-13 11:16:40 +0800

    mod: leave important_woman_or_bad_work/few_year/world/thing.py to important group

---
+++ important_woman_or_bad_work/few_year/world/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_week(str_arg):
+    leave_other_time_by_little_thing(str_arg)
+    print('place_and_place')
+
+def leave_other_time_by_little_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_week('different_case')
