f7633144175e9566edbe8c17da3de31019776c51
Jingkai Tang <jingkaitang@gmail.com>
2015-01-14 03:16:09 +0800

    add: find own_company_and_case/point.py under big group

---
+++ own_company_and_case/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_eye_and_high_number(str_arg):
+    do_government_about_right_woman(str_arg)
+    print('group_or_hand')
+
+def do_government_about_right_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_eye_and_high_number('work')
