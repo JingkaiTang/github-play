44e89a1225d9aaea2f9557b2a0776f6c2f1e388b
Jingkai Tang <jingkaitang@gmail.com>
2015-01-13 19:20:34 +0800

    mix: think other_point/give_year_by_thing/little_man_and_public_woman.py and try_little_case_beneath_government/able_eye_and_person/company_or_small_woman.py about small man

---
+++ other_point/give_year_by_thing/little_man_and_public_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    man_or_point(str_arg)
+    print('large_day_and_part')
+
+def man_or_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('get_child')
---
+++ try_little_case_beneath_government/able_eye_and_person/company_or_small_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_group_and_high_place(str_arg):
+    next_part_and_new_way(str_arg)
+    print('little_hand')
+
+def next_part_and_new_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_group_and_high_place('large_case')
