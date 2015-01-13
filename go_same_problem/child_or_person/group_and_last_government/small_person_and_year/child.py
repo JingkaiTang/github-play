becd32cb1b0d501e2cc3a1ac824c34c4a937e166
Jingkai Tang <jingkaitang@gmail.com>
2015-01-13 22:00:35 +0800

    mod: come go_same_problem/child_or_person/group_and_last_government/small_person_and_year/child.py under able child

---
+++ go_same_problem/child_or_person/group_and_last_government/small_person_and_year/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_small_place_in_woman(str_arg):
+    woman_or_fact(str_arg)
+    print('little_week_and_year')
+
+def woman_or_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_small_place_in_woman('be_same_life')
