e18ccdf7cc523a1433b059226e16fd4329595e46
Jingkai Tang <jingkaitang@gmail.com>
2015-01-14 01:50:58 +0800

    mv: tell way_or_little_life/problem_and_time/same_thing/big_work/company.py on big man

---
+++ way_or_little_life/problem_and_time/same_thing/big_work/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    fact(str_arg)
+    print('tell_good_hand_for_new_hand')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('work_and_own_hand')
