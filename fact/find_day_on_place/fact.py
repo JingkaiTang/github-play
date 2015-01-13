e90b49b543588a632665f52cd13849f62f0121ce
Jingkai Tang <jingkaitang@gmail.com>
2015-01-14 03:12:13 +0800

    add: see fact/find_day_on_place/fact.py for small problem

---
+++ fact/find_day_on_place/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_year(str_arg):
+    old_fact_or_small_number(str_arg)
+    print('work')
+
+def old_fact_or_small_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_year('large_time')
