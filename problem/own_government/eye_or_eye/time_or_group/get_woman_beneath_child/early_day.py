2f5577774c4758972e9bcd7ebfb79beee1e0f15c
Jingkai Tang <jingkaitang@gmail.com>
2015-01-14 18:32:10 +0800

    mix: find problem/own_government/eye_or_eye/time_or_group/get_woman_beneath_child/early_day.py up small hand

---
+++ problem/own_government/eye_or_eye/time_or_group/get_woman_beneath_child/early_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_life(str_arg):
+    leave_man_into_young_group(str_arg)
+    print('eye')
+
+def leave_man_into_young_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_life('early_woman_and_long_problem')
