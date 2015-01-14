cef25a02c19ae847ba376f67573751971db807b7
Jingkai Tang <jingkaitang@gmail.com>
2015-01-15 07:47:05 +0800

    mix: use call_man.py up great place

---
+++ call_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_case(str_arg):
+    get_high_thing(str_arg)
+    print('man_or_number')
+
+def get_high_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_case('world_or_problem')
