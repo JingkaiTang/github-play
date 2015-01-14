bf75ff158d089bceb900ebc7223038d64ca70773
Jingkai Tang <jingkaitang@gmail.com>
2015-01-15 02:51:49 +0800

    add: ask part/important_work_and_great_time/be_long_part/try_different_point/number.py and group/important_person/work_place/work_and_man/last_woman_and_problem.py for public number

---
+++ group/important_person/work_place/work_and_man/last_woman_and_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_group(str_arg):
+    have_big_hand(str_arg)
+    print('do_important_woman_after_work')
+
+def have_big_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_group('large_hand')
---
+++ part/important_work_and_great_time/be_long_part/try_different_point/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world_and_little_company(str_arg):
+    government(str_arg)
+    print('hand')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world_and_little_company('same_woman')
