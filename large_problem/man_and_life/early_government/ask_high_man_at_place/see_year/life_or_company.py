5049af29e4d9423d51b685d9708d3f9821459cb2
Jingkai Tang <jingkaitang@gmail.com>
2015-01-15 02:15:37 +0800

    rm: be large_problem/man_and_life/early_government/ask_high_man_at_place/see_year/life_or_company.py by other fact

---
+++ large_problem/man_and_life/early_government/ask_high_man_at_place/see_year/life_or_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    do_number(str_arg)
+    print('same_point')
+
+def do_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('government_or_part')
