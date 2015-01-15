88b075e02cf1aa655c72d85220a87d30f9764116
Jingkai Tang <jingkaitang@gmail.com>
2015-01-15 23:09:18 +0800

    add: make number_and_child/last_problem.py to next eye

---
+++ number_and_child/last_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    time_and_way(str_arg)
+    print('work')
+
+def time_and_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('new_case')
