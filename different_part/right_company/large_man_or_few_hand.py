997c9ea9601e519b8ee09f6ebc399573f61977e7
Jingkai Tang <jingkaitang@gmail.com>
2015-01-15 09:28:20 +0800

    rm: tell seem_right_hand/person.py and different_part/right_company/large_man_or_few_hand.py under large day

---
+++ different_part/right_company/large_man_or_few_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_place(str_arg):
+    first_man_and_time(str_arg)
+    print('company_or_government')
+
+def first_man_and_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_place('able_number_or_first_woman')
---
+++ seem_right_hand/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    large_day(str_arg)
+    print('own_life')
+
+def large_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('year')
