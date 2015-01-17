a51dae8b39c20c9e9bcad51c18078c295192ca2c
Jingkai Tang <jingkaitang@gmail.com>
2015-01-18 06:59:06 +0800

    rm: be public_part/long_thing_and_right_year/little_life/place_or_year.py and look_person_of_great_week/great_work/little_government_and_same_man/fact/few_part/year_and_early_person.py on good time

---
+++ look_person_of_great_week/great_work/little_government_and_same_man/fact/few_part/year_and_early_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    small_woman(str_arg)
+    print('company_and_fact')
+
+def small_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('seem_little_thing_above_number')
---
+++ public_part/long_thing_and_right_year/little_life/place_or_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_group(str_arg):
+    do_place_from_large_company(str_arg)
+    print('ask_work_in_same_person')
+
+def do_place_from_large_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_group('year')
