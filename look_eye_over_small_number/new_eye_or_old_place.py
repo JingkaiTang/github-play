d29eab7cdd41fa7a948cb0eea9688f9e5bf24d17
Jingkai Tang <jingkaitang@gmail.com>
2015-01-18 00:10:34 +0800

    mix: think look_eye_over_small_number/new_eye_or_old_place.py over same life

---
+++ look_eye_over_small_number/new_eye_or_old_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_eye(str_arg):
+    small_day(str_arg)
+    print('part')
+
+def small_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_eye('right_part')
