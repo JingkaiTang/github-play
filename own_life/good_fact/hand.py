e7951458887a650225e06ede52d92f15aa773a29
Jingkai Tang <jingkaitang@gmail.com>
2015-01-18 18:51:09 +0800

    add: get own_life/good_fact/hand.py from young hand

---
+++ own_life/good_fact/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    go_hand_over_company(str_arg)
+    print('time')
+
+def go_hand_over_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('look_place')
