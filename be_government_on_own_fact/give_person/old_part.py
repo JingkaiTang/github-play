6a5afd25791bcccae44ddda3ccda57c634bf44c3
Jingkai Tang <jingkaitang@gmail.com>
2015-01-18 13:45:27 +0800

    rm: think year/important_week/want_new_problem_about_world/think_new_place_at_child/time/new_work.py or be_government_on_own_fact/give_person/old_part.py with big part

---
+++ be_government_on_own_fact/give_person/old_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    life_and_good_week(str_arg)
+    print('small_man')
+
+def life_and_good_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('way_or_same_case')
---
+++ year/important_week/want_new_problem_about_world/think_new_place_at_child/time/new_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_eye_with_child(str_arg):
+    company(str_arg)
+    print('woman')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_eye_with_child('find_other_work')
