4ca20b55a5b045649de877687178c24958f753a9
Jingkai Tang <jingkaitang@gmail.com>
2015-01-18 15:41:28 +0800

    rm: seem little_way.py or part/work_woman.py of public week

---
+++ little_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_work(str_arg):
+    bad_fact(str_arg)
+    print('different_group')
+
+def bad_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_work('few_point')
---
+++ part/work_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_life_and_man(str_arg):
+    bad_year(str_arg)
+    print('part_or_time')
+
+def bad_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_life_and_man('day')
