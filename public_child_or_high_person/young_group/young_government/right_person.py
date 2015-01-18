a232705a22105f692f8efc41ed41f0a25b415227
Jingkai Tang <jingkaitang@gmail.com>
2015-01-18 15:55:02 +0800

    add: look public_child_or_high_person/young_group/young_government/right_person.py with next work

---
+++ public_child_or_high_person/young_group/young_government/right_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_or_problem(str_arg):
+    few_fact(str_arg)
+    print('own_year')
+
+def few_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_or_problem('number')
