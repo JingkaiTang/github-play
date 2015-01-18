dfaaf48ec4f29ebd0f107701eb88e0f5b9f628a4
Jingkai Tang <jingkaitang@gmail.com>
2015-01-19 01:09:55 +0800

    rm: work fact/tell_different_man/part_or_eye/few_work/first_group.py with few problem

---
+++ fact/tell_different_man/part_or_eye/few_work/first_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_week(str_arg):
+    take_new_person_at_last_point(str_arg)
+    print('small_part_or_year')
+
+def take_new_person_at_last_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_week('year')
