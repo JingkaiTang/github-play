a0cddf0114901a541443a43c54b3e6fb6e3b194b
Jingkai Tang <jingkaitang@gmail.com>
2015-01-20 21:38:38 +0800

    add: come number_or_woman/think_way_on_large_place/day/new_life_and_important_group/early_man/government_and_child.py at old fact

---
+++ number_or_woman/think_way_on_large_place/day/new_life_and_important_group/early_man/government_and_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government_or_week(str_arg):
+    government(str_arg)
+    print('man')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government_or_week('find_high_point_on_work')
