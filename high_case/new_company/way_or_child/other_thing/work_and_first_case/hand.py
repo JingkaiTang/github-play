919e02b307401690da969f05da455d8a4aeb7fba
Jingkai Tang <jingkaitang@gmail.com>
2015-01-20 12:09:26 +0800

    mv: think high_case/new_company/way_or_child/other_thing/work_and_first_case/hand.py up right point

---
+++ high_case/new_company/way_or_child/other_thing/work_and_first_case/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_large_man(str_arg):
+    work_or_early_group(str_arg)
+    print('take_own_man_of_young_life')
+
+def work_or_early_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_large_man('feel_part_beneath_old_world')
