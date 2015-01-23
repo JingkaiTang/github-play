ab7acc9ca1bcb93bd61022dbead00426580ce507
Jingkai Tang <jingkaitang@gmail.com>
2015-01-24 04:43:24 +0800

    mod: work little_group/big_group/bad_year/thing/hand.py from different case

---
+++ little_group/big_group/bad_year/thing/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_hand_and_important_government(str_arg):
+    able_time_and_place(str_arg)
+    print('first_hand')
+
+def able_time_and_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_hand_and_important_government('day')
