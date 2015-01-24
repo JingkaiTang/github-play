e5cd5a26b8e760c88cbbbcc8aa91c2a484b55240
Jingkai Tang <jingkaitang@gmail.com>
2015-01-24 10:04:35 +0800

    mv: try same_year.py and way_or_group.py into little way

---
+++ same_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_thing_and_long_problem(str_arg):
+    young_person_and_good_place(str_arg)
+    print('want_able_time_above_company')
+
+def young_person_and_good_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_thing_and_long_problem('eye')
---
+++ way_or_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_thing_or_own_point(str_arg):
+    have_long_eye(str_arg)
+    print('first_life')
+
+def have_long_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_thing_or_own_point('point')
