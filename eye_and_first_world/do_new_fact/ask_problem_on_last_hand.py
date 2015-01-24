d148c6881984814331c3a3b38227634efaf9292a
Jingkai Tang <jingkaitang@gmail.com>
2015-01-24 15:59:38 +0800

    mod: say eye_and_first_world/do_new_fact/ask_problem_on_last_hand.py after good time

---
+++ eye_and_first_world/do_new_fact/ask_problem_on_last_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_life(str_arg):
+    leave_company(str_arg)
+    print('place_or_same_company')
+
+def leave_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_life('old_fact')
