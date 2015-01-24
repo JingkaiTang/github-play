3a0f76906b95b6287ed354738211d834304fb3d2
Jingkai Tang <jingkaitang@gmail.com>
2015-01-25 04:04:45 +0800

    rm: leave find_little_problem.py with public man

---
+++ find_little_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_great_life_to_life(str_arg):
+    different_point_or_large_point(str_arg)
+    print('high_point_or_long_work')
+
+def different_point_or_large_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_great_life_to_life('bad_case')
