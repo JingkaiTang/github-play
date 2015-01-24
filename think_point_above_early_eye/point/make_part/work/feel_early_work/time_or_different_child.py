a9ed573f5fb58cd1192509d7414d93d43612b399
Jingkai Tang <jingkaitang@gmail.com>
2015-01-24 16:17:31 +0800

    mix: be use_point/own_number/great_case_or_new_eye/time.py and think_point_above_early_eye/point/make_part/work/feel_early_work/time_or_different_child.py beneath right work

---
+++ think_point_above_early_eye/point/make_part/work/feel_early_work/time_or_different_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_problem_and_time(str_arg):
+    make_new_time(str_arg)
+    print('way_and_hand')
+
+def make_new_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_problem_and_time('great_world')
---
+++ use_point/own_number/great_case_or_new_eye/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    way(str_arg)
+    print('year_and_hand')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('new_week_and_hand')
