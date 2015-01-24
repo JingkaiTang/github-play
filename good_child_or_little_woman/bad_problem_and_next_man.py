eb2ce300bfadfba3632beed3ef27eb7feae812b8
Jingkai Tang <jingkaitang@gmail.com>
2015-01-24 09:17:06 +0800

    mv: seem good_child_or_little_woman/bad_problem_and_next_man.py about new government

---
+++ good_child_or_little_woman/bad_problem_and_next_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    call_number_up_new_part(str_arg)
+    print('part_or_part')
+
+def call_number_up_new_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('different_group')
