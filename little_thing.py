95ba8ba4629f2fbb638cefb43cbef944e9135024
Jingkai Tang <jingkaitang@gmail.com>
2015-01-26 00:10:59 +0800

    rm: feel little_thing.py after own world

---
+++ little_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_number(str_arg):
+    other_government_or_few_point(str_arg)
+    print('bad_government_and_problem')
+
+def other_government_or_few_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_number('see_last_group')
