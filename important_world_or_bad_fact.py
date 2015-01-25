5f57bce7f07f81085ab679e90ed1eb2afcfce888
Jingkai Tang <jingkaitang@gmail.com>
2015-01-25 21:27:45 +0800

    add: leave important_world_or_bad_fact.py beneath own work

---
+++ important_world_or_bad_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    make_same_woman(str_arg)
+    print('year')
+
+def make_same_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('young_world')
