27e6db42465113b62ba0f41b088cf5b246391349
Jingkai Tang <jingkaitang@gmail.com>
2015-01-25 10:38:13 +0800

    rm: make give_place_above_part/group_or_part/find_other_fact/new_number/hand/ask_high_point_up_first_place.py about first case

---
+++ give_place_above_part/group_or_part/find_other_fact/new_number/hand/ask_high_point_up_first_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    same_world(str_arg)
+    print('see_life_with_eye')
+
+def same_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('last_person')
