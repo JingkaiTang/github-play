a97d8574bd3530ff43cf66f7e0fcb98184e58757
Jingkai Tang <jingkaitang@gmail.com>
2015-01-26 16:54:21 +0800

    rm: get fact/life/company/government_or_old_thing/eye/young_year.py and try_point_to_first_year.py after different point

---
+++ fact/life/company/government_or_old_thing/eye/young_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    bad_place(str_arg)
+    print('small_world')
+
+def bad_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('make_small_child_beneath_little_problem')
---
+++ try_point_to_first_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_day_by_fact(str_arg):
+    tell_number_after_time(str_arg)
+    print('way_or_year')
+
+def tell_number_after_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_day_by_fact('little_child')
