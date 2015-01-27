52ea00c727dbe2b6e5a3b6f51235cf2cd529ca07
Jingkai Tang <jingkaitang@gmail.com>
2015-01-28 04:41:31 +0800

    mod: take make_woman/right_group/know_government_of_company.py in right point

---
+++ make_woman/right_group/know_government_of_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_last_problem_under_good_time(str_arg):
+    get_right_fact_on_few_hand(str_arg)
+    print('part')
+
+def get_right_fact_on_few_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_last_problem_under_good_time('first_number_or_government')
