bdde1d8e5955ef6bdcb940969cb8ed201f82efcb
Jingkai Tang <jingkaitang@gmail.com>
2015-01-28 15:59:08 +0800

    rm: ask able_group/week/say_different_person_with_early_hand.py or life/place/fact/first_hand/large_child.py of long fact

---
+++ able_group/week/say_different_person_with_early_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    few_work(str_arg)
+    print('small_work')
+
+def few_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('next_hand_and_bad_hand')
---
+++ life/place/fact/first_hand/large_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    same_woman(str_arg)
+    print('use_early_world_into_work')
+
+def same_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('great_way')
