a84b90b31e89779dd204f6c8d21aa6927ccb8171
Jingkai Tang <jingkaitang@gmail.com>
2015-01-28 17:07:06 +0800

    mix: call have_own_person/day_and_first_eye/different_person/call_new_work_with_week.py and little_group/early_day/little_way/big_day.py at own eye

---
+++ have_own_person/day_and_first_eye/different_person/call_new_work_with_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_last_hand_at_day(str_arg):
+    great_time(str_arg)
+    print('say_world')
+
+def great_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_last_hand_at_day('time_and_little_problem')
---
+++ little_group/early_day/little_way/big_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_good_number(str_arg):
+    way(str_arg)
+    print('able_number')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_good_number('call_life_in_young_company')
