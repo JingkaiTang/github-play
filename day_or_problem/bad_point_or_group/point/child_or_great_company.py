e1106af918bbe4b6f1501bd5836406e29d597546
Jingkai Tang <jingkaitang@gmail.com>
2015-01-28 08:43:40 +0800

    mix: give day_or_problem/bad_point_or_group/point/child_or_great_company.py or important_hand/number_and_able_case/old_fact_and_own_thing/new_thing_or_own_time/place_or_company/life.py under great man

---
+++ day_or_problem/bad_point_or_group/point/child_or_great_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_point(str_arg):
+    think_world(str_arg)
+    print('first_man')
+
+def think_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_point('world_or_world')
