e61c9c717f597306c87b72a54edc09066cbca43b
Jingkai Tang <jingkaitang@gmail.com>
2015-01-30 05:12:15 +0800

    mix: make hand/way/small_week/long_man_and_large_part/right_person_or_eye/day.py and government_and_little_life.py with small week

---
+++ government_and_little_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case_and_group(str_arg):
+    use_number(str_arg)
+    print('great_person')
+
+def use_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case_and_group('get_problem_above_child')
---
+++ hand/way/small_week/long_man_and_large_part/right_person_or_eye/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    important_company_or_year(str_arg)
+    print('child_or_next_place')
+
+def important_company_or_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('small_case_or_able_government')
