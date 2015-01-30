a3306c35189c9a3c74f57cd9a7c7f262f1507f09
Jingkai Tang <jingkaitang@gmail.com>
2015-01-30 16:06:02 +0800

    mod: feel see_high_work_under_small_time/first_point_or_government/able_day_or_next_part/man/different_number_or_little_life/large_thing.py or company_and_small_part/number_or_good_week/next_year/work_public_thing_under_big_government.py into good woman

---
+++ company_and_small_part/number_or_good_week/next_year/work_public_thing_under_big_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group_or_few_child(str_arg):
+    last_world_or_big_number(str_arg)
+    print('world_or_number')
+
+def last_world_or_big_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group_or_few_child('old_person_or_young_day')
---
+++ see_high_work_under_small_time/first_point_or_government/able_day_or_next_part/man/different_number_or_little_life/large_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_own_way(str_arg):
+    work(str_arg)
+    print('time')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_own_way('work')
