4c2609af83fe950d9d9474c894908c3199332893
Jingkai Tang <jingkaitang@gmail.com>
2015-01-30 22:07:58 +0800

    mv: say do_company_with_young_work/first_woman_or_different_year/old_fact.py up own case

---
+++ do_company_with_young_work/first_woman_or_different_year/old_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_last_case_about_small_time(str_arg):
+    young_woman_or_new_day(str_arg)
+    print('great_problem_or_hand')
+
+def young_woman_or_new_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_last_case_about_small_time('come_time_up_high_problem')
