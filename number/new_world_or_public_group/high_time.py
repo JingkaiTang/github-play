63b0c36be79a28579b42454f03810f2889968cfd
Jingkai Tang <jingkaitang@gmail.com>
2015-01-31 10:28:24 +0800

    mv: get number/new_world_or_public_group/high_time.py above great person

---
+++ number/new_world_or_public_group/high_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_public_company_at_group(str_arg):
+    world(str_arg)
+    print('little_week')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_public_company_at_group('same_way')
