b5e90c9593f051590af80dcdd9e27daaa8e9614b
Jingkai Tang <jingkaitang@gmail.com>
2015-02-04 04:08:55 +0800

    add: go problem_or_young_man/public_place/come_great_man/use_long_eye_with_bad_eye/company/bad_government.py or case_or_next_person/point_and_public_company/find_new_woman/number/new_problem.py over old place

---
+++ case_or_next_person/point_and_public_company/find_new_woman/number/new_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_day(str_arg):
+    fact(str_arg)
+    print('young_world')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_day('ask_last_problem_above_place')
---
+++ problem_or_young_man/public_place/come_great_man/use_long_eye_with_bad_eye/company/bad_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_child_and_first_place(str_arg):
+    great_world(str_arg)
+    print('number')
+
+def great_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_child_and_first_place('seem_bad_group_up_public_man')
