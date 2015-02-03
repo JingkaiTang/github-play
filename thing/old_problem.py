41e0c38e1b98da25f53f6c3e4bf4aa6ccd40b4bb
Jingkai Tang <jingkaitang@gmail.com>
2015-02-04 03:06:29 +0800

    rm: do thing/old_problem.py by next hand

---
+++ thing/old_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company_and_part(str_arg):
+    own_child_or_own_year(str_arg)
+    print('give_government')
+
+def own_child_or_own_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company_and_part('eye_and_eye')
