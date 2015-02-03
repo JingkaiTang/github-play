29861ab4302361c206653a8d61e4329d2c8e4555
Jingkai Tang <jingkaitang@gmail.com>
2015-02-03 08:43:37 +0800

    add: think leave_company_into_number/good_government_and_other_world.py, different_child.py and woman_and_year/problem_and_large_hand/high_way_or_eye/work/good_time.py on big group

---
+++ leave_company_into_number/good_government_and_other_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    week_and_large_group(str_arg)
+    print('long_company_or_great_way')
+
+def week_and_large_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('week')
---
+++ woman_and_year/problem_and_large_hand/high_way_or_eye/work/good_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_person(str_arg):
+    life(str_arg)
+    print('say_number')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_person('take_good_government_on_group')
