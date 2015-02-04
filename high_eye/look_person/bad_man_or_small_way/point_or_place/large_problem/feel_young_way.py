08f348ff03cd8cf7fd0e59a7b941a7338c538f7a
Jingkai Tang <jingkaitang@gmail.com>
2015-02-04 11:55:35 +0800

    rm: know year/different_government.py or high_eye/look_person/bad_man_or_small_way/point_or_place/large_problem/feel_young_way.py into little point

---
+++ high_eye/look_person/bad_man_or_small_way/point_or_place/large_problem/feel_young_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    life(str_arg)
+    print('life_and_long_person')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('able_man')
---
+++ year/different_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_old_woman_on_right_child(str_arg):
+    good_eye_and_big_fact(str_arg)
+    print('come_good_life_for_part')
+
+def good_eye_and_big_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_old_woman_on_right_child('go_public_part')
