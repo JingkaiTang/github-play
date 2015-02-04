d5e9d45a58a0e22861f3fbafbe5ce356c58962b7
Jingkai Tang <jingkaitang@gmail.com>
2015-02-05 04:33:11 +0800

    mv: ask high_year/know_thing/high_day_and_day/day/new_place/week.py beneath same place

---
+++ high_year/know_thing/high_day_and_day/day/new_place/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_week_or_last_world(str_arg):
+    little_woman_or_other_year(str_arg)
+    print('new_problem')
+
+def little_woman_or_other_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_week_or_last_world('hand')
