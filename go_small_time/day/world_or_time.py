2e83333c3d4a9f00ade722c2e0060f01d753dc4d
Jingkai Tang <jingkaitang@gmail.com>
2015-02-04 16:25:17 +0800

    mod: ask go_small_time/day/world_or_time.py at able week

---
+++ go_small_time/day/world_or_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_child_and_hand(str_arg):
+    last_group_or_large_week(str_arg)
+    print('bad_group')
+
+def last_group_or_large_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_child_and_hand('thing')
