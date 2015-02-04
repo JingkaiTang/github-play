72a8bb883358f7bfa3d1437ed05c740824d18fb8
Jingkai Tang <jingkaitang@gmail.com>
2015-02-04 13:17:47 +0800

    mod: do own_group_and_old_number/part/different_group.py about first world

---
+++ own_group_and_old_number/part/different_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_company(str_arg):
+    other_place(str_arg)
+    print('think_small_child_on_different_problem')
+
+def other_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_company('person_and_number')
