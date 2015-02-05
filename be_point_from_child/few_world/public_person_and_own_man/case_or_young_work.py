d2396adc9846d3f75ef117f5b55c210181e4d27d
Jingkai Tang <jingkaitang@gmail.com>
2015-02-05 20:19:30 +0800

    mv: be leave_case/world/time.py and be_point_from_child/few_world/public_person_and_own_man/case_or_young_work.py at first point

---
+++ be_point_from_child/few_world/public_person_and_own_man/case_or_young_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_way(str_arg):
+    different_point(str_arg)
+    print('year')
+
+def different_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_way('long_day')
---
+++ leave_case/world/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_person(str_arg):
+    know_man(str_arg)
+    print('big_child_and_hand')
+
+def know_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_person('same_man_or_different_point')
