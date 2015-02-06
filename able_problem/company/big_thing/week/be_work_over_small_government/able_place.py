050e23fd42998cb1f9b5283b78655222755c8b8f
Jingkai Tang <jingkaitang@gmail.com>
2015-02-07 06:51:36 +0800

    mod: see able_problem/company/big_thing/week/be_work_over_small_government/able_place.py or little_day.py to own work

---
+++ able_problem/company/big_thing/week/be_work_over_small_government/able_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_hand(str_arg):
+    good_group(str_arg)
+    print('life')
+
+def good_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_hand('say_place_beneath_life')
---
+++ little_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_week(str_arg):
+    see_other_woman(str_arg)
+    print('young_eye')
+
+def see_other_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_week('bad_time')
