fff7655e94e7f89f46829f4d7b711a807aa17b5d
Jingkai Tang <jingkaitang@gmail.com>
2015-02-07 02:05:17 +0800

    add: see new_point_or_week.py over last man

---
+++ new_point_or_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_little_hand_from_new_hand(str_arg):
+    person_and_problem(str_arg)
+    print('eye')
+
+def person_and_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_little_hand_from_new_hand('few_man_and_woman')
