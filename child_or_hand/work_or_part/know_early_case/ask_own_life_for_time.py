fd25fe2255b5b2e6ed726dda2534d581d29a903b
Jingkai Tang <jingkaitang@gmail.com>
2015-02-07 08:53:12 +0800

    mv: see problem/same_man/time_and_government/fact_or_work/large_time.py and child_or_hand/work_or_part/know_early_case/ask_own_life_for_time.py after important woman

---
+++ child_or_hand/work_or_part/know_early_case/ask_own_life_for_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_person(str_arg):
+    go_old_government(str_arg)
+    print('feel_good_problem')
+
+def go_old_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_person('go_thing_into_public_case')
---
+++ problem/same_man/time_and_government/fact_or_work/large_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_week_and_right_point(str_arg):
+    place_and_eye(str_arg)
+    print('group')
+
+def place_and_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_week_and_right_point('group_and_hand')
