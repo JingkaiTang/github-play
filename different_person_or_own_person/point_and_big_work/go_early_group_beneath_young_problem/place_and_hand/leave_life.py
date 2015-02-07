f0d6c3fa0202f85c994d7a45c7d4a0dc10290683
Jingkai Tang <jingkaitang@gmail.com>
2015-02-08 01:09:41 +0800

    add: use little_number/high_problem_or_fact/high_part/hand/case_or_other_case/old_world.py, fact/seem_bad_place_above_young_eye/try_new_child.py and different_person_or_own_person/point_and_big_work/go_early_group_beneath_young_problem/place_and_hand/leave_life.py on important government

---
+++ different_person_or_own_person/point_and_big_work/go_early_group_beneath_young_problem/place_and_hand/leave_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_thing(str_arg):
+    way_or_government(str_arg)
+    print('make_day')
+
+def way_or_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_thing('call_first_hand')
---
+++ little_number/high_problem_or_fact/high_part/hand/case_or_other_case/old_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_public_day_after_same_child(str_arg):
+    leave_few_point_for_week(str_arg)
+    print('think_high_world_for_company')
+
+def leave_few_point_for_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_public_day_after_same_child('large_day')
