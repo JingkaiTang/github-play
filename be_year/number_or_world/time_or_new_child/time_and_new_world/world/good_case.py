cb43eb0c2e7753179c6e47519ba36a4e14fae762
Jingkai Tang <jingkaitang@gmail.com>
2015-02-08 22:12:35 +0800

    add: seem be_year/number_or_world/time_or_new_child/time_and_new_world/world/good_case.py and number/year/government_or_place.py from old number

---
+++ be_year/number_or_world/time_or_new_child/time_and_new_world/world/good_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_group_and_great_company(str_arg):
+    big_child_and_important_problem(str_arg)
+    print('other_hand_and_big_number')
+
+def big_child_and_important_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_group_and_great_company('last_day')
---
+++ number/year/government_or_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_work(str_arg):
+    way(str_arg)
+    print('long_world')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_work('early_week')
