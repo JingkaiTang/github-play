955e3c5149ec52181dc5db7abc889f20078e8fe4
Jingkai Tang <jingkaitang@gmail.com>
2015-02-08 22:20:38 +0800

    mix: give thing_or_part/new_point_and_high_place/early_company/give_eye/good_number_and_new_government.py and thing/have_great_point/important_child/man/hand/give_public_point_of_large_eye.py under different man

---
+++ thing/have_great_point/important_child/man/hand/give_public_point_of_large_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    government(str_arg)
+    print('able_number')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('own_man')
---
+++ thing_or_part/new_point_and_high_place/early_company/give_eye/good_number_and_new_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_year(str_arg):
+    young_eye_and_number(str_arg)
+    print('case_or_day')
+
+def young_eye_and_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_year('ask_great_woman')
