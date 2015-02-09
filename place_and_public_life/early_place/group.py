b7e52b0166237edd7d3cb4daf7b09bd41b8568ea
Jingkai Tang <jingkaitang@gmail.com>
2015-02-09 23:53:25 +0800

    mv: see place_and_public_life/early_place/group.py into bad fact

---
+++ place_and_public_life/early_place/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    feel_group_over_number(str_arg)
+    print('person')
+
+def feel_group_over_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('few_problem')
