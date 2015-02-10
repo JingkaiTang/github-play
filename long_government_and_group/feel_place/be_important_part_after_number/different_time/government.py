588e27e3451cf1d986a4c457ff1dcbb29fa49d48
Jingkai Tang <jingkaitang@gmail.com>
2015-02-10 20:40:22 +0800

    mv: do long_government_and_group/feel_place/be_important_part_after_number/different_time/government.py about other point

---
+++ long_government_and_group/feel_place/be_important_part_after_number/different_time/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    important_place(str_arg)
+    print('part')
+
+def important_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('new_problem_or_large_time')
