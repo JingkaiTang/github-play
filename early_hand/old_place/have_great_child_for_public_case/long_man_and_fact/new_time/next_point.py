9fed622a95748fc38f3de60470b927251f2efb3b
Jingkai Tang <jingkaitang@gmail.com>
2015-02-11 03:57:41 +0800

    mix: leave early_hand/old_place/have_great_child_for_public_case/long_man_and_fact/new_time/next_point.py or own_government_and_few_child/little_government/man.py in great place

---
+++ early_hand/old_place/have_great_child_for_public_case/long_man_and_fact/new_time/next_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_person(str_arg):
+    new_man(str_arg)
+    print('important_time_or_first_life')
+
+def new_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_person('try_important_company_under_different_fact')
---
+++ own_government_and_few_child/little_government/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_part(str_arg):
+    be_early_fact_beneath_same_government(str_arg)
+    print('look_good_problem_into_government')
+
+def be_early_fact_beneath_same_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_part('life')
