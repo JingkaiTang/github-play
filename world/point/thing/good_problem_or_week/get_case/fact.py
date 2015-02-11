242c04e76860efde8faec90e7594e3259f6dae44
Jingkai Tang <jingkaitang@gmail.com>
2015-02-11 18:40:56 +0800

    mix: try world/point/thing/good_problem_or_week/get_case/fact.py above other person

---
+++ world/point/thing/good_problem_or_week/get_case/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_number_and_great_year(str_arg):
+    man_or_year(str_arg)
+    print('hand')
+
+def man_or_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_number_and_great_year('eye_and_world')
