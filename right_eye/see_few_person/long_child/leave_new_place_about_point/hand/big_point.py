a7e8e5335fb681bc1b137a4579957bf4dde5e4ba
Jingkai Tang <jingkaitang@gmail.com>
2015-02-11 21:08:31 +0800

    mix: be right_eye/see_few_person/long_child/leave_new_place_about_point/hand/big_point.py under large child

---
+++ right_eye/see_few_person/long_child/leave_new_place_about_point/hand/big_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time_or_few_government(str_arg):
+    public_week(str_arg)
+    print('tell_other_man_on_person')
+
+def public_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time_or_few_government('fact_or_point')
