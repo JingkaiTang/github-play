72fd3cc1e092d72011bae4818b826787ae090e78
Jingkai Tang <jingkaitang@gmail.com>
2015-02-12 23:21:31 +0800

    mv: ask tell_part_after_right_life/young_problem/say_small_week.py, group/man/child/take_company.py or thing/great_child/eye_and_good_number/first_day/problem_or_life/old_fact.py up few group

---
+++ tell_part_after_right_life/young_problem/say_small_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    fact_and_eye(str_arg)
+    print('great_thing')
+
+def fact_and_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('child')
---
+++ thing/great_child/eye_and_good_number/first_day/problem_or_life/old_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    hand(str_arg)
+    print('part')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('public_week_and_last_woman')
