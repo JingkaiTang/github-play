97b1ede352679a26cca2578004f85eb9d4a256ef
Jingkai Tang <jingkaitang@gmail.com>
2015-02-13 08:53:39 +0800

    rm: try come_way_with_last_child/make_large_government/life_and_few_day.py or eye_or_eye/other_eye/get_old_problem_by_early_way.py up other week

---
+++ come_way_with_last_child/make_large_government/life_and_few_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    fact(str_arg)
+    print('world')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('number')
---
+++ eye_or_eye/other_eye/get_old_problem_by_early_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part_and_great_world(str_arg):
+    work(str_arg)
+    print('company_and_last_way')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part_and_great_world('first_hand_and_world')
