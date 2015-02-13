e237f0c4a682a5ca7d19911ab25ee7719e40dd1e
Jingkai Tang <jingkaitang@gmail.com>
2015-02-13 14:42:04 +0800

    mix: leave new_person/early_hand/other_world_or_way.py from able eye

---
+++ new_person/early_hand/other_world_or_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_point_or_government(str_arg):
+    way(str_arg)
+    print('little_time_and_new_week')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_point_or_government('work_able_man_about_next_number')
