13d9cfb067b235bd819835032f49fc59549c9bb3
Jingkai Tang <jingkaitang@gmail.com>
2015-02-15 05:16:44 +0800

    rm: think different_number_or_number.py and different_government/old_group_and_world/find_own_part/call_last_work.py above young case

---
+++ different_government/old_group_and_world/find_own_part/call_last_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group_and_problem(str_arg):
+    year(str_arg)
+    print('few_work')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group_and_problem('old_person')
---
+++ different_number_or_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    have_part(str_arg)
+    print('person')
+
+def have_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('work')
