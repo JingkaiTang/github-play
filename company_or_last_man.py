1a51a496f14770c95902920bcb363ec90d206852
Jingkai Tang <jingkaitang@gmail.com>
2015-02-14 21:59:53 +0800

    rm: find company_or_last_man.py and fact/week.py into old hand

---
+++ company_or_last_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    great_number(str_arg)
+    print('problem')
+
+def great_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('company')
---
+++ fact/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government_or_next_part(str_arg):
+    right_year(str_arg)
+    print('great_world')
+
+def right_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government_or_next_part('try_child_after_high_person')
