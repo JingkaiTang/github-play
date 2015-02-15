fc7fbb2f2fea1e78bfd8fcb62d220b800aa787b4
Jingkai Tang <jingkaitang@gmail.com>
2015-02-15 14:17:11 +0800

    mv: try number/year/life.py and fact/last_child_and_long_person/have_important_work_into_company/ask_big_fact_about_group/fact/old_day_or_company.py by big eye

---
+++ fact/last_child_and_long_person/have_important_work_into_company/ask_big_fact_about_group/fact/old_day_or_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_way_or_small_day(str_arg):
+    fact(str_arg)
+    print('life')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_way_or_small_day('large_woman_and_point')
---
+++ number/year/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_old_life_over_world(str_arg):
+    number_and_good_number(str_arg)
+    print('little_way')
+
+def number_and_good_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_old_life_over_world('point')
