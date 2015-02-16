b2d2d1cff07a3211cbbb9c2ec7bc57e4c200462b
Jingkai Tang <jingkaitang@gmail.com>
2015-02-17 07:31:45 +0800

    rm: work point/young_work/have_public_number_up_eye/life.py at long year

---
+++ point/young_work/have_public_number_up_eye/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_number(str_arg):
+    small_hand(str_arg)
+    print('big_child')
+
+def small_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_number('government')
