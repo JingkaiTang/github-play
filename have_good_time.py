f9242d5343a0a8ed616d2231949e376cab0d89b8
Jingkai Tang <jingkaitang@gmail.com>
2015-02-17 21:52:47 +0800

    rm: have have_good_time.py or woman/place/right_number_and_point/eye/take_woman.py beneath early child

---
+++ have_good_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    get_first_thing_under_child(str_arg)
+    print('company_and_public_child')
+
+def get_first_thing_under_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('public_world')
---
+++ woman/place/right_number_and_point/eye/take_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    own_fact(str_arg)
+    print('young_part')
+
+def own_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('bad_part')
