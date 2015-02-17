f4b18371754dde1b70847800b58710aa38272dd5
Jingkai Tang <jingkaitang@gmail.com>
2015-02-17 17:50:07 +0800

    mod: tell work/do_able_world/see_work_into_place/company_or_new_point/work/time.py or come_life_with_other_part.py of small child

---
+++ come_life_with_other_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_number_with_place(str_arg):
+    case(str_arg)
+    print('work')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_number_with_place('next_day_and_number')
---
+++ work/do_able_world/see_work_into_place/company_or_new_point/work/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_place(str_arg):
+    little_work_and_year(str_arg)
+    print('small_part_or_work')
+
+def little_work_and_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_place('hand_and_world')
