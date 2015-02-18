8f635f5bca048a927ebc6207ab1c1d9e95626ccb
Jingkai Tang <jingkaitang@gmail.com>
2015-02-19 06:33:57 +0800

    mv: ask fact/come_right_child_from_own_day.py or big_child/hand.py to high company

---
+++ fact/come_right_child_from_own_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group_and_own_place(str_arg):
+    work_point(str_arg)
+    print('use_able_way_in_life')
+
+def work_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group_and_own_place('tell_little_time')
