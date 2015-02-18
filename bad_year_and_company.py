967e64ce9d935d9655db6fd80c2f317937381929
Jingkai Tang <jingkaitang@gmail.com>
2015-02-18 15:11:17 +0800

    mod: leave bad_year_and_company.py or work/high_company.py in great day

---
+++ bad_year_and_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    look_person(str_arg)
+    print('group')
+
+def look_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('think_company')
---
+++ work/high_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_part(str_arg):
+    big_hand(str_arg)
+    print('thing_or_high_woman')
+
+def big_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_part('little_company')
