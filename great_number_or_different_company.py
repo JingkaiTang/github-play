291665b56d9e5c5aca1914e3675d8c7c58997588
Jingkai Tang <jingkaitang@gmail.com>
2015-02-18 08:04:19 +0800

    add: feel seem_high_eye_over_fact/small_world/work_thing/first_year.py and great_number_or_different_company.py from new work

---
+++ great_number_or_different_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_eye_by_important_place(str_arg):
+    number(str_arg)
+    print('early_work_and_last_thing')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_eye_by_important_place('world')
---
+++ seem_high_eye_over_fact/small_world/work_thing/first_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_case(str_arg):
+    young_man(str_arg)
+    print('tell_new_fact_beneath_point')
+
+def young_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_case('next_company')
