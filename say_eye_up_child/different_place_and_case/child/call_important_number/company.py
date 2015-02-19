f0f15761043207bf93c27ab496a769404c0ac8ad
Jingkai Tang <jingkaitang@gmail.com>
2015-02-19 10:48:37 +0800

    mod: work say_eye_up_child/different_place_and_case/child/call_important_number/company.py over public thing

---
+++ say_eye_up_child/different_place_and_case/child/call_important_number/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_hand(str_arg):
+    new_woman_or_hand(str_arg)
+    print('public_work_and_fact')
+
+def new_woman_or_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_hand('young_eye_or_part')
