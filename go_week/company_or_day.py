ee27022a4e21416a7f7cc072359dc3c38b4471e1
Jingkai Tang <jingkaitang@gmail.com>
2015-02-20 01:17:59 +0800

    add: say go_week/company_or_day.py by right number

---
+++ go_week/company_or_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    big_group_or_early_group(str_arg)
+    print('way')
+
+def big_group_or_early_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('day')
