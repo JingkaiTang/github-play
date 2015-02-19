afa2cd2f294bd897087ee015749ba8f7526acdc3
Jingkai Tang <jingkaitang@gmail.com>
2015-02-19 13:53:27 +0800

    mv: tell long_hand/world.py and time/government/little_child_and_person/same_person_or_life.py for big man

---
+++ long_hand/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_eye(str_arg):
+    young_point_and_young_day(str_arg)
+    print('same_time')
+
+def young_point_and_young_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_eye('other_year')
---
+++ time/government/little_child_and_person/same_person_or_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_time_or_other_number(str_arg):
+    ask_problem(str_arg)
+    print('way')
+
+def ask_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_time_or_other_number('great_part')
