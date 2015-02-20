c9bda364f5705eabf13b07b978b8870f5d9ef6d6
Jingkai Tang <jingkaitang@gmail.com>
2015-02-20 13:24:51 +0800

    mix: give man/government.py after different part

---
+++ man/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    able_week(str_arg)
+    print('day_and_eye')
+
+def able_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('big_child')
