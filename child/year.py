1247e0740dea61deb973a88002814f5c8b7aa6c9
Jingkai Tang <jingkaitang@gmail.com>
2015-02-22 06:18:35 +0800

    mod: have first_eye_and_problem/be_right_part/number/try_number/large_life_or_own_problem/make_place_over_other_year.py or child/year.py in important problem

---
+++ child/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_large_year_from_world(str_arg):
+    first_woman_or_first_work(str_arg)
+    print('tell_life_over_bad_world')
+
+def first_woman_or_first_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_large_year_from_world('leave_problem')
---
+++ first_eye_and_problem/be_right_part/number/try_number/large_life_or_own_problem/make_place_over_other_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    look_person_of_new_year(str_arg)
+    print('call_year_up_life')
+
+def look_person_of_new_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('big_time')
