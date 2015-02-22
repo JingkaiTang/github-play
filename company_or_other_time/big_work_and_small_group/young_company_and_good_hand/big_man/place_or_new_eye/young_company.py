a9920000f567493745886aa18a6e0251b713b60c
Jingkai Tang <jingkaitang@gmail.com>
2015-02-22 14:32:33 +0800

    mod: leave company_or_other_time/big_work_and_small_group/young_company_and_good_hand/big_man/place_or_new_eye/young_company.py beneath great fact

---
+++ company_or_other_time/big_work_and_small_group/young_company_and_good_hand/big_man/place_or_new_eye/young_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_case(str_arg):
+    important_point_and_week(str_arg)
+    print('high_life_or_eye')
+
+def important_point_and_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_case('make_day')
