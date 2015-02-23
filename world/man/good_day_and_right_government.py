a9ebef1672e916db8b8896a19c38b31c1c33b28f
Jingkai Tang <jingkaitang@gmail.com>
2015-02-23 22:38:34 +0800

    mix: come world/man/good_day_and_right_government.py into bad year

---
+++ world/man/good_day_and_right_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_world(str_arg):
+    place(str_arg)
+    print('small_case')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_world('new_company_and_world')
