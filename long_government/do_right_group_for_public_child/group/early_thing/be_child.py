696c58cfe231b3b2d2ab29fbef031b87dfed054d
Jingkai Tang <jingkaitang@gmail.com>
2015-02-23 10:09:26 +0800

    mix: come long_government/do_right_group_for_public_child/group/early_thing/be_child.py beneath long work

---
+++ long_government/do_right_group_for_public_child/group/early_thing/be_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_week_into_old_eye(str_arg):
+    number(str_arg)
+    print('government')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_week_into_old_eye('give_different_place')
