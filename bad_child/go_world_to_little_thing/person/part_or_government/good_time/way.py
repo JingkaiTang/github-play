3eddbdf8fe5256cf207c8dd80b2e65c7a6e1a689
Jingkai Tang <jingkaitang@gmail.com>
2015-02-23 14:24:01 +0800

    rm: give bad_child/go_world_to_little_thing/person/part_or_government/good_time/way.py and life/large_week_or_number/young_company/day.py at good number

---
+++ bad_child/go_world_to_little_thing/person/part_or_government/good_time/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    company_and_year(str_arg)
+    print('good_place')
+
+def company_and_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('be_last_company_from_early_life')
---
+++ life/large_week_or_number/young_company/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_group_and_part(str_arg):
+    same_problem_and_other_hand(str_arg)
+    print('same_point')
+
+def same_problem_and_other_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_group_and_part('high_eye')
