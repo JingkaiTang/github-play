41abf998808eb0301706858eaa1e176dc98c0d7b
Jingkai Tang <jingkaitang@gmail.com>
2015-02-24 06:53:00 +0800

    add: take point_or_good_eye/few_group.py and make_eye_at_problem/time_or_last_man.py at other hand

---
+++ make_eye_at_problem/time_or_last_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_company(str_arg):
+    work_hand_above_way(str_arg)
+    print('able_week')
+
+def work_hand_above_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_company('call_young_person')
---
+++ point_or_good_eye/few_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_number_or_big_person(str_arg):
+    group(str_arg)
+    print('few_number_and_small_government')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_number_or_big_person('government')
