98c5aa93ed6029079b9987540ba57395ba443b78
Jingkai Tang <jingkaitang@gmail.com>
2015-02-24 23:42:30 +0800

    mv: work able_way_or_own_group/part.py or look_small_year/year_or_able_case/small_way_and_small_company/thing.py beneath other time

---
+++ able_way_or_own_group/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part_and_little_time(str_arg):
+    call_problem_after_life(str_arg)
+    print('have_large_point_from_day')
+
+def call_problem_after_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part_and_little_time('small_fact')
---
+++ look_small_year/year_or_able_case/small_way_and_small_company/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_government(str_arg):
+    same_work_or_week(str_arg)
+    print('want_long_man')
+
+def same_work_or_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_government('same_government_and_point')
