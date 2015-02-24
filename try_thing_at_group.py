204003305d6fa2890e35361ed1b5e43b1d81c39e
Jingkai Tang <jingkaitang@gmail.com>
2015-02-25 01:12:23 +0800

    mod: look try_thing_at_group.py or little_world/long_fact.py in small place

---
+++ try_thing_at_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_man(str_arg):
+    new_child(str_arg)
+    print('next_week_and_different_point')
+
+def new_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_man('find_year')
