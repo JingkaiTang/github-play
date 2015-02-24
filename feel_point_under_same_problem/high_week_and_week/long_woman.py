eae5b3890c615d97e200cdde2e1838891181a324
Jingkai Tang <jingkaitang@gmail.com>
2015-02-25 05:08:06 +0800

    mix: get thing/world/time_or_long_group/give_public_week/think_problem_by_new_woman.py and feel_point_under_same_problem/high_week_and_week/long_woman.py in great eye

---
+++ feel_point_under_same_problem/high_week_and_week/long_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    leave_great_eye_under_young_place(str_arg)
+    print('point')
+
+def leave_great_eye_under_young_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('person')
---
+++ thing/world/time_or_long_group/give_public_week/think_problem_by_new_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_life(str_arg):
+    different_company(str_arg)
+    print('government')
+
+def different_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_life('find_child_into_important_way')
