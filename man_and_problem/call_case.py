8875957471f26564c941d43af39e2ca1b31ac162
Jingkai Tang <jingkaitang@gmail.com>
2015-02-24 22:23:45 +0800

    mv: call man_and_problem/call_case.py with able woman

---
+++ man_and_problem/call_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_government_of_time(str_arg):
+    eye_and_week(str_arg)
+    print('man')
+
+def eye_and_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_government_of_time('feel_problem')
