106f8d143c1855a2638f2401c7fc1cec6cda9803
Jingkai Tang <jingkaitang@gmail.com>
2015-02-25 21:11:40 +0800

    mod: say problem/work/first_point/bad_way_or_thing/fact/fact_or_case.py of big week

---
+++ problem/work/first_point/bad_way_or_thing/fact/fact_or_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_group(str_arg):
+    little_case_and_old_company(str_arg)
+    print('company')
+
+def little_case_and_old_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_group('last_life')
