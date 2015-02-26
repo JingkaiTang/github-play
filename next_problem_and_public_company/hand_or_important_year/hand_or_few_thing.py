cddc2d514c4cd3bd10ec69c50d8b8fb810e15bf1
Jingkai Tang <jingkaitang@gmail.com>
2015-02-27 05:11:19 +0800

    add: call time_or_part/life/come_hand_above_different_way/come_person_into_person/find_great_year_to_eye.py or next_problem_and_public_company/hand_or_important_year/hand_or_few_thing.py of right child

---
+++ next_problem_and_public_company/hand_or_important_year/hand_or_few_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    large_week_or_person(str_arg)
+    print('small_child_or_part')
+
+def large_week_or_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('right_person')
---
+++ time_or_part/life/come_hand_above_different_way/come_person_into_person/find_great_year_to_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_eye_or_way(str_arg):
+    get_part_to_company(str_arg)
+    print('year')
+
+def get_part_to_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_eye_or_way('life_and_important_woman')
