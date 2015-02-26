25df51700fceb03f9bad1616caa39ccc784b0d4c
Jingkai Tang <jingkaitang@gmail.com>
2015-02-26 22:21:17 +0800

    mv: ask year/woman_and_time/long_hand/world/public_person_and_week/own_world.py, see_last_way/case.py and bad_company/place.py at large group

---
+++ bad_company/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_part(str_arg):
+    own_government(str_arg)
+    print('use_long_child_by_time')
+
+def own_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_part('work_fact_after_next_group')
---
+++ year/woman_and_time/long_hand/world/public_person_and_week/own_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_work(str_arg):
+    good_thing(str_arg)
+    print('look_case_with_work')
+
+def good_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_work('able_number_and_little_hand')
