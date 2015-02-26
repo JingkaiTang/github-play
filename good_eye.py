ad20d8e05bfe531886f51e37ded542ef45eb8e2d
Jingkai Tang <jingkaitang@gmail.com>
2015-02-27 05:58:21 +0800

    add: ask old_part/way/day_and_great_part/long_hand/little_week/hand.py, work/week/hand/government.py and good_eye.py after big way

---
+++ good_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_group(str_arg):
+    come_group(str_arg)
+    print('way_or_child')
+
+def come_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_group('ask_man_about_fact')
---
+++ old_part/way/day_and_great_part/long_hand/little_week/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_eye_over_little_person(str_arg):
+    make_number_on_important_thing(str_arg)
+    print('great_eye')
+
+def make_number_on_important_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_eye_over_little_person('seem_great_week')
