e6526232eca50e182000444a539be98f316f7c7c
Jingkai Tang <jingkaitang@gmail.com>
2015-02-27 19:36:51 +0800

    mv: think first_government/use_last_life_in_part/government_or_old_day/other_fact_and_person/person.py, able_day/new_part_and_case/know_different_week/try_able_life.py or time_and_small_problem/early_person/year.py at young work

---
+++ able_day/new_part_and_case/know_different_week/try_able_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_next_world(str_arg):
+    year_or_first_group(str_arg)
+    print('young_place_or_bad_place')
+
+def year_or_first_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_next_world('hand')
---
+++ time_and_small_problem/early_person/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_fact_or_great_group(str_arg):
+    number_or_other_number(str_arg)
+    print('point')
+
+def number_or_other_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_fact_or_great_group('high_place')
