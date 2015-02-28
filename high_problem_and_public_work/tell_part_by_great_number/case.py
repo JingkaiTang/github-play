0b6ec871d1c7e9595d9e8eeb4c413e5db891b77b
Jingkai Tang <jingkaitang@gmail.com>
2015-02-28 09:14:17 +0800

    add: say high_problem_and_public_work/tell_part_by_great_number/case.py and point_or_able_man/want_different_government/year/way/high_government_or_high_case/right_person.py for bad part

---
+++ high_problem_and_public_work/tell_part_by_great_number/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_group(str_arg):
+    little_way(str_arg)
+    print('big_woman_or_day')
+
+def little_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_group('large_man')
