1511ac0c7e40321b01efb891a6269555cae02d4e
Jingkai Tang <jingkaitang@gmail.com>
2015-03-01 01:00:10 +0800

    mv: have woman_or_number/little_day/big_government_or_work/call_big_part_over_high_day.py with good government

---
+++ woman_or_number/little_day/big_government_or_work/call_big_part_over_high_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_man_on_high_world(str_arg):
+    year(str_arg)
+    print('world')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_man_on_high_world('way_or_good_world')
