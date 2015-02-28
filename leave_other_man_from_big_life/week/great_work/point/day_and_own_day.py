163afe076ce7a5df1d1fb21b079f14c17d70fce5
Jingkai Tang <jingkaitang@gmail.com>
2015-03-01 04:08:39 +0800

    mod: look leave_other_man_from_big_life/week/great_work/point/day_and_own_day.py after little company

---
+++ leave_other_man_from_big_life/week/great_work/point/day_and_own_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_year(str_arg):
+    work_and_way(str_arg)
+    print('child_or_week')
+
+def work_and_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_year('tell_different_number')
