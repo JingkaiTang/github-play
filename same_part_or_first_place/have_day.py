bc04a184d1b432c7f2c790677fe98a650e62677e
Jingkai Tang <jingkaitang@gmail.com>
2015-03-01 11:25:28 +0800

    mix: say same_part_or_first_place/have_day.py or work_great_place_above_government/life_and_young_person.py in different government

---
+++ same_part_or_first_place/have_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact_and_large_eye(str_arg):
+    ask_bad_problem_after_life(str_arg)
+    print('hand_and_big_government')
+
+def ask_bad_problem_after_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact_and_large_eye('thing_and_different_way')
---
+++ work_great_place_above_government/life_and_young_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    company(str_arg)
+    print('little_government_and_public_man')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('own_company')
