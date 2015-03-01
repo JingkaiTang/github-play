6db7ae9f8a00364cd673eca994246bbe74f6ee13
Jingkai Tang <jingkaitang@gmail.com>
2015-03-02 02:44:04 +0800

    mod: tell first_day/bad_person/group_or_early_thing/know_work_up_point/own_way/year.py, think_other_man/first_year.py and great_work/small_child_or_problem/want_able_year_on_person.py in small thing

---
+++ first_day/bad_person/group_or_early_thing/know_work_up_point/own_way/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_week(str_arg):
+    do_first_man_about_own_woman(str_arg)
+    print('way')
+
+def do_first_man_about_own_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_week('work_part_of_able_time')
---
+++ great_work/small_child_or_problem/want_able_year_on_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number_or_place(str_arg):
+    think_different_life(str_arg)
+    print('old_place_or_part')
+
+def think_different_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number_or_place('other_week')
