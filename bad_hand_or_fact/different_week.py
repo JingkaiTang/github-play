d1d62f91f7ecd53bea268a3d4a15d424261f5e1f
Jingkai Tang <jingkaitang@gmail.com>
2015-03-01 21:13:56 +0800

    rm: seem try_part/week_and_other_person/same_person/work_and_high_day.py and bad_hand_or_fact/different_week.py from early eye

---
+++ bad_hand_or_fact/different_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_point(str_arg):
+    world(str_arg)
+    print('use_last_day_on_day')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_point('hand_and_life')
---
+++ try_part/week_and_other_person/same_person/work_and_high_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_week(str_arg):
+    call_part(str_arg)
+    print('call_small_year')
+
+def call_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_week('come_next_work_over_early_life')
