1699e0428d8843f6bafc245cef0a97af9ca81406
Jingkai Tang <jingkaitang@gmail.com>
2015-03-02 10:44:05 +0800

    mv: ask feel_eye/young_year.py in small thing

---
+++ feel_eye/young_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_work(str_arg):
+    be_life(str_arg)
+    print('use_long_number')
+
+def be_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_work('life')
