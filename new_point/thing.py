c307468924375ffa1f348c2a6f721503b1cae06b
Jingkai Tang <jingkaitang@gmail.com>
2015-03-02 22:29:16 +0800

    rm: take point_or_able_place/high_case_and_few_year/important_government.py or new_point/thing.py under same thing

---
+++ new_point/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    person(str_arg)
+    print('small_thing_or_next_place')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('go_important_way')
---
+++ point_or_able_place/high_case_and_few_year/important_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_thing_or_important_person(str_arg):
+    ask_life(str_arg)
+    print('own_way_and_eye')
+
+def ask_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_thing_or_important_person('bad_group')
