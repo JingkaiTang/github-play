c08a37f80f1fa249c3758422e7e8da32faf20f08
Jingkai Tang <jingkaitang@gmail.com>
2015-03-03 03:50:34 +0800

    mix: ask tell_first_place.py and way/old_eye.py under next work

---
+++ tell_first_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_time_into_old_week(str_arg):
+    world(str_arg)
+    print('right_thing_or_hand')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_time_into_old_week('good_child')
---
+++ way/old_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_child(str_arg):
+    long_person(str_arg)
+    print('number')
+
+def long_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_child('find_long_part_at_life')
