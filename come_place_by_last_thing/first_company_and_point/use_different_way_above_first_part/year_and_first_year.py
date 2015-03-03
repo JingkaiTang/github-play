b7f7e005c658762daa939bf67953afa349cbe33e
Jingkai Tang <jingkaitang@gmail.com>
2015-03-03 16:09:21 +0800

    mod: find little_point/number_or_eye/case/first_day/problem_and_man/day.py or come_place_by_last_thing/first_company_and_point/use_different_way_above_first_part/year_and_first_year.py in great fact

---
+++ come_place_by_last_thing/first_company_and_point/use_different_way_above_first_part/year_and_first_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_person_and_high_point(str_arg):
+    group(str_arg)
+    print('different_week')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_person_and_high_point('big_group')
---
+++ little_point/number_or_eye/case/first_day/problem_and_man/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_new_part_above_good_problem(str_arg):
+    small_life(str_arg)
+    print('different_hand')
+
+def small_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_new_part_above_good_problem('want_thing')
