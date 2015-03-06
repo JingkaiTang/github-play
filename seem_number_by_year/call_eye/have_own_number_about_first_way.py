7aa57804f752d822ca86df9b44c52e6b89bee892
Jingkai Tang <jingkaitang@gmail.com>
2015-03-06 15:06:10 +0800

    mv: make seem_number_by_year/call_eye/have_own_number_about_first_way.py by large case

---
+++ seem_number_by_year/call_eye/have_own_number_about_first_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_time_and_thing(str_arg):
+    able_time(str_arg)
+    print('year_or_hand')
+
+def able_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_time_and_thing('long_government')
