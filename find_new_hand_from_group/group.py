63a0400856e78ce9eb8d131f4fcec2f2154483c1
Jingkai Tang <jingkaitang@gmail.com>
2015-03-06 13:38:41 +0800

    mv: give find_new_hand_from_group/group.py after last case

---
+++ find_new_hand_from_group/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_life(str_arg):
+    part_or_bad_year(str_arg)
+    print('child')
+
+def part_or_bad_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_life('get_next_group_by_important_fact')
