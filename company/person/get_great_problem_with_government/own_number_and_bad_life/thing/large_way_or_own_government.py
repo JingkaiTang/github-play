de15b5b868271d438e5ab0f15f3eec1119f1f439
Jingkai Tang <jingkaitang@gmail.com>
2015-03-08 06:28:48 +0800

    rm: work company/person/get_great_problem_with_government/own_number_and_bad_life/thing/large_way_or_own_government.py by bad number

---
+++ company/person/get_great_problem_with_government/own_number_and_bad_life/thing/large_way_or_own_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_eye(str_arg):
+    great_group_and_high_time(str_arg)
+    print('case_or_few_year')
+
+def great_group_and_high_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_eye('group')
