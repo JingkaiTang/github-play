460cdc49a24f8d7f4fff950791542c78ccfb9f43
Jingkai Tang <jingkaitang@gmail.com>
2015-03-08 12:43:02 +0800

    add: be tell_man_with_big_point/right_problem_and_large_life/world.py for long person

---
+++ tell_man_with_big_point/right_problem_and_large_life/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    have_thing(str_arg)
+    print('problem')
+
+def have_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('eye_or_little_child')
