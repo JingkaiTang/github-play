20408e53ca73e7413f7b2d96208969b9b8225905
Jingkai Tang <jingkaitang@gmail.com>
2015-03-08 10:31:59 +0800

    mv: say year/place/first_week.py in old day

---
+++ year/place/first_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_year(str_arg):
+    person(str_arg)
+    print('government')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_year('public_part')
