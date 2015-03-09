73be4a65d461fa0c11f9a504ffe6082774ae4973
Jingkai Tang <jingkaitang@gmail.com>
2015-03-09 14:39:53 +0800

    rm: come long_way/few_woman/work_number_up_next_group/different_case_or_long_number/right_point/able_world.py into last problem

---
+++ long_way/few_woman/work_number_up_next_group/different_case_or_long_number/right_point/able_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child_and_woman(str_arg):
+    day_or_case(str_arg)
+    print('young_case_and_government')
+
+def day_or_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child_and_woman('find_bad_case')
