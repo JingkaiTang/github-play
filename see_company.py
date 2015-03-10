2751a89ca9542c89305bdea4b81766ebd7b9b58d
Jingkai Tang <jingkaitang@gmail.com>
2015-03-10 11:50:39 +0800

    mv: do see_company.py into little work

---
+++ see_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_way(str_arg):
+    think_time(str_arg)
+    print('hand')
+
+def think_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_way('tell_week')
