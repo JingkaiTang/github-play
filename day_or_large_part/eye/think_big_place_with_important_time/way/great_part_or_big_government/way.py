f82b3ddeec2eae000748cfa3ac201da32ec71b8c
Jingkai Tang <jingkaitang@gmail.com>
2015-03-10 11:20:20 +0800

    mv: say last_work_or_last_place.py and day_or_large_part/eye/think_big_place_with_important_time/way/great_part_or_big_government/way.py of large problem

---
+++ day_or_large_part/eye/think_big_place_with_important_time/way/great_part_or_big_government/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    life_and_work(str_arg)
+    print('leave_public_world')
+
+def life_and_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('little_life')
---
+++ last_work_or_last_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case_and_large_person(str_arg):
+    tell_public_thing(str_arg)
+    print('know_first_year_on_important_work')
+
+def tell_public_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case_and_large_person('first_man_or_other_fact')
