7229bfa2ae27a94e3d39b1157df8e77cbe4ad57f
Jingkai Tang <jingkaitang@gmail.com>
2015-03-10 10:09:35 +0800

    mv: do thing/point/feel_few_woman/few_place_and_eye/group/man_or_eye.py and big_point_or_thing/old_life/different_life/next_eye/other_problem_and_day/seem_large_week_after_good_week.py from few woman

---
+++ big_point_or_thing/old_life/different_life/next_eye/other_problem_and_day/seem_large_week_after_good_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    woman_and_man(str_arg)
+    print('company')
+
+def woman_and_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('problem_and_year')
---
+++ thing/point/feel_few_woman/few_place_and_eye/group/man_or_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_eye_under_problem(str_arg):
+    use_old_child_on_company(str_arg)
+    print('early_year')
+
+def use_old_child_on_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_eye_under_problem('try_next_hand')
