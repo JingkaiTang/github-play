b3e4ecb6635fcc33e40c9afb6bf9a93b11bcfd6c
Jingkai Tang <jingkaitang@gmail.com>
2015-03-10 08:29:51 +0800

    mv: find first_way_or_time/point.py up small eye

---
+++ first_way_or_time/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_case(str_arg):
+    same_work(str_arg)
+    print('hand')
+
+def same_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_case('be_part')
