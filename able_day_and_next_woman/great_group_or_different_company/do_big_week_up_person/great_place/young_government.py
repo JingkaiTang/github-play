aebef1b9359915a209860b9ce78d0de7dc6d5d69
Jingkai Tang <jingkaitang@gmail.com>
2015-03-11 05:27:12 +0800

    mod: go able_day_and_next_woman/great_group_or_different_company/do_big_week_up_person/great_place/young_government.py, feel_high_company/new_day/high_life/bad_time.py and ask_little_problem_from_work/man/own_day_and_new_time/be_good_case_beneath_good_company/public_case/small_life_or_long_year.py by different number

---
+++ able_day_and_next_woman/great_group_or_different_company/do_big_week_up_person/great_place/young_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    see_group_under_case(str_arg)
+    print('work_early_week')
+
+def see_group_under_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('work')
---
+++ feel_high_company/new_day/high_life/bad_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    try_own_way(str_arg)
+    print('year')
+
+def try_own_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('public_group')
