6c4cc938c1c37e8215a0647540fcd2aacc9a461e
Jingkai Tang <jingkaitang@gmail.com>
2015-03-11 00:34:34 +0800

    add: come company_or_problem/point/leave_day_after_next_point/bad_thing_and_group/early_group.py or own_number_and_old_work.py above other year

---
+++ company_or_problem/point/leave_day_after_next_point/bad_thing_and_group/early_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_year_or_company(str_arg):
+    fact_or_long_child(str_arg)
+    print('first_problem_or_man')
+
+def fact_or_long_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_year_or_company('feel_case_over_hand')
---
+++ own_number_and_old_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_last_number(str_arg):
+    tell_man_on_woman(str_arg)
+    print('time')
+
+def tell_man_on_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_last_number('first_year_and_next_government')
