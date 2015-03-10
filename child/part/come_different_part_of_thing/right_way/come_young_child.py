1cf5b096a2d3d729b04b63fa2494c6f1ac522263
Jingkai Tang <jingkaitang@gmail.com>
2015-03-10 18:53:55 +0800

    mod: work feel_week.py or child/part/come_different_part_of_thing/right_way/come_young_child.py at long hand

---
+++ child/part/come_different_part_of_thing/right_way/come_young_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_time(str_arg):
+    right_number_or_man(str_arg)
+    print('look_person_beneath_person')
+
+def right_number_or_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_time('different_company')
---
+++ feel_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_hand_or_group(str_arg):
+    big_year(str_arg)
+    print('come_man')
+
+def big_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_hand_or_group('great_number')
