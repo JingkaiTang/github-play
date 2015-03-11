415c0ec950c5781b86176d4681383f0a4ce619f4
Jingkai Tang <jingkaitang@gmail.com>
2015-03-11 13:47:40 +0800

    add: make hand/know_large_man/tell_person/public_woman/different_time.py about different thing

---
+++ hand/know_large_man/tell_person/public_woman/different_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_government_to_young_case(str_arg):
+    hand(str_arg)
+    print('think_way')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_government_to_young_case('different_part')
