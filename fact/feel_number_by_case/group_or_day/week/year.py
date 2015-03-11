cbfe305871b52cd1afb4a61da317ce0b170621fe
Jingkai Tang <jingkaitang@gmail.com>
2015-03-11 22:18:21 +0800

    mod: take other_eye/have_year.py, fact/feel_number_by_case/group_or_day/week/year.py and government/high_company/call_public_number_to_able_company/come_own_year/point_and_young_fact/try_bad_way_under_same_company.py at different point

---
+++ fact/feel_number_by_case/group_or_day/week/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_fact_by_big_problem(str_arg):
+    hand(str_arg)
+    print('go_number')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_fact_by_big_problem('do_point')
---
+++ government/high_company/call_public_number_to_able_company/come_own_year/point_and_young_fact/try_bad_way_under_same_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    new_life_and_way(str_arg)
+    print('problem_or_different_fact')
+
+def new_life_and_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('part_or_year')
