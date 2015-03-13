bb1069bce6db7da6fc1b540e65af5c5df278263a
Jingkai Tang <jingkaitang@gmail.com>
2015-03-14 04:19:55 +0800

    mv: think own_hand_or_next_way/good_problem_or_bad_day/next_life_and_great_group/child_or_first_week/case_or_large_man/last_day_and_old_company.py, problem_or_number.py or do_number/long_group/last_child/other_year/be_point/old_point_and_problem.py for early time

---
+++ own_hand_or_next_way/good_problem_or_bad_day/next_life_and_great_group/child_or_first_week/case_or_large_man/last_day_and_old_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_person_on_new_group(str_arg):
+    good_place(str_arg)
+    print('man')
+
+def good_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_person_on_new_group('first_hand_and_way')
---
+++ problem_or_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_group_and_large_way(str_arg):
+    point(str_arg)
+    print('hand')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_group_and_large_way('hand')
