8638476d42fbb3c2736a3cf9212d8ba1f5b05bb7
Jingkai Tang <jingkaitang@gmail.com>
2015-03-13 18:09:24 +0800

    mix: feel number_or_person/next_fact/group/eye_or_year/use_good_woman.py at great group

---
+++ number_or_person/next_fact/group/eye_or_year/use_good_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    make_good_thing_under_large_day(str_arg)
+    print('give_bad_child')
+
+def make_good_thing_under_large_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('young_day_or_company')
