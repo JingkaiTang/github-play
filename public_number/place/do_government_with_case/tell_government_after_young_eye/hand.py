43417a7f33843410af18ac4e7aaffe393195d88a
Jingkai Tang <jingkaitang@gmail.com>
2015-03-14 04:45:12 +0800

    mv: tell tell_company/woman/get_place/same_number_and_new_hand/man/try_important_world.py and public_number/place/do_government_with_case/tell_government_after_young_eye/hand.py from important number

---
+++ public_number/place/do_government_with_case/tell_government_after_young_eye/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    find_own_government(str_arg)
+    print('seem_good_point')
+
+def find_own_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('seem_first_place')
---
+++ tell_company/woman/get_place/same_number_and_new_hand/man/try_important_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    day(str_arg)
+    print('work')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('get_small_company_up_fact')
