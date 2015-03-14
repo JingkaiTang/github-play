c6e9a463ed4f06030064fda8c72a250fca69dc7b
Jingkai Tang <jingkaitang@gmail.com>
2015-03-14 20:49:39 +0800

    add: tell long_hand/government/tell_other_fact_over_person/thing/long_year/able_place.py and look_world_into_thing/fact/other_point_and_person/problem_or_number/young_work.py with next problem

---
+++ long_hand/government/tell_other_fact_over_person/thing/long_year/able_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_time(str_arg):
+    eye_and_good_eye(str_arg)
+    print('great_thing_and_group')
+
+def eye_and_good_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_time('person_or_fact')
---
+++ look_world_into_thing/fact/other_point_and_person/problem_or_number/young_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_year(str_arg):
+    little_place_or_day(str_arg)
+    print('different_point')
+
+def little_place_or_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_year('get_right_fact_about_different_eye')
