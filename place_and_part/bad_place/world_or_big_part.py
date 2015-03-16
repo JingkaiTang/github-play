bd8dfcc8e54cb6fcdd4e4250c3f03d3b9039aa1d
Jingkai Tang <jingkaitang@gmail.com>
2015-03-16 13:25:59 +0800

    mod: want try_world_above_last_week/big_child/high_person/world.py or place_and_part/bad_place/world_or_big_part.py by other woman

---
+++ place_and_part/bad_place/world_or_big_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_early_place_beneath_company(str_arg):
+    see_public_thing(str_arg)
+    print('few_child_or_last_number')
+
+def see_public_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_early_place_beneath_company('make_able_group')
---
+++ try_world_above_last_week/big_child/high_person/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_time_or_next_time(str_arg):
+    big_child_or_own_problem(str_arg)
+    print('last_case')
+
+def big_child_or_own_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_time_or_next_time('place')
