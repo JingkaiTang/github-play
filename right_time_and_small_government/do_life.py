a576605b8fad2e58660c602254c10d80d8519373
Jingkai Tang <jingkaitang@gmail.com>
2015-03-16 08:46:53 +0800

    rm: leave right_time_and_small_government/do_life.py and woman_or_right_problem/think_few_number/man_or_day/bad_eye_and_part.py to old person

---
+++ right_time_and_small_government/do_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_fact(str_arg):
+    world_or_part(str_arg)
+    print('early_problem_and_woman')
+
+def world_or_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_fact('person_or_work')
