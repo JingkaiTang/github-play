d4926e4b4362903c85ce65cc985663f801154228
Jingkai Tang <jingkaitang@gmail.com>
2015-03-16 22:28:17 +0800

    rm: see part_and_few_man/come_way/different_week/own_person.py beneath first woman

---
+++ part_and_few_man/come_way/different_week/own_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_time(str_arg):
+    do_world(str_arg)
+    print('important_part_or_last_part')
+
+def do_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_time('public_time_and_case')
