fba360eff0ca16125ce37723d5eb952154e394e7
Jingkai Tang <jingkaitang@gmail.com>
2015-03-16 23:00:17 +0800

    mod: say own_work_or_able_life/long_man/important_man_and_first_government/number/long_point/life.py and first_man_and_work/year_or_right_company/young_hand/have_problem.py to last week

---
+++ first_man_and_work/year_or_right_company/young_hand/have_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    come_old_point(str_arg)
+    print('public_hand_or_high_way')
+
+def come_old_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('eye')
---
+++ own_work_or_able_life/long_man/important_man_and_first_government/number/long_point/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_case_or_part(str_arg):
+    small_fact(str_arg)
+    print('number')
+
+def small_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_case_or_part('few_life')
