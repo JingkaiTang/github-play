f07cb623fbc7372d965a6269d459f64cf5adbb03
Jingkai Tang <jingkaitang@gmail.com>
2015-03-16 15:54:04 +0800

    rm: say part/public_year_or_bad_thing/bad_fact.py on small way

---
+++ part/public_year_or_bad_thing/bad_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_group(str_arg):
+    thing_or_young_person(str_arg)
+    print('find_young_government')
+
+def thing_or_young_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_group('use_fact_under_thing')
