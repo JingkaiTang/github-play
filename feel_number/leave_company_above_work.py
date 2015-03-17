f8200b9d95777bc7a3e58c45261af8b095e9b976
Jingkai Tang <jingkaitang@gmail.com>
2015-03-18 01:06:17 +0800

    mix: make feel_number/leave_company_above_work.py to high year

---
+++ feel_number/leave_company_above_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    first_problem(str_arg)
+    print('eye')
+
+def first_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('other_fact')
