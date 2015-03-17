ca20a4c2596c36eb2005b87afc54df1536afd1a1
Jingkai Tang <jingkaitang@gmail.com>
2015-03-17 19:58:05 +0800

    mod: find bad_case.py in little world

---
+++ bad_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_problem(str_arg):
+    problem_or_world(str_arg)
+    print('give_year')
+
+def problem_or_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_problem('hand')
