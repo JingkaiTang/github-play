cd3db4ae515e03ad6a7be93796f226f6f9d205da
Jingkai Tang <jingkaitang@gmail.com>
2015-03-18 12:51:59 +0800

    mix: try large_woman/last_year_and_woman/person.py at last thing

---
+++ large_woman/last_year_and_woman/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    other_case(str_arg)
+    print('next_eye')
+
+def other_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('number')
