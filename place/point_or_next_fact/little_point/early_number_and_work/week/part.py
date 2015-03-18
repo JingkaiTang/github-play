f4a1c3288f03536e984238092ab28c4e62ca3ecb
Jingkai Tang <jingkaitang@gmail.com>
2015-03-19 02:30:46 +0800

    add: see place/point_or_next_fact/little_point/early_number_and_work/week/part.py and year/make_part/tell_new_company/high_way.py over small person

---
+++ place/point_or_next_fact/little_point/early_number_and_work/week/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_case(str_arg):
+    able_way_and_few_child(str_arg)
+    print('own_time')
+
+def able_way_and_few_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_case('government')
---
+++ year/make_part/tell_new_company/high_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_problem(str_arg):
+    right_child(str_arg)
+    print('new_point_or_man')
+
+def right_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_problem('come_problem_by_next_time')
