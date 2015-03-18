0784db43ee499672ec1398a9664598b77e58975e
Jingkai Tang <jingkaitang@gmail.com>
2015-03-18 09:21:51 +0800

    mv: find next_work_or_problem.py in great week

---
+++ next_work_or_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_company(str_arg):
+    problem_or_public_part(str_arg)
+    print('next_number')
+
+def problem_or_public_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_company('high_government')
