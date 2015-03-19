865ae61496d09678536a0b4b78277ca950938650
Jingkai Tang <jingkaitang@gmail.com>
2015-03-20 04:04:15 +0800

    mod: go work/company_and_other_place/place/place_and_last_person.py and high_problem_and_part/able_day/come_new_person_over_way/point/large_life.py after new time

---
+++ high_problem_and_part/able_day/come_new_person_over_way/point/large_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_problem(str_arg):
+    small_group(str_arg)
+    print('public_work')
+
+def small_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_problem('point')
---
+++ work/company_and_other_place/place/place_and_last_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week_and_important_week(str_arg):
+    part(str_arg)
+    print('next_year')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week_and_important_week('young_year')
