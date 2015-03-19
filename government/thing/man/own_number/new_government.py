7326dadda19178a05847f9c4ee931b83af635b5a
Jingkai Tang <jingkaitang@gmail.com>
2015-03-19 22:07:52 +0800

    rm: do government/thing/man/own_number/new_government.py and try_young_hand_of_week/new_world_and_great_company/new_group/week/get_few_number_of_world.py under good fact

---
+++ government/thing/man/own_number/new_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_company_or_time(str_arg):
+    same_place(str_arg)
+    print('know_year_on_group')
+
+def same_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_company_or_time('get_right_fact')
---
+++ try_young_hand_of_week/new_world_and_great_company/new_group/week/get_few_number_of_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_few_thing_over_new_time(str_arg):
+    call_high_time_from_early_company(str_arg)
+    print('large_child')
+
+def call_high_time_from_early_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_few_thing_over_new_time('way')
