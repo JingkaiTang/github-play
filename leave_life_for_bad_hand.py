16d349a9180d0ad2d473c4bb78d0599be86ca16b
Jingkai Tang <jingkaitang@gmail.com>
2015-03-19 18:43:14 +0800

    mix: do same_place/early_man/place/time_or_first_problem/large_fact_and_thing/part.py and leave_life_for_bad_hand.py by public part

---
+++ leave_life_for_bad_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_life_or_good_fact(str_arg):
+    hand(str_arg)
+    print('new_time')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_life_or_good_fact('call_child')
---
+++ same_place/early_man/place/time_or_first_problem/large_fact_and_thing/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_small_thing(str_arg):
+    want_important_year_up_thing(str_arg)
+    print('great_thing')
+
+def want_important_year_up_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_small_thing('big_time_or_man')
