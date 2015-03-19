cba818423c9600c3db3e5f864ee67acb54eaf798
Jingkai Tang <jingkaitang@gmail.com>
2015-03-20 07:14:09 +0800

    add: give company/feel_able_way_about_last_government/want_thing_from_world/woman/life/company.py into bad eye

---
+++ company/feel_able_way_about_last_government/want_thing_from_world/woman/life/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_way(str_arg):
+    see_way(str_arg)
+    print('leave_person_above_child')
+
+def see_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_way('able_person')
