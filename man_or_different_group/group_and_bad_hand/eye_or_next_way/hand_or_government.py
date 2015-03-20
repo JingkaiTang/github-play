1171ab6bb3ad760f225a454d8421d2ee24dc350f
Jingkai Tang <jingkaitang@gmail.com>
2015-03-20 08:52:18 +0800

    mix: work man_or_different_group/group_and_bad_hand/eye_or_next_way/hand_or_government.py at public place

---
+++ man_or_different_group/group_and_bad_hand/eye_or_next_way/hand_or_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day_and_year(str_arg):
+    important_year(str_arg)
+    print('right_world')
+
+def important_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day_and_year('part')
