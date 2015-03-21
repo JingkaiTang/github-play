7766ee0937f053d993114f8d197e4386a05bd2a3
Jingkai Tang <jingkaitang@gmail.com>
2015-03-21 20:54:24 +0800

    rm: give look_great_problem_above_few_woman/small_group/seem_bad_case_beneath_place/government_or_person.py or same_government/able_government_and_early_thing.py with big year

---
+++ look_great_problem_above_few_woman/small_group/seem_bad_case_beneath_place/government_or_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_person_with_young_time(str_arg):
+    day(str_arg)
+    print('feel_public_place_on_person')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_person_with_young_time('first_hand_and_fact')
---
+++ same_government/able_government_and_early_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_hand_or_large_woman(str_arg):
+    call_world(str_arg)
+    print('day')
+
+def call_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_hand_or_large_woman('child_or_fact')
