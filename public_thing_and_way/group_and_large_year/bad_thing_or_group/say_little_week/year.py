490f5a43643b7796aa60b230551bc3a36bc292c7
Jingkai Tang <jingkaitang@gmail.com>
2015-03-22 09:17:45 +0800

    add: work public_thing_and_way/group_and_large_year/bad_thing_or_group/say_little_week/year.py above other eye

---
+++ public_thing_and_way/group_and_large_year/bad_thing_or_group/say_little_week/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_hand_beneath_number(str_arg):
+    long_fact(str_arg)
+    print('child')
+
+def long_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_hand_beneath_number('do_thing_for_good_problem')
