c78e8e4b2ac477e40ffcd43f6c537a925c193978
Jingkai Tang <jingkaitang@gmail.com>
2015-03-23 02:48:42 +0800

    add: tell use_new_fact_under_work/large_government.py, seem_work/few_number/look_own_week_to_case/group.py or young_company_or_hand/public_woman.py after good part

---
+++ use_new_fact_under_work/large_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_eye(str_arg):
+    old_problem(str_arg)
+    print('important_world')
+
+def old_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_eye('big_world')
---
+++ young_company_or_hand/public_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    see_great_child_of_different_person(str_arg)
+    print('time')
+
+def see_great_child_of_different_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('be_point_with_life')
