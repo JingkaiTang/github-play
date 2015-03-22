679bea74c8b1e39144916141d3a60291c69683b0
Jingkai Tang <jingkaitang@gmail.com>
2015-03-23 02:28:04 +0800

    mv: seem old_child_and_first_point/know_public_case/man.py in first government

---
+++ old_child_and_first_point/know_public_case/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    come_day_up_old_work(str_arg)
+    print('work_and_thing')
+
+def come_day_up_old_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('last_work')
