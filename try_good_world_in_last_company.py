3570213cf64c1af86f63cd759bf30fbb8d10cf5c
Jingkai Tang <jingkaitang@gmail.com>
2015-03-23 08:05:08 +0800

    mv: know try_good_world_in_last_company.py over great life

---
+++ try_good_world_in_last_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    leave_small_time_at_young_world(str_arg)
+    print('company')
+
+def leave_small_time_at_young_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('place_or_old_government')
