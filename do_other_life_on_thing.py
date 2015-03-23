e49ba92f3523e518c96dde72f1a018f41d2a0c9b
Jingkai Tang <jingkaitang@gmail.com>
2015-03-24 03:52:18 +0800

    rm: come seem_problem/get_life.py and do_other_life_on_thing.py of high year

---
+++ do_other_life_on_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    government(str_arg)
+    print('few_case')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('thing_and_way')
