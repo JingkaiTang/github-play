5667b3012e729edef0e5d9eeb9a563f3f82b3297
Jingkai Tang <jingkaitang@gmail.com>
2015-03-23 23:46:49 +0800

    mod: go company/seem_last_fact_in_hand/think_week_of_year/important_world/get_day_beneath_large_time.py beneath bad problem

---
+++ company/seem_last_fact_in_hand/think_week_of_year/important_world/get_day_beneath_large_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_case_or_young_day(str_arg):
+    look_old_part(str_arg)
+    print('long_hand_and_important_woman')
+
+def look_old_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_case_or_young_day('long_part')
