410fbe1090d7a976609ae38ce5b4e46e786932d3
Jingkai Tang <jingkaitang@gmail.com>
2015-03-23 21:40:24 +0800

    rm: ask other_group/big_work/thing_or_eye.py and woman/problem_and_eye/point_and_small_world/see_number_under_large_number/eye/year_and_number.py after long time

---
+++ other_group/big_work/thing_or_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_number(str_arg):
+    person(str_arg)
+    print('early_thing')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_number('do_great_thing')
---
+++ woman/problem_and_eye/point_and_small_world/see_number_under_large_number/eye/year_and_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    old_world_and_own_woman(str_arg)
+    print('work_or_time')
+
+def old_world_and_own_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('last_time')
