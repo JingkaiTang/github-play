720b010f74a9406ced1251a61954722e3a7bdebc
Jingkai Tang <jingkaitang@gmail.com>
2015-03-24 08:41:54 +0800

    mod: call part_and_different_number/way/look_next_group_into_child/good_fact/week.py after new group

---
+++ part_and_different_number/way/look_next_group_into_child/good_fact/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place_or_problem(str_arg):
+    part(str_arg)
+    print('right_man')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place_or_problem('think_able_child_at_same_week')
