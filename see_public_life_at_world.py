e3b3183b6309e2a558f7f46a72a398c7d4e54b89
Jingkai Tang <jingkaitang@gmail.com>
2015-03-24 11:21:11 +0800

    mod: think see_public_life_at_world.py beneath own child

---
+++ see_public_life_at_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    early_case_and_place(str_arg)
+    print('take_different_man_up_company')
+
+def early_case_and_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('early_week')
