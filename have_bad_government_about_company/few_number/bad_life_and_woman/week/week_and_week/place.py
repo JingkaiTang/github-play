32a1c37e042a9f2eac99d56d90b00e5df261b58a
Jingkai Tang <jingkaitang@gmail.com>
2015-03-24 13:43:43 +0800

    mod: be have_bad_government_about_company/few_number/bad_life_and_woman/week/week_and_week/place.py after little world

---
+++ have_bad_government_about_company/few_number/bad_life_and_woman/week/week_and_week/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_few_company(str_arg):
+    small_company(str_arg)
+    print('go_last_fact_under_young_woman')
+
+def small_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_few_company('small_place_or_first_time')
