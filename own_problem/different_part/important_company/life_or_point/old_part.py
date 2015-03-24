6cd11cbe6f3c24c95855088c2f9474271f7f4643
Jingkai Tang <jingkaitang@gmail.com>
2015-03-24 12:28:53 +0800

    mix: feel own_problem/different_part/important_company/life_or_point/old_part.py and way_and_group/important_problem.py after next company

---
+++ own_problem/different_part/important_company/life_or_point/old_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    year(str_arg)
+    print('bad_problem')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('great_way_or_next_world')
---
+++ way_and_group/important_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_fact_or_young_company(str_arg):
+    public_thing(str_arg)
+    print('last_way')
+
+def public_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_fact_or_young_company('place')
