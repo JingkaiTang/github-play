425f5fb4757cf699d5160190a4c130dcd6cbca93
Jingkai Tang <jingkaitang@gmail.com>
2015-03-27 04:32:28 +0800

    rm: come week/hand/able_work/day_and_able_eye/other_woman_or_number.py or hand/work.py on old person

---
+++ hand/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    old_case(str_arg)
+    print('small_number')
+
+def old_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('high_part')
---
+++ week/hand/able_work/day_and_able_eye/other_woman_or_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_company_at_first_fact(str_arg):
+    life_and_able_number(str_arg)
+    print('other_point')
+
+def life_and_able_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_company_at_first_fact('give_young_group_for_number')
