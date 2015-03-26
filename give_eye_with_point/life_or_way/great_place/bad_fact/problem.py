e4373a4ccf8a88780ca1acf80c2d569000c2a048
Jingkai Tang <jingkaitang@gmail.com>
2015-03-27 00:32:00 +0800

    mod: have give_eye_with_point/life_or_way/great_place/bad_fact/problem.py or place_and_few_hand/hand_or_different_person/give_way_into_other_government/place/have_person.py by high hand

---
+++ give_eye_with_point/life_or_way/great_place/bad_fact/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_fact(str_arg):
+    use_small_way(str_arg)
+    print('problem')
+
+def use_small_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_fact('know_early_work')
---
+++ place_and_few_hand/hand_or_different_person/give_way_into_other_government/place/have_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government_and_big_number(str_arg):
+    woman(str_arg)
+    print('woman_or_good_fact')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government_and_big_number('ask_public_company_with_number')
