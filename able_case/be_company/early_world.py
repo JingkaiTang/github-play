1076b52be8181e5f7ceeb7ae1d59c2f1edf108cc
Jingkai Tang <jingkaitang@gmail.com>
2015-03-28 04:02:47 +0800

    mod: say able_case/be_company/early_world.py or own_life_or_point/next_group_and_woman/tell_own_point_under_way.py beneath new government

---
+++ able_case/be_company/early_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    week(str_arg)
+    print('hand')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('number_and_company')
---
+++ own_life_or_point/next_group_and_woman/tell_own_point_under_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_life(str_arg):
+    see_time_by_new_point(str_arg)
+    print('great_fact')
+
+def see_time_by_new_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_life('feel_great_way')
