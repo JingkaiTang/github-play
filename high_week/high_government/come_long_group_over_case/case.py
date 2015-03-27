388d875ab7c1e8c289a5352af6552ed2c6394d5e
Jingkai Tang <jingkaitang@gmail.com>
2015-03-28 02:58:42 +0800

    mix: use high_week/high_government/come_long_group_over_case/case.py into last life

---
+++ high_week/high_government/come_long_group_over_case/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_child(str_arg):
+    last_woman_or_little_child(str_arg)
+    print('do_important_week_by_last_person')
+
+def last_woman_or_little_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_child('little_group')
