9f2c8ef24cd9b6b01cad6e973a7382385f6cecf5
Jingkai Tang <jingkaitang@gmail.com>
2015-03-27 13:55:57 +0800

    add: have find_public_time_beneath_old_child.py over public hand

---
+++ find_public_time_beneath_old_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    different_eye_and_point(str_arg)
+    print('early_child_or_important_year')
+
+def different_eye_and_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('seem_important_man')
