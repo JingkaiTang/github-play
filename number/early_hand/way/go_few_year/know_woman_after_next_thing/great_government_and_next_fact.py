8066e71dc13fe3619e07e7500575feea9f5a26e8
Jingkai Tang <jingkaitang@gmail.com>
2015-03-28 20:08:18 +0800

    mix: look number/early_hand/way/go_few_year/know_woman_after_next_thing/great_government_and_next_fact.py or thing_and_child/day_and_new_work/time/part_or_eye/person/long_point_or_group.py by important time

---
+++ number/early_hand/way/go_few_year/know_woman_after_next_thing/great_government_and_next_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life_and_day(str_arg):
+    use_high_woman(str_arg)
+    print('thing_or_next_woman')
+
+def use_high_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life_and_day('want_last_way')
---
+++ thing_and_child/day_and_new_work/time/part_or_eye/person/long_point_or_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_problem_by_same_thing(str_arg):
+    time(str_arg)
+    print('work_last_woman_with_first_problem')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_problem_by_same_thing('group')
