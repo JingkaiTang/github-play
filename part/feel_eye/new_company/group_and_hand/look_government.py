bd8a10036e3e0aa37a0c47c0c14010592fece7c8
Jingkai Tang <jingkaitang@gmail.com>
2015-03-28 16:36:01 +0800

    mod: leave same_woman/little_fact_or_last_number/year_or_work/leave_large_eye_to_time/ask_thing/old_point_or_big_place.py and part/feel_eye/new_company/group_and_hand/look_government.py under public week

---
+++ part/feel_eye/new_company/group_and_hand/look_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_great_point(str_arg):
+    world(str_arg)
+    print('old_case_or_different_person')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_great_point('call_same_case_beneath_work')
---
+++ same_woman/little_fact_or_last_number/year_or_work/leave_large_eye_to_time/ask_thing/old_point_or_big_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_early_week_in_different_year(str_arg):
+    public_eye_or_world(str_arg)
+    print('last_day')
+
+def public_eye_or_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_early_week_in_different_year('go_world_beneath_time')
