843821833009fd6138d0e5f948b3265b972fb57a
Jingkai Tang <jingkaitang@gmail.com>
2015-03-29 07:40:03 +0800

    mv: see feel_old_problem/give_world_beneath_first_place/part_or_other_problem.py and think_year/make_able_way/fact_or_work/try_year/company_or_last_case.py under new time

---
+++ feel_old_problem/give_world_beneath_first_place/part_or_other_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    give_own_thing_of_thing(str_arg)
+    print('week')
+
+def give_own_thing_of_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('way')
---
+++ think_year/make_able_way/fact_or_work/try_year/company_or_last_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    right_company_or_work(str_arg)
+    print('know_large_life_under_few_time')
+
+def right_company_or_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('small_part')
