c69b9ef46e2a6277d871c5e4cf2ddabcea9134bc
Jingkai Tang <jingkaitang@gmail.com>
2015-03-28 10:52:26 +0800

    mv: leave child_or_small_hand.py or child.py with big man

--- child.py
+++ child.py
@@ -1,12 +1,12 @@
 
 #! /usr/bin/env python
 
-def make_part_about_hand(str_arg):
-    want_small_thing(str_arg)
-    print('large_work_or_thing')
+def use_child_up_different_child(str_arg):
+    case_and_important_number(str_arg)
+    print('man')
 
-def want_small_thing(str_arg):
+def case_and_important_number(str_arg):
     print(str_arg)
 
 if __name__ == '__main__':
-    make_part_about_hand('long_company_or_fact')
+    use_child_up_different_child('public_year')
---
+++ child_or_small_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_government(str_arg):
+    point(str_arg)
+    print('other_thing_and_year')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_government('woman_or_place')
