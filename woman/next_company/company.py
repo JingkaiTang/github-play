054ce9ff553163d3c77a6ab327092b7a0f1faa0c
Jingkai Tang <jingkaitang@gmail.com>
2015-04-03 00:24:54 +0800

    mod: try woman/next_company/company.py into few problem

---
+++ woman/next_company/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_world(str_arg):
+    few_world(str_arg)
+    print('work_man')
+
+def few_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_world('right_world')
