ac6f8340ebf882b5e825cdd3329c84a9a1652ce4
Jingkai Tang <jingkaitang@gmail.com>
2015-04-03 02:20:09 +0800

    mix: look want_right_place_of_young_person/few_point_and_bad_time.py and life/point/right_part.py after old year

---
+++ life/point/right_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_child_and_good_time(str_arg):
+    company_or_child(str_arg)
+    print('next_day_and_problem')
+
+def company_or_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_child_and_good_time('young_week')
---
+++ want_right_place_of_young_person/few_point_and_bad_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_woman(str_arg):
+    same_point(str_arg)
+    print('long_place')
+
+def same_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_woman('find_few_day')
