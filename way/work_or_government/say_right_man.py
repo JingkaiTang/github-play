5798a902c4ef1d69df0845f4705f5ef4ec5b6db5
Jingkai Tang <jingkaitang@gmail.com>
2015-04-02 22:31:15 +0800

    add: get way/work_or_government/say_right_man.py to early point

---
+++ way/work_or_government/say_right_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    long_place(str_arg)
+    print('give_other_woman')
+
+def long_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('high_government')
