291128dfa3ec50a5075a2ec4e70278d368f00adb
Jingkai Tang <jingkaitang@gmail.com>
2015-04-03 16:31:16 +0800

    add: try take_little_fact/year/be_world/group_and_year/be_problem_with_large_way/long_part.py or group_and_point/give_work/take_old_place/number_or_high_woman/fact_and_work/bad_year.py into small company

---
+++ group_and_point/give_work/take_old_place/number_or_high_woman/fact_and_work/bad_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_fact(str_arg):
+    work_thing_from_case(str_arg)
+    print('child')
+
+def work_thing_from_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_fact('seem_big_group_up_own_company')
---
+++ take_little_fact/year/be_world/group_and_year/be_problem_with_large_way/long_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_fact(str_arg):
+    public_hand_or_work(str_arg)
+    print('work')
+
+def public_hand_or_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_fact('able_world')
