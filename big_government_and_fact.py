2f617a215e0cd0ca39c9c1944819da0cd3aed3d3
Jingkai Tang <jingkaitang@gmail.com>
2015-04-04 09:08:29 +0800

    add: ask big_government_and_fact.py by new eye

---
+++ big_government_and_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    way(str_arg)
+    print('ask_high_day_under_bad_way')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('week_and_life')
