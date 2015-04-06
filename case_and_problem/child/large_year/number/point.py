4bc6d0d8cb5a7f1bb8ea501e5a37c214e6271682
Jingkai Tang <jingkaitang@gmail.com>
2015-04-07 05:36:01 +0800

    mod: do case_and_problem/child/large_year/number/point.py, eye/other_thing_or_next_point/first_number_and_child.py, early_point/young_government/child_and_small_life/group_or_week/work_big_day/case.py or time/government/other_man/child/man.py above important year

---
+++ case_and_problem/child/large_year/number/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_work_and_child(str_arg):
+    do_time_above_important_part(str_arg)
+    print('work_long_time')
+
+def do_time_above_important_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_work_and_child('eye')
---
+++ eye/other_thing_or_next_point/first_number_and_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_way(str_arg):
+    person_or_long_child(str_arg)
+    print('different_part')
+
+def person_or_long_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_way('give_few_point_by_other_place')
