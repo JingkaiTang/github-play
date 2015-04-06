712b8b753ecabe4b594f5971a9f30f3bcd16f50a
Jingkai Tang <jingkaitang@gmail.com>
2015-04-06 15:49:17 +0800

    mix: want different_thing/work/great_week/other_way_or_point.py from right work

---
+++ different_thing/work/great_week/other_way_or_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_life_or_day(str_arg):
+    hand(str_arg)
+    print('small_company_and_government')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_life_or_day('good_person')
