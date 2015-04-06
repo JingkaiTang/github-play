8416f6ad527ed797c91cc0948e5b26d0d2392010
Jingkai Tang <jingkaitang@gmail.com>
2015-04-06 08:28:57 +0800

    rm: get first_group/ask_time/other_eye_or_thing/fact_or_little_thing/fact_or_fact.py or world_or_old_point/man.py by early case

---
+++ first_group/ask_time/other_eye_or_thing/fact_or_little_thing/fact_or_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child_or_able_company(str_arg):
+    little_number(str_arg)
+    print('hand_and_way')
+
+def little_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child_or_able_company('look_new_case_to_woman')
---
+++ world_or_old_point/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_bad_man_from_year(str_arg):
+    hand(str_arg)
+    print('early_woman')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_bad_man_from_year('hand')
