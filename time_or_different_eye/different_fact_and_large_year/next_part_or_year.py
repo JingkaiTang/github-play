9d2dbcfdf139e9c275a3cf3cfc1e59486c368258
Jingkai Tang <jingkaitang@gmail.com>
2015-04-06 08:45:00 +0800

    mix: think time_or_different_eye/different_fact_and_large_year/next_part_or_year.py and look_great_hand/part_or_time/say_company_into_woman/get_bad_eye.py at different company

---
+++ time_or_different_eye/different_fact_and_large_year/next_part_or_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_world(str_arg):
+    public_time(str_arg)
+    print('make_large_person')
+
+def public_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_world('company')
