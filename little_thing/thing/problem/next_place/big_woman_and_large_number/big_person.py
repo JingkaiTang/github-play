c283458588e304f3133d3c46afee65b1d8b3905c
Jingkai Tang <jingkaitang@gmail.com>
2015-04-06 12:43:18 +0800

    add: look little_thing/thing/problem/next_place/big_woman_and_large_number/big_person.py with small time

---
+++ little_thing/thing/problem/next_place/big_woman_and_large_number/big_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_big_work_after_way(str_arg):
+    have_great_world_in_way(str_arg)
+    print('important_group')
+
+def have_great_world_in_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_big_work_after_way('last_work')
