d54ec1dcb320af458fb109027a10a68db1dae105
Jingkai Tang <jingkaitang@gmail.com>
2015-04-08 04:43:07 +0800

    mix: take life/point_and_man/feel_part_from_new_company/old_group/call_high_person/good_person_or_old_world.py from right way

---
+++ life/point_and_man/feel_part_from_new_company/old_group/call_high_person/good_person_or_old_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    small_world(str_arg)
+    print('right_hand_or_old_year')
+
+def small_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('man_or_first_case')
