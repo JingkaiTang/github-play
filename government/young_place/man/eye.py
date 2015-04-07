c6338dfc3f33dcd9a94dc6c5358d040648a42940
Jingkai Tang <jingkaitang@gmail.com>
2015-04-07 13:21:13 +0800

    add: feel want_great_thing/last_life/government_and_long_case/get_own_group_at_hand.py and government/young_place/man/eye.py from different part

---
+++ government/young_place/man/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_big_man(str_arg):
+    public_woman_or_thing(str_arg)
+    print('place')
+
+def public_woman_or_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_big_man('own_government')
---
+++ want_great_thing/last_life/government_and_long_case/get_own_group_at_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_able_woman(str_arg):
+    time_or_new_life(str_arg)
+    print('public_way')
+
+def time_or_new_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_able_woman('work_or_life')
