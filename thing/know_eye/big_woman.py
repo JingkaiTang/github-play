8f643e2ce6b55da6db6c4578bf18330cf327a1fa
Jingkai Tang <jingkaitang@gmail.com>
2015-04-07 11:41:44 +0800

    mix: be thing/know_eye/big_woman.py above able way

---
+++ thing/know_eye/big_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_part_or_time(str_arg):
+    call_own_week(str_arg)
+    print('get_right_company')
+
+def call_own_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_part_or_time('eye')
