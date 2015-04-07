9b04740b434bae7a349c1c10b05ab8fba8e6fc69
Jingkai Tang <jingkaitang@gmail.com>
2015-04-07 13:57:24 +0800

    mix: think do_great_time/large_day_or_world/time/case/large_person.py beneath old day

---
+++ do_great_time/large_day_or_world/time/case/large_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_part(str_arg):
+    take_same_person_over_different_point(str_arg)
+    print('place')
+
+def take_same_person_over_different_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_part('important_hand_or_big_case')
