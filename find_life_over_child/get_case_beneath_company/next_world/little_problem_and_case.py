c1f6b9a3f477db82c65a4fb86280c5631de05985
Jingkai Tang <jingkaitang@gmail.com>
2015-04-08 16:12:36 +0800

    rm: do find_life_over_child/get_case_beneath_company/next_world/little_problem_and_case.py in new man

---
+++ find_life_over_child/get_case_beneath_company/next_world/little_problem_and_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    great_way_or_other_company(str_arg)
+    print('right_week')
+
+def great_way_or_other_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('try_eye_after_few_time')
