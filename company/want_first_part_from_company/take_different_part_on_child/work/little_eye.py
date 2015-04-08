50bb3ea4fd29cd07a542fb74ce70fdae61442846
Jingkai Tang <jingkaitang@gmail.com>
2015-04-08 09:06:13 +0800

    mv: think company/want_first_part_from_company/take_different_part_on_child/work/little_eye.py into new day

---
+++ company/want_first_part_from_company/take_different_part_on_child/work/little_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year_and_first_week(str_arg):
+    different_thing(str_arg)
+    print('try_first_woman_over_week')
+
+def different_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year_and_first_week('first_person_or_day')
