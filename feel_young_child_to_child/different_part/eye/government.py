99c93ca546eca8955435788ad439f79552ce91af
Jingkai Tang <jingkaitang@gmail.com>
2015-04-11 02:39:56 +0800

    mix: call feel_young_child_to_child/different_part/eye/government.py and get_different_time/number_and_big_place.py up public thing

---
+++ feel_young_child_to_child/different_part/eye/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time_or_new_woman(str_arg):
+    time_and_long_day(str_arg)
+    print('thing')
+
+def time_and_long_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time_or_new_woman('long_problem_or_life')
---
+++ get_different_time/number_and_big_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    government_and_thing(str_arg)
+    print('important_person')
+
+def government_and_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('own_group')
