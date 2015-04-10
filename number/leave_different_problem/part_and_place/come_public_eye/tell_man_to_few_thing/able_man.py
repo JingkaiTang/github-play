e88afc673f60bcf3ff9d0b7b3af672600f6d94c3
Jingkai Tang <jingkaitang@gmail.com>
2015-04-10 18:51:50 +0800

    mv: take number/leave_different_problem/part_and_place/come_public_eye/tell_man_to_few_thing/able_man.py to different point

---
+++ number/leave_different_problem/part_and_place/come_public_eye/tell_man_to_few_thing/able_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    world_and_case(str_arg)
+    print('feel_fact')
+
+def world_and_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('life')
