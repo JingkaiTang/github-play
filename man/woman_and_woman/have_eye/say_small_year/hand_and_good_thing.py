998c64d1e144ecd0fd7e99ad96a6f095a6c90ab6
Jingkai Tang <jingkaitang@gmail.com>
2015-04-10 16:08:26 +0800

    rm: look man/woman_and_woman/have_eye/say_small_year/hand_and_good_thing.py for young child

---
+++ man/woman_and_woman/have_eye/say_small_year/hand_and_good_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_time(str_arg):
+    come_problem_on_part(str_arg)
+    print('right_government_or_eye')
+
+def come_problem_on_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_time('public_eye')
