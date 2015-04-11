05f82bd039a1e03864269f12f84ee6521ab72fe9
Jingkai Tang <jingkaitang@gmail.com>
2015-04-12 06:36:51 +0800

    mv: call little_problem/new_problem/child/high_day.py or long_hand/week/feel_big_problem_with_day.py under same problem

---
+++ little_problem/new_problem/child/high_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye_and_number(str_arg):
+    small_life_and_little_day(str_arg)
+    print('great_group')
+
+def small_life_and_little_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye_and_number('number')
---
+++ long_hand/week/feel_big_problem_with_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_work(str_arg):
+    life(str_arg)
+    print('eye_and_government')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_work('ask_long_week')
