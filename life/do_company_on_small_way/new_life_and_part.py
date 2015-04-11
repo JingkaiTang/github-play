14e4524153fbfee328945d3e3d7e0b70054a1be9
Jingkai Tang <jingkaitang@gmail.com>
2015-04-12 00:35:09 +0800

    mix: leave world/small_fact/look_public_eye.py or life/do_company_on_small_way/new_life_and_part.py about own time

---
+++ life/do_company_on_small_way/new_life_and_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    day_or_group(str_arg)
+    print('last_eye')
+
+def day_or_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('same_week')
---
+++ world/small_fact/look_public_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    look_old_work(str_arg)
+    print('do_eye_into_early_eye')
+
+def look_old_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('own_woman')
