2e3fe44ae3b3e26ba97555df5455f0c1f742d4a6
Jingkai Tang <jingkaitang@gmail.com>
2015-04-11 17:43:03 +0800

    rm: go child_and_way/make_new_eye_from_problem/child/good_day_or_man/different_part/group.py or new_way/woman/new_time.py over large thing

---
+++ child_and_way/make_new_eye_from_problem/child/good_day_or_man/different_part/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_fact(str_arg):
+    hand_or_new_week(str_arg)
+    print('good_case')
+
+def hand_or_new_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_fact('next_company')
---
+++ new_way/woman/new_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_case(str_arg):
+    young_group(str_arg)
+    print('other_fact')
+
+def young_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_case('get_great_part_to_day')
