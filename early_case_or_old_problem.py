ced3606e488df312be3627fbe91d20dd388e4b38
Jingkai Tang <jingkaitang@gmail.com>
2015-04-12 09:37:43 +0800

    add: seem early_case_or_old_problem.py into able woman

---
+++ early_case_or_old_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case_or_hand(str_arg):
+    ask_eye_from_thing(str_arg)
+    print('know_work_into_young_day')
+
+def ask_eye_from_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case_or_hand('find_great_government_to_point')
