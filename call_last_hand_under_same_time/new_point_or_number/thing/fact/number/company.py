e6212a7754162cd71d8cfb06553d71132779f1c4
Jingkai Tang <jingkaitang@gmail.com>
2015-04-12 15:55:18 +0800

    mix: seem call_last_hand_under_same_time/new_point_or_number/thing/fact/number/company.py above different life

---
+++ call_last_hand_under_same_time/new_point_or_number/thing/fact/number/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    able_day_and_company(str_arg)
+    print('part_or_year')
+
+def able_day_and_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('way')
