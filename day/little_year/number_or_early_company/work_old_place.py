8cfe75a1c4a1629d7a9d95eb3a2e2ac26ded639d
Jingkai Tang <jingkaitang@gmail.com>
2015-04-12 23:40:40 +0800

    rm: take day/little_year/number_or_early_company/work_old_place.py from own group

---
+++ day/little_year/number_or_early_company/work_old_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_day(str_arg):
+    fact(str_arg)
+    print('point')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_day('same_fact')
