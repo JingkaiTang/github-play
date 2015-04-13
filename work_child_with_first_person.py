40224ed37fdbfa97b8daf6e5827658075f77c7f8
Jingkai Tang <jingkaitang@gmail.com>
2015-04-14 03:51:13 +0800

    rm: seem work_child_with_first_person.py for early number

---
+++ work_child_with_first_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    new_fact_and_early_world(str_arg)
+    print('get_important_government_at_year')
+
+def new_fact_and_early_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('big_company')
