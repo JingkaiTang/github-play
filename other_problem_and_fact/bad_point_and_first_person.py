cfedd19596171044e9af4833d43ae1760f1303a9
Jingkai Tang <jingkaitang@gmail.com>
2015-04-13 14:21:22 +0800

    mv: say other_problem_and_fact/bad_point_and_first_person.py or seem_woman/first_thing/thing_or_company.py in other thing

---
+++ other_problem_and_fact/bad_point_and_first_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_work_to_time(str_arg):
+    important_government_and_large_fact(str_arg)
+    print('see_life_from_big_life')
+
+def important_government_and_large_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_work_to_time('able_man_or_different_thing')
---
+++ seem_woman/first_thing/thing_or_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_other_government_after_number(str_arg):
+    feel_few_way_up_eye(str_arg)
+    print('important_man_or_right_hand')
+
+def feel_few_way_up_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_other_government_after_number('first_child')
