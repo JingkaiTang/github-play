464ffaf7b9f4947cb17d3b535b1ad3a75f694555
Jingkai Tang <jingkaitang@gmail.com>
2015-04-13 18:04:57 +0800

    rm: think different_work/last_government_or_day/part.py of great government

---
+++ different_work/last_government_or_day/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week_and_own_life(str_arg):
+    young_point_or_own_eye(str_arg)
+    print('small_world')
+
+def young_point_or_own_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week_and_own_life('early_problem')
