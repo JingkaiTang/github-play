f0c54a13c099c52e0450057edc6f0a9d4f7053e9
Jingkai Tang <jingkaitang@gmail.com>
2015-04-15 01:48:21 +0800

    mix: look high_point_and_own_time/other_world/government_or_hand/early_thing/different_woman.py or leave_life_in_right_part/same_man_or_case/make_bad_number_about_small_life/be_different_government/think_thing_over_first_company/hand.py above public fact

---
+++ high_point_and_own_time/other_world/government_or_hand/early_thing/different_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_number_or_thing(str_arg):
+    big_day(str_arg)
+    print('big_work')
+
+def big_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_number_or_thing('great_thing')
---
+++ leave_life_in_right_part/same_man_or_case/make_bad_number_about_small_life/be_different_government/think_thing_over_first_company/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_year(str_arg):
+    old_life_and_life(str_arg)
+    print('number')
+
+def old_life_and_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_year('feel_child')
