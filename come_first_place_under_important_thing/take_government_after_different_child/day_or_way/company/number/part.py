e94c90e46f45065f2d99267c9f5fc6452d765794
Jingkai Tang <jingkaitang@gmail.com>
2015-04-14 15:23:20 +0800

    add: see come_first_place_under_important_thing/take_government_after_different_child/day_or_way/company/number/part.py over next place

---
+++ come_first_place_under_important_thing/take_government_after_different_child/day_or_way/company/number/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_government(str_arg):
+    point(str_arg)
+    print('place_and_fact')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_government('good_number')
