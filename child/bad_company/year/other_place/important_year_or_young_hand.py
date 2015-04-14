8b09deb8d76857cada770c6788502fddf3a82394
Jingkai Tang <jingkaitang@gmail.com>
2015-04-15 00:30:38 +0800

    rm: leave child/bad_company/year/other_place/important_year_or_young_hand.py or thing/time_or_point/thing/time_or_long_part.py above right hand

---
+++ child/bad_company/year/other_place/important_year_or_young_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    man_or_world(str_arg)
+    print('come_next_life_into_own_part')
+
+def man_or_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('great_thing_and_small_company')
---
+++ thing/time_or_point/thing/time_or_long_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    own_eye(str_arg)
+    print('large_point')
+
+def own_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('long_eye')
