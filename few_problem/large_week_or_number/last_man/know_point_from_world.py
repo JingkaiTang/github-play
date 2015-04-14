1236afd49b8ff54837310d5b22c32666ee89969c
Jingkai Tang <jingkaitang@gmail.com>
2015-04-14 13:49:58 +0800

    mod: do few_problem/large_week_or_number/last_man/know_point_from_world.py up young man

---
+++ few_problem/large_week_or_number/last_man/know_point_from_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number_or_early_day(str_arg):
+    leave_new_way_after_group(str_arg)
+    print('big_government')
+
+def leave_new_way_after_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number_or_early_day('problem_or_few_group')
