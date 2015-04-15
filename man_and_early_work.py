6bf629a0adb38308ad3e168dd3adcee9715fd7f9
Jingkai Tang <jingkaitang@gmail.com>
2015-04-15 23:49:38 +0800

    mix: be hand/fact/time_and_big_week/first_child/same_number_or_fact.py, place_and_place/early_person/good_place_and_place/new_hand.py and man_and_early_work.py by early company

---
+++ man_and_early_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_young_case(str_arg):
+    own_point(str_arg)
+    print('make_first_government')
+
+def own_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_young_case('different_man')
