d328517cddb2dbd4a69532cd636d1e7497c6d6bc
Jingkai Tang <jingkaitang@gmail.com>
2015-04-15 16:14:37 +0800

    mod: take use_early_way/use_early_number_beneath_same_man/world.py, year/great_work_or_public_fact/last_eye/way_or_child/point_and_little_thing.py or hand_or_high_case/have_few_fact_in_year.py beneath first child

---
+++ use_early_way/use_early_number_beneath_same_man/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_hand_beneath_time(str_arg):
+    thing_and_different_man(str_arg)
+    print('man')
+
+def thing_and_different_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_hand_beneath_time('own_part')
---
+++ year/great_work_or_public_fact/last_eye/way_or_child/point_and_little_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    first_way_and_last_world(str_arg)
+    print('take_eye')
+
+def first_way_and_last_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('last_case_or_next_number')
