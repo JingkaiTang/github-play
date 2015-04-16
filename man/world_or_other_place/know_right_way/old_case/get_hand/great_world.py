4d628882e20639bae2b140a48087c62074466af1
Jingkai Tang <jingkaitang@gmail.com>
2015-04-16 20:22:52 +0800

    rm: work woman.py and man/world_or_other_place/know_right_way/old_case/get_hand/great_world.py under bad time

---
+++ man/world_or_other_place/know_right_way/old_case/get_hand/great_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_work_or_day(str_arg):
+    see_fact_above_company(str_arg)
+    print('same_world')
+
+def see_fact_above_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_work_or_day('big_hand_or_world')
--- woman.py
+++ woman.py
@@ -1,12 +1,12 @@
 
 #! /usr/bin/env python
 
-def place(str_arg):
-    place_and_day(str_arg)
-    print('come_last_year_from_bad_government')
+def thing(str_arg):
+    public_year(str_arg)
+    print('week_or_bad_world')
 
-def place_and_day(str_arg):
+def public_year(str_arg):
     print(str_arg)
 
 if __name__ == '__main__':
-    place('ask_new_case')
+    thing('company')
