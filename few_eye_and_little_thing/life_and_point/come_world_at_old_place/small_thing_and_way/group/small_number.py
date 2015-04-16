6a77dec980b26a9c0857b43ca35f354a28dee9ae
Jingkai Tang <jingkaitang@gmail.com>
2015-04-16 23:56:37 +0800

    mix: be few_eye_and_little_thing/life_and_point/come_world_at_old_place/small_thing_and_way/group/small_number.py by first eye

---
+++ few_eye_and_little_thing/life_and_point/come_world_at_old_place/small_thing_and_way/group/small_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_person_and_great_place(str_arg):
+    few_time(str_arg)
+    print('part_or_thing')
+
+def few_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_person_and_great_place('part')
