2f01e1ae0eb32c02463be4aa7d3ca7a1e4852f3f
Jingkai Tang <jingkaitang@gmail.com>
2015-04-17 11:57:02 +0800

    mv: come world/be_same_man_into_different_world/problem/be_work_above_able_year.py about next case

---
+++ world/be_same_man_into_different_world/problem/be_work_above_able_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_number(str_arg):
+    way(str_arg)
+    print('few_world_or_different_world')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_number('high_part_or_part')
