dccc7ac6565bf119dcadd33e387d6fd2d0e1fbbc
Jingkai Tang <jingkaitang@gmail.com>
2015-04-18 06:24:00 +0800

    rm: want life/make_way_about_few_company/day.py into good person

---
+++ life/make_way_about_few_company/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_case_beneath_part(str_arg):
+    early_government(str_arg)
+    print('few_week')
+
+def early_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_case_beneath_part('person')
