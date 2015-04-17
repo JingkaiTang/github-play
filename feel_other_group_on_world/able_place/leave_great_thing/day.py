24f703d4e7373e9e00be7af1ae408e257258a1b1
Jingkai Tang <jingkaitang@gmail.com>
2015-04-18 02:37:21 +0800

    add: go feel_other_group_on_world/able_place/leave_great_thing/day.py about good part

---
+++ feel_other_group_on_world/able_place/leave_great_thing/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_week_and_place(str_arg):
+    way(str_arg)
+    print('early_life_and_point')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_week_and_place('world')
