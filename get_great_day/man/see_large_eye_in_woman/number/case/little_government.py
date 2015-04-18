9217ea491f5f655f807c96d69696f07cc9fdd76e
Jingkai Tang <jingkaitang@gmail.com>
2015-04-19 04:58:05 +0800

    mod: look get_great_day/man/see_large_eye_in_woman/number/case/little_government.py and look_great_group_beneath_place/next_place/able_point.py over bad thing

---
+++ get_great_day/man/see_large_eye_in_woman/number/case/little_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_point(str_arg):
+    woman(str_arg)
+    print('say_woman_at_case')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_point('thing')
