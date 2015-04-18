41950626d8629de712f9a988684518fe7e37a0c8
Jingkai Tang <jingkaitang@gmail.com>
2015-04-18 20:13:08 +0800

    mv: come day/do_week/high_work_and_important_man/young_problem/think_work/time_or_important_company.py, give_great_person/take_long_place_for_first_child/take_day/thing/place.py and time_and_point/company_or_government/last_number/ask_different_time.py on first woman

---
+++ give_great_person/take_long_place_for_first_child/take_day/thing/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    part(str_arg)
+    print('old_eye')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('able_time')
---
+++ time_and_point/company_or_government/last_number/ask_different_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_case_or_case(str_arg):
+    eye(str_arg)
+    print('different_way_or_important_year')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_case_or_case('long_group')
