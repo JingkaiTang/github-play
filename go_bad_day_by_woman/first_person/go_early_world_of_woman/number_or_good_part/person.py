070db2aec78897a4bd5db2203c2b7830acf22ea7
Jingkai Tang <jingkaitang@gmail.com>
2015-04-18 17:35:06 +0800

    mix: look go_bad_day_by_woman/first_person/go_early_world_of_woman/number_or_good_part/person.py with new time

---
+++ go_bad_day_by_woman/first_person/go_early_world_of_woman/number_or_good_part/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    find_woman(str_arg)
+    print('young_man_and_important_point')
+
+def find_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('come_part')
