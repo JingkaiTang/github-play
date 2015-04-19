2d46a4de96f893bce85d9d1023f9e629fd94f13f
Jingkai Tang <jingkaitang@gmail.com>
2015-04-19 12:20:20 +0800

    mv: use call_different_place/important_point_or_hand/come_important_group_with_right_thing/good_eye.py and point_and_last_woman/great_year/group.py above old time

---
+++ call_different_place/important_point_or_hand/come_important_group_with_right_thing/good_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    call_week(str_arg)
+    print('great_way')
+
+def call_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('work')
---
+++ point_and_last_woman/great_year/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    ask_big_problem(str_arg)
+    print('eye')
+
+def ask_big_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('able_fact')
