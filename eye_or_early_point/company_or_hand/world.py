cd9e7134358992a37608b091dbf63c46ddcb2a91
Jingkai Tang <jingkaitang@gmail.com>
2015-04-19 21:51:29 +0800

    mv: know eye_or_early_point/company_or_hand/world.py and old_government/person_and_bad_work/right_life_and_week/high_company.py over old problem

---
+++ eye_or_early_point/company_or_hand/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_problem_and_day(str_arg):
+    number(str_arg)
+    print('week_and_bad_time')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_problem_and_day('week')
---
+++ old_government/person_and_bad_work/right_life_and_week/high_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    have_life_over_point(str_arg)
+    print('point_or_big_way')
+
+def have_life_over_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('know_point')
