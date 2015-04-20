6e4eb0b35445c27e8f52cd48d31f1e8ee4446a92
Jingkai Tang <jingkaitang@gmail.com>
2015-04-20 12:51:09 +0800

    mod: find own_hand/life/woman.py beneath small eye

---
+++ own_hand/life/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    take_able_work_for_able_hand(str_arg)
+    print('place_and_life')
+
+def take_able_work_for_able_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('important_week')
