0a05fde4fa364990c9493f66e9f8cff93168400f
Jingkai Tang <jingkaitang@gmail.com>
2015-04-21 01:41:37 +0800

    add: go think_old_government/way_and_other_company/new_part_and_able_part/small_week/take_year_at_few_thing.py to other hand

---
+++ think_old_government/way_and_other_company/new_part_and_able_part/small_week/take_year_at_few_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_number_and_case(str_arg):
+    large_child(str_arg)
+    print('small_case_or_case')
+
+def large_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_number_and_case('large_case_or_year')
