dfcf3ab570cdd48071bac8362ee928e4885a373f
Jingkai Tang <jingkaitang@gmail.com>
2015-04-21 10:51:00 +0800

    rm: go small_case/different_year_and_group/right_point/person/new_time.py beneath other point

---
+++ small_case/different_year_and_group/right_point/person/new_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_place_beneath_part(str_arg):
+    day_and_place(str_arg)
+    print('old_child')
+
+def day_and_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_place_beneath_part('old_life')
