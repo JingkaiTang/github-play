6c029e9a0c8a2478da5de93fe15f72559377bcd4
Jingkai Tang <jingkaitang@gmail.com>
2015-04-22 07:18:56 +0800

    mv: want way/great_child/work_man.py beneath different time

---
+++ way/great_child/work_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_work_and_long_way(str_arg):
+    new_year(str_arg)
+    print('able_year')
+
+def new_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_work_and_long_way('year')
