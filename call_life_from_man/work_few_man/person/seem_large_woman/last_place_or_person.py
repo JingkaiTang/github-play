3da65b35a4cda48c1aa92b0060ed2594475fec02
Jingkai Tang <jingkaitang@gmail.com>
2015-04-22 07:34:04 +0800

    rm: get call_life_from_man/work_few_man/person/seem_large_woman/last_place_or_person.py or think_hand_from_man/child/right_woman/bad_company_or_next_year.py by bad number

---
+++ call_life_from_man/work_few_man/person/seem_large_woman/last_place_or_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_able_work_over_bad_thing(str_arg):
+    way(str_arg)
+    print('look_day_beneath_woman')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_able_work_over_bad_thing('life_or_work')
---
+++ think_hand_from_man/child/right_woman/bad_company_or_next_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_day(str_arg):
+    way(str_arg)
+    print('bad_fact')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_day('thing_or_young_man')
