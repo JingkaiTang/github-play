e7288a2b5cc88e84a6c0a5430cc79b028dc6b642
Jingkai Tang <jingkaitang@gmail.com>
2015-04-22 17:34:44 +0800

    mix: feel place_or_great_group/public_problem/company.py up great work

---
+++ place_or_great_group/public_problem/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_child(str_arg):
+    bad_eye_or_number(str_arg)
+    print('feel_next_thing')
+
+def bad_eye_or_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_child('go_new_thing_about_able_fact')
