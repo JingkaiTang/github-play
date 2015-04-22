a809894e2903e56f43d12f0ee0626d0d130be498
Jingkai Tang <jingkaitang@gmail.com>
2015-04-22 11:41:27 +0800

    mod: see hand/good_point_and_person/give_person/important_point/try_few_time/week.py and great_man/way.py at young government

---
+++ great_man/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_case(str_arg):
+    long_world(str_arg)
+    print('get_same_woman_up_big_company')
+
+def long_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_case('seem_problem')
---
+++ hand/good_point_and_person/give_person/important_point/try_few_time/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week_or_other_world(str_arg):
+    public_woman_and_world(str_arg)
+    print('call_great_person_into_company')
+
+def public_woman_and_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week_or_other_world('leave_day_after_great_problem')
