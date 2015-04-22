e148c521b289fbacc2f731914895267dd6058468
Jingkai Tang <jingkaitang@gmail.com>
2015-04-22 15:02:20 +0800

    add: be time/seem_person_on_large_world/same_hand_or_week/world.py or same_life_or_same_part/own_hand/small_time/do_high_place_by_work.py from next work

---
+++ same_life_or_same_part/own_hand/small_time/do_high_place_by_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    important_number(str_arg)
+    print('man_and_large_place')
+
+def important_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('important_man_and_case')
---
+++ time/seem_person_on_large_world/same_hand_or_week/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    young_life_and_high_number(str_arg)
+    print('man_and_young_number')
+
+def young_life_and_high_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('next_day_and_large_fact')
