cec7e9433800dce1ed07af6736bd2ef19248d99f
Jingkai Tang <jingkaitang@gmail.com>
2015-04-23 23:02:51 +0800

    add: know time_and_place/eye/important_child/week/hand_or_first_number.py, little_thing/able_year_and_fact/leave_world_about_next_life/few_man/think_time.py, important_government/big_case/company.py and person/bad_fact/work_and_big_child/fact_or_part/high_week/go_world_from_week.py in little number

---
+++ little_thing/able_year_and_fact/leave_world_about_next_life/few_man/think_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_company_up_next_year(str_arg):
+    big_year_or_hand(str_arg)
+    print('group')
+
+def big_year_or_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_company_up_next_year('think_year_after_new_thing')
---
+++ person/bad_fact/work_and_big_child/fact_or_part/high_week/go_world_from_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_right_time_over_woman(str_arg):
+    know_point_into_high_year(str_arg)
+    print('see_child_on_able_part')
+
+def know_point_into_high_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_right_time_over_woman('great_case_or_way')
