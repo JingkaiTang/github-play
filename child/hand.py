bb0741764291867ec91de6294f59df850d613284
Jingkai Tang <jingkaitang@gmail.com>
2015-04-23 16:39:08 +0800

    rm: try child/hand.py for early government

---
+++ child/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_case_or_big_problem(str_arg):
+    able_company(str_arg)
+    print('fact_or_hand')
+
+def able_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_case_or_big_problem('week')
