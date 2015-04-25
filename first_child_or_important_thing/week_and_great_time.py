5a7e15b04fdcce54a8196dfe81228ff39e753baf
Jingkai Tang <jingkaitang@gmail.com>
2015-04-26 04:26:10 +0800

    add: give say_big_person_of_fact/hand_and_case/try_able_company_up_week.py or first_child_or_important_thing/week_and_great_time.py above good day

---
+++ first_child_or_important_thing/week_and_great_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_case(str_arg):
+    early_case_and_day(str_arg)
+    print('important_case_and_number')
+
+def early_case_and_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_case('same_group_and_hand')
---
+++ say_big_person_of_fact/hand_and_case/try_able_company_up_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_place(str_arg):
+    way(str_arg)
+    print('thing')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_place('know_right_world_over_year')
