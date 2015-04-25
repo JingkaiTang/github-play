d783a62d9633472103ae24b7a8d9c1ad33c8afb8
Jingkai Tang <jingkaitang@gmail.com>
2015-04-26 01:54:04 +0800

    mod: take government/have_bad_eye/time_and_young_group/place.py over small woman

---
+++ government/have_bad_eye/time_and_young_group/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_government(str_arg):
+    able_time(str_arg)
+    print('year')
+
+def able_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_government('same_thing_and_company')
