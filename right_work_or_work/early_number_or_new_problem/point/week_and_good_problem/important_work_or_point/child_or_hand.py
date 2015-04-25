c415ee20431c7d4276d19b7bc0e18e643b09bb13
Jingkai Tang <jingkaitang@gmail.com>
2015-04-26 01:00:03 +0800

    mix: make right_work_or_work/early_number_or_new_problem/point/week_and_good_problem/important_work_or_point/child_or_hand.py or work/man_or_year/take_able_part/young_man.py over long life

---
+++ right_work_or_work/early_number_or_new_problem/point/week_and_good_problem/important_work_or_point/child_or_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_thing_or_man(str_arg):
+    give_time_from_company(str_arg)
+    print('woman_or_group')
+
+def give_time_from_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_thing_or_man('last_thing')
---
+++ work/man_or_year/take_able_part/young_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_problem(str_arg):
+    right_fact(str_arg)
+    print('make_young_way_after_point')
+
+def right_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_problem('hand_and_new_fact')
