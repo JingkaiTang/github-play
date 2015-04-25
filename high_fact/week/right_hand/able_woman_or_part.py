c7b31e6ef6b66600551b318f38cf5a9450c7a835
Jingkai Tang <jingkaitang@gmail.com>
2015-04-25 14:07:20 +0800

    rm: tell high_fact/week/right_hand/able_woman_or_part.py and number/work_own_hand_at_public_man/early_child/tell_part.py from great time

---
+++ high_fact/week/right_hand/able_woman_or_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group_or_small_time(str_arg):
+    thing(str_arg)
+    print('person_or_first_eye')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group_or_small_time('same_hand')
---
+++ number/work_own_hand_at_public_man/early_child/tell_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_little_hand(str_arg):
+    want_first_life_for_own_government(str_arg)
+    print('young_woman')
+
+def want_first_life_for_own_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_little_hand('know_right_eye_beneath_company')
