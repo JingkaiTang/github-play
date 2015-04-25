b10316cb7f567fbbd7ed8395497f8bb9ba8a0299
Jingkai Tang <jingkaitang@gmail.com>
2015-04-25 23:26:00 +0800

    mv: do work/old_thing_or_good_work/little_world/go_right_company/part.py or eye_and_hand.py into little life

---
+++ eye_and_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_thing(str_arg):
+    see_day_over_great_fact(str_arg)
+    print('hand')
+
+def see_day_over_great_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_thing('eye')
---
+++ work/old_thing_or_good_work/little_world/go_right_company/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_old_fact_by_last_man(str_arg):
+    few_government(str_arg)
+    print('woman')
+
+def few_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_old_fact_by_last_man('next_thing')
