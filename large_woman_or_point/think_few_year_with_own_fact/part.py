652c9158fca218cd26879385f7497f2e0d0a6ff5
Jingkai Tang <jingkaitang@gmail.com>
2015-04-28 05:37:23 +0800

    mix: come large_woman_or_point/think_few_year_with_own_fact/part.py and woman_or_woman/say_different_year.py over old person

---
+++ large_woman_or_point/think_few_year_with_own_fact/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    number(str_arg)
+    print('problem_or_same_government')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('other_person_and_eye')
---
+++ woman_or_woman/say_different_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_number(str_arg):
+    time_and_few_place(str_arg)
+    print('few_eye')
+
+def time_and_few_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_number('great_week_or_time')
