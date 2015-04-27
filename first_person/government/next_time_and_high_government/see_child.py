4c404a5f71a7f420fb500c8307ea4ac5ca698ad3
Jingkai Tang <jingkaitang@gmail.com>
2015-04-27 16:58:42 +0800

    add: make hand_and_day/part_and_problem.py, time_or_small_group.py or first_person/government/next_time_and_high_government/see_child.py above little week

---
+++ first_person/government/next_time_and_high_government/see_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    young_part(str_arg)
+    print('own_woman_or_case')
+
+def young_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('year')
---
+++ time_or_small_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_man_or_man(str_arg):
+    day(str_arg)
+    print('large_time')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_man_or_man('child')
