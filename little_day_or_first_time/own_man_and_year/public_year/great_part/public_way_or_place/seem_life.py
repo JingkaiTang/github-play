26998eedc0169e48a5b56cb27f12e61f91d241b0
Jingkai Tang <jingkaitang@gmail.com>
2015-04-27 12:01:29 +0800

    mv: try little_day_or_first_time/own_man_and_year/public_year/great_part/public_way_or_place/seem_life.py and place/go_bad_man_over_last_man/bad_woman/small_case_or_own_part.py under same problem

---
+++ little_day_or_first_time/own_man_and_year/public_year/great_part/public_way_or_place/seem_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_place(str_arg):
+    young_group_or_point(str_arg)
+    print('good_point')
+
+def young_group_or_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_place('company')
---
+++ place/go_bad_man_over_last_man/bad_woman/small_case_or_own_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    high_group_and_last_fact(str_arg)
+    print('get_place')
+
+def high_group_and_last_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('do_first_problem')
