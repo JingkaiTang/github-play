ef2d53dd960eae0123e880e85467f6fef37295b6
Jingkai Tang <jingkaitang@gmail.com>
2015-04-28 08:01:36 +0800

    add: call important_week/other_world.py by large hand

---
+++ important_week/other_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_way_to_year(str_arg):
+    come_government(str_arg)
+    print('day')
+
+def come_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_way_to_year('have_little_problem')
