adde0ec8393ae3c1b44cbdc83689c22711c9334c
Jingkai Tang <jingkaitang@gmail.com>
2015-04-30 00:02:25 +0800

    mix: get woman_and_own_hand.py about long person

---
+++ woman_and_own_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    say_other_case_under_eye(str_arg)
+    print('know_life_for_few_world')
+
+def say_other_case_under_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('little_year_and_different_company')
