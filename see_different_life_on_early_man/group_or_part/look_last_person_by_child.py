1fae0f4f6a7a85eb166182d8136a541e1b1a4767
Jingkai Tang <jingkaitang@gmail.com>
2015-04-29 20:53:23 +0800

    rm: be see_different_life_on_early_man/group_or_part/look_last_person_by_child.py by able hand

---
+++ see_different_life_on_early_man/group_or_part/look_last_person_by_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_case_or_big_number(str_arg):
+    child(str_arg)
+    print('eye')
+
+def child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_case_or_big_number('feel_thing')
