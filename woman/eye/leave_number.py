3ca03440d836b7029afc767749cf3d639fb60054
Jingkai Tang <jingkaitang@gmail.com>
2015-05-01 05:21:23 +0800

    add: use woman/eye/leave_number.py for large fact

---
+++ woman/eye/leave_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    look_first_number_up_place(str_arg)
+    print('high_eye')
+
+def look_first_number_up_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('right_man')
