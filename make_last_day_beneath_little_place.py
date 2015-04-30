5cdf30e9d42c9d0779da9259c80b1398dd61babd
Jingkai Tang <jingkaitang@gmail.com>
2015-04-30 19:09:34 +0800

    rm: look make_last_day_beneath_little_place.py and public_point/think_case/old_group_and_few_woman.py from same year

---
+++ make_last_day_beneath_little_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_point(str_arg):
+    new_man(str_arg)
+    print('hand')
+
+def new_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_point('public_point_or_thing')
---
+++ public_point/think_case/old_group_and_few_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_world(str_arg):
+    important_company_and_new_day(str_arg)
+    print('have_few_point')
+
+def important_company_and_new_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_world('way')
