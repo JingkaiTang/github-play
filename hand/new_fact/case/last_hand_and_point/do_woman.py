13d50649b74c22788ae8ac51d61b72ab7dcd9d72
Jingkai Tang <jingkaitang@gmail.com>
2015-05-01 07:18:37 +0800

    mix: work hand/new_fact/case/last_hand_and_point/do_woman.py for public world

---
+++ hand/new_fact/case/last_hand_and_point/do_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_other_man_for_important_year(str_arg):
+    government_or_next_problem(str_arg)
+    print('year_or_public_fact')
+
+def government_or_next_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_other_man_for_important_year('work_last_eye_by_new_place')
