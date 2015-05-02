01aa0fa46b4c4a92b245087c2579a2a71a973222
Jingkai Tang <jingkaitang@gmail.com>
2015-05-03 05:00:18 +0800

    mv: want public_week_or_old_fact/leave_group/woman/get_year/person.py up first point

---
+++ public_week_or_old_fact/leave_group/woman/get_year/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_last_person(str_arg):
+    week(str_arg)
+    print('large_world')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_last_person('great_life')
