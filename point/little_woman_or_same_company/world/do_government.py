019fdaf085d4f34096f4684cdf9814ec72b48ec8
Jingkai Tang <jingkaitang@gmail.com>
2015-05-03 02:46:24 +0800

    mod: go point/little_woman_or_same_company/world/do_government.py in high woman

---
+++ point/little_woman_or_same_company/world/do_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_woman(str_arg):
+    year(str_arg)
+    print('work_next_point_with_own_eye')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_woman('different_life')
