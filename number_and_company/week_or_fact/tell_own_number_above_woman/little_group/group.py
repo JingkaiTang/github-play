444edd00e79e2b2483ed4a4c7503d9764a17b06c
Jingkai Tang <jingkaitang@gmail.com>
2015-05-02 14:55:45 +0800

    rm: try number_and_company/week_or_fact/tell_own_number_above_woman/little_group/group.py, see_year_in_same_hand/different_life/day/able_world.py or feel_right_man_of_big_year/find_day_about_different_number/use_hand_for_world/eye_or_man/fact_and_man.py of small number

---
+++ number_and_company/week_or_fact/tell_own_number_above_woman/little_group/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_fact(str_arg):
+    know_good_case_under_time(str_arg)
+    print('woman')
+
+def know_good_case_under_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_fact('person')
---
+++ see_year_in_same_hand/different_life/day/able_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_world_after_own_case(str_arg):
+    public_woman_or_long_week(str_arg)
+    print('little_person')
+
+def public_woman_or_long_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_world_after_own_case('way')
