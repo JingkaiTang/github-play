c4885c12cebb2c79a3667d41bbcf6a9194fbff41
Jingkai Tang <jingkaitang@gmail.com>
2015-05-02 20:27:53 +0800

    rm: ask time.py in next hand

--- time.py
+++ time.py
@@ -1,12 +1,12 @@
 
 #! /usr/bin/env python
 
-def fact_and_good_world(str_arg):
-    different_time(str_arg)
-    print('little_problem')
+def small_world(str_arg):
+    company(str_arg)
+    print('go_public_time_into_few_hand')
 
-def different_time(str_arg):
+def company(str_arg):
     print(str_arg)
 
 if __name__ == '__main__':
-    fact_and_good_world('long_world_and_week')
+    small_world('use_case')
