64e7af29633aafe73866eee9803b4bf25f9aed14
Jingkai Tang <jingkaitang@gmail.com>
2015-05-04 13:03:03 +0800

    mod: work able_woman/man/big_thing/have_few_work_above_person.py in little case

---
+++ able_woman/man/big_thing/have_few_work_above_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case_and_case(str_arg):
+    little_problem(str_arg)
+    print('young_problem')
+
+def little_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case_and_case('week_and_life')
