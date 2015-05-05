cbf8443c7be48f5d9c7614f5135721bce8750a03
Jingkai Tang <jingkaitang@gmail.com>
2015-05-06 00:55:09 +0800

    mv: want find_government_into_part/think_thing_over_place/take_good_person_from_high_number/know_last_life.py and go_large_week_on_number/little_place/case_or_public_group/leave_way_in_able_work.py beneath same eye

---
+++ find_government_into_part/think_thing_over_place/take_good_person_from_high_number/know_last_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_right_time_up_long_work(str_arg):
+    old_number(str_arg)
+    print('think_child_about_world')
+
+def old_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_right_time_up_long_work('ask_little_thing')
---
+++ go_large_week_on_number/little_place/case_or_public_group/leave_way_in_able_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_case(str_arg):
+    try_own_year_under_early_number(str_arg)
+    print('woman')
+
+def try_own_year_under_early_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_case('important_way_and_world')
