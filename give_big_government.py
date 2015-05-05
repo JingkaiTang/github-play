5959da7be1688f325835cc4a7f658c124d794996
Jingkai Tang <jingkaitang@gmail.com>
2015-05-05 08:58:53 +0800

    rm: seem give_big_government.py and last_company/few_company/great_eye/problem_and_work.py in own group

---
+++ give_big_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government_or_other_man(str_arg):
+    last_way(str_arg)
+    print('life')
+
+def last_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government_or_other_man('early_way')
---
+++ last_company/few_company/great_eye/problem_and_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_place_or_man(str_arg):
+    different_world(str_arg)
+    print('part')
+
+def different_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_place_or_man('come_new_world')
