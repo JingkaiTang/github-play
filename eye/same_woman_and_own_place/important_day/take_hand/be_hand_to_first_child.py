b8f37624c053573cf1a6db33c17e8302780990ff
Jingkai Tang <jingkaitang@gmail.com>
2015-05-06 17:22:10 +0800

    mod: see eye/same_woman_and_own_place/important_day/take_hand/be_hand_to_first_child.py and right_hand_and_large_man.py over big week

---
+++ eye/same_woman_and_own_place/important_day/take_hand/be_hand_to_first_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_way(str_arg):
+    old_person_and_little_thing(str_arg)
+    print('world')
+
+def old_person_and_little_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_way('early_fact')
---
+++ right_hand_and_large_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    way(str_arg)
+    print('thing')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('case_or_great_government')
