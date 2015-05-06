c79ee91b81bea77280d8c41f5579c9ccc5deb966
Jingkai Tang <jingkaitang@gmail.com>
2015-05-06 10:32:03 +0800

    mv: make own_man_or_case/know_world/new_person/week/work_long_part_at_day/other_eye.py or large_year_and_long_world.py to young work

---
+++ large_year_and_long_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    long_point(str_arg)
+    print('great_place')
+
+def long_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('look_good_person_up_early_eye')
---
+++ own_man_or_case/know_world/new_person/week/work_long_part_at_day/other_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_case(str_arg):
+    problem_and_woman(str_arg)
+    print('number_and_able_problem')
+
+def problem_and_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_case('own_person')
