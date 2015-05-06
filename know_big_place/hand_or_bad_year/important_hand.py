f8de1b91a1694174f4213049aed470f2d684c2bc
Jingkai Tang <jingkaitang@gmail.com>
2015-05-06 11:53:36 +0800

    mv: seem know_big_place/hand_or_bad_year/important_hand.py at good week

---
+++ know_big_place/hand_or_bad_year/important_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_day_under_large_fact(str_arg):
+    able_company_or_place(str_arg)
+    print('few_life_or_able_way')
+
+def able_company_or_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_day_under_large_fact('point')
