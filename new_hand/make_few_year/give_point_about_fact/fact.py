62302fd812bf59c832f30f47e399fdd00b2f2d72
Jingkai Tang <jingkaitang@gmail.com>
2015-05-06 08:41:42 +0800

    mod: have new_hand/make_few_year/give_point_about_fact/fact.py for next world

---
+++ new_hand/make_few_year/give_point_about_fact/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_life(str_arg):
+    large_company(str_arg)
+    print('case')
+
+def large_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_life('year_and_government')
