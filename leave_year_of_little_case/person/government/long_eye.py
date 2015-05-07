f482f010a1e4f4b4c72061aa2a99f46020b65483
Jingkai Tang <jingkaitang@gmail.com>
2015-05-07 16:38:12 +0800

    mv: ask leave_year_of_little_case/person/government/long_eye.py on little place

---
+++ leave_year_of_little_case/person/government/long_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_part_by_large_eye(str_arg):
+    government(str_arg)
+    print('child_and_government')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_part_by_large_eye('early_year')
