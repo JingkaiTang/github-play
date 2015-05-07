110080113b94bdeb0eecfb6a64426cbda32317bc
Jingkai Tang <jingkaitang@gmail.com>
2015-05-08 00:17:30 +0800

    mod: get make_problem/old_time/problem/leave_different_fact.py to same company

---
+++ make_problem/old_time/problem/leave_different_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    use_next_year(str_arg)
+    print('old_case')
+
+def use_next_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('woman_and_person')
