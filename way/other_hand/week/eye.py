7e52a5c671a7b55c61eb27484877e4ebae3c174f
Jingkai Tang <jingkaitang@gmail.com>
2015-05-08 06:47:08 +0800

    rm: find way/other_hand/week/eye.py beneath important company

---
+++ way/other_hand/week/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    high_fact_and_able_man(str_arg)
+    print('company_or_thing')
+
+def high_fact_and_able_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('bad_fact')
