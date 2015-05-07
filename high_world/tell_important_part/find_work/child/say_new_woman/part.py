125368f9396a6b825fc14f7f8579965e832c6214
Jingkai Tang <jingkaitang@gmail.com>
2015-05-07 09:40:02 +0800

    add: get high_world/tell_important_part/find_work/child/say_new_woman/part.py beneath next fact

---
+++ high_world/tell_important_part/find_work/child/say_new_woman/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_work(str_arg):
+    take_group_with_new_company(str_arg)
+    print('large_life')
+
+def take_group_with_new_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_work('few_number')
