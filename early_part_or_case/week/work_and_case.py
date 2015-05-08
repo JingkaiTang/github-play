f63df1da79c81760ff74e1b1c13c25b141f6129a
Jingkai Tang <jingkaitang@gmail.com>
2015-05-09 06:19:07 +0800

    mod: see early_part_or_case/week/work_and_case.py from great time

---
+++ early_part_or_case/week/work_and_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person_or_case(str_arg):
+    person_and_little_number(str_arg)
+    print('old_hand_and_woman')
+
+def person_and_little_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person_or_case('way')
