c2a66bc5721b0011802f4d62d8af635418537e48
Jingkai Tang <jingkaitang@gmail.com>
2015-05-08 14:59:50 +0800

    add: call use_new_world_from_place/ask_old_year/bad_year.py and right_point/week/place_and_right_government/life/get_early_child_beneath_day/time.py over high child

---
+++ right_point/week/place_and_right_government/life/get_early_child_beneath_day/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_hand_and_great_life(str_arg):
+    bad_point_or_place(str_arg)
+    print('few_eye')
+
+def bad_point_or_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_hand_and_great_life('other_person')
---
+++ use_new_world_from_place/ask_old_year/bad_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    able_child(str_arg)
+    print('give_part')
+
+def able_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('large_day')
