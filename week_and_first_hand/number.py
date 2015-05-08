c28ab4663a1984cb0616cc3e67d46626e93c2712
Jingkai Tang <jingkaitang@gmail.com>
2015-05-08 18:44:27 +0800

    mv: find week_and_first_hand/number.py and old_problem_and_point.py in right work

---
+++ week_and_first_hand/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_hand(str_arg):
+    use_number_by_next_life(str_arg)
+    print('day')
+
+def use_number_by_next_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_hand('call_case_of_day')
