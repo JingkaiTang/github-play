558bfe10ad96d33080b497b18bbeb458b9e7a4c1
Jingkai Tang <jingkaitang@gmail.com>
2015-05-08 17:07:52 +0800

    mix: try group/young_thing/public_group_or_thing/important_day/call_case/point.py and first_work.py about early world

---
+++ first_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    long_way(str_arg)
+    print('week_or_good_world')
+
+def long_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('give_group')
---
+++ group/young_thing/public_group_or_thing/important_day/call_case/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government_or_same_day(str_arg):
+    world(str_arg)
+    print('case')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government_or_same_day('tell_other_person_up_bad_number')
