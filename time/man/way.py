9af5c1c2ef8197979b4c71f80f908d4166ce7f1b
Jingkai Tang <jingkaitang@gmail.com>
2015-05-08 21:20:52 +0800

    mix: call time/man/way.py or year/eye/try_hand/find_long_person/problem.py in able eye

---
+++ time/man/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part_and_own_place(str_arg):
+    woman_and_week(str_arg)
+    print('thing_or_hand')
+
+def woman_and_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part_and_own_place('see_company_after_way')
---
+++ year/eye/try_hand/find_long_person/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_first_year(str_arg):
+    know_thing(str_arg)
+    print('week')
+
+def know_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_first_year('part_and_same_way')
