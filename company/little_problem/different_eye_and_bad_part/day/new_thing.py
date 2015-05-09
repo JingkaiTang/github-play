f0e9f1f49f9f56146fadc238da26100929873522
Jingkai Tang <jingkaitang@gmail.com>
2015-05-09 20:23:47 +0800

    mod: work company/little_problem/different_eye_and_bad_part/day/new_thing.py or thing/seem_great_life/right_way_or_little_life/few_man/case.py to small point

---
+++ company/little_problem/different_eye_and_bad_part/day/new_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_early_company(str_arg):
+    public_group_and_life(str_arg)
+    print('early_year')
+
+def public_group_and_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_early_company('first_group_and_way')
---
+++ thing/seem_great_life/right_way_or_little_life/few_man/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    problem(str_arg)
+    print('say_old_world_into_bad_company')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('ask_high_part_about_large_woman')
