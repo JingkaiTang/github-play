5bea2b25a2519f3c5234ee04aabb5c504b62aacb
Jingkai Tang <jingkaitang@gmail.com>
2015-05-09 16:17:05 +0800

    mv: feel woman/work/child/do_bad_point/small_child/feel_woman.py from high number

---
+++ woman/work/child/do_bad_point/small_child/feel_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_problem_beneath_week(str_arg):
+    look_work(str_arg)
+    print('time')
+
+def look_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_problem_beneath_week('find_person_over_fact')
