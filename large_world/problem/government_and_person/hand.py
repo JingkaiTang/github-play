51272130dd509439270a4b04e45d405bd260e3f7
Jingkai Tang <jingkaitang@gmail.com>
2015-05-11 05:19:10 +0800

    rm: use large_world/problem/government_and_person/hand.py at different number

---
+++ large_world/problem/government_and_person/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_work(str_arg):
+    high_eye(str_arg)
+    print('leave_young_case')
+
+def high_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_work('first_company_or_last_time')
