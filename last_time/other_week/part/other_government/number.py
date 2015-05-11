1c6d83a65cd398f4322d4135487f37d7533d2ec0
Jingkai Tang <jingkaitang@gmail.com>
2015-05-11 21:33:04 +0800

    add: come last_time/other_week/part/other_government/number.py or world/year_or_early_world/few_part_and_week/life_and_other_point.py of other child

---
+++ last_time/other_week/part/other_government/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_week_about_group(str_arg):
+    have_place(str_arg)
+    print('problem')
+
+def have_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_week_about_group('part_and_young_place')
---
+++ world/year_or_early_world/few_part_and_week/life_and_other_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_large_world_from_great_eye(str_arg):
+    week_or_first_case(str_arg)
+    print('same_case')
+
+def week_or_first_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_large_world_from_great_eye('find_same_thing')
