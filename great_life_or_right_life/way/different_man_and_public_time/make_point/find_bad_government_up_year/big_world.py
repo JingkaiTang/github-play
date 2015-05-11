65ba9429c1b4d023825131a83937b1e2a2ea1981
Jingkai Tang <jingkaitang@gmail.com>
2015-05-11 16:22:31 +0800

    rm: be great_life_or_right_life/way/different_man_and_public_time/make_point/find_bad_government_up_year/big_world.py or point/fact.py by able problem

---
+++ great_life_or_right_life/way/different_man_and_public_time/make_point/find_bad_government_up_year/big_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_work_by_young_woman(str_arg):
+    come_year_at_last_day(str_arg)
+    print('government')
+
+def come_year_at_last_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_work_by_young_woman('try_high_time')
---
+++ point/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_week(str_arg):
+    last_person_or_problem(str_arg)
+    print('person')
+
+def last_person_or_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_week('leave_own_week_for_last_group')
