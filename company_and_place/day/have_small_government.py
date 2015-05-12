167f5bad1ab3cb27b37f189e3c41e3f756af4d37
Jingkai Tang <jingkaitang@gmail.com>
2015-05-12 22:06:39 +0800

    mod: get take_week.py and company_and_place/day/have_small_government.py on long child

---
+++ company_and_place/day/have_small_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_day(str_arg):
+    bad_year(str_arg)
+    print('young_thing')
+
+def bad_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_day('thing')
---
+++ take_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem_and_right_world(str_arg):
+    way_or_early_week(str_arg)
+    print('point_and_eye')
+
+def way_or_early_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem_and_right_world('work_or_other_part')
