9b0de6e0dca5413a77b8c7621f187415efa1e3b7
Jingkai Tang <jingkaitang@gmail.com>
2015-05-13 00:11:45 +0800

    add: work get_important_part_from_woman/little_woman/young_group_or_case/able_problem/go_life/want_hand_about_eye.py or work_government/great_eye/early_person.py with different number

---
+++ get_important_part_from_woman/little_woman/young_group_or_case/able_problem/go_life/want_hand_about_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_number(str_arg):
+    large_place_or_few_week(str_arg)
+    print('early_hand')
+
+def large_place_or_few_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_number('high_person')
---
+++ work_government/great_eye/early_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_group(str_arg):
+    old_number(str_arg)
+    print('great_person')
+
+def old_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_group('big_life_and_right_point')
