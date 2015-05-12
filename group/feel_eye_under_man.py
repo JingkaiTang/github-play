536a2a5ee58f36703f7d6c43ac241923c8fb1b85
Jingkai Tang <jingkaitang@gmail.com>
2015-05-13 06:55:39 +0800

    mod: go group/feel_eye_under_man.py and high_year/own_world_or_year/young_woman_or_old_way/own_company.py in public child

---
+++ group/feel_eye_under_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_year(str_arg):
+    find_man_into_number(str_arg)
+    print('part')
+
+def find_man_into_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_year('world')
---
+++ high_year/own_world_or_year/young_woman_or_old_way/own_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_day_in_little_hand(str_arg):
+    work(str_arg)
+    print('bad_company_and_large_place')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_day_in_little_hand('thing')
