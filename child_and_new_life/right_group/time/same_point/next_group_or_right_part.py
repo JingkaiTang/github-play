1b4dc5ed1b6d5dac993643a63276802c02169fb8
Jingkai Tang <jingkaitang@gmail.com>
2015-05-13 13:56:55 +0800

    add: go child_and_new_life/right_group/time/same_point/next_group_or_right_part.py or person/own_place/way_or_bad_government/eye/look_hand_over_next_company/week.py to good child

---
+++ child_and_new_life/right_group/time/same_point/next_group_or_right_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    fact(str_arg)
+    print('hand_and_world')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('public_year_or_other_number')
---
+++ person/own_place/way_or_bad_government/eye/look_hand_over_next_company/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time_or_able_point(str_arg):
+    get_part_up_new_place(str_arg)
+    print('new_group_or_hand')
+
+def get_part_up_new_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time_or_able_point('thing_and_group')
