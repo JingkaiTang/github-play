512d14d03ffeeb892023c1af200a1f7a5cc8ca8f
Jingkai Tang <jingkaitang@gmail.com>
2015-05-13 14:06:21 +0800

    mod: call first_place/government/important_government/old_eye.py over great thing

---
+++ first_place/government/important_government/old_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_fact(str_arg):
+    seem_right_thing_for_large_week(str_arg)
+    print('seem_hand_at_public_eye')
+
+def seem_right_thing_for_large_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_fact('little_work')
