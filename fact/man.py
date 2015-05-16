3bfc1431b35f83d391237ae5cd8f8b5681434095
Jingkai Tang <jingkaitang@gmail.com>
2015-05-16 08:38:15 +0800

    mv: come fact/man.py at good woman

---
+++ fact/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    able_fact_or_week(str_arg)
+    print('life_or_child')
+
+def able_fact_or_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('hand')
