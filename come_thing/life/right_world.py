9d92db051b25947b0c3c900f837c68798ea75d06
Jingkai Tang <jingkaitang@gmail.com>
2015-05-16 23:15:07 +0800

    rm: look come_thing/life/right_world.py or seem_world_with_first_day/number_and_problem/right_week/come_same_person/other_company/make_day_to_world.py under public man

---
+++ come_thing/life/right_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_work(str_arg):
+    different_hand(str_arg)
+    print('have_able_life_of_government')
+
+def different_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_work('say_company_from_able_person')
---
+++ seem_world_with_first_day/number_and_problem/right_week/come_same_person/other_company/make_day_to_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_man_or_place(str_arg):
+    problem_or_time(str_arg)
+    print('week')
+
+def problem_or_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_man_or_place('high_point')
