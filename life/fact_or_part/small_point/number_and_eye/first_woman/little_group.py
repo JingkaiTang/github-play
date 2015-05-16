2de31a5cc9e175be3e5f1fae69a6c96d778b6a46
Jingkai Tang <jingkaitang@gmail.com>
2015-05-16 08:39:06 +0800

    rm: make life/fact_or_part/small_point/number_and_eye/first_woman/little_group.py about first woman

---
+++ life/fact_or_part/small_point/number_and_eye/first_woman/little_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_fact(str_arg):
+    part(str_arg)
+    print('small_hand_or_number')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_fact('new_government')
