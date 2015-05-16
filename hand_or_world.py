81497efe88e416bb892ec9f6afd6ef70b40cdccd
Jingkai Tang <jingkaitang@gmail.com>
2015-05-16 09:11:04 +0800

    add: say hand_or_world.py at first day

---
+++ hand_or_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_company_and_case(str_arg):
+    life(str_arg)
+    print('last_company')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_company_and_case('great_hand')
