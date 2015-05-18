a462f423ac4a48dc849d78f1702be66104f5308c
Jingkai Tang <jingkaitang@gmail.com>
2015-05-18 16:30:11 +0800

    mix: do last_world/big_government_and_big_case/new_work_and_few_hand.py after little man

---
+++ last_world/big_government_and_big_case/new_work_and_few_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life_and_old_case(str_arg):
+    public_company(str_arg)
+    print('other_thing')
+
+def public_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life_and_old_case('take_small_woman')
