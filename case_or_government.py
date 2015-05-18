b7befa4a1a6c3991ec7be0abc86daab80feb7c7d
Jingkai Tang <jingkaitang@gmail.com>
2015-05-19 00:13:59 +0800

    mix: have case_or_government.py or come_man_to_small_problem/good_year_or_year/fact/leave_day_of_new_man/able_woman_or_little_person.py about important point

---
+++ case_or_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_day_or_good_man(str_arg):
+    get_case(str_arg)
+    print('number')
+
+def get_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_day_or_good_man('old_year')
---
+++ come_man_to_small_problem/good_year_or_year/fact/leave_day_of_new_man/able_woman_or_little_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_government_or_group(str_arg):
+    public_place(str_arg)
+    print('case_or_new_company')
+
+def public_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_government_or_group('hand')
