08e982a2baa5360b46aa16dc91bfa23c5d9436ad
Jingkai Tang <jingkaitang@gmail.com>
2015-05-18 13:43:28 +0800

    mod: ask different_fact_and_work/important_life.py under few place

---
+++ different_fact_and_work/important_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    give_group(str_arg)
+    print('life_and_group')
+
+def give_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('public_part_and_world')
