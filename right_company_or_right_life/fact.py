4fedd4c340be677ea9d28beee20f25805d817779
Jingkai Tang <jingkaitang@gmail.com>
2015-05-20 02:24:26 +0800

    mv: think right_company_or_right_life/fact.py for young hand

---
+++ right_company_or_right_life/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_day(str_arg):
+    place_or_problem(str_arg)
+    print('company')
+
+def place_or_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_day('have_last_week_on_case')
