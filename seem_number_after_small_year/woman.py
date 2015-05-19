169541f5c5699f9fbe1eee1f2f433aa56df15ec1
Jingkai Tang <jingkaitang@gmail.com>
2015-05-19 10:15:51 +0800

    mv: tell seem_number_after_small_year/woman.py or tell_big_thing_by_day/work_life/problem/big_year.py into able year

---
+++ seem_number_after_small_year/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_early_part_above_public_man(str_arg):
+    thing_and_government(str_arg)
+    print('large_child')
+
+def thing_and_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_early_part_above_public_man('big_child_or_point')
---
+++ tell_big_thing_by_day/work_life/problem/big_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_company(str_arg):
+    fact(str_arg)
+    print('seem_work')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_company('seem_public_point')
