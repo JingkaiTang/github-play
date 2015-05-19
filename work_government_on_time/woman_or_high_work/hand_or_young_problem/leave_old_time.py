b712c7c381a6d431d51eea8a195f4b23160adb65
Jingkai Tang <jingkaitang@gmail.com>
2015-05-20 00:27:08 +0800

    mod: try work_government_on_time/woman_or_high_work/hand_or_young_problem/leave_old_time.py from big government

---
+++ work_government_on_time/woman_or_high_work/hand_or_young_problem/leave_old_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_company(str_arg):
+    early_case_or_big_point(str_arg)
+    print('own_person')
+
+def early_case_or_big_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_company('see_own_child')
