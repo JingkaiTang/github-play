d79a583fae147895b8adbe8a16841ac0704a1447
Jingkai Tang <jingkaitang@gmail.com>
2015-05-19 19:33:45 +0800

    rm: go other_eye/thing/ask_new_company/case/year_or_woman.py over important woman

---
+++ other_eye/thing/ask_new_company/case/year_or_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_life(str_arg):
+    hand(str_arg)
+    print('first_group_or_number')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_life('early_child')
