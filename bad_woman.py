44f03e695ac968549d6b0f57730969dd31c24924
Jingkai Tang <jingkaitang@gmail.com>
2015-05-19 12:06:28 +0800

    rm: give bad_woman.py with own place

---
+++ bad_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_first_place(str_arg):
+    man(str_arg)
+    print('early_point_or_different_week')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_first_place('make_other_group')
