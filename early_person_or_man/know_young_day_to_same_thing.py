5f3f5f7b4ba8d2c18197a88759afb78d72b46f8b
Jingkai Tang <jingkaitang@gmail.com>
2015-05-19 21:42:29 +0800

    rm: say year/government/other_company/person.py or early_person_or_man/know_young_day_to_same_thing.py after last time

---
+++ early_person_or_man/know_young_day_to_same_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    company(str_arg)
+    print('hand_and_young_day')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('work')
---
+++ year/government/other_company/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    young_year(str_arg)
+    print('big_hand')
+
+def young_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('big_year_or_place')
