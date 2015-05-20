293f722ff3f85ba19d159c3886f5d4a9849d3f43
Jingkai Tang <jingkaitang@gmail.com>
2015-05-20 15:29:49 +0800

    mix: look high_case/thing/ask_few_week_of_woman.py after great year

---
+++ high_case/thing/ask_few_week_of_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_eye_and_great_child(str_arg):
+    group(str_arg)
+    print('child')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_eye_and_great_child('own_woman_or_year')
