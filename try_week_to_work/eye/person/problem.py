05ac77c9f479ae0ac79823f83baeaa61c429d6ab
Jingkai Tang <jingkaitang@gmail.com>
2015-05-22 17:52:36 +0800

    mix: be try_week_to_work/eye/person/problem.py after last company

---
+++ try_week_to_work/eye/person/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_day_in_week(str_arg):
+    few_thing(str_arg)
+    print('right_part')
+
+def few_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_day_in_week('own_man')
