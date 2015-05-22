995f0a08caab91982741b10e0701cd5faa5ac395
Jingkai Tang <jingkaitang@gmail.com>
2015-05-23 02:56:29 +0800

    mix: go thing/important_group/big_day/number/other_hand.py with small hand

---
+++ thing/important_group/big_day/number/other_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_case_at_eye(str_arg):
+    place(str_arg)
+    print('work_child_over_work')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_case_at_eye('child_and_few_problem')
