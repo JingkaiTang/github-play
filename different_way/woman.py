95d22d218dc6430218ccd25efd3336fc2df90984
Jingkai Tang <jingkaitang@gmail.com>
2015-05-23 15:24:36 +0800

    mix: tell different_way/woman.py up good point

---
+++ different_way/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world_or_public_place(str_arg):
+    case_or_work(str_arg)
+    print('few_group')
+
+def case_or_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world_or_public_place('woman')
