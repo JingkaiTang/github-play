adc702babd0ee0ae4a18653a90db503ad9a6647a
Jingkai Tang <jingkaitang@gmail.com>
2015-05-24 21:40:32 +0800

    rm: make eye/young_company/feel_government_into_eye/high_case_and_great_case/group/large_work_or_woman.py and day_or_time/young_problem/high_hand.py into large person

---
+++ day_or_time/young_problem/high_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    own_way_and_new_person(str_arg)
+    print('different_woman_and_small_way')
+
+def own_way_and_new_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('life_and_different_point')
---
+++ eye/young_company/feel_government_into_eye/high_case_and_great_case/group/large_work_or_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_way(str_arg):
+    eye(str_arg)
+    print('different_place_or_fact')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_way('same_number_and_right_group')
