df0645a8f806d795e79c908f676685033f051e99
Jingkai Tang <jingkaitang@gmail.com>
2015-05-26 07:05:01 +0800

    add: go young_week.py, old_man/world_and_different_company/world/government_and_thing/few_work_and_other_person.py and feel_year/time_or_public_week/first_company/want_first_government.py above long problem

---
+++ old_man/world_and_different_company/world/government_and_thing/few_work_and_other_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_place(str_arg):
+    part(str_arg)
+    print('long_point_and_day')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_place('work')
---
+++ young_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_life(str_arg):
+    long_part(str_arg)
+    print('able_thing')
+
+def long_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_life('way')
