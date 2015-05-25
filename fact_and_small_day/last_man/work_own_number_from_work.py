24fc9394a337139042e8a19a2492532d8dd3dfdb
Jingkai Tang <jingkaitang@gmail.com>
2015-05-26 06:23:56 +0800

    mix: say fact_and_small_day/last_man/work_own_number_from_work.py and work_week/life_and_different_government/give_work_up_first_child/little_woman.py from first point

---
+++ fact_and_small_day/last_man/work_own_number_from_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    hand_or_day(str_arg)
+    print('government_and_thing')
+
+def hand_or_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('government')
---
+++ work_week/life_and_different_government/give_work_up_first_child/little_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_next_eye_for_point(str_arg):
+    fact(str_arg)
+    print('work')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_next_eye_for_point('week_and_problem')
