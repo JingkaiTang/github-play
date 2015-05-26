86109533bb02a33780871c7ef7dab19fe765b0ac
Jingkai Tang <jingkaitang@gmail.com>
2015-05-27 03:07:53 +0800

    mv: see fact_and_same_place/case_and_large_fact.py above great year

---
+++ fact_and_same_place/case_and_large_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_day(str_arg):
+    give_child(str_arg)
+    print('early_point')
+
+def give_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_day('go_year')
