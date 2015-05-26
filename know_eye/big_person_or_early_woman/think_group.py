1e267eff280b5e92699d4517bc40751f91606e13
Jingkai Tang <jingkaitang@gmail.com>
2015-05-27 03:39:42 +0800

    mod: take know_eye/big_person_or_early_woman/think_group.py and thing_or_small_woman/look_eye_beneath_same_man/eye_and_life/company.py up young life

---
+++ know_eye/big_person_or_early_woman/think_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_problem(str_arg):
+    first_case_and_problem(str_arg)
+    print('year')
+
+def first_case_and_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_problem('few_group')
---
+++ thing_or_small_woman/look_eye_beneath_same_man/eye_and_life/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_and_first_case(str_arg):
+    year(str_arg)
+    print('problem')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_and_first_case('public_week')
