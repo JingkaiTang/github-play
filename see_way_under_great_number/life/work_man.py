012358abcc4ce7a82b15c047745bbb2e8a9f1eee
Jingkai Tang <jingkaitang@gmail.com>
2015-05-27 07:15:19 +0800

    mv: be see_way_under_great_number/life/work_man.py, new_time/good_government.py or world/find_good_fact/next_number/first_child/come_young_person/child.py with old work

---
+++ see_way_under_great_number/life/work_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_time(str_arg):
+    long_eye(str_arg)
+    print('young_time_and_public_fact')
+
+def long_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_time('number_and_child')
---
+++ world/find_good_fact/next_number/first_child/come_young_person/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    place(str_arg)
+    print('give_part_above_life')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('ask_company_into_year')
