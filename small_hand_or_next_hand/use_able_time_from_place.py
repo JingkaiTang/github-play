798e8328f81d8017319298748846018595eec224
Jingkai Tang <jingkaitang@gmail.com>
2015-05-27 13:00:57 +0800

    mix: give small_hand_or_next_hand/use_able_time_from_place.py of own place

---
+++ small_hand_or_next_hand/use_able_time_from_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_early_man(str_arg):
+    day_and_woman(str_arg)
+    print('point')
+
+def day_and_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_early_man('bad_problem_or_company')
