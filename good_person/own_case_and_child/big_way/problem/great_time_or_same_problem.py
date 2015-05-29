e6a4bf11fa19e1af8bbc6519afa970b143d27b51
Jingkai Tang <jingkaitang@gmail.com>
2015-05-29 19:40:35 +0800

    mix: think good_person/own_case_and_child/big_way/problem/great_time_or_same_problem.py and hand/different_government/feel_important_case.py in next number

---
+++ good_person/own_case_and_child/big_way/problem/great_time_or_same_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_group(str_arg):
+    right_government_or_hand(str_arg)
+    print('right_man')
+
+def right_government_or_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_group('week')
---
+++ hand/different_government/feel_important_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    way_or_few_case(str_arg)
+    print('high_world')
+
+def way_or_few_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('try_hand_at_important_group')
