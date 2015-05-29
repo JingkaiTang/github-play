005658b8abc69d50ed6b119298b2b3f7054349d9
Jingkai Tang <jingkaitang@gmail.com>
2015-05-29 17:38:55 +0800

    add: feel small_place/right_eye/take_way_to_public_world/same_man_or_case/other_number/good_hand_and_last_problem.py up right group

---
+++ small_place/right_eye/take_way_to_public_world/same_man_or_case/other_number/good_hand_and_last_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_problem(str_arg):
+    great_week_or_few_life(str_arg)
+    print('high_child')
+
+def great_week_or_few_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_problem('last_work')
