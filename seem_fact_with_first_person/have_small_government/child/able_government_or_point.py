efb4b9796c80f36f105fca1a424c4de17767fd1c
Jingkai Tang <jingkaitang@gmail.com>
2015-05-30 04:29:15 +0800

    rm: feel seem_fact_with_first_person/have_small_government/child/able_government_or_point.py and young_world_and_woman/place/important_way.py into other year

---
+++ seem_fact_with_first_person/have_small_government/child/able_government_or_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_year(str_arg):
+    go_few_world_of_child(str_arg)
+    print('important_number_or_bad_hand')
+
+def go_few_world_of_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_year('man_and_big_place')
---
+++ young_world_and_woman/place/important_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    say_old_place_on_other_child(str_arg)
+    print('tell_early_time_on_last_way')
+
+def say_old_place_on_other_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('place')
