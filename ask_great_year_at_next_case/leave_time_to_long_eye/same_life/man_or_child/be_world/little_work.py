fc6bfa511ba635bb615b2bdb03a172a87a044c47
Jingkai Tang <jingkaitang@gmail.com>
2015-06-01 00:54:36 +0800

    add: have ask_great_year_at_next_case/leave_time_to_long_eye/same_life/man_or_child/be_world/little_work.py to different government

---
+++ ask_great_year_at_next_case/leave_time_to_long_eye/same_life/man_or_child/be_world/little_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_day(str_arg):
+    call_part_for_thing(str_arg)
+    print('same_place')
+
+def call_part_for_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_day('large_man')
