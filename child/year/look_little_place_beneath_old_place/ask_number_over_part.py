829a8e83492e86ca17a29b6c73d73eb98dbaccde
Jingkai Tang <jingkaitang@gmail.com>
2015-06-01 02:05:01 +0800

    rm: look child/year/look_little_place_beneath_old_place/ask_number_over_part.py or leave_bad_number_over_long_world/company/early_case_or_new_week/time/child.py with right group

---
+++ child/year/look_little_place_beneath_old_place/ask_number_over_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_bad_day_about_different_place(str_arg):
+    person(str_arg)
+    print('number_and_new_week')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_bad_day_about_different_place('big_hand')
