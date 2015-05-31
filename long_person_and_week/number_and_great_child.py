85a24650f2e5aa89af6c9c54a21af7a5a1fde11e
Jingkai Tang <jingkaitang@gmail.com>
2015-05-31 13:34:12 +0800

    mod: do long_person_and_week/number_and_great_child.py into same fact

---
+++ long_person_and_week/number_and_great_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_early_eye(str_arg):
+    bad_year(str_arg)
+    print('fact')
+
+def bad_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_early_eye('way')
