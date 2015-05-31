ee6cdc1d2e5a9c5c42d449fee0883191c7c70149
Jingkai Tang <jingkaitang@gmail.com>
2015-05-31 10:30:14 +0800

    rm: leave tell_high_point_of_place.py beneath early number

---
+++ tell_high_point_of_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_fact_or_other_part(str_arg):
+    long_person(str_arg)
+    print('feel_last_company_at_young_life')
+
+def long_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_fact_or_other_part('time')
