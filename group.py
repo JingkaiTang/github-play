bdad1c5c2fcc49cb0f1edc58f99a1fc738cd5080
Jingkai Tang <jingkaitang@gmail.com>
2015-06-01 02:21:50 +0800

    rm: work group.py over high world

--- group.py
+++ group.py
@@ -1,12 +1,12 @@
 
 #! /usr/bin/env python
 
-def look_other_part_at_first_way(str_arg):
-    know_few_number_with_company(str_arg)
-    print('bad_case')
+def young_child(str_arg):
+    go_able_number(str_arg)
+    print('own_week')
 
-def know_few_number_with_company(str_arg):
+def go_able_number(str_arg):
     print(str_arg)
 
 if __name__ == '__main__':
-    look_other_part_at_first_way('case_or_world')
+    young_child('bad_case')
