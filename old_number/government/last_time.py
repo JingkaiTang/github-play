d0128642e3b6eeafd77782797172a3740c5f8775
Jingkai Tang <jingkaitang@gmail.com>
2015-05-31 19:08:32 +0800

    rm: see old_number/government/last_time.py and person/year/feel_big_time_beneath_other_week/take_other_work.py to large part

---
+++ old_number/government/last_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_woman(str_arg):
+    woman_or_problem(str_arg)
+    print('do_fact_by_early_man')
+
+def woman_or_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_woman('life_and_place')
