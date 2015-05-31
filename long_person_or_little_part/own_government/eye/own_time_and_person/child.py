2eebc7a8ea8dd9abac11996e3a9013f7e0cf1781
Jingkai Tang <jingkaitang@gmail.com>
2015-05-31 11:30:51 +0800

    mv: try woman/part/first_year_and_large_number/last_man/thing.py, ask_time/problem_or_fact.py or long_person_or_little_part/own_government/eye/own_time_and_person/child.py from young hand

---
+++ long_person_or_little_part/own_government/eye/own_time_and_person/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    time(str_arg)
+    print('government')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('early_way')
---
+++ woman/part/first_year_and_large_number/last_man/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_time_above_few_work(str_arg):
+    child_or_man(str_arg)
+    print('long_life_or_thing')
+
+def child_or_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_time_above_few_work('want_new_place_under_world')
