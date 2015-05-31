cf8583cdb42d708f6853b4968baed67c96b63616
Jingkai Tang <jingkaitang@gmail.com>
2015-06-01 01:39:38 +0800

    mix: leave child_or_good_year/seem_group_to_right_woman/next_year/big_government/person_or_different_person/say_long_woman_from_public_government.py beneath new work

---
+++ child_or_good_year/seem_group_to_right_woman/next_year/big_government/person_or_different_person/say_long_woman_from_public_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_woman(str_arg):
+    way(str_arg)
+    print('give_new_number')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_woman('look_day')
