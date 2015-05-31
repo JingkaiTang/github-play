8df964210f67a9b604aff03af6aaeed75bb5ef2b
Jingkai Tang <jingkaitang@gmail.com>
2015-06-01 04:00:59 +0800

    mix: want company/leave_child.py above few hand

---
+++ company/leave_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    know_own_thing(str_arg)
+    print('think_high_government')
+
+def know_own_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('place')
