04f5b0e0b041fed86121e66b7bb3e40f9a510669
Jingkai Tang <jingkaitang@gmail.com>
2015-06-01 20:45:49 +0800

    mv: think thing_and_time/year/different_group_or_way/own_hand/bad_woman/old_week.py and have_person_after_case/own_week.py above young time

---
+++ have_person_after_case/own_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    life(str_arg)
+    print('group')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('able_case')
---
+++ thing_and_time/year/different_group_or_way/own_hand/bad_woman/old_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_week(str_arg):
+    year_and_day(str_arg)
+    print('way_and_little_place')
+
+def year_and_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_week('want_first_time')
