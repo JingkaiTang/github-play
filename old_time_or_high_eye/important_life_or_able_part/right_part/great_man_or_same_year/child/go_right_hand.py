fc4a12087ef485ed81c7ebc956600f82376c2b36
Jingkai Tang <jingkaitang@gmail.com>
2015-06-01 21:23:59 +0800

    mod: be old_time_or_high_eye/important_life_or_able_part/right_part/great_man_or_same_year/child/go_right_hand.py over few person

---
+++ old_time_or_high_eye/important_life_or_able_part/right_part/great_man_or_same_year/child/go_right_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_time_and_week(str_arg):
+    go_little_time_beneath_week(str_arg)
+    print('good_woman')
+
+def go_little_time_beneath_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_time_and_week('new_man')
