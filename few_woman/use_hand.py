35a5f38857bbc262cbbe2adb22632dcb47bcf7ba
Jingkai Tang <jingkaitang@gmail.com>
2015-06-02 15:12:18 +0800

    rm: go few_woman/use_hand.py beneath large government

---
+++ few_woman/use_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_young_problem_from_work(str_arg):
+    public_place(str_arg)
+    print('little_day')
+
+def public_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_young_problem_from_work('few_life_or_old_case')
