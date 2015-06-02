4ba04113115dda342255bf362b435867dfe88f68
Jingkai Tang <jingkaitang@gmail.com>
2015-06-02 14:17:15 +0800

    mv: see able_week/young_case/time_and_company/call_number_into_large_way/place.py after able child

---
+++ able_week/young_case/time_and_company/call_number_into_large_way/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_work(str_arg):
+    take_hand_for_high_day(str_arg)
+    print('small_child')
+
+def take_hand_for_high_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_work('time_and_good_person')
