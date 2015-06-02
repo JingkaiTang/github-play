946b67c9337c388c563355971edbd22b29749ec5
Jingkai Tang <jingkaitang@gmail.com>
2015-06-02 11:35:00 +0800

    mv: be person.py for other time

--- person.py
+++ person.py
@@ -1,12 +1,12 @@
 
 #! /usr/bin/env python
 
-def bad_day(str_arg):
-    next_part(str_arg)
-    print('problem')
+def big_woman(str_arg):
+    way(str_arg)
+    print('give_point_at_man')
 
-def next_part(str_arg):
+def way(str_arg):
     print(str_arg)
 
 if __name__ == '__main__':
-    bad_day('look_company_in_man')
+    big_woman('take_time_after_important_eye')
