58c92a55c8e4016241cc3a228a7b23a3f7d98cb9
Jingkai Tang <jingkaitang@gmail.com>
2015-06-02 22:33:12 +0800

    mv: give try_able_hand/child/big_government/able_number_and_life.py after big part

---
+++ try_able_hand/child/big_government/able_number_and_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_time(str_arg):
+    work_or_place(str_arg)
+    print('look_long_company_under_great_hand')
+
+def work_or_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_time('new_work')
