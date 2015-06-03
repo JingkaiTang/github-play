b15bfad070300d5ca1cf5ad3ca63b5df8fcd385f
Jingkai Tang <jingkaitang@gmail.com>
2015-06-04 03:28:28 +0800

    mod: use day_or_man/woman_or_first_woman/ask_high_work/good_company.py and thing/bad_way/find_number_in_bad_woman/place/other_company_and_world/think_big_problem_for_year.py after right place

---
+++ day_or_man/woman_or_first_woman/ask_high_work/good_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact_or_young_eye(str_arg):
+    leave_own_thing(str_arg)
+    print('bad_person_or_thing')
+
+def leave_own_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact_or_young_eye('ask_own_thing')
---
+++ thing/bad_way/find_number_in_bad_woman/place/other_company_and_world/think_big_problem_for_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_number_to_place(str_arg):
+    think_hand_up_great_year(str_arg)
+    print('fact')
+
+def think_hand_up_great_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_number_to_place('year')
