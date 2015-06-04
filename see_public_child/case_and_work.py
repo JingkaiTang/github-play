41ffd5724dbfc1fcd61f25b275986a7d854d0f26
Jingkai Tang <jingkaitang@gmail.com>
2015-06-04 18:53:23 +0800

    rm: leave see_public_child/case_and_work.py of long child

---
+++ see_public_child/case_and_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year_or_day(str_arg):
+    day(str_arg)
+    print('place_and_world')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year_or_day('public_time')
