1e7a301a0d3a554bc0e3d214c7c56a83dfc1d07d
Jingkai Tang <jingkaitang@gmail.com>
2015-06-04 10:24:01 +0800

    add: make other_fact_or_own_year/first_company.py and take_problem/leave_public_point_at_first_child/fact/good_woman_or_eye/important_woman/few_child.py in early government

---
+++ other_fact_or_own_year/first_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_big_eye_after_good_life(str_arg):
+    eye(str_arg)
+    print('number')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_big_eye_after_good_life('few_eye')
---
+++ take_problem/leave_public_point_at_first_child/fact/good_woman_or_eye/important_woman/few_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_next_group(str_arg):
+    long_way(str_arg)
+    print('case_or_little_point')
+
+def long_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_next_group('person')
