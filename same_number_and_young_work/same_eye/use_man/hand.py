44dcf96cae46654650775d67c79ea44131f04eaa
Jingkai Tang <jingkaitang@gmail.com>
2015-06-05 07:27:37 +0800

    rm: ask same_number_and_young_work/same_eye/use_man/hand.py over next point

---
+++ same_number_and_young_work/same_eye/use_man/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    right_thing(str_arg)
+    print('number')
+
+def right_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('way')
