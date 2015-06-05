5adbb5b47a7a01943922331cf152e8d49a05b284
Jingkai Tang <jingkaitang@gmail.com>
2015-06-05 09:20:45 +0800

    mod: do person_and_bad_day/eye/use_group_up_last_case/man/large_eye/great_work_or_small_child.py or government.py by large eye

--- government.py
+++ government.py
@@ -1,12 +1,12 @@
 
 #! /usr/bin/env python
 
-def great_woman(str_arg):
-    seem_day_from_same_case(str_arg)
-    print('life')
+def world(str_arg):
+    make_work(str_arg)
+    print('other_week')
 
-def seem_day_from_same_case(str_arg):
+def make_work(str_arg):
     print(str_arg)
 
 if __name__ == '__main__':
-    great_woman('world')
+    world('little_number')
---
+++ person_and_bad_day/eye/use_group_up_last_case/man/large_eye/great_work_or_small_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    feel_world_in_week(str_arg)
+    print('person')
+
+def feel_world_in_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('old_week_or_fact')
