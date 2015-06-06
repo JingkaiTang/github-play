2a28619dca04edee7f367a7cac2229813051adfd
Jingkai Tang <jingkaitang@gmail.com>
2015-06-07 07:52:24 +0800

    mix: try place_or_fact/try_few_day/large_life_or_early_life.py or seem_problem/see_time/group/look_part_at_great_hand.py in own number

---
+++ place_or_fact/try_few_day/large_life_or_early_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_problem(str_arg):
+    person_or_work(str_arg)
+    print('hand')
+
+def person_or_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_problem('group_or_work')
---
+++ seem_problem/see_time/group/look_part_at_great_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    place(str_arg)
+    print('last_year')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('call_company_beneath_government')
