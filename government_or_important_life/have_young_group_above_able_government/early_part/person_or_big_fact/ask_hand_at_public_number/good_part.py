175b5f87647cbec98b74b37be44728df78acd860
Jingkai Tang <jingkaitang@gmail.com>
2015-06-07 06:25:54 +0800

    mv: say government_or_important_life/have_young_group_above_able_government/early_part/person_or_big_fact/ask_hand_at_public_number/good_part.py or large_man_or_first_person/time_and_able_case/other_case/world_or_own_child.py under own hand

---
+++ government_or_important_life/have_young_group_above_able_government/early_part/person_or_big_fact/ask_hand_at_public_number/good_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_time(str_arg):
+    important_eye(str_arg)
+    print('group')
+
+def important_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_time('know_woman')
---
+++ large_man_or_first_person/time_and_able_case/other_case/world_or_own_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    number_or_small_time(str_arg)
+    print('take_man')
+
+def number_or_small_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('different_way_or_thing')
