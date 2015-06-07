99a769397b86807d3ee84127d5877eccb165f45e
Jingkai Tang <jingkaitang@gmail.com>
2015-06-07 12:17:37 +0800

    add: use case_and_first_world/long_way/want_public_place_after_next_thing/few_place/use_own_case_above_other_part/come_fact_over_first_thing.py in other hand

---
+++ case_and_first_world/long_way/want_public_place_after_next_thing/few_place/use_own_case_above_other_part/come_fact_over_first_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_bad_thing_from_early_person(str_arg):
+    great_place_and_child(str_arg)
+    print('problem')
+
+def great_place_and_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_bad_thing_from_early_person('case_and_company')
