ba6090e40fc4be9c2d6e0332025fe57cfb9ed733
Jingkai Tang <jingkaitang@gmail.com>
2015-06-07 16:55:08 +0800

    mod: tell other_point_and_large_day/woman_and_young_number/old_company/child/big_work.py over high thing

---
+++ other_point_and_large_day/woman_and_young_number/old_company/child/big_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_bad_work_at_young_point(str_arg):
+    see_new_day_up_case(str_arg)
+    print('fact')
+
+def see_new_day_up_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_bad_work_at_young_point('old_world_and_big_thing')
