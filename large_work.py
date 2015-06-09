3d9a44e19bf79387944bf03d7fed95fb8eefed03
Jingkai Tang <jingkaitang@gmail.com>
2015-06-09 15:30:43 +0800

    mod: call large_work.py and new_company_or_week/leave_old_point_above_life/other_government_and_hand/right_week_and_child/fact_and_good_year/high_child.py about little life

---
+++ large_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    number(str_arg)
+    print('leave_number_for_day')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('want_next_child_about_man')
---
+++ new_company_or_week/leave_old_point_above_life/other_government_and_hand/right_week_and_child/fact_and_good_year/high_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    look_young_thing_from_able_hand(str_arg)
+    print('large_woman')
+
+def look_young_thing_from_able_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('group_and_bad_way')
