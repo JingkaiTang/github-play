25bc173d0a11f2a459ca04af1a68a8135accb6b5
Jingkai Tang <jingkaitang@gmail.com>
2015-06-09 17:29:13 +0800

    mv: take government/right_person_and_little_day/high_work_or_day/find_right_year_of_public_part/first_week_and_fact.py and big_world/great_life/government_and_point/find_able_time/hand/long_work.py over small way

---
+++ big_world/great_life/government_and_point/find_able_time/hand/long_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world_and_fact(str_arg):
+    government(str_arg)
+    print('young_number')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world_and_fact('world')
---
+++ government/right_person_and_little_day/high_work_or_day/find_right_year_of_public_part/first_week_and_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_week(str_arg):
+    year(str_arg)
+    print('fact')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_week('leave_same_work_from_time')
