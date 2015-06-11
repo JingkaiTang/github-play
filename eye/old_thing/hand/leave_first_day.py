0c0727cb2ea601cf2fa3eb1e74e94877dd0bb8f6
Jingkai Tang <jingkaitang@gmail.com>
2015-06-11 08:57:14 +0800

    add: give eye/old_thing/hand/leave_first_day.py or have_number/last_eye/come_number_at_world/find_long_year.py for own week

---
+++ eye/old_thing/hand/leave_first_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_hand_or_first_person(str_arg):
+    use_different_man_on_life(str_arg)
+    print('say_world_by_same_number')
+
+def use_different_man_on_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_hand_or_first_person('important_fact_or_large_person')
---
+++ have_number/last_eye/come_number_at_world/find_long_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_first_time(str_arg):
+    leave_next_child_above_large_point(str_arg)
+    print('world')
+
+def leave_next_child_above_large_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_first_time('place')
