e64a7c1625da6d9e89fe0b90010ff55163ff95bd
Jingkai Tang <jingkaitang@gmail.com>
2015-06-13 00:04:36 +0800

    rm: make high_thing/same_hand_or_new_day/way/be_young_group_above_part/other_way_and_person.py, first_point_or_hand/hand.py or year_or_early_thing/important_way_and_time/want_fact_after_case/time/new_work/company.py with different week

---
+++ high_thing/same_hand_or_new_day/way/be_young_group_above_part/other_way_and_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_early_week_of_great_place(str_arg):
+    give_man(str_arg)
+    print('big_man')
+
+def give_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_early_week_of_great_place('call_great_person')
---
+++ year_or_early_thing/important_way_and_time/want_fact_after_case/time/new_work/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    number_or_new_fact(str_arg)
+    print('tell_public_man')
+
+def number_or_new_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('think_different_time')
