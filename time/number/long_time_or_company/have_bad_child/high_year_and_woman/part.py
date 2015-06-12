7d7c396c34560bcced9adb2d4d568cd0aca4b665
Jingkai Tang <jingkaitang@gmail.com>
2015-06-12 08:14:25 +0800

    mod: come man_or_old_world.py and time/number/long_time_or_company/have_bad_child/high_year_and_woman/part.py on new world

---
+++ time/number/long_time_or_company/have_bad_child/high_year_and_woman/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_problem_for_part(str_arg):
+    time(str_arg)
+    print('fact_or_important_part')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_problem_for_part('get_same_thing')
