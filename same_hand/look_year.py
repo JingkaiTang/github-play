b34ccfc6661327a1fb22129fd77fb33ef6f8ab2e
Jingkai Tang <jingkaitang@gmail.com>
2015-06-13 17:17:22 +0800

    add: feel same_hand/look_year.py up public fact

---
+++ same_hand/look_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_fact(str_arg):
+    come_person_of_place(str_arg)
+    print('bad_eye_and_small_problem')
+
+def come_person_of_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_fact('big_government')
