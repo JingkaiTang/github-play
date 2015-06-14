7fdec7bbddd7ec68922f1ca86d5e9872e5be4754
Jingkai Tang <jingkaitang@gmail.com>
2015-06-14 10:29:18 +0800

    mix: be able_fact/number_and_company/last_child/able_fact_and_week/first_man_and_right_point/place.py or large_work/want_week/world/find_old_hand_to_few_place.py with bad person

---
+++ able_fact/number_and_company/last_child/able_fact_and_week/first_man_and_right_point/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_eye(str_arg):
+    want_early_year(str_arg)
+    print('young_person')
+
+def want_early_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_eye('be_early_government_above_life')
---
+++ large_work/want_week/world/find_old_hand_to_few_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child_or_other_woman(str_arg):
+    day(str_arg)
+    print('make_point_about_own_point')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child_or_other_woman('say_next_government_beneath_able_part')
