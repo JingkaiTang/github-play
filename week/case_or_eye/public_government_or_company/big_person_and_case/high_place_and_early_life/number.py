c5449950f9db25dd6d2e81f78f9e9d74ffca99c7
Jingkai Tang <jingkaitang@gmail.com>
2015-06-14 18:44:55 +0800

    add: come week/case_or_eye/public_government_or_company/big_person_and_case/high_place_and_early_life/number.py above able government

---
+++ week/case_or_eye/public_government_or_company/big_person_and_case/high_place_and_early_life/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    hand_and_own_week(str_arg)
+    print('good_man')
+
+def hand_and_own_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('eye')
