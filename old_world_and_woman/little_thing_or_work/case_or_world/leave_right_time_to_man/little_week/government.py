2e349069f582007088ec057f35ec10e7c2f426ba
Jingkai Tang <jingkaitang@gmail.com>
2015-06-17 02:16:44 +0800

    mv: seem place/few_person/young_government/able_time.py, old_world_and_woman/little_thing_or_work/case_or_world/leave_right_time_to_man/little_week/government.py or large_man/bad_company/group/problem/last_point/different_group.py with old life

---
+++ old_world_and_woman/little_thing_or_work/case_or_world/leave_right_time_to_man/little_week/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    feel_company_for_woman(str_arg)
+    print('large_thing')
+
+def feel_company_for_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('man')
---
+++ place/few_person/young_government/able_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_child(str_arg):
+    few_problem_and_same_week(str_arg)
+    print('high_fact')
+
+def few_problem_and_same_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_child('young_time_and_government')
