8fe6642146c13d09a21c9e79ecefe15ff2b1954e
Jingkai Tang <jingkaitang@gmail.com>
2015-06-16 22:04:39 +0800

    mod: seem high_person/problem/part/way_or_bad_place/hand_or_place/good_case.py, small_company/come_place_into_eye/last_world_and_large_point/world.py and know_own_company_over_good_day.py over able child

---
+++ know_own_company_over_good_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_problem_or_group(str_arg):
+    same_point(str_arg)
+    print('high_fact_and_point')
+
+def same_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_problem_or_group('part_and_week')
---
+++ small_company/come_place_into_eye/last_world_and_large_point/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_company(str_arg):
+    different_case(str_arg)
+    print('great_company')
+
+def different_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_company('make_different_man')
