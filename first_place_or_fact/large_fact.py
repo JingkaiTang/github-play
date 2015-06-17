6998240ad1a91847c700bc9ced704a985c46f474
Jingkai Tang <jingkaitang@gmail.com>
2015-06-18 07:52:57 +0800

    mod: see first_place_or_fact/large_fact.py from next place

---
+++ first_place_or_fact/large_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    problem_and_public_part(str_arg)
+    print('fact_or_part')
+
+def problem_and_public_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('place_or_way')
