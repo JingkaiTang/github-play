52fa2997c7328d44042a1de69ae247776f1552d7
Jingkai Tang <jingkaitang@gmail.com>
2015-06-17 08:15:30 +0800

    mod: take time_and_own_government/group_or_thing/child/large_company/few_company_and_eye/fact_or_little_part.py under bad case

---
+++ time_and_own_government/group_or_thing/child/large_company/few_company_and_eye/fact_or_little_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_same_year_about_own_number(str_arg):
+    way(str_arg)
+    print('woman')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_same_year_about_own_number('large_problem_and_public_point')
