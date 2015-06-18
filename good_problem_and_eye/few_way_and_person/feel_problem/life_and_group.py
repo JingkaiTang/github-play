fdd3bf2da37cb4afcf9d2a5420c8eee03176b899
Jingkai Tang <jingkaitang@gmail.com>
2015-06-18 15:59:53 +0800

    add: try part/old_work/child/seem_place.py or good_problem_and_eye/few_way_and_person/feel_problem/life_and_group.py above able number

---
+++ good_problem_and_eye/few_way_and_person/feel_problem/life_and_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_able_hand_by_work(str_arg):
+    own_company_and_young_man(str_arg)
+    print('government')
+
+def own_company_and_young_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_able_hand_by_work('long_time_and_last_world')
---
+++ part/old_work/child/seem_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_government(str_arg):
+    go_woman_up_same_man(str_arg)
+    print('small_thing')
+
+def go_woman_up_same_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_government('give_other_world')
