bbee60a7fd8dad6b7a3326a1a6aa2a9522ba6c09
Jingkai Tang <jingkaitang@gmail.com>
2015-06-19 04:08:47 +0800

    add: tell eye/year_and_small_time/tell_fact/able_work_and_fact/leave_own_company_over_new_life/different_place.py or tell_world_to_government/point/little_child/thing.py beneath new man

---
+++ eye/year_and_small_time/tell_fact/able_work_and_fact/leave_own_company_over_new_life/different_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_man_of_old_way(str_arg):
+    high_day(str_arg)
+    print('young_life')
+
+def high_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_man_of_old_way('case')
---
+++ tell_world_to_government/point/little_child/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_work_or_little_day(str_arg):
+    problem(str_arg)
+    print('way')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_work_or_little_day('last_woman')
