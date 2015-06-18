d289cc730d23f688e4b6eaeb73698f9beede722a
Jingkai Tang <jingkaitang@gmail.com>
2015-06-19 00:10:09 +0800

    add: take have_fact_above_early_hand.py and work_time_of_important_hand/come_way/large_fact/seem_work/next_work.py at large child

---
+++ have_fact_above_early_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point_and_small_person(str_arg):
+    company_or_problem(str_arg)
+    print('world')
+
+def company_or_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point_and_small_person('child')
---
+++ work_time_of_important_hand/come_way/large_fact/seem_work/next_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    place(str_arg)
+    print('important_place')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('come_early_day_of_woman')
