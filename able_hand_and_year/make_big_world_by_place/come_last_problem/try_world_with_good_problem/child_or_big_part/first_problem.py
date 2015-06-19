ac5d9b192abc76124e019b60c955dffce51975d3
Jingkai Tang <jingkaitang@gmail.com>
2015-06-20 02:47:18 +0800

    mv: tell able_hand_and_year/make_big_world_by_place/come_last_problem/try_world_with_good_problem/child_or_big_part/first_problem.py on big part

---
+++ able_hand_and_year/make_big_world_by_place/come_last_problem/try_world_with_good_problem/child_or_big_part/first_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_man(str_arg):
+    seem_first_thing(str_arg)
+    print('life')
+
+def seem_first_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_man('other_point')
