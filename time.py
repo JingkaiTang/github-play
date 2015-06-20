4c3b8e9cadb91cf12fcaf6ca6d3d5d582a6d3304
Jingkai Tang <jingkaitang@gmail.com>
2015-06-21 01:11:30 +0800

    mv: know time.py for own world

--- time.py
+++ time.py
@@ -1,12 +1,12 @@
 
 #! /usr/bin/env python
 
-def take_right_number(str_arg):
-    place_and_life(str_arg)
-    print('company_and_great_place')
+def new_life(str_arg):
+    important_company(str_arg)
+    print('way')
 
-def place_and_life(str_arg):
+def important_company(str_arg):
     print(str_arg)
 
 if __name__ == '__main__':
-    take_right_number('number_or_able_way')
+    new_life('public_government_or_eye')
