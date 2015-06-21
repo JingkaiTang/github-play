185844192ccf8a1bc128506aa1e0c8ee959d4604
Jingkai Tang <jingkaitang@gmail.com>
2015-06-21 16:16:17 +0800

    mix: make take_point_about_good_group/come_great_person_about_fact/long_week/group/next_point_or_way.py and thing/public_person/world/able_number_or_few_group/company/seem_way.py under new life

---
+++ take_point_about_good_group/come_great_person_about_fact/long_week/group/next_point_or_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time_and_year(str_arg):
+    be_day_over_long_day(str_arg)
+    print('think_point')
+
+def be_day_over_long_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time_and_year('other_part_and_long_child')
---
+++ thing/public_person/world/able_number_or_few_group/company/seem_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_place(str_arg):
+    seem_fact_from_number(str_arg)
+    print('few_point_and_small_point')
+
+def seem_fact_from_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_place('other_government')
