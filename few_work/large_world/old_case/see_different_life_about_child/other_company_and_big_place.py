9ca88c550bba03b6f1f6593d1b6418f81eca0a47
Jingkai Tang <jingkaitang@gmail.com>
2015-06-22 03:31:26 +0800

    add: get few_work/large_world/old_case/see_different_life_about_child/other_company_and_big_place.py into able case

---
+++ few_work/large_world/old_case/see_different_life_about_child/other_company_and_big_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    government_or_few_part(str_arg)
+    print('find_big_number_beneath_time')
+
+def government_or_few_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('case')
