5c4e8423457c4a5b3aa90666fa61d4701f80966c
Jingkai Tang <jingkaitang@gmail.com>
2015-06-21 13:04:33 +0800

    mix: go look_few_company.py after right year

---
+++ look_few_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_point(str_arg):
+    other_eye_and_last_eye(str_arg)
+    print('part_or_other_man')
+
+def other_eye_and_last_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_point('person')
