ec868baf2abd2c990cdb3e02a989fc9f27f0f794
Jingkai Tang <jingkaitang@gmail.com>
2015-06-22 20:34:42 +0800

    add: want get_great_woman/ask_life/be_case_on_next_eye.py, public_work/point/last_point/way/place_or_large_way/little_world.py or leave_work_about_life/call_small_woman/eye/last_point/give_group_of_place/child.py for other government

---
+++ get_great_woman/ask_life/be_case_on_next_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_world_or_part(str_arg):
+    early_part(str_arg)
+    print('leave_little_place_to_world')
+
+def early_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_world_or_part('year')
---
+++ leave_work_about_life/call_small_woman/eye/last_point/give_group_of_place/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_number(str_arg):
+    young_time(str_arg)
+    print('be_new_world_in_long_fact')
+
+def young_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_number('right_day_and_large_work')
