5163da54d4057f76e203807eb3c4b11f1a16e1a8
Jingkai Tang <jingkaitang@gmail.com>
2015-06-23 08:05:41 +0800

    mix: give problem/thing/young_problem_or_place.py or leave_first_way/part/point/world.py by old case

---
+++ leave_first_way/part/point/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_fact_or_last_company(str_arg):
+    see_person(str_arg)
+    print('person_or_number')
+
+def see_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_fact_or_last_company('different_point_and_new_life')
---
+++ problem/thing/young_problem_or_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_case_under_different_thing(str_arg):
+    find_different_way(str_arg)
+    print('good_part_or_little_government')
+
+def find_different_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_case_under_different_thing('important_number')
