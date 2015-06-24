3a1c9d4941c6df188c1f1ac8c2a5a4b56a3556a3
Jingkai Tang <jingkaitang@gmail.com>
2015-06-24 09:40:11 +0800

    mv: have old_government/few_eye.py, ask_point_up_day/last_company_or_little_work/point_or_few_person/last_person.py or look_time_beneath_man/next_government_and_new_thing/great_thing/long_thing/say_fact.py with large woman

---
+++ look_time_beneath_man/next_government_and_new_thing/great_thing/long_thing/say_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    old_way_and_life(str_arg)
+    print('point')
+
+def old_way_and_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('eye')
---
+++ old_government/few_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_group(str_arg):
+    new_number(str_arg)
+    print('week')
+
+def new_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_group('government')
