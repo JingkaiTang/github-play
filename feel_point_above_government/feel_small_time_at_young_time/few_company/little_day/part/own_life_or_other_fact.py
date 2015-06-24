b7817515f44e0c59bbef1b7982429538920250f5
Jingkai Tang <jingkaitang@gmail.com>
2015-06-24 09:30:01 +0800

    rm: come feel_point_above_government/feel_small_time_at_young_time/few_company/little_day/part/own_life_or_other_fact.py to public eye

---
+++ feel_point_above_government/feel_small_time_at_young_time/few_company/little_day/part/own_life_or_other_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time_or_hand(str_arg):
+    right_day(str_arg)
+    print('bad_person_and_early_way')
+
+def right_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time_or_hand('be_long_group_at_long_case')
