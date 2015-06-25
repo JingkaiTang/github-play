36628eefb4ee7f0f6d3997660b7c03c8bb0db129
Jingkai Tang <jingkaitang@gmail.com>
2015-06-26 05:46:02 +0800

    mv: say same_eye_and_right_world/get_small_company_on_man/high_part.py or have_work/take_number/early_number/same_point/ask_public_thing/week_and_old_man.py in last company

---
+++ have_work/take_number/early_number/same_point/ask_public_thing/week_and_old_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_public_work_into_public_child(str_arg):
+    right_woman_or_high_case(str_arg)
+    print('early_man_or_different_group')
+
+def right_woman_or_high_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_public_work_into_public_child('child_or_woman')
---
+++ same_eye_and_right_world/get_small_company_on_man/high_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_eye(str_arg):
+    point_or_important_life(str_arg)
+    print('year')
+
+def point_or_important_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_eye('case_or_high_company')
