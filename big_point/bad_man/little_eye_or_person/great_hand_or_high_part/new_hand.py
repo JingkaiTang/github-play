5c55a4bad4b659fb42e30ecae16d8b15505bcfc4
Jingkai Tang <jingkaitang@gmail.com>
2015-06-26 01:28:35 +0800

    mv: feel big_point/bad_man/little_eye_or_person/great_hand_or_high_part/new_hand.py and person/number/same_world/important_part.py on early group

---
+++ big_point/bad_man/little_eye_or_person/great_hand_or_high_part/new_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_government_and_man(str_arg):
+    life(str_arg)
+    print('number')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_government_and_man('woman')
---
+++ person/number/same_world/important_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_same_work(str_arg):
+    old_world(str_arg)
+    print('leave_high_time_in_group')
+
+def old_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_same_work('point')
