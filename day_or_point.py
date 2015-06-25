f1dd41fdb5d62fece55ca20d3b2cfc677f15b567
Jingkai Tang <jingkaitang@gmail.com>
2015-06-25 10:48:18 +0800

    add: be day_or_point.py of next group

---
+++ day_or_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_point_into_important_work(str_arg):
+    right_thing(str_arg)
+    print('way')
+
+def right_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_point_into_important_work('have_hand_in_few_work')
