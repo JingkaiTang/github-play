c5c279c7f80f0724ae5274df78c03da27baca535
Jingkai Tang <jingkaitang@gmail.com>
2015-06-26 06:03:55 +0800

    mv: know woman_and_high_year/first_life/come_way.py and last_number/have_person_after_person/work_important_point/next_person.py about bad company

---
+++ last_number/have_person_after_person/work_important_point/next_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_hand_beneath_week(str_arg):
+    call_high_point_on_first_week(str_arg)
+    print('next_case_and_way')
+
+def call_high_point_on_first_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_hand_beneath_week('work')
---
+++ woman_and_high_year/first_life/come_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_thing(str_arg):
+    look_point_from_right_company(str_arg)
+    print('place')
+
+def look_point_from_right_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_thing('make_other_government_into_early_company')
