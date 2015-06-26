85ec90a23afc5534e12b9d44c5c9c95fb804e0c5
Jingkai Tang <jingkaitang@gmail.com>
2015-06-26 14:12:36 +0800

    mv: ask public_thing/try_young_week_to_little_time/week_and_first_hand/different_government.py and hand_or_number/able_part_and_time/own_part/first_eye.py to new fact

---
+++ public_thing/try_young_week_to_little_time/week_and_first_hand/different_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_hand_or_big_thing(str_arg):
+    group_or_world(str_arg)
+    print('think_point_for_old_point')
+
+def group_or_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_hand_or_big_thing('person_and_place')
