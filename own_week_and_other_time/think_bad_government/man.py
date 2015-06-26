92abd2a4d25b204ef739a31d5ae99171836951c7
Jingkai Tang <jingkaitang@gmail.com>
2015-06-26 08:36:49 +0800

    mv: seem own_week_and_other_time/think_bad_government/man.py under public point

---
+++ own_week_and_other_time/think_bad_government/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_year(str_arg):
+    group(str_arg)
+    print('place_or_right_company')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_year('time')
