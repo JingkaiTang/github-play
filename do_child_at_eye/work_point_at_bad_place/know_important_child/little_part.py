655bf84e4612e5fad6ee1ac72cbafafada0b220a
Jingkai Tang <jingkaitang@gmail.com>
2015-07-01 06:52:26 +0800

    mv: say do_child_at_eye/work_point_at_bad_place/know_important_child/little_part.py on own eye

---
+++ do_child_at_eye/work_point_at_bad_place/know_important_child/little_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_own_case_into_woman(str_arg):
+    right_child(str_arg)
+    print('give_big_way')
+
+def right_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_own_case_into_woman('right_person')
