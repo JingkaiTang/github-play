5c40147da1549764e929845413154aee7836f318
Jingkai Tang <jingkaitang@gmail.com>
2015-06-30 13:52:06 +0800

    mix: seem big_year/young_hand/way/say_hand.py, own_year/man_and_day.py or other_number/person_and_world/say_public_way_on_world/week_and_other_woman/young_case_and_person/call_fact.py beneath long work

---
+++ big_year/young_hand/way/say_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_number_or_fact(str_arg):
+    time_and_work(str_arg)
+    print('ask_great_year_after_work')
+
+def time_and_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_number_or_fact('look_thing')
---
+++ own_year/man_and_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand_or_small_world(str_arg):
+    use_young_case_for_company(str_arg)
+    print('last_fact_or_life')
+
+def use_young_case_for_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand_or_small_world('tell_group')
