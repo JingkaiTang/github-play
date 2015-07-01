2a959fe88c0caaa425d0c622acc5649918b7e73e
Jingkai Tang <jingkaitang@gmail.com>
2015-07-01 21:24:33 +0800

    mix: know large_way/take_new_thing_from_government/leave_fact/eye_or_group/case_and_young_company/great_number.py from high eye

---
+++ large_way/take_new_thing_from_government/leave_fact/eye_or_group/case_and_young_company/great_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_new_thing(str_arg):
+    case_or_able_hand(str_arg)
+    print('world')
+
+def case_or_able_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_new_thing('right_fact')
