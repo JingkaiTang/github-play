fa79dd5d270848460641fc2e5cf849a2edbd3941
Jingkai Tang <jingkaitang@gmail.com>
2015-07-02 05:21:58 +0800

    mod: try big_person_or_good_group.py to important person

---
+++ big_person_or_good_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    call_time(str_arg)
+    print('life')
+
+def call_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('child')
