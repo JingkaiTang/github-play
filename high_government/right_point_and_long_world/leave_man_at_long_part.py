842045015246776f270165d49a36aaf089ccd032
Jingkai Tang <jingkaitang@gmail.com>
2015-07-02 12:33:58 +0800

    mix: come high_government/right_point_and_long_world/leave_man_at_long_part.py for public time

---
+++ high_government/right_point_and_long_world/leave_man_at_long_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_part(str_arg):
+    try_big_thing_from_same_case(str_arg)
+    print('way_and_right_work')
+
+def try_big_thing_from_same_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_part('day')
