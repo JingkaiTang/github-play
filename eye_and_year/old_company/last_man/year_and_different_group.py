01a01c2f7a56651ac0b4f3674723ab2b5fe047bd
Jingkai Tang <jingkaitang@gmail.com>
2015-07-03 07:17:45 +0800

    mix: feel eye_and_year/old_company/last_man/year_and_different_group.py and use_long_man/woman/week/world_and_small_woman/long_year_or_point/world_or_group.py for right child

---
+++ eye_and_year/old_company/last_man/year_and_different_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_part(str_arg):
+    seem_new_case(str_arg)
+    print('case')
+
+def seem_new_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_part('same_year_and_new_year')
---
+++ use_long_man/woman/week/world_and_small_woman/long_year_or_point/world_or_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_year(str_arg):
+    thing_and_big_case(str_arg)
+    print('see_able_number_up_child')
+
+def thing_and_big_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_year('different_hand')
