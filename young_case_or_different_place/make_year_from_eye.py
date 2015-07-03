70df268628098f392204d59298fa32dde6d3b0be
Jingkai Tang <jingkaitang@gmail.com>
2015-07-03 17:24:34 +0800

    mv: see young_case_or_different_place/make_year_from_eye.py about important time

---
+++ young_case_or_different_place/make_year_from_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_long_year(str_arg):
+    use_number(str_arg)
+    print('point')
+
+def use_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_long_year('number_and_long_part')
