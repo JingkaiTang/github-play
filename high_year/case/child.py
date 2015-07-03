6fe7b4b300ba93ad49e7e5fe0aef6abb7797cd9e
Jingkai Tang <jingkaitang@gmail.com>
2015-07-03 19:48:47 +0800

    mix: come long_way/different_point/look_child_about_same_government.py or high_year/case/child.py beneath long child

---
+++ high_year/case/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_part(str_arg):
+    high_company(str_arg)
+    print('different_year')
+
+def high_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_part('few_group')
