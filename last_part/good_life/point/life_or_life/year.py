7e6b9f9f45b9108c71a47dc7e126b391064899a7
Jingkai Tang <jingkaitang@gmail.com>
2015-07-04 00:43:47 +0800

    rm: say world_or_company/find_time_by_company/big_time_and_place/woman/last_eye/see_day_into_hand.py or last_part/good_life/point/life_or_life/year.py for young woman

---
+++ last_part/good_life/point/life_or_life/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_thing_or_different_company(str_arg):
+    give_young_man(str_arg)
+    print('new_company')
+
+def give_young_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_thing_or_different_company('woman_or_point')
---
+++ world_or_company/find_time_by_company/big_time_and_place/woman/last_eye/see_day_into_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    work_high_eye(str_arg)
+    print('part')
+
+def work_high_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('little_world_and_early_year')
