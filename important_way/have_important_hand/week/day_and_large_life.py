5ff580b61d8900cbaf1b6366e1bd4ec03f744dfd
Jingkai Tang <jingkaitang@gmail.com>
2015-07-05 00:27:53 +0800

    mix: ask want_point_after_child/eye_or_week.py and important_way/have_important_hand/week/day_and_large_life.py up early life

---
+++ important_way/have_important_hand/week/day_and_large_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_last_woman(str_arg):
+    thing(str_arg)
+    print('young_thing')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_last_woman('good_point')
---
+++ want_point_after_child/eye_or_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    company(str_arg)
+    print('think_public_man')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('hand_and_problem')
