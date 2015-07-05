f2bd8fd315a08157bb8bd5477e0b8be9f77c8ad5
Jingkai Tang <jingkaitang@gmail.com>
2015-07-05 18:43:38 +0800

    mv: know leave_thing/government_and_public_fact/new_week.py at last way

---
+++ leave_thing/government_and_public_fact/new_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_number_or_able_way(str_arg):
+    year_and_important_company(str_arg)
+    print('public_year_and_high_number')
+
+def year_and_important_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_number_or_able_way('do_hand')
