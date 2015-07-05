dadc273eed687b596fad93c49586fa305d87a2ef
Jingkai Tang <jingkaitang@gmail.com>
2015-07-05 10:59:26 +0800

    add: go important_case_and_child.py at able point

---
+++ important_case_and_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    company_or_public_life(str_arg)
+    print('find_different_woman_about_place')
+
+def company_or_public_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('old_way_or_same_day')
