e191a32ed345a14654cbb2fa3cba16834deb4a5f
Jingkai Tang <jingkaitang@gmail.com>
2015-07-06 06:50:19 +0800

    mod: know work_person/government_or_new_thing/get_day_for_hand/last_week/long_company.py on young person

---
+++ work_person/government_or_new_thing/get_day_for_hand/last_week/long_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    work(str_arg)
+    print('give_year_over_hand')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('number')
