b7efce7a7670acf28ac0e5f6ff6bb3489138aadd
Jingkai Tang <jingkaitang@gmail.com>
2015-07-06 17:32:27 +0800

    mod: make find_case_by_important_week/number/great_week/give_place_on_young_case/time.py to old woman

---
+++ find_case_by_important_week/number/great_week/give_place_on_young_case/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    know_important_world_with_different_day(str_arg)
+    print('feel_public_week_up_last_person')
+
+def know_important_world_with_different_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('point')
