93a597ce3ca2dc8e34d98a92c109e4c4306b5ff8
Jingkai Tang <jingkaitang@gmail.com>
2015-07-06 12:50:59 +0800

    add: make number/thing/early_eye/few_work.py and case/case/say_fact_about_number/group.py under right place

---
+++ case/case/say_fact_about_number/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    problem(str_arg)
+    print('number_and_week')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('little_company_and_thing')
---
+++ number/thing/early_eye/few_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    group(str_arg)
+    print('life')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('year_and_bad_person')
