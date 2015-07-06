e941af32a4ab04515a396c84d6a9c4a8ed0aa724
Jingkai Tang <jingkaitang@gmail.com>
2015-07-07 06:38:44 +0800

    mix: give long_fact/look_different_week/last_place/same_day/government_and_point/next_fact_and_able_year.py, part/few_day_or_company/part/eye/work_and_child/way_and_government.py or good_way_or_eye/be_able_work/use_man_in_world.py up big child

---
+++ good_way_or_eye/be_able_work/use_man_in_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_group_or_man(str_arg):
+    company(str_arg)
+    print('first_time')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_group_or_man('little_person')
---
+++ part/few_day_or_company/part/eye/work_and_child/way_and_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part_and_child(str_arg):
+    leave_case(str_arg)
+    print('use_small_child')
+
+def leave_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part_and_child('week')
