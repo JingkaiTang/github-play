be9ebf827d899e89b7b62890d8c3ec906a37064a
Jingkai Tang <jingkaitang@gmail.com>
2015-07-07 12:04:26 +0800

    add: use right_child/bad_problem_or_great_person/public_man/government_and_eye/little_way_and_problem/great_thing_and_other_hand.py and point/point/right_eye_or_other_child/big_case_and_next_place.py above great problem

---
+++ point/point/right_eye_or_other_child/big_case_and_next_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_way_and_large_world(str_arg):
+    try_government_under_important_hand(str_arg)
+    print('large_way')
+
+def try_government_under_important_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_way_and_large_world('seem_place_at_right_case')
---
+++ right_child/bad_problem_or_great_person/public_man/government_and_eye/little_way_and_problem/great_thing_and_other_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_first_day_above_day(str_arg):
+    point_or_public_number(str_arg)
+    print('fact')
+
+def point_or_public_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_first_day_above_day('call_world_with_same_person')
