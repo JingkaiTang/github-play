9ca481f3fd29f4d59dd1fdb8e811ed0bc97aa337
Jingkai Tang <jingkaitang@gmail.com>
2015-07-08 06:21:41 +0800

    rm: call other_case.py of right life

--- other_case.py
+++ other_case.py
@@ -1,12 +1,12 @@
 
 #! /usr/bin/env python
 
-def child_and_other_part(str_arg):
-    small_group(str_arg)
-    print('place_or_eye')
+def point(str_arg):
+    find_own_year_on_man(str_arg)
+    print('good_part')
 
-def small_group(str_arg):
+def find_own_year_on_man(str_arg):
     print(str_arg)
 
 if __name__ == '__main__':
-    child_and_other_part('bad_group')
+    point('person')
