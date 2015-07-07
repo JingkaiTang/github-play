6d6b6608c86f5fd2dd0c614b1cf4b32448cb2c07
Jingkai Tang <jingkaitang@gmail.com>
2015-07-07 19:18:04 +0800

    mv: ask want_problem/man/place_and_different_time.py into new eye

---
+++ want_problem/man/place_and_different_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_person(str_arg):
+    group(str_arg)
+    print('long_hand_or_large_number')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_person('world')
