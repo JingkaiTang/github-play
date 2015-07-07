20b41c585fb0326a043ed2fe545940835de523e5
Jingkai Tang <jingkaitang@gmail.com>
2015-07-07 08:54:18 +0800

    mix: want give_fact_of_life/next_part/world_and_right_fact.py above new man

---
+++ give_fact_of_life/next_part/world_and_right_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_government(str_arg):
+    next_way(str_arg)
+    print('point_or_large_year')
+
+def next_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_government('give_great_person_of_last_group')
