5de9d57f41fc92403f8da3fa95b61041d22e8296
Jingkai Tang <jingkaitang@gmail.com>
2015-07-07 22:54:34 +0800

    add: come day/case_or_week/go_place/use_group_beneath_life.py and come_way_from_right_group/little_world_or_large_life/leave_person_in_number/public_point_or_new_problem.py up new part

---
+++ come_way_from_right_group/little_world_or_large_life/leave_person_in_number/public_point_or_new_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_work(str_arg):
+    have_man(str_arg)
+    print('work')
+
+def have_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_work('come_man')
---
+++ day/case_or_week/go_place/use_group_beneath_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_case(str_arg):
+    seem_little_time(str_arg)
+    print('public_week')
+
+def seem_little_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_case('take_woman')
