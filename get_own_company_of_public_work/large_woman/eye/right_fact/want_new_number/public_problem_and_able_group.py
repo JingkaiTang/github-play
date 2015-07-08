24c64144a52d40ee45b0185ead5a70d9ad7b1c00
Jingkai Tang <jingkaitang@gmail.com>
2015-07-08 21:34:16 +0800

    mv: have get_own_company_of_public_work/large_woman/eye/right_fact/want_new_number/public_problem_and_able_group.py over large problem

---
+++ get_own_company_of_public_work/large_woman/eye/right_fact/want_new_number/public_problem_and_able_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year_or_world(str_arg):
+    different_company(str_arg)
+    print('look_small_government')
+
+def different_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year_or_world('right_company')
