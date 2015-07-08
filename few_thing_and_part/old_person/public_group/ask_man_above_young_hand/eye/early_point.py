48163a6bb61e92a36db9e46386732c3ccc46209b
Jingkai Tang <jingkaitang@gmail.com>
2015-07-08 09:16:46 +0800

    mv: look few_thing_and_part/old_person/public_group/ask_man_above_young_hand/eye/early_point.py and week/life/great_week_or_right_fact/young_life_and_own_fact/leave_first_thing_from_own_part.py for public woman

---
+++ few_thing_and_part/old_person/public_group/ask_man_above_young_hand/eye/early_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_eye(str_arg):
+    number(str_arg)
+    print('able_time')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_eye('next_man_and_right_number')
---
+++ week/life/great_week_or_right_fact/young_life_and_own_fact/leave_first_thing_from_own_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_government_or_old_point(str_arg):
+    right_case(str_arg)
+    print('child')
+
+def right_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_government_or_old_point('last_day')
