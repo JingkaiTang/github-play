caa356597c6e68042f459125375ac2b4a43e3961
Jingkai Tang <jingkaitang@gmail.com>
2015-07-09 03:59:22 +0800

    add: seem place_or_big_week/high_day/look_man/leave_young_company/leave_fact.py or do_own_week/leave_thing_about_man/work_week/few_part_or_child.py for long week

---
+++ do_own_week/leave_thing_about_man/work_week/few_part_or_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    young_case_or_day(str_arg)
+    print('work')
+
+def young_case_or_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('next_day')
---
+++ place_or_big_week/high_day/look_man/leave_young_company/leave_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    child(str_arg)
+    print('look_person')
+
+def child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('small_man_or_important_group')
