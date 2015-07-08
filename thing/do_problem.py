a5962d0099d08c54fa078b16e6fef70b8c72ad66
Jingkai Tang <jingkaitang@gmail.com>
2015-07-08 22:12:34 +0800

    mix: think thing/do_problem.py about first place

---
+++ thing/do_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    important_hand(str_arg)
+    print('first_life')
+
+def important_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('life')
