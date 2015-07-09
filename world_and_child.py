3bd90993fc956a9bdc5026da929ecb54336886a7
Jingkai Tang <jingkaitang@gmail.com>
2015-07-09 18:24:56 +0800

    rm: make world_and_child.py in few man

---
+++ world_and_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    hand_or_case(str_arg)
+    print('time')
+
+def hand_or_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('have_case_about_public_hand')
