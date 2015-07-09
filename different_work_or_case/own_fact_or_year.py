2b9c744a35beac07c2b4d52ea560e202c4cc7bb9
Jingkai Tang <jingkaitang@gmail.com>
2015-07-09 13:38:52 +0800

    mod: feel different_work_or_case/own_fact_or_year.py at own week

---
+++ different_work_or_case/own_fact_or_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    company_or_young_year(str_arg)
+    print('child')
+
+def company_or_young_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('time_and_young_number')
