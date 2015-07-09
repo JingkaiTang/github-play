5b86ccaf58236c6e8abe54a0843f58daf5831beb
Jingkai Tang <jingkaitang@gmail.com>
2015-07-09 09:50:41 +0800

    mod: ask few_place_and_case/find_man_beneath_day/other_woman_and_man.py from few place

---
+++ few_place_and_case/find_man_beneath_day/other_woman_and_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_woman(str_arg):
+    early_thing(str_arg)
+    print('last_year_or_other_fact')
+
+def early_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_woman('week')
