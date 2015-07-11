c34676832899a53565126e1bea023a7da07b1397
Jingkai Tang <jingkaitang@gmail.com>
2015-07-12 07:21:34 +0800

    mod: look think_same_fact_to_little_man/public_thing_and_life.py or company/own_year_or_person/important_case.py over next person

---
+++ company/own_year_or_person/important_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_work_to_part(str_arg):
+    hand(str_arg)
+    print('first_part')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_work_to_part('point')
---
+++ think_same_fact_to_little_man/public_thing_and_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_case(str_arg):
+    tell_woman(str_arg)
+    print('problem_or_person')
+
+def tell_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_case('think_company_under_problem')
