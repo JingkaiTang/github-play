3f326fdf6d33f3096b24cfeb88e74faaf0af4d17
Jingkai Tang <jingkaitang@gmail.com>
2015-07-12 01:45:13 +0800

    mix: tell important_time_or_year/important_fact/time_or_number/long_fact/person_or_number.py or tell_new_woman_to_problem.py up small thing

---
+++ important_time_or_year/important_fact/time_or_number/long_fact/person_or_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    small_hand(str_arg)
+    print('work')
+
+def small_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('few_case')
---
+++ tell_new_woman_to_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    important_place_or_small_case(str_arg)
+    print('leave_few_person')
+
+def important_place_or_small_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('bad_life')
