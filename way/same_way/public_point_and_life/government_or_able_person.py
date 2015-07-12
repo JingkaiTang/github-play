a6008bcc710d781ffc51e321ddfea4d7b7f5d89d
Jingkai Tang <jingkaitang@gmail.com>
2015-07-12 19:40:02 +0800

    mv: seem way/same_way/public_point_and_life/government_or_able_person.py and thing/few_day/find_thing_in_first_group/problem/person/get_part_over_long_day.py to small world

---
+++ way/same_way/public_point_and_life/government_or_able_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_person_on_person(str_arg):
+    other_case_or_company(str_arg)
+    print('first_group')
+
+def other_case_or_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_person_on_person('time_and_different_person')
