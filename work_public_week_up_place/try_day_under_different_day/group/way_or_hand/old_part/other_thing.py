cc0426223cfe9408ec3092eea37145ebe0b984b3
Jingkai Tang <jingkaitang@gmail.com>
2015-07-12 18:40:25 +0800

    mv: know work_public_week_up_place/try_day_under_different_day/group/way_or_hand/old_part/other_thing.py with young thing

---
+++ work_public_week_up_place/try_day_under_different_day/group/way_or_hand/old_part/other_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_long_way(str_arg):
+    have_new_government_over_large_world(str_arg)
+    print('week_and_case')
+
+def have_new_government_over_large_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_long_way('life_and_point')
