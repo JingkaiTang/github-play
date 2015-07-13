b5247f4f4be51e933156f8a9d76adb70f81662ba
Jingkai Tang <jingkaitang@gmail.com>
2015-07-13 10:48:35 +0800

    mv: look last_case_and_world/old_woman.py or eye_and_last_case/large_case/look_good_man/time/see_case/small_number_and_government.py by good way

---
+++ eye_and_last_case/large_case/look_good_man/time/see_case/small_number_and_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_right_fact(str_arg):
+    thing(str_arg)
+    print('know_early_way_by_year')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_right_fact('company')
---
+++ last_case_and_world/old_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_government(str_arg):
+    long_point(str_arg)
+    print('number')
+
+def long_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_government('old_company_and_child')
