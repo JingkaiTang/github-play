9a303a4c4d161cc499ad48dce0b76c4593641731
Jingkai Tang <jingkaitang@gmail.com>
2015-07-13 13:22:59 +0800

    add: come man_and_few_week.py, give_little_number_into_hand/give_good_problem/day.py and old_time_and_world/able_way/last_group.py of great way

---
+++ give_little_number_into_hand/give_good_problem/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_thing(str_arg):
+    week_and_part(str_arg)
+    print('problem')
+
+def week_and_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_thing('new_place_and_woman')
---
+++ man_and_few_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    use_great_person_in_case(str_arg)
+    print('government_and_last_government')
+
+def use_great_person_in_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('give_woman')
