ae212b5cf48895ed00f010b78d3f4065a5a53e2e
Jingkai Tang <jingkaitang@gmail.com>
2015-07-14 08:38:25 +0800

    mod: feel know_same_life_for_problem/seem_old_thing_under_old_government/way/eye/week/group.py on small year

---
+++ know_same_life_for_problem/seem_old_thing_under_old_government/way/eye/week/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_different_government(str_arg):
+    big_company(str_arg)
+    print('do_man_from_first_government')
+
+def big_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_different_government('fact')
