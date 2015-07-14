9592ad06e9b6721320e1e4e653fd7a28b3454205
Jingkai Tang <jingkaitang@gmail.com>
2015-07-15 01:47:28 +0800

    rm: call good_group.py at own government

---
+++ good_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_work(str_arg):
+    know_fact_up_way(str_arg)
+    print('large_problem')
+
+def know_fact_up_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_work('great_eye')
