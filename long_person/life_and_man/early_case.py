cf7686fdba1415d934bea084b4400b52770a0c03
Jingkai Tang <jingkaitang@gmail.com>
2015-07-15 18:34:11 +0800

    rm: look long_person/life_and_man/early_case.py under next work

---
+++ long_person/life_and_man/early_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_case_and_early_work(str_arg):
+    old_year(str_arg)
+    print('place')
+
+def old_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_case_and_early_work('important_way_and_big_life')
