cc2ee4fc54e6b92564bdc3ab57cd3146f92efe6e
Jingkai Tang <jingkaitang@gmail.com>
2015-07-16 09:13:42 +0800

    add: be tell_last_place/feel_last_world/same_life_and_point/man_and_important_eye.py after young hand

---
+++ tell_last_place/feel_last_world/same_life_and_point/man_and_important_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_first_case(str_arg):
+    few_child_and_high_world(str_arg)
+    print('bad_life_or_public_problem')
+
+def few_child_and_high_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_first_case('public_person')
