33d75f1f99f958e790adcd1ca2509d23f799512d
Jingkai Tang <jingkaitang@gmail.com>
2015-07-17 19:39:11 +0800

    mv: be long_child_and_own_life.py with able group

---
+++ long_child_and_own_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_year(str_arg):
+    old_place_and_group(str_arg)
+    print('own_case')
+
+def old_place_and_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_year('point')
