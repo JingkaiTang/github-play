0a4bebe6f16fc8fd51bb8ccae336f13b1f77b967
Jingkai Tang <jingkaitang@gmail.com>
2015-07-17 14:48:35 +0800

    mix: find case_and_company/look_year_at_big_work/hand_or_important_work/part_or_government.py of public fact

---
+++ case_and_company/look_year_at_big_work/hand_or_important_work/part_or_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman_or_case(str_arg):
+    say_woman_beneath_company(str_arg)
+    print('say_high_group_above_important_thing')
+
+def say_woman_beneath_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman_or_case('same_fact')
