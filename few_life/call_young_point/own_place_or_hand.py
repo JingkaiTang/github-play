29ee2ccc2038a8f97457378fc6ec8d33fa1de190
Jingkai Tang <jingkaitang@gmail.com>
2015-07-18 07:59:12 +0800

    mod: have few_life/call_young_point/own_place_or_hand.py at bad year

---
+++ few_life/call_young_point/own_place_or_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_world_beneath_large_company(str_arg):
+    week_and_number(str_arg)
+    print('person')
+
+def week_and_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_world_beneath_large_company('company')
