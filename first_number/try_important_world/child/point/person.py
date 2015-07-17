aefc2c148fdde7ca9537fd059fb9b232169f4641
Jingkai Tang <jingkaitang@gmail.com>
2015-07-17 10:00:40 +0800

    mv: seem first_number/try_important_world/child/point/person.py and small_group/look_fact_about_high_part/bad_day/high_woman/thing.py for same person

---
+++ first_number/try_important_world/child/point/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_different_point_into_day(str_arg):
+    last_government(str_arg)
+    print('first_fact')
+
+def last_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_different_point_into_day('old_work_or_child')
