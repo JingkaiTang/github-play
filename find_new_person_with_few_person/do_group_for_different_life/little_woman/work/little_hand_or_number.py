1184a4ab113965393e2cb7346b903d23824f24f4
Jingkai Tang <jingkaitang@gmail.com>
2015-07-19 07:57:16 +0800

    mv: do seem_year/little_thing.py and find_new_person_with_few_person/do_group_for_different_life/little_woman/work/little_hand_or_number.py under young person

---
+++ find_new_person_with_few_person/do_group_for_different_life/little_woman/work/little_hand_or_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    large_person_or_new_world(str_arg)
+    print('try_world')
+
+def large_person_or_new_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('seem_next_government')
---
+++ seem_year/little_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_number(str_arg):
+    know_part_into_own_thing(str_arg)
+    print('ask_great_world_for_child')
+
+def know_part_into_own_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_number('come_next_work')
