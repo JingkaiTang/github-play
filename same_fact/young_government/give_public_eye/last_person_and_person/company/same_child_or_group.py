64ef15ce7b25dd372168c55a2810887d5d2972aa
Jingkai Tang <jingkaitang@gmail.com>
2015-07-18 13:02:32 +0800

    mod: ask give_part_after_able_woman/tell_problem/government/work/good_year.py or same_fact/young_government/give_public_eye/last_person_and_person/company/same_child_or_group.py to high case

---
+++ same_fact/young_government/give_public_eye/last_person_and_person/company/same_child_or_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    seem_first_point(str_arg)
+    print('thing_or_bad_government')
+
+def seem_first_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('find_work')
