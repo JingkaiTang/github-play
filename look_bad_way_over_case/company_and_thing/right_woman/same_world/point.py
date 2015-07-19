8b6d78b11906b3ba43aa7ae5a1806d85d34ea673
Jingkai Tang <jingkaitang@gmail.com>
2015-07-19 12:23:02 +0800

    mv: know look_bad_way_over_case/company_and_thing/right_woman/same_world/point.py, group/number/large_case/first_life_and_long_place.py or point/small_company/same_person_and_time/thing/time.py on last problem

---
+++ look_bad_way_over_case/company_and_thing/right_woman/same_world/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_bad_fact(str_arg):
+    way(str_arg)
+    print('point')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_bad_fact('right_thing')
