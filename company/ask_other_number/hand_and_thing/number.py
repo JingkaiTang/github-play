40c4e07385a76ba4669f82e1ece20d2b03106be0
Jingkai Tang <jingkaitang@gmail.com>
2015-07-19 12:47:49 +0800

    mod: call company/ask_other_number/hand_and_thing/number.py under good place

---
+++ company/ask_other_number/hand_and_thing/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_day(str_arg):
+    hand(str_arg)
+    print('day')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_day('different_fact_or_week')
