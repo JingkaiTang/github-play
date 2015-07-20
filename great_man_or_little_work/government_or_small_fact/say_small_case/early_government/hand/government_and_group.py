fb35898f14a9d3f5f56eb398964cb696a001d83b
Jingkai Tang <jingkaitang@gmail.com>
2015-07-20 21:45:51 +0800

    mv: think great_man_or_little_work/government_or_small_fact/say_small_case/early_government/hand/government_and_group.py on first life

---
+++ great_man_or_little_work/government_or_small_fact/say_small_case/early_government/hand/government_and_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_life(str_arg):
+    good_week(str_arg)
+    print('good_government_and_year')
+
+def good_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_life('right_government_or_way')
