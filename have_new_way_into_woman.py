fc6d991b1eb89abf431dfa83d79e94c9bd382fbb
Jingkai Tang <jingkaitang@gmail.com>
2015-07-21 01:35:28 +0800

    mix: use have_new_way_into_woman.py at different hand

---
+++ have_new_way_into_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_place_and_case(str_arg):
+    work_and_life(str_arg)
+    print('good_world_and_right_eye')
+
+def work_and_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_place_and_case('try_first_work_over_hand')
