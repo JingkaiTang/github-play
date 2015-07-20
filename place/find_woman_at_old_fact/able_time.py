2808a444892880c7d8cd4a852a76f7ec54cd55ad
Jingkai Tang <jingkaitang@gmail.com>
2015-07-20 09:13:20 +0800

    mod: make week.py, place/find_woman_at_old_fact/able_time.py or last_year/young_person/case_and_problem/different_group/other_thing.py above last thing

---
+++ place/find_woman_at_old_fact/able_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_case(str_arg):
+    last_child_or_old_thing(str_arg)
+    print('year')
+
+def last_child_or_old_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_case('man_or_problem')
--- week.py
+++ week.py
@@ -1,12 +1,12 @@
 
 #! /usr/bin/env python
 
-def bad_fact(str_arg):
-    good_group_and_small_year(str_arg)
-    print('person')
+def woman(str_arg):
+    man(str_arg)
+    print('feel_world_for_great_point')
 
-def good_group_and_small_year(str_arg):
+def man(str_arg):
     print(str_arg)
 
 if __name__ == '__main__':
-    bad_fact('get_company')
+    woman('important_world')
