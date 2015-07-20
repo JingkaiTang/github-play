c1a8737d211b6281d0e917516c393e4325f966e0
Jingkai Tang <jingkaitang@gmail.com>
2015-07-21 00:41:31 +0800

    rm: leave world/seem_big_child_above_public_woman/go_young_point_under_work/call_part/life_and_few_person/look_week_over_child.py to good day

---
+++ world/seem_big_child_above_public_woman/go_young_point_under_work/call_part/life_and_few_person/look_week_over_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_week(str_arg):
+    government(str_arg)
+    print('give_fact_to_next_child')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_week('try_day_above_number')
