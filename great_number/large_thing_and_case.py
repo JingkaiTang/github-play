4f03bd00fb715661d37dbd7aef4fd775b59f48d7
Jingkai Tang <jingkaitang@gmail.com>
2015-07-24 20:52:37 +0800

    mv: know work_and_problem/say_number_under_way/world/year/great_man_or_person.py and great_number/large_thing_and_case.py to last part

---
+++ great_number/large_thing_and_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_person(str_arg):
+    feel_young_eye(str_arg)
+    print('day')
+
+def feel_young_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_person('place')
---
+++ work_and_problem/say_number_under_way/world/year/great_man_or_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_place(str_arg):
+    hand(str_arg)
+    print('great_world')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_place('big_number_or_old_world')
