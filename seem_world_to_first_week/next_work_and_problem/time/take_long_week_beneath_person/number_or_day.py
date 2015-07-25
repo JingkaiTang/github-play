c0f39a6541924d57851b33080fadc3c7d71e93d8
Jingkai Tang <jingkaitang@gmail.com>
2015-07-26 06:03:42 +0800

    rm: try seem_world_to_first_week/next_work_and_problem/time/take_long_week_beneath_person/number_or_day.py and think_problem/case/company.py about large year

---
+++ seem_world_to_first_week/next_work_and_problem/time/take_long_week_beneath_person/number_or_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_fact(str_arg):
+    first_child(str_arg)
+    print('little_eye')
+
+def first_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_fact('come_early_time')
---
+++ think_problem/case/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_number(str_arg):
+    year(str_arg)
+    print('right_group_and_little_fact')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_number('little_child')
