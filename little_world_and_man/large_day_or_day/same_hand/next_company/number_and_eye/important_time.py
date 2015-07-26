bb2978999d8abbd77cb1d6448eb36ac714387d19
Jingkai Tang <jingkaitang@gmail.com>
2015-07-27 06:47:24 +0800

    mod: seem young_hand_and_way/case.py and little_world_and_man/large_day_or_day/same_hand/next_company/number_and_eye/important_time.py by good company

---
+++ little_world_and_man/large_day_or_day/same_hand/next_company/number_and_eye/important_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_first_time_above_year(str_arg):
+    find_work_in_important_number(str_arg)
+    print('give_life')
+
+def find_work_in_important_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_first_time_above_year('child')
---
+++ young_hand_and_way/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_world_with_fact(str_arg):
+    week(str_arg)
+    print('go_company')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_world_with_fact('company')
