5fcaf2b65b22815ac1c50165f69ce47fc1e5479e
Jingkai Tang <jingkaitang@gmail.com>
2015-07-26 20:02:55 +0800

    mix: do big_case_and_large_part/number_and_point/week.py under big problem

---
+++ big_case_and_large_part/number_and_point/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact_or_few_time(str_arg):
+    do_other_person(str_arg)
+    print('public_world')
+
+def do_other_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact_or_few_time('group_or_eye')
