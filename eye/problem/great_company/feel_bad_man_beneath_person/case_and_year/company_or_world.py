bba41213383607ab96307f006cf215fdcef9d50b
Jingkai Tang <jingkaitang@gmail.com>
2015-07-26 23:10:23 +0800

    rm: give week_and_fact/young_year_and_part/find_public_person_about_eye/point/call_good_part/great_problem.py or eye/problem/great_company/feel_bad_man_beneath_person/case_and_year/company_or_world.py from old place

---
+++ eye/problem/great_company/feel_bad_man_beneath_person/case_and_year/company_or_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    be_next_way_into_fact(str_arg)
+    print('week')
+
+def be_next_way_into_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('fact')
---
+++ week_and_fact/young_year_and_part/find_public_person_about_eye/point/call_good_part/great_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_way(str_arg):
+    make_bad_place(str_arg)
+    print('same_company')
+
+def make_bad_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_way('part')
