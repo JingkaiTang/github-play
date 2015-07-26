159062e44385bdc1718870f9969df95368121c36
Jingkai Tang <jingkaitang@gmail.com>
2015-07-26 23:04:49 +0800

    mv: have work_or_work/number_or_small_fact.py over last eye

---
+++ work_or_work/number_or_small_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    large_place_and_case(str_arg)
+    print('week')
+
+def large_place_and_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('public_man_and_right_life')
