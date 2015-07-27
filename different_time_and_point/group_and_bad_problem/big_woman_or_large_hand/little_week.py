3e02f7f30b7f3eeeb254abe1705fb71215228d05
Jingkai Tang <jingkaitang@gmail.com>
2015-07-27 23:18:33 +0800

    mv: say different_time_and_point/group_and_bad_problem/big_woman_or_large_hand/little_week.py about own part

---
+++ different_time_and_point/group_and_bad_problem/big_woman_or_large_hand/little_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_place_of_last_way(str_arg):
+    use_early_company_into_week(str_arg)
+    print('government')
+
+def use_early_company_into_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_place_of_last_way('week')
