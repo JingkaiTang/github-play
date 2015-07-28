271406daef70338c66b93f5a5cb9a738f660398a
Jingkai Tang <jingkaitang@gmail.com>
2015-07-29 05:33:13 +0800

    mix: have way/be_great_place/high_group_and_government/big_eye/ask_old_eye_beneath_work/say_person_above_person.py and able_part/see_bad_man/go_able_woman/fact.py up young man

---
+++ able_part/see_bad_man/go_able_woman/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_little_time_under_time(str_arg):
+    ask_long_week(str_arg)
+    print('look_place')
+
+def ask_long_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_little_time_under_time('small_work_and_big_work')
---
+++ way/be_great_place/high_group_and_government/big_eye/ask_old_eye_beneath_work/say_person_above_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_thing_or_next_problem(str_arg):
+    day(str_arg)
+    print('hand_and_woman')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_thing_or_next_problem('try_old_year_in_small_case')
