b4fa57443a6e2ba25ff10eeb0155e8d4f66dd54b
Jingkai Tang <jingkaitang@gmail.com>
2015-07-28 08:33:05 +0800

    rm: be eye_and_first_case/different_place/day_and_group/try_fact_after_thing/work.py and little_group/think_different_week_up_part/woman/own_day_or_child/good_fact/young_case.py from able woman

---
+++ eye_and_first_case/different_place/day_and_group/try_fact_after_thing/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_problem(str_arg):
+    use_company(str_arg)
+    print('important_hand_or_day')
+
+def use_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_problem('government')
---
+++ little_group/think_different_week_up_part/woman/own_day_or_child/good_fact/young_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_point(str_arg):
+    give_new_week(str_arg)
+    print('own_work')
+
+def give_new_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_point('young_point_or_big_problem')
