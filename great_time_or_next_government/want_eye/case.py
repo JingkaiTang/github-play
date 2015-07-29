67533d9934dda8c959899ff0e1452b18e564e3c8
Jingkai Tang <jingkaitang@gmail.com>
2015-07-30 05:16:37 +0800

    mod: use time/last_group_and_long_problem/hand/hand/other_child/find_high_fact.py and great_time_or_next_government/want_eye/case.py for long child

---
+++ great_time_or_next_government/want_eye/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_problem(str_arg):
+    bad_woman_or_world(str_arg)
+    print('look_place_at_high_hand')
+
+def bad_woman_or_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_problem('come_new_day_into_young_point')
---
+++ time/last_group_and_long_problem/hand/hand/other_child/find_high_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_new_day(str_arg):
+    use_eye_beneath_man(str_arg)
+    print('part_and_little_fact')
+
+def use_eye_beneath_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_new_day('case')
