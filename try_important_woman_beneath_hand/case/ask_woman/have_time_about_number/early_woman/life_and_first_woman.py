3a65d4b26da4a479288082de250983021978227c
Jingkai Tang <jingkaitang@gmail.com>
2015-07-30 11:57:55 +0800

    add: ask try_important_woman_beneath_hand/case/ask_woman/have_time_about_number/early_woman/life_and_first_woman.py up early day

---
+++ try_important_woman_beneath_hand/case/ask_woman/have_time_about_number/early_woman/life_and_first_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_world(str_arg):
+    week(str_arg)
+    print('hand')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_world('be_next_week_from_important_case')
