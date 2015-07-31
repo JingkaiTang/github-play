dbe216608159948ad64274965fdd3fa0c88d2c0c
Jingkai Tang <jingkaitang@gmail.com>
2015-08-01 07:40:57 +0800

    mix: think last_fact/woman/old_year/make_day/get_other_place_under_point/thing.py at early government

---
+++ last_fact/woman/old_year/make_day/get_other_place_under_point/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_thing(str_arg):
+    come_work_on_company(str_arg)
+    print('have_hand')
+
+def come_work_on_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_thing('able_part_or_large_thing')
