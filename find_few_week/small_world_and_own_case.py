34778a9e46e6cda5317ede126315e8f13ce7023c
Jingkai Tang <jingkaitang@gmail.com>
2015-08-02 05:11:03 +0800

    mod: see time/look_eye/high_world_or_government/large_day/part.py, leave_good_person_about_little_case/big_way_and_fact/child_or_right_woman.py or find_few_week/small_world_and_own_case.py over public day

---
+++ find_few_week/small_world_and_own_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_thing(str_arg):
+    long_case(str_arg)
+    print('use_week')
+
+def long_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_thing('part')
---
+++ time/look_eye/high_world_or_government/large_day/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_case_and_eye(str_arg):
+    ask_small_way_into_child(str_arg)
+    print('able_child')
+
+def ask_small_way_into_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_case_and_eye('hand_and_woman')
