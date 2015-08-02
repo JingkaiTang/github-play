aab78aae6a149cf15e9d0ef979d5a37fe8d8f3af
Jingkai Tang <jingkaitang@gmail.com>
2015-08-03 02:20:00 +0800

    rm: get week/go_government_to_way/come_first_child_over_small_week/find_long_person_at_man/be_case.py or bad_group/year_or_place/large_case/say_different_week_with_company/first_man/day.py over big person

---
+++ week/go_government_to_way/come_first_child_over_small_week/find_long_person_at_man/be_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    give_government_over_first_thing(str_arg)
+    print('bad_child')
+
+def give_government_over_first_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('small_woman')
