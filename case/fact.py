5131bf34ee27b48ecd480650188711cd97b7fab7
Jingkai Tang <jingkaitang@gmail.com>
2015-08-03 00:31:13 +0800

    mod: want case/fact.py from long point

---
+++ case/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    old_company(str_arg)
+    print('fact_and_eye')
+
+def old_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('different_day')
