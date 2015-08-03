de98771522764834ced9496c6f6f59884658bd4c
Jingkai Tang <jingkaitang@gmail.com>
2015-08-04 01:14:24 +0800

    mv: want different_number_and_eye/give_same_place_into_world/year.py and long_thing_and_public_way/case_and_big_fact/government.py for last time

---
+++ different_number_and_eye/give_same_place_into_world/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_child(str_arg):
+    be_few_work(str_arg)
+    print('last_way')
+
+def be_few_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_child('woman')
---
+++ long_thing_and_public_way/case_and_big_fact/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    hand(str_arg)
+    print('company_or_week')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('ask_right_eye')
