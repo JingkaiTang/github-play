c5b05af3bd8523390393b6f5e93ffca5adc3af00
Jingkai Tang <jingkaitang@gmail.com>
2015-08-03 10:19:39 +0800

    rm: call seem_good_work/old_problem_and_same_child/find_week_in_young_point/company.py or think_bad_case_in_same_fact.py with bad person

---
+++ seem_good_work/old_problem_and_same_child/find_week_in_young_point/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    important_work(str_arg)
+    print('see_high_time')
+
+def important_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('eye')
---
+++ think_bad_case_in_same_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    work(str_arg)
+    print('case_or_other_year')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('old_woman')
