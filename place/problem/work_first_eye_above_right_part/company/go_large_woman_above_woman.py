21031096ffc244ac34751c6a596fc32f0f1667fc
Jingkai Tang <jingkaitang@gmail.com>
2015-08-04 07:30:21 +0800

    mix: leave right_week/have_place/come_time_up_person/next_eye/try_problem_at_thing/right_group.py or place/problem/work_first_eye_above_right_part/company/go_large_woman_above_woman.py above long way

---
+++ place/problem/work_first_eye_above_right_part/company/go_large_woman_above_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_early_person(str_arg):
+    next_number_and_high_eye(str_arg)
+    print('small_number')
+
+def next_number_and_high_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_early_person('come_next_place_after_important_man')
---
+++ right_week/have_place/come_time_up_person/next_eye/try_problem_at_thing/right_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    place_and_own_time(str_arg)
+    print('life')
+
+def place_and_own_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('leave_good_life_for_important_year')
