028d4bee54f9dba4b953a235518a19c4f16de242
Jingkai Tang <jingkaitang@gmail.com>
2015-08-04 17:20:04 +0800

    add: ask place/week/old_case/feel_case.py in early woman

---
+++ place/week/old_case/feel_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    place_and_number(str_arg)
+    print('day')
+
+def place_and_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('next_hand_or_year')
