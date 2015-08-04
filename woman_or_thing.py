e37b51f93aee5fa3a9e504e4ac62ccc4c4a516e3
Jingkai Tang <jingkaitang@gmail.com>
2015-08-04 19:53:03 +0800

    add: try year/little_hand/different_number_and_high_time.py or woman_or_thing.py from long life

---
+++ woman_or_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman_or_case(str_arg):
+    have_point(str_arg)
+    print('first_thing')
+
+def have_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman_or_case('good_hand')
---
+++ year/little_hand/different_number_and_high_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_case(str_arg):
+    first_life_and_work(str_arg)
+    print('small_hand')
+
+def first_life_and_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_case('thing')
