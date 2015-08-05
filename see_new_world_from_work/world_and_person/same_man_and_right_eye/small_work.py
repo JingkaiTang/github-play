8d46ac2f8ccc7024654bb662d8dac2ff9b951075
Jingkai Tang <jingkaitang@gmail.com>
2015-08-05 12:00:07 +0800

    rm: think see_new_world_from_work/world_and_person/same_man_and_right_eye/small_work.py up first thing

---
+++ see_new_world_from_work/world_and_person/same_man_and_right_eye/small_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_fact_up_world(str_arg):
+    year_or_work(str_arg)
+    print('few_woman')
+
+def year_or_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_fact_up_world('ask_other_person')
