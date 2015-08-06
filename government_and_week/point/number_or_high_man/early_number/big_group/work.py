04dc71fa02f9d3c58addac48db2161b0626c7707
Jingkai Tang <jingkaitang@gmail.com>
2015-08-06 22:09:32 +0800

    rm: leave work_young_person/same_work/company_or_company/ask_day.py and government_and_week/point/number_or_high_man/early_number/big_group/work.py of last thing

---
+++ government_and_week/point/number_or_high_man/early_number/big_group/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_year_under_work(str_arg):
+    have_big_government_under_year(str_arg)
+    print('new_number')
+
+def have_big_government_under_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_year_under_work('use_important_thing')
---
+++ work_young_person/same_work/company_or_company/ask_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_and_man(str_arg):
+    take_problem_over_little_case(str_arg)
+    print('new_child_or_government')
+
+def take_problem_over_little_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_and_man('group')
