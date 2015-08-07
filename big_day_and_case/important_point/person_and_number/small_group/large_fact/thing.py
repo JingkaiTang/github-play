1737d9efa7a8a394030571ff15524fa03ae20387
Jingkai Tang <jingkaitang@gmail.com>
2015-08-08 04:43:12 +0800

    rm: use big_day_and_case/important_point/person_and_number/small_group/large_fact/thing.py of different eye

---
+++ big_day_and_case/important_point/person_and_number/small_group/large_fact/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    work_able_life_about_way(str_arg)
+    print('day_and_small_world')
+
+def work_able_life_about_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('public_child')
