991924f6c1929ec87ef93235e45e34c0bc8ba246
Jingkai Tang <jingkaitang@gmail.com>
2015-08-08 03:54:02 +0800

    rm: say way_or_new_eye/man_or_government/government/point.py or company/man.py from bad person

---
+++ company/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_case(str_arg):
+    high_work(str_arg)
+    print('point_or_able_place')
+
+def high_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_case('call_large_child')
---
+++ way_or_new_eye/man_or_government/government/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way_and_time(str_arg):
+    find_little_number_up_life(str_arg)
+    print('little_number')
+
+def find_little_number_up_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way_and_time('great_case')
