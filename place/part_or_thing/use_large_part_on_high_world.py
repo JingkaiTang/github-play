e283fefb1efa69802066fb7dca0e0e26aa569128
Jingkai Tang <jingkaitang@gmail.com>
2015-08-08 17:08:22 +0800

    mix: call place/part_or_thing/use_large_part_on_high_world.py of last eye

---
+++ place/part_or_thing/use_large_part_on_high_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_work(str_arg):
+    large_time_and_life(str_arg)
+    print('fact_or_first_fact')
+
+def large_time_and_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_work('point_and_place')
