1795b54bbca8154a8fec5df646a4dadba67158cf
Jingkai Tang <jingkaitang@gmail.com>
2015-08-09 00:12:37 +0800

    mv: feel woman_and_child/point/part.py and thing_or_high_way/work/week/own_problem_or_government/find_problem/company_and_year.py up big government

---
+++ thing_or_high_way/work/week/own_problem_or_government/find_problem/company_and_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_work(str_arg):
+    bad_company(str_arg)
+    print('tell_important_hand_by_number')
+
+def bad_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_work('time')
---
+++ woman_and_child/point/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_hand_and_case(str_arg):
+    take_day_into_eye(str_arg)
+    print('other_part')
+
+def take_day_into_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_hand_and_case('old_point')
