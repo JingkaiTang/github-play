d3293b200978404136542812d2bdc224c4dcb52a
Jingkai Tang <jingkaitang@gmail.com>
2015-08-10 08:19:59 +0800

    mix: use right_place/case/world/go_fact/hand_or_world.py and get_bad_man_to_government.py with old week

---
+++ get_bad_man_to_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_work_or_few_person(str_arg):
+    first_fact(str_arg)
+    print('way')
+
+def first_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_work_or_few_person('case_and_group')
---
+++ right_place/case/world/go_fact/hand_or_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    feel_case(str_arg)
+    print('thing')
+
+def feel_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('want_different_week_of_work')
