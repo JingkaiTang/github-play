21ba6314ced86f41391b419a6c7175c1817c708b
Jingkai Tang <jingkaitang@gmail.com>
2015-08-10 23:17:37 +0800

    mix: say high_year/other_eye/do_hand.py to big problem

---
+++ high_year/other_eye/do_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_last_part_to_high_person(str_arg):
+    company(str_arg)
+    print('way')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_last_part_to_high_person('be_fact_for_last_hand')
