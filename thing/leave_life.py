6b055992649bd4d34b2b2d80013675b9f8528094
Jingkai Tang <jingkaitang@gmail.com>
2015-08-11 00:38:51 +0800

    mod: go thing/leave_life.py by different company

---
+++ thing/leave_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    next_problem(str_arg)
+    print('little_week_and_man')
+
+def next_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('young_world')
