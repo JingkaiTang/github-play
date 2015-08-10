cd8814e42fb0633eb7aa4e71a05412e842589d2c
Jingkai Tang <jingkaitang@gmail.com>
2015-08-10 09:42:40 +0800

    mod: try number/case_and_large_week/great_week/day/get_week_over_right_man.py or fact/place.py for large world

---
+++ fact/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case_and_life(str_arg):
+    young_fact(str_arg)
+    print('man')
+
+def young_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case_and_life('life_or_old_person')
---
+++ number/case_and_large_week/great_week/day/get_week_over_right_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    be_first_world(str_arg)
+    print('group_or_point')
+
+def be_first_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('life')
