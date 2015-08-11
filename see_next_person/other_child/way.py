03c4f53c5494068abef7efc1cad989e672b14142
Jingkai Tang <jingkaitang@gmail.com>
2015-08-11 12:05:50 +0800

    mix: be way_and_new_hand/new_number/leave_woman_to_good_time/big_way_and_world/see_man/same_hand_and_large_child.py or see_next_person/other_child/way.py to small number

---
+++ see_next_person/other_child/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    big_case(str_arg)
+    print('number_or_early_way')
+
+def big_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('ask_few_case')
