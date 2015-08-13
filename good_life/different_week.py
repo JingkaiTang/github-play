91e325ec5f8dc4e09b7fb29de9774bce3c7ee71f
Jingkai Tang <jingkaitang@gmail.com>
2015-08-14 05:21:06 +0800

    mv: get good_life/different_week.py or know_government_on_man/part_or_world/tell_place_in_government/few_man_or_young_government.py beneath new company

---
+++ good_life/different_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    right_point(str_arg)
+    print('time')
+
+def right_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('do_early_life')
---
+++ know_government_on_man/part_or_world/tell_place_in_government/few_man_or_young_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    seem_own_point_for_week(str_arg)
+    print('think_good_part_about_point')
+
+def seem_own_point_for_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('little_problem_or_good_woman')
