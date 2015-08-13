83483b6d02088036f0f27e7e135a7b2e1f11f5f1
Jingkai Tang <jingkaitang@gmail.com>
2015-08-14 00:07:10 +0800

    mix: go able_government/be_able_eye.py for new part

---
+++ able_government/be_able_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_own_case(str_arg):
+    early_person(str_arg)
+    print('good_case')
+
+def early_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_own_case('small_year')
