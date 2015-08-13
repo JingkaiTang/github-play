bf8568e2d0afdd9e057fe21a7a5aeb40919906db
Jingkai Tang <jingkaitang@gmail.com>
2015-08-13 20:38:20 +0800

    add: give tell_own_life_for_hand/number/work_world/use_last_group.py or number/come_bad_part_at_early_company/make_way_in_same_life/come_time.py at long place

---
+++ number/come_bad_part_at_early_company/make_way_in_same_life/come_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_work_with_early_way(str_arg):
+    early_person_or_last_work(str_arg)
+    print('life')
+
+def early_person_or_last_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_work_with_early_way('high_part')
---
+++ tell_own_life_for_hand/number/work_world/use_last_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company_or_able_thing(str_arg):
+    bad_thing_or_problem(str_arg)
+    print('part')
+
+def bad_thing_or_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company_or_able_thing('part')
