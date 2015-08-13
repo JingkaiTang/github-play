6d41cd03e814bffddf5f6583aefcdfd9dcf52bb5
Jingkai Tang <jingkaitang@gmail.com>
2015-08-13 19:38:17 +0800

    mix: find week/ask_hand_in_different_case.py or old_government_or_old_group.py by next day

---
+++ old_government_or_old_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time_or_great_week(str_arg):
+    have_large_part(str_arg)
+    print('want_case')
+
+def have_large_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time_or_great_week('different_work')
---
+++ week/ask_hand_in_different_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_year(str_arg):
+    want_eye_of_time(str_arg)
+    print('part')
+
+def want_eye_of_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_year('tell_little_week')
