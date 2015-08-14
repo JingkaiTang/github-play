0f0ad580e14730803b44a7bc9147c657740d210e
Jingkai Tang <jingkaitang@gmail.com>
2015-08-14 22:18:30 +0800

    mv: be life/be_own_way/small_time/small_way/hand.py and high_hand/way/last_company_and_problem.py after other eye

---
+++ high_hand/way/last_company_and_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_small_person_on_early_case(str_arg):
+    different_part(str_arg)
+    print('company')
+
+def different_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_small_person_on_early_case('hand')
---
+++ life/be_own_way/small_time/small_way/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_week(str_arg):
+    few_thing(str_arg)
+    print('say_good_number')
+
+def few_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_week('use_man')
