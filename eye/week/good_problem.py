cc3cb560006f3c4a22c95142d06d123f6a88dd0c
Jingkai Tang <jingkaitang@gmail.com>
2015-08-14 14:17:27 +0800

    mv: have great_case/bad_world_or_group/same_hand_and_point/come_big_time_of_life.py or eye/week/good_problem.py at large point

---
+++ eye/week/good_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_year(str_arg):
+    high_eye_and_company(str_arg)
+    print('time_or_great_part')
+
+def high_eye_and_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_year('eye')
---
+++ great_case/bad_world_or_group/same_hand_and_point/come_big_time_of_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_life_of_life(str_arg):
+    great_time(str_arg)
+    print('hand_and_man')
+
+def great_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_life_of_life('give_right_person_with_old_man')
