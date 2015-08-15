c5f4b53a4169fcd1089aad0bb57a0ed87e74f0bf
Jingkai Tang <jingkaitang@gmail.com>
2015-08-15 17:27:03 +0800

    mix: work work/great_point/small_fact_and_problem.py above small hand

---
+++ work/great_point/small_fact_and_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man_or_number(str_arg):
+    same_week(str_arg)
+    print('company')
+
+def same_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man_or_number('able_time')
