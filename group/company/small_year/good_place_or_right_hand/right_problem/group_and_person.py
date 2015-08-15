1cb4736572e3b443fe2099cea4d2610e4e545572
Jingkai Tang <jingkaitang@gmail.com>
2015-08-16 03:04:19 +0800

    mv: want group/company/small_year/good_place_or_right_hand/right_problem/group_and_person.py over small eye

---
+++ group/company/small_year/good_place_or_right_hand/right_problem/group_and_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_thing_and_important_case(str_arg):
+    early_woman(str_arg)
+    print('come_next_woman_beneath_right_company')
+
+def early_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_thing_and_important_case('early_child')
