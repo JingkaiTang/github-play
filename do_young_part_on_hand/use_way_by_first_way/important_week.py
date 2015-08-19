c7211219b737fe29f8a9e3298502aee233035325
Jingkai Tang <jingkaitang@gmail.com>
2015-08-19 17:06:36 +0800

    add: tell do_young_part_on_hand/use_way_by_first_way/important_week.py or part_and_last_day/company/way/way_and_fact/first_day.py to own government

---
+++ do_young_part_on_hand/use_way_by_first_way/important_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_point(str_arg):
+    government(str_arg)
+    print('year')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_point('same_government')
---
+++ part_and_last_day/company/way/way_and_fact/first_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    other_way(str_arg)
+    print('day')
+
+def other_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('large_case')
