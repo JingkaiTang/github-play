b5eff493914f9af9f2869b30be64639e7d50a380
Jingkai Tang <jingkaitang@gmail.com>
2015-08-22 14:46:25 +0800

    rm: seem eye_and_right_place.py and hand/able_way/man_and_year/day/high_group_and_year/fact.py by new day

---
+++ eye_and_right_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_person(str_arg):
+    place_or_point(str_arg)
+    print('large_place_or_time')
+
+def place_or_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_person('place')
---
+++ hand/able_way/man_and_year/day/high_group_and_year/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_hand(str_arg):
+    man(str_arg)
+    print('eye')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_hand('say_small_company_by_eye')
