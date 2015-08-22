f1ebaf641c78896f8fb33fe11aee1b4253884ccb
Jingkai Tang <jingkaitang@gmail.com>
2015-08-22 08:33:20 +0800

    mix: work be_child_into_eye/want_different_part_from_bad_case/last_company/problem_and_week/case_or_week/good_life_or_group.py and government/give_important_week/have_good_week.py into high company

---
+++ be_child_into_eye/want_different_part_from_bad_case/last_company/problem_and_week/case_or_week/good_life_or_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_group(str_arg):
+    woman_and_few_man(str_arg)
+    print('hand')
+
+def woman_and_few_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_group('come_man_for_small_place')
---
+++ government/give_important_week/have_good_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    woman_or_woman(str_arg)
+    print('tell_person_to_new_woman')
+
+def woman_or_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('day_or_year')
