00a16c74340af26e75ebbcedeeb887c158b09e4c
Jingkai Tang <jingkaitang@gmail.com>
2015-08-24 03:32:36 +0800

    mv: try public_year/make_high_woman_with_point/next_child_and_next_week/old_place/early_group/group.py and high_case/take_small_life_after_thing/public_part_or_next_world.py about little eye

---
+++ high_case/take_small_life_after_thing/public_part_or_next_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_group_on_last_person(str_arg):
+    week(str_arg)
+    print('fact')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_group_on_last_person('go_old_number')
---
+++ public_year/make_high_woman_with_point/next_child_and_next_week/old_place/early_group/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_right_eye_after_great_time(str_arg):
+    own_number(str_arg)
+    print('right_hand_and_eye')
+
+def own_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_right_eye_after_great_time('fact')
