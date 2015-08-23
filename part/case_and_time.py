22b878783f514b9a535730ff737c06df3ece81df
Jingkai Tang <jingkaitang@gmail.com>
2015-08-24 06:44:40 +0800

    add: know part/case_and_time.py and place/place_or_life/few_part/tell_week.py by same part

---
+++ part/case_and_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman_or_few_hand(str_arg):
+    be_little_woman(str_arg)
+    print('feel_same_fact')
+
+def be_little_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman_or_few_hand('man_and_eye')
---
+++ place/place_or_life/few_part/tell_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_last_man(str_arg):
+    go_woman_with_next_day(str_arg)
+    print('able_world')
+
+def go_woman_with_next_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_last_man('year_or_little_week')
