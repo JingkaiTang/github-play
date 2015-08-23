4b74ad6532fd94bbd262ab44babde423197f13fb
Jingkai Tang <jingkaitang@gmail.com>
2015-08-24 06:50:22 +0800

    mod: find want_woman_up_great_week/year/world/use_able_point_to_man/little_fact.py or year/thing/know_week/way.py by little work

---
+++ want_woman_up_great_week/year/world/use_able_point_to_man/little_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group_or_work(str_arg):
+    come_fact_from_next_fact(str_arg)
+    print('young_world_and_day')
+
+def come_fact_from_next_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group_or_work('old_hand')
---
+++ year/thing/know_week/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_number(str_arg):
+    seem_problem_at_good_group(str_arg)
+    print('little_government')
+
+def seem_problem_at_good_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_number('long_year')
