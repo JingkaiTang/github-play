ff7cad7e5f8d4641e309302651d821c96bfb91ad
Jingkai Tang <jingkaitang@gmail.com>
2015-08-24 20:55:47 +0800

    mv: look ask_same_time_after_eye/same_problem_and_same_day/see_problem_of_high_year/world/year/hand_or_little_man.py from large year

---
+++ ask_same_time_after_eye/same_problem_and_same_day/see_problem_of_high_year/world/year/hand_or_little_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_week(str_arg):
+    case(str_arg)
+    print('last_group')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_week('small_place')
