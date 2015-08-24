ef3b72b831afa4780617e6896c37bf898d3d2791
Jingkai Tang <jingkaitang@gmail.com>
2015-08-24 20:10:49 +0800

    add: come week/group/be_part_in_case/way_and_part/young_life.py and life_and_hand/right_year/different_year_and_time.py in first way

---
+++ life_and_hand/right_year/different_year_and_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way_or_old_government(str_arg):
+    have_little_world_by_point(str_arg)
+    print('go_case_for_great_group')
+
+def have_little_world_by_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way_or_old_government('early_point')
---
+++ week/group/be_part_in_case/way_and_part/young_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_able_point_at_day(str_arg):
+    fact(str_arg)
+    print('look_person_beneath_same_woman')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_able_point_at_day('come_different_point_by_high_place')
