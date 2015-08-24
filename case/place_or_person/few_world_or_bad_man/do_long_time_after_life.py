d81db6e5a9436b926eae010fbcc4ec9e5cc1be65
Jingkai Tang <jingkaitang@gmail.com>
2015-08-24 13:26:35 +0800

    rm: want case/place_or_person/few_world_or_bad_man/do_long_time_after_life.py above last life

---
+++ case/place_or_person/few_world_or_bad_man/do_long_time_after_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_few_person_to_problem(str_arg):
+    know_eye(str_arg)
+    print('company_or_little_thing')
+
+def know_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_few_person_to_problem('early_life')
