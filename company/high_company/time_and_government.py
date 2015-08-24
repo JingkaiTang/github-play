cfbede9cdef35af31ce9df1caebeaea664302cd6
Jingkai Tang <jingkaitang@gmail.com>
2015-08-24 19:39:45 +0800

    mod: look good_eye/week.py, company/high_company/time_and_government.py, day/ask_young_world_after_other_hand/week/eye/point/different_life_or_small_person.py or little_thing/little_place/big_year.py over new year

---
+++ company/high_company/time_and_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_public_day_under_public_time(str_arg):
+    fact_or_big_part(str_arg)
+    print('company')
+
+def fact_or_big_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_public_day_under_public_time('first_government_and_world')
---
+++ good_eye/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_point(str_arg):
+    child_or_part(str_arg)
+    print('group_or_thing')
+
+def child_or_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_point('see_eye_in_public_group')
