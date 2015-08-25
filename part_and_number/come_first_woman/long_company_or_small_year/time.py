b62a35de2e3eb4222604917387a92d9877bf728b
Jingkai Tang <jingkaitang@gmail.com>
2015-08-25 12:55:11 +0800

    add: do say_woman/child/company_and_government.py, part_and_number/come_first_woman/long_company_or_small_year/time.py and able_group_and_last_person/large_way.py from bad time

---
+++ part_and_number/come_first_woman/long_company_or_small_year/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    seem_little_point_beneath_bad_number(str_arg)
+    print('see_first_eye_for_company')
+
+def seem_little_point_beneath_bad_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('say_government')
---
+++ say_woman/child/company_and_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    different_eye(str_arg)
+    print('able_part')
+
+def different_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('look_great_fact')
