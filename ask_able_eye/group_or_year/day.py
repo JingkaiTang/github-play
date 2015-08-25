569381e97a1efd1278c900b11d4b61cb96e1f43f
Jingkai Tang <jingkaitang@gmail.com>
2015-08-26 05:55:33 +0800

    mix: have ask_able_eye/group_or_year/day.py beneath first day

---
+++ ask_able_eye/group_or_year/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_number_beneath_life(str_arg):
+    use_week(str_arg)
+    print('place')
+
+def use_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_number_beneath_life('good_man')
