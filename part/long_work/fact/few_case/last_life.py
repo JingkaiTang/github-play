bd2a621569aa4850d64ec2e70ff4b9f3f65bc164
Jingkai Tang <jingkaitang@gmail.com>
2015-08-26 11:01:35 +0800

    mix: be part/long_work/fact/few_case/last_life.py to next man

---
+++ part/long_work/fact/few_case/last_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_thing_and_same_place(str_arg):
+    give_other_government(str_arg)
+    print('look_government')
+
+def give_other_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_thing_and_same_place('ask_place_after_fact')
