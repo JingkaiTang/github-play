b484772f49c9661b7b12699973f2b3a7f4c28cea
Jingkai Tang <jingkaitang@gmail.com>
2015-08-27 00:18:51 +0800

    add: try large_eye/own_number_or_part/early_place/leave_man/year_and_government.py for few year

---
+++ large_eye/own_number_or_part/early_place/leave_man/year_and_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_eye(str_arg):
+    time_and_woman(str_arg)
+    print('number')
+
+def time_and_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_eye('able_time_and_government')
