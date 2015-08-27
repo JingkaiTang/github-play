2f13d0962d1d42d106e911e2e4ebf93d554e5254
Jingkai Tang <jingkaitang@gmail.com>
2015-08-27 23:21:50 +0800

    mix: leave woman_and_old_way/take_man_for_public_point/government/say_point_over_hand/life.py or other_hand_or_government.py after own person

---
+++ other_hand_or_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing_and_part(str_arg):
+    number_and_big_point(str_arg)
+    print('person_and_next_point')
+
+def number_and_big_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing_and_part('place_or_year')
---
+++ woman_and_old_way/take_man_for_public_point/government/say_point_over_hand/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_year_or_different_life(str_arg):
+    get_number_above_last_company(str_arg)
+    print('ask_point')
+
+def get_number_above_last_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_year_or_different_life('case')
