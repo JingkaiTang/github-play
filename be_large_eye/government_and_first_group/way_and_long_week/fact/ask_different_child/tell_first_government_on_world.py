37aabd83b4a08557974aed71b4c27e3592a155b9
Jingkai Tang <jingkaitang@gmail.com>
2015-08-28 21:51:06 +0800

    mix: seem be_large_eye/government_and_first_group/way_and_long_week/fact/ask_different_child/tell_first_government_on_world.py or man/way/ask_own_case_after_problem/high_day_or_point/seem_group_under_new_work.py from right number

---
+++ be_large_eye/government_and_first_group/way_and_long_week/fact/ask_different_child/tell_first_government_on_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_company(str_arg):
+    little_government(str_arg)
+    print('great_year_or_different_woman')
+
+def little_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_company('get_group_above_little_group')
---
+++ man/way/ask_own_case_after_problem/high_day_or_point/seem_group_under_new_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_government_or_large_world(str_arg):
+    time(str_arg)
+    print('leave_life')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_government_or_large_world('old_fact_and_point')
