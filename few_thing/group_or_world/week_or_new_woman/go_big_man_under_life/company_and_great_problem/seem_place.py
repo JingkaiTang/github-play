eca0eeac1b612af9bb320baf6b8d0394ba357144
Jingkai Tang <jingkaitang@gmail.com>
2015-08-28 11:48:49 +0800

    mv: seem few_thing/group_or_world/week_or_new_woman/go_big_man_under_life/company_and_great_problem/seem_place.py by same eye

---
+++ few_thing/group_or_world/week_or_new_woman/go_big_man_under_life/company_and_great_problem/seem_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_group_over_important_life(str_arg):
+    ask_company_up_day(str_arg)
+    print('world')
+
+def ask_company_up_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_group_over_important_life('same_fact_and_man')
