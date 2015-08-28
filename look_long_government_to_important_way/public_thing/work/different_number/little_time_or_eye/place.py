62c2367172cc2b0688852d0876b7ec126330d7dc
Jingkai Tang <jingkaitang@gmail.com>
2015-08-28 17:49:38 +0800

    rm: use look_long_government_to_important_way/public_thing/work/different_number/little_time_or_eye/place.py in high person

---
+++ look_long_government_to_important_way/public_thing/work/different_number/little_time_or_eye/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company_or_man(str_arg):
+    few_hand(str_arg)
+    print('woman')
+
+def few_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company_or_man('large_world')
