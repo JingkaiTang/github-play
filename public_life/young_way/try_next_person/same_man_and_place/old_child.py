c313854be34cbfde56d65d7645f1a8b1456e5a1c
Jingkai Tang <jingkaitang@gmail.com>
2015-08-31 00:59:19 +0800

    mod: leave public_life/young_way/try_next_person/same_man_and_place/old_child.py to right thing

---
+++ public_life/young_way/try_next_person/same_man_and_place/old_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    problem(str_arg)
+    print('right_world')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('work_case_after_right_number')
