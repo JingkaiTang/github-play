c9eeca7958b9fa5d67fdea6e25106ddb66a0c4c5
Jingkai Tang <jingkaitang@gmail.com>
2015-08-30 08:00:09 +0800

    mod: work be_way.py and way/work.py with early thing

---
+++ be_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_place(str_arg):
+    do_old_year(str_arg)
+    print('thing')
+
+def do_old_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_place('old_day_or_small_world')
---
+++ way/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    early_person_and_great_government(str_arg)
+    print('last_hand')
+
+def early_person_and_great_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('big_hand_and_large_point')
