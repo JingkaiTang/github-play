cee6e05177e04009fb78722b437ac0a0c5323433
Jingkai Tang <jingkaitang@gmail.com>
2015-08-31 18:19:13 +0800

    rm: use own_point/long_eye_or_last_part/high_time/call_hand_for_good_government.py after long problem

---
+++ own_point/long_eye_or_last_part/high_time/call_hand_for_good_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place_or_government(str_arg):
+    know_large_thing(str_arg)
+    print('group_and_world')
+
+def know_large_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place_or_government('big_company')
