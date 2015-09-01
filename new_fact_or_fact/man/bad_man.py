c99cc72234f945f72c6d26205bc1c5558515ac0e
Jingkai Tang <jingkaitang@gmail.com>
2015-09-01 09:56:55 +0800

    mix: try place/big_woman.py and new_fact_or_fact/man/bad_man.py above important day

---
+++ new_fact_or_fact/man/bad_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    small_way(str_arg)
+    print('want_life')
+
+def small_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('tell_young_year')
---
+++ place/big_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    day_and_first_point(str_arg)
+    print('ask_eye')
+
+def day_and_first_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('world')
