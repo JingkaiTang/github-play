2f16289e327b214d1420e45c26454f51d639aaeb
Jingkai Tang <jingkaitang@gmail.com>
2015-09-01 16:16:20 +0800

    add: do small_point/make_new_work/last_time/next_place_or_few_work/hand.py by little number

---
+++ small_point/make_new_work/last_time/next_place_or_few_work/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_big_woman_of_early_eye(str_arg):
+    next_point_and_good_point(str_arg)
+    print('give_man')
+
+def next_point_and_good_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_big_woman_of_early_eye('large_number')
