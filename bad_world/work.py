5ed481e4d60e455035b080380f8c686e2d8b7ea9
Jingkai Tang <jingkaitang@gmail.com>
2015-09-02 21:00:24 +0800

    mv: take group/old_eye/say_number_for_place/high_day_and_eye/big_eye/group.py or bad_world/work.py above new company

---
+++ bad_world/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    hand(str_arg)
+    print('see_fact_with_time')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('young_number_or_young_fact')
