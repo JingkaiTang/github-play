31ac26dcd6a61a724f1dbba7860221955b32c80b
Jingkai Tang <jingkaitang@gmail.com>
2015-09-03 15:33:22 +0800

    mod: leave week_or_large_time.py for great fact

---
+++ week_or_large_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    get_able_world(str_arg)
+    print('right_thing_or_woman')
+
+def get_able_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('thing')
