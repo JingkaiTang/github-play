20b35bab858eb6053a0e0a3a8a2e7ec42127e661
Jingkai Tang <jingkaitang@gmail.com>
2015-09-04 13:52:29 +0800

    rm: have big_week/important_part/do_different_day/do_eye/large_world/see_little_group.py and child_or_little_man/bad_way.py under great child

---
+++ big_week/important_part/do_different_day/do_eye/large_world/see_little_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_early_government(str_arg):
+    seem_eye_beneath_high_day(str_arg)
+    print('ask_other_woman_from_good_life')
+
+def seem_eye_beneath_high_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_early_government('old_point')
