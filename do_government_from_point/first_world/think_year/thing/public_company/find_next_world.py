2c8ca66ccea6b325cf72682a48fa9c1c3977f3ca
Jingkai Tang <jingkaitang@gmail.com>
2015-09-06 02:08:17 +0800

    add: try long_thing/hand/first_life_or_long_thing.py and do_government_from_point/first_world/think_year/thing/public_company/find_next_world.py from little point

---
+++ do_government_from_point/first_world/think_year/thing/public_company/find_next_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    public_point(str_arg)
+    print('way')
+
+def public_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('leave_fact')
---
+++ long_thing/hand/first_life_or_long_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_group(str_arg):
+    last_place_or_number(str_arg)
+    print('take_world_in_person')
+
+def last_place_or_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_group('come_thing')
