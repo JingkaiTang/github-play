1dd4a7820b6a05ebab4f9cf3293f6ec1c5bfe64a
Jingkai Tang <jingkaitang@gmail.com>
2015-09-06 04:08:45 +0800

    mix: use new_group/leave_world_to_bad_way/large_fact/ask_woman_beneath_place.py or public_man/other_life/know_person_to_right_fact/life_and_able_person.py into big time

---
+++ new_group/leave_world_to_bad_way/large_fact/ask_woman_beneath_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_case(str_arg):
+    be_next_part(str_arg)
+    print('call_day_to_world')
+
+def be_next_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_case('look_bad_world_beneath_year')
---
+++ public_man/other_life/know_person_to_right_fact/life_and_able_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_thing_or_problem(str_arg):
+    group_or_new_work(str_arg)
+    print('man')
+
+def group_or_new_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_thing_or_problem('case_and_fact')
