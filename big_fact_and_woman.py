a8ceec61a6b6850b19d47a32d953e7dd270dbb30
Jingkai Tang <jingkaitang@gmail.com>
2015-09-06 11:09:01 +0800

    mod: call big_fact_and_woman.py after new point

---
+++ big_fact_and_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_thing(str_arg):
+    public_week(str_arg)
+    print('eye_or_place')
+
+def public_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_thing('place')
