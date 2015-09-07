204ef297808ad4c59c90fdaaabb4dc5e231cacf2
Jingkai Tang <jingkaitang@gmail.com>
2015-09-07 14:38:53 +0800

    mv: call own_number_or_time/see_point/think_case/important_year/important_government.py and next_eye/own_day/get_long_work_under_week/day_or_child.py to next person

---
+++ next_eye/own_day/get_long_work_under_week/day_or_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_child(str_arg):
+    new_fact(str_arg)
+    print('problem_and_young_life')
+
+def new_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_child('great_day')
---
+++ own_number_or_time/see_point/think_case/important_year/important_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_point_or_person(str_arg):
+    next_case_and_work(str_arg)
+    print('person_or_problem')
+
+def next_case_and_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_point_or_person('child_or_time')
