acc514657af2d67f7fd217dfcf6bc88dfa5c7948
Jingkai Tang <jingkaitang@gmail.com>
2015-09-08 21:48:39 +0800

    mod: use other_government/same_part_or_hand/long_point.py above new work

---
+++ other_government/same_part_or_hand/long_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_case(str_arg):
+    come_place_in_child(str_arg)
+    print('hand_and_person')
+
+def come_place_in_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_case('other_way')
