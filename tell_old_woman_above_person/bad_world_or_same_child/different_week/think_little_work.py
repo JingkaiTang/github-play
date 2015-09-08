ff502d16fb66e5493f7342ba181a8380c6ffa0e9
Jingkai Tang <jingkaitang@gmail.com>
2015-09-09 00:56:50 +0800

    mod: work hand/own_fact_or_company/high_person/different_work/same_company_or_year.py or tell_old_woman_above_person/bad_world_or_same_child/different_week/think_little_work.py under few number

---
+++ tell_old_woman_above_person/bad_world_or_same_child/different_week/think_little_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    company(str_arg)
+    print('small_government_or_small_case')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('number')
