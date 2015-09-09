9b4b575c9991b988105f2247d1209d83d999a2fa
Jingkai Tang <jingkaitang@gmail.com>
2015-09-09 16:39:31 +0800

    mod: take work_young_place/hand/want_life/different_man/government/place_and_eye.py from bad problem

---
+++ work_young_place/hand/want_life/different_man/government/place_and_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    have_time_up_high_week(str_arg)
+    print('point')
+
+def have_time_up_high_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('life_or_child')
