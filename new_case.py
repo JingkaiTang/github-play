8df3c9202f4a19a12473b7c89be5b5fc1560e86c
Jingkai Tang <jingkaitang@gmail.com>
2015-09-10 05:42:10 +0800

    mod: call right_life.py or new_case.py of small group

---
+++ new_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_person(str_arg):
+    give_long_eye(str_arg)
+    print('other_hand')
+
+def give_long_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_person('want_long_work_up_next_world')
---
+++ right_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_case_over_company(str_arg):
+    know_right_week_of_place(str_arg)
+    print('right_man')
+
+def know_right_week_of_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_case_over_company('new_fact')
