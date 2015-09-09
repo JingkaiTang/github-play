56bb41aeae22b98e9dde5a768fa4b86de2f5b2f2
Jingkai Tang <jingkaitang@gmail.com>
2015-09-10 06:00:59 +0800

    rm: think first_work/small_point.py beneath small year

---
+++ first_work/small_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_same_number(str_arg):
+    fact_or_hand(str_arg)
+    print('have_small_hand_in_company')
+
+def fact_or_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_same_number('small_fact')
