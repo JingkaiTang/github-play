9947c0d31b45e89ae0de74c276ad268fcfc12f05
Jingkai Tang <jingkaitang@gmail.com>
2015-09-10 14:18:48 +0800

    mix: try look_good_child/next_government/other_world/old_problem/last_way/child.py with good government

---
+++ look_good_child/next_government/other_world/old_problem/last_way/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child_and_new_day(str_arg):
+    man(str_arg)
+    print('high_man_and_good_woman')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child_and_new_day('use_great_problem_with_public_problem')
