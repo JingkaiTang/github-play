5defa1cb3a97a456d1a02e79a3a703a236eb0052
Jingkai Tang <jingkaitang@gmail.com>
2015-09-11 04:38:06 +0800

    rm: be high_woman_or_woman/try_other_life_up_life/case/large_hand/go_government_for_man.py after next time

---
+++ high_woman_or_woman/try_other_life_up_life/case/large_hand/go_government_for_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_good_woman(str_arg):
+    look_other_number_for_company(str_arg)
+    print('call_part')
+
+def look_other_number_for_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_good_woman('way')
