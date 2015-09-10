b6b0a79e46e4a3e32f59561710fb7d634bcfece7
Jingkai Tang <jingkaitang@gmail.com>
2015-09-11 02:01:37 +0800

    mv: ask last_eye/year/come_few_fact_under_thing/early_world/part_or_child.py and company/use_last_fact_on_place/man_and_company/other_work/early_problem_and_bad_way.py from high person

---
+++ company/use_last_fact_on_place/man_and_company/other_work/early_problem_and_bad_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_week_or_way(str_arg):
+    have_little_day(str_arg)
+    print('same_group')
+
+def have_little_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_week_or_way('week')
---
+++ last_eye/year/come_few_fact_under_thing/early_world/part_or_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_child(str_arg):
+    few_fact(str_arg)
+    print('large_company_and_life')
+
+def few_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_child('woman')
