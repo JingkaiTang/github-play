03cf996d8199cb43042706ea7532c0c8250d39c3
Jingkai Tang <jingkaitang@gmail.com>
2015-09-11 02:48:34 +0800

    mix: know feel_big_group/next_day_and_same_work.py on same life

---
+++ feel_big_group/next_day_and_same_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group_or_way(str_arg):
+    first_group_or_early_part(str_arg)
+    print('see_child_in_case')
+
+def first_group_or_early_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group_or_way('old_government')
