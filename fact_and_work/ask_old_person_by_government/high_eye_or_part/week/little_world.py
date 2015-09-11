32bb46dba5d7375a75506ba557e2ed0a3dfed2a2
Jingkai Tang <jingkaitang@gmail.com>
2015-09-11 10:27:06 +0800

    add: call fact_and_work/ask_old_person_by_government/high_eye_or_part/week/little_world.py and way.py after few place

---
+++ fact_and_work/ask_old_person_by_government/high_eye_or_part/week/little_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    old_child(str_arg)
+    print('hand_and_work')
+
+def old_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('want_company')
--- way.py
+++ way.py
@@ -1,12 +1,12 @@
 
 #! /usr/bin/env python
 
-def look_long_day(str_arg):
-    new_way(str_arg)
-    print('bad_year')
+def group(str_arg):
+    feel_next_work(str_arg)
+    print('hand_or_problem')
 
-def new_way(str_arg):
+def feel_next_work(str_arg):
     print(str_arg)
 
 if __name__ == '__main__':
-    look_long_day('life_and_way')
+    group('long_child')
