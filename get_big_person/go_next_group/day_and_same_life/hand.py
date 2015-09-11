b41f1ebf6728759e4a3d5c7f1f57a895a58bf893
Jingkai Tang <jingkaitang@gmail.com>
2015-09-11 15:59:08 +0800

    mod: do way_and_week/hand.py and get_big_person/go_next_group/day_and_same_life/hand.py at great company

---
+++ get_big_person/go_next_group/day_and_same_life/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    get_young_hand(str_arg)
+    print('good_person')
+
+def get_young_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('right_child')
---
+++ way_and_week/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    little_government(str_arg)
+    print('give_time_by_problem')
+
+def little_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('group')
