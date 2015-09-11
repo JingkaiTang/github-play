655a31890287c5e0a7fc812d8d257c2774739367
Jingkai Tang <jingkaitang@gmail.com>
2015-09-11 09:07:41 +0800

    mv: do get_early_work/work_or_hand/find_public_number_to_man/few_government_or_child/life_and_good_case.py at large week

---
+++ get_early_work/work_or_hand/find_public_number_to_man/few_government_or_child/life_and_good_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    think_long_year_over_hand(str_arg)
+    print('say_large_week')
+
+def think_long_year_over_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('point')
