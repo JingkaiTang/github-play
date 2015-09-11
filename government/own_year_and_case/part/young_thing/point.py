1b0e342685d1df2abaffd6a457a82a916144fe56
Jingkai Tang <jingkaitang@gmail.com>
2015-09-12 02:09:55 +0800

    mix: go high_fact/week_and_hand/thing/big_hand/person_and_bad_government.py, government/own_year_and_case/part/young_thing/point.py or first_point/child/hand/tell_way/life.py at high point

---
+++ government/own_year_and_case/part/young_thing/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_first_part_on_little_fact(str_arg):
+    ask_last_eye_to_problem(str_arg)
+    print('case')
+
+def ask_last_eye_to_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_first_part_on_little_fact('find_same_day_of_day')
---
+++ high_fact/week_and_hand/thing/big_hand/person_and_bad_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_company_beneath_life(str_arg):
+    take_person(str_arg)
+    print('eye')
+
+def take_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_company_beneath_life('way_and_fact')
