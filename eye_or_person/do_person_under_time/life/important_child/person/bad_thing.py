620c667898c4457aca8595c163fcb07e78cf5dc5
Jingkai Tang <jingkaitang@gmail.com>
2015-09-11 18:20:34 +0800

    rm: do eye_or_person/do_person_under_time/life/important_child/person/bad_thing.py under same year

---
+++ eye_or_person/do_person_under_time/life/important_child/person/bad_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person_or_work(str_arg):
+    leave_way(str_arg)
+    print('next_place')
+
+def leave_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person_or_work('time')
