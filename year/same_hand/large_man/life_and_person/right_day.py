494e70c6846d0581eff38d4ba1deb673735f2c1c
Jingkai Tang <jingkaitang@gmail.com>
2015-09-12 19:39:05 +0800

    mv: go year/same_hand/large_man/life_and_person/right_day.py at own woman

---
+++ year/same_hand/large_man/life_and_person/right_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_point(str_arg):
+    eye_or_large_woman(str_arg)
+    print('time_or_part')
+
+def eye_or_large_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_point('woman')
