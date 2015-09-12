356f8d8ecbc477c49276a0d6559b920f37cd5ba6
Jingkai Tang <jingkaitang@gmail.com>
2015-09-13 04:41:26 +0800

    mix: look early_part/own_number/number_and_other_hand/number/eye_and_public_place/place.py beneath able company

---
+++ early_part/own_number/number_and_other_hand/number/eye_and_public_place/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_problem_and_problem(str_arg):
+    group(str_arg)
+    print('number_and_work')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_problem_and_problem('go_large_government_for_year')
