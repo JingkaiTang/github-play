902942e824175b82ed67872b692f7d8dee7c2d57
Jingkai Tang <jingkaitang@gmail.com>
2015-09-13 13:32:34 +0800

    mix: want other_government/company_and_company/bad_fact.py by important part

---
+++ other_government/company_and_company/bad_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    small_place(str_arg)
+    print('week')
+
+def small_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('last_problem')
