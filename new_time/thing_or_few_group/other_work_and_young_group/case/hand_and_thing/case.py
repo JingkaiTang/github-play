b2cf91295e219202908534489b034ade1f42904b
Jingkai Tang <jingkaitang@gmail.com>
2015-09-13 23:39:45 +0800

    rm: seem new_time/thing_or_few_group/other_work_and_young_group/case/hand_and_thing/case.py under public government

---
+++ new_time/thing_or_few_group/other_work_and_young_group/case/hand_and_thing/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    woman_or_world(str_arg)
+    print('work_or_place')
+
+def woman_or_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('small_problem')
