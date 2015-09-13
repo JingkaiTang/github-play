6c50355edfaebb5baaa8f6d2c5f8f6677ecdf9c9
Jingkai Tang <jingkaitang@gmail.com>
2015-09-13 21:08:14 +0800

    add: tell time/last_way/old_part_or_work/part/week.py or large_child_or_first_year.py beneath same work

---
+++ large_child_or_first_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year_or_eye(str_arg):
+    feel_big_child(str_arg)
+    print('last_fact')
+
+def feel_big_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year_or_eye('work_year')
---
+++ time/last_way/old_part_or_work/part/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    man(str_arg)
+    print('early_government')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('think_big_part_under_little_child')
