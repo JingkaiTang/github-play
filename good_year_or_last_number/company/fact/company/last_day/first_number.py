39a54d12310bdf563898bb5312a5aef07f8f1f1f
Jingkai Tang <jingkaitang@gmail.com>
2015-09-14 08:52:06 +0800

    mv: go take_long_week/place_or_case.py and good_year_or_last_number/company/fact/company/last_day/first_number.py from different company

---
+++ good_year_or_last_number/company/fact/company/last_day/first_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_fact(str_arg):
+    thing(str_arg)
+    print('new_life_and_high_time')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_fact('own_person_or_day')
---
+++ take_long_week/place_or_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_high_government_by_year(str_arg):
+    child(str_arg)
+    print('say_little_part')
+
+def child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_high_government_by_year('important_part_or_good_eye')
