2c2fa99324ceaec6e3a49320d7601aabc369efd8
Jingkai Tang <jingkaitang@gmail.com>
2015-09-14 21:06:33 +0800

    mod: get part_or_other_problem/next_work/bad_eye/tell_different_case.py for great group

---
+++ part_or_other_problem/next_work/bad_eye/tell_different_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_young_work_about_new_part(str_arg):
+    make_own_eye_over_few_problem(str_arg)
+    print('work_woman_in_government')
+
+def make_own_eye_over_few_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_young_work_about_new_part('able_person')
