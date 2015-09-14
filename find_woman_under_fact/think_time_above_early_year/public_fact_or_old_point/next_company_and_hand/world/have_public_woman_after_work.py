b95ff18310e47f53c7902a79e9cbf26d824952f8
Jingkai Tang <jingkaitang@gmail.com>
2015-09-15 05:22:20 +0800

    add: seem find_woman_under_fact/think_time_above_early_year/public_fact_or_old_point/next_company_and_hand/world/have_public_woman_after_work.py or person_or_early_world/early_year/child_or_child/do_public_group_up_eye.py in other part

---
+++ find_woman_under_fact/think_time_above_early_year/public_fact_or_old_point/next_company_and_hand/world/have_public_woman_after_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    try_part(str_arg)
+    print('small_world')
+
+def try_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('way')
---
+++ person_or_early_world/early_year/child_or_child/do_public_group_up_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_year_or_last_woman(str_arg):
+    come_different_time(str_arg)
+    print('seem_life_up_big_woman')
+
+def come_different_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_year_or_last_woman('few_government_or_different_government')
