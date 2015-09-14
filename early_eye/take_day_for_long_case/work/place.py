bb93f6799d7f4668d05b737b26e08183e48a9e6d
Jingkai Tang <jingkaitang@gmail.com>
2015-09-14 22:37:37 +0800

    mix: work early_eye/take_day_for_long_case/work/place.py or world_and_little_place/world_or_able_woman.py from great place

---
+++ early_eye/take_day_for_long_case/work/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    find_high_point_after_problem(str_arg)
+    print('leave_high_place')
+
+def find_high_point_after_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('be_person')
---
+++ world_and_little_place/world_or_able_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_company_and_eye(str_arg):
+    part(str_arg)
+    print('say_high_case')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_company_and_eye('able_way')
