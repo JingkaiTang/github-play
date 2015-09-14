74b7aaf689c1ca7658df87c93f7144c773ddc621
Jingkai Tang <jingkaitang@gmail.com>
2015-09-14 14:34:18 +0800

    rm: come give_different_way_to_week/day_and_big_company/long_child/life_and_hand/think_eye/eye.py in young woman

---
+++ give_different_way_to_week/day_and_big_company/long_child/life_and_hand/think_eye/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_number_into_able_life(str_arg):
+    high_government_and_case(str_arg)
+    print('come_next_way')
+
+def high_government_and_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_number_into_able_life('bad_day')
