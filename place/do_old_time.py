8df99ff14baf51c68209c7959c7cca9ca62ace92
Jingkai Tang <jingkaitang@gmail.com>
2015-09-15 21:57:37 +0800

    rm: see place/do_old_time.py above high day

---
+++ place/do_old_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    large_year(str_arg)
+    print('own_time')
+
+def large_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('case_and_company')
