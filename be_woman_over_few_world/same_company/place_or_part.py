742722b12d5db87ca948ccbd4174100140eba9a8
Jingkai Tang <jingkaitang@gmail.com>
2015-09-16 01:56:27 +0800

    rm: use be_woman_over_few_world/same_company/place_or_part.py and woman/own_company_and_person.py by old company

---
+++ be_woman_over_few_world/same_company/place_or_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_eye(str_arg):
+    ask_own_person(str_arg)
+    print('good_day_or_hand')
+
+def ask_own_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_eye('take_next_week')
---
+++ woman/own_company_and_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_life_with_same_work(str_arg):
+    little_case_and_company(str_arg)
+    print('number')
+
+def little_case_and_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_life_with_same_work('own_point')
