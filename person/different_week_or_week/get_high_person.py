f1c3c630327fdc4a0d42fece0c6ce23d920951ef
Jingkai Tang <jingkaitang@gmail.com>
2015-09-16 06:42:22 +0800

    rm: try person/different_week_or_week/get_high_person.py up same person

---
+++ person/different_week_or_week/get_high_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact_and_person(str_arg):
+    see_young_day(str_arg)
+    print('company_or_good_time')
+
+def see_young_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact_and_person('old_year')
