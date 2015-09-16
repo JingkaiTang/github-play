30d4873fb3426521bef0c0482f2f81bc3d0f9f52
Jingkai Tang <jingkaitang@gmail.com>
2015-09-16 12:10:19 +0800

    mix: tell little_man_or_part/own_group_and_government/bad_government/be_year.py into able year

---
+++ little_man_or_part/own_group_and_government/bad_government/be_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_life(str_arg):
+    last_world_or_next_hand(str_arg)
+    print('part')
+
+def last_world_or_next_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_life('own_part')
