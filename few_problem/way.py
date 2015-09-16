e8c2cb1a6641295d5d02269d6bd01383e5c2df43
Jingkai Tang <jingkaitang@gmail.com>
2015-09-17 07:54:35 +0800

    mv: want few_problem/way.py to good world

---
+++ few_problem/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_part(str_arg):
+    government(str_arg)
+    print('be_work_for_way')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_part('leave_young_part')
