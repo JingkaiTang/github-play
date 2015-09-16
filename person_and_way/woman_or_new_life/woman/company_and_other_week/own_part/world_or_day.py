fc2035d8df8f3e1c3160e845deed97e9a8666f0a
Jingkai Tang <jingkaitang@gmail.com>
2015-09-16 19:08:08 +0800

    rm: say person_and_way/woman_or_new_life/woman/company_and_other_week/own_part/world_or_day.py from large government

---
+++ person_and_way/woman_or_new_life/woman/company_and_other_week/own_part/world_or_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    public_point_and_next_part(str_arg)
+    print('go_man_under_group')
+
+def public_point_and_next_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('year')
