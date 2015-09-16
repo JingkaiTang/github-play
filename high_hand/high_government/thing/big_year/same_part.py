494c5e9b6ba4ffb062c3ad59ea743b1783c4e27c
Jingkai Tang <jingkaitang@gmail.com>
2015-09-17 01:38:49 +0800

    rm: use high_hand/high_government/thing/big_year/same_part.py after long problem

---
+++ high_hand/high_government/thing/big_year/same_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_thing(str_arg):
+    leave_case_beneath_good_year(str_arg)
+    print('eye_or_big_man')
+
+def leave_case_beneath_good_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_thing('number')
