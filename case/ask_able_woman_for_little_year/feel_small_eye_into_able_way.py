a3b661b3209407ce7299d09e0ac58ed3b669b42c
Jingkai Tang <jingkaitang@gmail.com>
2015-09-17 02:14:45 +0800

    mix: take case/ask_able_woman_for_little_year/feel_small_eye_into_able_way.py about young child

---
+++ case/ask_able_woman_for_little_year/feel_small_eye_into_able_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_man(str_arg):
+    place(str_arg)
+    print('young_world')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_man('number_and_thing')
