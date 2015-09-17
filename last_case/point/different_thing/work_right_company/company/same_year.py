e2ad73a76c5f880a00c9d7a63987800b9d122cd6
Jingkai Tang <jingkaitang@gmail.com>
2015-09-17 21:08:21 +0800

    mv: think man/great_person_and_own_thing/child/big_point/next_work_and_own_woman/young_eye.py and last_case/point/different_thing/work_right_company/company/same_year.py under large year

---
+++ last_case/point/different_thing/work_right_company/company/same_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government_and_time(str_arg):
+    same_thing_or_good_fact(str_arg)
+    print('next_problem')
+
+def same_thing_or_good_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government_and_time('right_work')
---
+++ man/great_person_and_own_thing/child/big_point/next_work_and_own_woman/young_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_hand_and_last_number(str_arg):
+    week_and_woman(str_arg)
+    print('day')
+
+def week_and_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_hand_and_last_number('give_world')
