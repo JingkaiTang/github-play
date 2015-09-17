aa312c5f33f735bc5bbe170a5f8f86d0ac5075ea
Jingkai Tang <jingkaitang@gmail.com>
2015-09-17 16:55:43 +0800

    mv: come know_public_thing/young_year/long_year.py or first_world_or_high_part/say_public_problem/way_and_few_day.py in first number

---
+++ first_world_or_high_part/say_public_problem/way_and_few_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    number(str_arg)
+    print('point')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('take_government')
