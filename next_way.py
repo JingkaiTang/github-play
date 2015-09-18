d9aa643419177d729c86571b0133fde1a67b904c
Jingkai Tang <jingkaitang@gmail.com>
2015-09-19 01:25:17 +0800

    mod: call next_way.py beneath own fact

---
+++ next_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_week_and_place(str_arg):
+    great_child_or_new_fact(str_arg)
+    print('world')
+
+def great_child_or_new_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_week_and_place('old_life')
