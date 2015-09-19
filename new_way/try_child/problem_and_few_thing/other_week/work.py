e4c2df7a6c61729efc1d3f02ebe15e03997dd6de
Jingkai Tang <jingkaitang@gmail.com>
2015-09-20 05:07:11 +0800

    add: look new_way/try_child/problem_and_few_thing/other_week/work.py for good week

---
+++ new_way/try_child/problem_and_few_thing/other_week/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    long_hand_or_time(str_arg)
+    print('use_bad_time')
+
+def long_hand_or_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('man_and_great_hand')
