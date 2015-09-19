f57805b4b128fdefb3cfd3d58133e32838e08c2c
Jingkai Tang <jingkaitang@gmail.com>
2015-09-19 17:35:40 +0800

    mod: get week/case.py or place_or_other_eye.py on young world

---
+++ place_or_other_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_fact_and_next_year(str_arg):
+    young_hand_and_fact(str_arg)
+    print('right_way_or_way')
+
+def young_hand_and_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_fact_and_next_year('do_first_year')
