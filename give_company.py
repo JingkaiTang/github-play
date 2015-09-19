5ccd55a0d44a862b2a449fdb035d9afea29a1bf5
Jingkai Tang <jingkaitang@gmail.com>
2015-09-19 19:44:20 +0800

    mv: go give_company.py from able point

---
+++ give_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_man_or_next_person(str_arg):
+    life(str_arg)
+    print('fact')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_man_or_next_person('see_public_government')
