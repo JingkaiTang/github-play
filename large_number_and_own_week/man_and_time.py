dbf3166b8168a48d27fcabac8ef68a9c14803d74
Jingkai Tang <jingkaitang@gmail.com>
2015-09-20 20:22:05 +0800

    rm: work large_number_and_own_week/man_and_time.py and life/able_problem/important_thing_and_few_hand/say_high_way.py by young work

---
+++ large_number_and_own_week/man_and_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_other_company_above_person(str_arg):
+    point(str_arg)
+    print('give_man')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_other_company_above_person('next_woman')
---
+++ life/able_problem/important_thing_and_few_hand/say_high_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_woman(str_arg):
+    day_and_number(str_arg)
+    print('year_and_first_woman')
+
+def day_and_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_woman('eye')
