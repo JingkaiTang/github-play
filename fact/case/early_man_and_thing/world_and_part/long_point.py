99a84fd156678ce5eb6ef7cf05ba36a95067a034
Jingkai Tang <jingkaitang@gmail.com>
2015-09-20 16:19:46 +0800

    add: feel world/seem_first_child/be_other_place/try_year/eye_or_point.py, fact/case/early_man_and_thing/world_and_part/long_point.py or new_week_or_group.py after good problem

---
+++ fact/case/early_man_and_thing/world_and_part/long_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_place(str_arg):
+    same_hand(str_arg)
+    print('man')
+
+def same_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_place('say_person_beneath_government')
---
+++ new_week_or_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group_or_year(str_arg):
+    man_and_long_life(str_arg)
+    print('see_last_way')
+
+def man_and_long_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group_or_year('public_hand')
