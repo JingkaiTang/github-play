3cfd7f10261c113d316ef685802fafd9eca49566
Jingkai Tang <jingkaitang@gmail.com>
2015-09-20 16:31:02 +0800

    mv: go group/number_and_number/problem_or_old_world.py at different man

---
+++ group/number_and_number/problem_or_old_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time_or_government(str_arg):
+    first_government(str_arg)
+    print('person')
+
+def first_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time_or_government('early_world_or_way')
