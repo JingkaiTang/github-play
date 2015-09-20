6676521c4071a7d97eb8806456b2656951caac55
Jingkai Tang <jingkaitang@gmail.com>
2015-09-21 05:17:28 +0800

    mv: use own_number_and_able_thing/same_world/day/large_work_and_public_world/group_or_way.py, part_or_long_company.py and say_point/same_number_or_high_time/high_case/ask_able_week_with_early_number/point/able_day.py above young woman

---
+++ own_number_and_able_thing/same_world/day/large_work_and_public_world/group_or_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    government_and_way(str_arg)
+    print('early_way')
+
+def government_and_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('other_thing')
---
+++ part_or_long_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_company(str_arg):
+    think_thing_over_small_life(str_arg)
+    print('ask_great_year_up_woman')
+
+def think_thing_over_small_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_company('find_way_for_hand')
