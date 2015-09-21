a2dea6a6a21c965121624214d91aa4cc6d68480b
Jingkai Tang <jingkaitang@gmail.com>
2015-09-22 01:51:27 +0800

    add: know little_problem/want_different_life_at_good_government/leave_young_number_at_person/old_year.py above small case

---
+++ little_problem/want_different_life_at_good_government/leave_young_number_at_person/old_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    make_point(str_arg)
+    print('work_year')
+
+def make_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('old_thing')
