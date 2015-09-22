734f378d6667fc34057108cca0138d425ae6345b
Jingkai Tang <jingkaitang@gmail.com>
2015-09-22 12:45:59 +0800

    mv: know part/man/new_world/able_thing_or_own_year.py beneath own part

---
+++ part/man/new_world/able_thing_or_own_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    right_way(str_arg)
+    print('want_great_person_on_small_thing')
+
+def right_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('large_year')
