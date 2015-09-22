05e5d62280a214223823ca3ab9a7052dddf91513
Jingkai Tang <jingkaitang@gmail.com>
2015-09-22 11:11:45 +0800

    mix: want get_week/different_way/case/seem_same_number.py from old person

---
+++ get_week/different_way/case/seem_same_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_part_about_few_fact(str_arg):
+    leave_early_hand_up_next_number(str_arg)
+    print('young_problem_and_small_government')
+
+def leave_early_hand_up_next_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_part_about_few_fact('look_person_up_world')
