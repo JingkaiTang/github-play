dac5ebd1b2b193b34f675ffa5ef7133a762953e3
Jingkai Tang <jingkaitang@gmail.com>
2015-09-23 12:12:42 +0800

    mv: have new_work/person.py at right work

---
+++ new_work/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_work(str_arg):
+    problem(str_arg)
+    print('right_place_or_first_part')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_work('point_or_work')
