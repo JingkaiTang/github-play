641698a422f7e18e35a266be47d460d9451b744f
Jingkai Tang <jingkaitang@gmail.com>
2015-09-25 04:34:25 +0800

    mv: look ask_life/first_way/important_number.py under last person

---
+++ ask_life/first_way/important_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    little_government(str_arg)
+    print('find_way')
+
+def little_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('life')
