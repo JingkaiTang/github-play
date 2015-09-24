2fbfa4855dbc45a830c669acbe37c10f7535535f
Jingkai Tang <jingkaitang@gmail.com>
2015-09-24 19:58:19 +0800

    mix: feel same_company/right_number.py and feel_able_point/young_world.py over old number

---
+++ feel_able_point/young_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    think_government(str_arg)
+    print('great_place_and_eye')
+
+def think_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('high_life_and_number')
---
+++ same_company/right_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    eye_and_government(str_arg)
+    print('feel_new_man_at_work')
+
+def eye_and_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('go_thing_above_young_group')
