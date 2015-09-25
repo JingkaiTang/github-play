c2ef3a65956133f1c49469aeed2d013c99d5faff
Jingkai Tang <jingkaitang@gmail.com>
2015-09-25 10:51:19 +0800

    mod: have person/last_person/have_week_after_last_way.py beneath other group

---
+++ person/last_person/have_week_after_last_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man_and_year(str_arg):
+    call_eye(str_arg)
+    print('few_work')
+
+def call_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man_and_year('day')
