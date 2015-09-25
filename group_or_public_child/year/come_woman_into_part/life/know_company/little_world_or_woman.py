7e6fdd99318c3bb314afba8bbd78c0f1513ae832
Jingkai Tang <jingkaitang@gmail.com>
2015-09-25 09:49:06 +0800

    add: tell own_point/right_week/man_or_small_number/day/bad_eye/life.py and group_or_public_child/year/come_woman_into_part/life/know_company/little_world_or_woman.py to same number

---
+++ group_or_public_child/year/come_woman_into_part/life/know_company/little_world_or_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_case_and_work(str_arg):
+    able_world(str_arg)
+    print('work_thing_in_work')
+
+def able_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_case_and_work('see_part')
---
+++ own_point/right_week/man_or_small_number/day/bad_eye/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    early_child_and_company(str_arg)
+    print('place')
+
+def early_child_and_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('world')
