bade16ada198fc623d96fe27722fdb68e682446b
Jingkai Tang <jingkaitang@gmail.com>
2015-09-27 04:59:34 +0800

    add: try hand/tell_public_year_for_point/do_last_point_beneath_bad_point/child.py above bad company

---
+++ hand/tell_public_year_for_point/do_last_point_beneath_bad_point/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_year(str_arg):
+    point(str_arg)
+    print('company')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_year('part')
