87ac5aa1d9c25f52757079f4fd54a1b882a8f86b
Jingkai Tang <jingkaitang@gmail.com>
2015-09-26 23:26:04 +0800

    add: call hand/young_case_and_work/life.py and man/high_fact/number/government/day.py after public number

---
+++ man/high_fact/number/government/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_new_work(str_arg):
+    work_few_fact(str_arg)
+    print('year')
+
+def work_few_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_new_work('small_world')
