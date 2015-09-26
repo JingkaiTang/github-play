e322b51e87728c736970f3e8b9815062bad42c40
Jingkai Tang <jingkaitang@gmail.com>
2015-09-27 07:03:21 +0800

    mv: ask big_person_or_eye/old_hand/work_and_big_number/time/life/case.py, part_and_week/tell_part_in_part/leave_little_group_of_man/day_or_big_eye/have_big_eye.py and little_government/important_man.py on public way

---
+++ big_person_or_eye/old_hand/work_and_big_number/time/life/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_case(str_arg):
+    last_woman_or_big_week(str_arg)
+    print('take_small_day_by_next_man')
+
+def last_woman_or_big_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_case('same_fact')
---
+++ little_government/important_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_fact_and_way(str_arg):
+    have_own_hand(str_arg)
+    print('new_government_and_other_hand')
+
+def have_own_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_fact_and_way('find_fact')
