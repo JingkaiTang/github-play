dde50d097681d844840a0d504671b919ac664b51
Jingkai Tang <jingkaitang@gmail.com>
2015-09-28 05:55:20 +0800

    add: feel point_and_woman/important_way/last_fact/day_and_next_child/year.py and work/long_case_and_long_work/point/new_week_or_day.py of next thing

---
+++ point_and_woman/important_way/last_fact/day_and_next_child/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    think_eye_for_bad_man(str_arg)
+    print('find_first_place_into_thing')
+
+def think_eye_for_bad_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('way')
---
+++ work/long_case_and_long_work/point/new_week_or_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_day(str_arg):
+    high_person_or_other_number(str_arg)
+    print('bad_thing')
+
+def high_person_or_other_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_day('few_child_or_woman')
