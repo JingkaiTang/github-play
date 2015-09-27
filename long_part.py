57d353cb17ccea5579aae015513637f4b32c4b16
Jingkai Tang <jingkaitang@gmail.com>
2015-09-28 04:44:37 +0800

    rm: take long_part.py with next time

---
+++ long_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_world_or_few_case(str_arg):
+    young_person(str_arg)
+    print('right_point')
+
+def young_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_world_or_few_case('hand_or_day')
