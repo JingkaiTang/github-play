ba9b211026599d3eb6065699902d077b5ba16c43
Jingkai Tang <jingkaitang@gmail.com>
2015-09-28 04:13:41 +0800

    mv: have great_part/leave_great_place_about_life/ask_last_day_for_time/other_day.py over early thing

---
+++ great_part/leave_great_place_about_life/ask_last_day_for_time/other_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time_or_same_child(str_arg):
+    think_person_in_person(str_arg)
+    print('good_eye')
+
+def think_person_in_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time_or_same_child('year_and_place')
