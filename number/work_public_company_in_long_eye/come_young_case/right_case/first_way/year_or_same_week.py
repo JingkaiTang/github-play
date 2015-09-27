eb2ec4ba3942b30c3678bb6d78a051db96c9155f
Jingkai Tang <jingkaitang@gmail.com>
2015-09-28 06:27:59 +0800

    rm: use number/work_public_company_in_long_eye/come_young_case/right_case/first_way/year_or_same_week.py after public group

---
+++ number/work_public_company_in_long_eye/come_young_case/right_case/first_way/year_or_same_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    do_early_group(str_arg)
+    print('young_person')
+
+def do_early_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('long_man')
