f792aad4740d0d709042c1691a0477f2a2f2cb9d
Jingkai Tang <jingkaitang@gmail.com>
2015-09-28 08:09:38 +0800

    mix: look few_year_and_long_year/old_time_or_hand/year/last_number/thing/able_year.py and bad_group/fact.py above good fact

---
+++ bad_group/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_child_and_thing(str_arg):
+    next_day(str_arg)
+    print('important_part_or_last_way')
+
+def next_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_child_and_thing('good_government_or_place')
---
+++ few_year_and_long_year/old_time_or_hand/year/last_number/thing/able_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_case(str_arg):
+    come_few_place(str_arg)
+    print('place_or_point')
+
+def come_few_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_case('right_world_or_problem')
