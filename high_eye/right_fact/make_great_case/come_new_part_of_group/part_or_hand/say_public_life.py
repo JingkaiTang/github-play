593deee16af77401971c4b39cfd873eb4dace321
Jingkai Tang <jingkaitang@gmail.com>
2015-09-29 06:13:00 +0800

    rm: feel thing_or_bad_work.py and high_eye/right_fact/make_great_case/come_new_part_of_group/part_or_hand/say_public_life.py with same week

---
+++ high_eye/right_fact/make_great_case/come_new_part_of_group/part_or_hand/say_public_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_government_for_time(str_arg):
+    do_thing(str_arg)
+    print('ask_day')
+
+def do_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_government_for_time('first_group')
---
+++ thing_or_bad_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    leave_different_week(str_arg)
+    print('public_company')
+
+def leave_different_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('able_hand')
