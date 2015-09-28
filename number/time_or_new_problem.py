c4165b14a7040ae68ba6b7c512a68debbaccb420
Jingkai Tang <jingkaitang@gmail.com>
2015-09-28 13:42:15 +0800

    mv: take number/time_or_new_problem.py about large year

---
+++ number/time_or_new_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case_or_last_time(str_arg):
+    person(str_arg)
+    print('year_or_long_problem')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case_or_last_time('company_or_company')
