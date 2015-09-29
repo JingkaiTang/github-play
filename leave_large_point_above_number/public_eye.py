22e83adc746348ca78fad4a19630250aeb730ad2
Jingkai Tang <jingkaitang@gmail.com>
2015-09-29 17:25:46 +0800

    mv: find leave_large_point_above_number/public_eye.py from young week

---
+++ leave_large_point_above_number/public_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_time(str_arg):
+    think_own_fact_on_week(str_arg)
+    print('great_person_or_next_place')
+
+def think_own_fact_on_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_time('find_large_place')
