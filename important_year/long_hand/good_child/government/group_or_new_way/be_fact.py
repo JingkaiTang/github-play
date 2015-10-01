407437648426ebdd308c72231f2f6d40c2f1d882
Jingkai Tang <jingkaitang@gmail.com>
2015-10-01 11:47:45 +0800

    rm: feel important_year/long_hand/good_child/government/group_or_new_way/be_fact.py under long week

---
+++ important_year/long_hand/good_child/government/group_or_new_way/be_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_point_or_group(str_arg):
+    get_year_beneath_other_company(str_arg)
+    print('try_man_by_problem')
+
+def get_year_beneath_other_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_point_or_group('part_or_early_thing')
