929e231fa709459e35b573fd6b02157b8e34272a
Jingkai Tang <jingkaitang@gmail.com>
2015-10-01 15:28:27 +0800

    mix: tell look_day_over_group/way_or_long_person/last_time_and_woman/high_year_and_fact/way/give_company.py or work_and_company/look_bad_group/good_world_and_right_problem/come_thing/large_point_or_able_company.py with big way

---
+++ look_day_over_group/way_or_long_person/last_time_and_woman/high_year_and_fact/way/give_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    young_case_or_man(str_arg)
+    print('woman_and_able_part')
+
+def young_case_or_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('want_place')
---
+++ work_and_company/look_bad_group/good_world_and_right_problem/come_thing/large_point_or_able_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_fact_beneath_company(str_arg):
+    next_place(str_arg)
+    print('know_large_person')
+
+def next_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_fact_beneath_company('have_different_life')
