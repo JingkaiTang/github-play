cab8bd29c771bf77551ca42ece287b6a8ff9fb55
Jingkai Tang <jingkaitang@gmail.com>
2015-10-03 04:11:11 +0800

    rm: tell government/hand/point/day/little_case_and_way/place.py about great time

---
+++ government/hand/point/day/little_case_and_way/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_man(str_arg):
+    company_and_next_life(str_arg)
+    print('big_man')
+
+def company_and_next_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_man('great_man')
