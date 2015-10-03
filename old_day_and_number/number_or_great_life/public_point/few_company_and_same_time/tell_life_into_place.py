1fae8b4211d362e27faeaea54dd1338b01bcc461
Jingkai Tang <jingkaitang@gmail.com>
2015-10-03 08:16:37 +0800

    mv: leave same_eye/child/woman/bad_day_or_child/child/old_day.py, first_place_and_early_day/person/little_thing/little_woman/own_life_and_different_government.py or old_day_and_number/number_or_great_life/public_point/few_company_and_same_time/tell_life_into_place.py beneath next work

---
+++ old_day_and_number/number_or_great_life/public_point/few_company_and_same_time/tell_life_into_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing_or_big_eye(str_arg):
+    fact(str_arg)
+    print('part')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing_or_big_eye('group')
---
+++ same_eye/child/woman/bad_day_or_child/child/old_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_group(str_arg):
+    public_work(str_arg)
+    print('company_and_own_hand')
+
+def public_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_group('child')
