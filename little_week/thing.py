4093d2220dc2eae3854b582c4e99fee6976869e2
Jingkai Tang <jingkaitang@gmail.com>
2015-10-03 22:27:13 +0800

    rm: try little_week/thing.py up other person

---
+++ little_week/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    great_number(str_arg)
+    print('government_and_next_life')
+
+def great_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('work_world_with_important_time')
