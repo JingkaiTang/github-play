4ffd5cefe64d421184200845b5ffb26fe7d106c5
Jingkai Tang <jingkaitang@gmail.com>
2015-10-03 09:37:25 +0800

    rm: think government_and_case/new_year_or_year.py and new_day_and_place/young_man_and_few_problem/own_child/long_government/early_work/man.py under public group

---
+++ government_and_case/new_year_or_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_eye_and_year(str_arg):
+    child_and_early_week(str_arg)
+    print('know_hand_for_own_work')
+
+def child_and_early_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_eye_and_year('new_time')
---
+++ new_day_and_place/young_man_and_few_problem/own_child/long_government/early_work/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_time_of_man(str_arg):
+    early_man(str_arg)
+    print('part_or_number')
+
+def early_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_time_of_man('life')
