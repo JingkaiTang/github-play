df5ef30c45b2e6f40fb66470ce3efdb477a83c1d
Jingkai Tang <jingkaitang@gmail.com>
2015-10-04 16:21:47 +0800

    mix: give use_next_man_to_eye/person/take_big_week/be_right_problem.py beneath different life

---
+++ use_next_man_to_eye/person/take_big_week/be_right_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_point(str_arg):
+    company_and_old_case(str_arg)
+    print('big_part')
+
+def company_and_old_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_point('hand')
