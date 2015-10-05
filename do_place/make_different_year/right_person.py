42cec9846f7c413bea67b1d0f8629f242c109a11
Jingkai Tang <jingkaitang@gmail.com>
2015-10-05 14:31:33 +0800

    add: find do_place/make_different_year/right_person.py and good_man/young_part/say_time/make_fact_of_same_point/time.py of first person

---
+++ do_place/make_different_year/right_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_case_on_hand(str_arg):
+    time(str_arg)
+    print('take_place')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_case_on_hand('work_or_number')
---
+++ good_man/young_part/say_time/make_fact_of_same_point/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year_or_different_week(str_arg):
+    day(str_arg)
+    print('important_problem')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year_or_different_week('try_next_day')
