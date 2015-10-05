87fd0d64f91e7a1e48f5dbb1fe87fba5fe5f31e5
Jingkai Tang <jingkaitang@gmail.com>
2015-10-06 03:53:53 +0800

    add: think time/great_company/person/think_thing/large_person.py and able_part_and_thing/time_or_fact/week_or_last_world.py by large hand

---
+++ able_part_and_thing/time_or_fact/week_or_last_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_eye(str_arg):
+    problem(str_arg)
+    print('own_place')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_eye('small_group')
---
+++ time/great_company/person/think_thing/large_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_point_and_important_person(str_arg):
+    own_life(str_arg)
+    print('small_government')
+
+def own_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_point_and_important_person('tell_high_fact_about_next_government')
