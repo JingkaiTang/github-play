0ea0de9065cd6d4c6ce59ef694f318c9e963987f
Jingkai Tang <jingkaitang@gmail.com>
2015-10-08 02:56:22 +0800

    mv: look know_part/child/important_week/work_time_to_young_place/same_hand.py or little_work.py into same year

---
+++ know_part/child/important_week/work_time_to_young_place/same_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    eye_and_world(str_arg)
+    print('number_or_thing')
+
+def eye_and_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('think_thing')
--- little_work.py
+++ little_work.py
@@ -1,12 +1,12 @@
 
 #! /usr/bin/env python
 
-def able_place_and_next_time(str_arg):
-    way(str_arg)
-    print('able_life_and_child')
+def take_way_beneath_hand(str_arg):
+    try_great_way(str_arg)
+    print('own_time_or_government')
 
-def way(str_arg):
+def try_great_way(str_arg):
     print(str_arg)
 
 if __name__ == '__main__':
-    able_place_and_next_time('call_bad_fact')
+    take_way_beneath_hand('hand_and_last_life')
