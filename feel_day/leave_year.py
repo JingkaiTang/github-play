97f925d61adc7cb25570a8262002316645ed52cc
Jingkai Tang <jingkaitang@gmail.com>
2015-10-07 15:37:23 +0800

    add: give way/last_year_and_company/next_place/time_or_last_thing/fact.py and feel_day/leave_year.py with bad fact

---
+++ feel_day/leave_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_person(str_arg):
+    say_right_problem_into_fact(str_arg)
+    print('problem_or_government')
+
+def say_right_problem_into_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_person('big_part_and_problem')
---
+++ way/last_year_and_company/next_place/time_or_last_thing/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_time(str_arg):
+    way(str_arg)
+    print('give_other_child_in_person')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_time('want_early_work')
