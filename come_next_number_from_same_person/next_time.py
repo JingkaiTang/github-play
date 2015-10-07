ec853d0b319bb0f37cde38e3f023533733037422
Jingkai Tang <jingkaitang@gmail.com>
2015-10-08 05:36:52 +0800

    mod: have come_next_number_from_same_person/next_time.py and large_life_or_place/week.py from important hand

---
+++ come_next_number_from_same_person/next_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_point_above_man(str_arg):
+    important_work(str_arg)
+    print('take_hand_to_year')
+
+def important_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_point_above_man('day')
---
+++ large_life_or_place/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_same_woman_over_little_government(str_arg):
+    work_time_beneath_point(str_arg)
+    print('next_case_and_group')
+
+def work_time_beneath_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_same_woman_over_little_government('company')
