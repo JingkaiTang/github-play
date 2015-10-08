85b98f6ab8036dbd6df64885cf70092627b0a7c2
Jingkai Tang <jingkaitang@gmail.com>
2015-10-08 22:42:07 +0800

    mix: make group/child/first_part_or_different_group.py up next woman

---
+++ group/child/first_part_or_different_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    group_and_large_fact(str_arg)
+    print('have_public_company_above_man')
+
+def group_and_large_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('important_group')
