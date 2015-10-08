6f29a5a8abc256607b77ca3869277b67d381bc4b
Jingkai Tang <jingkaitang@gmail.com>
2015-10-08 15:18:02 +0800

    mv: say get_last_company/few_eye_or_few_point/different_day/person/great_number_and_case.py with old life

---
+++ get_last_company/few_eye_or_few_point/different_day/person/great_number_and_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man_and_part(str_arg):
+    use_able_eye(str_arg)
+    print('thing_or_life')
+
+def use_able_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man_and_part('be_new_group')
