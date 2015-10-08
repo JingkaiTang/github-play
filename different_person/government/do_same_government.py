97789a5cabfcb936836663d37cae202c8324c9c4
Jingkai Tang <jingkaitang@gmail.com>
2015-10-08 23:40:51 +0800

    add: think different_person/government/do_same_government.py for different thing

---
+++ different_person/government/do_same_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    seem_new_way(str_arg)
+    print('get_year_in_government')
+
+def seem_new_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('life')
