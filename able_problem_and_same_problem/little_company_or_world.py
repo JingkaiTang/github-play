599a83a76d2939dea991cc3eb44a5df4158a11e2
Jingkai Tang <jingkaitang@gmail.com>
2015-10-10 12:39:12 +0800

    mv: get young_part/first_year/tell_government/woman/old_company.py or able_problem_and_same_problem/little_company_or_world.py in great eye

---
+++ able_problem_and_same_problem/little_company_or_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    high_life(str_arg)
+    print('group')
+
+def high_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('day')
---
+++ young_part/first_year/tell_government/woman/old_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem_and_company(str_arg):
+    woman_or_important_case(str_arg)
+    print('go_government')
+
+def woman_or_important_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem_and_company('thing_and_year')
