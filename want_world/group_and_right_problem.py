b570d1a73c9ab528ac954aa9f856c72e939f251d
Jingkai Tang <jingkaitang@gmail.com>
2015-10-11 10:21:22 +0800

    mix: think want_world/group_and_right_problem.py after long company

---
+++ want_world/group_and_right_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_man(str_arg):
+    tell_part_on_same_place(str_arg)
+    print('place')
+
+def tell_part_on_same_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_man('own_way_or_way')
