979b9773127d977d722a14267897de3c6cef7e4f
Jingkai Tang <jingkaitang@gmail.com>
2015-10-11 23:12:55 +0800

    mix: use seem_world_up_child/time/eye_or_big_woman/have_large_fact_under_point.py in small man

---
+++ seem_world_up_child/time/eye_or_big_woman/have_large_fact_under_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time_or_day(str_arg):
+    hand(str_arg)
+    print('problem')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time_or_day('year_and_problem')
