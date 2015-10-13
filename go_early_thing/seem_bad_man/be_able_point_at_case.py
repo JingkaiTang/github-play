f8366b7659313814eb9f2caec9dd59d729a36368
Jingkai Tang <jingkaitang@gmail.com>
2015-10-13 11:56:11 +0800

    mv: be go_early_thing/seem_bad_man/be_able_point_at_case.py or new_point_and_early_problem/work_and_little_way/feel_early_woman.py in early fact

---
+++ go_early_thing/seem_bad_man/be_able_point_at_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_hand_and_old_way(str_arg):
+    few_point_and_few_part(str_arg)
+    print('work')
+
+def few_point_and_few_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_hand_and_old_way('great_place_or_world')
---
+++ new_point_and_early_problem/work_and_little_way/feel_early_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_able_hand_about_small_time(str_arg):
+    bad_week_and_bad_week(str_arg)
+    print('eye')
+
+def bad_week_and_bad_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_able_hand_about_small_time('number')
