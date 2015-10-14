af167b89b322ea1acb5a70af2ff8ab531ee6f5c3
Jingkai Tang <jingkaitang@gmail.com>
2015-10-14 10:20:01 +0800

    mod: seem thing_and_little_person/look_able_number_by_same_number/case/give_different_child/part.py or go_next_part/high_hand/tell_time/next_work_or_eye/right_eye/man.py for able place

---
+++ go_next_part/high_hand/tell_time/next_work_or_eye/right_eye/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_world_and_able_thing(str_arg):
+    big_number(str_arg)
+    print('long_government')
+
+def big_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_world_and_able_thing('other_work_or_long_day')
---
+++ thing_and_little_person/look_able_number_by_same_number/case/give_different_child/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_place_or_child(str_arg):
+    take_day_after_bad_woman(str_arg)
+    print('child_and_old_work')
+
+def take_day_after_bad_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_place_or_child('way')
