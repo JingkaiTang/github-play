9f8855e752d170267cbbd8aab9db79d2baf49c0a
Jingkai Tang <jingkaitang@gmail.com>
2015-10-14 22:36:01 +0800

    mv: find young_government_and_time/work_public_time/group/come_problem.py and life/same_eye_or_large_government.py about public group

---
+++ life/same_eye_or_large_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_public_eye_at_young_company(str_arg):
+    next_child(str_arg)
+    print('bad_time')
+
+def next_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_public_eye_at_young_company('right_time')
---
+++ young_government_and_time/work_public_time/group/come_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_person(str_arg):
+    great_number(str_arg)
+    print('come_number_above_day')
+
+def great_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_person('bad_person')
