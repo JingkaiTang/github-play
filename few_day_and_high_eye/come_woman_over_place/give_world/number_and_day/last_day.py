6faca564dc141156fa8cd02d209a2d08b4cc33ac
Jingkai Tang <jingkaitang@gmail.com>
2015-10-17 03:40:43 +0800

    add: look few_day_and_high_eye/come_woman_over_place/give_world/number_and_day/last_day.py at new year

---
+++ few_day_and_high_eye/come_woman_over_place/give_world/number_and_day/last_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    company(str_arg)
+    print('look_great_eye_with_first_work')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('make_life')
