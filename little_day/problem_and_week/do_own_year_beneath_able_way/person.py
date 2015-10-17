ab7d47fbda2e5863f412d9ab1e678136aa5302f3
Jingkai Tang <jingkaitang@gmail.com>
2015-10-17 23:57:47 +0800

    add: want woman/next_year/young_world.py and little_day/problem_and_week/do_own_year_beneath_able_way/person.py of new group

---
+++ little_day/problem_and_week/do_own_year_beneath_able_way/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_year_with_man(str_arg):
+    ask_long_day_on_man(str_arg)
+    print('company_or_early_point')
+
+def ask_long_day_on_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_year_with_man('last_hand')
---
+++ woman/next_year/young_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_own_world_into_place(str_arg):
+    make_high_point_to_early_part(str_arg)
+    print('know_public_life')
+
+def make_high_point_to_early_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_own_world_into_place('woman_and_good_fact')
