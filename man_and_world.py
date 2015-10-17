5437c85ebdaf1ec1aa0c8e2b93374a65249b0b95
Jingkai Tang <jingkaitang@gmail.com>
2015-10-17 19:50:17 +0800

    add: go man_and_world.py with early person

---
+++ man_and_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_group(str_arg):
+    able_woman(str_arg)
+    print('give_thing')
+
+def able_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_group('have_place')
