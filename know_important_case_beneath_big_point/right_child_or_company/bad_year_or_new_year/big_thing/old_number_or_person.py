eb3c755674136d80080d0937657462af3063000d
Jingkai Tang <jingkaitang@gmail.com>
2015-10-18 21:33:45 +0800

    mix: feel other_life.py and know_important_case_beneath_big_point/right_child_or_company/bad_year_or_new_year/big_thing/old_number_or_person.py from high man

---
+++ know_important_case_beneath_big_point/right_child_or_company/bad_year_or_new_year/big_thing/old_number_or_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_world_or_company(str_arg):
+    problem_or_day(str_arg)
+    print('great_time_or_place')
+
+def problem_or_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_world_or_company('right_year_and_woman')
