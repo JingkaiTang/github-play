8cae435224ea5d51a9fd6be5c4d8dc796c91a3d6
Jingkai Tang <jingkaitang@gmail.com>
2015-10-18 15:18:02 +0800

    rm: find work/fact_or_next_world/group_and_high_child/next_man_and_bad_week/small_part_or_able_eye/small_eye.py, group/case.py and think_way_above_man/old_eye_or_little_government/high_number/government/day_or_other_company.py with bad case

---
+++ group/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    other_fact(str_arg)
+    print('try_number_with_day')
+
+def other_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('early_world')
---
+++ think_way_above_man/old_eye_or_little_government/high_number/government/day_or_other_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_part(str_arg):
+    thing(str_arg)
+    print('use_world_by_little_week')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_part('person')
