9b475b0037d68c3dc9837104e8b3b5a5b4025615
Jingkai Tang <jingkaitang@gmail.com>
2015-10-22 02:36:06 +0800

    mv: try time/problem_or_small_week.py under high part

---
+++ time/problem_or_small_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_place(str_arg):
+    new_problem(str_arg)
+    print('man')
+
+def new_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_place('go_child')
