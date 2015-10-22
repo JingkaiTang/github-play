997eac9ba93ea0c1d7103831232f58fa6f803a72
Jingkai Tang <jingkaitang@gmail.com>
2015-10-22 12:20:47 +0800

    mod: call get_place_beneath_company/man_and_man/fact_and_good_child/say_high_problem_up_time/give_thing.py by little child

---
+++ get_place_beneath_company/man_and_man/fact_and_good_child/say_high_problem_up_time/give_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_hand(str_arg):
+    group_and_few_time(str_arg)
+    print('first_life')
+
+def group_and_few_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_hand('next_world')
