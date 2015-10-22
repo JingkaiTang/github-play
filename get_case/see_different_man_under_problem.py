6d180a8f269074fa48f9f40370f363dcd249eb5e
Jingkai Tang <jingkaitang@gmail.com>
2015-10-22 17:00:05 +0800

    mix: give get_case/see_different_man_under_problem.py with last thing

---
+++ get_case/see_different_man_under_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_group_or_eye(str_arg):
+    woman(str_arg)
+    print('large_way_and_day')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_group_or_eye('case')
