00625f2cf8419e91e099b06834215b473b39ac93
Jingkai Tang <jingkaitang@gmail.com>
2015-10-23 07:25:27 +0800

    rm: be time/life/fact/own_work/have_first_way/young_eye.py and few_time/get_way/part/new_year/company_or_big_world.py for other year

---
+++ few_time/get_way/part/new_year/company_or_big_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_child_and_world(str_arg):
+    use_year(str_arg)
+    print('government_and_high_week')
+
+def use_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_child_and_world('company')
---
+++ time/life/fact/own_work/have_first_way/young_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_child(str_arg):
+    thing(str_arg)
+    print('man_or_last_government')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_child('person')
