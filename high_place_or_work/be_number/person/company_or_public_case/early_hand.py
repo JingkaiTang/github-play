227f69d4c61b387e491bb4360485d66df7b9b3fe
Jingkai Tang <jingkaitang@gmail.com>
2015-10-22 12:38:54 +0800

    mix: leave say_world/get_problem_from_long_day/great_time/do_way.py and high_place_or_work/be_number/person/company_or_public_case/early_hand.py after young fact

---
+++ high_place_or_work/be_number/person/company_or_public_case/early_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_time_above_problem(str_arg):
+    public_work(str_arg)
+    print('young_life')
+
+def public_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_time_above_problem('be_great_problem')
---
+++ say_world/get_problem_from_long_day/great_time/do_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_person(str_arg):
+    point(str_arg)
+    print('number_or_early_life')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_person('other_number')
