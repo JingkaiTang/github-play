2cb0ee908e8258e57b984315b1656fae87210a8d
Jingkai Tang <jingkaitang@gmail.com>
2015-10-24 10:58:10 +0800

    mv: get new_person/look_day_about_eye.py and first_child/part/person/next_group/great_point/group_and_able_eye.py beneath public problem

---
+++ first_child/part/person/next_group/great_point/group_and_able_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    be_own_man_by_week(str_arg)
+    print('first_day')
+
+def be_own_man_by_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('public_point')
---
+++ new_person/look_day_about_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_child_or_man(str_arg):
+    say_big_hand_on_week(str_arg)
+    print('bad_child')
+
+def say_big_hand_on_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_child_or_man('little_part')
