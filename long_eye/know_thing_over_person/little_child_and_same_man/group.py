82fd74ad19e14bc95e6f93ea2af166d360e97259
Jingkai Tang <jingkaitang@gmail.com>
2015-10-25 19:22:46 +0800

    mod: leave seem_early_day_beneath_problem/next_woman.py or long_eye/know_thing_over_person/little_child_and_same_man/group.py by different government

---
+++ long_eye/know_thing_over_person/little_child_and_same_man/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_part(str_arg):
+    different_work(str_arg)
+    print('hand_and_great_company')
+
+def different_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_part('great_world')
---
+++ seem_early_day_beneath_problem/next_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_way(str_arg):
+    problem(str_arg)
+    print('call_next_woman')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_way('early_company')
