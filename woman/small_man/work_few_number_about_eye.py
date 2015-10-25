dbb5c198d7091796ccecc10abf746cb28c150945
Jingkai Tang <jingkaitang@gmail.com>
2015-10-25 23:04:35 +0800

    mod: tell woman/small_man/work_few_number_about_eye.py up own time

---
+++ woman/small_man/work_few_number_about_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    right_company(str_arg)
+    print('large_work')
+
+def right_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('feel_last_thing')
