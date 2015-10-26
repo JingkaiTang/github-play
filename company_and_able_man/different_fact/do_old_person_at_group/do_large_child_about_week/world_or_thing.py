27c2e0eea7591644932a0fed4d501958932eb959
Jingkai Tang <jingkaitang@gmail.com>
2015-10-27 07:39:34 +0800

    rm: know man.py and company_and_able_man/different_fact/do_old_person_at_group/do_large_child_about_week/world_or_thing.py under different world

---
+++ company_and_able_man/different_fact/do_old_person_at_group/do_large_child_about_week/world_or_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_government(str_arg):
+    give_thing(str_arg)
+    print('get_other_group_on_right_eye')
+
+def give_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_government('world')
--- man.py
+++ man.py
@@ -1,12 +1,12 @@
 
 #! /usr/bin/env python
 
-def year(str_arg):
-    early_problem(str_arg)
-    print('place')
+def big_work(str_arg):
+    leave_able_day(str_arg)
+    print('different_company_and_high_person')
 
-def early_problem(str_arg):
+def leave_able_day(str_arg):
     print(str_arg)
 
 if __name__ == '__main__':
-    year('public_fact_or_next_person')
+    big_work('great_number')
