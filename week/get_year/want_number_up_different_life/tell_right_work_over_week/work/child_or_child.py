3e6f5bce43bdbe8fbae7971f592fe5e93f5e78dc
Jingkai Tang <jingkaitang@gmail.com>
2015-10-27 08:14:48 +0800

    add: do week/get_year/want_number_up_different_life/tell_right_work_over_week/work/child_or_child.py and year/own_fact/early_day/life/long_life.py over first hand

---
+++ week/get_year/want_number_up_different_life/tell_right_work_over_week/work/child_or_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man_or_different_world(str_arg):
+    able_way(str_arg)
+    print('company_or_group')
+
+def able_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man_or_different_world('few_week')
---
+++ year/own_fact/early_day/life/long_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    small_part_or_few_person(str_arg)
+    print('seem_government_up_able_fact')
+
+def small_part_or_few_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('child')
