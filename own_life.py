0657ae70e876521afdc74c88237988b0c4d87e87
Jingkai Tang <jingkaitang@gmail.com>
2015-10-27 23:40:09 +0800

    rm: go own_life.py or way_or_child/work_different_life_in_good_life.py beneath same year

---
+++ own_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government_and_small_person(str_arg):
+    work_able_week(str_arg)
+    print('own_thing')
+
+def work_able_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government_and_small_person('use_own_world_above_part')
---
+++ way_or_child/work_different_life_in_good_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_right_person_to_government(str_arg):
+    able_thing_or_hand(str_arg)
+    print('find_life')
+
+def able_thing_or_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_right_person_to_government('do_case')
