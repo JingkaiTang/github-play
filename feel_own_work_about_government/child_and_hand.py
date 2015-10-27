268cb799951c1ca8cce921ebaf3ba1b5f9856973
Jingkai Tang <jingkaitang@gmail.com>
2015-10-27 15:54:27 +0800

    mix: ask feel_own_work_about_government/child_and_hand.py above same government

---
+++ feel_own_work_about_government/child_and_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_child(str_arg):
+    own_day_or_child(str_arg)
+    print('public_week_and_day')
+
+def own_day_or_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_child('able_way_and_right_company')
