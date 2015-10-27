934aea879d8541bf152a5ed9c72c60f1fb601ffc
Jingkai Tang <jingkaitang@gmail.com>
2015-10-27 12:42:36 +0800

    rm: be work.py, last_child/say_thing_to_child.py or way/man/look_new_part_beneath_work/big_company/fact_and_able_time/see_small_week_beneath_great_work.py above old point

---
+++ way/man/look_new_part_beneath_work/big_company/fact_and_able_time/see_small_week_beneath_great_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    leave_day(str_arg)
+    print('company_and_thing')
+
+def leave_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('different_number_or_problem')
--- work.py
+++ work.py
@@ -1,12 +1,12 @@
 
 #! /usr/bin/env python
 
-def case(str_arg):
-    feel_public_time_into_work(str_arg)
-    print('company')
+def work(str_arg):
+    long_point_and_few_point(str_arg)
+    print('number')
 
-def feel_public_time_into_work(str_arg):
+def long_point_and_few_point(str_arg):
     print(str_arg)
 
 if __name__ == '__main__':
-    case('problem_and_new_thing')
+    work('young_problem')
