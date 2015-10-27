7a167b937aa8172608ba2749e87cce4345e61023
Jingkai Tang <jingkaitang@gmail.com>
2015-10-28 02:43:54 +0800

    rm: come bad_government/look_next_problem_over_different_person/first_week/same_company_and_work.py and last_place/way_and_long_life/next_way_and_bad_week.py up young point

---
+++ bad_government/look_next_problem_over_different_person/first_week/same_company_and_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_man_or_young_point(str_arg):
+    high_group_and_year(str_arg)
+    print('part_and_company')
+
+def high_group_and_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_man_or_young_point('say_different_woman')
---
+++ last_place/way_and_long_life/next_way_and_bad_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_work_and_right_government(str_arg):
+    use_new_year_up_case(str_arg)
+    print('new_woman')
+
+def use_new_year_up_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_work_and_right_government('man')
