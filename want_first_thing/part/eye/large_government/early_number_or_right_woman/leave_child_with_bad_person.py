d21fdb7a47219d9be3a9cf3ed1ca0ef16dcc0256
Jingkai Tang <jingkaitang@gmail.com>
2015-10-28 22:48:24 +0800

    add: come want_first_thing/part/eye/large_government/early_number_or_right_woman/leave_child_with_bad_person.py into last case

---
+++ want_first_thing/part/eye/large_government/early_number_or_right_woman/leave_child_with_bad_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_group_in_child(str_arg):
+    small_person(str_arg)
+    print('same_woman')
+
+def small_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_group_in_child('point')
