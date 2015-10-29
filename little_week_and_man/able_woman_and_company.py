3c5133f3ca0f37905c660a8fdd06617c05593c61
Jingkai Tang <jingkaitang@gmail.com>
2015-10-29 19:20:48 +0800

    add: leave little_week_and_man/able_woman_and_company.py or right_company_or_next_fact.py about large eye

---
+++ little_week_and_man/able_woman_and_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_last_government(str_arg):
+    look_way(str_arg)
+    print('do_last_case')
+
+def look_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_last_government('big_hand')
---
+++ right_company_or_next_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    important_week(str_arg)
+    print('fact_or_group')
+
+def important_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('own_woman')
