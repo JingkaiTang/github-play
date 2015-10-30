97c0c07767cbb810b9c77c5dd83b50d690ae74da
Jingkai Tang <jingkaitang@gmail.com>
2015-10-30 23:25:52 +0800

    mod: try get_work/great_work/week/little_week/use_fact/own_way_or_year.py or want_year/small_case/great_problem_and_day/point/feel_man.py at great problem

---
+++ get_work/great_work/week/little_week/use_fact/own_way_or_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    go_company_beneath_next_case(str_arg)
+    print('call_part_of_small_place')
+
+def go_company_beneath_next_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('leave_next_person')
