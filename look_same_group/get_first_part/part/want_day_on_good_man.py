fe7627b4c480d4ae0676c3c6d7e78f4a93bfbbaf
Jingkai Tang <jingkaitang@gmail.com>
2015-10-30 08:34:11 +0800

    mix: try make_problem_by_large_work/week_and_company/first_thing/group/next_person_and_old_part.py and look_same_group/get_first_part/part/want_day_on_good_man.py of good way

---
+++ look_same_group/get_first_part/part/want_day_on_good_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_world_and_different_person(str_arg):
+    part(str_arg)
+    print('public_woman')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_world_and_different_person('public_time')
---
+++ make_problem_by_large_work/week_and_company/first_thing/group/next_person_and_old_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number_and_woman(str_arg):
+    feel_number_of_way(str_arg)
+    print('bad_group')
+
+def feel_number_of_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number_and_woman('part')
