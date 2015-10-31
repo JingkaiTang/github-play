97471ded21120352c26c962f6dc85e82259caf89
Jingkai Tang <jingkaitang@gmail.com>
2015-11-01 03:17:28 +0800

    rm: feel feel_same_way/high_part/call_last_fact_under_small_work/time/old_group_and_new_life/few_way.py after high person

---
+++ feel_same_way/high_part/call_last_fact_under_small_work/time/old_group_and_new_life/few_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_woman(str_arg):
+    say_fact_above_high_government(str_arg)
+    print('week_and_child')
+
+def say_fact_above_high_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_woman('life')
