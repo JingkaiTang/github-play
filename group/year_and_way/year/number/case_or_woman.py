281de62d1b85b59d1627b6fc62f6cbc592a60764
Jingkai Tang <jingkaitang@gmail.com>
2015-11-01 18:29:04 +0800

    mod: have group/year_and_way/year/number/case_or_woman.py from early day

---
+++ group/year_and_way/year/number/case_or_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_large_thing_above_eye(str_arg):
+    fact(str_arg)
+    print('do_number_of_problem')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_large_thing_above_eye('way_or_hand')
