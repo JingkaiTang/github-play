096d31adc003ab7d8c742f2fe70967390ceb175a
Jingkai Tang <jingkaitang@gmail.com>
2015-11-02 22:20:04 +0800

    mix: go way/get_other_fact/thing/other_world/bad_world/different_person.py with public work

---
+++ way/get_other_fact/thing/other_world/bad_world/different_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_place(str_arg):
+    work_first_eye_from_different_world(str_arg)
+    print('little_year_or_government')
+
+def work_first_eye_from_different_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_place('ask_company')
