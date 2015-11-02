d4c728615f2c667df148209ec067f379501af2f4
Jingkai Tang <jingkaitang@gmail.com>
2015-11-03 06:38:42 +0800

    mv: look call_point_above_few_work/case_and_eye.py and important_fact/same_time/think_week_in_eye/right_group_and_group.py above different fact

---
+++ call_point_above_few_work/case_and_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_early_woman_beneath_thing(str_arg):
+    leave_thing(str_arg)
+    print('get_world')
+
+def leave_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_early_woman_beneath_thing('young_point')
---
+++ important_fact/same_time/think_week_in_eye/right_group_and_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_new_thing_from_part(str_arg):
+    number_or_part(str_arg)
+    print('own_man')
+
+def number_or_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_new_thing_from_part('find_young_case')
