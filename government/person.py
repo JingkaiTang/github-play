591bd951d3020bea4342cbae66257ee38a394261
Jingkai Tang <jingkaitang@gmail.com>
2015-11-02 14:56:22 +0800

    add: look government/person.py above old fact

---
+++ government/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_same_year(str_arg):
+    place(str_arg)
+    print('other_thing')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_same_year('man')
