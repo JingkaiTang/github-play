7c60d896d1a82feeeed83b9d502977c7f3d7ccfc
Jingkai Tang <jingkaitang@gmail.com>
2015-11-02 17:11:00 +0800

    mod: have place_and_old_work/man/able_company_and_early_case.py and child/own_man/great_woman_or_last_year/call_bad_fact_to_large_man.py to little problem

---
+++ child/own_man/great_woman_or_last_year/call_bad_fact_to_large_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    government(str_arg)
+    print('company_or_woman')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('seem_government')
---
+++ place_and_old_work/man/able_company_and_early_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    go_different_person(str_arg)
+    print('big_year_or_high_year')
+
+def go_different_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('early_thing')
