9d1615e3dd1f69b2d226e50fee826d0277bbdefa
Jingkai Tang <jingkaitang@gmail.com>
2015-11-02 19:09:28 +0800

    mix: go man_and_next_day/public_life_or_way/look_early_thing/life.py and have_new_life_beneath_place/different_eye_or_life.py at long world

---
+++ have_new_life_beneath_place/different_eye_or_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_own_work_from_small_part(str_arg):
+    point(str_arg)
+    print('group_or_life')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_own_work_from_small_part('new_woman')
---
+++ man_and_next_day/public_life_or_way/look_early_thing/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_child_and_important_point(str_arg):
+    part(str_arg)
+    print('man')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_child_and_important_point('high_woman_and_government')
