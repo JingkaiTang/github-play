3d00711c9f32758f579ffae9cf7de964ed5f1f48
Jingkai Tang <jingkaitang@gmail.com>
2015-11-02 19:48:05 +0800

    add: want woman/right_case_and_important_company.py into public way

---
+++ woman/right_case_and_important_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_or_eye(str_arg):
+    part_and_thing(str_arg)
+    print('problem')
+
+def part_and_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_or_eye('day_or_fact')
