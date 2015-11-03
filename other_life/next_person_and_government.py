ea7d14860e8239fb08c897d0427822ebfc722f0f
Jingkai Tang <jingkaitang@gmail.com>
2015-11-03 08:02:19 +0800

    add: look other_life/next_person_and_government.py over new hand

---
+++ other_life/next_person_and_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    right_part(str_arg)
+    print('small_thing')
+
+def right_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('high_life')
