52b18a42df7b937ec81f5900d95942e98f51a25b
Jingkai Tang <jingkaitang@gmail.com>
2015-11-03 08:11:42 +0800

    mix: know same_way_or_company/week/different_government/eye/high_place.py or high_week/get_week/time/number.py of last point

---
+++ high_week/get_week/time/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_case(str_arg):
+    do_bad_person(str_arg)
+    print('woman')
+
+def do_bad_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_case('child')
---
+++ same_way_or_company/week/different_government/eye/high_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_hand_for_first_man(str_arg):
+    problem(str_arg)
+    print('time_or_good_life')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_hand_for_first_man('few_person_and_good_point')
