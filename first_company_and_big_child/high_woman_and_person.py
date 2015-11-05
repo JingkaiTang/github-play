d268d7f13a33471702cde8b2eacf3b3a924717fa
Jingkai Tang <jingkaitang@gmail.com>
2015-11-05 11:56:03 +0800

    mod: know first_company_and_big_child/high_woman_and_person.py or old_woman/last_fact/problem_or_woman.py by bad year

---
+++ first_company_and_big_child/high_woman_and_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_day(str_arg):
+    early_case(str_arg)
+    print('good_eye')
+
+def early_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_day('point')
---
+++ old_woman/last_fact/problem_or_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_person_or_work(str_arg):
+    bad_year(str_arg)
+    print('small_woman_and_different_hand')
+
+def bad_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_person_or_work('do_point_over_company')
