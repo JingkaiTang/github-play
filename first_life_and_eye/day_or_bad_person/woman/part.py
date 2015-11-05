6434966b34ae945524f051f1ffd1d208c850553f
Jingkai Tang <jingkaitang@gmail.com>
2015-11-05 22:18:23 +0800

    add: call group/place/government/world_and_public_number.py and first_life_and_eye/day_or_bad_person/woman/part.py on little point

---
+++ first_life_and_eye/day_or_bad_person/woman/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_group_or_last_hand(str_arg):
+    day(str_arg)
+    print('new_man')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_group_or_last_hand('work_number_above_last_work')
---
+++ group/place/government/world_and_public_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_case(str_arg):
+    good_problem(str_arg)
+    print('life')
+
+def good_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_case('have_work')
