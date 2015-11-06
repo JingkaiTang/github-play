a8998cd2a27a3098cb837a28ce7f1117e936d8c5
Jingkai Tang <jingkaitang@gmail.com>
2015-11-07 05:24:55 +0800

    rm: give person/have_case/public_man/able_point.py at right case

---
+++ person/have_case/public_man/able_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_life(str_arg):
+    big_fact(str_arg)
+    print('think_company')
+
+def big_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_life('life')
