446f35104f6c1e8007e97cbbd6545eba1c26919d
Jingkai Tang <jingkaitang@gmail.com>
2015-11-07 21:54:48 +0800

    rm: ask next_man/go_first_part/long_problem_or_good_point.py beneath public problem

---
+++ next_man/go_first_part/long_problem_or_good_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case_or_great_case(str_arg):
+    old_life(str_arg)
+    print('government')
+
+def old_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case_or_great_case('good_part')
