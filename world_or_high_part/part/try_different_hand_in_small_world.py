de0403c121dab4659fa7aa0c833260bd9697935e
Jingkai Tang <jingkaitang@gmail.com>
2015-11-07 11:57:02 +0800

    rm: call world_or_high_part/part/try_different_hand_in_small_world.py up bad place

---
+++ world_or_high_part/part/try_different_hand_in_small_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_day(str_arg):
+    have_bad_problem(str_arg)
+    print('right_man')
+
+def have_bad_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_day('hand_or_high_place')
