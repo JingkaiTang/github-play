e85992f560a1c0d8260a24671eda098a925d47a7
Jingkai Tang <jingkaitang@gmail.com>
2015-11-09 07:45:20 +0800

    rm: find old_work_or_group/case_or_right_government/want_big_fact/great_company_and_important_fact/know_thing/day.py into few point

---
+++ old_work_or_group/case_or_right_government/want_big_fact/great_company_and_important_fact/know_thing/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_day(str_arg):
+    young_number(str_arg)
+    print('big_child')
+
+def young_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_day('look_way_up_next_week')
