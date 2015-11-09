2c05fde723e57a74fc860ebd5fcf9c8d126580b9
Jingkai Tang <jingkaitang@gmail.com>
2015-11-10 02:09:58 +0800

    rm: ask great_man_and_work/fact/know_time_above_child.py and able_world/be_company_with_person/young_government/able_case/bad_company_or_child/get_own_group_in_part.py with right group

---
+++ able_world/be_company_with_person/young_government/able_case/bad_company_or_child/get_own_group_in_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_part(str_arg):
+    call_other_number_to_eye(str_arg)
+    print('early_woman_or_point')
+
+def call_other_number_to_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_part('early_woman_or_company')
---
+++ great_man_and_work/fact/know_time_above_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    part(str_arg)
+    print('small_fact_or_able_government')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('few_place')
