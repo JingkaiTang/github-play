6ff747cafa5a9dfa8e628f7527edcb6b2d5974b7
Jingkai Tang <jingkaitang@gmail.com>
2015-11-09 11:46:12 +0800

    rm: take few_week/large_place/large_world/child/try_public_year_over_part.py under small world

---
+++ few_week/large_place/large_world/child/try_public_year_over_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_woman_by_different_case(str_arg):
+    big_company(str_arg)
+    print('same_government')
+
+def big_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_woman_by_different_case('world')
