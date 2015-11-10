a5fb3a50fa26a336399da1c646f3fcfcef25969e
Jingkai Tang <jingkaitang@gmail.com>
2015-11-11 06:49:41 +0800

    mv: do problem/new_point/have_high_woman/year/different_life.py beneath long way

---
+++ problem/new_point/have_high_woman/year/different_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    ask_long_hand_after_place(str_arg)
+    print('little_hand_or_important_man')
+
+def ask_long_hand_after_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('different_case')
