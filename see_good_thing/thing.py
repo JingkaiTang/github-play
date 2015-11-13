9f4d4dfa57fa9cfd7720f057241e4653998cd09c
Jingkai Tang <jingkaitang@gmail.com>
2015-11-14 04:36:32 +0800

    add: want see_good_thing/thing.py, little_hand/take_different_thing.py and time_or_week/little_day.py over new person

---
+++ see_good_thing/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_different_way_into_point(str_arg):
+    last_year(str_arg)
+    print('be_high_person')
+
+def last_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_different_way_into_point('year')
---
+++ time_or_week/little_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_life_and_problem(str_arg):
+    hand_or_eye(str_arg)
+    print('right_way')
+
+def hand_or_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_life_and_problem('problem')
