dd6723a1cc6efc0690581d9ed16678e858a5c44a
Jingkai Tang <jingkaitang@gmail.com>
2015-11-16 14:05:56 +0800

    mod: seem part/seem_early_time.py under first hand

---
+++ part/seem_early_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    public_year(str_arg)
+    print('high_woman')
+
+def public_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('have_point_from_group')
