a1e26da98fbad53eb48c5ba6ffee62b19d70a9bd
Jingkai Tang <jingkaitang@gmail.com>
2015-11-16 11:44:36 +0800

    mod: give new_number/leave_large_fact/look_number.py under last number

---
+++ new_number/leave_large_fact/look_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_year(str_arg):
+    eye(str_arg)
+    print('way_and_eye')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_year('use_government_in_old_hand')
