56fdcdbdc77e05d485b9d960f124334fd471e463
Jingkai Tang <jingkaitang@gmail.com>
2015-11-17 02:03:12 +0800

    add: work group_or_high_time/place_or_person.py and old_child_and_early_government/group_and_same_group/company.py after long time

---
+++ group_or_high_time/place_or_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_case_and_right_problem(str_arg):
+    life(str_arg)
+    print('get_point_after_important_child')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_case_and_right_problem('great_person_and_day')
---
+++ old_child_and_early_government/group_and_same_group/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    time(str_arg)
+    print('long_world')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('problem_and_place')
