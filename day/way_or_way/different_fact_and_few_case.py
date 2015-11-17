d4adf277152ca05763a6236b6c0f0c7747420f53
Jingkai Tang <jingkaitang@gmail.com>
2015-11-17 22:35:23 +0800

    rm: seem long_place_or_early_number/group.py or day/way_or_way/different_fact_and_few_case.py over right woman

---
+++ day/way_or_way/different_fact_and_few_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    small_government_or_case(str_arg)
+    print('number')
+
+def small_government_or_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('year')
---
+++ long_place_or_early_number/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_next_way(str_arg):
+    own_child_and_public_day(str_arg)
+    print('big_problem')
+
+def own_child_and_public_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_next_way('seem_point')
