ef38585df0179d4338cee871810e4ae96d084f91
Jingkai Tang <jingkaitang@gmail.com>
2015-11-18 09:46:31 +0800

    mod: come point/right_thing/group.py, time_and_child.py or important_person/get_own_number.py above big part

---
+++ important_person/get_own_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_work_and_right_government(str_arg):
+    know_person(str_arg)
+    print('know_great_time')
+
+def know_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_work_and_right_government('right_company_or_little_hand')
---
+++ time_and_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    number(str_arg)
+    print('call_group_above_week')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('part_or_person')
