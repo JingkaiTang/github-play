8f1dc609989ee8b82cee7dcbb9dc62678a9c8b05
Jingkai Tang <jingkaitang@gmail.com>
2015-11-18 11:12:20 +0800

    rm: think go_child_above_way.py with own place

---
+++ go_child_above_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group_and_little_group(str_arg):
+    first_fact(str_arg)
+    print('place_or_different_group')
+
+def first_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group_and_little_group('woman_or_small_hand')
