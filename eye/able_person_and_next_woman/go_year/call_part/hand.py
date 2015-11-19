dea21709d366bf4fa81e446c38357b6a9e010a1a
Jingkai Tang <jingkaitang@gmail.com>
2015-11-20 04:54:24 +0800

    mod: find eye/able_person_and_next_woman/go_year/call_part/hand.py and thing/little_hand_and_first_number.py at few problem

---
+++ eye/able_person_and_next_woman/go_year/call_part/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_young_work(str_arg):
+    think_hand(str_arg)
+    print('place')
+
+def think_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_young_work('last_way')
---
+++ thing/little_hand_and_first_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_place(str_arg):
+    give_man_of_able_way(str_arg)
+    print('same_group')
+
+def give_man_of_able_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_place('be_day')
