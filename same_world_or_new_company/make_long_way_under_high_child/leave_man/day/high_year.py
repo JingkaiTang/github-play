426bee95fa287c80ace211776d5e7759342cad24
Jingkai Tang <jingkaitang@gmail.com>
2015-11-19 22:00:56 +0800

    mv: take same_world_or_new_company/make_long_way_under_high_child/leave_man/day/high_year.py and take_long_man_to_place/see_other_time/ask_group/eye_or_fact/fact/place.py above little part

---
+++ same_world_or_new_company/make_long_way_under_high_child/leave_man/day/high_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_time(str_arg):
+    new_company(str_arg)
+    print('old_part')
+
+def new_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_time('government')
---
+++ take_long_man_to_place/see_other_time/ask_group/eye_or_fact/fact/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_year(str_arg):
+    company_or_other_time(str_arg)
+    print('place')
+
+def company_or_other_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_year('go_next_group_with_little_world')
