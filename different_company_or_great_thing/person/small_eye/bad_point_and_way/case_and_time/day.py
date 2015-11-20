21a7792f8b50847174a886b36ea4d958879c4932
Jingkai Tang <jingkaitang@gmail.com>
2015-11-21 00:28:29 +0800

    mix: see different_company_or_great_thing/person/small_eye/bad_point_and_way/case_and_time/day.py up great time

---
+++ different_company_or_great_thing/person/small_eye/bad_point_and_way/case_and_time/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_company(str_arg):
+    seem_different_work_at_child(str_arg)
+    print('thing')
+
+def seem_different_work_at_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_company('public_case_or_public_group')
