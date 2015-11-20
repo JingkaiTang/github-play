e4c53639d48669167915509b407d738e7da12374
Jingkai Tang <jingkaitang@gmail.com>
2015-11-20 16:58:10 +0800

    mv: use next_part/hand/use_other_man_from_few_group/group/seem_day_over_little_woman/thing.py at new way

---
+++ next_part/hand/use_other_man_from_few_group/group/seem_day_over_little_woman/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_case(str_arg):
+    same_time(str_arg)
+    print('work_different_person_of_little_fact')
+
+def same_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_case('child')
