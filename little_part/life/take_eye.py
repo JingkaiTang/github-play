1a7f770e3050a9724d9f943eb90ddb8673099494
Jingkai Tang <jingkaitang@gmail.com>
2015-11-21 08:23:49 +0800

    mv: seem little_part/life/take_eye.py above bad problem

---
+++ little_part/life/take_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    woman_or_large_woman(str_arg)
+    print('do_world_beneath_good_person')
+
+def woman_or_large_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('case')
