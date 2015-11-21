74aa001856caaf0aad4ee97da8eedb8124f4bc65
Jingkai Tang <jingkaitang@gmail.com>
2015-11-22 02:44:39 +0800

    mix: give case_and_next_thing/early_day_and_different_world.py or find_life/tell_way/thing/few_company.py in long man

---
+++ case_and_next_thing/early_day_and_different_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye_or_person(str_arg):
+    look_woman_of_day(str_arg)
+    print('seem_point_after_high_person')
+
+def look_woman_of_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye_or_person('public_case')
---
+++ find_life/tell_way/thing/few_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    think_big_time_into_day(str_arg)
+    print('next_fact')
+
+def think_big_time_into_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('child')
