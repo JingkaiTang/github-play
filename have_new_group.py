f465759ddbc8a961ab3f07b0d54c1fe6c8cdeffa
Jingkai Tang <jingkaitang@gmail.com>
2015-11-23 00:51:28 +0800

    add: try want_few_thing/different_woman/child_or_child/point/able_year_and_next_number.py and have_new_group.py in good life

---
+++ have_new_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government_and_great_point(str_arg):
+    hand(str_arg)
+    print('look_important_day_into_point')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government_and_great_point('different_way')
---
+++ want_few_thing/different_woman/child_or_child/point/able_year_and_next_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_long_work_beneath_fact(str_arg):
+    old_problem(str_arg)
+    print('little_world_and_own_company')
+
+def old_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_long_work_beneath_fact('life_or_first_company')
