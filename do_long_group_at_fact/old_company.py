d054f5b6688d142f31dd0730fc1507bfd9732425
Jingkai Tang <jingkaitang@gmail.com>
2015-11-22 12:11:48 +0800

    rm: feel go_other_man/feel_hand/know_high_year_about_case/do_small_child_into_week/fact.py or do_long_group_at_fact/old_company.py above own government

---
+++ do_long_group_at_fact/old_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    go_child_for_old_year(str_arg)
+    print('feel_thing')
+
+def go_child_for_old_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('know_bad_point_in_large_week')
---
+++ go_other_man/feel_hand/know_high_year_about_case/do_small_child_into_week/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part_and_thing(str_arg):
+    bad_number(str_arg)
+    print('person')
+
+def bad_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part_and_thing('high_place')
