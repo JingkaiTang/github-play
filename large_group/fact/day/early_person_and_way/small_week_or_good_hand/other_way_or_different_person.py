5e39e46a5a0c44f773045e37895b1b97819e57c8
Jingkai Tang <jingkaitang@gmail.com>
2015-11-23 03:11:58 +0800

    add: call large_group/fact/day/early_person_and_way/small_week_or_good_hand/other_way_or_different_person.py or next_point_and_long_life/early_day/feel_company.py over same place

---
+++ large_group/fact/day/early_person_and_way/small_week_or_good_hand/other_way_or_different_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_point(str_arg):
+    next_government_and_last_group(str_arg)
+    print('great_year_and_next_world')
+
+def next_government_and_last_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_point('get_government_above_same_time')
---
+++ next_point_and_long_life/early_day/feel_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_number_or_few_group(str_arg):
+    try_eye(str_arg)
+    print('public_company_or_world')
+
+def try_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_number_or_few_group('person')
