510328a94e7070b26d26c7550bf860c9523f8887
Jingkai Tang <jingkaitang@gmail.com>
2015-11-23 05:05:52 +0800

    mv: want seem_same_fact_about_time.py to small point

---
+++ seem_same_fact_about_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_group_and_eye(str_arg):
+    bad_child(str_arg)
+    print('place')
+
+def bad_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_group_and_eye('give_long_company')
