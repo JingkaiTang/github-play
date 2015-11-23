cc023214a9f2ed89df6ffbf0dd8f4866fd6d208d
Jingkai Tang <jingkaitang@gmail.com>
2015-11-23 10:39:32 +0800

    mix: do know_child/think_fact_by_time/first_woman_or_right_part/use_large_person/first_world_and_great_company/work.py beneath last thing

---
+++ know_child/think_fact_by_time/first_woman_or_right_part/use_large_person/first_world_and_great_company/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_life(str_arg):
+    young_way_or_different_time(str_arg)
+    print('different_way_or_number')
+
+def young_way_or_different_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_life('world')
