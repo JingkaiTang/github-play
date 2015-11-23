2de4cd3cf1823c17fac82753689b2cf2cf383361
Jingkai Tang <jingkaitang@gmail.com>
2015-11-23 12:12:33 +0800

    mix: think important_place_or_important_work/come_thing_over_able_group/way.py in important woman

---
+++ important_place_or_important_work/come_thing_over_able_group/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_world(str_arg):
+    try_other_week(str_arg)
+    print('government')
+
+def try_other_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_world('early_fact_or_government')
