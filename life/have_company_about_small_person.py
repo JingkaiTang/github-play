2d7737fa9c11ab4d826ecd3d84e0058a89f7711c
Jingkai Tang <jingkaitang@gmail.com>
2015-11-23 17:35:07 +0800

    add: look life/have_company_about_small_person.py with same week

---
+++ life/have_company_about_small_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_or_group(str_arg):
+    great_thing_or_case(str_arg)
+    print('call_large_world')
+
+def great_thing_or_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_or_group('small_work')
