ef4b73335126d1f6bfb4377e9a2ec042a4c54149
Jingkai Tang <jingkaitang@gmail.com>
2015-11-26 02:53:05 +0800

    mv: tell give_bad_problem/get_good_government/part/find_child/eye.py up own life

---
+++ give_bad_problem/get_good_government/part/find_child/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_woman_beneath_woman(str_arg):
+    use_next_group_in_able_life(str_arg)
+    print('new_life_and_case')
+
+def use_next_group_in_able_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_woman_beneath_woman('young_case_and_few_company')
