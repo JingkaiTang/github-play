9979d9684de66f1e2558f3f354bc54fa102c1726
Jingkai Tang <jingkaitang@gmail.com>
2015-11-25 09:41:27 +0800

    mix: say year_and_government/small_problem/early_day/little_problem/good_life.py above other week

---
+++ year_and_government/small_problem/early_day/little_problem/good_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_important_time_after_other_government(str_arg):
+    few_child(str_arg)
+    print('ask_time_up_few_child')
+
+def few_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_important_time_after_other_government('work_year_above_first_part')
