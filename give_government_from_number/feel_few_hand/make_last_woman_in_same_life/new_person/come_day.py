67833d92c4e3aa4dcc3862b1e87d5c9f8a24c354
Jingkai Tang <jingkaitang@gmail.com>
2015-11-27 07:01:14 +0800

    mix: want give_government_from_number/feel_few_hand/make_last_woman_in_same_life/new_person/come_day.py or week/week_or_group/different_place/part/work.py with little day

---
+++ give_government_from_number/feel_few_hand/make_last_woman_in_same_life/new_person/come_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    seem_world(str_arg)
+    print('eye')
+
+def seem_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('same_time')
---
+++ week/week_or_group/different_place/part/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_problem(str_arg):
+    long_fact_or_part(str_arg)
+    print('have_child_with_different_world')
+
+def long_fact_or_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_problem('leave_place_at_public_child')
