1ec62163a50e8714532428f7b69af0179f7a1769
Jingkai Tang <jingkaitang@gmail.com>
2015-11-27 00:30:22 +0800

    mv: take go_hand_for_child/other_man/right_thing/week/little_thing.py and call_child/number/work_week.py into able point

---
+++ call_child/number/work_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_able_time_from_different_world(str_arg):
+    small_group_or_year(str_arg)
+    print('get_company')
+
+def small_group_or_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_able_time_from_different_world('man')
---
+++ go_hand_for_child/other_man/right_thing/week/little_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_hand_or_large_thing(str_arg):
+    great_world(str_arg)
+    print('great_day')
+
+def great_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_hand_or_large_thing('able_hand_or_place')
