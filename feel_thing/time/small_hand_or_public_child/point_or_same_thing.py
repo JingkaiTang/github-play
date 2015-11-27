097b785e4dd397955304367bb2817c524a1f664f
Jingkai Tang <jingkaitang@gmail.com>
2015-11-28 04:18:33 +0800

    mv: work feel_thing/time/small_hand_or_public_child/point_or_same_thing.py from small year

---
+++ feel_thing/time/small_hand_or_public_child/point_or_same_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_problem_and_government(str_arg):
+    thing(str_arg)
+    print('do_eye_with_fact')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_problem_and_government('high_number')
