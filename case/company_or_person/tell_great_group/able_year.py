b076c740d6107f45bb9286a8acf23d471699e4ae
Jingkai Tang <jingkaitang@gmail.com>
2015-11-30 07:06:35 +0800

    mod: ask case/company_or_person/tell_great_group/able_year.py after own woman

---
+++ case/company_or_person/tell_great_group/able_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_government(str_arg):
+    government(str_arg)
+    print('same_world')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_government('important_woman')
