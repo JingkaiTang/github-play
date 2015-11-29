ac035bec34d99551618fefd96f988fbaf7514dfd
Jingkai Tang <jingkaitang@gmail.com>
2015-11-29 14:52:32 +0800

    add: feel other_thing/think_person_after_first_man/try_group.py or long_company/way/way/long_year/last_week.py to next eye

---
+++ long_company/way/way/long_year/last_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_time_and_public_company(str_arg):
+    long_way_or_fact(str_arg)
+    print('case_and_life')
+
+def long_way_or_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_time_and_public_company('first_work_and_day')
---
+++ other_thing/think_person_after_first_man/try_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child_and_last_fact(str_arg):
+    life(str_arg)
+    print('day')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child_and_last_fact('young_time')
