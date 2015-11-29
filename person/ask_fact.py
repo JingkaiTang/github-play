17c9eb91292a91143d70ce8495562dc8ef4f4a9f
Jingkai Tang <jingkaitang@gmail.com>
2015-11-29 17:04:25 +0800

    mv: find person/ask_fact.py beneath important part

---
+++ person/ask_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    woman(str_arg)
+    print('say_work_with_day')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('find_company')
