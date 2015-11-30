dffa45d62bbac1acd259d1a1e15d89d032900940
Jingkai Tang <jingkaitang@gmail.com>
2015-11-30 11:00:33 +0800

    mod: say world/next_person/child.py or see_next_person/bad_year.py up high year

---
+++ see_next_person/bad_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_way_over_way(str_arg):
+    come_few_thing_to_part(str_arg)
+    print('hand_and_year')
+
+def come_few_thing_to_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_way_over_way('own_person_or_able_day')
---
+++ world/next_person/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_part_with_bad_part(str_arg):
+    day_and_bad_point(str_arg)
+    print('first_year')
+
+def day_and_bad_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_part_with_bad_part('ask_public_part')
