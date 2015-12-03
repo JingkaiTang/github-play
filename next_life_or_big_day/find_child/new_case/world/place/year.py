17a0083a4fbe307ca96e4f9bcf4e9b86fb7f81d2
Jingkai Tang <jingkaitang@gmail.com>
2015-12-04 00:01:58 +0800

    mod: have next_life_or_big_day/find_child/new_case/world/place/year.py or point/case_and_child/group/say_thing_after_next_government/able_government/man_or_good_part.py under few life

---
+++ next_life_or_big_day/find_child/new_case/world/place/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    go_little_life_over_person(str_arg)
+    print('be_eye_from_woman')
+
+def go_little_life_over_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('come_large_group')
---
+++ point/case_and_child/group/say_thing_after_next_government/able_government/man_or_good_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_high_place_after_world(str_arg):
+    own_man(str_arg)
+    print('number_or_last_work')
+
+def own_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_high_place_after_world('important_woman_and_place')
