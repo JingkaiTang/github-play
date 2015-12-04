f478f28ed675833df326a3c512c64f2b85d4d52c
Jingkai Tang <jingkaitang@gmail.com>
2015-12-04 17:05:13 +0800

    add: say early_part_and_little_thing/government/place/government.py into next problem

---
+++ early_part_and_little_thing/government/place/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world_or_last_child(str_arg):
+    say_day(str_arg)
+    print('case')
+
+def say_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world_or_last_child('little_problem_or_able_way')
