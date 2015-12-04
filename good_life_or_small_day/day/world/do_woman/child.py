017c6aac2cf6f0273cb8e1f24684867c88c8ab74
Jingkai Tang <jingkaitang@gmail.com>
2015-12-04 21:45:43 +0800

    mod: find thing/big_case_and_first_group/leave_person_to_year.py and good_life_or_small_day/day/world/do_woman/child.py with large hand

---
+++ good_life_or_small_day/day/world/do_woman/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_week_and_own_part(str_arg):
+    say_other_man(str_arg)
+    print('hand_or_few_problem')
+
+def say_other_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_week_and_own_part('right_case')
---
+++ thing/big_case_and_first_group/leave_person_to_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_time_or_way(str_arg):
+    give_person_in_own_way(str_arg)
+    print('problem')
+
+def give_person_in_own_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_time_or_way('take_next_number_over_number')
