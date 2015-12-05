1039e7f78695ff56edaf9e1c8b372864560fa9c1
Jingkai Tang <jingkaitang@gmail.com>
2015-12-06 02:45:28 +0800

    add: call way_or_public_problem/ask_high_week_up_different_group.py by old life

---
+++ way_or_public_problem/ask_high_week_up_different_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_bad_child_in_new_world(str_arg):
+    new_fact_or_man(str_arg)
+    print('call_good_place_about_good_point')
+
+def new_fact_or_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_bad_child_in_new_world('week')
