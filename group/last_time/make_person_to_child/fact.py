3a6be0a544186b95b35ecfc426c427ae6abc2fae
Jingkai Tang <jingkaitang@gmail.com>
2015-12-08 16:55:37 +0800

    add: try old_thing/leave_woman/group/child.py and group/last_time/make_person_to_child/fact.py for own government

---
+++ group/last_time/make_person_to_child/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_fact(str_arg):
+    work_and_time(str_arg)
+    print('person')
+
+def work_and_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_fact('work')
---
+++ old_thing/leave_woman/group/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    case(str_arg)
+    print('come_small_person')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('case')
