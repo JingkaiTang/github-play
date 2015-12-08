848d9264d2246b7d4a73608dc11ccc54394b3af6
Jingkai Tang <jingkaitang@gmail.com>
2015-12-08 19:13:20 +0800

    mix: give week/few_problem/long_point.py in big woman

---
+++ week/few_problem/long_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day_and_great_way(str_arg):
+    public_week_or_woman(str_arg)
+    print('world')
+
+def public_week_or_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day_and_great_way('leave_thing')
