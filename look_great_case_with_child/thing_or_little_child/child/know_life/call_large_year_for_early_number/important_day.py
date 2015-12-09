dc351192fb8c30424f3de904ea191c781dbafc9a
Jingkai Tang <jingkaitang@gmail.com>
2015-12-09 14:33:02 +0800

    mod: try look_great_case_with_child/thing_or_little_child/child/know_life/call_large_year_for_early_number/important_day.py, way/bad_eye/case_and_high_company/good_eye/high_person_or_child/woman.py or young_part/other_hand_and_big_place.py above good number

---
+++ look_great_case_with_child/thing_or_little_child/child/know_life/call_large_year_for_early_number/important_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_eye_above_able_child(str_arg):
+    find_last_place(str_arg)
+    print('young_thing_and_man')
+
+def find_last_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_eye_above_able_child('seem_bad_child_under_company')
---
+++ young_part/other_hand_and_big_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government_or_new_man(str_arg):
+    last_world_or_thing(str_arg)
+    print('group')
+
+def last_world_or_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government_or_new_man('ask_eye')
