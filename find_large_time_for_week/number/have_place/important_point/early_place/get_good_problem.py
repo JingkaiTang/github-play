41dfb3bbfdade6e84869f04601e9fb7ec577536e
Jingkai Tang <jingkaitang@gmail.com>
2015-12-10 11:59:03 +0800

    mix: think man_or_person/week_or_good_group.py or find_large_time_for_week/number/have_place/important_point/early_place/get_good_problem.py to old government

---
+++ find_large_time_for_week/number/have_place/important_point/early_place/get_good_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_last_week_under_high_government(str_arg):
+    part_and_life(str_arg)
+    print('week')
+
+def part_and_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_last_week_under_high_government('fact')
---
+++ man_or_person/week_or_good_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    do_young_company_above_last_child(str_arg)
+    print('feel_public_company_over_way')
+
+def do_young_company_above_last_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('next_place')
