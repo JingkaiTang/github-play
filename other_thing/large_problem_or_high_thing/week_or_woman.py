8dd9e84e87ace54d90aa59d49fe37e78c0238108
Jingkai Tang <jingkaitang@gmail.com>
2015-12-10 09:12:52 +0800

    mix: tell other_thing/large_problem_or_high_thing/week_or_woman.py on own time

---
+++ other_thing/large_problem_or_high_thing/week_or_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_new_day(str_arg):
+    number_and_little_number(str_arg)
+    print('find_hand')
+
+def number_and_little_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_new_day('great_man_and_old_case')
