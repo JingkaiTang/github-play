78775a7811725a72c28bff5cda34d70151a401c0
Jingkai Tang <jingkaitang@gmail.com>
2015-12-10 14:13:35 +0800

    add: work few_day_and_right_way/old_child_or_week.py over high part

---
+++ few_day_and_right_way/old_child_or_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand_or_group(str_arg):
+    place(str_arg)
+    print('work')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand_or_group('try_place')
