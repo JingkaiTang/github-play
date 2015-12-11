57a41cfa5668898a6a0101ea1a1e61973f966d81
Jingkai Tang <jingkaitang@gmail.com>
2015-12-12 03:20:33 +0800

    mod: think case/be_few_time/fact/fact_and_different_year.py or problem/good_problem/great_man.py beneath same man

---
+++ case/be_few_time/fact/fact_and_different_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_little_world(str_arg):
+    first_year(str_arg)
+    print('make_small_part')
+
+def first_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_little_world('part')
---
+++ problem/good_problem/great_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_way(str_arg):
+    small_day(str_arg)
+    print('good_man')
+
+def small_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_way('think_week_up_point')
