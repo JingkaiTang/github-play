e71e00d431ebf3bc23fd56e22ec380ab50ac50c1
Jingkai Tang <jingkaitang@gmail.com>
2015-12-13 05:28:03 +0800

    rm: leave year/big_hand/be_last_problem/same_part/company_or_good_place.py on high place

---
+++ year/big_hand/be_last_problem/same_part/company_or_good_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    say_person(str_arg)
+    print('day')
+
+def say_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('last_group')
