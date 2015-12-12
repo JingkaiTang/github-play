7fc08d456204cc7d98499625542ad6a40fdbe212
Jingkai Tang <jingkaitang@gmail.com>
2015-12-13 00:32:14 +0800

    add: make public_number_and_good_group/life.py at right group

---
+++ public_number_and_good_group/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    come_bad_woman(str_arg)
+    print('week')
+
+def come_bad_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('child')
