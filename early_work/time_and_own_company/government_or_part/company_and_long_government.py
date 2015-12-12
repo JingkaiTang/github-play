fddc1f8afdce711e29af448f8b985380481fd4c3
Jingkai Tang <jingkaitang@gmail.com>
2015-12-13 03:15:08 +0800

    mix: give early_work/time_and_own_company/government_or_part/company_and_long_government.py, point_or_last_woman/next_year/week.py and man_or_old_week/new_week/week_and_old_company.py in early part

---
+++ early_work/time_and_own_company/government_or_part/company_and_long_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_work(str_arg):
+    way_and_large_case(str_arg)
+    print('make_case')
+
+def way_and_large_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_work('problem_and_case')
---
+++ man_or_old_week/new_week/week_and_old_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_world(str_arg):
+    time_or_bad_part(str_arg)
+    print('important_time_or_few_point')
+
+def time_or_bad_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_world('get_person')
