f014a5e2ae9f9e06f2cacb397c83ec86a72bc3b3
Jingkai Tang <jingkaitang@gmail.com>
2015-12-13 01:54:13 +0800

    rm: take be_woman.py and young_person_and_case/feel_early_year_after_hand/way/say_great_man/eye_and_first_fact/last_life.py up next part

---
+++ be_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year_or_number(str_arg):
+    high_group_or_high_place(str_arg)
+    print('world_and_great_government')
+
+def high_group_or_high_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year_or_number('great_work_and_same_hand')
---
+++ young_person_and_case/feel_early_year_after_hand/way/say_great_man/eye_and_first_fact/last_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_problem(str_arg):
+    life(str_arg)
+    print('man')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_problem('few_case')
