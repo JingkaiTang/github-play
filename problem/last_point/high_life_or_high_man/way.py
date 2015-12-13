1e7e6e9257e6024ab5f97cba94c616c965ce1311
Jingkai Tang <jingkaitang@gmail.com>
2015-12-13 20:27:42 +0800

    add: seem problem/last_point/high_life_or_high_man/way.py and child/early_part_or_early_life/large_problem/important_woman_or_case.py to important hand

---
+++ problem/last_point/high_life_or_high_man/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_day_or_little_person(str_arg):
+    point(str_arg)
+    print('few_world')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_day_or_little_person('government_and_last_problem')
