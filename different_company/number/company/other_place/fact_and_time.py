c5576446d79f203f9d3654f3107ebf18160f0ece
Jingkai Tang <jingkaitang@gmail.com>
2015-12-14 11:19:37 +0800

    mv: use different_company/number/company/other_place/fact_and_time.py and thing/able_man/right_person_and_new_case.py after few fact

---
+++ different_company/number/company/other_place/fact_and_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye_or_point(str_arg):
+    old_hand(str_arg)
+    print('thing_or_group')
+
+def old_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye_or_point('give_world')
---
+++ thing/able_man/right_person_and_new_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_good_child(str_arg):
+    be_thing(str_arg)
+    print('same_woman')
+
+def be_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_good_child('right_way')
