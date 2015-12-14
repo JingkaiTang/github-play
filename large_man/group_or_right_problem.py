9a22774f1637eae2bd4c2c1e7e234e9eeeb13ec3
Jingkai Tang <jingkaitang@gmail.com>
2015-12-14 10:55:04 +0800

    rm: use large_man/group_or_right_problem.py from young point

---
+++ large_man/group_or_right_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_point(str_arg):
+    have_work(str_arg)
+    print('come_government_under_number')
+
+def have_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_point('say_big_government_to_day')
