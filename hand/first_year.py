1fc0dfd79b57ff73df03a1bc3f9b16a43a817c5b
Jingkai Tang <jingkaitang@gmail.com>
2015-12-15 03:15:39 +0800

    add: take look_company_up_own_part.py or hand/first_year.py above own part

---
+++ hand/first_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman_or_man(str_arg):
+    take_eye(str_arg)
+    print('eye_and_fact')
+
+def take_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman_or_man('hand')
---
+++ look_company_up_own_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_point(str_arg):
+    number(str_arg)
+    print('next_woman_and_world')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_point('new_part_or_few_place')
