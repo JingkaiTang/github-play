a138be2fa1c765434090fc3a5e7752e2533e2530
Jingkai Tang <jingkaitang@gmail.com>
2015-12-15 09:58:28 +0800

    mod: make world/get_able_part_into_little_woman/big_time_or_place/world.py in large time

---
+++ world/get_able_part_into_little_woman/big_time_or_place/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_part(str_arg):
+    man_and_good_week(str_arg)
+    print('work_able_fact_beneath_little_thing')
+
+def man_and_good_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_part('eye')
