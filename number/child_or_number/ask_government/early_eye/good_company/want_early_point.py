df357aa7b6539b2b42767e4fe7e8fe205c78d55e
Jingkai Tang <jingkaitang@gmail.com>
2015-12-15 18:47:04 +0800

    rm: do number/child_or_number/ask_government/early_eye/good_company/want_early_point.py of new place

---
+++ number/child_or_number/ask_government/early_eye/good_company/want_early_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    year(str_arg)
+    print('feel_high_day_about_life')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('young_number')
