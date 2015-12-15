0d4400b3f9d8458677ea6e07cb82aeeedc2d27cf
Jingkai Tang <jingkaitang@gmail.com>
2015-12-15 23:01:09 +0800

    rm: have find_last_woman_up_man.py and day/bad_woman_or_large_day/work_group.py to bad work

---
+++ day/bad_woman_or_large_day/work_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    young_number(str_arg)
+    print('next_problem')
+
+def young_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('leave_hand_in_small_problem')
---
+++ find_last_woman_up_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    right_work(str_arg)
+    print('bad_group')
+
+def right_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('small_company_and_good_case')
