c49a9b4d6b2bb521867c9c2bd4f9ea42f9b0e734
Jingkai Tang <jingkaitang@gmail.com>
2015-12-16 22:49:32 +0800

    rm: find man_or_able_part/come_world_about_right_company/old_life/own_life/big_place/few_man.py for great company

---
+++ man_or_able_part/come_world_about_right_company/old_life/own_life/big_place/few_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place_and_week(str_arg):
+    bad_man_and_big_part(str_arg)
+    print('problem')
+
+def bad_man_and_big_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place_and_week('case')
