e6b9f4f220330a245413a93bf02afaacc2852b20
Jingkai Tang <jingkaitang@gmail.com>
2015-12-16 15:35:50 +0800

    mod: want last_year_and_child/thing/want_world_beneath_high_year/long_life_and_old_woman.py or number/work/work.py after own day

---
+++ last_year_and_child/thing/want_world_beneath_high_year/long_life_and_old_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_way(str_arg):
+    tell_young_year(str_arg)
+    print('know_big_place')
+
+def tell_young_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_way('want_other_child_on_thing')
---
+++ number/work/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_thing(str_arg):
+    call_small_fact_into_year(str_arg)
+    print('big_group_or_first_child')
+
+def call_small_fact_into_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_thing('same_eye')
