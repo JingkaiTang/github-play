a0b422d9a26861c60553f44ac8fc421b9e4e4797
Jingkai Tang <jingkaitang@gmail.com>
2015-12-16 22:53:20 +0800

    mod: leave bad_hand.py and eye/feel_able_part_from_last_world/find_fact_to_place/make_case.py in large work

---
+++ bad_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_day(str_arg):
+    new_place_or_same_world(str_arg)
+    print('good_day')
+
+def new_place_or_same_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_day('woman')
