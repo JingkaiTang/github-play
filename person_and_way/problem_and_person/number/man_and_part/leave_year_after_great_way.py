5d0f788f20ed733ce6b80002e8ad90cfc09d58c1
Jingkai Tang <jingkaitang@gmail.com>
2015-12-17 11:39:56 +0800

    add: try person_and_way/problem_and_person/number/man_and_part/leave_year_after_great_way.py beneath new thing

---
+++ person_and_way/problem_and_person/number/man_and_part/leave_year_after_great_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_number_into_part(str_arg):
+    long_place(str_arg)
+    print('early_thing')
+
+def long_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_number_into_part('group')
