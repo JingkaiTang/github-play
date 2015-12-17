ca90f4d7ffbb97a6d8b59cced8dd28bd2020fd1d
Jingkai Tang <jingkaitang@gmail.com>
2015-12-17 13:55:41 +0800

    rm: tell problem/have_child/place.py in first part

---
+++ problem/have_child/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_time(str_arg):
+    great_woman(str_arg)
+    print('point_and_eye')
+
+def great_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_time('point')
