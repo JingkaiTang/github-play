1496ce82de6d640d3cc7e15aa855b3f0c95a81c6
Jingkai Tang <jingkaitang@gmail.com>
2015-12-19 07:49:13 +0800

    mod: go ask_world/hand/hand_and_long_woman/last_hand_or_eye.py in long government

---
+++ ask_world/hand/hand_and_long_woman/last_hand_or_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    make_eye_above_woman(str_arg)
+    print('hand')
+
+def make_eye_above_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('problem')
