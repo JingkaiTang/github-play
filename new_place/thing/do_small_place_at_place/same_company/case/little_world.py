4abf9d3907cb33e79ea29966c60ede5c18383ab5
Jingkai Tang <jingkaitang@gmail.com>
2015-12-20 02:17:34 +0800

    add: go new_place/thing/do_small_place_at_place/same_company/case/little_world.py beneath bad place

---
+++ new_place/thing/do_small_place_at_place/same_company/case/little_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part_and_small_day(str_arg):
+    place(str_arg)
+    print('problem')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part_and_small_day('person')
