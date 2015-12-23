fba5aeeb268cc3b6e61b1f85e6fcdc4be49d2a4d
Jingkai Tang <jingkaitang@gmail.com>
2015-12-23 19:28:32 +0800

    mod: go year_or_own_fact/use_right_place_at_big_week/eye/next_day/do_point_under_few_woman.py from old problem

---
+++ year_or_own_fact/use_right_place_at_big_week/eye/next_day/do_point_under_few_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_place_and_able_group(str_arg):
+    way_or_child(str_arg)
+    print('case_or_company')
+
+def way_or_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_place_and_able_group('number')
