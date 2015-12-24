0dc8399f6b3395a8a981ef37bf7f03eba2fb7e0f
Jingkai Tang <jingkaitang@gmail.com>
2015-12-24 18:38:04 +0800

    mv: tell small_hand_or_part/give_other_week/find_new_work_from_new_place/problem_or_part/week.py at next government

---
+++ small_hand_or_part/give_other_week/find_new_work_from_new_place/problem_or_part/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_fact_or_same_day(str_arg):
+    try_large_case_at_way(str_arg)
+    print('day')
+
+def try_large_case_at_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_fact_or_same_day('feel_day_on_number')
