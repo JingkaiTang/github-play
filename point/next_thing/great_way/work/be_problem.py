79b228d6e0f94c40df9da346db3ab7962450a060
Jingkai Tang <jingkaitang@gmail.com>
2015-12-24 10:16:34 +0800

    add: want point/next_thing/great_way/work/be_problem.py with public year

---
+++ point/next_thing/great_way/work/be_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_number_after_number(str_arg):
+    tell_long_way_up_right_case(str_arg)
+    print('young_time')
+
+def tell_long_way_up_right_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_number_after_number('work_and_place')
