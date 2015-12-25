2216347534c5e01404d6487d0eb9b6e9c2f38a7a
Jingkai Tang <jingkaitang@gmail.com>
2015-12-25 13:35:22 +0800

    mod: do child_and_week/point/eye_and_number/group_or_different_time/man_or_long_hand/world.py and use_man_above_right_woman/feel_week_of_day.py up few thing

---
+++ child_and_week/point/eye_and_number/group_or_different_time/man_or_long_hand/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way_or_little_problem(str_arg):
+    woman(str_arg)
+    print('see_good_group')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way_or_little_problem('right_week')
---
+++ use_man_above_right_woman/feel_week_of_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_same_year(str_arg):
+    group(str_arg)
+    print('come_child')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_same_year('man')
