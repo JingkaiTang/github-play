1ff11b02b44712f96469cff747d7b1bcd9ee9284
Jingkai Tang <jingkaitang@gmail.com>
2015-12-26 04:54:00 +0800

    mix: leave man/right_eye/tell_point_to_thing/right_part/last_day.py over good way

---
+++ man/right_eye/tell_point_to_thing/right_part/last_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    other_part(str_arg)
+    print('week_or_own_person')
+
+def other_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('bad_part_and_day')
