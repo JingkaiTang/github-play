a521dc52d3d4333d04550e2483fd96be39604473
Jingkai Tang <jingkaitang@gmail.com>
2015-12-26 10:05:43 +0800

    mv: call early_place/high_man/work/work_and_work.py by next case

---
+++ early_place/high_man/work/work_and_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point_and_eye(str_arg):
+    little_life_or_last_case(str_arg)
+    print('number')
+
+def little_life_or_last_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point_and_eye('leave_company_from_hand')
