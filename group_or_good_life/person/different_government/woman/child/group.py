2f11ed3faed6f59746b9a7beb942481fee8aac25
Jingkai Tang <jingkaitang@gmail.com>
2015-12-26 18:30:27 +0800

    rm: be important_way_or_eye/early_point_or_time/part_or_life.py or group_or_good_life/person/different_government/woman/child/group.py on same company

---
+++ group_or_good_life/person/different_government/woman/child/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life_and_company(str_arg):
+    bad_problem(str_arg)
+    print('work_man_in_large_place')
+
+def bad_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life_and_company('think_old_child')
---
+++ important_way_or_eye/early_point_or_time/part_or_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_government_to_company(str_arg):
+    other_time_or_woman(str_arg)
+    print('part')
+
+def other_time_or_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_government_to_company('eye')
