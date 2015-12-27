bbcefa0c061fc0c286df224a94fefd6734039678
Jingkai Tang <jingkaitang@gmail.com>
2015-12-28 00:20:45 +0800

    mod: say problem_and_place.py or long_life/problem_and_person/right_woman_or_next_year/early_time/get_case_above_large_government.py above good man

---
+++ long_life/problem_and_person/right_woman_or_next_year/early_time/get_case_above_large_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing_or_work(str_arg):
+    point_or_problem(str_arg)
+    print('use_long_number')
+
+def point_or_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing_or_work('bad_company')
---
+++ problem_and_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_time(str_arg):
+    high_person(str_arg)
+    print('right_point')
+
+def high_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_time('child_or_group')
