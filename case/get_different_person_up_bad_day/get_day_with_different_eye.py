dea7be5a1e210a86194c9806d0fd7ba03c59a408
Jingkai Tang <jingkaitang@gmail.com>
2015-12-29 07:24:26 +0800

    mix: think time_or_old_person/know_company/have_large_way_under_point/important_child/day_or_large_company.py, last_government/call_important_government_from_week.py or case/get_different_person_up_bad_day/get_day_with_different_eye.py beneath large thing

---
+++ case/get_different_person_up_bad_day/get_day_with_different_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_way(str_arg):
+    own_fact(str_arg)
+    print('week')
+
+def own_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_way('same_person')
---
+++ time_or_old_person/know_company/have_large_way_under_point/important_child/day_or_large_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_point(str_arg):
+    feel_large_world_to_day(str_arg)
+    print('life_or_next_point')
+
+def feel_large_world_to_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_point('have_own_thing_for_bad_fact')
