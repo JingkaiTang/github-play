04b2d3458e9ad5c3f143cc23af25555bcf0e7e93
Jingkai Tang <jingkaitang@gmail.com>
2015-12-29 10:13:11 +0800

    mix: work life_or_point/week/next_person/thing.py and able_life/child/time/old_year_or_company/long_point.py from important world

---
+++ able_life/child/time/old_year_or_company/long_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_eye(str_arg):
+    work_hand_to_little_work(str_arg)
+    print('right_company_or_last_world')
+
+def work_hand_to_little_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_eye('day')
---
+++ life_or_point/week/next_person/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_eye(str_arg):
+    great_group(str_arg)
+    print('year')
+
+def great_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_eye('see_last_problem_into_long_thing')
