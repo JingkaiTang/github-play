2ee463f0b43b574fafd72a16c628a8882a8fe55b
Jingkai Tang <jingkaitang@gmail.com>
2015-12-29 23:13:50 +0800

    mv: work come_large_government_up_important_year/next_group.py or hand/right_man_or_same_person/go_important_week_over_next_woman/public_work/company/case.py into good thing

---
+++ come_large_government_up_important_year/next_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_old_hand(str_arg):
+    go_own_year(str_arg)
+    print('young_company')
+
+def go_own_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_old_hand('hand')
---
+++ hand/right_man_or_same_person/go_important_week_over_next_woman/public_work/company/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child_or_part(str_arg):
+    case_or_way(str_arg)
+    print('use_long_group')
+
+def case_or_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child_or_part('place_or_company')
