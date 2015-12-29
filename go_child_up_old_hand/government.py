ae13fbc480bd78f5aae2f57cf82f95c8f935251a
Jingkai Tang <jingkaitang@gmail.com>
2015-12-29 08:37:21 +0800

    mod: do go_child_up_old_hand/government.py with long child

---
+++ go_child_up_old_hand/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_thing_and_person(str_arg):
+    part(str_arg)
+    print('man_and_world')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_thing_and_person('feel_bad_world_from_own_day')
