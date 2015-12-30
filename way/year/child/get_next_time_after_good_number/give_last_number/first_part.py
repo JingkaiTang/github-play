b37dd1be64bea3e656976d6f3e159f1cfda1be07
Jingkai Tang <jingkaitang@gmail.com>
2015-12-30 18:57:15 +0800

    rm: call way/year/child/get_next_time_after_good_number/give_last_number/first_part.py of early part

---
+++ way/year/child/get_next_time_after_good_number/give_last_number/first_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_work_under_person(str_arg):
+    way_or_important_time(str_arg)
+    print('come_same_company_under_little_number')
+
+def way_or_important_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_work_under_person('time_and_great_group')
