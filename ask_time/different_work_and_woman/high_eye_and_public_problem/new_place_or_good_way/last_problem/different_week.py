dc8b2df5ec029a5a0c774a4e6aac0b49d799f87b
Jingkai Tang <jingkaitang@gmail.com>
2015-12-30 10:25:32 +0800

    mix: have ask_time/different_work_and_woman/high_eye_and_public_problem/new_place_or_good_way/last_problem/different_week.py on little place

---
+++ ask_time/different_work_and_woman/high_eye_and_public_problem/new_place_or_good_way/last_problem/different_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_work_and_problem(str_arg):
+    old_hand_and_good_group(str_arg)
+    print('big_eye')
+
+def old_hand_and_good_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_work_and_problem('different_group')
