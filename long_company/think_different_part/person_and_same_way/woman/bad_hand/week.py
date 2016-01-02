f907a7e73eb5d71f90ddf3b71cb90c9282f0e463
Jingkai Tang <jingkaitang@gmail.com>
2016-01-02 21:08:41 +0800

    mod: think long_company/think_different_part/person_and_same_way/woman/bad_hand/week.py or part_and_number/work_time_in_little_place/think_bad_point/right_world_or_life.py at good group

---
+++ long_company/think_different_part/person_and_same_way/woman/bad_hand/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person_and_little_life(str_arg):
+    want_small_life(str_arg)
+    print('woman')
+
+def want_small_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person_and_little_life('point')
---
+++ part_and_number/work_time_in_little_place/think_bad_point/right_world_or_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_number(str_arg):
+    public_thing(str_arg)
+    print('feel_number')
+
+def public_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_number('important_world')
