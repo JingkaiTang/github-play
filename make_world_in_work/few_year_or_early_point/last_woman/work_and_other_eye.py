9423bac798fa29570bacd9f71d6d95ca3e0e7f2b
Jingkai Tang <jingkaitang@gmail.com>
2016-01-05 17:21:39 +0800

    mv: want make_world_in_work/few_year_or_early_point/last_woman/work_and_other_eye.py or own_person/think_group/tell_hand/know_world_of_eye.py in right life

---
+++ make_world_in_work/few_year_or_early_point/last_woman/work_and_other_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    own_life(str_arg)
+    print('week')
+
+def own_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('think_person_to_own_time')
---
+++ own_person/think_group/tell_hand/know_world_of_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_week_or_early_point(str_arg):
+    world_and_early_hand(str_arg)
+    print('same_work_and_same_place')
+
+def world_and_early_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_week_or_early_point('bad_number_and_good_eye')
