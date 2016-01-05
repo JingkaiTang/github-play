5b6b719b4f06c9834a62bdd073781e00150c1b28
Jingkai Tang <jingkaitang@gmail.com>
2016-01-05 23:22:05 +0800

    add: work same_man_and_other_life/go_week_above_young_hand/woman/right_way/take_other_company.py on great time

---
+++ same_man_and_other_life/go_week_above_young_hand/woman/right_way/take_other_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week_and_man(str_arg):
+    old_thing(str_arg)
+    print('old_part')
+
+def old_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week_and_man('man')
