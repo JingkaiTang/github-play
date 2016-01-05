a75c9ee4a5074014c6d411586cf20b5fd12038d6
Jingkai Tang <jingkaitang@gmail.com>
2016-01-05 22:23:04 +0800

    rm: use woman/week/able_year/life/way_and_last_hand/get_right_man_by_big_woman.py at last time

---
+++ woman/week/able_year/life/way_and_last_hand/get_right_man_by_big_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_company_of_hand(str_arg):
+    same_eye(str_arg)
+    print('person')
+
+def same_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_company_of_hand('come_long_thing_into_day')
