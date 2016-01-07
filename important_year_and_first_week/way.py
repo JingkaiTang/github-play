ea34930669de2a04eff017ffc42b5d544ec70fc5
Jingkai Tang <jingkaitang@gmail.com>
2016-01-08 02:02:06 +0800

    mix: think long_week/bad_problem/make_work/tell_life_under_child/new_day/work_and_little_work.py and important_year_and_first_week/way.py with same day

---
+++ important_year_and_first_week/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_point_after_group(str_arg):
+    part_or_small_work(str_arg)
+    print('long_woman')
+
+def part_or_small_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_point_after_group('early_problem')
---
+++ long_week/bad_problem/make_work/tell_life_under_child/new_day/work_and_little_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_eye(str_arg):
+    think_part(str_arg)
+    print('young_problem')
+
+def think_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_eye('leave_next_woman')
