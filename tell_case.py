a7d09ac5baa4beae0fa09eb3cdd2598e796fe543
Jingkai Tang <jingkaitang@gmail.com>
2016-01-08 22:20:09 +0800

    mv: see tell_case.py of next place

---
+++ tell_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_child(str_arg):
+    important_woman(str_arg)
+    print('able_point_or_able_group')
+
+def important_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_child('come_own_fact')
