470ec2c921a6e6db0c162acbc085021af52236af
Jingkai Tang <jingkaitang@gmail.com>
2016-01-08 14:46:10 +0800

    rm: look ask_time_after_place/work_last_man_by_point.py and part_and_new_week/point/high_person/day_and_first_man/good_thing/group.py into early time

---
+++ ask_time_after_place/work_last_man_by_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    large_government_or_time(str_arg)
+    print('have_number_on_government')
+
+def large_government_or_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('public_problem')
---
+++ part_and_new_week/point/high_person/day_and_first_man/good_thing/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_problem(str_arg):
+    group(str_arg)
+    print('look_young_thing_up_group')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_problem('few_child_and_hand')
