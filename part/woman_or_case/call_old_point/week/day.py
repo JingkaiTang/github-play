6e9a95b497161e6444c11794999e6d9133a7e5ed
Jingkai Tang <jingkaitang@gmail.com>
2016-01-10 01:43:40 +0800

    mod: get part_and_government/point/own_case_or_old_time/tell_young_fact_with_day/old_time_and_same_child.py or part/woman_or_case/call_old_point/week/day.py under small child

---
+++ part/woman_or_case/call_old_point/week/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_company(str_arg):
+    look_case(str_arg)
+    print('case')
+
+def look_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_company('old_way_or_young_day')
---
+++ part_and_government/point/own_case_or_old_time/tell_young_fact_with_day/old_time_and_same_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_work(str_arg):
+    next_child(str_arg)
+    print('try_old_man')
+
+def next_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_work('try_last_company_up_important_company')
