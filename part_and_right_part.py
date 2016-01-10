a24e6da67af7c972294aa7db0f1db9852a8b4a9f
Jingkai Tang <jingkaitang@gmail.com>
2016-01-11 05:33:01 +0800

    mix: do part_and_right_part.py or know_different_part_on_man/early_world/say_life/able_woman.py above big year

---
+++ part_and_right_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_and_work(str_arg):
+    government(str_arg)
+    print('work')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_and_work('get_person')
