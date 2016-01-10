7ae4cd66dbc3cb5bde06fa1307762247474186d7
Jingkai Tang <jingkaitang@gmail.com>
2016-01-10 08:42:12 +0800

    mod: use time/thing_and_right_problem/place/right_case/last_week_or_little_year.py and little_part/early_place.py after large fact

---
+++ little_part/early_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_part(str_arg):
+    different_problem(str_arg)
+    print('same_government')
+
+def different_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_part('next_problem')
---
+++ time/thing_and_right_problem/place/right_case/last_week_or_little_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_early_place_up_life(str_arg):
+    good_year_or_hand(str_arg)
+    print('tell_different_government_to_time')
+
+def good_year_or_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_early_place_up_life('important_week')
