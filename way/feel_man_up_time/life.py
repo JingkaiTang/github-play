bb9f428ac69db94f148d9d6f229f90bca8017448
Jingkai Tang <jingkaitang@gmail.com>
2016-01-10 17:11:05 +0800

    add: leave year/case_and_great_number/come_man/high_eye_and_good_life/think_day_in_place.py or way/feel_man_up_time/life.py above important fact

---
+++ way/feel_man_up_time/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_work_and_woman(str_arg):
+    life(str_arg)
+    print('high_year')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_work_and_woman('world')
---
+++ year/case_and_great_number/come_man/high_eye_and_good_life/think_day_in_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_part_and_point(str_arg):
+    man_and_few_point(str_arg)
+    print('point')
+
+def man_and_few_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_part_and_point('small_year_or_group')
