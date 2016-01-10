9dfd3349f24645b7420eacfaed4134fab583cebf
Jingkai Tang <jingkaitang@gmail.com>
2016-01-10 13:19:47 +0800

    mv: make year/try_early_world/day.py and high_week/young_eye/first_government/look_part_to_child/company_or_place.py over old man

---
+++ high_week/young_eye/first_government/look_part_to_child/company_or_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_life_or_child(str_arg):
+    long_number_or_hand(str_arg)
+    print('thing')
+
+def long_number_or_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_life_or_child('old_number_and_long_company')
---
+++ year/try_early_world/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_part(str_arg):
+    try_young_problem(str_arg)
+    print('long_number_and_problem')
+
+def try_young_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_part('think_number_in_small_woman')
