422312633ffe18de265bf15cadd0c447f86a5884
Jingkai Tang <jingkaitang@gmail.com>
2016-01-12 04:15:20 +0800

    mv: try man_or_hand.py to next place

---
+++ man_or_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_own_child(str_arg):
+    able_company(str_arg)
+    print('important_group_and_government')
+
+def able_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_own_child('life_or_other_problem')
