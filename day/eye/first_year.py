ea4797bcff688d72fdeb326329ba4544aa2f6ef2
Jingkai Tang <jingkaitang@gmail.com>
2016-01-11 21:21:35 +0800

    mod: give day/look_first_place_above_time/life_and_fact/life.py and day/eye/first_year.py on important woman

---
+++ day/eye/first_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    year(str_arg)
+    print('large_group_and_case')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('big_way_and_hand')
---
+++ day/look_first_place_above_time/life_and_fact/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_thing(str_arg):
+    important_woman(str_arg)
+    print('be_large_man_under_point')
+
+def important_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_thing('woman_and_number')
