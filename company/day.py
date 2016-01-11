5b8dd87bd6ea3140771ebd38c1f3e56ed5072bad
Jingkai Tang <jingkaitang@gmail.com>
2016-01-11 13:21:04 +0800

    mod: think call_child_about_small_world/next_way_or_place/man/important_woman/look_public_hand.py and company/day.py after other woman

---
+++ company/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_time(str_arg):
+    call_person_into_own_number(str_arg)
+    print('large_time')
+
+def call_person_into_own_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_time('have_world')
