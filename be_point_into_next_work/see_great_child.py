a3dee7f0ea035421ce8e2ccafc46a6f4419e7723
Jingkai Tang <jingkaitang@gmail.com>
2016-01-12 12:57:06 +0800

    mv: get be_point_into_next_work/see_great_child.py or last_problem.py over bad child

---
+++ be_point_into_next_work/see_great_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_man(str_arg):
+    case(str_arg)
+    print('big_work')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_man('group_and_important_point')
---
+++ last_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    day(str_arg)
+    print('able_child')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('day')
