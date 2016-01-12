a0a60623c796f7d359a0f3b959e6239ebbb571b8
Jingkai Tang <jingkaitang@gmail.com>
2016-01-12 23:54:58 +0800

    mv: ask way_or_person/life/eye/great_world/problem/think_own_part.py or other_woman/see_young_day_with_place.py above new woman

---
+++ other_woman/see_young_day_with_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    point(str_arg)
+    print('call_able_eye')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('point')
---
+++ way_or_person/life/eye/great_world/problem/think_own_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number_or_public_woman(str_arg):
+    big_thing(str_arg)
+    print('thing_or_way')
+
+def big_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number_or_public_woman('time_and_different_point')
