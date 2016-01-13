d7005ea25d1ad79df67e184f18a6c7ed84cac590
Jingkai Tang <jingkaitang@gmail.com>
2016-01-14 05:38:36 +0800

    mv: try last_week_or_hand.py on long point

---
+++ last_week_or_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_public_child(str_arg):
+    other_point_and_few_number(str_arg)
+    print('high_group')
+
+def other_point_and_few_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_public_child('eye_and_other_government')
