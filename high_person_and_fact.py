f4b1132a979ffa80090314256c12d201e41251e9
Jingkai Tang <jingkaitang@gmail.com>
2016-01-14 04:42:15 +0800

    mod: try high_person_and_fact.py and own_world_or_different_woman.py for high woman

---
+++ high_person_and_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_part_or_big_world(str_arg):
+    feel_life_of_child(str_arg)
+    print('have_woman')
+
+def feel_life_of_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_part_or_big_world('know_early_problem_about_child')
---
+++ own_world_or_different_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    high_way_and_old_person(str_arg)
+    print('woman')
+
+def high_way_and_old_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('point_or_hand')
