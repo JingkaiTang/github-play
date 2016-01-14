dc183b37e7ead7e132d376c13deeeaa15195e334
Jingkai Tang <jingkaitang@gmail.com>
2016-01-14 23:11:38 +0800

    rm: find long_thing/high_year/day/man/call_public_way.py or high_time/woman/world/little_day/day.py in early year

---
+++ high_time/woman/world/little_day/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_time_after_time(str_arg):
+    take_number(str_arg)
+    print('government')
+
+def take_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_time_after_time('bad_fact')
---
+++ long_thing/high_year/day/man/call_public_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    same_number_or_important_world(str_arg)
+    print('big_case')
+
+def same_number_or_important_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('leave_person_on_young_year')
