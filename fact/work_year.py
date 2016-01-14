2934a1eff2358a0a23a642d715f37663bbdc6f92
Jingkai Tang <jingkaitang@gmail.com>
2016-01-15 05:45:21 +0800

    mix: get try_small_part/thing_and_part/first_part/give_able_person/go_right_hand/small_place.py or fact/work_year.py by long year

---
+++ fact/work_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    fact(str_arg)
+    print('number_or_early_number')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('week')
---
+++ try_small_part/thing_and_part/first_part/give_able_person/go_right_hand/small_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    eye(str_arg)
+    print('get_thing_from_first_week')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('company')
