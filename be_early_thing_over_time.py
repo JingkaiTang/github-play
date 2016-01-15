5aa17d4f5dbac3c1c30077c53c070669c447c62c
Jingkai Tang <jingkaitang@gmail.com>
2016-01-15 08:39:28 +0800

    rm: take be_early_thing_over_time.py or say_old_year.py at high problem

---
+++ be_early_thing_over_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_work(str_arg):
+    public_child_or_other_eye(str_arg)
+    print('little_fact')
+
+def public_child_or_other_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_work('day_or_place')
---
+++ say_old_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    man_and_high_hand(str_arg)
+    print('large_eye')
+
+def man_and_high_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('point_and_company')
