41b1c993dc1b025551ef1e4012aef30fbc74b00b
Jingkai Tang <jingkaitang@gmail.com>
2016-01-15 23:01:06 +0800

    add: look early_problem/see_different_child_beneath_old_way/say_large_life_for_high_case/high_day/other_part.py and government/look_other_part/point/large_thing.py for other thing

---
+++ early_problem/see_different_child_beneath_old_way/say_large_life_for_high_case/high_day/other_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world_and_group(str_arg):
+    life(str_arg)
+    print('week_and_new_world')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world_and_group('great_way')
