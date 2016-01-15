44e9da76a2973836c408c6ea655b2308fa09a6e9
Jingkai Tang <jingkaitang@gmail.com>
2016-01-15 13:57:37 +0800

    mv: go find_group/young_man/man/person/work_hand/same_person.py to high hand

---
+++ find_group/young_man/man/person/work_hand/same_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    time_and_other_child(str_arg)
+    print('man')
+
+def time_and_other_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('case')
