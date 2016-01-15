2385a8208821adf9a9d2e4ee8fa39d04add207fd
Jingkai Tang <jingkaitang@gmail.com>
2016-01-15 19:41:58 +0800

    add: work use_life_beneath_last_fact/give_eye_up_group/first_part_and_world.py and public_eye_or_last_way/feel_part_about_little_government.py with right place

---
+++ public_eye_or_last_way/feel_part_about_little_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_part(str_arg):
+    child_and_work(str_arg)
+    print('high_fact_and_place')
+
+def child_and_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_part('part')
---
+++ use_life_beneath_last_fact/give_eye_up_group/first_part_and_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    government(str_arg)
+    print('life_or_thing')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('public_week_or_week')
