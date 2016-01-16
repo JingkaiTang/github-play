dea3f220b5c6a3a189638c4fd19fba4f15d35e83
Jingkai Tang <jingkaitang@gmail.com>
2016-01-16 11:10:13 +0800

    rm: be good_government_or_old_company/hand/little_thing/little_fact/part.py up own hand

---
+++ good_government_or_old_company/hand/little_thing/little_fact/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_work(str_arg):
+    group(str_arg)
+    print('do_life_above_day')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_work('feel_other_world')
