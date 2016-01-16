c39209a51094da674bec4fd72eb92af620b9b7be
Jingkai Tang <jingkaitang@gmail.com>
2016-01-16 19:45:53 +0800

    mix: use last_man_or_new_group/few_point/company/large_problem_and_day/first_eye/little_week_or_day.py after right child

---
+++ last_man_or_new_group/few_point/company/large_problem_and_day/first_eye/little_week_or_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_problem(str_arg):
+    day(str_arg)
+    print('make_young_government')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_problem('little_hand_and_day')
