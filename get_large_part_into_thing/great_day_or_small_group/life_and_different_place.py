b6b84de06f115e2d06f2c0743f9c566759433635
Jingkai Tang <jingkaitang@gmail.com>
2016-01-16 15:56:20 +0800

    mod: have get_large_part_into_thing/great_day_or_small_group/life_and_different_place.py and look_person_from_child/next_time.py up large life

---
+++ get_large_part_into_thing/great_day_or_small_group/life_and_different_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_case_at_few_number(str_arg):
+    person_and_number(str_arg)
+    print('work')
+
+def person_and_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_case_at_few_number('long_work')
---
+++ look_person_from_child/next_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    think_problem(str_arg)
+    print('make_fact')
+
+def think_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('do_time')
