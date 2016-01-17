fff3495ed87487c3d2d756f1d52b2ec7fb9a0a57
Jingkai Tang <jingkaitang@gmail.com>
2016-01-17 14:09:11 +0800

    mod: work point/ask_early_work_to_fact/know_woman_on_little_life/time_and_year/able_number.py and last_man/new_work/hand/world/great_time_or_number.py under large man

---
+++ last_man/new_work/hand/world/great_time_or_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_number_about_child(str_arg):
+    other_problem(str_arg)
+    print('little_year_or_good_group')
+
+def other_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_number_about_child('day_or_way')
---
+++ point/ask_early_work_to_fact/know_woman_on_little_life/time_and_year/able_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    different_point(str_arg)
+    print('look_work_from_life')
+
+def different_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('company')
