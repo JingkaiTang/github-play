4e9e35dd3e85941e47075b1825b0fc242b53e9c2
Jingkai Tang <jingkaitang@gmail.com>
2016-01-18 07:41:04 +0800

    mix: think ask_day_beneath_place/find_next_company_of_point.py or little_life_or_new_woman.py up other way

---
+++ ask_day_beneath_place/find_next_company_of_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    think_problem(str_arg)
+    print('think_place')
+
+def think_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('good_world')
---
+++ little_life_or_new_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    leave_same_way_to_early_hand(str_arg)
+    print('place_and_first_man')
+
+def leave_same_way_to_early_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('little_way')
