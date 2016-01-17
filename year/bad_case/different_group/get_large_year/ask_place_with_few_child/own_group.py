bdc89c776bc93116240d84ebe230ac12deb94ae5
Jingkai Tang <jingkaitang@gmail.com>
2016-01-17 21:06:37 +0800

    add: know year/bad_case/different_group/get_large_year/ask_place_with_few_child/own_group.py into important government

---
+++ year/bad_case/different_group/get_large_year/ask_place_with_few_child/own_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_point(str_arg):
+    give_year_under_high_part(str_arg)
+    print('old_hand')
+
+def give_year_under_high_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_point('year')
