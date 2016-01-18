9b6be9f9a7342e9678498215d5ae604371b6bf78
Jingkai Tang <jingkaitang@gmail.com>
2016-01-19 03:42:08 +0800

    mv: call next_person_or_early_part.py or place_and_hand/able_thing/way/good_hand.py at different fact

---
+++ next_person_or_early_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_problem(str_arg):
+    little_life_and_thing(str_arg)
+    print('case_or_different_life')
+
+def little_life_and_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_problem('way')
---
+++ place_and_hand/able_thing/way/good_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group_or_bad_world(str_arg):
+    seem_point_after_year(str_arg)
+    print('next_company')
+
+def seem_point_after_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group_or_bad_world('time')
