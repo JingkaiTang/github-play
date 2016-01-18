b465f799c5a414503e0ed41812126e85074f0b09
Jingkai Tang <jingkaitang@gmail.com>
2016-01-18 08:35:20 +0800

    mix: think group/great_time_and_public_problem.py under big eye

---
+++ group/great_time_and_public_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_place_or_company(str_arg):
+    group_or_day(str_arg)
+    print('thing_and_thing')
+
+def group_or_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_place_or_company('long_life_or_way')
