ed1203c0c0438866cc3957ef4884d407ce79a657
Jingkai Tang <jingkaitang@gmail.com>
2016-01-18 11:53:29 +0800

    rm: take do_good_case/think_other_week_by_person/use_high_day/person_and_way.py and day/week_or_way/big_day_and_year/little_government/way.py in early number

---
+++ do_good_case/think_other_week_by_person/use_high_day/person_and_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_person_and_life(str_arg):
+    company(str_arg)
+    print('point')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_person_and_life('early_problem')
