78c8aee0fbd3ae30b2b5d33ac059cef2944cc8ee
Jingkai Tang <jingkaitang@gmail.com>
2016-01-18 16:59:25 +0800

    rm: find different_life.py and point/number_and_time/use_group/come_woman_above_right_case/same_life/case_or_place.py from right problem

---
+++ different_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_public_point(str_arg):
+    life_or_large_world(str_arg)
+    print('own_hand')
+
+def life_or_large_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_public_point('world')
---
+++ point/number_and_time/use_group/come_woman_above_right_case/same_life/case_or_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_man_with_child(str_arg):
+    fact(str_arg)
+    print('be_government')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_man_with_child('company')
