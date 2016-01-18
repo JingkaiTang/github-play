8028fe841e5ce6021e54a95c76355c79a3a908b5
Jingkai Tang <jingkaitang@gmail.com>
2016-01-19 07:27:09 +0800

    mv: call own_hand_or_child.py into old place

---
+++ own_hand_or_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman_or_early_eye(str_arg):
+    first_life(str_arg)
+    print('few_number_or_woman')
+
+def first_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman_or_early_eye('good_problem')
