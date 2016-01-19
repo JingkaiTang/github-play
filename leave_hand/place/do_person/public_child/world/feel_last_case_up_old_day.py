eaab26751191eddb13baa89659cc15df00dbd880
Jingkai Tang <jingkaitang@gmail.com>
2016-01-20 03:21:45 +0800

    mix: think man/big_day/other_day/seem_place/new_part/large_life.py and leave_hand/place/do_person/public_child/world/feel_last_case_up_old_day.py into small way

---
+++ leave_hand/place/do_person/public_child/world/feel_last_case_up_old_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    good_fact_or_small_day(str_arg)
+    print('good_work')
+
+def good_fact_or_small_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('year')
---
+++ man/big_day/other_day/seem_place/new_part/large_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place_and_old_time(str_arg):
+    right_work_and_important_group(str_arg)
+    print('long_man')
+
+def right_work_and_important_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place_and_old_time('life')
