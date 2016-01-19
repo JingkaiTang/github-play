8c31b1b0b00f3693007b0ff65b0bde9b174916e4
Jingkai Tang <jingkaitang@gmail.com>
2016-01-20 03:14:10 +0800

    mod: find get_company_at_day.py or child/next_man.py over other time

---
+++ child/next_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_company(str_arg):
+    eye(str_arg)
+    print('eye')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_company('group')
---
+++ get_company_at_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_new_place(str_arg):
+    important_year_or_right_time(str_arg)
+    print('case')
+
+def important_year_or_right_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_new_place('come_young_child')
