6f8b0a052920d2d9f16801ee8405d96637d4620f
Jingkai Tang <jingkaitang@gmail.com>
2016-01-21 01:23:16 +0800

    mod: work go_small_number_after_man/number/person_or_point/right_child/place_and_right_part.py with early day

---
+++ go_small_number_after_man/number/person_or_point/right_child/place_and_right_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_part(str_arg):
+    new_eye_or_few_thing(str_arg)
+    print('life_or_number')
+
+def new_eye_or_few_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_part('know_group_for_year')
