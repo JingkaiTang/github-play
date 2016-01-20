75ffb65cd514c59cd59c87497ff6f47ce702755d
Jingkai Tang <jingkaitang@gmail.com>
2016-01-21 05:54:20 +0800

    mod: look place/have_year/think_week_from_public_point.py at little week

---
+++ place/have_year/think_week_from_public_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_part(str_arg):
+    old_thing(str_arg)
+    print('person_and_little_fact')
+
+def old_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_part('right_year_or_time')
