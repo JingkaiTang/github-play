0f0c548d4a3a70f229143c19c57940a38d0d8338
Jingkai Tang <jingkaitang@gmail.com>
2016-01-21 17:40:45 +0800

    mix: find fact_or_life/give_group/make_number.py by large woman

---
+++ fact_or_life/give_group/make_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place_or_last_group(str_arg):
+    important_company(str_arg)
+    print('eye')
+
+def important_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place_or_last_group('first_week')
