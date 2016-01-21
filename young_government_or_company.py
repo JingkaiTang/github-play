924e639d20ee2afd48d0212fad7f72abc8faf8d3
Jingkai Tang <jingkaitang@gmail.com>
2016-01-21 14:21:48 +0800

    mod: try young_government_or_company.py from first case

---
+++ young_government_or_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    public_man(str_arg)
+    print('say_bad_fact_of_place')
+
+def public_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('eye')
