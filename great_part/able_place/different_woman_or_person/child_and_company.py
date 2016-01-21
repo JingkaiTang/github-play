1d15cd138812cc768c0b379a8994ec04503e470a
Jingkai Tang <jingkaitang@gmail.com>
2016-01-21 19:56:11 +0800

    rm: feel part/week_and_last_company/own_group/other_point/find_point.py and great_part/able_place/different_woman_or_person/child_and_company.py on early time

---
+++ great_part/able_place/different_woman_or_person/child_and_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_hand_in_day(str_arg):
+    have_different_case_over_government(str_arg)
+    print('few_hand')
+
+def have_different_case_over_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_hand_in_day('know_man_over_public_place')
