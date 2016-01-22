096d636ad41688dcd12b3b04016e1eaa037fa6e5
Jingkai Tang <jingkaitang@gmail.com>
2016-01-23 03:51:25 +0800

    mix: use number_and_high_life/week/good_man.py, problem_and_great_woman/able_time.py or public_eye/right_eye_and_early_part/number/way.py after first group

---
+++ number_and_high_life/week/good_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    own_work(str_arg)
+    print('want_early_thing')
+
+def own_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('few_number')
---
+++ problem_and_great_woman/able_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_good_week_at_government(str_arg):
+    own_life(str_arg)
+    print('small_way')
+
+def own_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_good_week_at_government('want_company_with_person')
