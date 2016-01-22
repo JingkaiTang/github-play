c0eca83d7a228966be1ffdd8323ff5d4380c99a6
Jingkai Tang <jingkaitang@gmail.com>
2016-01-23 00:13:00 +0800

    rm: go find_point_up_child/man/government/public_person.py over first group

---
+++ find_point_up_child/man/government/public_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_day(str_arg):
+    old_part_or_old_eye(str_arg)
+    print('different_time')
+
+def old_part_or_old_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_day('woman')
