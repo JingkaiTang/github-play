b7f032b470a17f97563aeb5c57d270c442c2d7e7
Jingkai Tang <jingkaitang@gmail.com>
2016-01-23 01:49:05 +0800

    mix: be want_thing_on_problem/know_part_under_person/day_or_point/fact.py above young government

---
+++ want_thing_on_problem/know_part_under_person/day_or_point/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_eye(str_arg):
+    have_public_child_beneath_different_case(str_arg)
+    print('place_and_man')
+
+def have_public_child_beneath_different_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_eye('think_old_man_under_case')
