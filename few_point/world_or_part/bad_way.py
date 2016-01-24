22b29e54420616cbddaf59a798ff147afd3c11cc
Jingkai Tang <jingkaitang@gmail.com>
2016-01-24 23:26:53 +0800

    add: do few_point/world_or_part/bad_way.py with good thing

---
+++ few_point/world_or_part/bad_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_case_and_day(str_arg):
+    problem(str_arg)
+    print('child')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_case_and_day('important_week_or_person')
