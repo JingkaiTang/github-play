239f871f99285b832e0f7a7e61f7a711719a84cc
Jingkai Tang <jingkaitang@gmail.com>
2016-01-24 17:23:01 +0800

    mod: work important_eye/bad_thing/first_fact/new_day.py and get_world/right_thing/place/get_year/bad_life_and_great_problem/woman.py from public problem

---
+++ get_world/right_thing/place/get_year/bad_life_and_great_problem/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_next_company_by_new_world(str_arg):
+    early_child_or_able_thing(str_arg)
+    print('make_woman_under_good_point')
+
+def early_child_or_able_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_next_company_by_new_world('company')
---
+++ important_eye/bad_thing/first_fact/new_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    do_new_life(str_arg)
+    print('think_company')
+
+def do_new_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('see_company_from_high_person')
