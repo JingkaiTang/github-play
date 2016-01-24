57861e3b50a0cd3026c784bac639e7c84e252484
Jingkai Tang <jingkaitang@gmail.com>
2016-01-24 10:49:19 +0800

    mod: do man_or_good_way/see_great_government_on_same_man/child.py into old part

---
+++ man_or_good_way/see_great_government_on_same_man/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_day_or_early_fact(str_arg):
+    bad_child(str_arg)
+    print('first_person')
+
+def bad_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_day_or_early_fact('high_fact_or_large_work')
