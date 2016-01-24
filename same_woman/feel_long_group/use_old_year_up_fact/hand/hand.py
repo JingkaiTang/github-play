88e0ef548c6c496fa63c9bf5d6e3b3d3211937cb
Jingkai Tang <jingkaitang@gmail.com>
2016-01-25 05:45:23 +0800

    add: know same_woman/feel_long_group/use_old_year_up_fact/hand/hand.py at large woman

---
+++ same_woman/feel_long_group/use_old_year_up_fact/hand/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_number_up_early_woman(str_arg):
+    take_day_under_bad_world(str_arg)
+    print('say_high_place')
+
+def take_day_under_bad_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_number_up_early_woman('eye')
