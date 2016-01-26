c74058f4f70df7ac52ced61966d7a13175e3ddc0
Jingkai Tang <jingkaitang@gmail.com>
2016-01-26 10:54:42 +0800

    add: seem be_man_for_different_eye/person/great_group/great_point/see_small_week_above_hand.py or number/world_and_life/problem_or_big_group/few_hand_and_old_government/know_life_to_next_person.py to next man

---
+++ be_man_for_different_eye/person/great_group/great_point/see_small_week_above_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_woman(str_arg):
+    have_right_thing_under_day(str_arg)
+    print('hand')
+
+def have_right_thing_under_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_woman('say_own_company')
---
+++ number/world_and_life/problem_or_big_group/few_hand_and_old_government/know_life_to_next_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_life_and_day(str_arg):
+    make_eye(str_arg)
+    print('try_big_time')
+
+def make_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_life_and_day('public_fact')
