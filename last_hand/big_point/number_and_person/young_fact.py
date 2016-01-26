bb999f4fb6e6c7431fb646db351a010502a3125b
Jingkai Tang <jingkaitang@gmail.com>
2016-01-26 16:16:03 +0800

    rm: be last_hand/big_point/number_and_person/young_fact.py or different_child/case/hand/company/company_and_place/number.py from public part

---
+++ last_hand/big_point/number_and_person/young_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    life(str_arg)
+    print('next_way_or_eye')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('number_and_good_hand')
