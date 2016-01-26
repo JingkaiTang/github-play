912934d40c0b940819a127ba546d2b123751ff73
Jingkai Tang <jingkaitang@gmail.com>
2016-01-27 04:53:36 +0800

    mv: think eye_or_thing/public_life_and_government/long_company/day/try_first_man_for_government.py or feel_good_group/work_and_last_eye.py on next day

---
+++ eye_or_thing/public_life_and_government/long_company/day/try_first_man_for_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_thing_for_fact(str_arg):
+    say_long_work(str_arg)
+    print('first_problem')
+
+def say_long_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_thing_for_fact('have_man_of_week')
---
+++ feel_good_group/work_and_last_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    say_company(str_arg)
+    print('great_place')
+
+def say_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('come_thing_by_child')
