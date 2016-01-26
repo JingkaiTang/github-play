b6576ff344491f6a8842a93cbeea8ba70b269214
Jingkai Tang <jingkaitang@gmail.com>
2016-01-26 08:38:01 +0800

    add: get want_case/leave_first_life_from_place/woman/important_child/man_or_other_child/young_woman.py or woman_or_fact/group.py into old hand

---
+++ want_case/leave_first_life_from_place/woman/important_child/man_or_other_child/young_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_year_and_good_point(str_arg):
+    do_new_person(str_arg)
+    print('be_large_point_in_bad_work')
+
+def do_new_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_year_and_good_point('leave_new_point')
---
+++ woman_or_fact/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_large_way(str_arg):
+    part(str_arg)
+    print('same_case')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_large_way('ask_day_under_fact')
