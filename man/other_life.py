39646dad64bf886f0c8cd8990db3ea991e9d4742
Jingkai Tang <jingkaitang@gmail.com>
2016-01-26 13:23:58 +0800

    mv: go man/other_life.py under next government

---
+++ man/other_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_day(str_arg):
+    feel_important_hand(str_arg)
+    print('leave_other_group_to_other_fact')
+
+def feel_important_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_day('important_life_and_woman')
