1c77bd57b37c12c53dd05b9bf483a16ebfaa3b0e
Jingkai Tang <jingkaitang@gmail.com>
2016-01-26 20:26:33 +0800

    rm: feel young_group/see_person/high_life_and_person/way_or_eye/case/other_point.py under public problem

---
+++ young_group/see_person/high_life_and_person/way_or_eye/case/other_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_number(str_arg):
+    point(str_arg)
+    print('important_number')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_number('work_and_work')
