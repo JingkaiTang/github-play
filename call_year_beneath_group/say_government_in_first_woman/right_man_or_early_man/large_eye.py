e6e30e07d54f8f1428fe8de3e7f86f412abc0521
Jingkai Tang <jingkaitang@gmail.com>
2016-01-26 22:54:24 +0800

    add: give call_year_beneath_group/say_government_in_first_woman/right_man_or_early_man/large_eye.py on public man

---
+++ call_year_beneath_group/say_government_in_first_woman/right_man_or_early_man/large_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_woman_and_life(str_arg):
+    great_point(str_arg)
+    print('work')
+
+def great_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_woman_and_life('make_week')
