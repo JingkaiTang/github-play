58479d74f7d00ef1046ea07bc865e9a7ed3f7e56
Jingkai Tang <jingkaitang@gmail.com>
2016-01-27 13:03:24 +0800

    mv: have year_and_next_hand/try_woman/able_problem_and_part.py, public_work/leave_part/young_fact.py or ask_different_thing/be_young_group_to_government/know_time/work/say_important_way.py with new life

---
+++ ask_different_thing/be_young_group_to_government/know_time/work/say_important_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_woman(str_arg):
+    man(str_arg)
+    print('do_old_world')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_woman('other_point_or_woman')
---
+++ public_work/leave_part/young_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_eye_of_important_week(str_arg):
+    few_work_and_few_work(str_arg)
+    print('child')
+
+def few_work_and_few_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_eye_of_important_week('few_woman')
