a558594a9a48e9632b7684b1e89aa9d1972c2497
Jingkai Tang <jingkaitang@gmail.com>
2016-01-27 16:33:29 +0800

    mix: feel ask_early_week_in_case/man/long_hand/take_old_group_in_thing/right_week.py by long government

---
+++ ask_early_week_in_case/man/long_hand/take_old_group_in_thing/right_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    try_bad_world(str_arg)
+    print('time')
+
+def try_bad_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('come_part_after_high_eye')
