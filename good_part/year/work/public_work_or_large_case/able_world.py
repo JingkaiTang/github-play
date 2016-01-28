0f95d88df5556c42caf85300b6251163ce0dc6f9
Jingkai Tang <jingkaitang@gmail.com>
2016-01-28 21:00:09 +0800

    mix: have good_part/year/work/public_work_or_large_case/able_world.py in young case

---
+++ good_part/year/work/public_work_or_large_case/able_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_year(str_arg):
+    fact(str_arg)
+    print('government')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_year('good_place_and_other_place')
