b39a137a1ad6c43f29c178712bbf6261339d6530
Jingkai Tang <jingkaitang@gmail.com>
2016-01-30 17:13:02 +0800

    mix: call be_company/few_man_or_way/own_life_and_next_company/few_work_and_child/public_group_or_high_place/important_day_or_other_problem.py above able eye

---
+++ be_company/few_man_or_way/own_life_and_next_company/few_work_and_child/public_group_or_high_place/important_day_or_other_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_day_and_person(str_arg):
+    long_world(str_arg)
+    print('go_important_person')
+
+def long_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_day_and_person('come_government_about_other_woman')
