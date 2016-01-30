9b58a650afdfb2d6d70257610b1af8f6c09c21c6
Jingkai Tang <jingkaitang@gmail.com>
2016-01-30 16:59:21 +0800

    rm: have high_hand_and_public_point.py above old day

---
+++ high_hand_and_public_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_point(str_arg):
+    company_and_man(str_arg)
+    print('life')
+
+def company_and_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_point('problem')
