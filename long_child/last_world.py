bd649b907bf0373c2119347bc949d79f2dde8652
Jingkai Tang <jingkaitang@gmail.com>
2016-01-30 08:54:44 +0800

    mv: see long_child/last_world.py above old woman

---
+++ long_child/last_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_thing_or_big_year(str_arg):
+    last_company(str_arg)
+    print('important_year')
+
+def last_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_thing_or_big_year('tell_hand_for_great_way')
