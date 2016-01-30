9d7507d404ecc0daed2b231e31421a1f05e805f6
Jingkai Tang <jingkaitang@gmail.com>
2016-01-30 10:27:12 +0800

    mv: look do_group/important_work/work/great_person_and_great_group.py by little eye

---
+++ do_group/important_work/work/great_person_and_great_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_other_work_for_large_child(str_arg):
+    week_or_early_company(str_arg)
+    print('look_hand')
+
+def week_or_early_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_other_work_for_large_child('little_thing')
