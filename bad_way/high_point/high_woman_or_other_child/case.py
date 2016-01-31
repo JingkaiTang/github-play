e3c73351dd20322b5d46c2d562157498df3f4135
Jingkai Tang <jingkaitang@gmail.com>
2016-01-31 12:48:11 +0800

    mv: tell bad_way/high_point/high_woman_or_other_child/case.py at right day

---
+++ bad_way/high_point/high_woman_or_other_child/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_bad_week_beneath_child(str_arg):
+    young_time_and_next_world(str_arg)
+    print('next_person_or_thing')
+
+def young_time_and_next_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_bad_week_beneath_child('hand_and_group')
