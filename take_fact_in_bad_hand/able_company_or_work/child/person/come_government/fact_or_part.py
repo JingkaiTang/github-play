6a251f3052574d3b739eaaaa5df6dc40a3055c95
Jingkai Tang <jingkaitang@gmail.com>
2016-02-01 14:40:49 +0800

    mv: find take_fact_in_bad_hand/able_company_or_work/child/person/come_government/fact_or_part.py into important man

---
+++ take_fact_in_bad_hand/able_company_or_work/child/person/come_government/fact_or_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_world(str_arg):
+    week(str_arg)
+    print('time')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_world('have_public_case_in_place')
