efba1ac4555c049a74546220af2bc79c93aeb281
Jingkai Tang <jingkaitang@gmail.com>
2016-02-02 16:36:39 +0800

    mv: want go_life_under_few_group.py and eye/case/first_eye.py under few week

---
+++ eye/case/first_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government_or_small_week(str_arg):
+    good_child(str_arg)
+    print('other_fact')
+
+def good_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government_or_small_week('woman_or_number')
---
+++ go_life_under_few_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_world(str_arg):
+    come_part_about_own_thing(str_arg)
+    print('high_child')
+
+def come_part_about_own_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_world('come_week_under_last_fact')
