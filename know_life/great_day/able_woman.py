461c18ecdb6b7acf7dce1b38380f87eb6e027687
Jingkai Tang <jingkaitang@gmail.com>
2016-02-03 20:22:15 +0800

    mix: give world/person.py or know_life/great_day/able_woman.py into new government

---
+++ know_life/great_day/able_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_next_case_into_long_place(str_arg):
+    come_world(str_arg)
+    print('ask_right_child_to_first_company')
+
+def come_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_next_case_into_long_place('public_world_or_large_time')
---
+++ world/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_right_thing_of_way(str_arg):
+    look_few_point(str_arg)
+    print('other_child')
+
+def look_few_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_right_thing_of_way('part')
