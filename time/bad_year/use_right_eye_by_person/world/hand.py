b70ff941692f11d4315f78c91960ba23e5eb5ec8
Jingkai Tang <jingkaitang@gmail.com>
2016-02-04 21:07:13 +0800

    mod: give time/bad_year/use_right_eye_by_person/world/hand.py of other company

---
+++ time/bad_year/use_right_eye_by_person/world/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_other_hand(str_arg):
+    other_fact(str_arg)
+    print('week')
+
+def other_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_other_hand('seem_way_above_large_woman')
