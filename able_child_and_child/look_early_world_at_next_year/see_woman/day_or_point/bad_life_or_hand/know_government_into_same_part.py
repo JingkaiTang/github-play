6ec6599daab72ee8f5ae708d0a3e983c51d48655
Jingkai Tang <jingkaitang@gmail.com>
2016-02-05 04:25:36 +0800

    mix: tell important_time/long_child_or_person/work_right_time.py or able_child_and_child/look_early_world_at_next_year/see_woman/day_or_point/bad_life_or_hand/know_government_into_same_part.py with next time

---
+++ able_child_and_child/look_early_world_at_next_year/see_woman/day_or_point/bad_life_or_hand/know_government_into_same_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_good_government(str_arg):
+    last_problem(str_arg)
+    print('go_other_thing_after_new_child')
+
+def last_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_good_government('give_public_government')
---
+++ important_time/long_child_or_person/work_right_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_point(str_arg):
+    point(str_arg)
+    print('come_point_about_place')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_point('problem')
