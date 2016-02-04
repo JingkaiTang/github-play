da196200bb580b107088fd7bd27286215c977e32
Jingkai Tang <jingkaitang@gmail.com>
2016-02-04 17:30:45 +0800

    mix: leave problem_or_part/work_or_high_place/feel_young_person_at_old_work/make_problem_on_early_hand/day_or_next_part/woman.py and other_eye_and_great_year/young_woman_or_year/work_last_year_to_person/different_thing.py under old time

---
+++ other_eye_and_great_year/young_woman_or_year/work_last_year_to_person/different_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_day_of_eye(str_arg):
+    time_or_world(str_arg)
+    print('great_point')
+
+def time_or_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_day_of_eye('old_time_or_day')
---
+++ problem_or_part/work_or_high_place/feel_young_person_at_old_work/make_problem_on_early_hand/day_or_next_part/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_week_or_group(str_arg):
+    come_bad_problem(str_arg)
+    print('young_number')
+
+def come_bad_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_week_or_group('part_or_different_eye')
