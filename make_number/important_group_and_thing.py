a125597cd360ea12158b242863dafd7c0a00af1a
Jingkai Tang <jingkaitang@gmail.com>
2016-02-04 11:23:12 +0800

    add: try world/tell_able_person/problem_or_week/thing/other_group/ask_able_hand_into_able_life.py and make_number/important_group_and_thing.py for new life

---
+++ make_number/important_group_and_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    next_woman(str_arg)
+    print('good_child')
+
+def next_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('government')
