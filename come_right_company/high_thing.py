2e48db1c911748eefc3ac443daff6439c5360c82
Jingkai Tang <jingkaitang@gmail.com>
2016-02-05 07:02:27 +0800

    mod: work come_right_company/high_thing.py over young point

---
+++ come_right_company/high_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    day(str_arg)
+    print('say_little_place')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('tell_place')
