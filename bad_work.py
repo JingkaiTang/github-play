d913447d3ee3cb10f09772ac54f9f015586924b4
Jingkai Tang <jingkaitang@gmail.com>
2016-02-05 13:34:20 +0800

    mv: find big_thing/tell_early_company/come_few_person_about_early_place/able_eye/good_man_and_good_group.py or bad_work.py into important case

---
+++ bad_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_way(str_arg):
+    able_thing(str_arg)
+    print('high_company')
+
+def able_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_way('way')
---
+++ big_thing/tell_early_company/come_few_person_about_early_place/able_eye/good_man_and_good_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    company(str_arg)
+    print('right_part_and_part')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('try_old_person')
