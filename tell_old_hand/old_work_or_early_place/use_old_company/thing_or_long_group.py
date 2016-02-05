50f5e41135412dc39b28755e6bc14877a905defe
Jingkai Tang <jingkaitang@gmail.com>
2016-02-06 04:46:37 +0800

    mix: use next_company_and_time/eye/seem_first_child_up_week/time.py, tell_old_hand/old_work_or_early_place/use_old_company/thing_or_long_group.py and time/child_and_own_week/high_fact/go_good_day/try_able_hand/next_number.py after different week

---
+++ tell_old_hand/old_work_or_early_place/use_old_company/thing_or_long_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem_or_small_person(str_arg):
+    high_way_or_right_woman(str_arg)
+    print('great_point')
+
+def high_way_or_right_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem_or_small_person('use_day')
---
+++ time/child_and_own_week/high_fact/go_good_day/try_able_hand/next_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_child(str_arg):
+    other_child(str_arg)
+    print('government')
+
+def other_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_child('company')
