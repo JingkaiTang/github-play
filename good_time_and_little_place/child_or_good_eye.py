4f9dd444f52511054a7a26f215235c112625c8bc
Jingkai Tang <jingkaitang@gmail.com>
2016-02-07 07:47:57 +0800

    add: come good_time_and_little_place/child_or_good_eye.py and other_man_and_day/last_fact/next_way/life_and_long_week.py by old group

---
+++ good_time_and_little_place/child_or_good_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case_or_world(str_arg):
+    take_problem(str_arg)
+    print('say_good_man')
+
+def take_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case_or_world('feel_high_hand_by_world')
---
+++ other_man_and_day/last_fact/next_way/life_and_long_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_person_on_young_place(str_arg):
+    woman(str_arg)
+    print('life')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_person_on_young_place('hand_and_own_government')
