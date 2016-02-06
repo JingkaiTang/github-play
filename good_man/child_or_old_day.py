4794db35471762926627195585f7af66228d4880
Jingkai Tang <jingkaitang@gmail.com>
2016-02-06 21:15:17 +0800

    add: seem good_man/child_or_old_day.py of first way

---
+++ good_man/child_or_old_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_woman(str_arg):
+    long_number_or_different_life(str_arg)
+    print('call_early_point_of_thing')
+
+def long_number_or_different_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_woman('seem_world')
