92b3521395d68225ee7f52864ffbcee7837f146e
Jingkai Tang <jingkaitang@gmail.com>
2016-02-07 13:55:05 +0800

    mix: feel man/call_last_world_with_next_woman/want_last_case/come_new_hand/few_person.py, have_day/man_and_few_man.py or find_company_into_place/leave_child.py up public world

---
+++ find_company_into_place/leave_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_good_life_of_government(str_arg):
+    old_way(str_arg)
+    print('person')
+
+def old_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_good_life_of_government('work_and_work')
