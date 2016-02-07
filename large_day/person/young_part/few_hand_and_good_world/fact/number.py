06401da38e903823164edb37a28162651f63923c
Jingkai Tang <jingkaitang@gmail.com>
2016-02-07 22:33:06 +0800

    rm: seem big_case.py, old_world/company.py or large_day/person/young_part/few_hand_and_good_world/fact/number.py up few thing

---
+++ large_day/person/young_part/few_hand_and_good_world/fact/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    child_or_own_week(str_arg)
+    print('think_old_world')
+
+def child_or_own_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('last_way')
