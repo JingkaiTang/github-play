bbaee9930d7c17c2da53be072d1dfd7006b455e7
Jingkai Tang <jingkaitang@gmail.com>
2016-02-07 14:07:44 +0800

    add: come seem_case_into_old_fact/work/time_and_time/try_good_hand/be_same_day/see_public_part.py and eye/first_person/work_small_man/give_life_beneath_public_thing.py for large day

---
+++ eye/first_person/work_small_man/give_life_beneath_public_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_person_and_thing(str_arg):
+    child(str_arg)
+    print('come_important_fact_to_other_thing')
+
+def child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_person_and_thing('leave_old_fact_at_early_person')
---
+++ seem_case_into_old_fact/work/time_and_time/try_good_hand/be_same_day/see_public_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_part(str_arg):
+    important_place_or_part(str_arg)
+    print('think_world_about_little_time')
+
+def important_place_or_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_part('think_place')
