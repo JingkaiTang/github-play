b4b76486ba76308c242853e44c90e0277939b527
Jingkai Tang <jingkaitang@gmail.com>
2016-02-07 20:27:14 +0800

    rm: look woman/company/life/different_problem/problem_and_long_day/make_different_problem_by_life.py of new week

---
+++ woman/company/life/different_problem/problem_and_long_day/make_different_problem_by_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_group(str_arg):
+    try_long_case(str_arg)
+    print('large_week')
+
+def try_long_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_group('good_time_or_time')
