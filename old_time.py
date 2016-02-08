a12d650f8a7146cea4c1073426bfb0814386df1d
Jingkai Tang <jingkaitang@gmail.com>
2016-02-09 05:27:11 +0800

    mix: say old_time.py on large way

---
+++ old_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life_or_man(str_arg):
+    tell_first_case_beneath_same_eye(str_arg)
+    print('try_part')
+
+def tell_first_case_beneath_same_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life_or_man('person')
