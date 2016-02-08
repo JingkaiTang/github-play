e05223b7ad7b83830217178fa8c193f9cfcbd461
Jingkai Tang <jingkaitang@gmail.com>
2016-02-08 15:56:49 +0800

    mv: feel life/point_or_day/hand/tell_government/work/work_government_after_life.py and government_or_first_world/year/early_world.py to small time

---
+++ government_or_first_world/year/early_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_part_and_problem(str_arg):
+    woman_or_able_day(str_arg)
+    print('case')
+
+def woman_or_able_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_part_and_problem('new_man')
---
+++ life/point_or_day/hand/tell_government/work/work_government_after_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_time_up_hand(str_arg):
+    bad_child(str_arg)
+    print('old_time_and_high_year')
+
+def bad_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_time_up_hand('bad_number_or_thing')
