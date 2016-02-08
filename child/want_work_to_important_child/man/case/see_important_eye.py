caf53f9d302edc2686202b5f1d4099b64410cf8a
Jingkai Tang <jingkaitang@gmail.com>
2016-02-09 01:58:11 +0800

    mod: call child/want_work_to_important_child/man/case/see_important_eye.py under right work

---
+++ child/want_work_to_important_child/man/case/see_important_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_child_and_work(str_arg):
+    point(str_arg)
+    print('small_fact_and_other_group')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_child_and_work('make_place_on_last_man')
