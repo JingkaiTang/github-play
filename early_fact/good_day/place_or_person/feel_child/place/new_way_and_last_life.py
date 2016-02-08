adaf89c6d7874d1c63e0d894a711ac050346cebf
Jingkai Tang <jingkaitang@gmail.com>
2016-02-08 21:14:43 +0800

    mod: try early_fact/good_day/place_or_person/feel_child/place/new_way_and_last_life.py by public year

---
+++ early_fact/good_day/place_or_person/feel_child/place/new_way_and_last_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    important_problem(str_arg)
+    print('other_week')
+
+def important_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('first_child_and_group')
