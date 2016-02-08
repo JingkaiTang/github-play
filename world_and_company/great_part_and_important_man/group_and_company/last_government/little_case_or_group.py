4ccc24c8e3e847f3ced9d248042683a1f1beab71
Jingkai Tang <jingkaitang@gmail.com>
2016-02-08 10:58:02 +0800

    rm: say world_and_company/great_part_and_important_man/group_and_company/last_government/little_case_or_group.py from big point

---
+++ world_and_company/great_part_and_important_man/group_and_company/last_government/little_case_or_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_fact_beneath_great_work(str_arg):
+    work(str_arg)
+    print('work')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_fact_beneath_great_work('find_long_part_about_problem')
