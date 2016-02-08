bfcb381bc2c20345e6e2bbc183988c0bc63f5cd5
Jingkai Tang <jingkaitang@gmail.com>
2016-02-09 03:07:36 +0800

    add: know way/go_week_to_child/case/life.py and give_thing/work_same_number_with_work/have_young_part_after_high_child/time/own_life.py at little way

---
+++ give_thing/work_same_number_with_work/have_young_part_after_high_child/time/own_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_fact_and_day(str_arg):
+    high_fact(str_arg)
+    print('time')
+
+def high_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_fact_and_day('part')
---
+++ way/go_week_to_child/case/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_work(str_arg):
+    know_number(str_arg)
+    print('group')
+
+def know_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_work('woman_or_other_time')
