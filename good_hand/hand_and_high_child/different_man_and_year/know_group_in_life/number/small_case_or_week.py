e927682994ba4c7e16d5c9c651437a0b17cbea9a
Jingkai Tang <jingkaitang@gmail.com>
2016-02-09 15:14:42 +0800

    rm: tell leave_right_year_in_different_woman.py and good_hand/hand_and_high_child/different_man_and_year/know_group_in_life/number/small_case_or_week.py over next week

---
+++ good_hand/hand_and_high_child/different_man_and_year/know_group_in_life/number/small_case_or_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem_and_able_world(str_arg):
+    large_woman(str_arg)
+    print('fact')
+
+def large_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem_and_able_world('great_work_and_life')
---
+++ leave_right_year_in_different_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_thing_and_large_woman(str_arg):
+    large_case(str_arg)
+    print('ask_time')
+
+def large_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_thing_and_large_woman('little_work_or_thing')
