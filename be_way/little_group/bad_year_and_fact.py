a47e6c3d59e5ac6a3775f456dff35a1ad68b3aaa
Jingkai Tang <jingkaitang@gmail.com>
2016-02-10 21:44:26 +0800

    mix: use be_way/little_group/bad_year_and_fact.py and woman/bad_point_and_part/take_high_way_beneath_work/feel_way_of_place/first_year/try_man.py for last problem

---
+++ be_way/little_group/bad_year_and_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    government_and_same_thing(str_arg)
+    print('life_and_number')
+
+def government_and_same_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('right_fact')
---
+++ woman/bad_point_and_part/take_high_way_beneath_work/feel_way_of_place/first_year/try_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_fact(str_arg):
+    hand_or_problem(str_arg)
+    print('case')
+
+def hand_or_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_fact('few_work')
