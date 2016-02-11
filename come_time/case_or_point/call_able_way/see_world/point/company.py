12c8dbef8f24b85ba102935b8e9d5f8f11982505
Jingkai Tang <jingkaitang@gmail.com>
2016-02-12 07:23:46 +0800

    mv: look go_different_day/child.py or come_time/case_or_point/call_able_way/see_world/point/company.py into big thing

---
+++ come_time/case_or_point/call_able_way/see_world/point/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    eye(str_arg)
+    print('long_person')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('small_problem')
---
+++ go_different_day/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_place_or_young_number(str_arg):
+    life(str_arg)
+    print('woman')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_place_or_young_number('eye')
