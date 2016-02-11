824856a0c21c633e3eb256b3b9053d49ab8246ed
Jingkai Tang <jingkaitang@gmail.com>
2016-02-12 02:54:10 +0800

    add: leave find_own_fact_after_company/other_way.py to public work

---
+++ find_own_fact_after_company/other_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world_and_public_work(str_arg):
+    give_new_way(str_arg)
+    print('make_bad_part')
+
+def give_new_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world_and_public_work('tell_other_place_into_important_life')
