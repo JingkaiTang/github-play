47e1cd4a5ab5ec193de85fbd383a1b7b55b9a981
Jingkai Tang <jingkaitang@gmail.com>
2016-02-12 21:06:56 +0800

    add: try great_man_or_fact/next_thing/eye/year.py by big child

---
+++ great_man_or_fact/next_thing/eye/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_group_and_own_woman(str_arg):
+    first_child(str_arg)
+    print('important_time')
+
+def first_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_group_and_own_woman('little_time_or_old_life')
