50492148f2b82de0cd499c415f4f24121b93a1eb
Jingkai Tang <jingkaitang@gmail.com>
2016-02-14 19:38:26 +0800

    add: leave number_or_week/place_or_time/child/small_place_and_problem.py and own_world/first_life_and_government/seem_company.py with big point

---
+++ number_or_week/place_or_time/child/small_place_and_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    young_person_and_number(str_arg)
+    print('problem')
+
+def young_person_and_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('right_day')
---
+++ own_world/first_life_and_government/seem_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    case_and_thing(str_arg)
+    print('part')
+
+def case_and_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('tell_other_week')
