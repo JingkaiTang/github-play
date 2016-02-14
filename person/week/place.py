8969a9c28ed5f3a7a616932976c1dfc3ea3bb7d7
Jingkai Tang <jingkaitang@gmail.com>
2016-02-14 15:54:57 +0800

    mix: go person/week/place.py at bad world

---
+++ person/week/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    know_point(str_arg)
+    print('person')
+
+def know_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('bad_way_and_person')
