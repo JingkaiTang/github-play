1e680d13f189e6f8edaa368edc6150d350e3e131
Jingkai Tang <jingkaitang@gmail.com>
2016-02-15 11:08:14 +0800

    rm: take group_and_work.py and get_new_man_on_new_fact.py about public child

---
+++ get_new_man_on_new_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_hand(str_arg):
+    long_point(str_arg)
+    print('great_company_or_company')
+
+def long_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_hand('eye')
---
+++ group_and_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_woman(str_arg):
+    big_eye(str_arg)
+    print('case')
+
+def big_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_woman('same_woman_and_world')
