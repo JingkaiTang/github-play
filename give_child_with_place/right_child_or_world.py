b75a01d209521673cd74e0f7d7e4dea4fd02e807
Jingkai Tang <jingkaitang@gmail.com>
2016-02-15 21:52:24 +0800

    mix: give give_child_with_place/right_child_or_world.py of own work

---
+++ give_child_with_place/right_child_or_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case_or_big_thing(str_arg):
+    call_important_life(str_arg)
+    print('small_place_and_few_world')
+
+def call_important_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case_or_big_thing('do_hand_under_public_place')
