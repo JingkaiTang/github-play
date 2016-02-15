37028a39554596e177eff3f1e7a4413250a2abcc
Jingkai Tang <jingkaitang@gmail.com>
2016-02-16 05:27:27 +0800

    add: get thing_and_first_work/child/able_work/new_eye.py above right place

---
+++ thing_and_first_work/child/able_work/new_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_hand(str_arg):
+    year(str_arg)
+    print('point_or_new_child')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_hand('say_person')
