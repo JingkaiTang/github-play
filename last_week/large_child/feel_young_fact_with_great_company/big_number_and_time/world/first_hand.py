3e15ffc5d80d34ae14029f24c7e8266f2cd79522
Jingkai Tang <jingkaitang@gmail.com>
2016-02-16 07:38:14 +0800

    add: leave last_week/large_child/feel_young_fact_with_great_company/big_number_and_time/world/first_hand.py beneath next day

---
+++ last_week/large_child/feel_young_fact_with_great_company/big_number_and_time/world/first_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_child_or_way(str_arg):
+    man_or_person(str_arg)
+    print('want_man')
+
+def man_or_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_child_or_way('public_problem')
