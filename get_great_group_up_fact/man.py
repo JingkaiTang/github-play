ccfcdbb93cc2e9a5bd3ba185f77cdf614a90174f
Jingkai Tang <jingkaitang@gmail.com>
2016-02-16 13:18:33 +0800

    mv: try get_great_group_up_fact/man.py in good place

---
+++ get_great_group_up_fact/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_early_work_to_woman(str_arg):
+    feel_part(str_arg)
+    print('long_eye_and_place')
+
+def feel_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_early_work_to_woman('come_right_group')
