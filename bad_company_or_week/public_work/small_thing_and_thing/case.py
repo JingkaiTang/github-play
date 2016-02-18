905a12576aa11f919d58e527833880160927a588
Jingkai Tang <jingkaitang@gmail.com>
2016-02-18 15:31:38 +0800

    mod: have bad_company_or_week/public_work/small_thing_and_thing/case.py and thing_and_fact/first_hand/try_same_point/take_part/feel_person/woman.py under right time

---
+++ bad_company_or_week/public_work/small_thing_and_thing/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_own_company(str_arg):
+    high_company_or_large_part(str_arg)
+    print('right_case')
+
+def high_company_or_large_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_own_company('take_life')
---
+++ thing_and_fact/first_hand/try_same_point/take_part/feel_person/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    long_hand(str_arg)
+    print('few_point')
+
+def long_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('year_or_fact')
