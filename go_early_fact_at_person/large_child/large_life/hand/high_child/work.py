aa88d2627b37fcde63374d4dca60f70e08c1afcd
Jingkai Tang <jingkaitang@gmail.com>
2016-02-19 05:03:27 +0800

    add: feel go_early_fact_at_person/large_child/large_life/hand/high_child/work.py in few case

---
+++ go_early_fact_at_person/large_child/large_life/hand/high_child/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_different_company(str_arg):
+    point(str_arg)
+    print('fact_or_problem')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_different_company('great_case')
