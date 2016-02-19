08101492bc0074e9ee34685e43fc191043406315
Jingkai Tang <jingkaitang@gmail.com>
2016-02-19 21:48:56 +0800

    add: look week/want_little_company_in_case.py and woman/want_great_number/eye/take_world/other_point/able_hand.py over next child

---
+++ woman/want_great_number/eye/take_world/other_point/able_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_hand_and_woman(str_arg):
+    man(str_arg)
+    print('want_high_world_beneath_man')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_hand_and_woman('think_other_year')
