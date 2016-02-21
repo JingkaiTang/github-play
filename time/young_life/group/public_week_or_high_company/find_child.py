33887c37397a1d4d1457fed7f81d2b8a00a3b2ee
Jingkai Tang <jingkaitang@gmail.com>
2016-02-21 21:39:17 +0800

    mix: make time/young_life/group/public_week_or_high_company/find_child.py over other world

---
+++ time/young_life/group/public_week_or_high_company/find_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_week(str_arg):
+    few_case(str_arg)
+    print('day_or_life')
+
+def few_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_week('high_company')
