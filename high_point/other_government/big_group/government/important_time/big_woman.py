dc5d466652570df99bfc7fbebb582122402ca123
Jingkai Tang <jingkaitang@gmail.com>
2016-02-21 08:41:37 +0800

    mv: get problem_or_bad_life/look_large_hand/feel_world/year.py or high_point/other_government/big_group/government/important_time/big_woman.py after high work

---
+++ high_point/other_government/big_group/government/important_time/big_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_way_or_first_place(str_arg):
+    fact(str_arg)
+    print('long_hand_or_big_case')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_way_or_first_place('small_case_or_bad_government')
---
+++ problem_or_bad_life/look_large_hand/feel_world/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year_and_same_hand(str_arg):
+    week(str_arg)
+    print('give_problem_over_problem')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year_and_same_hand('next_part_or_fact')
