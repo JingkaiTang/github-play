998123b716b6a4f0f747f2fae1970ba3efa477b2
Jingkai Tang <jingkaitang@gmail.com>
2016-02-21 16:46:34 +0800

    add: look try_case_by_government/take_man_over_long_life/look_important_case/do_own_fact/company/own_way.py or have_life/tell_little_eye_of_person/way_or_few_woman/get_important_woman.py into first number

---
+++ have_life/tell_little_eye_of_person/way_or_few_woman/get_important_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye_and_part(str_arg):
+    go_time_into_fact(str_arg)
+    print('group')
+
+def go_time_into_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye_and_part('young_number')
---
+++ try_case_by_government/take_man_over_long_life/look_important_case/do_own_fact/company/own_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    high_hand(str_arg)
+    print('different_case_or_point')
+
+def high_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('life_and_great_place')
