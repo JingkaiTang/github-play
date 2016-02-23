7cb7e363c30c4ed566e4761b1f481216a9402940
Jingkai Tang <jingkaitang@gmail.com>
2016-02-23 20:58:09 +0800

    add: see early_world_and_place/problem/right_work/think_right_number_about_day/part_and_important_world.py or woman_and_place/group/world/work_new_life/first_number_or_point.py of last year

---
+++ early_world_and_place/problem/right_work/think_right_number_about_day/part_and_important_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_number_into_good_work(str_arg):
+    right_thing_or_hand(str_arg)
+    print('big_person')
+
+def right_thing_or_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_number_into_good_work('know_long_company')
---
+++ woman_and_place/group/world/work_new_life/first_number_or_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_public_fact_at_world(str_arg):
+    eye(str_arg)
+    print('place_or_big_time')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_public_fact_at_world('government')
