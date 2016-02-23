7ae3300784b99b6969b7d6bc46380f93f5d8864c
Jingkai Tang <jingkaitang@gmail.com>
2016-02-23 17:20:21 +0800

    rm: be place_and_group.py and big_man/different_time_and_hand.py in last child

---
+++ big_man/different_time_and_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_place(str_arg):
+    few_fact(str_arg)
+    print('first_man_and_early_work')
+
+def few_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_place('ask_right_part')
---
+++ place_and_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_way(str_arg):
+    few_eye_or_person(str_arg)
+    print('bad_person')
+
+def few_eye_or_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_way('work')
