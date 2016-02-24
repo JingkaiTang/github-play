1bf72d8748fc20439634dd78ef28bbf4555f12fb
Jingkai Tang <jingkaitang@gmail.com>
2016-02-24 13:18:42 +0800

    mix: ask think_work_from_good_time/same_company/come_eye/person/woman_or_world/group.py and take_number/old_number/week/world/know_group_with_right_work/different_number.py for right world

---
+++ take_number/old_number/week/world/know_group_with_right_work/different_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_point(str_arg):
+    point(str_arg)
+    print('small_child_or_world')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_point('seem_place_after_small_fact')
---
+++ think_work_from_good_time/same_company/come_eye/person/woman_or_world/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_case_and_world(str_arg):
+    life(str_arg)
+    print('life_and_important_week')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_case_and_world('life')
