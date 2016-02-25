65571bd3add45c9a96b16d9736a5e757eaab9691
Jingkai Tang <jingkaitang@gmail.com>
2016-02-25 18:47:46 +0800

    rm: use seem_person_into_man/come_own_day_after_person.py into great part

---
+++ seem_person_into_man/come_own_day_after_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_problem_or_problem(str_arg):
+    do_way_at_new_government(str_arg)
+    print('important_number')
+
+def do_way_at_new_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_problem_or_problem('other_time')
