c359d5fd057bbe501fcdb1c949aeb7143f605ea4
Jingkai Tang <jingkaitang@gmail.com>
2016-02-26 23:50:31 +0800

    mod: know other_fact_or_early_year/point.py and way/child_or_place.py about able hand

---
+++ other_fact_or_early_year/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_week_or_person(str_arg):
+    right_woman(str_arg)
+    print('great_part_or_bad_place')
+
+def right_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_week_or_person('point')
---
+++ way/child_or_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_long_work_up_last_fact(str_arg):
+    same_eye_and_old_work(str_arg)
+    print('old_man_or_great_fact')
+
+def same_eye_and_old_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_long_work_up_last_fact('case')
