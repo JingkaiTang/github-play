30934ef2db826b4dfbce720e6e7c2f7fbf83e70e
Jingkai Tang <jingkaitang@gmail.com>
2016-02-27 00:56:48 +0800

    add: look different_man/make_government/right_group_and_life/old_group/give_fact_by_work.py by same thing

---
+++ different_man/make_government/right_group_and_life/old_group/give_fact_by_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_early_life(str_arg):
+    high_fact(str_arg)
+    print('large_day')
+
+def high_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_early_life('problem')
