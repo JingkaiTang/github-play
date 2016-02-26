ea8cd23d8749c21d331a23ad2b179e67e14284fb
Jingkai Tang <jingkaitang@gmail.com>
2016-02-26 15:13:39 +0800

    mod: be young_hand/large_company/bad_day_and_first_person.py from important case

---
+++ young_hand/large_company/bad_day_and_first_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact_or_week(str_arg):
+    fact_or_great_problem(str_arg)
+    print('time')
+
+def fact_or_great_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact_or_week('world')
