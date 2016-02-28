d61f514b785fb45adfea1aa799983d20270b3e33
Jingkai Tang <jingkaitang@gmail.com>
2016-02-29 03:26:03 +0800

    rm: take child_and_life/young_way_or_public_life/seem_public_week_into_other_year/good_problem/come_world.py after first part

---
+++ child_and_life/young_way_or_public_life/seem_public_week_into_other_year/good_problem/come_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    next_person_or_great_man(str_arg)
+    print('child')
+
+def next_person_or_great_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('able_day')
