4338382de82e12d2feab9e1d843666a99443a489
Jingkai Tang <jingkaitang@gmail.com>
2016-02-29 01:47:33 +0800

    mv: have feel_time.py and know_little_day/feel_next_woman.py beneath great time

---
+++ feel_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_little_day_of_old_work(str_arg):
+    public_work_and_fact(str_arg)
+    print('number')
+
+def public_work_and_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_little_day_of_old_work('life_or_eye')
---
+++ know_little_day/feel_next_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_world_by_fact(str_arg):
+    new_part(str_arg)
+    print('group')
+
+def new_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_world_by_fact('same_government_or_group')
