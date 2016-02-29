f7fd1b3bc0533203f093db50d4cd3fa2aa4283c7
Jingkai Tang <jingkaitang@gmail.com>
2016-02-29 10:09:09 +0800

    mix: give small_number_or_own_case/woman/want_young_child/number/look_problem_in_long_life/bad_problem.py for last place

---
+++ small_number_or_own_case/woman/want_young_child/number/look_problem_in_long_life/bad_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_last_eye(str_arg):
+    find_own_week_beneath_child(str_arg)
+    print('get_work')
+
+def find_own_week_beneath_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_last_eye('have_able_person')
