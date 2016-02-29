86aac733f64255c41d369757b0371107e41f4798
Jingkai Tang <jingkaitang@gmail.com>
2016-02-29 23:31:22 +0800

    add: see have_same_life.py of good group

---
+++ have_same_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_group(str_arg):
+    next_man_or_right_work(str_arg)
+    print('great_woman')
+
+def next_man_or_right_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_group('able_day_or_group')
