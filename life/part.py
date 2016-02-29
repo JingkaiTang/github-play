5e8b6460aedbcfe06e19da255588f25f92221320
Jingkai Tang <jingkaitang@gmail.com>
2016-03-01 07:35:28 +0800

    mod: work life/part.py up early child

---
+++ life/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    able_world(str_arg)
+    print('go_thing')
+
+def able_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('right_case')
