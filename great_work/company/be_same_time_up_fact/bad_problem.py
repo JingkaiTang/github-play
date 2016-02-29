92d05f89a124718bbe21e2094c7c4580cc339e3b
Jingkai Tang <jingkaitang@gmail.com>
2016-02-29 13:06:43 +0800

    add: get great_work/company/be_same_time_up_fact/bad_problem.py or problem/feel_new_point_of_small_life/work_same_fact/next_hand_or_year/fact/place.py on other way

---
+++ great_work/company/be_same_time_up_fact/bad_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_way_and_able_time(str_arg):
+    company(str_arg)
+    print('get_last_life')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_way_and_able_time('call_public_group')
---
+++ problem/feel_new_point_of_small_life/work_same_fact/next_hand_or_year/fact/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_year(str_arg):
+    next_group(str_arg)
+    print('few_time')
+
+def next_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_year('week')
