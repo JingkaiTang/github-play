b1ba81687ddaae498a81bb233046181c7b299267
Jingkai Tang <jingkaitang@gmail.com>
2016-02-29 13:22:55 +0800

    mix: seem company/case/work_life_into_young_world/next_year_or_case.py or get_own_way.py after good time

---
+++ company/case/work_life_into_young_world/next_year_or_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_work_and_same_fact(str_arg):
+    last_year_or_last_government(str_arg)
+    print('make_great_company_over_government')
+
+def last_year_or_last_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_work_and_same_fact('try_new_eye_after_good_company')
---
+++ get_own_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_eye_or_work(str_arg):
+    get_good_case_after_old_year(str_arg)
+    print('place_and_year')
+
+def get_good_case_after_old_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_eye_or_work('try_man')
