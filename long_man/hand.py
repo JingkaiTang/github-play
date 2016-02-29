2f68382a47b04b7257a860da2620ad9423f3b237
Jingkai Tang <jingkaitang@gmail.com>
2016-03-01 00:50:43 +0800

    mix: try long_man/hand.py over high way

---
+++ long_man/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    young_company(str_arg)
+    print('good_week')
+
+def young_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('try_little_year')
