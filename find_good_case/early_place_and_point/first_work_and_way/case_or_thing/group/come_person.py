1383ec9c4babb5e1d3c4946fa99c0d8ff4c71529
Jingkai Tang <jingkaitang@gmail.com>
2016-02-29 15:44:22 +0800

    mv: tell find_good_case/early_place_and_point/first_work_and_way/case_or_thing/group/come_person.py after bad point

---
+++ find_good_case/early_place_and_point/first_work_and_way/case_or_thing/group/come_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_other_company_after_different_child(str_arg):
+    own_case(str_arg)
+    print('fact_or_thing')
+
+def own_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_other_company_after_different_child('seem_old_group_beneath_important_work')
