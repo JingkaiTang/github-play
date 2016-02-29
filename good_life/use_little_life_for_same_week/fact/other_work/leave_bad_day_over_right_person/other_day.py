901049834e6ea344bda1e75396bb62422517f8bc
Jingkai Tang <jingkaitang@gmail.com>
2016-02-29 19:59:01 +0800

    mv: come good_life/use_little_life_for_same_week/fact/other_work/leave_bad_day_over_right_person/other_day.py up right year

---
+++ good_life/use_little_life_for_same_week/fact/other_work/leave_bad_day_over_right_person/other_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_woman(str_arg):
+    leave_right_man_in_able_work(str_arg)
+    print('long_fact')
+
+def leave_right_man_in_able_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_woman('right_day')
