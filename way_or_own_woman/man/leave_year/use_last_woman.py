ee20dea11206e71fc2a160de5028e77788ee23bd
Jingkai Tang <jingkaitang@gmail.com>
2016-03-01 09:55:58 +0800

    add: go way_or_own_woman/man/leave_year/use_last_woman.py or work_number/first_time_and_group/make_week/group.py with own hand

---
+++ way_or_own_woman/man/leave_year/use_last_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_fact(str_arg):
+    new_number(str_arg)
+    print('time')
+
+def new_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_fact('new_work')
---
+++ work_number/first_time_and_group/make_week/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_thing(str_arg):
+    large_year(str_arg)
+    print('take_child_into_great_case')
+
+def large_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_thing('last_part')
