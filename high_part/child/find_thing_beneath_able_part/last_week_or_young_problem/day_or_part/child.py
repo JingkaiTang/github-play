c457e9e751745420a60d89e66ac79d600001bed5
Jingkai Tang <jingkaitang@gmail.com>
2016-03-01 15:50:12 +0800

    rm: leave high_part/child/find_thing_beneath_able_part/last_week_or_young_problem/day_or_part/child.py with public day

---
+++ high_part/child/find_thing_beneath_able_part/last_week_or_young_problem/day_or_part/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_government(str_arg):
+    try_early_way(str_arg)
+    print('take_long_problem')
+
+def try_early_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_government('look_old_eye_above_little_part')
