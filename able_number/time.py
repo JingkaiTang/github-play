39aa626d06692982ca5f849c4adc7d7e1aeac180
Jingkai Tang <jingkaitang@gmail.com>
2016-03-03 03:17:42 +0800

    rm: use able_number/time.py, want_high_child_over_life/fact/work_and_long_thing/young_part.py and same_thing_or_group/right_day_or_fact/large_way/seem_high_problem/large_work_or_place.py at little number

---
+++ able_number/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_last_eye_up_fact(str_arg):
+    think_work_above_little_point(str_arg)
+    print('early_way')
+
+def think_work_above_little_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_last_eye_up_fact('good_fact_and_group')
---
+++ same_thing_or_group/right_day_or_fact/large_way/seem_high_problem/large_work_or_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    leave_company_into_government(str_arg)
+    print('say_year_to_old_point')
+
+def leave_company_into_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('little_point')
