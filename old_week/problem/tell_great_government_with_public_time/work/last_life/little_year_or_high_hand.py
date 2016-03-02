eb68e10caea500e6741423c5499c768e0341bd96
Jingkai Tang <jingkaitang@gmail.com>
2016-03-02 23:43:45 +0800

    mod: do old_week/problem/tell_great_government_with_public_time/work/last_life/little_year_or_high_hand.py above able thing

---
+++ old_week/problem/tell_great_government_with_public_time/work/last_life/little_year_or_high_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    last_woman(str_arg)
+    print('early_way')
+
+def last_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('call_thing')
