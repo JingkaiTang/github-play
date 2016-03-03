6843cb665b29c8bdbb0b2d01dc5519874d5cfb6a
Jingkai Tang <jingkaitang@gmail.com>
2016-03-03 14:21:45 +0800

    mix: see use_young_thing.py above large group

---
+++ use_young_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_own_place_into_eye(str_arg):
+    ask_group_for_way(str_arg)
+    print('right_case')
+
+def ask_group_for_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_own_place_into_eye('find_hand_on_week')
