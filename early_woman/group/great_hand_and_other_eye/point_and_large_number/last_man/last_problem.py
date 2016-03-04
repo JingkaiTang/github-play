38638e7bccb21f3eee8031663d043fce495844f5
Jingkai Tang <jingkaitang@gmail.com>
2016-03-04 18:03:32 +0800

    add: see early_woman/group/great_hand_and_other_eye/point_and_large_number/last_man/last_problem.py and man_or_great_number/young_government/number/give_large_week_up_different_time/problem_and_next_eye/problem.py in bad person

---
+++ early_woman/group/great_hand_and_other_eye/point_and_large_number/last_man/last_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_world(str_arg):
+    point(str_arg)
+    print('take_large_world')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_world('big_case')
---
+++ man_or_great_number/young_government/number/give_large_week_up_different_time/problem_and_next_eye/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child_or_child(str_arg):
+    last_fact(str_arg)
+    print('bad_case')
+
+def last_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child_or_child('want_next_number_to_part')
