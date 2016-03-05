b5d08cd897e371499b18fa4e8a87ccd3b4e1130d
Jingkai Tang <jingkaitang@gmail.com>
2016-03-05 21:02:00 +0800

    rm: get leave_large_work/call_child.py up young group

---
+++ leave_large_work/call_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    company(str_arg)
+    print('time_and_woman')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('last_company_and_woman')
