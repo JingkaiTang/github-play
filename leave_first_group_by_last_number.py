23e96239cbed70fda70b20ea90a96b78e1e4c2c9
Jingkai Tang <jingkaitang@gmail.com>
2016-03-05 20:05:40 +0800

    rm: have leave_first_group_by_last_number.py above great year

---
+++ leave_first_group_by_last_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_government(str_arg):
+    new_part_or_way(str_arg)
+    print('small_place_and_high_case')
+
+def new_part_or_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_government('same_part')
