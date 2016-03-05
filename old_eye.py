d5bd7a64e6fb4a533b5ec9edeb9b505179ee1b8a
Jingkai Tang <jingkaitang@gmail.com>
2016-03-05 23:53:10 +0800

    mix: get old_eye.py with young place

---
+++ old_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_woman_in_same_government(str_arg):
+    world_and_large_child(str_arg)
+    print('group')
+
+def world_and_large_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_woman_in_same_government('use_number_of_other_life')
