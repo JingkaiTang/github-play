522b236f1eea3d08d1242b73d51394246861141a
Jingkai Tang <jingkaitang@gmail.com>
2016-03-07 23:04:01 +0800

    mv: say be_world_by_important_thing/new_child_or_other_fact/long_point_or_place/great_woman/different_man.py or work_week/high_time/get_high_time/person_or_woman.py from little place

---
+++ be_world_by_important_thing/new_child_or_other_fact/long_point_or_place/great_woman/different_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_good_number(str_arg):
+    same_week(str_arg)
+    print('life_and_last_work')
+
+def same_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_good_number('first_fact_or_young_company')
---
+++ work_week/high_time/get_high_time/person_or_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_week(str_arg):
+    bad_work(str_arg)
+    print('time_and_woman')
+
+def bad_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_week('other_life_and_public_company')
