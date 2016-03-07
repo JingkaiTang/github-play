78d773e5a43a77756e0530c963df2deb684c57a5
Jingkai Tang <jingkaitang@gmail.com>
2016-03-07 13:02:08 +0800

    mv: seem first_woman_or_different_man/thing/woman.py by great year

---
+++ first_woman_or_different_man/thing/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    number_or_first_life(str_arg)
+    print('tell_time_to_fact')
+
+def number_or_first_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('hand')
