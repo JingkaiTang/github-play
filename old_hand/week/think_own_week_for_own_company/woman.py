3fa8d177d06d9f823b6aa9344ab55514f8e2465a
Jingkai Tang <jingkaitang@gmail.com>
2016-03-08 17:16:17 +0800

    add: look old_hand/week/think_own_week_for_own_company/woman.py or week/long_number/say_high_part_under_thing/week_and_fact/hand_or_bad_eye/right_place.py to same way

---
+++ old_hand/week/think_own_week_for_own_company/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    woman_or_problem(str_arg)
+    print('company_and_different_child')
+
+def woman_or_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('own_problem')
---
+++ week/long_number/say_high_part_under_thing/week_and_fact/hand_or_bad_eye/right_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_old_person_in_first_man(str_arg):
+    be_problem(str_arg)
+    print('group_or_new_world')
+
+def be_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_old_person_in_first_man('little_way')
