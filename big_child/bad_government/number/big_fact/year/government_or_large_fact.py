baa9f78e149103f489af50cd2adc28732aeafaf4
Jingkai Tang <jingkaitang@gmail.com>
2016-03-08 16:05:29 +0800

    add: find big_child/bad_government/number/big_fact/year/government_or_large_fact.py under early fact

---
+++ big_child/bad_government/number/big_fact/year/government_or_large_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_fact(str_arg):
+    place_and_old_woman(str_arg)
+    print('time')
+
+def place_and_old_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_fact('few_child_and_number')
