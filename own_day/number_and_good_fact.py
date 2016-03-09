dd780a557e28e4ecae068921097e4cdbad4c9b63
Jingkai Tang <jingkaitang@gmail.com>
2016-03-10 04:52:23 +0800

    mix: try own_day/number_and_good_fact.py by different time

---
+++ own_day/number_and_good_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_great_day(str_arg):
+    say_fact_at_own_world(str_arg)
+    print('know_point_under_time')
+
+def say_fact_at_own_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_great_day('big_thing')
