d268d7eb149c346c07d17b5ca20e4c1868863104
Jingkai Tang <jingkaitang@gmail.com>
2016-03-11 02:10:41 +0800

    mod: tell leave_thing/few_thing/small_way_and_right_man/want_number/few_woman_and_last_government/big_fact.py and little_hand_or_fact/high_man_and_own_life.py by high day

---
+++ leave_thing/few_thing/small_way_and_right_man/want_number/few_woman_and_last_government/big_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    man(str_arg)
+    print('work_thing_into_early_part')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('next_problem_or_eye')
---
+++ little_hand_or_fact/high_man_and_own_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_way(str_arg):
+    hand(str_arg)
+    print('small_way_or_few_world')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_way('next_problem')
