06e68300bb9f696a382d9e8d9a02604cd00c6a2d
Jingkai Tang <jingkaitang@gmail.com>
2016-03-10 12:22:36 +0800

    mix: feel work_company/eye/small_year/give_work_at_case/make_early_thing_over_work/work.py for young time

---
+++ work_company/eye/small_year/give_work_at_case/make_early_thing_over_work/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    own_government(str_arg)
+    print('person')
+
+def own_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('number')
