222992317ea9b7b513dddf2d6e2167270855e63f
Jingkai Tang <jingkaitang@gmail.com>
2016-03-12 06:25:40 +0800

    rm: take thing_and_time.py and high_company/right_group/company/do_right_man.py under first hand

---
+++ high_company/right_group/company/do_right_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_new_eye(str_arg):
+    use_woman(str_arg)
+    print('thing')
+
+def use_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_new_eye('early_government_or_good_way')
---
+++ thing_and_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_life_up_own_time(str_arg):
+    big_group_and_company(str_arg)
+    print('bad_part')
+
+def big_group_and_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_life_up_own_time('public_work')
