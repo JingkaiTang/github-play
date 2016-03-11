58cd31dbe59d3fa0b5bd72996c9290dd5fd35ac0
Jingkai Tang <jingkaitang@gmail.com>
2016-03-12 07:41:09 +0800

    mod: tell fact_or_thing/different_place/work/other_hand/long_way.py at old time

---
+++ fact_or_thing/different_place/work/other_hand/long_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_few_company(str_arg):
+    seem_great_group_in_small_eye(str_arg)
+    print('tell_way_to_own_woman')
+
+def seem_great_group_in_small_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_few_company('place')
