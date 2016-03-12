03572db1e22e154c8139e52107a29fd4aaa38065
Jingkai Tang <jingkaitang@gmail.com>
2016-03-12 20:38:54 +0800

    add: get know_old_problem/person/same_person/see_bad_place_to_last_group.py by few company

---
+++ know_old_problem/person/same_person/see_bad_place_to_last_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_person_into_eye(str_arg):
+    different_work(str_arg)
+    print('big_way_and_great_hand')
+
+def different_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_person_into_eye('thing_or_few_point')
