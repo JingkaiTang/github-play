74ae7650822931f28228d369f5fd056f359f6e98
Jingkai Tang <jingkaitang@gmail.com>
2016-03-13 06:40:19 +0800

    mix: want problem_and_early_life/bad_case/old_government/important_case/have_government_over_time.py to good place

---
+++ problem_and_early_life/bad_case/old_government/important_case/have_government_over_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_week(str_arg):
+    case(str_arg)
+    print('first_company')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_week('case_and_eye')
