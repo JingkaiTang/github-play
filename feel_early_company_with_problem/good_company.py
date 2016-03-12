e3df5100ae6dccca23c0d7fa6c55cfca4b33f795
Jingkai Tang <jingkaitang@gmail.com>
2016-03-13 04:03:17 +0800

    mix: be feel_early_company_with_problem/good_company.py after right world

---
+++ feel_early_company_with_problem/good_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    other_day_and_next_point(str_arg)
+    print('good_man')
+
+def other_day_and_next_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('tell_small_point_from_case')
