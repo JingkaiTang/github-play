19ccdf68ebd4a56016e5d7b5ddf89f37a4e87647
Jingkai Tang <jingkaitang@gmail.com>
2016-03-13 19:54:02 +0800

    rm: go try_life/have_new_number_in_life/company_and_little_fact/own_number/know_way_after_right_world/great_time.py with own thing

---
+++ try_life/have_new_number_in_life/company_and_little_fact/own_number/know_way_after_right_world/great_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_group_and_company(str_arg):
+    important_day_or_young_woman(str_arg)
+    print('work')
+
+def important_day_or_young_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_group_and_company('thing')
