a707fbdbb76329c73f37195b28c3c3815d3ea377
Jingkai Tang <jingkaitang@gmail.com>
2016-03-14 07:23:26 +0800

    mod: try say_young_point/take_same_government_over_important_person.py and early_child/little_work_and_large_person/little_problem_and_great_time.py beneath small hand

---
+++ early_child/little_work_and_large_person/little_problem_and_great_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_person(str_arg):
+    government_or_government(str_arg)
+    print('little_government')
+
+def government_or_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_person('number')
---
+++ say_young_point/take_same_government_over_important_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    number(str_arg)
+    print('man_or_work')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('do_public_child_at_few_fact')
