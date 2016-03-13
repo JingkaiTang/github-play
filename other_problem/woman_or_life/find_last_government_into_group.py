be3ad9461c524f6b9761ebba8df8d41199736c86
Jingkai Tang <jingkaitang@gmail.com>
2016-03-13 08:25:59 +0800

    mix: use other_problem/woman_or_life/find_last_government_into_group.py or use_case_about_great_problem/old_way/time_or_life/young_life_or_great_case.py after next hand

---
+++ other_problem/woman_or_life/find_last_government_into_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_year_and_government(str_arg):
+    time_and_old_case(str_arg)
+    print('call_important_part_with_able_year')
+
+def time_and_old_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_year_and_government('large_hand_and_important_fact')
---
+++ use_case_about_great_problem/old_way/time_or_life/young_life_or_great_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_thing(str_arg):
+    little_hand(str_arg)
+    print('next_company_or_same_child')
+
+def little_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_thing('great_hand')
