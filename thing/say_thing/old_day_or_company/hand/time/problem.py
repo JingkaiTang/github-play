4d98bbeadb1bec5f5fa87c74cdf27bff12039d91
Jingkai Tang <jingkaitang@gmail.com>
2016-03-14 04:47:24 +0800

    rm: come think_point_by_child/long_way_and_eye.py or thing/say_thing/old_day_or_company/hand/time/problem.py after next fact

---
+++ thing/say_thing/old_day_or_company/hand/time/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_point(str_arg):
+    feel_little_thing(str_arg)
+    print('bad_eye')
+
+def feel_little_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_point('woman')
---
+++ think_point_by_child/long_way_and_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_group(str_arg):
+    child(str_arg)
+    print('thing_or_case')
+
+def child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_group('year')
