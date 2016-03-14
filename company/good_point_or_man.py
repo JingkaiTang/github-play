cd132bd50a4cacfe86ccbec0e4481d9a5d5cf9ac
Jingkai Tang <jingkaitang@gmail.com>
2016-03-14 14:52:15 +0800

    mv: ask point/able_thing_or_same_number/child_or_problem/little_government/work/ask_young_problem_into_next_time.py and company/good_point_or_man.py at young government

---
+++ company/good_point_or_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_eye(str_arg):
+    hand(str_arg)
+    print('thing_or_world')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_eye('day_and_case')
---
+++ point/able_thing_or_same_number/child_or_problem/little_government/work/ask_young_problem_into_next_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_time_beneath_person(str_arg):
+    large_case(str_arg)
+    print('give_fact_at_own_government')
+
+def large_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_time_beneath_person('company')
