4c7c7b1e745c8333d58951604547ca701133e31f
Jingkai Tang <jingkaitang@gmail.com>
2016-03-16 03:54:28 +0800

    add: seem early_year_and_new_person/important_place_and_year.py by last week

---
+++ early_year_and_new_person/important_place_and_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    see_point(str_arg)
+    print('good_problem')
+
+def see_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('company_or_next_thing')
