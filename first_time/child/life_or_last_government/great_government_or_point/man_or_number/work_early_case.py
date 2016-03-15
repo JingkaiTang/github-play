4fbc8c71538c8390cee101479343019a65d97a6b
Jingkai Tang <jingkaitang@gmail.com>
2016-03-15 14:42:20 +0800

    mv: ask first_time/child/life_or_last_government/great_government_or_point/man_or_number/work_early_case.py, person/great_thing/find_day/life_and_large_child/person.py and think_public_man.py to great work

---
+++ first_time/child/life_or_last_government/great_government_or_point/man_or_number/work_early_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_day(str_arg):
+    go_first_group(str_arg)
+    print('next_part')
+
+def go_first_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_day('important_company')
---
+++ person/great_thing/find_day/life_and_large_child/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman_and_life(str_arg):
+    tell_hand(str_arg)
+    print('first_part')
+
+def tell_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman_and_life('bad_hand')
