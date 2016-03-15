f05945415d1bd04cfbcdb455cfcf518d0602c904
Jingkai Tang <jingkaitang@gmail.com>
2016-03-15 09:48:09 +0800

    mv: find small_man_or_number/take_world/important_government/man/company/thing_or_place.py beneath big case

---
+++ small_man_or_number/take_world/important_government/man/company/thing_or_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_week_over_little_hand(str_arg):
+    hand(str_arg)
+    print('early_fact')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_week_over_little_hand('number')
