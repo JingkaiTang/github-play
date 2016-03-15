aa07eeb4651b62da3edc913dd5744047085cff37
Jingkai Tang <jingkaitang@gmail.com>
2016-03-15 11:25:46 +0800

    rm: see time_or_day.py about different year

---
+++ time_or_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    time(str_arg)
+    print('call_next_fact_up_large_fact')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('fact')
