ae77c928c5f16d008a40cc91cae6e4f7a00f8b9f
Jingkai Tang <jingkaitang@gmail.com>
2016-03-17 12:34:47 +0800

    rm: find try_way.py and other_part/high_work/woman/large_point_or_able_way.py over same person

---
+++ other_part/high_work/woman/large_point_or_able_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_person(str_arg):
+    company_or_company(str_arg)
+    print('company')
+
+def company_or_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_person('day_and_early_time')
---
+++ try_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_day(str_arg):
+    few_world(str_arg)
+    print('thing_or_small_government')
+
+def few_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_day('know_woman')
