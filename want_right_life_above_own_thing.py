04bbe189d67805814ac4d8ada3b6eb08c81be90b
Jingkai Tang <jingkaitang@gmail.com>
2016-03-18 11:46:25 +0800

    add: come want_right_life_above_own_thing.py with little work

---
+++ want_right_life_above_own_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_man(str_arg):
+    feel_public_company(str_arg)
+    print('case_and_point')
+
+def feel_public_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_man('case')
