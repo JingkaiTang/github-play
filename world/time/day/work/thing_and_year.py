ba6ba74655700bfc4b1f33e5f03bb49e08264475
Jingkai Tang <jingkaitang@gmail.com>
2016-03-19 14:04:22 +0800

    mv: give world/time/day/work/thing_and_year.py in early fact

---
+++ world/time/day/work/thing_and_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_group(str_arg):
+    point(str_arg)
+    print('different_way')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_group('different_way')
