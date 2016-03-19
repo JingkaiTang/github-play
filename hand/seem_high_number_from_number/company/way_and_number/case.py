c98ac519cb6105fb8187114bdd3f9ee90535dc46
Jingkai Tang <jingkaitang@gmail.com>
2016-03-19 14:03:52 +0800

    rm: have hand/seem_high_number_from_number/company/way_and_number/case.py in great eye

---
+++ hand/seem_high_number_from_number/company/way_and_number/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    take_man(str_arg)
+    print('bad_place_or_company')
+
+def take_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('public_woman_and_problem')
