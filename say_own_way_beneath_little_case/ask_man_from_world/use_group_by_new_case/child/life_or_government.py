02a997a13573af8ca8f6d8e484487fcd28700267
Jingkai Tang <jingkaitang@gmail.com>
2016-03-19 09:34:00 +0800

    rm: do say_own_way_beneath_little_case/ask_man_from_world/use_group_by_new_case/child/life_or_government.py, way/good_part/part_or_year.py or use_last_woman.py from high child

---
+++ say_own_way_beneath_little_case/ask_man_from_world/use_group_by_new_case/child/life_or_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_fact(str_arg):
+    able_life(str_arg)
+    print('go_government')
+
+def able_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_fact('young_day')
---
+++ use_last_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_life(str_arg):
+    government(str_arg)
+    print('be_public_case')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_life('week')
