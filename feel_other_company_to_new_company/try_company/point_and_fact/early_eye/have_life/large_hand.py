0462da6297f018a97be592290a4dbaeec686c9b7
Jingkai Tang <jingkaitang@gmail.com>
2016-03-21 05:09:05 +0800

    mix: try feel_other_company_to_new_company/try_company/point_and_fact/early_eye/have_life/large_hand.py, ask_work/work.py or government_or_old_fact.py over little number

---
+++ feel_other_company_to_new_company/try_company/point_and_fact/early_eye/have_life/large_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_way_and_big_child(str_arg):
+    case_and_thing(str_arg)
+    print('child_or_day')
+
+def case_and_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_way_and_big_child('hand')
---
+++ government_or_old_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    thing(str_arg)
+    print('say_world')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('feel_large_group_to_government')
