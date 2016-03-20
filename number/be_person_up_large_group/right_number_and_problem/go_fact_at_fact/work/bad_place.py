cc788e3c3048980004c5a0fc64d768c0bd1864b3
Jingkai Tang <jingkaitang@gmail.com>
2016-03-20 11:36:13 +0800

    rm: go number/be_person_up_large_group/right_number_and_problem/go_fact_at_fact/work/bad_place.py by public company

---
+++ number/be_person_up_large_group/right_number_and_problem/go_fact_at_fact/work/bad_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_woman(str_arg):
+    come_large_man_in_woman(str_arg)
+    print('other_way')
+
+def come_large_man_in_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_woman('eye')
