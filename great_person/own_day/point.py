c81dadc73d0c78f657ce25a7dfa14769263d1d1c
Jingkai Tang <jingkaitang@gmail.com>
2016-03-20 12:02:40 +0800

    mix: find great_person/own_day/point.py about same person

---
+++ great_person/own_day/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    case(str_arg)
+    print('different_child')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('way')
