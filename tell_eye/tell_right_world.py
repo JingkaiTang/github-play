a92fefdedea3f54ab7e7cea14fa4563d3783a92f
Jingkai Tang <jingkaitang@gmail.com>
2016-03-21 21:07:20 +0800

    mod: feel tell_eye/tell_right_world.py with big year

---
+++ tell_eye/tell_right_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    tell_big_way_for_large_way(str_arg)
+    print('important_problem_and_different_place')
+
+def tell_big_way_for_large_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('high_hand_or_early_eye')
