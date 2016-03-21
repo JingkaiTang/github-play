fb9b4249a36b1bd765d551a250ffe7a2f210f89a
Jingkai Tang <jingkaitang@gmail.com>
2016-03-21 08:41:20 +0800

    mix: call group.py and new_part/early_point_or_woman/seem_child_in_eye/work_person.py with right case

--- group.py
+++ group.py
@@ -1,12 +1,12 @@
 
 #! /usr/bin/env python
 
-def come_right_way_after_child(str_arg):
-    own_year_or_long_world(str_arg)
-    print('come_way_beneath_different_group')
+def bad_part(str_arg):
+    child(str_arg)
+    print('first_world')
 
-def own_year_or_long_world(str_arg):
+def child(str_arg):
     print(str_arg)
 
 if __name__ == '__main__':
-    come_right_way_after_child('man')
+    bad_part('think_life')
---
+++ new_part/early_point_or_woman/seem_child_in_eye/work_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_man(str_arg):
+    right_part_and_number(str_arg)
+    print('look_same_woman')
+
+def right_part_and_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_man('great_fact_and_large_man')
