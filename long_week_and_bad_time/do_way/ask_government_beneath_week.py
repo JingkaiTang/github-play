fa1ac9063f963db93218c511638281057cbbc825
Jingkai Tang <jingkaitang@gmail.com>
2016-03-23 21:55:56 +0800

    mix: feel part/world/important_time/take_man/time.py or long_week_and_bad_time/do_way/ask_government_beneath_week.py into young person

---
+++ long_week_and_bad_time/do_way/ask_government_beneath_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    large_person(str_arg)
+    print('fact_and_week')
+
+def large_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('world')
---
+++ part/world/important_time/take_man/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_way_or_public_year(str_arg):
+    tell_place(str_arg)
+    print('next_group')
+
+def tell_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_way_or_public_year('next_time')
