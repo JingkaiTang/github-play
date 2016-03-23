98ce7f9d4a941b7ce0bc5fd2e547a4435f5cacb9
Jingkai Tang <jingkaitang@gmail.com>
2016-03-23 13:18:32 +0800

    rm: tell own_eye_or_early_case/group_and_last_problem/large_person_and_person/be_next_child/important_case/great_woman.py into other company

---
+++ own_eye_or_early_case/group_and_last_problem/large_person_and_person/be_next_child/important_case/great_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    time_or_public_woman(str_arg)
+    print('see_own_company_by_large_year')
+
+def time_or_public_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('early_problem')
