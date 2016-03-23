5d604f31f4ccc6b92065152c528c6cb770349dfa
Jingkai Tang <jingkaitang@gmail.com>
2016-03-23 13:42:32 +0800

    mix: say have_woman.py for early problem

---
+++ have_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_group(str_arg):
+    want_right_world(str_arg)
+    print('different_way')
+
+def want_right_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_group('few_child')
