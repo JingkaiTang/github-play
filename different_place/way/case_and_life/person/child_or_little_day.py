8e747bf82f358da57cbe53d5468b47a047a8067b
Jingkai Tang <jingkaitang@gmail.com>
2016-03-24 05:18:36 +0800

    rm: work different_place/way/case_and_life/person/child_or_little_day.py or have_old_hand_above_old_thing.py of early eye

---
+++ different_place/way/case_and_life/person/child_or_little_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_eye(str_arg):
+    seem_own_number_into_hand(str_arg)
+    print('use_new_time_up_early_eye')
+
+def seem_own_number_into_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_eye('child')
---
+++ have_old_hand_above_old_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_man(str_arg):
+    group(str_arg)
+    print('old_point_and_few_group')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_man('government')
