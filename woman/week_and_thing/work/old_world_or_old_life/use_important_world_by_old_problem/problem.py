a08eaa1a16c357b5864b0edba4c3685fe2033aad
Jingkai Tang <jingkaitang@gmail.com>
2016-03-23 17:52:57 +0800

    mix: use woman/week_and_thing/work/old_world_or_old_life/use_important_world_by_old_problem/problem.py from big number

---
+++ woman/week_and_thing/work/old_world_or_old_life/use_important_world_by_old_problem/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_young_way(str_arg):
+    last_government_and_able_fact(str_arg)
+    print('point_and_important_number')
+
+def last_government_and_able_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_young_way('problem_and_few_work')
