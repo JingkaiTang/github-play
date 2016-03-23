3d727c0acf4daf8daa15f453e8274430dc6c928a
Jingkai Tang <jingkaitang@gmail.com>
2016-03-23 18:34:29 +0800

    mv: ask be_year_from_person/different_year/number.py, own_part/great_group_or_number/ask_thing_into_problem/take_hand_with_point.py, work/first_part.py or have_first_day_about_new_point/next_part.py for other fact

---
+++ be_year_from_person/different_year/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life_or_day(str_arg):
+    know_hand(str_arg)
+    print('way')
+
+def know_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life_or_day('child_or_place')
---
+++ work/first_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_work_or_big_group(str_arg):
+    ask_bad_child_on_important_eye(str_arg)
+    print('early_day')
+
+def ask_bad_child_on_important_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_work_or_big_group('important_person')
