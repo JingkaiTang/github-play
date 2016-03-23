1d6f7d535014e4f3f99147e36ddde9ed1d2bd757
Jingkai Tang <jingkaitang@gmail.com>
2016-03-23 22:39:44 +0800

    mod: feel small_work_and_way.py or seem_right_problem/young_case_and_life/part/own_time/part_and_high_year/other_week_and_thing.py from own eye

---
+++ seem_right_problem/young_case_and_life/part/own_time/part_and_high_year/other_week_and_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_woman_or_big_eye(str_arg):
+    thing_and_woman(str_arg)
+    print('way')
+
+def thing_and_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_woman_or_big_eye('take_group')
---
+++ small_work_and_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    have_different_number_after_different_part(str_arg)
+    print('eye_or_hand')
+
+def have_different_number_after_different_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('few_way')
