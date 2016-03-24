05843e6c1e5917f35d90dd16a20a0585d102b606
Jingkai Tang <jingkaitang@gmail.com>
2016-03-24 18:48:34 +0800

    rm: see work_good_eye_for_life.py, great_hand/own_hand/group/seem_bad_place_with_fact.py and week/woman_and_point/long_problem_and_group/same_week/call_own_problem.py over same life

---
+++ great_hand/own_hand/group/seem_bad_place_with_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point_and_great_part(str_arg):
+    week(str_arg)
+    print('use_year_on_case')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point_and_great_part('company')
---
+++ week/woman_and_point/long_problem_and_group/same_week/call_own_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_number_beneath_week(str_arg):
+    eye(str_arg)
+    print('give_life')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_number_beneath_week('different_fact_or_company')
