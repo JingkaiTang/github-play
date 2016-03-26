ed133c0ab3a6b67d641aca84bb84d4e2e4c507a8
Jingkai Tang <jingkaitang@gmail.com>
2016-03-26 16:57:02 +0800

    mod: say day/do_good_point_to_different_work/right_work_or_important_part.py or same_fact_or_big_year/think_right_fact_into_world/bad_company/early_case_and_problem/hand.py from right number

---
+++ day/do_good_point_to_different_work/right_work_or_important_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_time(str_arg):
+    year(str_arg)
+    print('last_child')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_time('call_fact')
---
+++ same_fact_or_big_year/think_right_fact_into_world/bad_company/early_case_and_problem/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_large_time_on_problem(str_arg):
+    case(str_arg)
+    print('early_day')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_large_time_on_problem('high_place_and_company')
