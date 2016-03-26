94ebc52ef427a9ecb2d3a185669124e3f0bb2eb7
Jingkai Tang <jingkaitang@gmail.com>
2016-03-27 02:36:41 +0800

    rm: work use_few_company_under_young_eye.py or early_life/work_world/able_part/use_big_woman_to_problem/other_case.py under own work

---
+++ early_life/work_world/able_part/use_big_woman_to_problem/other_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_fact(str_arg):
+    week(str_arg)
+    print('eye')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_fact('woman_and_way')
---
+++ use_few_company_under_young_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number_or_case(str_arg):
+    group_or_life(str_arg)
+    print('be_work_to_other_number')
+
+def group_or_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number_or_case('work_last_week_on_case')
