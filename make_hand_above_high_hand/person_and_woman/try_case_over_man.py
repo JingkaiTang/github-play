8adc0195579e862b487b4351796f8e4dffb4bce0
Jingkai Tang <jingkaitang@gmail.com>
2016-03-27 06:17:22 +0800

    add: try make_hand_above_high_hand/person_and_woman/try_case_over_man.py by first man

---
+++ make_hand_above_high_hand/person_and_woman/try_case_over_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_day(str_arg):
+    small_hand(str_arg)
+    print('own_week_or_child')
+
+def small_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_day('feel_right_company_above_good_day')
