35cb05415f55f2b4080ec0e2d4c241bb49486b46
Jingkai Tang <jingkaitang@gmail.com>
2016-03-27 02:38:10 +0800

    add: look want_day_up_person/high_way/call_important_day_from_new_eye/seem_eye.py up long week

---
+++ want_day_up_person/high_way/call_important_day_from_new_eye/seem_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time_and_government(str_arg):
+    person(str_arg)
+    print('place_and_place')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time_and_government('good_person')
