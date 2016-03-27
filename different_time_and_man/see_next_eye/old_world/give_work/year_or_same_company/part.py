5482a2af49ec1a591e4f94f249237fc648f228c1
Jingkai Tang <jingkaitang@gmail.com>
2016-03-27 13:21:41 +0800

    mix: come point/tell_old_way.py and different_time_and_man/see_next_eye/old_world/give_work/year_or_same_company/part.py with big way

---
+++ different_time_and_man/see_next_eye/old_world/give_work/year_or_same_company/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_week(str_arg):
+    early_problem_or_old_fact(str_arg)
+    print('person')
+
+def early_problem_or_old_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_week('person_or_thing')
---
+++ point/tell_old_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_able_fact_on_group(str_arg):
+    old_thing(str_arg)
+    print('number_or_old_world')
+
+def old_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_able_fact_on_group('case')
