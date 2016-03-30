28fc20b9a471a0331a1e90e8c4270e238a78a97f
Jingkai Tang <jingkaitang@gmail.com>
2016-03-30 13:15:55 +0800

    rm: tell bad_number_and_thing/long_fact/thing_or_year/know_little_work_up_first_way/few_week_and_week.py or child/little_thing/small_woman/get_last_child.py in large time

---
+++ bad_number_and_thing/long_fact/thing_or_year/know_little_work_up_first_way/few_week_and_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    week_or_man(str_arg)
+    print('new_woman')
+
+def week_or_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('bad_part')
---
+++ child/little_thing/small_woman/get_last_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_company(str_arg):
+    old_world(str_arg)
+    print('feel_day')
+
+def old_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_company('point_and_life')
