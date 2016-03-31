37662720519d8c5119743e0857a7c11bddf8374d
Jingkai Tang <jingkaitang@gmail.com>
2016-04-01 04:45:15 +0800

    mix: leave child/public_part/first_work/ask_few_eye/life_or_hand.py after good number

---
+++ child/public_part/first_work/ask_few_eye/life_or_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_fact_or_same_company(str_arg):
+    part_and_bad_group(str_arg)
+    print('large_eye')
+
+def part_and_bad_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_fact_or_same_company('early_life_and_work')
