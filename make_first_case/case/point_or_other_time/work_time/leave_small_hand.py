c5ba2516134fc1e547b47205a50b3037456700ec
Jingkai Tang <jingkaitang@gmail.com>
2016-03-31 23:04:41 +0800

    mod: find own_world/child_and_world/man_or_way/day_and_great_case.py or make_first_case/case/point_or_other_time/work_time/leave_small_hand.py beneath early time

---
+++ make_first_case/case/point_or_other_time/work_time/leave_small_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    good_world(str_arg)
+    print('woman')
+
+def good_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('go_different_life')
---
+++ own_world/child_and_world/man_or_way/day_and_great_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_new_life_from_large_year(str_arg):
+    early_case_or_large_problem(str_arg)
+    print('give_new_person_above_government')
+
+def early_case_or_large_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_new_life_from_large_year('first_life')
