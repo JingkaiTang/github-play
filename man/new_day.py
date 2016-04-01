c7c322101810337f6f2cb8482b2a3f0c2254cb22
Jingkai Tang <jingkaitang@gmail.com>
2016-04-02 02:54:18 +0800

    rm: make man/new_day.py with public thing

---
+++ man/new_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_eye(str_arg):
+    have_new_thing(str_arg)
+    print('want_government')
+
+def have_new_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_eye('feel_problem')
