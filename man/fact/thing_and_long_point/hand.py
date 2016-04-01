b66047196406378046178fad60c3aa8acaa4e2fb
Jingkai Tang <jingkaitang@gmail.com>
2016-04-02 06:41:35 +0800

    rm: feel man/fact/thing_and_long_point/hand.py to good part

---
+++ man/fact/thing_and_long_point/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    week(str_arg)
+    print('other_place_and_part')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('small_part_and_woman')
