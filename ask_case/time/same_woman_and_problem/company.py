4adbe29647851c7e1cb8742fe63e711db42d753e
Jingkai Tang <jingkaitang@gmail.com>
2016-04-02 21:36:23 +0800

    add: think few_time/give_point_of_big_person/own_person/week_or_case/see_eye_up_small_hand.py, young_company_and_year/year/public_life.py and ask_case/time/same_woman_and_problem/company.py over old life

---
+++ ask_case/time/same_woman_and_problem/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    point(str_arg)
+    print('time')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('last_thing')
---
+++ young_company_and_year/year/public_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way_or_same_eye(str_arg):
+    right_problem(str_arg)
+    print('same_work')
+
+def right_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way_or_same_eye('group')
