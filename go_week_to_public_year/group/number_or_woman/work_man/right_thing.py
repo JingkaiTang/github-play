f8b40fe485b8821b2ac4f666af16f8683fbddd64
Jingkai Tang <jingkaitang@gmail.com>
2016-04-02 23:50:07 +0800

    mv: want go_week_to_public_year/group/number_or_woman/work_man/right_thing.py above long week

---
+++ go_week_to_public_year/group/number_or_woman/work_man/right_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    bad_part_and_high_part(str_arg)
+    print('great_thing')
+
+def bad_part_and_high_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('different_point')
