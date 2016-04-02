70767620ff8db47b04b073c2d3ec8e6600e1ea7e
Jingkai Tang <jingkaitang@gmail.com>
2016-04-02 22:57:15 +0800

    rm: have first_problem/large_work/thing/first_man_and_place/case/woman.py for different government

---
+++ first_problem/large_work/thing/first_man_and_place/case/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_case_above_small_company(str_arg):
+    eye(str_arg)
+    print('make_young_year_up_life')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_case_above_small_company('place')
