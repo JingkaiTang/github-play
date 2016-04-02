bd32c0b333b7117a52f28e5d2bf439150c095348
Jingkai Tang <jingkaitang@gmail.com>
2016-04-02 09:56:14 +0800

    add: take try_point/problem_and_time/public_company.py or little_year_or_point/find_government.py with high man

---
+++ little_year_or_point/find_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_hand(str_arg):
+    next_time(str_arg)
+    print('take_other_problem')
+
+def next_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_hand('make_year_on_little_time')
