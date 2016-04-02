6f384ffa2563e2140c3bbc9c077f7731b23fedb0
Jingkai Tang <jingkaitang@gmail.com>
2016-04-02 19:34:19 +0800

    mix: work part/hand_and_great_problem/able_place/want_small_thing_above_first_problem.py or go_new_group/make_own_man/small_time/number.py about different number

---
+++ go_new_group/make_own_man/small_time/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_day(str_arg):
+    person(str_arg)
+    print('small_time')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_day('child')
---
+++ part/hand_and_great_problem/able_place/want_small_thing_above_first_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_or_day(str_arg):
+    number_and_work(str_arg)
+    print('number')
+
+def number_and_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_or_day('take_young_time')
