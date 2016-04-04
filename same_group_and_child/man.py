e800112722fd04f177957ce6c7d4a4b2880fc7a2
Jingkai Tang <jingkaitang@gmail.com>
2016-04-05 03:04:43 +0800

    mv: have same_group_and_child/man.py beneath public place

---
+++ same_group_and_child/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    person_or_person(str_arg)
+    print('world_or_thing')
+
+def person_or_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('give_man')
