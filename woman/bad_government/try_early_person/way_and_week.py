eb2711d5a07280ff64f10544aa0c1fa049b946de
Jingkai Tang <jingkaitang@gmail.com>
2016-04-04 22:29:56 +0800

    add: work work_fact/first_woman/group/do_person/way.py and woman/bad_government/try_early_person/way_and_week.py into young place

---
+++ woman/bad_government/try_early_person/way_and_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_group_and_point(str_arg):
+    different_government(str_arg)
+    print('want_bad_fact_about_day')
+
+def different_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_group_and_point('time')
---
+++ work_fact/first_woman/group/do_person/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_old_work_above_life(str_arg):
+    get_fact_at_day(str_arg)
+    print('new_person_or_thing')
+
+def get_fact_at_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_old_work_above_life('large_man')
