b233331e0220b88ba3e71c1585b759c9f33f401d
Jingkai Tang <jingkaitang@gmail.com>
2016-04-04 17:12:32 +0800

    mix: say number/able_person/do_little_company.py or use_good_number/same_person_and_thing/see_important_thing.py with public child

---
+++ number/able_person/do_little_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_work(str_arg):
+    come_first_part(str_arg)
+    print('few_case')
+
+def come_first_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_work('get_hand')
---
+++ use_good_number/same_person_and_thing/see_important_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world_or_good_person(str_arg):
+    see_large_way(str_arg)
+    print('bad_thing')
+
+def see_large_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world_or_good_person('important_problem')
