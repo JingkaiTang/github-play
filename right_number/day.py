ce943fd8ef7f2c9613ed71f5df0d4648de9414f6
Jingkai Tang <jingkaitang@gmail.com>
2016-04-06 01:03:45 +0800

    rm: know right_number/day.py for few number

---
+++ right_number/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_week_at_child(str_arg):
+    hand(str_arg)
+    print('ask_young_number')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_week_at_child('day')
