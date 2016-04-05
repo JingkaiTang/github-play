260f3e627f6c60d4b994c82a9312e10311bb0906
Jingkai Tang <jingkaitang@gmail.com>
2016-04-05 16:46:00 +0800

    rm: make seem_part_to_hand/year/own_part_and_way/early_hand/make_place/woman.py or woman/life/work/company/right_man_or_important_person.py about old time

---
+++ seem_part_to_hand/year/own_part_and_way/early_hand/make_place/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    early_hand(str_arg)
+    print('fact')
+
+def early_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('own_child')
---
+++ woman/life/work/company/right_man_or_important_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    have_part(str_arg)
+    print('government')
+
+def have_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('think_week_over_early_company')
