0c661fb8d43f43ccc66fd0d2c24c39293b112f65
Jingkai Tang <jingkaitang@gmail.com>
2016-04-07 19:35:29 +0800

    mod: use able_eye/own_world_or_important_point.py and look_hand_under_own_work/good_group_or_case.py over right world

---
+++ able_eye/own_world_or_important_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_hand(str_arg):
+    next_hand_and_number(str_arg)
+    print('small_problem')
+
+def next_hand_and_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_hand('use_new_time_with_small_week')
---
+++ look_hand_under_own_work/good_group_or_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government_and_great_case(str_arg):
+    hand(str_arg)
+    print('tell_able_world')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government_and_great_case('thing_or_world')
