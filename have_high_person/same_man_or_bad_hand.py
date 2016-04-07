3f4500d8f94a4ef899628ec3e21848165f2bd569
Jingkai Tang <jingkaitang@gmail.com>
2016-04-07 09:26:49 +0800

    mix: work have_high_person/same_man_or_bad_hand.py beneath own case

---
+++ have_high_person/same_man_or_bad_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    different_work_or_right_part(str_arg)
+    print('other_group')
+
+def different_work_or_right_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('see_number')
