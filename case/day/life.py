7865e74b68e8a399c54ba4dfca5c56a9bafdd3d0
Jingkai Tang <jingkaitang@gmail.com>
2016-04-07 12:52:30 +0800

    rm: see case/day/life.py and point_or_child/part/early_thing_and_bad_day/do_life.py about old life

---
+++ case/day/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part_and_hand(str_arg):
+    group(str_arg)
+    print('small_eye_or_next_way')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part_and_hand('know_own_way')
---
+++ point_or_child/part/early_thing_and_bad_day/do_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_eye(str_arg):
+    seem_child(str_arg)
+    print('case')
+
+def seem_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_eye('time')
