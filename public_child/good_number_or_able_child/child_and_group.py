cf56a65e63b79747be23d1b9fb3826d065cd0dd2
Jingkai Tang <jingkaitang@gmail.com>
2016-04-07 23:34:06 +0800

    mix: know right_life/same_thing_or_work/early_fact_or_fact/feel_day/ask_eye_to_able_time/work_year.py and public_child/good_number_or_able_child/child_and_group.py at few company

---
+++ public_child/good_number_or_able_child/child_and_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_year_or_bad_time(str_arg):
+    large_child_or_young_work(str_arg)
+    print('long_hand')
+
+def large_child_or_young_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_year_or_bad_time('important_work')
---
+++ right_life/same_thing_or_work/early_fact_or_fact/feel_day/ask_eye_to_able_time/work_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_life_or_other_case(str_arg):
+    life(str_arg)
+    print('number')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_life_or_other_case('problem_or_own_world')
