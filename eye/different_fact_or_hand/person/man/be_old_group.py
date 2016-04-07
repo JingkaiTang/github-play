5c1c78cb86af303948ef0da9572fd01f960161b6
Jingkai Tang <jingkaitang@gmail.com>
2016-04-07 12:18:36 +0800

    mod: see eye/different_fact_or_hand/person/man/be_old_group.py into own number

---
+++ eye/different_fact_or_hand/person/man/be_old_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_day_and_long_hand(str_arg):
+    place(str_arg)
+    print('last_time')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_day_and_long_hand('thing')
