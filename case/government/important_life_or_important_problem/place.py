6393711fb6b2324d96cf7f028a214133cf39274e
Jingkai Tang <jingkaitang@gmail.com>
2016-04-08 11:23:01 +0800

    add: ask case/government/important_life_or_important_problem/place.py of first time

---
+++ case/government/important_life_or_important_problem/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    same_day_and_part(str_arg)
+    print('feel_week')
+
+def same_day_and_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('little_fact')
