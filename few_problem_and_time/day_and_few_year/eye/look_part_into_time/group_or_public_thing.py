a01f71612bb61bd0517a8f0a618464f42dbcad64
Jingkai Tang <jingkaitang@gmail.com>
2016-04-08 23:42:40 +0800

    mix: see other_way/problem_and_first_point/person/big_case/problem/few_day.py and few_problem_and_time/day_and_few_year/eye/look_part_into_time/group_or_public_thing.py above high case

---
+++ few_problem_and_time/day_and_few_year/eye/look_part_into_time/group_or_public_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_thing_up_day(str_arg):
+    bad_day(str_arg)
+    print('person')
+
+def bad_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_thing_up_day('little_world_and_life')
---
+++ other_way/problem_and_first_point/person/big_case/problem/few_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_thing_and_last_work(str_arg):
+    know_other_place_into_important_number(str_arg)
+    print('part')
+
+def know_other_place_into_important_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_thing_and_last_work('number')
