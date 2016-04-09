b9dd397b2e4158589202db3a9df914384df5c940
Jingkai Tang <jingkaitang@gmail.com>
2016-04-09 19:58:58 +0800

    rm: feel child/eye/thing/public_woman/tell_day_under_public_time/same_thing.py under last part

---
+++ child/eye/thing/public_woman/tell_day_under_public_time/same_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_part_after_long_man(str_arg):
+    point_and_few_place(str_arg)
+    print('new_point')
+
+def point_and_few_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_part_after_long_man('do_government_under_hand')
