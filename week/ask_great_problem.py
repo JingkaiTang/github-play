e44d68c615f016da1284a8b98d2893a93be21ed1
Jingkai Tang <jingkaitang@gmail.com>
2016-04-09 19:54:22 +0800

    add: have week/ask_great_problem.py with last case

---
+++ week/ask_great_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_problem(str_arg):
+    important_way(str_arg)
+    print('company')
+
+def important_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_problem('child')
