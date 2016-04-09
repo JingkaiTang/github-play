fe404f504abb07a246a00717e0f17e3a65b57866
Jingkai Tang <jingkaitang@gmail.com>
2016-04-10 06:40:09 +0800

    add: do eye/leave_case_about_public_way/look_thing_to_week/work_same_man.py by long place

---
+++ eye/leave_case_about_public_way/look_thing_to_week/work_same_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place_and_early_woman(str_arg):
+    man_and_new_place(str_arg)
+    print('government')
+
+def man_and_new_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place_and_early_woman('week')
