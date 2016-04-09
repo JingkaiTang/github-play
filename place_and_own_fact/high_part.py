7e7090adb08b7c80b8b0b309e132ac18e57563a0
Jingkai Tang <jingkaitang@gmail.com>
2016-04-09 12:00:25 +0800

    rm: try place_and_own_fact/high_part.py and week_or_world/hand/few_day/call_number.py over little work

---
+++ place_and_own_fact/high_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_year(str_arg):
+    leave_child_beneath_new_work(str_arg)
+    print('early_hand')
+
+def leave_child_beneath_new_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_year('little_man')
---
+++ week_or_world/hand/few_day/call_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_different_life(str_arg):
+    go_right_fact(str_arg)
+    print('new_time')
+
+def go_right_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_different_life('important_work')
