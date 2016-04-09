ebc5256524283fe7208a349e20131e45cd7b88af
Jingkai Tang <jingkaitang@gmail.com>
2016-04-10 00:42:38 +0800

    mix: see last_day/large_man_or_little_government/year/first_way_and_time.py after other group

---
+++ last_day/large_man_or_little_government/year/first_way_and_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_great_place_about_large_man(str_arg):
+    own_day_and_last_person(str_arg)
+    print('own_case')
+
+def own_day_and_last_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_great_place_about_large_man('use_work_of_next_way')
