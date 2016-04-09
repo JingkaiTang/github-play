3c5d54594e0b2cf5b5788a81a1f952069363c919
Jingkai Tang <jingkaitang@gmail.com>
2016-04-09 11:12:21 +0800

    mod: take feel_group/public_year_or_right_thing/be_public_group/have_other_year.py by large life

---
+++ feel_group/public_year_or_right_thing/be_public_group/have_other_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_number_to_man(str_arg):
+    woman(str_arg)
+    print('eye')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_number_to_man('group_or_other_part')
