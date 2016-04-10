472547de804a5cda69540066f9a6e9df13d16751
Jingkai Tang <jingkaitang@gmail.com>
2016-04-10 21:12:14 +0800

    add: call work_government_for_great_child/bad_place/bad_hand_or_man/young_government_and_high_week.py and world/use_week_over_point/say_work_on_young_day/big_point/seem_number/small_eye.py after few eye

---
+++ work_government_for_great_child/bad_place/bad_hand_or_man/young_government_and_high_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    early_fact_or_bad_year(str_arg)
+    print('fact')
+
+def early_fact_or_bad_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('first_case')
---
+++ world/use_week_over_point/say_work_on_young_day/big_point/seem_number/small_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_point(str_arg):
+    ask_place_about_fact(str_arg)
+    print('high_eye_or_company')
+
+def ask_place_about_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_point('public_number')
