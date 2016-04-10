f8769ed3e3ca34564b81e3f696c6267fc8a69700
Jingkai Tang <jingkaitang@gmail.com>
2016-04-10 13:15:59 +0800

    rm: do high_fact/last_world_or_year/do_time_at_group/eye/problem_or_important_fact.py beneath small work

---
+++ high_fact/last_world_or_year/do_time_at_group/eye/problem_or_important_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_place(str_arg):
+    government(str_arg)
+    print('way')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_place('early_place')
