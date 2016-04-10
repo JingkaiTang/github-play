f91ba22fcfb0bce9cb432d23eb89f8195857a654
Jingkai Tang <jingkaitang@gmail.com>
2016-04-11 07:07:38 +0800

    mv: call bad_number/know_year_at_time/life.py on own life

---
+++ bad_number/know_year_at_time/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    few_company(str_arg)
+    print('point')
+
+def few_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('have_company')
