e6035e68c13d68e1773129605cbbefc33c7f8f94
Jingkai Tang <jingkaitang@gmail.com>
2016-04-11 10:32:49 +0800

    mod: get child_and_young_place.py with small part

---
+++ child_and_young_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    man(str_arg)
+    print('group')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('small_week')
