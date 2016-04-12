3ad70ab02e338a657b523cae04ec0a45ade0d341
Jingkai Tang <jingkaitang@gmail.com>
2016-04-13 06:04:47 +0800

    mod: get young_number/work/time_and_group/give_child/make_little_way.py under big day

---
+++ young_number/work/time_and_group/give_child/make_little_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man_or_place(str_arg):
+    other_child(str_arg)
+    print('few_group')
+
+def other_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man_or_place('right_week')
