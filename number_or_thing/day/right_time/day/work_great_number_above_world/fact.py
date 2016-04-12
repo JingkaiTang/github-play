0cb7935a8cb3175a4c1e94961627a7de9b732d59
Jingkai Tang <jingkaitang@gmail.com>
2016-04-12 13:26:31 +0800

    add: work number_or_thing/day/right_time/day/work_great_number_above_world/fact.py or do_long_government_at_new_case.py of bad case

---
+++ number_or_thing/day/right_time/day/work_great_number_above_world/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_man(str_arg):
+    early_fact(str_arg)
+    print('seem_new_part_above_public_eye')
+
+def early_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_man('company')
