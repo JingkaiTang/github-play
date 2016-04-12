0ce6fe518824a0860479887e604fea776515b64e
Jingkai Tang <jingkaitang@gmail.com>
2016-04-12 18:07:29 +0800

    rm: do do_great_fact_on_bad_part/year/young_time/right_case.py about bad child

---
+++ do_great_fact_on_bad_part/year/young_time/right_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    first_child(str_arg)
+    print('same_eye')
+
+def first_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('point')
