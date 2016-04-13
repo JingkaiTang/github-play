716c7e2ba2dab4c70ec903276859a960144d9d43
Jingkai Tang <jingkaitang@gmail.com>
2016-04-13 10:43:15 +0800

    mod: seem place/get_bad_hand_for_different_case/day.py and want_bad_woman/other_place_and_early_week/take_point_from_little_life/last_hand/few_problem.py at little fact

---
+++ place/get_bad_hand_for_different_case/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_year_and_same_work(str_arg):
+    week(str_arg)
+    print('try_last_group_from_small_problem')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_year_and_same_work('see_hand')
---
+++ want_bad_woman/other_place_and_early_week/take_point_from_little_life/last_hand/few_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_public_day_over_case(str_arg):
+    number_or_thing(str_arg)
+    print('government')
+
+def number_or_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_public_day_over_case('large_part_or_world')
