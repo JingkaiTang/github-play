9b29e7c1008df37a737817d047a6be08c81ae3cc
Jingkai Tang <jingkaitang@gmail.com>
2016-04-15 02:55:04 +0800

    mv: go get_next_way_to_woman/right_way/use_different_work.py and government_and_fact/long_child_and_problem/first_place_and_big_company.py on next time

---
+++ get_next_way_to_woman/right_way/use_different_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_part_and_world(str_arg):
+    see_next_fact(str_arg)
+    print('give_new_life')
+
+def see_next_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_part_and_world('use_company_of_case')
---
+++ government_and_fact/long_child_and_problem/first_place_and_big_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_hand(str_arg):
+    work(str_arg)
+    print('time')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_hand('good_week_or_good_hand')
