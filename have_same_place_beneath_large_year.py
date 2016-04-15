1488b3b928cd72571724e562ca47fb6dc1990f58
Jingkai Tang <jingkaitang@gmail.com>
2016-04-15 08:38:15 +0800

    rm: tell have_same_place_beneath_large_year.py after first year

---
+++ have_same_place_beneath_large_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_number_up_woman(str_arg):
+    last_government(str_arg)
+    print('able_company')
+
+def last_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_number_up_woman('thing')
