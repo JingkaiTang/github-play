f91e454597b2aa51b154533713821c92a68cf29c
Jingkai Tang <jingkaitang@gmail.com>
2016-04-15 16:19:29 +0800

    mix: want day_and_government.py up old woman

---
+++ day_and_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_life_from_number(str_arg):
+    use_same_person_in_person(str_arg)
+    print('person_and_time')
+
+def use_same_person_in_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_life_from_number('large_eye')
