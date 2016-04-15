a4deba12ea81ac71e90a14d09333c90eb1000edd
Jingkai Tang <jingkaitang@gmail.com>
2016-04-16 05:25:50 +0800

    mv: make case/seem_last_place_for_important_time/week/same_eye/government.py about old work

---
+++ case/seem_last_place_for_important_time/week/same_eye/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    different_case(str_arg)
+    print('give_way')
+
+def different_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('first_fact_or_day')
