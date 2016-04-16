d578de7ede52e5e9e44d108974f49a59e0eacd39
Jingkai Tang <jingkaitang@gmail.com>
2016-04-16 09:27:17 +0800

    mod: make fact/work_fact_under_own_group/company.py and seem_problem.py into next thing

---
+++ fact/work_fact_under_own_group/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_point(str_arg):
+    take_government_at_way(str_arg)
+    print('go_life_for_old_point')
+
+def take_government_at_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_point('person')
---
+++ seem_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    important_group_or_woman(str_arg)
+    print('come_man')
+
+def important_group_or_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('great_fact_or_old_number')
