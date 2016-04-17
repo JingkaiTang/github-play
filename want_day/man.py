13f1cb31ea092351ee646b237a748c3d775fc7cc
Jingkai Tang <jingkaitang@gmail.com>
2016-04-18 07:35:26 +0800

    rm: ask want_day/man.py of right company

---
+++ want_day/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way_or_day(str_arg):
+    first_way(str_arg)
+    print('year')
+
+def first_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way_or_day('good_world_and_right_woman')
