d792c74956743a6c20f0b98e93638925f3f85885
Jingkai Tang <jingkaitang@gmail.com>
2016-04-18 21:34:13 +0800

    mod: leave life_and_young_place/hand/able_life/last_group_and_other_hand/small_year.py and have_public_company/be_man_of_able_life/first_group_or_group/thing/year.py after great government

---
+++ have_public_company/be_man_of_able_life/first_group_or_group/thing/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year_and_year(str_arg):
+    same_part(str_arg)
+    print('eye')
+
+def same_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year_and_year('long_time')
---
+++ life_and_young_place/hand/able_life/last_group_and_other_hand/small_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_way(str_arg):
+    week(str_arg)
+    print('able_part')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_way('few_world')
