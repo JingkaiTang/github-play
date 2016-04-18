b5966b5226ce607821170d32c11e0f941c84fd7e
Jingkai Tang <jingkaitang@gmail.com>
2016-04-18 12:48:28 +0800

    mod: use long_woman/other_woman/time/tell_work/get_time/know_week_for_same_work.py and take_large_part/high_day_or_important_week/large_hand.py of public way

---
+++ long_woman/other_woman/time/tell_work/get_time/know_week_for_same_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_person(str_arg):
+    great_life_or_man(str_arg)
+    print('case_or_person')
+
+def great_life_or_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_person('old_part')
---
+++ take_large_part/high_day_or_important_week/large_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_man_or_government(str_arg):
+    use_government_with_old_time(str_arg)
+    print('large_woman')
+
+def use_government_with_old_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_man_or_government('different_week')
