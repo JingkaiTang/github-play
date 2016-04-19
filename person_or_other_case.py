38e7ae3418f0729abba9404225c61f50775f0b1c
Jingkai Tang <jingkaitang@gmail.com>
2016-04-19 15:57:26 +0800

    mix: get person_or_other_case.py under bad work

---
+++ person_or_other_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_hand_or_same_hand(str_arg):
+    work(str_arg)
+    print('do_large_fact_at_child')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_hand_or_same_hand('feel_large_world')
