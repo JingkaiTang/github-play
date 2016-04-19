552bac3fc708a891721c00d7194100bacfcd8b7a
Jingkai Tang <jingkaitang@gmail.com>
2016-04-20 05:09:34 +0800

    rm: work find_child/work_and_problem/leave_long_way/get_new_number/way.py over little part

---
+++ find_child/work_and_problem/leave_long_way/get_new_number/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_same_woman(str_arg):
+    find_fact_beneath_place(str_arg)
+    print('point_or_number')
+
+def find_fact_beneath_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_same_woman('fact')
