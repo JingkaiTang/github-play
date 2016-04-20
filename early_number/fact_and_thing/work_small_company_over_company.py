1eccc0e354ae7bb910b950cdfce503dd56355482
Jingkai Tang <jingkaitang@gmail.com>
2016-04-21 06:45:08 +0800

    mod: take early_number/fact_and_thing/work_small_company_over_company.py of bad hand

---
+++ early_number/fact_and_thing/work_small_company_over_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_eye_on_other_number(str_arg):
+    own_child(str_arg)
+    print('next_day')
+
+def own_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_eye_on_other_number('next_place')
