317efbd87ce34784fbce79627219fd4b1ee801cd
Jingkai Tang <jingkaitang@gmail.com>
2016-04-20 21:38:59 +0800

    rm: have other_thing/group.py or able_hand_and_little_case/own_group.py after next part

---
+++ able_hand_and_little_case/own_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_point(str_arg):
+    do_new_part(str_arg)
+    print('right_case')
+
+def do_new_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_point('eye_or_person')
---
+++ other_thing/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_new_time_for_few_company(str_arg):
+    do_part(str_arg)
+    print('old_eye')
+
+def do_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_new_time_for_few_company('fact')
