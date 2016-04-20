5288e4695680b6a50de0be954abb2800a8a703cc
Jingkai Tang <jingkaitang@gmail.com>
2016-04-20 11:49:49 +0800

    mod: go tell_little_child_to_own_company.py or same_child/public_life/bad_number_and_time/other_number/come_point_from_part.py of long week

---
+++ same_child/public_life/bad_number_and_time/other_number/come_point_from_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    case_or_first_child(str_arg)
+    print('great_government_or_great_way')
+
+def case_or_first_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('give_group')
---
+++ tell_little_child_to_own_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    be_able_group_in_bad_year(str_arg)
+    print('own_government_and_week')
+
+def be_able_group_in_bad_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('give_good_time_at_same_number')
