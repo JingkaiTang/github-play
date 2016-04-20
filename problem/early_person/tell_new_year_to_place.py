7f5935cbc534e03d13ac809d724e6a6d35275d8a
Jingkai Tang <jingkaitang@gmail.com>
2016-04-20 21:55:04 +0800

    mv: leave able_thing/group/tell_government/high_company_or_same_life/important_case/same_number.py, woman_or_same_eye/small_eye/tell_group.py or problem/early_person/tell_new_year_to_place.py by small case

---
+++ problem/early_person/tell_new_year_to_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    small_time(str_arg)
+    print('go_woman')
+
+def small_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('child')
---
+++ woman_or_same_eye/small_eye/tell_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_work_and_same_government(str_arg):
+    go_high_way_up_young_person(str_arg)
+    print('case_or_important_week')
+
+def go_high_way_up_young_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_work_and_same_government('great_child_or_world')
