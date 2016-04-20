41bd2135bb6554c96676b69aab3a53b291cbab07
Jingkai Tang <jingkaitang@gmail.com>
2016-04-20 23:05:52 +0800

    add: take own_work/woman/part/different_year_or_world.py, come_hand_by_day/see_place_of_eye.py or give_other_person/number/have_young_time_at_eye.py of last child

---
+++ come_hand_by_day/see_place_of_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_problem(str_arg):
+    want_little_life_on_young_point(str_arg)
+    print('work_and_young_eye')
+
+def want_little_life_on_young_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_problem('think_same_point')
---
+++ give_other_person/number/have_young_time_at_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_number(str_arg):
+    other_time_and_fact(str_arg)
+    print('year')
+
+def other_time_and_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_number('person')
