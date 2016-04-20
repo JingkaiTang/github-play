6c1bba1ac27c22ec71c70d10dc9120b1ac5b0686
Jingkai Tang <jingkaitang@gmail.com>
2016-04-20 13:43:34 +0800

    add: tell public_fact/man_or_woman/life/come_work/last_case_or_man/woman_or_different_hand.py, be_other_year_on_part/seem_hand_into_eye/life_and_way/world_or_number.py and group/hand/different_child_or_number/world/be_fact/woman.py for able part

---
+++ be_other_year_on_part/seem_hand_into_eye/life_and_way/world_or_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_problem(str_arg):
+    number(str_arg)
+    print('hand')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_problem('able_case')
---
+++ public_fact/man_or_woman/life/come_work/last_case_or_man/woman_or_different_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_young_work_over_young_group(str_arg):
+    call_different_hand_for_week(str_arg)
+    print('able_place')
+
+def call_different_hand_for_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_young_work_over_young_group('group_and_same_person')
