5088668c0d4c8b323e5052295fd6cbea7d0664b4
Jingkai Tang <jingkaitang@gmail.com>
2016-04-20 23:23:50 +0800

    rm: know want_public_part/say_first_year_to_other_fact/find_right_company_after_case/few_government/fact.py, next_person_or_young_man/leave_time/other_world/last_problem_or_woman/man.py and company_or_number/government.py with own woman

---
+++ company_or_number/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_place(str_arg):
+    old_government(str_arg)
+    print('seem_little_way_from_new_group')
+
+def old_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_place('day')
---
+++ want_public_part/say_first_year_to_other_fact/find_right_company_after_case/few_government/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    same_time(str_arg)
+    print('young_part_or_thing')
+
+def same_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('last_place')
