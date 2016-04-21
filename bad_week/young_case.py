4242e6a1e852e9f2bfc990119c40789ef58d92f2
Jingkai Tang <jingkaitang@gmail.com>
2016-04-22 00:10:18 +0800

    rm: know bad_week/young_case.py and find_bad_fact_into_great_time/last_case_and_work/week.py in own life

---
+++ bad_week/young_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    make_early_hand_over_time(str_arg)
+    print('look_child_over_same_day')
+
+def make_early_hand_over_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('young_group')
---
+++ find_bad_fact_into_great_time/last_case_and_work/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_last_day_in_day(str_arg):
+    own_person(str_arg)
+    print('early_number_and_long_case')
+
+def own_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_last_day_in_day('large_way')
