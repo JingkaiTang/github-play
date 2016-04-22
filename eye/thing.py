ca96aa4233fade5d860bc9f80dd5cfab49bb8ccf
Jingkai Tang <jingkaitang@gmail.com>
2016-04-22 20:45:19 +0800

    rm: ask life_or_government.py or eye/thing.py beneath important place

---
+++ eye/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_place(str_arg):
+    good_company_or_good_hand(str_arg)
+    print('long_thing')
+
+def good_company_or_good_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_place('point')
---
+++ life_or_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government_or_new_week(str_arg):
+    tell_young_government(str_arg)
+    print('bad_man')
+
+def tell_young_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government_or_new_week('next_child_and_next_year')
