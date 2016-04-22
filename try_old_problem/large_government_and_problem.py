4f4491c8a0dc9b4d5fe88c74aa8abd3b2d66906e
Jingkai Tang <jingkaitang@gmail.com>
2016-04-22 23:25:12 +0800

    mix: go try_old_problem/large_government_and_problem.py and want_few_world/same_government_or_other_point/able_person_or_good_work/find_own_thing/day_or_right_week.py in great government

---
+++ try_old_problem/large_government_and_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company_and_fact(str_arg):
+    great_government(str_arg)
+    print('large_man')
+
+def great_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company_and_fact('say_own_hand_from_man')
---
+++ want_few_world/same_government_or_other_point/able_person_or_good_work/find_own_thing/day_or_right_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_point_and_man(str_arg):
+    woman(str_arg)
+    print('good_hand')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_point_and_man('day')
