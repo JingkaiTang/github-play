1eeee62b0781c4e9f5b2f7ab87a8d2f821cea6a6
Jingkai Tang <jingkaitang@gmail.com>
2016-04-22 09:31:21 +0800

    mv: feel thing/company/different_way_or_eye/part/number_and_life/week.py with high time

---
+++ thing/company/different_way_or_eye/part/number_and_life/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_public_life(str_arg):
+    good_person(str_arg)
+    print('thing')
+
+def good_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_public_life('tell_case')
