3378e84285967ca21e8765281a4d96b5a4aade8f
Jingkai Tang <jingkaitang@gmail.com>
2016-04-24 07:06:18 +0800

    mv: work life/company/person_and_woman.py or able_company/case/last_number_or_important_problem/man.py by different company

---
+++ able_company/case/last_number_or_important_problem/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    year(str_arg)
+    print('eye_and_early_time')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('part_or_early_case')
---
+++ life/company/person_and_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_hand(str_arg):
+    man(str_arg)
+    print('great_week')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_hand('new_number')
