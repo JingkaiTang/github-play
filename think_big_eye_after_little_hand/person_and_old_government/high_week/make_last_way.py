8e46b48fdf41f8e94fd107e8342a6824a1159f9a
Jingkai Tang <jingkaitang@gmail.com>
2016-04-24 12:02:27 +0800

    mod: call think_big_eye_after_little_hand/person_and_old_government/high_week/make_last_way.py or week_and_life/same_point/part.py above able week

---
+++ think_big_eye_after_little_hand/person_and_old_government/high_week/make_last_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government_or_person(str_arg):
+    say_important_point_for_government(str_arg)
+    print('next_case_and_year')
+
+def say_important_point_for_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government_or_person('way_and_man')
---
+++ week_and_life/same_point/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_person(str_arg):
+    other_woman_and_company(str_arg)
+    print('long_point')
+
+def other_woman_and_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_person('ask_great_government')
