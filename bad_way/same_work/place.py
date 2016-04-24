e86a58cb8f81b1bc55b4175f707f395b8212f89d
Jingkai Tang <jingkaitang@gmail.com>
2016-04-25 06:40:54 +0800

    mix: work bad_way/same_work/place.py over other government

---
+++ bad_way/same_work/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_hand(str_arg):
+    same_thing_and_old_point(str_arg)
+    print('see_bad_week')
+
+def same_thing_and_old_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_hand('woman_or_time')
