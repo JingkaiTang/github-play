b02b0cf9969fe1efab3817cf25f381aa75d78eed
Jingkai Tang <jingkaitang@gmail.com>
2016-04-24 21:30:18 +0800

    mv: go world/life_or_first_child/early_year/small_government/last_eye.py to right life

---
+++ world/life_or_first_child/early_year/small_government/last_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    company(str_arg)
+    print('think_woman_into_man')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('say_long_part')
