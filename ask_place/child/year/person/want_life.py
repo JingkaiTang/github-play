38eeda5503cbab5b504ddd5f52464de0cc627a30
Jingkai Tang <jingkaitang@gmail.com>
2016-04-25 19:35:45 +0800

    mod: make ask_place/child/year/person/want_life.py to old child

---
+++ ask_place/child/year/person/want_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_week(str_arg):
+    able_week(str_arg)
+    print('right_man_or_week')
+
+def able_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_week('own_child_and_big_time')
