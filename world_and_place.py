5f7a6279610efee3efc987dafefb351d16de675b
Jingkai Tang <jingkaitang@gmail.com>
2016-04-25 17:32:36 +0800

    rm: do world_and_place.py about first way

---
+++ world_and_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_point(str_arg):
+    right_place_or_person(str_arg)
+    print('point_and_fact')
+
+def right_place_or_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_point('world')
