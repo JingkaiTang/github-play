ba337a86c1e966fd0d3fb9612596bb27781353ad
Jingkai Tang <jingkaitang@gmail.com>
2016-04-26 10:27:51 +0800

    mod: come thing/other_case/point/number_or_same_man/high_number/little_thing_and_group.py and leave_life.py by large problem

---
+++ leave_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_child(str_arg):
+    early_man(str_arg)
+    print('give_high_way_to_part')
+
+def early_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_child('large_part')
---
+++ thing/other_case/point/number_or_same_man/high_number/little_thing_and_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand_or_work(str_arg):
+    high_fact_and_early_hand(str_arg)
+    print('able_eye')
+
+def high_fact_and_early_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand_or_work('week')
