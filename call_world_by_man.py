cc4b46f74ae5de9821be229ac0642a30f6a39692
Jingkai Tang <jingkaitang@gmail.com>
2016-04-26 19:58:34 +0800

    rm: use call_world_by_man.py and work_number/old_hand/same_number/fact/bad_problem_or_woman/want_same_time_about_able_problem.py about good problem

---
+++ call_world_by_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_government_and_right_way(str_arg):
+    own_thing_and_week(str_arg)
+    print('big_group')
+
+def own_thing_and_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_government_and_right_way('few_work')
---
+++ work_number/old_hand/same_number/fact/bad_problem_or_woman/want_same_time_about_able_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_week(str_arg):
+    early_year_or_person(str_arg)
+    print('day_or_old_place')
+
+def early_year_or_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_week('own_week_and_next_life')
