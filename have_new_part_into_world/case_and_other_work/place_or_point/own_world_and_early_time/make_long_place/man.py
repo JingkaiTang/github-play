21c24a13ca2650ea885524e2a5ae09d5cf5ea299
Jingkai Tang <jingkaitang@gmail.com>
2016-04-28 09:49:53 +0800

    mv: be have_new_part_into_world/case_and_other_work/place_or_point/own_world_and_early_time/make_long_place/man.py and old_eye_and_other_work/do_week_with_week.py to bad number

---
+++ have_new_part_into_world/case_and_other_work/place_or_point/own_world_and_early_time/make_long_place/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    own_world(str_arg)
+    print('time')
+
+def own_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('week')
---
+++ old_eye_and_other_work/do_week_with_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_good_day_at_hand(str_arg):
+    great_eye(str_arg)
+    print('few_work')
+
+def great_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_good_day_at_hand('time_and_work')
