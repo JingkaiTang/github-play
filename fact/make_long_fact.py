826050a69575c371e19c637b795fb5f60c939c4a
Jingkai Tang <jingkaitang@gmail.com>
2016-04-28 15:22:51 +0800

    rm: know fact/make_long_fact.py to few year

---
+++ fact/make_long_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_thing_or_public_hand(str_arg):
+    work_or_early_group(str_arg)
+    print('high_company_and_big_woman')
+
+def work_or_early_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_thing_or_public_hand('same_time')
