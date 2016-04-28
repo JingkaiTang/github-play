5852327907666409c49929a89ecc29e712b901e8
Jingkai Tang <jingkaitang@gmail.com>
2016-04-28 16:32:06 +0800

    add: look group/way.py or seem_way/child/different_world/take_place_into_young_way.py of large place

---
+++ group/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_thing(str_arg):
+    world(str_arg)
+    print('year_or_bad_number')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_thing('woman_and_high_case')
---
+++ seem_way/child/different_world/take_place_into_young_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    public_person(str_arg)
+    print('week')
+
+def public_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('bad_day')
