346fa2d0d74b0a0303889e247bd704e74d14f129
Jingkai Tang <jingkaitang@gmail.com>
2016-04-29 13:29:18 +0800

    mod: say young_week/small_time/other_work/first_problem.py for great day

---
+++ young_week/small_time/other_work/first_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    child(str_arg)
+    print('early_world')
+
+def child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('public_day_or_work')
