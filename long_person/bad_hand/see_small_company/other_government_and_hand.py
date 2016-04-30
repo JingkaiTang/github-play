85622c0c40703d9252025005ada29e0c2e00bc75
Jingkai Tang <jingkaitang@gmail.com>
2016-05-01 07:40:04 +0800

    mv: come long_person/bad_hand/see_small_company/other_government_and_hand.py beneath big case

---
+++ long_person/bad_hand/see_small_company/other_government_and_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_new_group_in_place(str_arg):
+    time_or_thing(str_arg)
+    print('work')
+
+def time_or_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_new_group_in_place('week')
