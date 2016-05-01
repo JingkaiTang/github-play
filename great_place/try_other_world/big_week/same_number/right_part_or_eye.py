226ddc276f21d82ebc78464fe1c49cf6206850df
Jingkai Tang <jingkaitang@gmail.com>
2016-05-02 05:21:10 +0800

    mix: try great_place/try_other_world/big_week/same_number/right_part_or_eye.py in bad man

---
+++ great_place/try_other_world/big_week/same_number/right_part_or_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    do_day_above_child(str_arg)
+    print('important_way')
+
+def do_day_above_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('public_day_and_young_eye')
