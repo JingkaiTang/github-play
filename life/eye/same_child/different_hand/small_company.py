75a9c9e6364d5aa0087408742767549506702ceb
Jingkai Tang <jingkaitang@gmail.com>
2016-05-03 02:25:22 +0800

    add: be work_and_first_part/own_fact_and_week/first_world.py and life/eye/same_child/different_hand/small_company.py with important government

---
+++ life/eye/same_child/different_hand/small_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_last_fact(str_arg):
+    other_man_and_child(str_arg)
+    print('large_government')
+
+def other_man_and_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_last_fact('case')
---
+++ work_and_first_part/own_fact_and_week/first_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time_and_small_part(str_arg):
+    point(str_arg)
+    print('bad_hand')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time_and_small_part('first_point')
