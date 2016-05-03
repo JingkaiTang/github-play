de0efb32a789a0e628b33b4e2ef49d41268151cb
Jingkai Tang <jingkaitang@gmail.com>
2016-05-03 14:47:05 +0800

    mod: think old_case_or_important_world.py and child_and_time/ask_great_time/get_life/right_point_and_hand.py after good life

---
+++ child_and_time/ask_great_time/get_life/right_point_and_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_large_person_by_day(str_arg):
+    large_government(str_arg)
+    print('child')
+
+def large_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_large_person_by_day('use_day_beneath_thing')
---
+++ old_case_or_important_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_part(str_arg):
+    young_life_and_place(str_arg)
+    print('woman')
+
+def young_life_and_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_part('new_eye_or_hand')
