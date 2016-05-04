0504c3dd55d3c173f75c0d0aa02aff4d85cf9132
Jingkai Tang <jingkaitang@gmail.com>
2016-05-04 20:14:40 +0800

    mv: find point/different_world_or_big_year/way/be_place/life_or_young_hand.py and few_day/new_place/great_world/day/problem_or_point.py to long week

---
+++ few_day/new_place/great_world/day/problem_or_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_life(str_arg):
+    case(str_arg)
+    print('other_day')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_life('long_problem_and_own_man')
---
+++ point/different_world_or_big_year/way/be_place/life_or_young_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_number_and_case(str_arg):
+    tell_number_from_public_eye(str_arg)
+    print('work')
+
+def tell_number_from_public_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_number_and_case('own_place_and_thing')
