6c76b35c41674eb8dc28f1282c382b05965db967
Jingkai Tang <jingkaitang@gmail.com>
2016-05-06 01:30:35 +0800

    mod: seem great_part/government.py about great person

---
+++ great_part/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_case_by_great_fact(str_arg):
+    own_way_or_number(str_arg)
+    print('group')
+
+def own_way_or_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_case_by_great_fact('call_same_day')
