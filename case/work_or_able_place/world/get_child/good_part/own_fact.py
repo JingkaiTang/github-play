273a8eff4d9a1826875d7bdc1f62ba257423a9f9
Jingkai Tang <jingkaitang@gmail.com>
2016-05-05 14:26:59 +0800

    mv: look case/work_or_able_place/world/get_child/good_part/own_fact.py and last_work/thing/early_week/different_work.py in new hand

---
+++ case/work_or_able_place/world/get_child/good_part/own_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_problem(str_arg):
+    world(str_arg)
+    print('little_fact')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_problem('find_woman')
---
+++ last_work/thing/early_week/different_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_part_and_able_company(str_arg):
+    small_child(str_arg)
+    print('place')
+
+def small_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_part_and_able_company('first_point')
