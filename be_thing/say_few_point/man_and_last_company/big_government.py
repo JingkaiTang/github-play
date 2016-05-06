bff3ac2247899965deacadcffaea8b5fe94c92a7
Jingkai Tang <jingkaitang@gmail.com>
2016-05-07 02:02:56 +0800

    mix: feel problem/time/few_company/small_place_and_big_man/early_company.py and be_thing/say_few_point/man_and_last_company/big_government.py by great year

---
+++ be_thing/say_few_point/man_and_last_company/big_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_eye_of_week(str_arg):
+    do_place(str_arg)
+    print('know_large_thing_after_able_eye')
+
+def do_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_eye_of_week('come_young_year')
---
+++ problem/time/few_company/small_place_and_big_man/early_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    small_point_and_work(str_arg)
+    print('good_place')
+
+def small_point_and_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('want_problem')
