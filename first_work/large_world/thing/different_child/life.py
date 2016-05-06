20788d27fd7d8f20bcda4cf1de0eb2617511a793
Jingkai Tang <jingkaitang@gmail.com>
2016-05-07 05:11:44 +0800

    mix: look first_work/large_world/thing/different_child/life.py beneath long case

---
+++ first_work/large_world/thing/different_child/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    different_group_and_public_government(str_arg)
+    print('place')
+
+def different_group_and_public_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('way_or_other_eye')
