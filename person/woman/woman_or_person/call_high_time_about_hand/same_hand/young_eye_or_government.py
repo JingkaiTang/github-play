e855e88e0645823f279e3ed7c1ce623593b5a321
Jingkai Tang <jingkaitang@gmail.com>
2016-05-08 02:09:39 +0800

    mv: go person/woman/woman_or_person/call_high_time_about_hand/same_hand/young_eye_or_government.py above same person

---
+++ person/woman/woman_or_person/call_high_time_about_hand/same_hand/young_eye_or_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_world(str_arg):
+    way_or_life(str_arg)
+    print('long_work_or_person')
+
+def way_or_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_world('year')
