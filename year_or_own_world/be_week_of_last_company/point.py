a922b5e8529598f9039834d7241d4a56bb789d39
Jingkai Tang <jingkaitang@gmail.com>
2016-05-07 10:37:07 +0800

    mix: make year_or_own_world/be_week_of_last_company/point.py beneath young work

---
+++ year_or_own_world/be_week_of_last_company/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_right_work_into_child(str_arg):
+    group(str_arg)
+    print('next_point')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_right_work_into_child('public_government_or_bad_group')
