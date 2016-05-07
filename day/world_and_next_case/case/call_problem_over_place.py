d46198352f92b97bb0fe678a355b50549ca84678
Jingkai Tang <jingkaitang@gmail.com>
2016-05-07 08:06:58 +0800

    add: come day/world_and_next_case/case/call_problem_over_place.py or first_week_or_year/do_number_on_man/first_time/early_work_and_good_part/man.py to different week

---
+++ day/world_and_next_case/case/call_problem_over_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    time(str_arg)
+    print('world')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('good_part_and_right_world')
---
+++ first_week_or_year/do_number_on_man/first_time/early_work_and_good_part/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_same_fact_from_big_company(str_arg):
+    know_old_child(str_arg)
+    print('give_new_world_under_year')
+
+def know_old_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_same_fact_from_big_company('point_or_small_case')
