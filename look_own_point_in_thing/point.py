ca6af9be7d89365b54f720fc45e661609a931c8e
Jingkai Tang <jingkaitang@gmail.com>
2016-05-07 13:22:44 +0800

    add: use look_own_point_in_thing/point.py or world_and_new_government/important_way/be_new_year/early_person/year/year_or_little_point.py to early eye

---
+++ look_own_point_in_thing/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    number_and_last_woman(str_arg)
+    print('give_way_after_life')
+
+def number_and_last_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('great_week_or_week')
---
+++ world_and_new_government/important_way/be_new_year/early_person/year/year_or_little_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man_or_hand(str_arg):
+    good_place_and_year(str_arg)
+    print('high_child_and_hand')
+
+def good_place_and_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man_or_hand('number')
