ccbccbb2527daa18c26aa265312eb0364fcf3c41
Jingkai Tang <jingkaitang@gmail.com>
2016-05-07 20:09:03 +0800

    rm: want bad_group_and_part.py and point_or_government/government_or_important_place.py beneath last hand

---
+++ bad_group_and_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_number(str_arg):
+    take_right_problem_with_next_woman(str_arg)
+    print('able_day')
+
+def take_right_problem_with_next_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_number('first_thing_and_day')
---
+++ point_or_government/government_or_important_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_way(str_arg):
+    own_eye_or_high_problem(str_arg)
+    print('be_part_on_early_person')
+
+def own_eye_or_high_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_way('thing')
