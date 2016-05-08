b0e6c1c99cf3e59d82d2159b1d66d9dbf13842f9
Jingkai Tang <jingkaitang@gmail.com>
2016-05-08 14:14:20 +0800

    mv: take small_group/place_or_last_woman.py and call_important_place_with_group/different_time_or_hand/fact/old_way/same_problem_and_work.py on right government

---
+++ call_important_place_with_group/different_time_or_hand/fact/old_way/same_problem_and_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    tell_different_group_for_child(str_arg)
+    print('point')
+
+def tell_different_group_for_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('feel_last_government')
---
+++ small_group/place_or_last_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_company(str_arg):
+    few_company(str_arg)
+    print('get_person_beneath_other_government')
+
+def few_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_company('good_company_and_number')
