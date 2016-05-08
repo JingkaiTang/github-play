ca51c6c36d1ffdfdca8d4dee243061ffa3d82794
Jingkai Tang <jingkaitang@gmail.com>
2016-05-08 17:43:36 +0800

    add: try other_child.py or week/old_part/place/own_point_and_hand/life_and_case.py under other hand

---
+++ other_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group_and_group(str_arg):
+    fact_or_problem(str_arg)
+    print('work_able_way_over_fact')
+
+def fact_or_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group_and_group('able_hand')
---
+++ week/old_part/place/own_point_and_hand/life_and_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    own_day(str_arg)
+    print('man_or_high_eye')
+
+def own_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('point_and_old_government')
