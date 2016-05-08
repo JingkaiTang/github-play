6bfacf778a83d44e80ea71ebea3ac9ec1468c4ce
Jingkai Tang <jingkaitang@gmail.com>
2016-05-08 18:48:41 +0800

    add: call year/little_world.py or hand/young_point_and_eye/able_hand_or_old_life.py about small hand

---
+++ hand/young_point_and_eye/able_hand_or_old_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_last_place_after_early_life(str_arg):
+    child_and_important_year(str_arg)
+    print('different_problem')
+
+def child_and_important_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_last_place_after_early_life('child')
---
+++ year/little_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_person(str_arg):
+    call_fact_after_point(str_arg)
+    print('work_thing')
+
+def call_fact_after_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_person('find_new_day')
