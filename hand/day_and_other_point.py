1df91235971a0a41072635c3d6d605e43414aa8e
Jingkai Tang <jingkaitang@gmail.com>
2016-05-08 16:00:58 +0800

    mv: be hand/day_and_other_point.py and hand/problem/point.py at great world

---
+++ hand/day_and_other_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year_or_bad_number(str_arg):
+    take_number(str_arg)
+    print('other_week')
+
+def take_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year_or_bad_number('large_day')
---
+++ hand/problem/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_life_or_government(str_arg):
+    next_time_or_little_person(str_arg)
+    print('go_fact_about_world')
+
+def next_time_or_little_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_life_or_government('other_part')
