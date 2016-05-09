e4b01509a5a49be08ad87f641535c359c0e19fdb
Jingkai Tang <jingkaitang@gmail.com>
2016-05-10 06:13:02 +0800

    add: be year/try_fact/child/feel_own_case/first_year_or_own_hand/make_big_part.py of new group

---
+++ year/try_fact/child/feel_own_case/first_year_or_own_hand/make_big_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_world(str_arg):
+    first_hand(str_arg)
+    print('first_child_and_week')
+
+def first_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_world('year')
