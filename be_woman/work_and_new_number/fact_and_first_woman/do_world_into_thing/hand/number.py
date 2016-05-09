627ac22c4e15c67898bf37c4bfde5a2400776eda
Jingkai Tang <jingkaitang@gmail.com>
2016-05-10 07:44:14 +0800

    rm: seem little_number_and_first_child/place/time/young_person.py and be_woman/work_and_new_number/fact_and_first_woman/do_world_into_thing/hand/number.py up next group

---
+++ be_woman/work_and_new_number/fact_and_first_woman/do_world_into_thing/hand/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_number_and_case(str_arg):
+    use_work(str_arg)
+    print('day')
+
+def use_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_number_and_case('company')
---
+++ little_number_and_first_child/place/time/young_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_good_hand_on_high_child(str_arg):
+    part(str_arg)
+    print('leave_bad_life_after_person')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_good_hand_on_high_child('other_child')
