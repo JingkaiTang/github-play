0dc55f30bc12d9ad86cb000b83800c04b2529037
Jingkai Tang <jingkaitang@gmail.com>
2016-05-10 17:43:44 +0800

    mod: find other_hand_or_long_child/little_government/important_place/group/person/time.py into old part

---
+++ other_hand_or_long_child/little_government/important_place/group/person/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time_or_week(str_arg):
+    way(str_arg)
+    print('problem')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time_or_week('leave_case_about_year')
