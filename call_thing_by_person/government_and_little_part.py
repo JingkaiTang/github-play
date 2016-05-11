5970ce7da6091a825572b5517903e3791a9a2c1c
Jingkai Tang <jingkaitang@gmail.com>
2016-05-11 08:40:24 +0800

    mix: come call_thing_by_person/government_and_little_part.py under bad part

---
+++ call_thing_by_person/government_and_little_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_last_man_by_point(str_arg):
+    come_first_year_beneath_young_thing(str_arg)
+    print('hand_and_way')
+
+def come_first_year_beneath_young_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_last_man_by_point('company')
