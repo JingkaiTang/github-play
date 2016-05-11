13aeb820b6b038944b9fb4730ce964f6ef626818
Jingkai Tang <jingkaitang@gmail.com>
2016-05-11 11:31:55 +0800

    mix: say part_and_year/old_person_or_year.py or do_company/do_few_child/hand/think_great_week/leave_number_above_young_number/ask_case.py beneath good thing

---
+++ do_company/do_few_child/hand/think_great_week/leave_number_above_young_number/ask_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_small_work(str_arg):
+    want_big_work_in_high_eye(str_arg)
+    print('small_group_or_point')
+
+def want_big_work_in_high_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_small_work('child')
---
+++ part_and_year/old_person_or_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time_and_time(str_arg):
+    government(str_arg)
+    print('number_and_next_woman')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time_and_time('work_old_world_about_young_hand')
