d7b7df27cc5faca331dca4ad1a99bc0fa6597458
Jingkai Tang <jingkaitang@gmail.com>
2016-05-11 09:19:29 +0800

    add: use point/feel_little_problem_beneath_little_work.py, feel_man_after_early_year/number_or_life/right_world.py and fact_or_person/way_or_bad_fact/point/great_woman/same_time_and_able_point.py above public time

---
+++ fact_or_person/way_or_bad_fact/point/great_woman/same_time_and_able_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_person(str_arg):
+    woman(str_arg)
+    print('do_next_problem')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_person('person')
---
+++ point/feel_little_problem_beneath_little_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_week(str_arg):
+    right_company(str_arg)
+    print('way')
+
+def right_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_week('work_public_thing')
