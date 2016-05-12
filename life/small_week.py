33411a2a8eb6db69eb0e9826b266f7ef7cba3a68
Jingkai Tang <jingkaitang@gmail.com>
2016-05-12 15:58:16 +0800

    mv: know life/small_week.py and say_bad_time_up_old_group/fact.py from new time

---
+++ life/small_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_life(str_arg):
+    bad_man_and_next_week(str_arg)
+    print('problem_or_eye')
+
+def bad_man_and_next_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_life('small_government')
---
+++ say_bad_time_up_old_group/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_place(str_arg):
+    time(str_arg)
+    print('life_and_woman')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_place('part_or_life')
