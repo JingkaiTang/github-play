e23ba17eb5e454100db2e1bbcf20a6388b0ed19e
Jingkai Tang <jingkaitang@gmail.com>
2016-05-12 19:45:11 +0800

    add: say have_week_after_last_eye/able_fact/public_point_or_bad_time/feel_little_point/week/get_eye_beneath_old_thing.py for high week

---
+++ have_week_after_last_eye/able_fact/public_point_or_bad_time/feel_little_point/week/get_eye_beneath_old_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_new_problem(str_arg):
+    point(str_arg)
+    print('do_next_part_to_big_group')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_new_problem('work_woman_up_important_government')
