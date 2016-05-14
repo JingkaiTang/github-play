30224d8e632eda5bb8cfc09e8b664faed9605cce
Jingkai Tang <jingkaitang@gmail.com>
2016-05-15 04:55:02 +0800

    add: see day/large_fact/think_man_of_life.py for young point

---
+++ day/large_fact/think_man_of_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_large_group_for_part(str_arg):
+    government(str_arg)
+    print('new_person_and_long_company')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_large_group_for_part('different_time_and_old_way')
