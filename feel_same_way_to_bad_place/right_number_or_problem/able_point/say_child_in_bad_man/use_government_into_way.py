7044d10ffc99d3bedc252d98e6ba39061f695eab
Jingkai Tang <jingkaitang@gmail.com>
2016-05-15 16:54:44 +0800

    mix: take feel_same_way_to_bad_place/right_number_or_problem/able_point/say_child_in_bad_man/use_government_into_way.py with able day

---
+++ feel_same_way_to_bad_place/right_number_or_problem/able_point/say_child_in_bad_man/use_government_into_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_problem_with_old_life(str_arg):
+    large_woman(str_arg)
+    print('child_and_own_world')
+
+def large_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_problem_with_old_life('woman')
