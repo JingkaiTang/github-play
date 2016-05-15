0ef3ad8b5cc8a325d3620f467a11c1d7adfc2a4e
Jingkai Tang <jingkaitang@gmail.com>
2016-05-15 21:12:52 +0800

    mix: give old_thing/feel_person_with_child.py over high work

---
+++ old_thing/feel_person_with_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    call_other_day(str_arg)
+    print('important_world_and_year')
+
+def call_other_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('same_day')
