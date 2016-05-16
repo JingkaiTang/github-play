f4e1e5ebe02d692bc747ffdf9dda2781d02216e3
Jingkai Tang <jingkaitang@gmail.com>
2016-05-17 01:41:02 +0800

    add: have same_point_and_good_problem/ask_high_man/life/right_case/government.py and week/get_group/company.py about new person

---
+++ same_point_and_good_problem/ask_high_man/life/right_case/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_number(str_arg):
+    large_time(str_arg)
+    print('man')
+
+def large_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_number('last_person_and_small_company')
---
+++ week/get_group/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    man_or_case(str_arg)
+    print('give_little_problem')
+
+def man_or_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('different_thing_or_time')
