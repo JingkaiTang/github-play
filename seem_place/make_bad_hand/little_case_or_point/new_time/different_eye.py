16e9d031be9de5cf95030c7ab02f234016638a23
Jingkai Tang <jingkaitang@gmail.com>
2016-05-18 00:56:39 +0800

    add: do leave_important_hand_on_child/hand/take_man_in_little_day/say_public_woman/good_time_and_group/company.py or seem_place/make_bad_hand/little_case_or_point/new_time/different_eye.py with last problem

---
+++ seem_place/make_bad_hand/little_case_or_point/new_time/different_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number_and_problem(str_arg):
+    able_week_and_man(str_arg)
+    print('year_or_work')
+
+def able_week_and_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number_and_problem('new_child')
