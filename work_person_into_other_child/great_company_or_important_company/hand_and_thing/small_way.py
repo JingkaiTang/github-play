8b86374b5e3f0257ec768c0a550bb8142867d063
Jingkai Tang <jingkaitang@gmail.com>
2016-05-18 21:47:57 +0800

    mod: make work_person_into_other_child/great_company_or_important_company/hand_and_thing/small_way.py above big point

---
+++ work_person_into_other_child/great_company_or_important_company/hand_and_thing/small_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_life_beneath_able_number(str_arg):
+    think_person_to_time(str_arg)
+    print('use_work_up_fact')
+
+def think_person_to_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_life_beneath_able_number('problem_or_problem')
