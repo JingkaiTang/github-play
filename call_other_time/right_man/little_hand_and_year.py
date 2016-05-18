a64e8db066f6f20d09f75347f986ded101ff10fa
Jingkai Tang <jingkaitang@gmail.com>
2016-05-18 18:58:20 +0800

    mv: see call_other_time/right_man/little_hand_and_year.py in right number

---
+++ call_other_time/right_man/little_hand_and_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_world(str_arg):
+    work_point(str_arg)
+    print('large_fact_or_important_day')
+
+def work_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_world('public_person')
