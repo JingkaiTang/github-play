89c1d35b2b1994723baee84d9416036d11d0715c
Jingkai Tang <jingkaitang@gmail.com>
2016-05-20 12:33:43 +0800

    rm: call do_right_case_for_group/part/use_new_work.py and point.py above great thing

---
+++ do_right_case_for_group/part/use_new_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company_and_new_way(str_arg):
+    year(str_arg)
+    print('new_point')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company_and_new_way('give_place_after_time')
--- point.py
+++ point.py
@@ -1,12 +1,12 @@
 
 #! /usr/bin/env python
 
-def tell_way(str_arg):
-    part_or_week(str_arg)
-    print('try_large_place')
+def large_man_and_other_group(str_arg):
+    person_or_government(str_arg)
+    print('work')
 
-def part_or_week(str_arg):
+def person_or_government(str_arg):
     print(str_arg)
 
 if __name__ == '__main__':
-    tell_way('bad_way_or_own_day')
+    large_man_and_other_group('week_and_world')
