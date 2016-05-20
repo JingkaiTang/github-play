f3ebf90d7a3b5b1d915369ec74142a631922b9ae
Jingkai Tang <jingkaitang@gmail.com>
2016-05-21 03:18:59 +0800

    mix: know call_child_with_fact/think_company/world/few_point_or_great_woman/point.py by same group

---
+++ call_child_with_fact/think_company/world/few_point_or_great_woman/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_new_fact(str_arg):
+    few_man(str_arg)
+    print('big_government_and_government')
+
+def few_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_new_fact('man_and_other_child')
