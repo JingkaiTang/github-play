82c0e184aa1ab60eb1eb7cc6aecf7672b8f30d6c
Jingkai Tang <jingkaitang@gmail.com>
2016-05-20 08:59:50 +0800

    mod: feel eye_or_group/last_eye.py by other thing

---
+++ eye_or_group/last_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_thing_or_first_thing(str_arg):
+    problem(str_arg)
+    print('time')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_thing_or_first_thing('number_or_group')
