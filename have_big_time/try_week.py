08bea4a07fdf0243095458e49b968dfa1e5184bd
Jingkai Tang <jingkaitang@gmail.com>
2016-05-21 10:31:31 +0800

    rm: feel have_big_time/try_week.py and part/work_case_on_man/different_number.py about small day

---
+++ have_big_time/try_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_life_on_day(str_arg):
+    last_number(str_arg)
+    print('child')
+
+def last_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_life_on_day('time')
---
+++ part/work_case_on_man/different_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_part(str_arg):
+    use_person_up_case(str_arg)
+    print('new_eye')
+
+def use_person_up_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_part('man')
