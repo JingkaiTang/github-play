74deaee327f3675ae1676d592aa755a5c781849a
Jingkai Tang <jingkaitang@gmail.com>
2016-05-21 16:29:25 +0800

    mix: use tell_man/fact.py under bad company

---
+++ tell_man/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_work_or_small_point(str_arg):
+    try_child(str_arg)
+    print('fact_and_own_fact')
+
+def try_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_work_or_small_point('take_child_for_first_work')
