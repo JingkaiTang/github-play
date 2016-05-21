6e8c534e58529dffab490aca3573ce2d49d5cf3e
Jingkai Tang <jingkaitang@gmail.com>
2016-05-22 07:07:31 +0800

    mv: go eye_and_place/eye_or_government/say_long_point_at_right_government/take_case_about_other_time.py in little fact

---
+++ eye_and_place/eye_or_government/say_long_point_at_right_government/take_case_about_other_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_problem_and_year(str_arg):
+    company(str_arg)
+    print('first_woman')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_problem_and_year('early_thing')
