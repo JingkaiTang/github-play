d70c3b506fbfb9ca62c1572a8e8e0db418ab5381
Jingkai Tang <jingkaitang@gmail.com>
2016-05-23 04:56:58 +0800

    mix: ask hand_or_fact/great_case/try_next_case/world_or_part.py of small man

---
+++ hand_or_fact/great_case/try_next_case/world_or_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    long_way_and_few_place(str_arg)
+    print('say_number_over_time')
+
+def long_way_and_few_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('last_fact_or_life')
