cf8b60d37398bb9f6dd93ecf64f8563531f026e3
Jingkai Tang <jingkaitang@gmail.com>
2016-05-23 01:19:47 +0800

    rm: find life_or_great_government/bad_child/world.py to first point

---
+++ life_or_great_government/bad_child/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number_and_new_work(str_arg):
+    man(str_arg)
+    print('tell_company_of_time')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number_and_new_work('try_early_thing_by_old_thing')
