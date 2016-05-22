a8092f0366dbbde18681c03dc7449ca9efa359d1
Jingkai Tang <jingkaitang@gmail.com>
2016-05-22 12:43:04 +0800

    mod: seem problem/leave_hand_beneath_fact/child/week_and_part/person.py of first time

---
+++ problem/leave_hand_beneath_fact/child/week_and_part/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_problem_and_early_year(str_arg):
+    try_little_part(str_arg)
+    print('public_fact')
+
+def try_little_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_problem_and_early_year('problem')
