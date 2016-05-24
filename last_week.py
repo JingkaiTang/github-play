83df169fff514f2d4dd1897b6f755baf8f269ded
Jingkai Tang <jingkaitang@gmail.com>
2016-05-24 18:09:50 +0800

    mv: give last_week.py after new hand

---
+++ last_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_few_problem_in_large_point(str_arg):
+    next_way(str_arg)
+    print('early_place')
+
+def next_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_few_problem_in_large_point('have_first_time')
