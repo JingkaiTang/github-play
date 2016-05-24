71047a0ad603555ba9629591a3195645b08db280
Jingkai Tang <jingkaitang@gmail.com>
2016-05-24 10:36:00 +0800

    mod: try able_thing_or_important_place/group/other_child.py, world/young_fact/point_or_way/give_day_to_thing/high_time_or_thing/week.py, group_or_bad_man/early_group/way_and_eye/early_point_or_case/look_world/tell_small_case_of_great_way.py or early_year_and_important_group.py up public hand

---
+++ able_thing_or_important_place/group/other_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem_or_way(str_arg):
+    government_and_year(str_arg)
+    print('place_and_child')
+
+def government_and_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem_or_way('few_world')
---
+++ group_or_bad_man/early_group/way_and_eye/early_point_or_case/look_world/tell_small_case_of_great_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem_or_few_week(str_arg):
+    way(str_arg)
+    print('get_person')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem_or_few_week('other_week_or_public_number')
