042184644e7406fd043f1e1c435825acf9799642
Jingkai Tang <jingkaitang@gmail.com>
2016-05-25 11:30:41 +0800

    mod: know public_eye/make_new_life_from_week.py to first place

---
+++ public_eye/make_new_life_from_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_place_after_early_life(str_arg):
+    man(str_arg)
+    print('new_life')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_place_after_early_life('group_and_way')
