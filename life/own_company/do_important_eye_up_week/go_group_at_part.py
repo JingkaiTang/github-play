a9171b61743dbc7ede4bf64202146889d43c1f23
Jingkai Tang <jingkaitang@gmail.com>
2016-05-26 18:39:56 +0800

    rm: make life/own_company/do_important_eye_up_week/go_group_at_part.py in old case

---
+++ life/own_company/do_important_eye_up_week/go_group_at_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_man_or_case(str_arg):
+    use_large_woman(str_arg)
+    print('great_hand_or_company')
+
+def use_large_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_man_or_case('place')
