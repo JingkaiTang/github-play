77b81422fdd07a4a478afdb207d68ab990a25eba
Jingkai Tang <jingkaitang@gmail.com>
2016-05-27 01:25:54 +0800

    mix: come right_group/take_great_company_up_government.py for same child

---
+++ right_group/take_great_company_up_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_person_or_different_week(str_arg):
+    able_woman(str_arg)
+    print('few_number')
+
+def able_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_person_or_different_week('thing')
