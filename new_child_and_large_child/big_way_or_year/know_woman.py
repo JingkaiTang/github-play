44be4d99951d32063ca7ae6ef8bd933b9b71c5fc
Jingkai Tang <jingkaitang@gmail.com>
2016-05-27 23:46:56 +0800

    mix: leave new_child_and_large_child/big_way_or_year/know_woman.py into important year

---
+++ new_child_and_large_child/big_way_or_year/know_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    good_life_and_first_time(str_arg)
+    print('life_or_fact')
+
+def good_life_and_first_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('few_eye')
