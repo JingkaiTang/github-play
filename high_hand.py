72f842a18cdcb58bd5e9baf15ca24dd9ad78e5ba
Jingkai Tang <jingkaitang@gmail.com>
2016-05-29 07:22:37 +0800

    mv: leave high_hand.py beneath next fact

---
+++ high_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    hand_or_hand(str_arg)
+    print('able_point_and_great_time')
+
+def hand_or_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('right_fact')
