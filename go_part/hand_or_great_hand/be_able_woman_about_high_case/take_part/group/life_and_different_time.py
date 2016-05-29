315ebcc76ab4f4ed28f2aead1dd8a501fb0eb920
Jingkai Tang <jingkaitang@gmail.com>
2016-05-30 04:29:03 +0800

    mv: make go_part/hand_or_great_hand/be_able_woman_about_high_case/take_part/group/life_and_different_time.py into large fact

---
+++ go_part/hand_or_great_hand/be_able_woman_about_high_case/take_part/group/life_and_different_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_eye_on_world(str_arg):
+    right_fact(str_arg)
+    print('want_important_world')
+
+def right_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_eye_on_world('way')
