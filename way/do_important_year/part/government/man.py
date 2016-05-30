b94e972b40833e57c3d324a9264909bb3f764ded
Jingkai Tang <jingkaitang@gmail.com>
2016-05-30 11:09:58 +0800

    rm: go way/do_important_year/part/government/man.py from little work

---
+++ way/do_important_year/part/government/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_good_part(str_arg):
+    number(str_arg)
+    print('problem_and_person')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_good_part('case')
