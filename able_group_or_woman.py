c3a0c9fee7ba0987416502493bc0da8df4b72c34
Jingkai Tang <jingkaitang@gmail.com>
2016-06-01 06:59:25 +0800

    mix: tell able_group_or_woman.py at own thing

---
+++ able_group_or_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    problem_or_place(str_arg)
+    print('fact_and_life')
+
+def problem_or_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('child')
