6deb70268066c224329fb92e8f4b201e220b9644
Jingkai Tang <jingkaitang@gmail.com>
2016-06-01 08:45:49 +0800

    rm: tell good_company.py and public_point_and_few_company/place/bad_group.py over different place

---
+++ good_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year_or_great_work(str_arg):
+    small_problem_and_government(str_arg)
+    print('way')
+
+def small_problem_and_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year_or_great_work('new_week')
---
+++ public_point_and_few_company/place/bad_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_day(str_arg):
+    early_number(str_arg)
+    print('problem')
+
+def early_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_day('own_part')
