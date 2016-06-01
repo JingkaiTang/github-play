9dd1d1e4ccff79275bb773b20e3a1867995f93a7
Jingkai Tang <jingkaitang@gmail.com>
2016-06-01 11:54:34 +0800

    rm: make company_or_public_day/early_government.py up different week

---
+++ company_or_public_day/early_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_small_man_from_different_woman(str_arg):
+    find_problem(str_arg)
+    print('other_fact')
+
+def find_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_small_man_from_different_woman('large_time_or_government')
