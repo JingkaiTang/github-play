48c60d9df6139a35608692f3b4194b0044f7afab
Jingkai Tang <jingkaitang@gmail.com>
2016-06-01 13:58:50 +0800

    mix: tell year/long_week/take_group/work.py, time_or_new_world/company_and_man/part_and_little_point.py, bad_person/great_point.py and old_woman/other_group_or_right_woman/child/take_few_point_for_next_case/make_old_woman_above_number.py about old way

---
+++ bad_person/great_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_place(str_arg):
+    world(str_arg)
+    print('go_man')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_place('public_week')
---
+++ year/long_week/take_group/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    person(str_arg)
+    print('woman_and_government')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('work')
