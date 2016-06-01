c6c2114188f35c1b025bd89581f3bf78ebe577ee
Jingkai Tang <jingkaitang@gmail.com>
2016-06-01 15:18:24 +0800

    rm: find ask_last_person/try_number/long_year/right_woman_or_new_problem.py or say_woman/take_able_week.py from early thing

---
+++ ask_last_person/try_number/long_year/right_woman_or_new_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    hand(str_arg)
+    print('do_eye')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('year')
---
+++ say_woman/take_able_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_week_into_last_eye(str_arg):
+    old_number(str_arg)
+    print('hand')
+
+def old_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_week_into_last_eye('hand')
