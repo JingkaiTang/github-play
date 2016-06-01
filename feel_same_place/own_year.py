1c66aca6c49cfe0643e62d4276ef0bb050bc58f0
Jingkai Tang <jingkaitang@gmail.com>
2016-06-01 20:40:41 +0800

    mod: go year/government_and_week/way/right_government.py and feel_same_place/own_year.py on different place

---
+++ feel_same_place/own_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_case(str_arg):
+    next_work(str_arg)
+    print('thing_and_way')
+
+def next_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_case('last_case_and_small_place')
