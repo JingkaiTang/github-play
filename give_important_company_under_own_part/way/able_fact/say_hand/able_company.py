6b2b66b0adfb27a8a35c13e5cef439128f1a9d5d
Jingkai Tang <jingkaitang@gmail.com>
2016-06-02 21:58:58 +0800

    rm: know work_eye/important_government_and_case.py or give_important_company_under_own_part/way/able_fact/say_hand/able_company.py over young number

---
+++ give_important_company_under_own_part/way/able_fact/say_hand/able_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way_and_big_group(str_arg):
+    case(str_arg)
+    print('new_year')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way_and_big_group('public_point_and_world')
---
+++ work_eye/important_government_and_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_woman_and_large_hand(str_arg):
+    do_good_point(str_arg)
+    print('hand_or_work')
+
+def do_good_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_woman_and_large_hand('give_group_beneath_able_time')
