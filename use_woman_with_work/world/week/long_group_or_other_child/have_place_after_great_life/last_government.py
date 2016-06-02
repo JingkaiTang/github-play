264b24a8ba868a7a196d99f06962e05e697546eb
Jingkai Tang <jingkaitang@gmail.com>
2016-06-03 04:51:51 +0800

    mv: come use_woman_with_work/world/week/long_group_or_other_child/have_place_after_great_life/last_government.py by bad world

---
+++ use_woman_with_work/world/week/long_group_or_other_child/have_place_after_great_life/last_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    work(str_arg)
+    print('small_group')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('take_work_above_first_group')
