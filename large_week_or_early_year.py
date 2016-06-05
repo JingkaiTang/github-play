a08ac24076055696d7e07e8865c1cfb932ebe507
Jingkai Tang <jingkaitang@gmail.com>
2016-06-06 05:28:18 +0800

    mix: look large_week_or_early_year.py by different point

---
+++ large_week_or_early_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_little_case(str_arg):
+    little_part_or_fact(str_arg)
+    print('public_thing_or_important_case')
+
+def little_part_or_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_little_case('work')
