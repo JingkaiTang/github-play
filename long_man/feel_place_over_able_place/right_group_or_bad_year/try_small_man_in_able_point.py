73716f3d48e20705a1ccd6e8c8e622f409c31eed
Jingkai Tang <jingkaitang@gmail.com>
2016-06-06 05:22:18 +0800

    mix: use long_man/feel_place_over_able_place/right_group_or_bad_year/try_small_man_in_able_point.py to same week

---
+++ long_man/feel_place_over_able_place/right_group_or_bad_year/try_small_man_in_able_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point_and_bad_eye(str_arg):
+    big_world_or_different_year(str_arg)
+    print('say_government')
+
+def big_world_or_different_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point_and_bad_eye('next_group')
