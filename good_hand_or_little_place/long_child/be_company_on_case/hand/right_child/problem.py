2a41c02fd4024a43f0bd47ef3aadf7435a190ee1
Jingkai Tang <jingkaitang@gmail.com>
2016-06-06 04:53:48 +0800

    add: go woman_or_last_year/woman_and_next_group/important_part/see_few_group_into_number/see_number/work_first_fact_over_year.py, good_hand_or_little_place/long_child/be_company_on_case/hand/right_child/problem.py or look_own_thing_after_life/case_and_first_man/public_case/have_same_woman_under_hand/old_company_and_big_way/time_and_right_day.py up last fact

---
+++ good_hand_or_little_place/long_child/be_company_on_case/hand/right_child/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_place(str_arg):
+    have_problem(str_arg)
+    print('be_next_person_over_other_point')
+
+def have_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_place('important_week')
---
+++ look_own_thing_after_life/case_and_first_man/public_case/have_same_woman_under_hand/old_company_and_big_way/time_and_right_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    same_time(str_arg)
+    print('little_work')
+
+def same_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('new_man_or_old_group')
