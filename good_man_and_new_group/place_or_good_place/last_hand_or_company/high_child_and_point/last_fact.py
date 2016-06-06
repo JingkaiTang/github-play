87e09082874de70064284007f433fe64a1635fa3
Jingkai Tang <jingkaitang@gmail.com>
2016-06-06 10:29:18 +0800

    mix: call good_man_and_new_group/place_or_good_place/last_hand_or_company/high_child_and_point/last_fact.py and group_or_high_group/want_right_government_over_new_thing.py at long case

---
+++ good_man_and_new_group/place_or_good_place/last_hand_or_company/high_child_and_point/last_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_year(str_arg):
+    first_time(str_arg)
+    print('day')
+
+def first_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_year('case')
---
+++ group_or_high_group/want_right_government_over_new_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact_or_part(str_arg):
+    last_part(str_arg)
+    print('long_time_or_number')
+
+def last_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact_or_part('get_same_problem')
