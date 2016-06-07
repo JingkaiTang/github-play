e4d244c805aa118a5b98bfaf591e48295555cc48
Jingkai Tang <jingkaitang@gmail.com>
2016-06-08 05:11:57 +0800

    add: look ask_point_beneath_woman/bad_thing.py and do_hand/go_important_place_of_problem/same_company/eye/high_problem_or_company.py with big company

---
+++ ask_point_beneath_woman/bad_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    government(str_arg)
+    print('number_or_small_problem')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('public_world_and_time')
