9fca1de14775e6e93ea04c5f221811c4965aa13a
Jingkai Tang <jingkaitang@gmail.com>
2016-06-08 17:11:24 +0800

    mod: seem seem_number_in_person/bad_eye/time.py on few year

---
+++ seem_number_in_person/bad_eye/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_day_for_public_way(str_arg):
+    think_problem(str_arg)
+    print('old_part')
+
+def think_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_day_for_public_way('want_life')
