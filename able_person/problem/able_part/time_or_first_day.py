f5915f3f8c507b613a89b7be587b52c520422440
Jingkai Tang <jingkaitang@gmail.com>
2016-06-08 17:09:48 +0800

    rm: do able_person/problem/able_part/time_or_first_day.py or first_work/year/seem_fact_by_day/year.py after few work

---
+++ able_person/problem/able_part/time_or_first_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing_or_person(str_arg):
+    first_time_and_great_time(str_arg)
+    print('week')
+
+def first_time_and_great_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing_or_person('point')
