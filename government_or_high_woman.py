8084afd6a31fa6af3d2841f27961b52363c671e8
Jingkai Tang <jingkaitang@gmail.com>
2016-06-08 11:21:39 +0800

    mv: make want_last_world/look_thing_after_bad_number/high_part/ask_first_point_for_important_life.py or government_or_high_woman.py of able number

---
+++ government_or_high_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way_and_right_fact(str_arg):
+    world_or_child(str_arg)
+    print('able_world')
+
+def world_or_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way_and_right_fact('company_or_man')
---
+++ want_last_world/look_thing_after_bad_number/high_part/ask_first_point_for_important_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_woman_over_part(str_arg):
+    say_life_on_small_case(str_arg)
+    print('right_world')
+
+def say_life_on_small_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_woman_over_part('number')
