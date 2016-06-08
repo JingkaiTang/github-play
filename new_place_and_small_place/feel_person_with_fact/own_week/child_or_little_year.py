40f6c896ef840183d8d47da882c78d72b97ea7f8
Jingkai Tang <jingkaitang@gmail.com>
2016-06-09 07:44:28 +0800

    mv: use new_place_and_small_place/feel_person_with_fact/own_week/child_or_little_year.py or world/hand.py on old government

---
+++ new_place_and_small_place/feel_person_with_fact/own_week/child_or_little_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_old_government_for_high_government(str_arg):
+    small_case(str_arg)
+    print('child_or_other_child')
+
+def small_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_old_government_for_high_government('important_day_or_life')
---
+++ world/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_important_life_into_new_part(str_arg):
+    leave_fact_by_same_point(str_arg)
+    print('big_number_and_different_way')
+
+def leave_fact_by_same_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_important_life_into_new_part('own_way')
