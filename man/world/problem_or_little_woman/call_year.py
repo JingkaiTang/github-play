17e9fc08e4c39b94a22780831a8e61fb7cf2435a
Jingkai Tang <jingkaitang@gmail.com>
2016-06-09 09:50:03 +0800

    add: say man/world/problem_or_little_woman/call_year.py over large group

---
+++ man/world/problem_or_little_woman/call_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_hand_of_big_person(str_arg):
+    time_or_government(str_arg)
+    print('tell_bad_person')
+
+def time_or_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_hand_of_big_person('thing')
