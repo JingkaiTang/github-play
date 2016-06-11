a80ef2f2a2e598c2ffd7e4621cc51de75801c72b
Jingkai Tang <jingkaitang@gmail.com>
2016-06-11 13:01:30 +0800

    add: call different_part_or_problem/man.py beneath small woman

---
+++ different_part_or_problem/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem_or_same_hand(str_arg):
+    place(str_arg)
+    print('do_person_above_group')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem_or_same_hand('child')
