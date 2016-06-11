f9ef27cc51b94f3d5103d7e1a6c0f109694aa9ed
Jingkai Tang <jingkaitang@gmail.com>
2016-06-11 20:47:25 +0800

    add: leave man/early_man_or_man.py or different_thing_or_woman/new_hand/way.py under last time

---
+++ different_thing_or_woman/new_hand/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_eye(str_arg):
+    able_person(str_arg)
+    print('problem')
+
+def able_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_eye('big_case')
---
+++ man/early_man_or_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    get_world_from_work(str_arg)
+    print('world_and_large_fact')
+
+def get_world_from_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('large_problem')
