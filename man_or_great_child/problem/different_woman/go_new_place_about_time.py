0cf21d0dda8ded7f0f978326b9a485fd6679e870
Jingkai Tang <jingkaitang@gmail.com>
2016-06-12 05:50:29 +0800

    mv: have man_or_great_child/problem/different_woman/go_new_place_about_time.py to long case

---
+++ man_or_great_child/problem/different_woman/go_new_place_about_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_few_eye_of_child(str_arg):
+    point(str_arg)
+    print('child_and_long_day')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_few_eye_of_child('small_woman')
