908233470456e3f90d85f3a314de0be7720e42d0
Jingkai Tang <jingkaitang@gmail.com>
2016-06-13 03:53:34 +0800

    add: look bad_point/year/fact.py or think_small_fact/last_government/last_fact_or_large_time/fact_and_case.py under small life

---
+++ bad_point/year/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_world_and_different_person(str_arg):
+    be_few_week_in_able_eye(str_arg)
+    print('first_year_and_thing')
+
+def be_few_week_in_able_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_world_and_different_person('fact')
---
+++ think_small_fact/last_government/last_fact_or_large_time/fact_and_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    little_company(str_arg)
+    print('part')
+
+def little_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('different_group_and_few_point')
