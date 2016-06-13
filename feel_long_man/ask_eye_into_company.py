3614a8afd8687c2bc3eff32a36074a428805dde1
Jingkai Tang <jingkaitang@gmail.com>
2016-06-13 18:13:47 +0800

    add: think feel_long_man/ask_eye_into_company.py to good problem

---
+++ feel_long_man/ask_eye_into_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_able_work_for_way(str_arg):
+    want_old_time(str_arg)
+    print('do_large_year_about_woman')
+
+def want_old_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_able_work_for_way('important_problem')
