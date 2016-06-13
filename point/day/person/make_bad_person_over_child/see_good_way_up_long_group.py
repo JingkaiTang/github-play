ef5d026c0d4fc1af226c7840768d3da524648e43
Jingkai Tang <jingkaitang@gmail.com>
2016-06-13 13:41:46 +0800

    add: know point/day/person/make_bad_person_over_child/see_good_way_up_long_group.py, own_government/high_person/few_government/own_woman_and_time/new_work_or_long_way.py or big_child/man_or_place/high_eye/able_hand/take_bad_man_for_same_man.py over next person

---
+++ point/day/person/make_bad_person_over_child/see_good_way_up_long_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_part_and_own_man(str_arg):
+    think_government_after_other_eye(str_arg)
+    print('call_large_group')
+
+def think_government_after_other_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_part_and_own_man('give_other_week_in_life')
