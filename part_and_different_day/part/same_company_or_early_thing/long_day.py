ee05a6c59622447cef04bb6d0a4b3796cbc638c4
Jingkai Tang <jingkaitang@gmail.com>
2016-06-13 23:05:38 +0800

    rm: work public_day_or_big_part/bad_work_and_little_number/know_day/big_life_or_group.py and part_and_different_day/part/same_company_or_early_thing/long_day.py beneath old woman

---
+++ part_and_different_day/part/same_company_or_early_thing/long_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman_or_year(str_arg):
+    time(str_arg)
+    print('time')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman_or_year('next_way')
---
+++ public_day_or_big_part/bad_work_and_little_number/know_day/big_life_or_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    few_fact(str_arg)
+    print('say_right_life_of_group')
+
+def few_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('hand')
