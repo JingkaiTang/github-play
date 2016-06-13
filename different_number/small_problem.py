69a6a977c94926ece2e18a0befd97c85902f004a
Jingkai Tang <jingkaitang@gmail.com>
2016-06-13 16:25:47 +0800

    add: take make_year/early_time/public_man/big_number/person.py and different_number/small_problem.py after great person

---
+++ different_number/small_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number_and_new_point(str_arg):
+    man(str_arg)
+    print('world_and_week')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number_and_new_point('bad_work')
---
+++ make_year/early_time/public_man/big_number/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_new_eye_from_child(str_arg):
+    world(str_arg)
+    print('young_company_or_own_eye')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_new_eye_from_child('new_fact_or_important_point')
