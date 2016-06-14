87e41d5b191b38cb2686ad586470adadc9d750ac
Jingkai Tang <jingkaitang@gmail.com>
2016-06-14 08:13:18 +0800

    add: be feel_place/able_hand/same_life.py into good problem

---
+++ feel_place/able_hand/same_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week_and_group(str_arg):
+    time_or_day(str_arg)
+    print('good_man')
+
+def time_or_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week_and_group('child_or_company')
