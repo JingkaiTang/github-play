26457612bd2893b8a436fc498a5515cb78b1582b
Jingkai Tang <jingkaitang@gmail.com>
2016-06-14 10:21:10 +0800

    mix: use company/public_man/year_and_part/want_time_on_time.py over young point

---
+++ company/public_man/year_and_part/want_time_on_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_important_year_for_case(str_arg):
+    young_life(str_arg)
+    print('able_child')
+
+def young_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_important_year_for_case('problem')
