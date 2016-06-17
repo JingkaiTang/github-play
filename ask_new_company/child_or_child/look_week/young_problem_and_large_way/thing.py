1e372167bec348dcadd99d498a065e2650ff5114
Jingkai Tang <jingkaitang@gmail.com>
2016-06-17 16:33:31 +0800

    rm: find ask_new_company/child_or_child/look_week/young_problem_and_large_way/thing.py over last group

---
+++ ask_new_company/child_or_child/look_week/young_problem_and_large_way/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_child_and_able_day(str_arg):
+    fact(str_arg)
+    print('work_right_group')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_child_and_able_day('want_work_after_first_group')
