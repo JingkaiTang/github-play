04bfbc0a9c171779f3241e1a197cafb28564c977
Jingkai Tang <jingkaitang@gmail.com>
2016-06-17 14:13:03 +0800

    add: try come_way/eye.py and different_person/work/other_child.py in bad world

---
+++ come_way/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_same_company(str_arg):
+    great_fact(str_arg)
+    print('make_young_week')
+
+def great_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_same_company('few_eye')
---
+++ different_person/work/other_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_small_government(str_arg):
+    child(str_arg)
+    print('seem_great_year')
+
+def child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_small_government('few_government_and_bad_fact')
