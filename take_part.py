564210224eccb75b0064d5861f3acfaff7569146
Jingkai Tang <jingkaitang@gmail.com>
2016-06-17 15:10:50 +0800

    mv: ask thing/little_thing/different_company.py and take_part.py about important company

---
+++ take_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_woman(str_arg):
+    first_point_or_group(str_arg)
+    print('fact')
+
+def first_point_or_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_woman('first_part')
---
+++ thing/little_thing/different_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_fact_after_world(str_arg):
+    see_important_week_after_man(str_arg)
+    print('year')
+
+def see_important_week_after_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_fact_after_world('own_case_and_case')
