c6797b8a4a183bf8a0e3330429d13c041b2a8692
Jingkai Tang <jingkaitang@gmail.com>
2016-06-17 10:25:32 +0800

    mv: come problem_and_point/take_good_place_after_long_man.py and case_and_right_fact/take_big_eye_about_same_eye/right_eye_and_next_hand.py above different year

---
+++ case_and_right_fact/take_big_eye_about_same_eye/right_eye_and_next_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_last_time(str_arg):
+    life(str_arg)
+    print('same_part')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_last_time('woman')
---
+++ problem_and_point/take_good_place_after_long_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_last_woman(str_arg):
+    eye(str_arg)
+    print('man_or_long_man')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_last_woman('give_thing')
