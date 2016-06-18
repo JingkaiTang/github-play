77c8104371840fc670ffe5d219c5fc84a43d3ace
Jingkai Tang <jingkaitang@gmail.com>
2016-06-18 08:57:33 +0800

    mod: call seem_problem_of_number/number/time.py into new way

---
+++ seem_problem_of_number/number/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_big_group(str_arg):
+    be_young_person(str_arg)
+    print('seem_last_person_up_same_man')
+
+def be_young_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_big_group('group_or_day')
