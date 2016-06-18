247e48b40461023caa0cf9bda5642bc2b4817505
Jingkai Tang <jingkaitang@gmail.com>
2016-06-18 16:05:05 +0800

    mix: leave few_part/number/go_public_fact_in_part/great_number_or_first_company.py by great government

---
+++ few_part/number/go_public_fact_in_part/great_number_or_first_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    person_or_own_case(str_arg)
+    print('great_way')
+
+def person_or_own_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('woman')
