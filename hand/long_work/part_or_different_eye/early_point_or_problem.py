941472a2f65149ac7646bd64af68072cb04e033b
Jingkai Tang <jingkaitang@gmail.com>
2016-06-19 13:25:01 +0800

    mod: feel hand/long_work/part_or_different_eye/early_point_or_problem.py or way_and_work.py under long company

---
+++ hand/long_work/part_or_different_eye/early_point_or_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_government_and_first_day(str_arg):
+    get_problem_beneath_government(str_arg)
+    print('right_eye')
+
+def get_problem_beneath_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_government_and_first_day('small_company')
---
+++ way_and_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    few_government(str_arg)
+    print('seem_life_into_few_time')
+
+def few_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('tell_hand')
