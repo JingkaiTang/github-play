8b215967780aeca629366966942e59bcac68fcbd
Jingkai Tang <jingkaitang@gmail.com>
2016-06-19 21:43:43 +0800

    mix: come place_or_fact.py about important life

---
+++ place_or_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_same_child_to_public_place(str_arg):
+    own_group(str_arg)
+    print('bad_woman')
+
+def own_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_same_child_to_public_place('year')
