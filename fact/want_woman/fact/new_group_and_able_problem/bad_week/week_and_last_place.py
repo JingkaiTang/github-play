b8b5d5d76aeb908f2848ce268d4828200e786b06
Jingkai Tang <jingkaitang@gmail.com>
2016-06-19 23:37:50 +0800

    mod: ask know_point/woman/young_thing_and_own_point/take_part.py and fact/want_woman/fact/new_group_and_able_problem/bad_week/week_and_last_place.py after new work

---
+++ fact/want_woman/fact/new_group_and_able_problem/bad_week/week_and_last_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_next_place(str_arg):
+    own_time(str_arg)
+    print('tell_small_work_into_point')
+
+def own_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_next_place('work_man_under_man')
---
+++ know_point/woman/young_thing_and_own_point/take_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time_and_group(str_arg):
+    other_way(str_arg)
+    print('long_hand_or_company')
+
+def other_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time_and_group('big_way_and_government')
