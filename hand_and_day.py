21d163e6188a78a2deedd0124ccd5d0643e2a01f
Jingkai Tang <jingkaitang@gmail.com>
2016-06-19 16:36:35 +0800

    mv: call long_point/want_large_fact_from_great_hand.py or hand_and_day.py over able person

---
+++ hand_and_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman_and_week(str_arg):
+    good_time_or_problem(str_arg)
+    print('same_work')
+
+def good_time_or_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman_and_week('child')
---
+++ long_point/want_large_fact_from_great_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    old_year(str_arg)
+    print('government')
+
+def old_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('ask_part')
