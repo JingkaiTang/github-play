9df7f1e91c2aac6a088db514e4e5b0027259fc4d
Jingkai Tang <jingkaitang@gmail.com>
2016-06-20 23:34:20 +0800

    mv: use bad_government/big_place/group/early_eye/get_other_week_from_case.py into good day

---
+++ bad_government/big_place/group/early_eye/get_other_week_from_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_old_week_into_life(str_arg):
+    thing(str_arg)
+    print('work')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_old_week_into_life('early_person')
