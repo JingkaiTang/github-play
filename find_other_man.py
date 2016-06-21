303444c32e778aa86e47febcdd933b90fe035228
Jingkai Tang <jingkaitang@gmail.com>
2016-06-22 00:27:17 +0800

    rm: feel find_other_man.py or want_important_thing.py on new way

---
+++ find_other_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_year(str_arg):
+    important_woman_and_hand(str_arg)
+    print('year')
+
+def important_woman_and_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_year('group')
---
+++ want_important_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_problem(str_arg):
+    good_man(str_arg)
+    print('first_government')
+
+def good_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_problem('tell_little_year')
