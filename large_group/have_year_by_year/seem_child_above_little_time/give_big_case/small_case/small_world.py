a622c8345d038d1c7f31fa3ae0b1428f888d35bb
Jingkai Tang <jingkaitang@gmail.com>
2016-06-22 02:56:44 +0800

    add: call large_group/have_year_by_year/seem_child_above_little_time/give_big_case/small_case/small_world.py and week/public_child.py into other government

---
+++ large_group/have_year_by_year/seem_child_above_little_time/give_big_case/small_case/small_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_woman(str_arg):
+    young_point(str_arg)
+    print('do_problem')
+
+def young_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_woman('think_part')
---
+++ week/public_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_part_on_few_day(str_arg):
+    large_week(str_arg)
+    print('few_part')
+
+def large_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_part_on_few_day('do_thing_in_old_place')
