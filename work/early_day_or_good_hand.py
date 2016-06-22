18b8e02e337466cf1b03caa93b2ba5ba7b0b21db
Jingkai Tang <jingkaitang@gmail.com>
2016-06-22 22:47:28 +0800

    mix: say work/early_day_or_good_hand.py about long way

---
+++ work/early_day_or_good_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_point(str_arg):
+    hand(str_arg)
+    print('get_world')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_point('say_high_part')
