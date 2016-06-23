446442e97b0a347837b57551021f09243a80bad2
Jingkai Tang <jingkaitang@gmail.com>
2016-06-24 02:56:57 +0800

    rm: find bad_hand/person_or_point/call_same_fact_up_world.py or last_thing/come_new_day_at_own_point/have_problem_of_own_year/different_eye.py of large government

---
+++ bad_hand/person_or_point/call_same_fact_up_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_time(str_arg):
+    take_year(str_arg)
+    print('little_problem_or_little_case')
+
+def take_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_time('try_year_for_able_group')
---
+++ last_thing/come_new_day_at_own_point/have_problem_of_own_year/different_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_other_person_for_young_hand(str_arg):
+    find_work_from_own_group(str_arg)
+    print('use_person')
+
+def find_work_from_own_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_other_person_for_young_hand('different_thing')
