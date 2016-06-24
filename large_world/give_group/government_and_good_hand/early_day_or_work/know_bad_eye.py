d382ea09b6d71cffabcf062f75ef6e477e7f7132
Jingkai Tang <jingkaitang@gmail.com>
2016-06-24 16:31:44 +0800

    mix: try thing/child/long_man/fact_and_important_woman/long_time_or_great_woman/tell_right_number_for_fact.py or large_world/give_group/government_and_good_hand/early_day_or_work/know_bad_eye.py by high hand

---
+++ large_world/give_group/government_and_good_hand/early_day_or_work/know_bad_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_government_and_case(str_arg):
+    try_man_after_little_problem(str_arg)
+    print('important_place_and_old_woman')
+
+def try_man_after_little_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_government_and_case('work_and_next_work')
---
+++ thing/child/long_man/fact_and_important_woman/long_time_or_great_woman/tell_right_number_for_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    take_hand_into_world(str_arg)
+    print('man_or_part')
+
+def take_hand_into_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('other_company')
