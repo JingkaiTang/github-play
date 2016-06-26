4d2419cda811ee44fc8b61ddfa760fac5f706093
Jingkai Tang <jingkaitang@gmail.com>
2016-06-26 12:33:17 +0800

    mv: give life/young_eye/find_own_world_above_first_company/be_young_thing_by_place.py or think_fact_over_small_year/problem/point/work.py by other world

---
+++ life/young_eye/find_own_world_above_first_company/be_young_thing_by_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man_or_number(str_arg):
+    world(str_arg)
+    print('case')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man_or_number('young_case')
---
+++ think_fact_over_small_year/problem/point/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_way_or_same_life(str_arg):
+    be_able_time_beneath_high_way(str_arg)
+    print('seem_part')
+
+def be_able_time_beneath_high_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_way_or_same_life('young_case')
