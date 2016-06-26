866e4e119033fac9c6e2cdc9a683dc6f95006115
Jingkai Tang <jingkaitang@gmail.com>
2016-06-26 18:43:08 +0800

    mv: know great_way.py for little year

--- great_way.py
+++ great_way.py
@@ -1,12 +1,12 @@
 
 #! /usr/bin/env python
 
-def own_way_or_work(str_arg):
-    get_large_work(str_arg)
-    print('know_good_point')
+def time(str_arg):
+    come_young_world(str_arg)
+    print('case')
 
-def get_large_work(str_arg):
+def come_young_world(str_arg):
     print(str_arg)
 
 if __name__ == '__main__':
-    own_way_or_work('know_right_child_beneath_man')
+    time('problem')
