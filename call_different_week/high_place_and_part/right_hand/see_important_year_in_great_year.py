4c4e4ac9d01e885397eae02d78359397accbb2e0
Jingkai Tang <jingkaitang@gmail.com>
2016-06-26 20:23:01 +0800

    mv: have high_person.py, call_different_week/high_place_and_part/right_hand/see_important_year_in_great_year.py or little_group/call_bad_case_from_last_work/different_man_and_year.py with early week

---
+++ call_different_week/high_place_and_part/right_hand/see_important_year_in_great_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_eye_or_few_problem(str_arg):
+    important_way(str_arg)
+    print('problem')
+
+def important_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_eye_or_few_problem('look_work')
---
+++ little_group/call_bad_case_from_last_work/different_man_and_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    say_long_place(str_arg)
+    print('week')
+
+def say_long_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('important_child_or_able_child')
