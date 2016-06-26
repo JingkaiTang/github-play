4042951fbdb4e2b41c088889127bf34a217711c4
Jingkai Tang <jingkaitang@gmail.com>
2016-06-26 08:53:45 +0800

    mix: think ask_able_place.py at long week

---
+++ ask_able_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_man_or_few_eye(str_arg):
+    little_person(str_arg)
+    print('woman')
+
+def little_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_man_or_few_eye('do_woman')
