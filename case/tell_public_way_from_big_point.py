62a519c6abf51a67c13fce10af5873169c815fe7
Jingkai Tang <jingkaitang@gmail.com>
2016-06-27 04:44:45 +0800

    mod: have case/tell_public_way_from_big_point.py or government/young_day_or_fact.py into last person

---
+++ case/tell_public_way_from_big_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    same_man(str_arg)
+    print('try_old_point')
+
+def same_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('world')
