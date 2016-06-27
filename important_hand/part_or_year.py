ee3a1230302b09b4c92b3626af7de9cd589e4c51
Jingkai Tang <jingkaitang@gmail.com>
2016-06-27 17:07:33 +0800

    rm: be important_hand/part_or_year.py under next year

---
+++ important_hand/part_or_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_government_or_point(str_arg):
+    first_time(str_arg)
+    print('make_week')
+
+def first_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_government_or_point('time')
