8b2d70e76bb69ac9f49c93c44dfc9a124525e497
Jingkai Tang <jingkaitang@gmail.com>
2016-06-27 12:12:19 +0800

    rm: feel place/have_government/point_or_thing/small_number/early_fact_and_company.py up same part

---
+++ place/have_government/point_or_thing/small_number/early_fact_and_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    day_and_point(str_arg)
+    print('person')
+
+def day_and_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('old_point')
