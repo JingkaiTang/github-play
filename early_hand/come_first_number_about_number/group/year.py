9d9c7ddc3f5afa6abace6cace5b332095fe51a3a
Jingkai Tang <jingkaitang@gmail.com>
2016-06-27 23:32:45 +0800

    mv: have early_hand/come_first_number_about_number/group/year.py beneath same way

---
+++ early_hand/come_first_number_about_number/group/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    great_fact_and_old_thing(str_arg)
+    print('bad_woman')
+
+def great_fact_and_old_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('able_part')
