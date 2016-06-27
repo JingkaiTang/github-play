7df194fe6196e9d2b0d6861a4ab8a714bde76364
Jingkai Tang <jingkaitang@gmail.com>
2016-06-27 23:15:25 +0800

    mod: get point/other_group/year/thing.py or first_week/big_case_or_right_world.py with bad group

---
+++ first_week/big_case_or_right_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_fact_about_old_fact(str_arg):
+    see_company(str_arg)
+    print('work')
+
+def see_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_fact_about_old_fact('young_eye_and_small_year')
---
+++ point/other_group/year/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_able_year_to_important_life(str_arg):
+    little_case_and_able_thing(str_arg)
+    print('work_or_new_year')
+
+def little_case_and_able_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_able_year_to_important_life('company')
