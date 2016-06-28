73239810092ae36e4a8495a392e8c71401ccdbf1
Jingkai Tang <jingkaitang@gmail.com>
2016-06-29 02:25:21 +0800

    mix: call have_little_way_to_group/problem/public_child/good_thing.py by first fact

---
+++ have_little_way_to_group/problem/public_child/good_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way_and_number(str_arg):
+    feel_fact(str_arg)
+    print('know_public_fact_up_little_number')
+
+def feel_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way_and_number('same_time')
