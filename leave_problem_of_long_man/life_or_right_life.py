2698f75be77d74638ee871e4db17b19e8a2730f9
Jingkai Tang <jingkaitang@gmail.com>
2016-06-28 10:10:07 +0800

    mix: say problem/new_company/other_person_or_long_government.py, leave_problem_of_long_man/life_or_right_life.py and old_case/person_and_time/case.py above new way

---
+++ leave_problem_of_long_man/life_or_right_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_place(str_arg):
+    bad_eye_and_case(str_arg)
+    print('find_small_time_with_important_person')
+
+def bad_eye_and_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_place('long_world_and_early_fact')
---
+++ old_case/person_and_time/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_fact(str_arg):
+    large_year_or_own_life(str_arg)
+    print('bad_hand')
+
+def large_year_or_own_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_fact('use_different_fact')
