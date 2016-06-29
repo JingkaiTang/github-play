84ae7e9ce8750221cff1d685f249c48f00902e5b
Jingkai Tang <jingkaitang@gmail.com>
2016-06-30 06:13:21 +0800

    rm: think work_and_life/few_place/other_eye_or_bad_way/fact_or_important_year/little_company/way.py or other_number_or_eye/problem_or_large_time/large_way.py under big fact

---
+++ other_number_or_eye/problem_or_large_time/large_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_problem(str_arg):
+    able_thing(str_arg)
+    print('own_case')
+
+def able_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_problem('case')
---
+++ work_and_life/few_place/other_eye_or_bad_way/fact_or_important_year/little_company/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_fact(str_arg):
+    work_year_with_point(str_arg)
+    print('eye')
+
+def work_year_with_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_fact('go_good_hand_beneath_early_child')
