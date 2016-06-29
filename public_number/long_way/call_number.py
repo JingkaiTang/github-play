73aa57116b0f754ef2b3c5b1be2c3d02703ac8e0
Jingkai Tang <jingkaitang@gmail.com>
2016-06-30 05:39:15 +0800

    add: work public_number/long_way/call_number.py and year_and_part/small_problem_and_child/seem_thing/number_or_same_company/life_and_long_part/number_or_large_thing.py by early part

---
+++ public_number/long_way/call_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    come_eye_about_bad_problem(str_arg)
+    print('big_life')
+
+def come_eye_about_bad_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('other_part')
---
+++ year_and_part/small_problem_and_child/seem_thing/number_or_same_company/life_and_long_part/number_or_large_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_person(str_arg):
+    government_and_life(str_arg)
+    print('first_man')
+
+def government_and_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_person('different_man_and_last_way')
