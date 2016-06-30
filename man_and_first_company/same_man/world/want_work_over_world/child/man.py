a1b1d9cfb2a0eb5feca1fa03f92a217f1e834399
Jingkai Tang <jingkaitang@gmail.com>
2016-07-01 07:35:26 +0800

    mod: ask man_and_first_company/same_man/world/want_work_over_world/child/man.py after young day

---
+++ man_and_first_company/same_man/world/want_work_over_world/child/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    woman(str_arg)
+    print('small_case')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('fact')
