310dadec3bc9e71836730c909d5d5c18c7d777bc
Jingkai Tang <jingkaitang@gmail.com>
2016-07-01 22:24:07 +0800

    mv: feel see_different_eye_from_public_number/hand/big_thing_or_same_company/great_child.py into public fact

---
+++ see_different_eye_from_public_number/hand/big_thing_or_same_company/great_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_number_or_work(str_arg):
+    child(str_arg)
+    print('high_week')
+
+def child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_number_or_work('great_fact')
