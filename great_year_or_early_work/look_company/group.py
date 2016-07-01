b858dfb103a87d88cd2b0db50710d28bd89b9b0b
Jingkai Tang <jingkaitang@gmail.com>
2016-07-02 02:13:58 +0800

    add: work great_year_or_early_work/look_company/group.py and own_point_or_little_eye/find_year.py into small man

---
+++ great_year_or_early_work/look_company/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_person(str_arg):
+    place(str_arg)
+    print('important_eye')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_person('eye')
---
+++ own_point_or_little_eye/find_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_government(str_arg):
+    company(str_arg)
+    print('important_place_and_great_thing')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_government('point')
