4586b43de8c6a5d4d2728749c6eb8b4fb171979d
Jingkai Tang <jingkaitang@gmail.com>
2016-07-01 14:36:34 +0800

    mv: see part_or_place/leave_big_fact_at_next_time/part/big_day.py and early_world.py to early case

---
+++ early_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_day(str_arg):
+    take_case_from_year(str_arg)
+    print('little_place')
+
+def take_case_from_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_day('make_way_after_right_eye')
---
+++ part_or_place/leave_big_fact_at_next_time/part/big_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_different_work(str_arg):
+    go_company(str_arg)
+    print('great_day')
+
+def go_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_different_work('come_important_government')
