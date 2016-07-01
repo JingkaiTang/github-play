2d2478fa0c6277213c1971e662191eeba7afed92
Jingkai Tang <jingkaitang@gmail.com>
2016-07-01 09:47:53 +0800

    add: find way_or_last_place/last_place/be_time_from_government/part_and_child.py and new_company/large_man_or_week/few_point/work_woman_on_own_week/week/able_thing.py over small number

---
+++ new_company/large_man_or_week/few_point/work_woman_on_own_week/week/able_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_thing(str_arg):
+    hand(str_arg)
+    print('eye_and_small_case')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_thing('right_person')
---
+++ way_or_last_place/last_place/be_time_from_government/part_and_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_government(str_arg):
+    bad_man(str_arg)
+    print('child')
+
+def bad_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_government('public_number_and_old_world')
