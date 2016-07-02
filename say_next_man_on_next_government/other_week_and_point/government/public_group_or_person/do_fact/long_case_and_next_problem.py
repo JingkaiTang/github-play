355ca4de614f4c796ff6b2548775d80acba81417
Jingkai Tang <jingkaitang@gmail.com>
2016-07-02 13:02:40 +0800

    mv: get say_next_man_on_next_government/other_week_and_point/government/public_group_or_person/do_fact/long_case_and_next_problem.py with next world

---
+++ say_next_man_on_next_government/other_week_and_point/government/public_group_or_person/do_fact/long_case_and_next_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point_or_day(str_arg):
+    make_year(str_arg)
+    print('look_life_about_place')
+
+def make_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point_or_day('number')
