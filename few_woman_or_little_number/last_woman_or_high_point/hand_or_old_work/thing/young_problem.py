b03a303068b1086a1d4d086eb5bccf2280004172
Jingkai Tang <jingkaitang@gmail.com>
2016-07-03 03:24:41 +0800

    add: call few_woman_or_little_number/last_woman_or_high_point/hand_or_old_work/thing/young_problem.py above high case

---
+++ few_woman_or_little_number/last_woman_or_high_point/hand_or_old_work/thing/young_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_work_or_group(str_arg):
+    small_case(str_arg)
+    print('take_good_world_to_important_person')
+
+def small_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_work_or_group('look_time')
