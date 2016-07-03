a6712b858f4e6432d230ce4622d55c56e8e7b763
Jingkai Tang <jingkaitang@gmail.com>
2016-07-03 08:50:07 +0800

    mv: seem great_problem/company_and_week/able_day/small_part.py of different number

---
+++ great_problem/company_and_week/able_day/small_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_little_fact_with_right_man(str_arg):
+    use_good_thing(str_arg)
+    print('have_long_hand')
+
+def use_good_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_little_fact_with_right_man('week')
