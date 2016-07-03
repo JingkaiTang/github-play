27f8793d3270fe24cfa7f2a4ef3e7d07222392aa
Jingkai Tang <jingkaitang@gmail.com>
2016-07-03 12:49:16 +0800

    mv: seem make_point/company_and_different_group.py from few way

---
+++ make_point/company_and_different_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_thing(str_arg):
+    first_case_and_other_child(str_arg)
+    print('first_year_and_woman')
+
+def first_case_and_other_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_thing('eye_or_world')
