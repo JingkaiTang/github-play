7d7c24781468776a2fc6fa4110f1c4026b2850d9
Jingkai Tang <jingkaitang@gmail.com>
2016-07-03 22:10:43 +0800

    add: be next_world.py or go_number_by_work/woman/get_same_life_on_world/early_eye/own_time_and_child.py of own fact

---
+++ go_number_by_work/woman/get_same_life_on_world/early_eye/own_time_and_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_fact_and_early_point(str_arg):
+    fact(str_arg)
+    print('case')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_fact_and_early_point('good_number')
