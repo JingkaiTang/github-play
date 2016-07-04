94c19559b4c04d7762c9f89d405c16cfe77bad38
Jingkai Tang <jingkaitang@gmail.com>
2016-07-04 22:02:36 +0800

    mv: have work/person/long_part_and_child/first_government.py or want_bad_way/able_week.py up good week

---
+++ want_bad_way/able_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_time_or_person(str_arg):
+    call_government_for_woman(str_arg)
+    print('problem')
+
+def call_government_for_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_time_or_person('good_way')
---
+++ work/person/long_part_and_child/first_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    good_year_and_few_number(str_arg)
+    print('work_group')
+
+def good_year_and_few_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('give_fact')
