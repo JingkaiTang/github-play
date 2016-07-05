264c3af99acaadf2f16d99a481c53fd64efdc327
Jingkai Tang <jingkaitang@gmail.com>
2016-07-06 04:46:45 +0800

    mv: give large_day_or_part/go_small_number/small_person/same_woman/early_time.py and woman/case/new_year.py for next fact

---
+++ large_day_or_part/go_small_number/small_person/same_woman/early_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    right_year(str_arg)
+    print('public_day')
+
+def right_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('young_case')
