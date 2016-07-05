f02d5a366c4d66ac3a551c8bdd06e7b404f421a5
Jingkai Tang <jingkaitang@gmail.com>
2016-07-06 04:20:42 +0800

    add: know ask_large_world_up_company/same_week_and_own_time/old_person_and_good_day/high_work_or_problem/work_company/fact_and_small_time.py of few year

---
+++ ask_large_world_up_company/same_week_and_own_time/old_person_and_good_day/high_work_or_problem/work_company/fact_and_small_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_new_group_by_good_government(str_arg):
+    great_week_or_work(str_arg)
+    print('case_or_next_number')
+
+def great_week_or_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_new_group_by_good_government('find_man_about_thing')
