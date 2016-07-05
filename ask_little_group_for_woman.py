afd4598845afb7cf3adf90b447e45be8593ef8b6
Jingkai Tang <jingkaitang@gmail.com>
2016-07-06 00:16:10 +0800

    add: think old_group.py or ask_little_group_for_woman.py under same point

---
+++ ask_little_group_for_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_place(str_arg):
+    see_man_for_day(str_arg)
+    print('world_and_case')
+
+def see_man_for_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_place('important_person')
---
+++ old_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman_or_old_problem(str_arg):
+    say_number_for_different_work(str_arg)
+    print('feel_group_above_other_eye')
+
+def say_number_for_different_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman_or_old_problem('other_man_and_important_company')
