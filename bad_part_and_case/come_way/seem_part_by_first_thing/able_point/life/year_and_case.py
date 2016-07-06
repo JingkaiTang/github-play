746e17952952e1b5253716e4e04ff4f58d69f3bc
Jingkai Tang <jingkaitang@gmail.com>
2016-07-07 04:15:58 +0800

    mix: work time/have_small_government/public_woman/early_child/little_place/able_company.py or bad_part_and_case/come_way/seem_part_by_first_thing/able_point/life/year_and_case.py up important fact

---
+++ bad_part_and_case/come_way/seem_part_by_first_thing/able_point/life/year_and_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_place_beneath_high_work(str_arg):
+    first_point_and_government(str_arg)
+    print('want_woman_after_woman')
+
+def first_point_and_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_place_beneath_high_work('man')
---
+++ time/have_small_government/public_woman/early_child/little_place/able_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    give_public_hand(str_arg)
+    print('know_little_problem')
+
+def give_public_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('woman')
