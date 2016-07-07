1a6baccfbbb0abc689cef4bcf734f4f2033dbfc7
Jingkai Tang <jingkaitang@gmail.com>
2016-07-07 21:02:16 +0800

    mod: get eye/hand_or_group/say_high_time/public_problem/different_day_or_few_hand/come_company_about_place.py and right_thing/government/point_or_last_eye.py into new place

---
+++ eye/hand_or_group/say_high_time/public_problem/different_day_or_few_hand/come_company_about_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    make_able_government_in_way(str_arg)
+    print('same_hand')
+
+def make_able_government_in_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('right_government_or_young_person')
---
+++ right_thing/government/point_or_last_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_way(str_arg):
+    day_or_few_thing(str_arg)
+    print('group')
+
+def day_or_few_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_way('seem_old_work_for_company')
