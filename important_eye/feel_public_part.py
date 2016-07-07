f20f8363ada1416d611d5966363e22a472ecb8f3
Jingkai Tang <jingkaitang@gmail.com>
2016-07-07 15:31:15 +0800

    mix: make important_eye/feel_public_part.py and new_thing/say_able_way_above_great_child/large_world/same_number_or_large_thing/tell_last_hand/day.py up first woman

---
+++ important_eye/feel_public_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_person_or_place(str_arg):
+    get_other_company(str_arg)
+    print('get_last_company')
+
+def get_other_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_person_or_place('work_number_for_old_government')
---
+++ new_thing/say_able_way_above_great_child/large_world/same_number_or_large_thing/tell_last_hand/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    good_person(str_arg)
+    print('fact')
+
+def good_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('come_own_company_for_first_thing')
