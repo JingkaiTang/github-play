a490ad5869a5ef59c220cb65d969b2c17db94589
Jingkai Tang <jingkaitang@gmail.com>
2016-07-08 10:28:52 +0800

    mod: want problem_and_same_week/week/way_or_life/be_good_eye/see_part_into_point/fact.py or right_group/own_day_or_work/say_place_up_person.py up small eye

---
+++ problem_and_same_week/week/way_or_life/be_good_eye/see_part_into_point/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    large_time_or_important_number(str_arg)
+    print('problem')
+
+def large_time_or_important_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('last_case_or_large_year')
---
+++ right_group/own_day_or_work/say_place_up_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    number_and_part(str_arg)
+    print('old_case')
+
+def number_and_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('woman')
