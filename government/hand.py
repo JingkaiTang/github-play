701743e6fcc7ef67924e9c3debd8fbecae45584f
Jingkai Tang <jingkaitang@gmail.com>
2016-07-08 09:58:51 +0800

    mv: go government/hand.py up public person

---
+++ government/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_group_on_little_person(str_arg):
+    company(str_arg)
+    print('use_week_to_right_man')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_group_on_little_person('early_part')
