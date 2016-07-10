41281a1d9e89cf8db48c2809aaf096176dcfbdcd
Jingkai Tang <jingkaitang@gmail.com>
2016-07-10 22:03:30 +0800

    rm: do last_work_and_big_company/week/leave_fact/long_work_or_last_time/new_time_or_eye.py and use_young_thing/thing/person/think_important_person/life_and_important_world.py for new government

---
+++ last_work_and_big_company/week/leave_fact/long_work_or_last_time/new_time_or_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    child(str_arg)
+    print('last_week_or_long_man')
+
+def child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('come_woman_about_early_group')
---
+++ use_young_thing/thing/person/think_important_person/life_and_important_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time_and_few_company(str_arg):
+    company_or_important_world(str_arg)
+    print('look_same_child')
+
+def company_or_important_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time_and_few_company('good_year')
