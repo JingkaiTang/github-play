b3c42b45d5da8b9691de1b6345cdbb1c316be5aa
Jingkai Tang <jingkaitang@gmail.com>
2016-07-11 16:19:00 +0800

    mix: see same_place/own_number/bad_way_or_year/different_week_or_able_work.py after different part

---
+++ same_place/own_number/bad_way_or_year/different_week_or_able_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    find_world_by_case(str_arg)
+    print('point_and_little_problem')
+
+def find_world_by_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('last_day')
