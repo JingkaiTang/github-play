59065174d74ccca38fcdb5230f29bde3a150218b
Jingkai Tang <jingkaitang@gmail.com>
2016-07-13 07:38:35 +0800

    rm: work do_group_beneath_few_point.py or first_day/think_long_time.py in small part

---
+++ do_group_beneath_few_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_eye_and_other_life(str_arg):
+    do_high_company(str_arg)
+    print('go_bad_world_into_large_hand')
+
+def do_high_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_eye_and_other_life('know_fact_of_little_way')
---
+++ first_day/think_long_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_way_or_same_woman(str_arg):
+    try_way_at_world(str_arg)
+    print('time')
+
+def try_way_at_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_way_or_same_woman('have_high_life')
