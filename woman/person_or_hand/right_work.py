f07f3f8364619623d7f52f14c9acd611176dc0a1
Jingkai Tang <jingkaitang@gmail.com>
2016-07-13 01:09:56 +0800

    mod: feel woman/person_or_hand/right_work.py under new year

---
+++ woman/person_or_hand/right_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_time(str_arg):
+    thing_and_eye(str_arg)
+    print('world')
+
+def thing_and_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_time('make_thing_over_eye')
