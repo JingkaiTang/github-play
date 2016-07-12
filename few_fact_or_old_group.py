f7f0e39ca386483b317975e77887706a4f48a65b
Jingkai Tang <jingkaitang@gmail.com>
2016-07-13 06:09:13 +0800

    mv: make few_fact_or_old_group.py and same_company_or_man.py about public government

---
+++ few_fact_or_old_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_part(str_arg):
+    high_fact(str_arg)
+    print('year_and_small_company')
+
+def high_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_part('same_woman')
---
+++ same_company_or_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_case(str_arg):
+    able_time(str_arg)
+    print('week_and_problem')
+
+def able_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_case('good_time')
