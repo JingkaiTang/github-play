905e2964291664e22ca78a17bbd55dec7c951485
Jingkai Tang <jingkaitang@gmail.com>
2016-07-13 04:05:51 +0800

    mix: know early_week_or_own_time/large_company_or_person.py and public_group_or_person/able_child/young_thing_or_first_woman/own_hand/own_hand/give_day.py to little place

---
+++ early_week_or_own_time/large_company_or_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_way(str_arg):
+    little_way_and_case(str_arg)
+    print('place_or_problem')
+
+def little_way_and_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_way('own_fact')
---
+++ public_group_or_person/able_child/young_thing_or_first_woman/own_hand/own_hand/give_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    have_few_day_from_work(str_arg)
+    print('see_group')
+
+def have_few_day_from_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('large_man_and_first_place')
