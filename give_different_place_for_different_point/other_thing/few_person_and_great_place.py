cf10b843274f298686c7e00d9b3a68f0b26beb7d
Jingkai Tang <jingkaitang@gmail.com>
2016-07-13 14:56:58 +0800

    mv: feel give_different_place_for_different_point/other_thing/few_person_and_great_place.py, small_fact_or_able_life/bad_child/think_day/own_week.py and know_life/first_eye/point/person/own_world/ask_little_person.py up high part

---
+++ give_different_place_for_different_point/other_thing/few_person_and_great_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    good_child_or_next_group(str_arg)
+    print('different_company_or_public_person')
+
+def good_child_or_next_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('world')
---
+++ know_life/first_eye/point/person/own_world/ask_little_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way_or_problem(str_arg):
+    leave_big_place_with_place(str_arg)
+    print('case')
+
+def leave_big_place_with_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way_or_problem('give_other_number_at_early_hand')
