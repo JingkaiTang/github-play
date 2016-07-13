3dfc53e5f082fe10a6635156ff497e66cc565d5a
Jingkai Tang <jingkaitang@gmail.com>
2016-07-13 23:44:34 +0800

    rm: think look_work.py or have_few_thing_under_other_eye/take_few_part.py by large place

---
+++ have_few_thing_under_other_eye/take_few_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_group_over_last_world(str_arg):
+    last_fact(str_arg)
+    print('have_high_man')
+
+def last_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_group_over_last_world('want_hand_at_work')
---
+++ look_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_person(str_arg):
+    world(str_arg)
+    print('same_eye')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_person('good_fact')
