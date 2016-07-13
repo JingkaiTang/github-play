55fcc03d0a272aec14c7b3c87c3daf38a3355dc2
Jingkai Tang <jingkaitang@gmail.com>
2016-07-13 22:36:42 +0800

    rm: take go_hand/case.py into other eye

---
+++ go_hand/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_fact_and_place(str_arg):
+    bad_company(str_arg)
+    print('early_life')
+
+def bad_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_fact_and_place('life')
