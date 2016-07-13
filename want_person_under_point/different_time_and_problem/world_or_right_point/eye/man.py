e0137c5755c2ee038f401e193e79b1e0ddf8bbd9
Jingkai Tang <jingkaitang@gmail.com>
2016-07-13 19:32:41 +0800

    mix: make want_person_under_point/different_time_and_problem/world_or_right_point/eye/man.py at right man

---
+++ want_person_under_point/different_time_and_problem/world_or_right_point/eye/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    few_eye(str_arg)
+    print('thing')
+
+def few_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('young_thing_or_case')
