029f94a58afbad2bc1211b6fc23a1d64061de0cf
Jingkai Tang <jingkaitang@gmail.com>
2016-07-14 17:41:55 +0800

    mix: do case/large_man/day.py and way_or_great_person/few_case/company.py up big child

---
+++ case/large_man/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_year(str_arg):
+    first_time(str_arg)
+    print('new_man')
+
+def first_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_year('way')
---
+++ way_or_great_person/few_case/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_problem_and_big_woman(str_arg):
+    come_day_after_point(str_arg)
+    print('first_world')
+
+def come_day_after_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_problem_and_big_woman('next_fact_and_public_hand')
