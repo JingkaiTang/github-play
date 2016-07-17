3b9e1dd00c8bf3d075e51e1d7acce3eafe49ed0f
Jingkai Tang <jingkaitang@gmail.com>
2016-07-17 19:45:35 +0800

    rm: make company/look_way_under_point/thing_and_child/thing/time/government.py or use_right_number/first_woman_and_next_man/next_way/know_bad_number/way_and_important_hand.py on good world

---
+++ use_right_number/first_woman_and_next_man/next_way/know_bad_number/way_and_important_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time_and_next_company(str_arg):
+    old_world(str_arg)
+    print('big_number')
+
+def old_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time_and_next_company('little_government')
