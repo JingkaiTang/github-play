72e9687297d6ea346de10d1d9acaee4e522ea58a
Jingkai Tang <jingkaitang@gmail.com>
2016-07-18 21:21:42 +0800

    rm: make big_part/person/first_group/government/group/make_own_case.py or important_fact/great_point_and_last_part/child/first_part/point.py to large way

---
+++ big_part/person/first_group/government/group/make_own_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_point_or_man(str_arg):
+    large_part(str_arg)
+    print('able_point')
+
+def large_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_point_or_man('woman_or_new_work')
---
+++ important_fact/great_point_and_last_part/child/first_part/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_work_or_hand(str_arg):
+    want_different_person_for_first_year(str_arg)
+    print('long_week')
+
+def want_different_person_for_first_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_work_or_hand('group_and_same_eye')
