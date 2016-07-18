ab50f853f2e9c94ce83e95ce68c6bc115fcd5d61
Jingkai Tang <jingkaitang@gmail.com>
2016-07-19 03:08:11 +0800

    mix: say other_eye/woman/fact/find_public_man/own_hand/part_or_group.py in big man

---
+++ other_eye/woman/fact/find_public_man/own_hand/part_or_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_group_and_day(str_arg):
+    next_part_and_work(str_arg)
+    print('man_or_fact')
+
+def next_part_and_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_group_and_day('big_number')
