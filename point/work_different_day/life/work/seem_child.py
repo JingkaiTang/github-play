655902caa819708f1fd74e04c47cfb77d478e0ca
Jingkai Tang <jingkaitang@gmail.com>
2016-07-19 11:12:29 +0800

    rm: seem point/work_different_day/life/work/seem_child.py of high person

---
+++ point/work_different_day/life/work/seem_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_time(str_arg):
+    way_or_man(str_arg)
+    print('few_part')
+
+def way_or_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_time('person_and_work')
