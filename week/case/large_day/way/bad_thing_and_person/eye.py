61868efe8c933bd47c3bcc6577b653553eb04deb
Jingkai Tang <jingkaitang@gmail.com>
2016-07-20 22:15:52 +0800

    add: call week/case/large_day/way/bad_thing_and_person/eye.py by last hand

---
+++ week/case/large_day/way/bad_thing_and_person/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time_or_old_fact(str_arg):
+    life(str_arg)
+    print('small_company')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time_or_old_fact('say_high_person_up_thing')
