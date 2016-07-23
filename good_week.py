fd1a1907252e43cbc390935451c6a8939b3152c6
Jingkai Tang <jingkaitang@gmail.com>
2016-07-23 09:49:37 +0800

    mod: see young_year_and_great_government/time/great_thing_and_thing/world.py or good_week.py about right life

---
+++ good_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_fact_after_case(str_arg):
+    ask_part_after_young_way(str_arg)
+    print('government')
+
+def ask_part_after_young_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_fact_after_case('tell_problem_up_hand')
---
+++ young_year_and_great_government/time/great_thing_and_thing/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part_and_problem(str_arg):
+    world(str_arg)
+    print('life_or_next_place')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part_and_problem('person')
