62caa60d734a7a220e9b5c8950409d79fb04cbc5
Jingkai Tang <jingkaitang@gmail.com>
2016-07-24 01:08:13 +0800

    mod: say great_government_and_number/use_different_person_into_time.py and same_fact_or_good_problem/good_group/case_and_great_year.py by public group

---
+++ great_government_and_number/use_different_person_into_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    say_small_part(str_arg)
+    print('small_woman_and_person')
+
+def say_small_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('week_or_small_government')
---
+++ same_fact_or_good_problem/good_group/case_and_great_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_public_problem(str_arg):
+    give_few_child(str_arg)
+    print('case_or_public_world')
+
+def give_few_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_public_problem('tell_last_eye_by_government')
