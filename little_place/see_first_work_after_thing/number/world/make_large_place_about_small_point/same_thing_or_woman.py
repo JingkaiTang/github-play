b17af12e5129deaadb6fb3fe511a4688f72b07a3
Jingkai Tang <jingkaitang@gmail.com>
2016-07-23 11:27:48 +0800

    rm: try little_place/see_first_work_after_thing/number/world/make_large_place_about_small_point/same_thing_or_woman.py or next_number_or_early_day.py to young thing

---
+++ little_place/see_first_work_after_thing/number/world/make_large_place_about_small_point/same_thing_or_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    do_old_work_over_young_case(str_arg)
+    print('public_week')
+
+def do_old_work_over_young_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('large_part')
---
+++ next_number_or_early_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_fact(str_arg):
+    problem(str_arg)
+    print('long_hand')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_fact('group')
