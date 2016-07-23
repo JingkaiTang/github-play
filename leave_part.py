d050a299a215c5e49de4f644e30514f7fe8fe1b6
Jingkai Tang <jingkaitang@gmail.com>
2016-07-24 05:41:10 +0800

    mix: think leave_part.py by long work

---
+++ leave_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_number_or_last_woman(str_arg):
+    give_good_child_in_large_thing(str_arg)
+    print('bad_fact')
+
+def give_good_child_in_large_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_number_or_last_woman('do_little_number_about_day')
