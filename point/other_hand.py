dc6387d4cb7b6fc93b8816827ab2936e5b67aa73
Jingkai Tang <jingkaitang@gmail.com>
2016-07-23 15:15:25 +0800

    rm: take point/other_hand.py with able case

---
+++ point/other_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_group(str_arg):
+    young_time(str_arg)
+    print('week_and_man')
+
+def young_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_group('part')
