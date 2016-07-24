a62f8fc0ad06738e171313398680800887455634
Jingkai Tang <jingkaitang@gmail.com>
2016-07-24 22:43:02 +0800

    rm: tell fact_or_life/next_government_or_great_fact/number.py or point/life_and_last_group/old_number/big_government/few_time/year.py to important place

---
+++ fact_or_life/next_government_or_great_fact/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_place(str_arg):
+    different_week(str_arg)
+    print('woman')
+
+def different_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_place('few_government')
---
+++ point/life_and_last_group/old_number/big_government/few_time/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    man_or_way(str_arg)
+    print('week')
+
+def man_or_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('part_or_problem')
