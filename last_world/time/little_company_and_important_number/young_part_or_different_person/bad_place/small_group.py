dc7fe3e8a086b05ddaf1bfbe614482eee8398664
Jingkai Tang <jingkaitang@gmail.com>
2016-07-24 16:20:19 +0800

    mv: see last_world/time/little_company_and_important_number/young_part_or_different_person/bad_place/small_group.py and person/public_person.py from large year

---
+++ last_world/time/little_company_and_important_number/young_part_or_different_person/bad_place/small_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_work(str_arg):
+    work_person_up_fact(str_arg)
+    print('week')
+
+def work_person_up_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_work('look_able_child_to_problem')
---
+++ person/public_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_case(str_arg):
+    high_man(str_arg)
+    print('come_new_thing')
+
+def high_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_case('company')
