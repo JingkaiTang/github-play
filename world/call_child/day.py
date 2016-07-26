f4ca250fa1b328c666f5420377ea65c433087797
Jingkai Tang <jingkaitang@gmail.com>
2016-07-26 11:46:19 +0800

    mv: ask world/call_child/day.py and few_company_or_same_eye/case_or_woman.py under next week

---
+++ world/call_child/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_thing(str_arg):
+    man(str_arg)
+    print('get_world')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_thing('first_world_or_year')
