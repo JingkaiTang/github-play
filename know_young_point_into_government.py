9080f57985b6b1449effb4bb4bd41e49d643feab
Jingkai Tang <jingkaitang@gmail.com>
2016-07-28 22:25:50 +0800

    mv: see know_young_point_into_government.py into right eye

---
+++ know_young_point_into_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place_and_early_world(str_arg):
+    first_work(str_arg)
+    print('great_number')
+
+def first_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place_and_early_world('long_life')
