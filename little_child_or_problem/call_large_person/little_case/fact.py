19ae4f95fbc63b0c268c7ac17b6b44ce927256c9
Jingkai Tang <jingkaitang@gmail.com>
2016-07-29 13:22:44 +0800

    mod: try little_child_or_problem/call_large_person/little_case/fact.py into different year

---
+++ little_child_or_problem/call_large_person/little_case/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    young_group_and_bad_number(str_arg)
+    print('other_day')
+
+def young_group_and_bad_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('early_problem')
