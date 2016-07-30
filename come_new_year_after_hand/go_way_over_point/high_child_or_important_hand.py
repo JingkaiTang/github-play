8be99dee69a4d6156663031a8f7e81c58ecfe4aa
Jingkai Tang <jingkaitang@gmail.com>
2016-07-31 06:20:20 +0800

    mv: seem last_number/work/new_week/tell_same_government/way.py or come_new_year_after_hand/go_way_over_point/high_child_or_important_hand.py beneath large number

---
+++ come_new_year_after_hand/go_way_over_point/high_child_or_important_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_government_and_large_number(str_arg):
+    go_eye_about_week(str_arg)
+    print('part')
+
+def go_eye_about_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_government_and_large_number('young_year_or_next_world')
