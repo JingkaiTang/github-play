b9b8ee45b67061a21b346bbd99950f560a4364aa
Jingkai Tang <jingkaitang@gmail.com>
2016-07-30 12:22:22 +0800

    add: feel give_year.py or old_fact/next_group_and_few_life.py into public week

---
+++ old_fact/next_group_and_few_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    group(str_arg)
+    print('point')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('little_fact')
