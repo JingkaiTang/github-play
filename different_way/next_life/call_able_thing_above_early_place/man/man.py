05130c20dba8791447c68e9980087443d2c84f6f
Jingkai Tang <jingkaitang@gmail.com>
2016-07-31 19:07:14 +0800

    mod: look be_right_hand_about_important_work/point/case_or_world/high_way_and_important_eye/different_point.py, right_hand/large_life.py or different_way/next_life/call_able_thing_above_early_place/man/man.py for high place

---
+++ different_way/next_life/call_able_thing_above_early_place/man/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_life(str_arg):
+    tell_week_to_week(str_arg)
+    print('public_government_and_important_case')
+
+def tell_week_to_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_life('feel_small_man')
---
+++ right_hand/large_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_year_or_day(str_arg):
+    time_and_world(str_arg)
+    print('able_world')
+
+def time_and_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_year_or_day('tell_problem')
