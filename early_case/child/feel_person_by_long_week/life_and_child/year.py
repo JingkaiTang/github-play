fa9ba0d6ceeeb0a96849d052ae84539d508906a3
Jingkai Tang <jingkaitang@gmail.com>
2016-07-31 22:21:01 +0800

    mod: know early_case/child/feel_person_by_long_week/life_and_child/year.py of different fact

---
+++ early_case/child/feel_person_by_long_week/life_and_child/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_eye_or_great_fact(str_arg):
+    problem(str_arg)
+    print('different_hand_and_little_number')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_eye_or_great_fact('hand')
