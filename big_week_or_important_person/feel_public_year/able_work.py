6b69588abc347710821185b80ce1ac7f100ed288
Jingkai Tang <jingkaitang@gmail.com>
2016-08-01 21:27:25 +0800

    rm: tell day_and_work/eye/seem_company/government/place.py or big_week_or_important_person/feel_public_year/able_work.py up early week

---
+++ big_week_or_important_person/feel_public_year/able_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_eye_and_world(str_arg):
+    good_thing(str_arg)
+    print('long_group')
+
+def good_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_eye_and_world('life')
---
+++ day_and_work/eye/seem_company/government/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    place(str_arg)
+    print('world')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('government_and_early_work')
