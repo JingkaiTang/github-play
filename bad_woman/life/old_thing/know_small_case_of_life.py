e08b5114ba215804de37c0670796548334964f7e
Jingkai Tang <jingkaitang@gmail.com>
2016-08-02 02:29:01 +0800

    mix: work bad_woman/life/old_thing/know_small_case_of_life.py with good hand

---
+++ bad_woman/life/old_thing/know_small_case_of_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_eye(str_arg):
+    long_man_or_little_problem(str_arg)
+    print('world')
+
+def long_man_or_little_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_eye('case')
