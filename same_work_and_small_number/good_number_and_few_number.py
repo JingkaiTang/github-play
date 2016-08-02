ec266b63b9091288a4abec9e366ef825796b754e
Jingkai Tang <jingkaitang@gmail.com>
2016-08-02 12:24:55 +0800

    mod: find same_work_and_small_number/good_number_and_few_number.py or think_first_year/get_few_government/eye/take_part/eye.py for last group

---
+++ same_work_and_small_number/good_number_and_few_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_old_company_to_little_hand(str_arg):
+    other_hand(str_arg)
+    print('week')
+
+def other_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_old_company_to_little_hand('day')
---
+++ think_first_year/get_few_government/eye/take_part/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_case(str_arg):
+    look_week(str_arg)
+    print('company')
+
+def look_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_case('ask_last_day_by_early_child')
