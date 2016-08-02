719ca74dbc7eca1ed9b8c82c05dcf93abf7481c9
Jingkai Tang <jingkaitang@gmail.com>
2016-08-02 14:53:08 +0800

    rm: find hand/new_week.py from little fact

---
+++ hand/new_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_company_and_child(str_arg):
+    other_child(str_arg)
+    print('small_woman')
+
+def other_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_company_and_child('early_person')
