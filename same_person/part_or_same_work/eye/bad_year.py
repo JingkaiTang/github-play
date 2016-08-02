5ed2f02cbdcdf2b2d964771aa2f3f60c6e23c833
Jingkai Tang <jingkaitang@gmail.com>
2016-08-02 08:00:57 +0800

    mix: leave same_person/part_or_same_work/eye/bad_year.py over next life

---
+++ same_person/part_or_same_work/eye/bad_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    number(str_arg)
+    print('find_bad_group')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('feel_different_way_with_important_case')
