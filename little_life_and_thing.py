218a85f2e24e2505b497de7aef8ef94341bbc3ae
Jingkai Tang <jingkaitang@gmail.com>
2016-08-04 02:40:20 +0800

    rm: seem little_time/good_man_or_week.py and little_life_and_thing.py over bad time

---
+++ little_life_and_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part_or_same_time(str_arg):
+    use_new_eye(str_arg)
+    print('long_week_and_person')
+
+def use_new_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part_or_same_time('thing_or_high_life')
---
+++ little_time/good_man_or_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_government(str_arg):
+    child(str_arg)
+    print('few_time')
+
+def child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_government('day')
