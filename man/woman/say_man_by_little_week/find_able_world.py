66072f9264031d14b3d408db995000f9e325dd49
Jingkai Tang <jingkaitang@gmail.com>
2016-08-04 05:52:13 +0800

    rm: want man/woman/say_man_by_little_week/find_able_world.py in bad life

---
+++ man/woman/say_man_by_little_week/find_able_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_government(str_arg):
+    child(str_arg)
+    print('feel_good_way')
+
+def child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_government('do_little_thing')
