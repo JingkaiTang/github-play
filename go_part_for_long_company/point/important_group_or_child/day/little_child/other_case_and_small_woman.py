e1f48aeb6b5af53ec49314776b3ab2e87a825752
Jingkai Tang <jingkaitang@gmail.com>
2016-08-03 15:33:47 +0800

    mix: want go_part_for_long_company/point/important_group_or_child/day/little_child/other_case_and_small_woman.py or work_person/old_person/bad_hand/want_person.py to large place

---
+++ go_part_for_long_company/point/important_group_or_child/day/little_child/other_case_and_small_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number_and_long_year(str_arg):
+    bad_case(str_arg)
+    print('high_case')
+
+def bad_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number_and_long_year('great_work')
---
+++ work_person/old_person/bad_hand/want_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_person(str_arg):
+    try_thing(str_arg)
+    print('important_place_and_important_world')
+
+def try_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_person('big_eye')
