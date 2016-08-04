e5929f9e29702771551804bcc91bc692f37f7848
Jingkai Tang <jingkaitang@gmail.com>
2016-08-04 11:22:57 +0800

    add: see bad_group/number/way_and_important_number.py beneath few problem

---
+++ bad_group/number/way_and_important_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    right_company(str_arg)
+    print('way_or_last_thing')
+
+def right_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('do_year_in_large_week')
