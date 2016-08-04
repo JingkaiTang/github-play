eec6240f6cdf0bb32b054333163da0b2a9fc2c7b
Jingkai Tang <jingkaitang@gmail.com>
2016-08-05 02:39:35 +0800

    mix: want know_few_company/take_good_work_under_little_work/high_government_or_child/be_able_problem_at_next_case/have_man.py by different thing

---
+++ know_few_company/take_good_work_under_little_work/high_government_or_child/be_able_problem_at_next_case/have_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_right_group_about_big_week(str_arg):
+    week(str_arg)
+    print('first_hand_or_own_part')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_right_group_about_big_week('company')
