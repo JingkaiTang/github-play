6e0aeb6cb44b10d8238748e328534a9add2ca15c
Jingkai Tang <jingkaitang@gmail.com>
2016-08-04 23:48:04 +0800

    rm: make call_high_place/first_thing_or_big_government/same_hand.py and next_group/fact/early_man_or_woman/work.py from important year

---
+++ call_high_place/first_thing_or_big_government/same_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_fact(str_arg):
+    problem_and_big_world(str_arg)
+    print('same_point')
+
+def problem_and_big_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_fact('fact')
---
+++ next_group/fact/early_man_or_woman/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_own_world(str_arg):
+    government(str_arg)
+    print('life')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_own_world('make_small_fact_beneath_big_thing')
