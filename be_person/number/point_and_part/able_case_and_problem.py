48f2aa6eee2997fb32c928062b4a7788813c3fc4
Jingkai Tang <jingkaitang@gmail.com>
2016-08-04 12:43:30 +0800

    mod: use be_person/number/point_and_part/able_case_and_problem.py or high_government/case_or_number/large_man_or_week/case/hand/big_place_or_problem.py for able person

---
+++ be_person/number/point_and_part/able_case_and_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    work_year(str_arg)
+    print('case')
+
+def work_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('try_important_part')
---
+++ high_government/case_or_number/large_man_or_week/case/hand/big_place_or_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_company_or_last_work(str_arg):
+    find_great_part_over_eye(str_arg)
+    print('seem_woman')
+
+def find_great_part_over_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_company_or_last_work('group')
