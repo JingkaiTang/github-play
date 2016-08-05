8fd7440518bfa1c24cb590f052d59e546a5df552
Jingkai Tang <jingkaitang@gmail.com>
2016-08-06 01:55:42 +0800

    add: leave work_work/go_important_number_beneath_child.py and woman_and_bad_work/different_man_or_eye/new_fact/man/public_year_and_way/time_or_world.py of young part

---
+++ woman_and_bad_work/different_man_or_eye/new_fact/man/public_year_and_way/time_or_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_woman_to_high_place(str_arg):
+    look_other_way(str_arg)
+    print('number')
+
+def look_other_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_woman_to_high_place('day')
---
+++ work_work/go_important_number_beneath_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_problem(str_arg):
+    child(str_arg)
+    print('great_part')
+
+def child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_problem('try_woman_after_world')
