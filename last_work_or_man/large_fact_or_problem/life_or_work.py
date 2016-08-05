32a02ad86fdd4db8ee7c5eb695ec864a75f3e238
Jingkai Tang <jingkaitang@gmail.com>
2016-08-06 01:57:26 +0800

    mix: give see_good_life.py and last_work_or_man/large_fact_or_problem/life_or_work.py about same number

---
+++ last_work_or_man/large_fact_or_problem/life_or_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    old_woman(str_arg)
+    print('week')
+
+def old_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('child_and_hand')
---
+++ see_good_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    work_little_work(str_arg)
+    print('come_case_up_old_world')
+
+def work_little_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('thing')
