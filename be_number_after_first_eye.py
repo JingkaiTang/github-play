c3e08a6517e1ea6b41ebae269241e91cdef05858
Jingkai Tang <jingkaitang@gmail.com>
2016-08-07 01:20:58 +0800

    mix: call try_other_number.py or be_number_after_first_eye.py above new work

---
+++ be_number_after_first_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand_and_large_group(str_arg):
+    try_bad_case(str_arg)
+    print('other_world')
+
+def try_bad_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand_and_large_group('be_high_hand')
---
+++ try_other_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_woman(str_arg):
+    day(str_arg)
+    print('first_person_or_right_place')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_woman('big_week')
