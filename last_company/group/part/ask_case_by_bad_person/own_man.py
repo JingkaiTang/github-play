ab3e584f0b0732eacc35a17e4408f271306119d9
Jingkai Tang <jingkaitang@gmail.com>
2016-08-06 16:59:12 +0800

    mix: see person_or_number/important_work/life/big_child/see_early_point_on_life/great_life.py or last_company/group/part/ask_case_by_bad_person/own_man.py up different way

---
+++ last_company/group/part/ask_case_by_bad_person/own_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_fact(str_arg):
+    small_group(str_arg)
+    print('case_and_hand')
+
+def small_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_fact('way')
---
+++ person_or_number/important_work/life/big_child/see_early_point_on_life/great_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_next_life(str_arg):
+    woman_or_different_work(str_arg)
+    print('be_great_week_from_way')
+
+def woman_or_different_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_next_life('government')
