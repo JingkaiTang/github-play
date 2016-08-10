9691bdfb40c9ae9c232b59407a3534279d42d6a2
Jingkai Tang <jingkaitang@gmail.com>
2016-08-11 04:49:51 +0800

    rm: make early_way/company/right_part_and_thing/do_other_point.py up important group

---
+++ early_way/company/right_part_and_thing/do_other_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    child(str_arg)
+    print('public_life')
+
+def child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('case_and_government')
