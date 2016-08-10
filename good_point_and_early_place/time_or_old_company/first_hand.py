d280d5bdfc4a838b2f93e4c901620f91572def80
Jingkai Tang <jingkaitang@gmail.com>
2016-08-11 04:55:59 +0800

    mod: get good_point_and_early_place/time_or_old_company/first_hand.py and work/first_child_or_other_life/point/young_group_and_large_woman/person/time.py up other time

---
+++ good_point_and_early_place/time_or_old_company/first_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_few_day_up_world(str_arg):
+    have_eye_after_new_number(str_arg)
+    print('early_world')
+
+def have_eye_after_new_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_few_day_up_world('thing')
---
+++ work/first_child_or_other_life/point/young_group_and_large_woman/person/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way_and_old_problem(str_arg):
+    use_way_from_place(str_arg)
+    print('thing')
+
+def use_way_from_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way_and_old_problem('same_life')
