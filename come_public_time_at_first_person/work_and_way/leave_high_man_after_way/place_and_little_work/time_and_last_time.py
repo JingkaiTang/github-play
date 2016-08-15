247ff6363e747187cafd003d27102259af7a447f
Jingkai Tang <jingkaitang@gmail.com>
2016-08-15 15:17:20 +0800

    mv: know come_public_time_at_first_person/work_and_way/leave_high_man_after_way/place_and_little_work/time_and_last_time.py or little_eye/work.py on bad eye

---
+++ come_public_time_at_first_person/work_and_way/leave_high_man_after_way/place_and_little_work/time_and_last_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_company(str_arg):
+    week_or_child(str_arg)
+    print('company')
+
+def week_or_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_company('want_last_place')
---
+++ little_eye/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_case_by_young_child(str_arg):
+    day_or_early_government(str_arg)
+    print('bad_point')
+
+def day_or_early_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_case_by_young_child('come_hand')
