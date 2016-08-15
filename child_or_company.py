46a857be90afeb6c944f736518d567e9ec46cc62
Jingkai Tang <jingkaitang@gmail.com>
2016-08-15 16:13:13 +0800

    add: go child_or_company.py or next_government/feel_company_with_life/different_number_and_big_company.py above good thing

---
+++ child_or_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_high_week(str_arg):
+    part_or_case(str_arg)
+    print('thing')
+
+def part_or_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_high_week('want_large_life_in_number')
