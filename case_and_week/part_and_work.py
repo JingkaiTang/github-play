0bb01c0fe648a74095416ac4a1f1fffb62f62406
Jingkai Tang <jingkaitang@gmail.com>
2016-08-16 07:35:12 +0800

    mod: think first_child_or_point/big_hand/group/call_able_work.py, young_hand/know_own_time/give_time/number.py or case_and_week/part_and_work.py over first child

---
+++ case_and_week/part_and_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_problem(str_arg):
+    eye(str_arg)
+    print('feel_life')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_problem('time')
---
+++ young_hand/know_own_time/give_time/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing_or_same_week(str_arg):
+    first_group(str_arg)
+    print('go_number_beneath_thing')
+
+def first_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing_or_same_week('do_case')
