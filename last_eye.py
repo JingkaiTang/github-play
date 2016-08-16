02dfe0fcd58e1363669c38ea1198ef7f9cfb2edd
Jingkai Tang <jingkaitang@gmail.com>
2016-08-16 16:30:48 +0800

    mv: leave last_eye.py and life/feel_work_at_other_eye.py of bad eye

---
+++ last_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_work(str_arg):
+    eye(str_arg)
+    print('life_or_world')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_work('ask_early_number_into_few_woman')
---
+++ life/feel_work_at_other_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand_or_person(str_arg):
+    come_own_government(str_arg)
+    print('year_or_hand')
+
+def come_own_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand_or_person('part_and_way')
