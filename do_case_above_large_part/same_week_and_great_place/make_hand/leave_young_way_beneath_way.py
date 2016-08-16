d8775ada1222b14bf04350b4f8beada47957a2eb
Jingkai Tang <jingkaitang@gmail.com>
2016-08-16 11:35:55 +0800

    mod: want do_case_above_large_part/same_week_and_great_place/make_hand/leave_young_way_beneath_way.py or little_number/place/right_week/place.py beneath small work

---
+++ do_case_above_large_part/same_week_and_great_place/make_hand/leave_young_way_beneath_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_point_and_part(str_arg):
+    old_world_and_little_part(str_arg)
+    print('use_new_day_from_year')
+
+def old_world_and_little_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_point_and_part('seem_government')
---
+++ little_number/place/right_week/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_work(str_arg):
+    big_government(str_arg)
+    print('bad_hand')
+
+def big_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_work('come_next_man')
