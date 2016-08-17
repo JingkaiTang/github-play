59664e4a0fd61e172f7cac618cba1db6bc3fb72c
Jingkai Tang <jingkaitang@gmail.com>
2016-08-18 06:06:47 +0800

    rm: see day_and_time/able_place.py with early eye

---
+++ day_and_time/able_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_eye(str_arg):
+    own_life_and_thing(str_arg)
+    print('good_day_and_person')
+
+def own_life_and_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_eye('company')
