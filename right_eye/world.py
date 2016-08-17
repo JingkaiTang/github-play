7019e852f895ffc843ad38c8732c1f5ed0f7ea88
Jingkai Tang <jingkaitang@gmail.com>
2016-08-17 11:31:12 +0800

    add: try right_eye/world.py or case_or_important_eye.py of large week

---
+++ right_eye/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company_and_little_group(str_arg):
+    find_small_woman_from_other_person(str_arg)
+    print('life')
+
+def find_small_woman_from_other_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company_and_little_group('world_or_bad_man')
