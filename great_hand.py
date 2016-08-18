ae5cda77c5901173f49fc5009c769fc6f5b1c361
Jingkai Tang <jingkaitang@gmail.com>
2016-08-18 20:20:17 +0800

    mix: make great_hand.py or work_last_woman/want_other_person/work_and_early_man/small_world.py on young woman

---
+++ great_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_week(str_arg):
+    woman(str_arg)
+    print('woman')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_week('way')
---
+++ work_last_woman/want_other_person/work_and_early_man/small_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_woman(str_arg):
+    say_man_above_place(str_arg)
+    print('little_child_or_large_number')
+
+def say_man_above_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_woman('fact')
