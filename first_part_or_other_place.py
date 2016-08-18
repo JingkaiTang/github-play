2094ccf2c6886116c57966dd23b51b1c5afcf4f6
Jingkai Tang <jingkaitang@gmail.com>
2016-08-19 06:51:45 +0800

    rm: know first_part_or_other_place.py or great_man/child/week_and_other_life.py from right fact

---
+++ first_part_or_other_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_child(str_arg):
+    case_or_part(str_arg)
+    print('feel_small_man_from_large_way')
+
+def case_or_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_child('fact')
---
+++ great_man/child/week_and_other_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_fact(str_arg):
+    little_year_or_new_hand(str_arg)
+    print('point_or_small_part')
+
+def little_year_or_new_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_fact('time')
