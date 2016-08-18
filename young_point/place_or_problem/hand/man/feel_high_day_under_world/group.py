8c4199ef230fae793967df5fdd93fed51c78b129
Jingkai Tang <jingkaitang@gmail.com>
2016-08-18 13:59:04 +0800

    mv: see young_point/place_or_problem/hand/man/feel_high_day_under_world/group.py of early work

---
+++ young_point/place_or_problem/hand/man/feel_high_day_under_world/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_way(str_arg):
+    number_or_last_part(str_arg)
+    print('want_large_way')
+
+def number_or_last_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_way('good_government_or_different_fact')
