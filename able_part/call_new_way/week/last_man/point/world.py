7430bfedca671801317311ad9fa219ac11baabe9
Jingkai Tang <jingkaitang@gmail.com>
2016-08-18 19:55:03 +0800

    mod: call able_part/call_new_way/week/last_man/point/world.py and make_good_part_to_man/woman_and_time.py under little person

---
+++ able_part/call_new_way/week/last_man/point/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_important_fact(str_arg):
+    number(str_arg)
+    print('government_or_problem')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_important_fact('work_and_woman')
---
+++ make_good_part_to_man/woman_and_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_place(str_arg):
+    little_company_and_same_problem(str_arg)
+    print('find_high_way_under_important_woman')
+
+def little_company_and_same_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_place('have_other_company_up_eye')
