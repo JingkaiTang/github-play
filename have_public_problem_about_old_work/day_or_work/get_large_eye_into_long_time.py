858d5633b48191535ae1c06b4409510269fa3d54
Jingkai Tang <jingkaitang@gmail.com>
2016-08-19 13:30:19 +0800

    mix: tell have_public_problem_about_old_work/day_or_work/get_large_eye_into_long_time.py over little way

---
+++ have_public_problem_about_old_work/day_or_work/get_large_eye_into_long_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    different_eye(str_arg)
+    print('have_hand_after_next_number')
+
+def different_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('group')
