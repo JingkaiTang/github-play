a2f781a4f1b1e02343153eeaa9a35a93137647a8
Jingkai Tang <jingkaitang@gmail.com>
2016-08-19 15:17:40 +0800

    mix: take good_world/life/eye_and_woman/early_work/child.py and last_life_or_point/first_hand_or_day/early_time/few_group/world/able_woman.py in first company

---
+++ last_life_or_point/first_hand_or_day/early_time/few_group/world/able_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_problem_or_child(str_arg):
+    young_problem_and_eye(str_arg)
+    print('little_fact')
+
+def young_problem_and_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_problem_or_child('make_large_problem')
