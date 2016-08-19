4d6cda652b7be62694da1a9fb9f5ec08048a753f
Jingkai Tang <jingkaitang@gmail.com>
2016-08-19 08:54:21 +0800

    rm: try person/want_year/number_and_case.py under old eye

---
+++ person/want_year/number_and_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time_or_thing(str_arg):
+    young_number(str_arg)
+    print('go_person')
+
+def young_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time_or_thing('work_and_few_world')
